e44c796577c10cd119a549b8dbf59123a4de79ed
posite <su84053@gmail.com>
2023-11-02 22:45:51 +0900

    feat: 출력을 위한 enum 작성

--- src/main/kotlin/lotto/constant/PrintText.kt
+++ src/main/kotlin/lotto/constant/PrintText.kt
@@ -4,5 +4,11 @@ enum class PrintText(val text: String) {
     REQUIRE_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PRINT_PURCHASE_AMOUNT("개를 구매했습니다."),
     REQUIRE_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    REQUIRE_BONUS_NUMBER("보너스 번호를 입력해 주세요.")
+    REQUIRE_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    SEPARATE_SAME_COUNT(" - "),
+    SEPARATE_LOTTES("\n"),
+    PRINT_RESULT("당첨 통계"),
+    SEPARATE_RESULT("---"),
+    PRINT_EARNING_RATE("총 수익률은 "),
+    PRINT_PERCENT("%입니다.")
 }
