7018e8323bc280097181b94c8646713897aedf78
okm1172 <okm1172@naver.com>
2023-11-02 20:20:18 +0900

    exception : 구입 금액 예외 처리

--- src/main/kotlin/lotto/domain/Purchase.kt
+++ src/main/kotlin/lotto/domain/Purchase.kt
@@ -1,8 +1,9 @@
 package lotto.domain
 
-import lotto.Input.inputAmount
+import lotto.Input
 import lotto.constant.Constant
 import lotto.constant.OutputMessage
+import lotto.constant.Exception
 
 class Purchase {
 
@@ -12,8 +13,13 @@ class Purchase {
     }
 
     fun getLottoCountFromAmount(): Int {
-        val amount = inputAmount().toInt()
+        val amount = Input.inputAmount().toInt()
+        checkValidationAmount(amount)
         return amount / Constant.PRICE_PER_LOTTO
     }
 
+    private fun checkValidationAmount(amount: Int) {
+        if(amount % 1000 != 0)
+            throw IllegalArgumentException(Exception.MESSAGE_EXCEPT_AMOUNT)
+    }
 }
