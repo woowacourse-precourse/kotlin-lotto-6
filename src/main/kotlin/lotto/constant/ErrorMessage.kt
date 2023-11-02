60d9cee79c72096d476109fa235a23bd625173cd
posite <su84053@gmail.com>
2023-11-02 22:46:36 +0900

    feat: 에러 및 기타 string을 위한 enum class 구현

--- src/main/kotlin/lotto/constant/ErrorMessage.kt
+++ src/main/kotlin/lotto/constant/ErrorMessage.kt
@@ -4,5 +4,10 @@ enum class ErrorMessage(val message: String) {
     NOT_NUMBER_PURCHASE_AMOUNT("[ERROR] 구매금액은 정수여야 합니다."),
     NOT_PURCHASE_AMOUNT_FORMAT("[ERROR] 구매금액은 1000원단위여야 합니다."),
     NOT_BONUS_NUMBER_FORMAT("[ERROR] 보너스 금액은 정수여야 합니다."),
-    NOT_NUMBER_WINNING_NUMBER("[ERROR] 당첨번호는 정수여야 합니다.")
+    NOT_NUMBER_WINNING_NUMBER("[ERROR] 당첨번호는 정수여야 합니다."),
+    NOT_SIX_WINNING_NUMBER("[ERROR] 당첨 번호는 6자리여야합니다."),
+    NOT_DUPLICATED_WINNING_NUMBER("[ERROR] 당첨 번호는 서로 다른 숫자여야합니다."),
+    NOT_NUMBER_RANGE_WINNING_NUMBER("[ERROR] 당첨 번호는 1에서 45 사이의 숫자여야합니다."),
+    NOT_NUMBER_RANGE_BONUS_NUMBER("[ERROR] 보너스 번호는 1에서 45 사이의 숫자여야합니다."),
+    NOT_DUPLICATED_BONUS_NUMBER("[ERROR] 보너스 번호는 서로 다른 숫자여야합니다.")
 }
