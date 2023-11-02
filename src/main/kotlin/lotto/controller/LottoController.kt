ade187ec78c8415fb2bdecc60a3741b400a30147
jaehan4707 <jahan4707@naver.com>
2023-11-03 01:07:14 +0900

    feat : 로또 번호 출력 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,15 +1,18 @@
 package lotto.controller
 
+import lotto.model.User
 import lotto.view.InputView
 import lotto.view.OutputView
 
 class LottoController(private val inputView: InputView = InputView(),private val outPutView : OutputView = OutputView()) {
 
+    private val user = User()
     fun run(){
         outPutView.printInputPrice()
         val price = inputView.inputPrice()
         outPutView.printBuyLotto(price)
-        //TODO 구입 금액으로 로또 생성
+        user.buyLotto(price)
+        outPutView.printUserLotto(user.lottoes)
         //TODO 당첨번호 입력
         //TODO 보너스 번호 입력
         //TODO 당첨 통계 구현
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,5 +1,6 @@
 package lotto.view
 
+import lotto.model.Lotto
 import lotto.util.Constant.INPUT_PRICE_MESSAGE
 import lotto.util.Constant.LOTTO_BUY_MESSAGE
 import lotto.util.Constant.UNIT_PRICE
@@ -7,5 +8,6 @@ import lotto.util.Constant.UNIT_PRICE
 class OutputView {
 
     fun printInputPrice() = println(INPUT_PRICE_MESSAGE)
-    fun printBuyLotto(price : Int) = println("\n${LOTTO_BUY_MESSAGE.format(price/ UNIT_PRICE)}")
+    fun printBuyLotto(price: Int) = println("\n${LOTTO_BUY_MESSAGE.format(price / UNIT_PRICE)}")
+    fun printUserLotto(lottoes: List<Lotto>) = lottoes.forEach { lotto -> println(lotto.getLottoNumbers()) }
 }
