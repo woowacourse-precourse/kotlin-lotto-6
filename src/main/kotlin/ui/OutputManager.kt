5a8b886edb92b53785b3f9b557e483da6e7c3583
jiwon2724 <stopone3119@naver.com>
2023-11-03 01:52:27 +0900

    [refactor] enum class 추가

--- src/main/kotlin/ui/OutputManager.kt
+++ src/main/kotlin/ui/OutputManager.kt
@@ -1,32 +1,32 @@
 package ui
 
 class OutputManager {
-    fun promptPurchaseAmount() = println(PROMPT_PURCHASE_AMOUNT)
-    fun invalidPurchaseAmount(): () -> Unit = { println(INVALID_PURCHASE_AMOUNT) }
-    fun invalidLottoNumbers(): () -> Unit = { println(INVALID_LOTTO_NUMBERS) }
-    fun purchaseLottoCount(lottoCount: Int) = println(PURCHASE_LOTTO_COUNT.format(lottoCount))
-    fun promptJackpotNumbers() = println(PROMPT_JACKPOT_NUMBERS)
-    fun promptBonusNumber() = println(PROMPT_BONUS_NUMBER)
-    fun lottoStats() = println(LOTTO_STATS)
-    fun line() = println(LINE)
+    fun promptPurchaseAmount() = println(OutputMessages.PROMPT_PURCHASE_AMOUNT)
+    fun invalidPurchaseAmount(): () -> Unit = { println(OutputMessages.INVALID_PURCHASE_AMOUNT) }
+    fun invalidLottoNumbers(): () -> Unit = { println(OutputMessages.INVALID_LOTTO_NUMBERS) }
+    fun purchaseLottoCount(lottoCount: Int) = println(OutputMessages.PURCHASE_LOTTO_COUNT.toString().format(lottoCount))
+    fun promptJackpotNumbers() = println(OutputMessages.PROMPT_JACKPOT_NUMBERS)
+    fun promptBonusNumber() = println(OutputMessages.PROMPT_BONUS_NUMBER)
+    fun lottoStats() = println(OutputMessages.LOTTO_STATS)
+    fun line() = println(OutputMessages.LINE)
     fun lottoResult(
         correctLottoNumbers: Int,
         winningAmount: String,
         winningCount: Int
-    ) = println(LOTTO_RESULT.format(correctLottoNumbers, winningAmount, winningCount))
+    ) = println(OutputMessages.LOTTO_RESULT.toString().format(correctLottoNumbers, winningAmount, winningCount))
+}
 
-    companion object {
-        private const val ERROR = "[ERROR]"
-        private const val INVALID_PURCHASE_AMOUNT = "$ERROR 유효하지 않은 금액입니다."
-        private const val INVALID_LOTTO_NUMBERS = "$ERROR 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+enum class OutputMessages(private val message: String) {
+    ERROR("[ERROR]"),
+    INVALID_PURCHASE_AMOUNT("$ERROR 유효하지 않은 금액입니다."),
+    INVALID_LOTTO_NUMBERS("$ERROR 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    PROMPT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    PROMPT_JACKPOT_NUMBERS("당첨 번호를 입력해 주세요."),
+    PROMPT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    PURCHASE_LOTTO_COUNT("%d개를 구매했습니다."),
+    LOTTO_RESULT("%d개 일치 (%s원) - %d개"),
+    LINE("---"),
+    LOTTO_STATS("당첨 통계");
 
-        private const val PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
-        private const val PROMPT_JACKPOT_NUMBERS = "당첨 번호를 입력해 주세요."
-        private const val PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
-        private const val PURCHASE_LOTTO_COUNT = "%d개를 구매했습니다."
-
-        private const val LOTTO_STATS = "당첨 통계"
-        private const val LINE = "---"
-        private const val LOTTO_RESULT = "%d개 일치 (%s원) - %d개"
-    }
+    override fun toString(): String = message
 }
