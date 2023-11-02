3ad524cedaf8847f3e98c28be627fbdb4701bd22
kimhm0728 <kimhm0728@naver.com>
2023-11-02 22:32:09 +0900

    feat: 당첨 번호 입력받는 기능 구현

--- src/main/kotlin/lotto/io/input/Input.kt
+++ src/main/kotlin/lotto/io/input/Input.kt
@@ -2,13 +2,21 @@ package lotto.io.input
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.model.Amount
+import lotto.model.Lotto
 
 class Input {
     private val validator = InputValidator()
+    private val converter = InputConverter()
 
-    fun inputPurchaseAmount(): Amount {
+    fun inputAmount(): Amount {
         val amount = Console.readLine()
         validator.checkAmount(amount)
         return Amount(amount.toInt())
+    }
+
+    fun inputWinningLotto(): Lotto {
+        val lottoWithComma = Console.readLine()
+        validator.checkWinningLotto(lottoWithComma)
+        return converter.convertLotto(lottoWithComma)
     }
 }
--- src/main/kotlin/lotto/io/input/InputValidator.kt
+++ src/main/kotlin/lotto/io/input/InputValidator.kt
@@ -8,12 +8,24 @@ class InputValidator {
         checkNotDigit(amount)
     }
 
+    fun checkWinningLotto(numbers: String) {
+        checkEmpty(numbers)
+        checkComma(numbers)
+        numbers.split(",").forEach { number -> checkNotDigit(number) }
+    }
+
     private fun checkEmpty(value: String) {
         require(value.isNotEmpty()) { Exception.EMPTY }
     }
 
     private fun checkNotDigit(value: String) {
         require(value.all { it.isDigit() }) { Exception.DIGIT }
+    }
+
+    private fun checkComma(value: String) {
+        require(!(value.startsWith(",") || value.endsWith(",") || value.contains(",,"))) {
+            Exception.COMMA
+        }
     }
 
     companion object {
