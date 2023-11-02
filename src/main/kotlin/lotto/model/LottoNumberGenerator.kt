778534633917c53026c32ea5e3e415f052ced61f
jaehan4707 <jahan4707@naver.com>
2023-11-03 01:04:43 +0900

    feat : 로또 번호 랜덤 생성기 추가

---
+++ src/main/kotlin/lotto/model/LottoNumberGenerator.kt
@@ -0,0 +1,11 @@
+package lotto.model
+
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.util.Constant.LOTTO_MAX_RANDOM_NUMBER
+import lotto.util.Constant.LOTTO_MIN_RANDOM_NUMBER
+import lotto.util.Constant.LOTTO_NUMBER_SIZE
+
+object LottoNumberGenerator {
+
+    fun makeLottoNumber(): List<Int> = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_RANDOM_NUMBER,LOTTO_MAX_RANDOM_NUMBER,LOTTO_NUMBER_SIZE)
+}
