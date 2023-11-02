44cb86aa439636015b9508878c20559bfd609440
Insu Kang <kin4496@naver.com>
2023-11-02 21:33:10 +0900

    feat: 1000 단위로 입력 받을 때 까지 입력 받는 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,26 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import kotlin.NumberFormatException
+
+const val PAYMENT_UNIT = 1000
 fun main() {
-    TODO("프로그램 구현")
+    var amount = getPayment()
+
+    while (amount != -1) {
+        amount = getPayment()
+    }
+}
+fun isPaymentValid(amount:Int) = (amount % PAYMENT_UNIT) == 0
+fun getPayment():Int
+{
+    var amount:Int
+    try {
+        amount = Console.readLine().toInt()
+        if(!isPaymentValid(amount)) throw IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.")
+    }catch (e:IllegalArgumentException)
+    {
+        return -1
+    }
+    return amount
 }
