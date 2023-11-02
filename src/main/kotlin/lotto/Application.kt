03cbb1414b6da60256e8d9f12ba857db42da2a06
Chankyu Lee <blueriz@naver.com>
2023-11-02 23:25:51 +0900

    당첨번호 받기, 통계 출력 함수 구현, 로또 번호 맞는 개수 세기 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,59 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
+enum class MatchCount() {
+    NONE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    FIVEBONUS,
+    ALL
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액으 입력해 주세요")
+    val money = readlnOrNull()
+    val price = 1000
+    val numoflotto = money?.toInt()?.div(price)
+    val mynumber = mutableListOf<List<Int>>()
+    val match = mutableListOf<MatchCount>()
+
+    for (i in 0 until numoflotto!!)
+    {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        mynumber.add(numbers)
+        Lotto(numbers).printnumbers()
+    }
+    println("당첨 번호를 입력해 주세요.")
+    val inputwinningnumber = readlnOrNull()
+    val winningnumber = inputwinningnumber?.split(",")?.map { it.toInt() }
+
+    println("보너스 번호를 입력해 주세요.")
+    val bonus = readlnOrNull()?.toInt()
+
+    for (i in 0 until numoflotto)
+    {
+        match.add(Lotto(mynumber[i]).checklotto(winningnumber, bonus))
+    }
+
+    printtotal(match)
+}
+
+fun printtotal(matchnum: List<MatchCount>) {
+    println("당첨 통계")
+    println("---")
+    val three = matchnum.count{it == MatchCount.THREE}
+    val four = matchnum.count{it == MatchCount.FOUR}
+    val five = matchnum.count{it == MatchCount.FIVE}
+    val bonus = matchnum.count{it == MatchCount.FIVEBONUS}
+    val all = matchnum.count{it == MatchCount.ALL}
+
+    println("3개 일치 (5,000원) - $three 개")
+    println("4개 일치 (50,000원) - $four 개")
+    println("5개 일치 (1,500,000원) - $five 개")
+    println("5개 일치, 보너스 볼 일치 (30,000,000원) - $bonus 개")
+    println("6개 일치 (2,000,000,000원) - $all 개")
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -9,4 +9,20 @@ class Lotto(private val numbers: List<Int>) {
         val nums = numbers.joinToString(", ")
         println("[$nums]")
     }
+
+    fun checklotto(winningnumbers: List<Int>, bonus: Int): MatchCount
+    {
+        val matchnumber = winningnumbers.intersect(numbers.toSet()).count()
+
+        return when (matchnumber) {
+            0 -> MatchCount.NONE
+            1 -> MatchCount.ONE
+            2 -> MatchCount.TWO
+            3 -> MatchCount.THREE
+            4 -> MatchCount.FOUR
+            5 -> if (bonus in winningnumbers) MatchCount.FIVEBONUS else MatchCount.FIVE
+            6 -> MatchCount.ALL
+            else -> throw IllegalArgumentException("유효한 숫자가 아닙니다.")
+        }
+    }
 }
