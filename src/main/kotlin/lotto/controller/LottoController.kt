89268de107cebcdd4fe6efc158d65bd8f65a4518
jaehan4707 <jahan4707@naver.com>
2023-11-02 23:54:46 +0900

    feat : LottoController TODO 작업 지정

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,12 @@
+package lotto.controller
+
+class LottoController() {
+
+    fun run(){
+        //TODO 구입 금액 입력
+        //TODO 구입 금액으로 로또 생성
+        //TODO 당첨번호 입력
+        //TODO 보너스 번호 입력
+        //TODO 당첨 통계 구현
+    }
+}
