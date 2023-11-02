887bf32208e7e55176da07dafedbbb968b8a183b
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-02 19:22:39 +0900

    feat:잘못된 금액 입력 시 다시 입력받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,6 +2,14 @@ package lotto
 import lottoView.LottoOutPut
 import lottoViewModel.ValidInput
 fun main() {
-    LottoOutPut()
-    ValidInput().validInputPurchase()
+    val lottoOutput = LottoOutPut()
+    val validInput=ValidInput()
+    val purchaseAmount=try{
+        validInput.validInputPurchase()
+    }catch (e:IllegalArgumentException){
+       println(e.message)
+        return main()
+    }
+    lottoOutput.purchaseDetailPrint(purchaseAmount)
 }
+
