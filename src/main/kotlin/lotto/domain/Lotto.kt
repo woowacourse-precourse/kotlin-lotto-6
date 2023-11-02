55ee520c0581af4a303f81ccb7becc8ba6c91695
posite <su84053@gmail.com>
2023-11-02 22:28:43 +0900

    feat: lotto번호의 비교를 위한 Lotto 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,14 +1,25 @@
-package lotto
+package lotto.domain
+
+import lotto.constant.ErrorMessage
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == WINNING_NUMBERS_SIZE)
+        require(numbers.size == WINNING_NUMBERS_SIZE) { ErrorMessage.NOT_SIX_WINNING_NUMBER.message }
     }
 
     fun toLottoNumbersResult(): String {
         return numbers.toString()
     }
 
+    fun compareWinningNumbers(winningNumbers: WinningNumbers): Int {
+        var sameWinningNumberCount = 0
+        winningNumbers.convertToInt().forEach {
+            if (numbers.contains(it)) sameWinningNumberCount++
+        }
+        return sameWinningNumberCount
+    }
+
+    fun compareBonusNumber(bonusNumber: Int): Int = if (numbers.contains(bonusNumber)) 1 else 0
 
     companion object {
         private const val WINNING_NUMBERS_SIZE = 6
