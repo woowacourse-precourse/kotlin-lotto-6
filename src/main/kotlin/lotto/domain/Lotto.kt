bb4b23490af61704ee645d966d120bb3c8105911
posite <su84053@gmail.com>
2023-11-02 22:29:44 +0900

    feat: bonus 번호와 비교기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -19,9 +19,12 @@ class Lotto(private val numbers: List<Int>) {
         return sameWinningNumberCount
     }
 
-    fun compareBonusNumber(bonusNumber: Int): Int = if (numbers.contains(bonusNumber)) 1 else 0
+    fun compareBonusNumber(bonusNumber: BonusNumber): Int =
+        if (numbers.contains(bonusNumber.loadBonusNumber())) CONTAIN_BONUS_NUMBER else NOT_CONTAIN_BONUS_NUMBER
 
     companion object {
         private const val WINNING_NUMBERS_SIZE = 6
+        private const val CONTAIN_BONUS_NUMBER = 1
+        private const val NOT_CONTAIN_BONUS_NUMBER = 0
     }
 }
