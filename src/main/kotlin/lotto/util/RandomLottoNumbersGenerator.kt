caf0b0cacdb02c4703ca389f42901d34d900f1e0
posite <su84053@gmail.com>
2023-11-02 22:41:32 +0900

    refactor: RandomNumberGenerator의 하드 코딩된 부분 변경

--- src/main/kotlin/lotto/util/RandomLottoNumbersGenerator.kt
+++ src/main/kotlin/lotto/util/RandomLottoNumbersGenerator.kt
@@ -4,5 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RandomLottoNumbersGenerator {
 
-    fun generate() = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    fun generate() = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT)
+
+    companion object {
+        private const val START_NUMBER = 1
+        private const val END_NUMBER = 45
+        private const val NUMBER_COUNT = 6
+    }
 }
