3ba8f1089f87651e9d1d2505e3cdd17e759527ca
jaehan4707 <jahan4707@naver.com>
2023-11-03 01:30:19 +0900

    feat : 당첨 번호 유효성 검증

--- src/main/kotlin/lotto/view/InputValidator.kt
+++ src/main/kotlin/lotto/view/InputValidator.kt
@@ -1,7 +1,11 @@
 package lotto.view
 
+import lotto.util.Constant.INPUT_LUCKY_NUMBER_LENGTH_ERROR_MESSAGE
+import lotto.util.Constant.INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE
+import lotto.util.Constant.INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE
 import lotto.util.Constant.INPUT_PRICE_NOT_NUMBER_ERROR_MESSAGE
 import lotto.util.Constant.INPUT_PRICE_UNIT_ERROR_MESSAGE
+import lotto.util.Constant.LOTTO_NUMBER_SIZE
 import lotto.util.Constant.UNIT_PRICE
 import java.lang.IllegalArgumentException
 
@@ -13,6 +17,18 @@ object InputValidator {
         }
         if(price.toInt() % UNIT_PRICE !=0){
             throw IllegalArgumentException(INPUT_PRICE_UNIT_ERROR_MESSAGE)
+        }
+    }
+    fun validateInputLuckyNumber(numbers:String){
+        val luckyNumbers = numbers.split(",")
+        if(luckyNumbers.size!= LOTTO_NUMBER_SIZE){
+            throw IllegalArgumentException(INPUT_LUCKY_NUMBER_LENGTH_ERROR_MESSAGE)
+        }
+        if(luckyNumbers.count{ luckynumber -> luckynumber.toIntOrNull()==null} > 0){
+            throw IllegalArgumentException(INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE)
+        }
+        if(luckyNumbers.toSet().size != LOTTO_NUMBER_SIZE){
+            throw  IllegalArgumentException(INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE)
         }
     }
 }
