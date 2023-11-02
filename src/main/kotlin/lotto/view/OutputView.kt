5be0982c1edace605ad6d638546e9399eddd3532
jaehan4707 <jahan4707@naver.com>
2023-11-03 01:29:27 +0900

    feat : 당첨번호 입력 메시지 추가

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,7 @@
 package lotto.view
 
 import lotto.model.Lotto
+import lotto.util.Constant.INPUT_LUCKY_NUMBER_MESSAGE
 import lotto.util.Constant.INPUT_PRICE_MESSAGE
 import lotto.util.Constant.LOTTO_BUY_MESSAGE
 import lotto.util.Constant.UNIT_PRICE
@@ -10,4 +11,6 @@ class OutputView {
     fun printInputPrice() = println(INPUT_PRICE_MESSAGE)
     fun printBuyLotto(price: Int) = println("\n${LOTTO_BUY_MESSAGE.format(price / UNIT_PRICE)}")
     fun printUserLotto(lottoes: List<Lotto>) = lottoes.forEach { lotto -> println(lotto.getLottoNumbers()) }
+
+    fun printInputLuckyNumber() = println("\n${INPUT_LUCKY_NUMBER_MESSAGE}")
 }
