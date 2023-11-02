86082f90b2febb8cb870b3b4cd258aa7ce170d28
jaehan4707 <jahan4707@naver.com>
2023-11-03 00:30:49 +0900

    feat : 로또 구입 메시지 출력

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -7,8 +7,8 @@ class LottoController(private val inputView: InputView = InputView(),private val
 
     fun run(){
         outPutView.printInputPrice()
-        inputView.inputPrice()
-        //TODO 구입 금액 입력
+        val price = inputView.inputPrice()
+        outPutView.printBuyLotto(price)
         //TODO 구입 금액으로 로또 생성
         //TODO 당첨번호 입력
         //TODO 보너스 번호 입력
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,8 +1,11 @@
 package lotto.view
 
 import lotto.util.Constant.INPUT_PRICE_MESSAGE
+import lotto.util.Constant.LOTTO_BUY_MESSAGE
+import lotto.util.Constant.UNIT_PRICE
 
 class OutputView {
 
     fun printInputPrice() = println(INPUT_PRICE_MESSAGE)
+    fun printBuyLotto(price : Int) = println("\n${LOTTO_BUY_MESSAGE.format(price/ UNIT_PRICE)}")
 }
