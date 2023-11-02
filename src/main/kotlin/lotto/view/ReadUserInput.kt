111b9d9e3250247256b1eeef4f46acb26622978d
posite <su84053@gmail.com>
2023-11-02 16:16:31 +0900

    feat: 사용자의 입력받기 기능 구현

---
+++ src/main/kotlin/lotto/view/ReadUserInput.kt
@@ -0,0 +1,29 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.constant.ExtraText
+import lotto.util.UserInputValidator
+
+class ReadUserInput {
+    private val validator = UserInputValidator()
+
+    fun readUserSingleNumberInput(): Int {
+        val userInput = Console.readLine()
+        validateUserPurchaseAmountInput(userInput)
+        return userInput.toInt()
+    }
+
+    fun readUserWinningNumberInput() {
+        val userInput = Console.readLine()
+        val splitUserInput = userInput.split(ExtraText.NUMBER_SEPARATOR.text)
+        validateUserWinningNumberInput(splitUserInput)
+    }
+
+    private fun validateUserPurchaseAmountInput(userInput: String) {
+        validator.isNumberFormat(userInput)
+    }
+
+    private fun validateUserWinningNumberInput(userInput: List<String>) {
+        validator.isNumberListFormat(userInput)
+    }
+}
