0e21744147cb7c90713a9155190b4544288635e6
posite <su84053@gmail.com>
2023-11-02 22:31:26 +0900

    feat: 당첨 번호를 다루기 위한 WinningNumbers 구현

---
+++ src/main/kotlin/lotto/domain/WinningNumbers.kt
@@ -0,0 +1,41 @@
+package lotto.domain
+
+import lotto.constant.ErrorMessage
+
+class WinningNumbers(private val winningNumbers: List<String>) {
+    init {
+        validateWinningNumbers()
+    }
+
+    fun convertToInt() = winningNumbers.map { it.toInt() }.toList()
+
+    private fun validateWinningNumbers() {
+        validateWinningNumbersSize()
+        validateWinningNumbersDuplicate()
+        validateWinningNumbersRange()
+    }
+
+    private fun validateWinningNumbersRange() {
+        val intWinningNumbers = winningNumbers.map { it.toInt() }.toList()
+        if (!intWinningNumbers.all { it in 1..45 }) {
+            throw IllegalStateException(ErrorMessage.NOT_NUMBER_RANGE_WINNING_NUMBER.message)
+        }
+    }
+
+    private fun validateWinningNumbersDuplicate() {
+        val distinctWinningNumbers = winningNumbers.distinct()
+        if (distinctWinningNumbers.size != winningNumbers.size) {
+            throw IllegalStateException(ErrorMessage.NOT_DUPLICATED_WINNING_NUMBER.message)
+        }
+    }
+
+    private fun validateWinningNumbersSize() {
+        if (winningNumbers.size != WINNING_NUMBER_SIZE) {
+            throw IllegalStateException(ErrorMessage.NOT_SIX_WINNING_NUMBER.message)
+        }
+    }
+
+    companion object {
+        private const val WINNING_NUMBER_SIZE = 6
+    }
+}
