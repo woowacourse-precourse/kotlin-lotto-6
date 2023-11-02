d02986d65f54b6a6faa6db1ac5fc9b826a18a1a2
Hogu59 <dbstjdguswkd@naver.com>
2023-11-03 01:22:00 +0900

    feat: 구입 금액 입력 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,9 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.*
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해 주세요.")
+    val purchaseAmount = Console.readLine()
+    //금액이 숫자가 아닌 경우, 범위를 초과하는 경우 예외처리
 }
