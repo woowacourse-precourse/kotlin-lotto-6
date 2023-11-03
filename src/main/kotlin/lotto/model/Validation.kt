3dc5f3104233428400e5ec2df1547c0071b9993f
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-03 23:23:11 +0900

    Feat (model) : 입력한 로또 구입 금액이 숫자인지 확인

--- src/main/kotlin/lotto/model/Validation.kt
+++ src/main/kotlin/lotto/model/Validation.kt
@@ -2,7 +2,12 @@ package lotto.model
 
 internal fun String.isAmountInThousand() =
     require(this.toInt() % 1000 == 0) { ErrorMessage.IsNotAmountInThousand }
+
+internal fun String.isDigit() =
+    requireNotNull(this.toIntOrNull()) { ErrorMessage.IsDigit }
+
 internal enum class ErrorMessage(private val message: String) {
-    IsNotAmountInThousand("[ERROR] 구입 금액은 1,000원 단위 이어야 합니다.");
+    IsNotAmountInThousand("[ERROR] 구입 금액은 1,000원 단위 이어야 합니다."),
+    IsDigit("[ERROR] 구입 금액은 숫자 이어야 합니다.");
     override fun toString() = message
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,10 +2,12 @@ package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.model.isAmountInThousand
+import lotto.model.isDigit
 
 class InputView {
     fun inputPurchaseAmount() {
         val amount = Console.readLine()
+        amount.isDigit()
         amount.isAmountInThousand()
     }
 }
