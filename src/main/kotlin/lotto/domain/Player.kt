4f8d27960142ca52a07a0236a40b14d81417bb77
MinhoiKoo <minlove2013@gmail.com>
2023-11-04 01:20:50 +0900

    feat: Player Class 생성
    -로또 구매 로직 생성

---
+++ src/main/kotlin/lotto/domain/Player.kt
@@ -0,0 +1,15 @@
+package lotto.domain
+
+class Player(private var money: Int) {
+
+    fun purchaseLotto(): List<Lotto> {
+        val lottoNumbers = mutableListOf<Lotto>()
+        val quantity = money / 1000
+        repeat(quantity) {
+            val numbers = Lotto(NumberIssuer.issueNumbers())
+            lottoNumbers.add(numbers)
+        }
+        return lottoNumbers
+    }
+
+}
