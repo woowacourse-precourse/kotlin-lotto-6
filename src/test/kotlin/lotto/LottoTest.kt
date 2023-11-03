b66918c3451c21e4befc974b387d2da6cd107d2a
jaejunlee11 <sktks11@naver.com>
2023-11-03 17:52:58 +0900

    로또 금액 입력 기능 테스트 케이스 생성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.assertThrows
 
 
 class LottoTest {
+    val lottoController = LottoController()
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -17,6 +18,20 @@ class LottoTest {
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `로또 금액이 1000 단위가 아닌 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            lottoController.checkLottoCost(1001)
+        }
+    }
+
+    @Test
+    fun `로또 금액이 0 이하인 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            lottoController.checkLottoCost(0)
         }
     }
 
