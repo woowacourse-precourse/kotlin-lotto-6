8056b019302773b26c1bc70fe018c4ab4639396c
lh99j <grand2181@gmail.com>
2023-11-03 17:39:53 +0900

    feat(LottoController): LottoController생성하여 MVC 패턴 적용

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,27 +1,12 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Console
-import lotto.model.LottoTicket
-import lotto.util.LottoGenerator
-import lotto.util.Validator.validate1000Unit
-import lotto.util.Validator.validateInteger
-import lotto.util.Validator.validateRange
+import lotto.controller.LottoController
 import lotto.view.InputView
 import lotto.view.OutputView
 
 fun main() {
     val inputView = InputView()
     val outputView = OutputView()
-    outputView.printGameStartMessage()
-    val userInputPrice = inputView.getValidateUserInput()
-    val purchaseCount = userInputPrice / 1000
-    val lottoTicket = LottoTicket(purchaseCount)
-    val lottoGenerator = LottoGenerator()
-    println()
-    outputView.printPurchaseCount(purchaseCount)
-    repeat(purchaseCount) {
-        val numbers = lottoGenerator.lottoPublish()
-        lottoTicket.addNumbers(numbers)
-    }
-    outputView.printLottoTicket(lottoTicket)
+    val lottoController = LottoController(inputView, outputView)
+    lottoController.run()
 }
---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,25 @@
+package lotto.controller
+
+import lotto.model.LottoTicket
+import lotto.util.LottoGenerator
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController(private val inputView: InputView, private val outputView: OutputView) {
+    private val lottoGenerator = LottoGenerator()
+
+    fun run() {
+        outputView.printGameStartMessage()
+        val userInputPrice = inputView.getValidateUserInput()
+        val purchaseCount = userInputPrice / 1000
+        val lottoTicket = LottoTicket(purchaseCount)
+        println()
+        outputView.printPurchaseCount(purchaseCount)
+        repeat(purchaseCount)
+        {
+            val numbers = lottoGenerator.lottoPublish()
+            lottoTicket.addNumbers(numbers)
+        }
+        outputView.printLottoTicket(lottoTicket)
+    }
+}
