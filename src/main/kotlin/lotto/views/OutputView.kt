65b576e84acbc613579f3d625d156c2e2f0d02fa
sunkk8482 <sunkk8482@naver.com>
2023-11-03 21:11:51 +0900

    Feat : 출력 View 추가
    금액 입력, 당첨 번호 입력, 보너스 번호 입력

---
+++ src/main/kotlin/lotto/views/OutputView.kt
@@ -0,0 +1,17 @@
+package lotto.views
+
+import lotto.constants.*
+
+object OutputView {
+    fun printEnterMoney(){
+        println(ENTER_MONEY)
+    }
+
+    fun printEnterWinningNumber(){
+        println(ENTER_WINNING_NUMBER)
+    }
+
+    fun printEnterBonusNumber(){
+        println(ENTER_BONUS_NUMBER)
+    }
+}
