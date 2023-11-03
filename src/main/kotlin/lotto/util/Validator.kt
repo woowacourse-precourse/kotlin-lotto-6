efec9ff26675258ea8ed607c85573055b42335f4
getupminaaa <getupmina@gmail.com>
2023-11-03 17:36:35 +0900

    feat: 입력 값이 1000으로 나누어떨어지는지 검사하는 메서드 구현

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -1,17 +1,13 @@
 package lotto.util
 
-/*
-- 1 이상의 정수인지
-- 1000원으로 나누어 떨어지는지
-- 공백이 아닌지
-*/
 private const val ERROR_CONVENTION = "[ERROR] "
 private const val NOT_INTEGER_MSG = "입력 값은 정수여야 합니다."
 private const val NOT_NEGATIVE_INTEGER_MSG = "입력 값은 0보다 큰 정수여야 합니다."
+private const val NOT_DIVISIBLE_MSG = "입력 값은 천 원 단위여야 합니다."
+private const val NOT_EMPTY_MSG = "입력 값은 공백이 될 수 없습니다."
 
 private const val POSITIVE_NUM = 1
-
-private const val NOT_EMPTY_MSG = "입력 값은 공백이 될 수 없습니다."
+private const val DIVISOR = 1000
 
 object Validator {
      fun isItInteger(input: String) {
@@ -23,5 +19,8 @@ object Validator {
     }
     fun isItNotEmpty(input: String){
         require(input.trim().isNotEmpty()){ERROR_CONVENTION + NOT_EMPTY_MSG }
+    }
+    fun isDivisibleByThousand(input: Int) {
+        require(input % DIVISOR ==0){ERROR_CONVENTION + NOT_DIVISIBLE_MSG}
     }
 }
