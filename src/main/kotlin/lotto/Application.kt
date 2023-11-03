6b9a364785348c5775d0e93b83b1fc39ecb09129
lh99j <grand2181@gmail.com>
2023-11-03 17:30:50 +0900

    feat: 로또 번호 출력까지의 로직 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,12 +2,23 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.model.LottoTicket
+import lotto.util.LottoGenerator
 import lotto.util.Validator.validate1000Unit
 import lotto.util.Validator.validateInteger
 import lotto.util.Validator.validateRange
 
 fun main() {
-
+    printGameStartMessage()
+    val purchaseCount = getValidateUserInput() / 1000
+    val lottoTicket = LottoTicket(purchaseCount)
+    val lottoGenerator = LottoGenerator()
+    println()
+    printPurchaseCount(purchaseCount)
+    repeat(purchaseCount) {
+        val numbers = lottoGenerator.lottoPublish()
+        lottoTicket.addNumbers(numbers)
+    }
+    printLottoTicket(lottoTicket)
 }
 
 fun printGameStartMessage() {
@@ -30,7 +41,7 @@ fun printPurchaseCount(purchaseCount: Int) {
 
 fun printLottoTicket(lottoTicket: LottoTicket) {
     val ticket = lottoTicket.numbers
-    repeat(ticket.size) {
-        println(ticket.toString())
+    repeat(ticket.size) {index ->
+        println(ticket[index].toString())
     }
 }
