5219177d6fbae3fca860200c2511c2beb2793926
yeji0214 <1225yj50@naver.com>
2023-11-03 15:52:24 +0900

    보너스 번호 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -55,14 +55,32 @@ fun main() {
             val lotto = Lotto(winningLotteryNumbers)
             validWinningNumbers = true
         } catch (e: NumberFormatException) {
-            println("당첨 번호는 모두 숫자여야 합니다.")
+            println("[ERROR] 당첨 번호는 모두 숫자여야 합니다.")
         } catch (e: IllegalArgumentException) {
             println(e.message)
         }
     }
     //println(winningLotteryNumbers)
 
-    println("\n보너스 번호를 입력해 주세요.")
-    val bonusNumber = Console.readLine().toInt()
-    println(bonusNumber)
+    var bonusNumber: Int
+    var validBonusNumber = false
+
+    while (!validBonusNumber) {
+        println("\n보너스 번호를 입력해 주세요.")
+        val bonusInput = Console.readLine()
+        try {
+            bonusNumber = bonusInput.trim().toInt()
+            if (bonusNumber < 1 || bonusNumber > 45) {
+                throw IllegalArgumentException("[ERROR] 1부터 45 사이의 숫자만 입력하세요.")
+            }
+            if (winningLotteryNumbers.contains(bonusNumber)) {
+                throw IllegalArgumentException("[ERROR] 기존 당첨 번호에 이미 같은 값이 있습니다.")
+            }
+            validBonusNumber = true
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 보너스 번호는 숫자여야 합니다.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
 }
