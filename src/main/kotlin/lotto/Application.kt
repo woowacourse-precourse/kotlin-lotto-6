243f07b52eb8d584d0e13fed4b1c15f35454c0e1
seonm9119 <seonm9119@gmail.com>
2023-11-03 21:56:59 +0900

    use the enum class to manage prompts & design a function that receive user inputs

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,24 @@
 package lotto
+import camp.nextstep.edu.missionutils.Console.readLine
+
+enum class UserPrompt(val message: String) {
+    PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    LOTTO_NUMBERS("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("보너스 번호를 입력해 주세요.")
+
+}
+
+fun getUserInput(prompt: UserPrompt): String{
+    println(prompt.message)
+    return readLine()
+}
 
 fun main() {
-    TODO("프로그램 구현")
+
+    val purchaseAmount = getUserInput(UserPrompt.PURCHASE_AMOUNT)
+    val lottoNumbers = getUserInput(UserPrompt.LOTTO_NUMBERS)
+    val bonusNumber = getUserInput(UserPrompt.BONUS_NUMBER)
+
+
+
 }
