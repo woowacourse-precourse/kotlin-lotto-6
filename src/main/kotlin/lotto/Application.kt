09b065b710a9aec681643c9d34014d8ae04feb41
junyong <junyong008@gmail.com>
2023-11-03 14:26:05 +0900

    feat(Application): 구입금액 입력 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import lotto.domain.Money
+import lotto.validator.InputValidator
+import lotto.view.InputView
+
 fun main() {
-    TODO("프로그램 구현")
+    val money: Money = getInputPurchaseAmount()
+}
+
+private fun getInputPurchaseAmount(): Money {
+    return try {
+        val input = InputView.inputPurchaseAmount()
+        InputValidator.validateInputPurchaseAmount(input)
+        Money(input.toInt())
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+        getInputPurchaseAmount()
+    }
 }
