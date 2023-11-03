518d7b39bb0ef6f78288ef82ba3518cd4c0bf07a
lh99j <grand2181@gmail.com>
2023-11-03 19:33:37 +0900

    feat(Exception): Exception enum class 생성

---
+++ src/main/kotlin/lotto/util/Exception.kt
@@ -0,0 +1,11 @@
+package lotto.util
+
+enum class Exception(private val message: String) {
+    INVALID_INTEGER("사용자의 입력이 숫자가 아닙니다."),
+    INVALID_RANGE("사용자의 입력이 유효하지 않은 범위의 숫자입니다."),
+    INVALID_1000_UNIT("사용자 입력이 1000원 단위가 아닙니다.");
+
+    private val format = "[ERROR] "
+
+    fun getMessage(): String = "$format $message"
+}
--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -4,15 +4,15 @@ import java.lang.IllegalArgumentException
 
 object Validator {
     fun validateInteger(input: String) {
-        input.toIntOrNull() ?: throw IllegalArgumentException("사용자의 입력이 숫자가 아닙니다.")
+        require(input.toIntOrNull() != null) { Exception.INVALID_INTEGER.getMessage() }
     }
 
     fun validateRange(input: String) {
-        require(input.toInt() in 1000..Int.MAX_VALUE) { "사용자의 입력이 유효하지 않은 범위의 숫자입니다." }
+        require(input.toInt() in 1000..Int.MAX_VALUE) { Exception.INVALID_RANGE.getMessage() }
     }
 
     fun validate1000Unit(input: String) {
         val number = input.toInt()
-        require(number % 1000 == 0) { "사용자 입력이 1000원 단위가 아닙니다." }
+        require(number % 1000 == 0) { Exception.INVALID_1000_UNIT.getMessage() }
     }
 }
