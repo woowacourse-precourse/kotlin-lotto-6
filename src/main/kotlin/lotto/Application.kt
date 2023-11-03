7f8fce3aa29e6260ab61d00506798ca42440672d
yeji0214 <1225yj50@naver.com>
2023-11-03 11:29:01 +0900

    구입금액 예외 처리 (숫자가 아닌 경우)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,6 +5,10 @@ fun main() {
     println("구입금액을 입력해주세요.")
     var purchaseAmount = Console.readLine()
 
-
+    try {
+        purchaseAmount.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 숫자를 입력하세요.")
+    }
 
 }
