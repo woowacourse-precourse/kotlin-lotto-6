83286efc7cb5cd84eb3441bb158e25ce3edc0220
lh99j <grand2181@gmail.com>
2023-11-03 14:29:35 +0900

    feat: 로또 가격의 범위 검증 기능 구현

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -6,4 +6,8 @@ object Validator {
     fun validateInteger(input: String) {
         input.toIntOrNull() ?: throw IllegalArgumentException("사용자의 입력이 숫자가 아닙니다.")
     }
+
+    fun validateRange(input: String) {
+        require(input.toInt() in 1000..Int.MAX_VALUE){ "사용자의 입력이 유효하지 않은 범위의 숫자입니다." }
+    }
 }
