0ae192a392e014a18be620d4bd12e94679aeec9c
Tnalxmsk <alstnwkd990@naver.com>
2023-11-03 23:52:49 +0900

    refactor(lotto): NumberGeneration 클래스 이름 수정
    NumberGeneration -> NumberGenerator

--- src/main/kotlin/lotto/NumberGenerator.kt
+++ src/main/kotlin/lotto/NumberGenerator.kt
@@ -2,7 +2,7 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class NumberGeneration {
+class NumberGenerator {
     fun generateNumbers(): List<Int> {
         val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
         return numbers
