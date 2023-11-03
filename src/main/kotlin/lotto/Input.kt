63e4937c6e36b90f20a3d57551dc372165574e00
Tnalxmsk <alstnwkd990@naver.com>
2023-11-04 00:08:48 +0900

    feat(lotto): Input 클래스 당첨 번호 입력 기능 추가
    사용자가 당첨 번호를 입력하는 InputWinningNumber() 메서드

--- src/main/kotlin/lotto/Input.kt
+++ src/main/kotlin/lotto/Input.kt
@@ -8,7 +8,15 @@ class Input {
         return Console.readLine().toInt()
     }
 
+    fun inputWinningNumber(): List<Int> {
+        println(INPUT_WINNING_NUMBER)
+        return Console.readLine().split(",").map { number ->
+            number.toInt()
+        }
+    }
+
     companion object {
         private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+        private const val INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요."
     }
 }
