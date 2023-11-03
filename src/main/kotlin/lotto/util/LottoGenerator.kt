19702c7f8dcb8f4e697fd6795f500d82a1beab0d
lh99j <grand2181@gmail.com>
2023-11-03 17:32:00 +0900

    feat: 로또 번호가 오름차순으로 출력되는 기능 구현

--- src/main/kotlin/lotto/util/LottoGenerator.kt
+++ src/main/kotlin/lotto/util/LottoGenerator.kt
@@ -3,5 +3,5 @@ package lotto.util
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoGenerator {
-    fun lottoPublish(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    fun lottoPublish(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
 }
