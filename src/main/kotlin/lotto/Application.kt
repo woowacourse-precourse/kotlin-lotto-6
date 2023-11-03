9e794e6144d6b6982c78603489d1295d0990c4f3
yeji0214 <1225yj50@naver.com>
2023-11-03 11:42:58 +0900

    구입금액 예외처리(1000보다 작거나 1000으로 나누어 떨어지지 않는 경우), 에러 발생 시 다시 입력 받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,13 +2,26 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("구입금액을 입력해주세요.")
-    var purchaseAmount = Console.readLine()
+    var amount = 0
+    while (true) {
+        println("구입금액을 입력해주세요.")
+        val purchaseAmount = Console.readLine()
 
-    try {
-        purchaseAmount.toInt()
-    } catch (e: NumberFormatException) {
-        throw IllegalArgumentException("[ERROR] 숫자를 입력하세요.")
+        try {
+            amount = purchaseAmount.toInt()
+
+            if (amount < 1000) {
+                throw IllegalArgumentException("[ERROR] 1000 이상의 금액을 입력하세요.")
+            } else if (amount % 1000 != 0) {
+                throw IllegalArgumentException("[ERROR] 1000으로 나누어 떨어지는 값을 입력하세요.")
+            } else {
+                break // Break out of the loop when valid input is provided
+            }
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 숫자를 입력하세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 
 }
