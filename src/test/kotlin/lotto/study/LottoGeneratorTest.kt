4ee6a6a172810578dda22e690738d1468ed41b95
lh99j <grand2181@gmail.com>
2023-11-03 16:31:32 +0900

    test: 로또 발행 테스트

---
+++ src/test/kotlin/lotto/study/LottoGeneratorTest.kt
@@ -0,0 +1,16 @@
+package lotto.study
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class LottoGeneratorTest {
+    private val lottoGenerator = LottoGenerator()
+
+    @Test
+    @DisplayName("로또 발행 테스트")
+    fun lottoPublishTest() {
+        val lottoNumbers: List<Int> = lottoGenerator.lottoPublish()
+        assertThat(lottoNumbers).hasSize(8).allSatisfy { value -> assertThat(value).isBetween(1, 45) }
+    }
+}
