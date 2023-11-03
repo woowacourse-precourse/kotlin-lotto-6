5602546769aa22a5892ce85b744af395c9d17a08
kimhm0728 <kimhm0728@naver.com>
2023-11-03 21:19:06 +0900

    feat: 소수점 첫째자리까지 반올림하는 기능 구현

--- src/main/kotlin/lotto/StringConverter.kt
+++ src/main/kotlin/lotto/StringConverter.kt
@@ -1,3 +1,5 @@
 package lotto
 
 fun Int.convertWithDigitComma() = "%,d".format(this)
+
+fun Double.convertWithRound() = "%.1f".format(this)
--- src/main/kotlin/lotto/io/output/Output.kt
+++ src/main/kotlin/lotto/io/output/Output.kt
@@ -2,6 +2,7 @@ package lotto.io.output
 
 import lotto.constants.Print
 import lotto.constants.WinningResult
+import lotto.convertWithRound
 import lotto.model.Lottos
 
 class Output {
@@ -22,6 +23,37 @@ class Output {
     fun printInputBonusNumbers() {
         lineBreak()
         println(Print.INPUT_BONUS_NUMBERS)
+    }
+
+    fun printWinningStat(winningCounts: Map<WinningResult, Int>, totalReturn: Double) {
+        lineBreak()
+        println(Print.WINNING_STAT)
+        println(Print.DIVIDE_LINE)
+
+        printWinningSituation(winningCounts)
+        printTotalReturn(totalReturn)
+    }
+
+    private fun printWinningSituation(winningCounts: Map<WinningResult, Int>) {
+        winningCounts.forEach { result ->
+            val winningResult = result.key
+            val winningCount = result.value
+            val bonusMessage = run {
+                if (winningResult == WinningResult.FIVE_BONUS) {
+                    Print.BONUS
+                } else ""
+            }
+
+            println(Print.MATCHING_NUMBER.format(
+                winningResult.matchingCount,
+                bonusMessage,
+                winningResult.amount,winningCount))
+        }
+    }
+
+    private fun printTotalReturn(totalReturn: Double) {
+        val totalReturnRound = totalReturn.convertWithRound()
+        println(Print.TOTAL_RETURN.format(totalReturnRound))
     }
 
     private fun Print.format(vararg args: Any) = this.toString().format(args)
