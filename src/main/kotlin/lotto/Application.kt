662cd86fac42a1aeb882f5ea529f044c29bcc4a1
yeji0214 <1225yj50@naver.com>
2023-11-03 11:28:11 +0900

    로또 구입금액 입력받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해주세요.")
+    var purchaseAmount = Console.readLine()
+
+
+
 }
