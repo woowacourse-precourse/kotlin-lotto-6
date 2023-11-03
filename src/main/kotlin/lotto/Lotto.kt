863526aaaf95d6339549491e1cef6991f36344db
lh99j <grand2181@gmail.com>
2023-11-03 11:44:30 +0900

    feat: 중복된 로또번호 검증 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct().size == 6)
     }
 
     // TODO: 추가 기능 구현
