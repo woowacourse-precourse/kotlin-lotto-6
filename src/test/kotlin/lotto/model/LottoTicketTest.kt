c09cfbd2267b8b0f2fe85c3e2e5aa7a462ed2cce
lh99j <grand2181@gmail.com>
2023-11-03 17:14:15 +0900

    test(LottoTicketTest): 로또 번호 관리 모델 테스트케이스 작성

---
+++ src/test/kotlin/lotto/model/LottoTicketTest.kt
@@ -0,0 +1,30 @@
+package lotto.model
+
+import lotto.util.LottoGenerator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class LottoTicketTest {
+    private val lottoGenerator = LottoGenerator()
+    private lateinit var lottoTicket: LottoTicket
+
+    @BeforeEach
+    fun setUp() {
+        lottoTicket = LottoTicket(2)
+        lottoTicket.addTickets(lottoGenerator.lottoPublish())
+        lottoTicket.addTickets(lottoGenerator.lottoPublish())
+    }
+
+    @Test
+    @DisplayName("로또 번호가 티켓에 잘 들어갔는지 테스트")
+    fun addTicketsTest() {
+        assertThat(lottoTicket.numbers).hasSize(2)
+            .allSatisfy { numbers ->
+                assertThat(numbers)
+                    .hasSize(6)
+                    .doesNotHaveDuplicates()
+            }
+    }
+}
