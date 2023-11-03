7ae3e30f7cfe58613b79893f0cff2244d400da9b
MinhoiKoo <minlove2013@gmail.com>
2023-11-03 21:27:20 +0900

    feat: LottoNumber Class 생성
    -Lotto 클래스의 numbers 원시값 포장 클래스
    -로또 숫자가 1부터 45사이가 아니면 예외 발생

---
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+import lotto.utils.Constant.MAX_LOTTO_NUMBER
+import lotto.utils.Constant.MIN_LOTTO_NUMBER
+
+private const val WRONG_NUMBER_RANGE_EXCEPTION_MESSAGE = "[ERROR]로또 번호는 1부터 45 사이의 숫자여야 합니다! "
+
+class LottoNumber(val number: Int) {
+
+    init {
+        require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { WRONG_NUMBER_RANGE_EXCEPTION_MESSAGE }
+    }
+}
