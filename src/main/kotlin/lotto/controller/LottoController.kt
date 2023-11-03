b5ff236ed5d6fddf92bbb467c70817d916e04aff
dpcks0509 <dpcks0509@gmail.com>
2023-11-04 02:26:10 +0900

    feat: 핵심 제어를 담당하는 LottoController 클래스 추가
    핵심 제어를 담당하는 LottoController 클래스를 새로 추가하였다.

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,13 @@
+package lotto.controller
+
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController {
+    val inputView = InputView()
+    val outputView = OutputView()
+
+    fun run() {
+
+    }
+}
