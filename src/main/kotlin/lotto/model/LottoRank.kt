db528ba331376e3b7ec21a42653136c39c86380d
Sheep1sik <bohg6799@naver.com>
2023-11-04 04:13:52 +0900

    feat: LottoRank 모델 구현
    - enum class 사용

---
+++ src/main/kotlin/lotto/model/LottoRank.kt
@@ -0,0 +1,19 @@
+package lotto.model
+
+enum class LottoRank() {
+    THREE_MATCH,
+    FOUR_MATCH,
+    FIVE_MATCH,
+    FIVE_MATCH_WITH_BONUS,
+    SIX_MATCH;
+
+    private var count = 0
+
+    fun increment() {
+        count++
+    }
+
+    fun getCount(): Int {
+        return count
+    }
+}
