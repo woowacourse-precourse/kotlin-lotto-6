2ad743f90a3cc4deb198a761a8e391ec79f3141c
jaejunlee11 <sktks11@naver.com>
2023-11-03 17:49:35 +0900

    로또 금액 입력 기능 구현

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -1,12 +1,32 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 class LottoController {
+    private var cost = 0
+
     fun start() {
 
     }
 
-    fun inputLottoNumber() {
-
+    fun checkLottoCost(cost: Int) {
+        if (cost % 1000 != 0) {
+            throw IllegalArgumentException("1000단위로 입력해 주세요.")
+        }
+        if (cost <= 0) {
+            throw IllegalArgumentException("0을 초과한 값을 입력해 주세요.")
+        }
+    }
+    fun inputLottoCost() {
+        val inputCost = Console.readLine()
+        val cost = inputCost.toInt()
+        try {
+            checkLottoCost(cost)
+            this.cost = cost
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] 알맞은 금액을 입력해주세요.")
+            inputLottoCost()
+        }
     }
 
     fun lottoGenerate() {
