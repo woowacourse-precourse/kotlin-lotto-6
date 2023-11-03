257bb672d58411e2161e6765edf53b9bf47ab1e0
kimhm0728 <kimhm0728@naver.com>
2023-11-03 23:50:11 +0900

    feat: 너무 많은 로또를 구입하려고 하는 경우 예외 처리 구현

--- src/main/kotlin/lotto/constants/Constants.kt
+++ src/main/kotlin/lotto/constants/Constants.kt
@@ -4,5 +4,6 @@ enum class Constants(val value: Int) {
     PURCHASE_AMOUNT_UNIT(1000),
     LOTTO_SIZE(6),
     LOTTO_START(1),
-    LOTTO_END(45)
+    LOTTO_END(45),
+    LOTTO_MAX_COUNT(20)
 }
--- src/main/kotlin/lotto/model/Amount.kt
+++ src/main/kotlin/lotto/model/Amount.kt
@@ -1,12 +1,15 @@
 package lotto.model
 
 import lotto.constants.Constants.PURCHASE_AMOUNT_UNIT
+import lotto.constants.Constants.LOTTO_MAX_COUNT
 import lotto.constants.Exception
 
 class Amount(private val amount: Int) {
     init {
         require(amount % PURCHASE_AMOUNT_UNIT.value == 0) { Exception.DIVISIBLE }
+        require(amount / PURCHASE_AMOUNT_UNIT.value >= LOTTO_MAX_COUNT.value) { Exception.MAX_COUNT }
     }
 
     fun getPurchaseCount() = amount / PURCHASE_AMOUNT_UNIT.value
+
 }
