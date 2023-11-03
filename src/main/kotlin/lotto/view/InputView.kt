feff20ecbf1e6ff8e37582646ed7a8e959db7a00
Sheep1sik <bohg6799@naver.com>
2023-11-04 04:19:38 +0900

    feat: 당첨 번호를 입력받는 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,12 +1,17 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.Constants.Companion.INPUT_ENTER_WINNING_NUMBERS_MESSAGE
 import lotto.Constants.Companion.INPUT_PURCHASE_AMOUNT_MESSAGE
 
 class InputView {
 
     fun inputPurchaseAmountMessage(): String {
         println(INPUT_PURCHASE_AMOUNT_MESSAGE)
+        return Console.readLine()
+    }
+    fun inputEnterWinningNumbersMessage(): String {
+        println("\n"+ INPUT_ENTER_WINNING_NUMBERS_MESSAGE)
         return Console.readLine()
     }
 }
