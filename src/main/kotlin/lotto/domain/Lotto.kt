e7d529e93450e18843b96fe94ad479df8ca49bef
MinhoiKoo <minlove2013@gmail.com>
2023-11-03 21:25:33 +0900

    refactor: 매직 넘버 제거 및 에러 메시지 추가
    -숫자의 갯수가 6개가 아니면 예외 발생

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto.domain
 
+import lotto.utils.Constant.LOTTO_NUMBER_SIZE
+
+private const val INVALID_SIZE_EXCEPTION_MESSAGE = "[ERROR] 로또의 숫자 갯수는 6개여야 합니다!"
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_SIZE_EXCEPTION_MESSAGE }
     }
-
-    // TODO: 추가 기능 구현
 }
