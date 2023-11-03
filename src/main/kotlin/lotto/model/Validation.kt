dc00a7b9e5941af1733fb6b4b8ba90bfbed04871
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-03 23:33:51 +0900

    Feat (model) : 입력한 로또 구입 금액이 1000원 이상인지 확인하는 기능 구현

--- src/main/kotlin/lotto/model/Validation.kt
+++ src/main/kotlin/lotto/model/Validation.kt
@@ -6,8 +6,13 @@ internal fun String.isAmountInThousand() =
 internal fun String.isDigit() =
     requireNotNull(this.toIntOrNull()) { ErrorMessage.IsDigit }
 
+internal fun String.isLessThanThousand() =
+    require(this.toInt() >= 1000) { ErrorMessage.IsLessThanThousand }
+
 internal enum class ErrorMessage(private val message: String) {
     IsNotAmountInThousand("[ERROR] 구입 금액은 1,000원 단위 이어야 합니다."),
-    IsDigit("[ERROR] 구입 금액은 숫자 이어야 합니다.");
+    IsDigit("[ERROR] 구입 금액은 숫자 이어야 합니다."),
+    IsLessThanThousand("[ERROR] 구입 금액은 1000원 이상 이어야 합니다.");
+
     override fun toString() = message
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,11 +3,13 @@ package lotto.view
 import camp.nextstep.edu.missionutils.Console
 import lotto.model.isAmountInThousand
 import lotto.model.isDigit
+import lotto.model.isLessThanThousand
 
 class InputView {
     fun inputPurchaseAmount() {
         val amount = Console.readLine()
         amount.isDigit()
+        amount.isLessThanThousand()
         amount.isAmountInThousand()
     }
 }
