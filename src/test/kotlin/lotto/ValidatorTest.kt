736a213928b30280640bf7eb42e7c6b32c81d353
lh99j <grand2181@gmail.com>
2023-11-03 14:26:35 +0900

    test: 로또 가격의 범위 검증 테스트 케이스 작성

--- src/test/kotlin/lotto/ValidatorTest.kt
+++ src/test/kotlin/lotto/ValidatorTest.kt
@@ -10,13 +10,31 @@ class ValidatorTest {
     @Test
     @DisplayName("로또 구매 가격이 숫자가 아니면 예외가 발생한다.")
     fun validateIntegerTest() {
-        assertSimpleTest{
+        assertSimpleTest {
             assertThrows<IllegalArgumentException> {
                 validateInteger("abcd")
             }
 
             assertThrows<IllegalArgumentException> {
                 validateInteger("1245!")
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("로또 구매 가격의 범위가 유효하지 않으면 예외가 발생한다.")
+    fun validateRangeTest() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> {
+                validateRange("-1000")
+            }
+
+            assertThrows<IllegalArgumentException> {
+                validateRange("999")
+            }
+
+            assertThrows<IllegalArgumentException> {
+                validateRange("0")
             }
         }
     }
