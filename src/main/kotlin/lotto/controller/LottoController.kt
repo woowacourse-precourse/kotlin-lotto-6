6d83cb6dab15873d4aaf09e239356e880d195e47
lh99j <grand2181@gmail.com>
2023-11-03 18:46:39 +0900

    refactor(LottoController): 로또 발행 횟수 기능 함수로 분리

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -7,19 +7,28 @@ import lotto.view.OutputView
 
 class LottoController(private val inputView: InputView, private val outputView: OutputView) {
     private val lottoGenerator = LottoGenerator()
+    private var purchaseCount = 0
 
     fun run() {
-        outputView.printGameStartMessage()
-        val userInputPrice = inputView.getValidateUserInput()
-        val purchaseCount = userInputPrice / 1000
+        settingPurchaseCount()
+
         val lottoTicket = LottoTicket(purchaseCount)
         println()
         outputView.printPurchaseCount(purchaseCount)
-        repeat(purchaseCount)
-        {
+        repeat(purchaseCount) {
             val numbers = lottoGenerator.getSortedNumbers()
             lottoTicket.addNumbers(numbers)
         }
         outputView.printLottoTicket(lottoTicket)
+    }
+
+    private fun settingPurchaseCount() {
+        outputView.printGameStartMessage()
+        purchaseCount = getPurchaseCount()
+    }
+
+    private fun getPurchaseCount(): Int {
+        val userInputPrice = inputView.getValidateUserInput()
+        return userInputPrice / 1000
     }
 }
