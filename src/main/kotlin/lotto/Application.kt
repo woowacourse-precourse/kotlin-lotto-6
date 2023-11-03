0e48a15a8b29e436305a1f5e34f95c146cda1ddc
kimhm0728 <kimhm0728@naver.com>
2023-11-03 23:22:22 +0900

    feat: 메인 함수 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
+import lotto.io.input.Input
+import lotto.io.output.Output
+
 fun main() {
-    TODO("프로그램 구현")
+    val input = Input()
+    val output = Output()
+    val lottoSupplier = LottoSupplier()
+    val lottoController = LottoController(input, output, lottoSupplier)
+
+    lottoController.run()
 }
