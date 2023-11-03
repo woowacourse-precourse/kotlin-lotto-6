df5f649af942a3bcbcf9d08a65073383ec9baef5
Sheep1sik <bohg6799@naver.com>
2023-11-04 04:57:01 +0900

    feat: 랜덤 로또 리스트를 생성하는 기능 구현
    - Map을 활용하여 티켓의 수만큼 랜덤 로또 리스트를 생성

--- src/main/kotlin/lotto/LottoGameService.kt
+++ src/main/kotlin/lotto/LottoGameService.kt
@@ -1,12 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.Constants.Companion.ERROR_INVALID_LOTTO_AMOUNT_EXCEPTION
 import lotto.Constants.Companion.LOTTO_PRICE
+import lotto.Constants.Companion.MAX_LOTTO_NUMBER
+import lotto.Constants.Companion.MIN_LOTTO_NUMBER
 import lotto.Constants.Companion.MIN_NUMBER
 
 class LottoGameService {
     fun calculateLottoPurchaseQuantity(purchaseAmount: Int): Int {
         require(purchaseAmount % LOTTO_PRICE == MIN_NUMBER){ ERROR_INVALID_LOTTO_AMOUNT_EXCEPTION }
         return purchaseAmount / LOTTO_PRICE
+    }
+    fun lottoNumberGenerator(ticket: Int): MutableMap<Int, List<Int>> {
+        val purchaseLottoLists = mutableMapOf<Int,List<Int>>()
+        for (ticketCheck in 0 until ticket) {
+            purchaseLottoLists[ticketCheck] = Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER, 6).sorted()
+        }
+        return purchaseLottoLists
     }
 }
