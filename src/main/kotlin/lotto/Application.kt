8c2e931797d6185c990101fd618251e45c372694
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-03 14:23:25 +0900

    DOCS:작업진행 중 에러사항 기록

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,19 +4,13 @@ import lottoViewModel.ValidInput
 
 fun main() {
     val lottoOutput = LottoOutPut()
-    val validInput= ValidInput()
-    val lotto = Lotto(emptyList())
+    val validInput = ValidInput()
     val purchaseAmount = try {
         validInput.validInputPurchase()
-    }catch (e:IllegalArgumentException){
-       println(e.message)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
         return main()
     }
     lottoOutput.purchaseDetailPrint(purchaseAmount)
-    val generatedLottoNumbers = lotto.reCreateRandomNumber(purchaseAmount)
-
-    for (numbers in generatedLottoNumbers) {
-        println(numbers.joinToString(", "))
-    }
 }
 
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,22 +1,11 @@
 package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import lottoViewModel.ValidInput
+import java.lang.IllegalArgumentException
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-    }
-
-    fun createRandomNumber(): List<Int> {
-        val randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-       return randomNumbers+numbers
-    }
-    fun reCreateRandomNumber(count: Int): List<List<Int>> {
-        val generatedNumbers = mutableListOf<List<Int>>()
-        for (generate in 0 until count) {
-            val randomNumbers = createRandomNumber()
-            generatedNumbers.add(randomNumbers)
-        }
-        return generatedNumbers
     }
 
 }
