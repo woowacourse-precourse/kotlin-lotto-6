9711d4cc691798a75a430d7b57ea1f9c2d5a58cf
seoyeon <seoyeonn051@gmail.com>
2023-11-04 18:22:21 +0900

    feat: LottoGameView.kt 생성
    로또 게임 대화 메세지 생성

---
+++ src/main/kotlin/lotto/view/LottoGameView.kt
@@ -0,0 +1,13 @@
+package lotto.view
+
+class LottoGameView {
+
+    companion object {
+        const val ENTER_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해주세요."
+        const val PURCHASED_LOTTO_COUNT_MESSAGE = "개를 구매했습니다."
+        const val REQUEST_WINNING_LOTTO_NUM_MESSAGE = "당첨 번호를 입력해 주세요."
+        const val REQUEST_BONUS_LOTTO_NUM_MESSAGE = "보너스 번호를 입력해 주세요."
+        const val WINNING_STATISTICS_MESSAGE = "당첨 통계"
+        const val COINCIDE_NUM_MESSAGE = "개 일치"
+    }
+}
