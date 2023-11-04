d0c8ad8d50642011548e2bd118699b69d2b37fd6
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-05 03:11:54 +0900

    feat: 에러 메세지를 담당하는 enum Class 작성

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -2,6 +2,10 @@ package util
 
 object Validator {
     fun containsOnlyDigits(price: String) {
-        require(price.all { it.isDigit() }) { "[ERROR] 숫자만 입력이 가능합니다." }
+        require(price.all { it.isDigit() }) { Error.INPUT_ONLY_NUMBER.message }
+    }
+
+    private enum class Error(val message: String) {
+        INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
     }
 }
