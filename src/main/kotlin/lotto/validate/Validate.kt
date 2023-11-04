b892890a64293926f3094acd79b217d381954c26
stopstone <wltjr6432@naver.com>
2023-11-04 15:13:44 +0900

    feat: 구입금액 문자가 입력된 경우 예외처리 추가
    - validateInputNotNumber(): 숫자가 아닌 문자가 입력되었을 경우 에러

--- src/main/kotlin/lotto/validate/Validate.kt
+++ src/main/kotlin/lotto/validate/Validate.kt
@@ -5,7 +5,14 @@ import lotto.utils.Messages
 class Validate {
     fun validateInputPrice(price: String): Int {
         validateInputIsEmpty(price)
+        validateInputNotNumber(price)
+
         return price.toInt()
+    }
+
+
+    private fun validateInputNotNumber(x: String) {
+        require(x.all { it.isDigit() }) {"${Messages.ERROR_MESSAGE} ${Messages.INVALID_INPUT}" }
     }
 
     private fun validateInputIsEmpty(x: String) {
