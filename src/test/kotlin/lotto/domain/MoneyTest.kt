7d89d2a473ed2d476c25c6e8389d024a7155784a
junyong <junyong008@gmail.com>
2023-11-04 16:37:03 +0900

    test(Money): 로또 가격 정보 제거

--- src/test/kotlin/lotto/domain/MoneyTest.kt
+++ src/test/kotlin/lotto/domain/MoneyTest.kt
@@ -3,33 +3,24 @@ package lotto.domain
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class MoneyTest {
-    @Test
-    fun `정상적인 객체 생성`() {
-        // given
-        val input = 5000
+    @ParameterizedTest
+    @ValueSource(ints = [5000, 100, 7777, 0])
+    fun `정상적인 객체 생성`(input: Int) {
         // when & then
         assertDoesNotThrow { Money(input) }
     }
 
     @Test
-    fun `객체 생성시 1,000 단위가 아닐 경우 예외 처리`() {
+    fun `음수 예외 처리`() {
         // given
-        val input = 1001
+        val input = -1
         // when & then
         Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
             .isThrownBy { Money(input) }
-            .withMessage("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.")
-    }
-
-    @Test
-    fun `객체 생성시 1,000 보다 적을 경우 예외 처리`() {
-        // given
-        val input = 0
-        // when & then
-        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
-            .isThrownBy { Money(input) }
-            .withMessage("[ERROR] 구입금액은 최소 1000원 이상 입력해야 합니다.")
+            .withMessage("[ERROR] 금액은 음수가 될 수 없습니다.")
     }
 }
