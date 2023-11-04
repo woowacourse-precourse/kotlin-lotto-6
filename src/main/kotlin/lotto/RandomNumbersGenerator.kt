4f265edbcf61d40b38ee44cb17c929cc84a03048
PParkcode <qazwsxedc1226@gmail.com>
2023-11-04 18:24:55 +0900

    Feat: 랜덤 생성한 번호들 정렬 기능 개발
    RandomNumbersGenerator 객체에서 랜덤 생성한 번호들을 정렬해서 리턴하도록 하였다.

--- src/main/kotlin/lotto/RandomNumbersGenerator.kt
+++ src/main/kotlin/lotto/RandomNumbersGenerator.kt
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class RandomNumbersGenerator {
     fun makeRandomNumbers():List<Int> {
-         return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return numbers.sorted()
     }
 }
 
