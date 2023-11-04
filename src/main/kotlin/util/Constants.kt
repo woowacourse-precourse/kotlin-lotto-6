af6e65b536efb3e0d43f0380ab8cff37df047ac7
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 04:12:53 +0900

    feat: 당첨 금액 상수 추가
    로또 당첨 순위에 따른 당첨 금액을 상수로 추가하였다.

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -5,4 +5,9 @@ object Constants {
     const val LOTTO_NUMBER_END = 45
     const val LOTTO_TOTAL_NUMBER = 6
     const val LOTTO_AMOUNT_UNIT = 1000
+    const val FIRST_PRIZE_AMOUNT = 2000000000
+    const val SECOND_PRIZE_AMOUNT = 30000000
+    const val THIRD_PRIZE_AMOUNT = 1500000
+    const val FOURTH_PRIZE_AMOUNT = 50000
+    const val FIFTH_PRIZE_AMOUNT = 5000
 }
