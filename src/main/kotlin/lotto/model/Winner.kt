945feb175c38616db5e9de4d3d26706fd4c8454f
민대인 <bamin0422@gmail.com>
2023-11-05 03:35:39 +0900

    feat: 당첨 내역 출력 메서드 printWinningDetails 구현

--- src/main/kotlin/lotto/model/Winner.kt
+++ src/main/kotlin/lotto/model/Winner.kt
@@ -1,9 +1,9 @@
 package lotto.model
 
 enum class Winner(val message: String, val correspondNumber: Int) {
-    FIRST_GRADE("6개 일치 (2,000,000,000원) - ", 6),
-    SECOND_GRADE("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5),
-    THIRD_GRADE("5개 일치 (1,500,000원) - ", 5),
+    FIFTH_GRADE("3개 일치 (5,000원) - ", 3),
     FOURTH_GRADE("4개 일치 (50,000원) - ", 4),
-    FIFTH_GRADE("3개 일치 (5,000원) - ", 3)
+    THIRD_GRADE("5개 일치 (1,500,000원) - ", 5),
+    SECOND_GRADE("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5),
+    FIRST_GRADE("6개 일치 (2,000,000,000원) - ", 6),
 }
--- src/main/kotlin/lotto/util/OutputUtil.kt
+++ src/main/kotlin/lotto/util/OutputUtil.kt
@@ -1,12 +1,15 @@
 package lotto.util
 
 import lotto.model.Lotto
+import lotto.model.Winner
+import java.util.*
 
 object OutputUtil {
     private const val MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요."
     private const val MESSAGE_PURCHASED_LOTTO = "개를 구매했습니다."
     private const val MESSAGE_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
     private const val MESSAGE_INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요."
+    private const val MESSAGE_WINNING_DETAIL = "\n당첨 통계\n---"
 
 
     fun printInputMoney() {
@@ -31,5 +34,12 @@ object OutputUtil {
 
     fun printInputBonusNumber() {
         println(MESSAGE_INPUT_BONUS_NUMBER)
+    }
+
+    fun printWinningDetails(winningMap: EnumMap<Winner, Int>, ) {
+        println(MESSAGE_WINNING_DETAIL)
+        Winner.values().forEach {_winner ->
+            println("${_winner.message}${winningMap.getOrDefault(_winner, 0)}개")
+        }
     }
 }
