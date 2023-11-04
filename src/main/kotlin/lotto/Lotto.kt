b4367ee2b0146d42f13cbc0fa8e5842286d27580
Hogu59 <dbstjdguswkd@naver.com>
2023-11-04 11:09:02 +0900

    fix: 로또 번호 오름차순 출력 오류 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,11 +5,10 @@ import camp.nextstep.edu.missionutils.*
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-        numbers.sorted()
     }
 
     fun getLottoNumberString() : String {
-        return numbers.toString()
+        return numbers.sorted().toString()
     }
 
     fun getRank(winningNumbers: List<Int>, bonusNumber : Int) : Int {
