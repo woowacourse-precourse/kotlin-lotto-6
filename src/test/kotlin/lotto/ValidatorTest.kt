3bdab3d54eebe5b94d013ca05d496e18c587f44a
getupminaaa <getupmina@gmail.com>
2023-11-04 17:33:10 +0900

    test: 당첨 번호 값이 유효한지에 대해 테스트 작성

--- src/test/kotlin/lotto/ValidatorTest.kt
+++ src/test/kotlin/lotto/ValidatorTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import lotto.util.Validator.isValidPurchaseAmount
+import lotto.util.Validator.isValidWinningNums
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -9,7 +10,7 @@ import org.junit.jupiter.api.assertThrows
 class ValidatorTest {
     @Test
     @DisplayName("구입 금액 입력이 유효하지 않다면 예외가 발생한다")
-    fun validatePurchaseAmount () {
+    fun validatePurchaseAmount() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
                 isValidPurchaseAmount("abcd")
@@ -30,6 +31,30 @@ class ValidatorTest {
                 isValidPurchaseAmount("8765")
             }
         }
+    }
 
+    @Test
+    @DisplayName("당첨 번호 입력이 유효하지 않는다면 예외를 발생한다")
+    fun validateWinningNums() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("")
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("1,2,3, ,5,6")
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("1,2")
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("a,b,c,d,e,f")
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("1,1,2,3,4,5")
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidWinningNums("0,2,3,4,47,100")
+            }
+        }
     }
 }
