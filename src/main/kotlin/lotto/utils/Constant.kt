4256545699f246a0b386d52773634b22e7623802
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:16:19 +0900

    feat: 당첨 정보와 출력 정보 상수화

--- src/main/kotlin/lotto/utils/Constant.kt
+++ src/main/kotlin/lotto/utils/Constant.kt
@@ -6,6 +6,31 @@ object Constant {
     const val MIN_LOTTO_NUMBER = 1
     const val MAX_LOTTO_NUMBER = 45
     const val LOTTO_SIZE = 6
+
+    // 상금 정보
+    const val FIRST_REWARD = 2_000_000_000
+    const val SECOND_REWARD = 30_000_000
+    const val THIRD_REWARD = 1_500_000
+    const val FOURTH_REWARD = 50_000
+    const val FIFTH_REWARD = 5_000
+
+    // 당첨 정보
+    const val FIRST_REWARD_MATCHED_COUNT = 6
+    const val SECOND_REWARD_MATCHED_COUNT = 5
+    const val THIRD_REWARD_MATCHED_COUNT = 5
+    const val FOURTH_REWARD_MATCHED_COUNT = 4
+    const val FIFTH_REWARD_MATCHED_COUNT = 3
+
+    // 상금 문자열 포맷
+    const val FIRST_REWARD_PRIZE = "2,000,000,000"
+    const val SECOND_REWARD_PRIZE = "30,000,000"
+    const val THIRD_REWARD_PRIZE = "1,500,000"
+    const val FOURTH_REWARD_PRIZE = "50,000"
+    const val FIFTH_REWARD_PRIZE = "5,000"
+
+    // 퍼센트
+    const val PERCENT = 100
+
     // 출력 메시지
     const val PURCHASE_AMOUNT_MESSAGE = "구매 금액을 입력해 주세요."
     const val LOTTO_QUANTITY_MESSAGE = "\n%d개를 구매했습니다."
