f5ff44511c45d7d63eea3241a351f7bca65631ca
stopstone <wltjr6432@naver.com>
2023-11-04 15:33:32 +0900

    test: 구입금액 입력에 대한 예외처리 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.Controller.LottoManager
+import lotto.validate.Validate
 import lotto.view.InputView
 import lotto.view.OutputView
 import org.junit.jupiter.api.Test
@@ -28,5 +29,37 @@ class LottoTest {
     fun `구입금액에 따라 티켓이 변경되는지 확인한다`() {
         val Manager = LottoManager(InputView(), OutputView())
             Manager.calculateTicket(1000).equals(1)
+    }
+
+    @Test
+    fun `구입금액이 1000원이 안되는 경우`() {
+        val validate = Validate()
+        assertThrows<IllegalArgumentException> {
+            validate.validateInputPrice("990")
+        }
+    }
+
+    @Test
+    fun `구입금액 입력이 숫자가 아닌경우`() {
+        val validate = Validate()
+        assertThrows<IllegalArgumentException> {
+            validate.validateInputPrice("wooteco")
+        }
+    }
+
+    @Test
+    fun `구입금액 입력이 없는 경우`() {
+        val validate = Validate()
+        assertThrows<IllegalArgumentException> {
+            validate.validateInputPrice("")
+        }
+    }
+
+    @Test
+    fun `구입금액 입력이 1000원으로 안나눠지는 수일 경우`() {
+        val validate = Validate()
+        assertThrows<IllegalArgumentException> {
+            validate.validateInputPrice("1111")
+        }
     }
 }
