2bfcd59db89c505280fbed069691779e6501778c
stopstone <wltjr6432@naver.com>
2023-11-05 03:51:28 +0900

    feat: 보너스번호 입력이 없을경우 예외처리

--- src/main/kotlin/lotto/validate/validateBonus.kt
+++ src/main/kotlin/lotto/validate/validateBonus.kt
@@ -5,6 +5,8 @@ import lotto.utils.Messages
 class validateBonus {
     fun validateBonus(bonusNumber: Int, winningNumbers: List<Int>) {
         validateDuplicateBonusNumber(bonusNumber, winningNumbers)
+        validateInputIsEmpty(bonusNumber.toString())
+
     }
 
     private fun validateDuplicateBonusNumber(bonus: Int, winningNumbers: List<Int>) {
@@ -12,4 +14,12 @@ class validateBonus {
             "${Messages.ERROR_MESSAGE} ${Messages.MY_NUMBERS_DUPLICATED_MESSAGE}"
         }
     }
+
+    private fun validateInputIsEmpty(myNumbers: String) {
+        require(myNumbers.isNotEmpty()) {
+            "${Messages.ERROR_MESSAGE} ${Messages.VALIDATE_INPUT_EMPTY}"
+        }
+    }
+
+
 }
