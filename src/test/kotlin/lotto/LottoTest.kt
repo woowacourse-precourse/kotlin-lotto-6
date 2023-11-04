18471816c1935564e9f26bc70a5475672e1ac425
minuxx <minux8905@gmail.com>
2023-11-05 01:23:38 +0900

    test: 로또 번호 범위 예외 발생 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -18,6 +18,18 @@ class LottoTest {
         assertThat(exception.message).isEqualTo(Lotto.INVALID_NUMBER_COUNT_ERROR_MESSAGE)
     }
 
+    @Test
+    fun `로또 번호의 범위가 1이상 45이하가 아니면 예외가 발생한다`() {
+        // given
+        val numbers = listOf(0, 2, 45, 4, 5, 6)
+
+        // when
+        val exception = assertThrows<IllegalArgumentException> { Lotto(numbers) }
+
+        // then
+        assertThat(exception.message).isEqualTo(Lotto.INVALID_RANGE_NUMBER)
+    }
+
     // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
