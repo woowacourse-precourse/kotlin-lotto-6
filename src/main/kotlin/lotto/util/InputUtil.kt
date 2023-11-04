3577e51bbab5956d6d327cee3a6383f626dd84c8
민대인 <bamin0422@gmail.com>
2023-11-04 16:08:54 +0900

    feat: inputMoney 메서드 예외처리

--- src/main/kotlin/lotto/util/InputUtil.kt
+++ src/main/kotlin/lotto/util/InputUtil.kt
@@ -1,9 +1,23 @@
 package lotto.util
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.constants.Error.EXCEPTION_MESSAGE_MONEY_NOT_DIVIDED_PRICE
+import lotto.constants.Error.EXCEPTION_MESSAGE_MONEY_NOT_NUMBER
+import lotto.constants.LottoConstants
 
 object InputUtil {
 
-    fun inputMoney() =
-        Console.readLine().toInt()
+    fun inputMoney(): Int =
+        checkMoneyAvailable(Console.readLine())
+
+    private fun checkMoneyAvailable(moneyString: String): Int =
+        try {
+            val money = moneyString.toInt()
+            if (money % LottoConstants.LOTTO_PRICE != 0) {
+                throw  IllegalArgumentException(EXCEPTION_MESSAGE_MONEY_NOT_DIVIDED_PRICE)
+            }
+            money
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(EXCEPTION_MESSAGE_MONEY_NOT_NUMBER)
+        }
 }
