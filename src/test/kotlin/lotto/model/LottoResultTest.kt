20c48db10d78cdfd56321c0b1f239596926eb2c8
lh99j <grand2181@gmail.com>
2023-11-04 17:40:27 +0900

    test(LottoResultTest): calculateRanking 테스트케이스 작성

---
+++ src/test/kotlin/lotto/model/LottoResultTest.kt
@@ -0,0 +1,20 @@
+package lotto.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class LottoResultTest {
+    private val lottoWinningNumber = listOf(1, 2, 3, 4, 5, 6)
+    private val bonusNumber = 7
+    private val lottoResult = LottoResult(lottoWinningNumber, bonusNumber)
+
+    @Test
+    @DisplayName("로또 랭킹이 올바르게 반환되는지 확인한다.")
+    fun calculateRankingTest() {
+        val input = listOf(1, 2, 3, 4, 10, 11)
+        val validation = lottoResult.calculateRanking(input)
+        val result = 4
+        assertThat(validation).isEqualTo(result)
+    }
+}
