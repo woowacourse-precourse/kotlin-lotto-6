fe93760b57ec8119452e0caa9a4a6fe5145a9e35
junyong <junyong008@gmail.com>
2023-11-04 22:35:57 +0900

    test(LottoNumber): 객체 생성 테스트 추가

---
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -0,0 +1,24 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoNumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = [1, 45, 20])
+    fun `정상적인 객체 생성`(input: Int) {
+        // when & then
+        assertDoesNotThrow { LottoNumber(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1, 50])
+    fun `범위 초과 예외 처리`(input: Int) {
+        // when & then
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { LottoNumber(input) }
+            .withMessage("[ERROR] 로또 숫자 범위는 1부터 45까지 입니다.")
+    }
+}
