15eef3e95e4a0b2dc400f177f1cc04c2e5ff9941
민대인 <bamin0422@gmail.com>
2023-11-04 13:56:38 +0900

    feat: showBuyViewAndReturnMoney 메서드 구현

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -1,4 +1,11 @@
 package lotto.view
 
+import lotto.util.InputUtil
+import lotto.util.OutputUtil
+
 class LottoView {
+    fun showBuyViewAndReturnMoney(): Int {
+        OutputUtil.printInputMoney()
+        return InputUtil.inputMoney()
+    }
 }
