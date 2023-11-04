cbd402f7532fa03c2f5b8f99559213576f9702ef
jaehan4707 <jahan4707@naver.com>
2023-11-04 18:50:58 +0900

    feat : 로또 번호 오름차순 정렬 구현

--- src/main/kotlin/lotto/model/LottoNumberGenerator.kt
+++ src/main/kotlin/lotto/model/LottoNumberGenerator.kt
@@ -7,5 +7,5 @@ import lotto.util.Constant.LOTTO_NUMBER_SIZE
 
 object LottoNumberGenerator {
 
-    fun makeLottoNumber(): List<Int> = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_RANDOM_NUMBER,LOTTO_MAX_RANDOM_NUMBER,LOTTO_NUMBER_SIZE)
+    fun makeLottoNumber(): List<Int> = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_RANDOM_NUMBER,LOTTO_MAX_RANDOM_NUMBER,LOTTO_NUMBER_SIZE).sorted()
 }
