c27b6b653d310b939064ffc05a553a00ea3588e2
sunkk8482 <sunkk8482@naver.com>
2023-11-04 21:51:14 +0900

    Feat : 메인 함수에서 기능 실행

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,39 @@
 package lotto
 
+import lotto.domain.Consumer
+import lotto.domain.WinningCheck
+import lotto.domain.prizeCheck
+import lotto.views.InputView
+import lotto.views.OutputView
+fun enterMoney() :Int{
+    OutputView.printEnterMoney()
+    return InputView.enterMoney()
+}
+
+
+fun enterWinningNumber (): List<Int>{
+    OutputView.printEnterWinningNumber()
+    return InputView.enterWinningNumber()
+}
+
+fun enterBonusNumber(winningNumber:List<Int>) : Int{
+    OutputView.printEnterBonusNumber()
+    return InputView.enterBonusNumber(winningNumber)
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    val consumer= Consumer()
+    val manage = consumer.getManager()
+    val myMoney = enterMoney()
+    consumer.purchaseLotto(myMoney/1000)
+    OutputView.printLotto(manage.getLotto())
+    val winningNumber = enterWinningNumber()
+    val bonusNumber = enterBonusNumber(winningNumber)
+    val winningChecker = WinningCheck()
+    val reward = winningChecker.numbersCheck(manage.getLotto(),winningNumber,bonusNumber)
+    val prizeChecker = prizeCheck()
+    OutputView.printReward(reward)
+    println(prizeChecker.checkPrize(reward,myMoney))
+
+
 }
