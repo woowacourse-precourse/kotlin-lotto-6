45a4919193789d2fd1b0de90212956ebcfb677b9
jiwon2724 <stopone3119@naver.com>
2023-11-05 02:53:49 +0900

    [feat] 올바르지 않은 보너스 번호 출력 함수 구현

--- src/main/kotlin/ui/OutputManager.kt
+++ src/main/kotlin/ui/OutputManager.kt
@@ -4,6 +4,7 @@ class OutputManager {
     fun promptPurchaseAmount() = println(OutputMessages.PROMPT_PURCHASE_AMOUNT)
     fun invalidPurchaseAmount() = println("${OutputMessages.INVALID_PURCHASE_AMOUNT} ${OutputMessages.RE_TRY}")
     fun invalidLottoNumbers() = println("${OutputMessages.INVALID_LOTTO_NUMBERS} ${OutputMessages.RE_TRY}")
+    fun invalidBonusNumber() = println("${OutputMessages.INVALID_BONUS_NUMBER} ${OutputMessages.RE_TRY}")
     fun purchaseLottoCount(lottoCount: Int) = println(OutputMessages.PURCHASE_LOTTO_COUNT.toString().format(lottoCount))
     fun lottoNumbers(numbers: List<Int>) = println(numbers)
     fun promptJackpotNumbers() = println(OutputMessages.PROMPT_JACKPOT_NUMBERS)
@@ -22,6 +23,7 @@ enum class OutputMessages(private val message: String) {
     ERROR("[ERROR]"),
     INVALID_PURCHASE_AMOUNT("$ERROR 유효하지 않은 금액입니다."),
     INVALID_LOTTO_NUMBERS("$ERROR 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_BONUS_NUMBER("$ERROR 보너스 번호는 당첨번호와 중복되면 안됩니다."),
     PROMPT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PROMPT_JACKPOT_NUMBERS("당첨 번호를 입력해 주세요."),
     PROMPT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
