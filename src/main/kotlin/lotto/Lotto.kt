e7332a685e6a7e75ab97544502711b220f5e5226
taewoong98 <kcong1@ajou.ac.kr>
2023-11-05 01:12:46 +0900

    feat: Lotto 객체 구성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,8 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "배열의 길이가 6이 아닙니다." }
+        require(numbers.all { it in 1..45}) { "1 ~ 45 범위 밖의 숫자가 존재합니다." }
     }
 
     // TODO: 추가 기능 구현
