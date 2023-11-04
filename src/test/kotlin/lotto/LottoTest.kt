c75dd0afcfde50e3c3e80fbeb2ea267c923406f1
book3039 <book3039@knou.ac.kr>
2023-11-04 12:02:27 +0900

    test: 로또 번호에 1미만 45 초과의 숫자가 있으면 예외 발생 테스트 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -21,6 +21,20 @@ class LottoTest {
     }
 
     @Test
+    fun `로또 번호에 1 미만의 숫자가 있으면 에외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호에 45 초과의 숫자가 있으면 에외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 44, 45, 46))
+        }
+    }
+
+    @Test
     fun `로또 번호를 오름차순으로 정렬하여 반환한다`() {
         assertThat(Lotto(listOf(6, 5, 4, 3, 2, 1)).getAscendingList()).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
     }
