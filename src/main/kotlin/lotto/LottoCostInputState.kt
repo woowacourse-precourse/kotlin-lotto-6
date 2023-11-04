70c542e6f3a53be471ad335849b13f63e8ee9c6a
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-11-04 19:36:02 +0900

    Refactor: "분리 LottoInputState"
    enum class 였던 LottoInputState를 LottoInputHandler로부터 분리

---
+++ src/main/kotlin/lotto/LottoCostInputState.kt
@@ -0,0 +1,7 @@
+package lotto
+
+enum class LottoCostInputState {
+    SUCCESSFUL,
+    NOT_DIVISIBLE_BY_1000,
+    OUT_OF_RANGE
+}
--- src/main/kotlin/lotto/LottoInputHandler.kt
+++ src/main/kotlin/lotto/LottoInputHandler.kt
@@ -11,21 +11,15 @@ object LottoInputHandler {
 
     fun isValidCost(cost : Int?) : Boolean{
         cost?.let {
-            if(isMultipleOfThousand(it) == LottoInputState.SUCCESSFUL && isInRange(it) == LottoInputState.SUCCESSFUL){
+            if(isMultipleOfThousand(it) == LottoCostInputState.SUCCESSFUL && isInRange(it) == LottoCostInputState.SUCCESSFUL){
                 return true
             }
         }
         return false
     }
 
-    private fun isMultipleOfThousand(int : Int) = if(int % 1000 == 0) LottoInputState.SUCCESSFUL else LottoInputState.NOT_DIVISIBLE_BY_1000
+    private fun isMultipleOfThousand(int : Int) = if(int % 1000 == 0) LottoCostInputState.SUCCESSFUL else LottoCostInputState.NOT_DIVISIBLE_BY_1000
 
-    private fun isInRange(int : Int) = if (int <= 45 && int >= 1) LottoInputState.SUCCESSFUL else LottoInputState.OUT_OF_RANGE
+    private fun isInRange(int : Int) = if (int <= 45 && int >= 1) LottoCostInputState.SUCCESSFUL else LottoCostInputState.OUT_OF_RANGE
 
-
-    enum class LottoInputState {
-        SUCCESSFUL,
-        NOT_DIVISIBLE_BY_1000,
-        OUT_OF_RANGE
-    }
 }
