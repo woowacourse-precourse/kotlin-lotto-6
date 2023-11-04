a5416c84ca93354278d618b85d34416204b9b6e1
lh99j <grand2181@gmail.com>
2023-11-04 17:24:59 +0900

    feat(LottoResult): 당첨개수 계산하는 클래스 생성

---
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -0,0 +1,23 @@
+package lotto.model
+
+class LottoResult(
+    private val winningNumbers: List<Int>,
+    private val bonus: Int
+) {
+    fun calculateRanking(lottoTicket: List<Int>): Int {
+        var winningCount = 0
+        winningNumbers.forEach {
+            if (lottoTicket.contains(it)) {
+                winningCount++
+            }
+        }
+
+        if (winningCount == 5) {
+            if (lottoTicket.contains(bonus)) {
+                winningCount++
+            }
+        }
+
+        return winningCount
+    }
+}
