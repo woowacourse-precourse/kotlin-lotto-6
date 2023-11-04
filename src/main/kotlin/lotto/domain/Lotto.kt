356920d7b9824455e579c40bd6ecafc7ff844ca7
junyong <junyong008@gmail.com>
2023-11-04 22:41:09 +0900

    refactor(Lotto): 로또 숫자 개수 상수화

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,13 +3,17 @@ package lotto.domain
 import lotto.exception.LottoException
 
 class Lotto(private val numbers: List<LottoNumber>) : List<LottoNumber> by numbers {
+    companion object {
+        const val LOTTO_SIZE = 6
+    }
+
     init {
         checkLottoSize(numbers)
         checkNumberDuplicate(numbers)
     }
 
     private fun checkLottoSize(numbers: List<LottoNumber>) =
-        require(numbers.size == 6) { LottoException.NUMBER_SIZE_NOT_MATCH.message }
+        require(numbers.size == LOTTO_SIZE) { LottoException.NUMBER_SIZE_NOT_MATCH.message }
 
     private fun checkNumberDuplicate(numbers: List<LottoNumber>) =
         require(numbers.toSet().size == numbers.size) { LottoException.DUPLICATE_NUMBER_EXIST.message }
