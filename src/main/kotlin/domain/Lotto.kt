e0b1449ca15b22f1239e7564eddeeda65ce9bc58
jiwon2724 <stopone3119@naver.com>
2023-11-04 21:46:39 +0900

    [feat] 로또 번호 검증 함수 추가

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -3,6 +3,7 @@ package domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct().size != 6)
     }
     fun getNumbers() = numbers
 }
