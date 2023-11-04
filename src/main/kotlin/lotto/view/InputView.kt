dcdecc8f9030cb78d1810cccb87ec6682718b3c7
hongjeZZ <ajyong316@gmail.com>
2023-11-04 19:45:32 +0900

    feat: 사용자로부터 구입금액 입력받는 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,7 +1,10 @@
 package lotto.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
-    fun readInputMoneyFromUser() {
+    fun readInputMoneyFromUser(): String {
         println("구입금액을 입력해 주세요.")
+        return Console.readLine().trim()
     }
 }
