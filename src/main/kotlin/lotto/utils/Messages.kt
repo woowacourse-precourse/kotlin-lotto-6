062424b7a812c337a0e73dbd885abfe03f294e8d
stopstone <wltjr6432@naver.com>
2023-11-04 14:13:48 +0900

    feat: 구입금액 입력 메시지 출력 함수 추가

---
+++ src/main/kotlin/lotto/utils/Messages.kt
@@ -0,0 +1,5 @@
+package lotto.utils
+
+object Messages {
+    const val BUY_PRICE_MESSAGE = "구입금액을 입력해주세요."
+}
---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,9 @@
+package lotto.view
+
+import lotto.utils.Messages
+
+class OutputView {
+    fun showInputBuyPriceMessage() {
+        println("{Messages.BUY_PRICE_MESSAGE}")
+    }
+}
