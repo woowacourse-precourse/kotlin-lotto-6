c9df84af5415396ee0705c660642a8c97869bade
getupminaaa <getupmina@gmail.com>
2023-11-04 15:51:48 +0900

    feat: "보너스 번호를 입력해주세요." 를 출력하는 기능 구현

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -3,6 +3,7 @@ package lotto.view
 private const val PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요."
 private const val LOTTO_COUNT_MSG = "%d개를 구매했습니다."
 private const val LOTTO_WINNING_NUMS_MSG = "당첨 번호를 입력해 주세요."
+private const val LOTTO_BONUS_NUM_MSG = "보너스 번호를 입력해 주세요."
 
 
 class OutputView {
@@ -19,5 +20,8 @@ class OutputView {
     }
     fun printRequireWinningNums(){
         println("\n$LOTTO_WINNING_NUMS_MSG")
+    }
+    fun printRequireBonusNums(){
+        println("\n$LOTTO_BONUS_NUM_MSG")
     }
 }
