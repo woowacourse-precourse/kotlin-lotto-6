7561ce87682a287e3cb91586e144c7028b4fff8a
junyong <junyong008@gmail.com>
2023-11-04 16:51:02 +0900

    refactor(MoneyException): 로또 가격 정보 제거

--- src/main/kotlin/lotto/exception/MoneyException.kt
+++ src/main/kotlin/lotto/exception/MoneyException.kt
@@ -1,8 +1,5 @@
 package lotto.exception
 
 enum class MoneyException(val message: String) {
-    NOT_DIVISIBLE("[ERROR] 구입금액은 %d원 단위로 입력해야 합니다."),
-    LESS_THAN_DIVISOR("[ERROR] 구입금액은 최소 %d원 이상 입력해야 합니다.");
-
-    fun getDivisor(divisor: Int): String = message.format(divisor)
+    NOT_POSITIVE_NUMBER("[ERROR] 금액은 음수가 될 수 없습니다.")
 }
