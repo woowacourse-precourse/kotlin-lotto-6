aa937f75b9a40426be6dc50e3b3f9e4fc96bf461
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-05 03:37:09 +0900

    feat: 1,000원 단위가 아닐 경우 예외 발생 기능 작성

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -5,7 +5,12 @@ object Validator {
         require(price.all { it.isDigit() }) { Error.INPUT_ONLY_NUMBER.message }
     }
 
+    fun isAmountInThousandWon(price: String) {
+        require(price.toInt() % 1000 == 0) { Error.PURCHASE_IN_THOUSAND_WON.message }
+    }
+
     private enum class Error(val message: String) {
         INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
+        PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
     }
 }
