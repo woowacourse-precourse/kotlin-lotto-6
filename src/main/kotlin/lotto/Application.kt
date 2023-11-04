50fb96340d6e77ff1df1ffd60f5cb0e48e386088
minuxx <minux8905@gmail.com>
2023-11-05 01:46:31 +0900

    feat: 로또 발행 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,7 +3,11 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-
-    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-    val lotto = Lotto(numbers)
+    publishLotto()
 }
+
+// 로또 번호 생성을 캡슐화를 해야 하나 고민
+fun publishLotto(): Lotto = Lotto(generateLottoNumbers())
+
+fun generateLottoNumbers(): List<Int> =
+    Randoms.pickUniqueNumbersInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER, Lotto.NUMBER_COUNT)
