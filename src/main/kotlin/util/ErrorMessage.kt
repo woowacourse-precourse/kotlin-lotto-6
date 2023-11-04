d27b0cb151f3fcbf0f4839b3fe30fcd3f2ecd2e0
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 00:44:34 +0900

    feat: 로또 보너스 번호 예외처리 에레 메세지 추가
    로또 보너스 번호 입력 예외처리에 필요한 에러 메세지를 추가하였다.

--- src/main/kotlin/util/ErrorMessage.kt
+++ src/main/kotlin/util/ErrorMessage.kt
@@ -6,5 +6,6 @@ enum class ErrorMessage(val message: String) {
     INVALID_PURCHASE_AMOUNT_UNIT("로또 구입 금액은 1,000원 단위로 입력 가능합니다."),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자만 가능합니다."),
     DUPLICATED_LOTTO_NUMBER("로또 번호는 중복되지 않는 숫자만 가능합니다."),
-    INVALID_LOTTO_COUNT("로또 번호는 6개의 숫자만 가능합니다.")
+    INVALID_LOTTO_COUNT("로또 번호는 6개의 숫자만 가능합니다."),
+    DUPLICATED_BONUS_NUMBER("로또 보너스 번호는 당첨 번호와 중복되지 않는 숫자만 가능합니다.")
 }
