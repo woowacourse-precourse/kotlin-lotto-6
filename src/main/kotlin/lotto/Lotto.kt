882acf0956f796073420efe01c8718d94ab2733c
book3039 <book3039@knou.ac.kr>
2023-11-04 10:54:23 +0900

    feat: 로또 번호에 중복된 숫자 발생시 예외처리 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(
+            numbers.distinct()
+                .size == 6
+        )
     }
-
-    // TODO: 추가 기능 구현
 }
