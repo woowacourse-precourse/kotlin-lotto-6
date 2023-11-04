0e490d48cb0b110d0516b39a6905e2832cb9108b
junyong <junyong008@gmail.com>
2023-11-04 22:33:38 +0900

    refactor(LottoNumber): 번호 범위 상수화

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,7 +1,16 @@
 package lotto.domain
 
+import lotto.exception.LottoNumberException
+
 data class LottoNumber(private val value: Int) {
+    companion object {
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
+    }
+
     init {
-        require(value in 1..45)
+        require(value in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
+            LottoNumberException.OUT_RANGE.getLottoNumberRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+        }
     }
 }
