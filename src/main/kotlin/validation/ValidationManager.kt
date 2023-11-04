0c2cb43e31f5c66e1efb1c972e226e013234f437
jiwon2724 <stopone3119@naver.com>
2023-11-04 12:23:52 +0900

    [feat] 로또 입력에 필요한 함수 구현

--- src/main/kotlin/validation/ValidationManager.kt
+++ src/main/kotlin/validation/ValidationManager.kt
@@ -6,12 +6,22 @@ class ValidationManager {
     fun validateAmount(lottoPurchaseAmount: Int): Int =
         lottoPurchaseAmount.lessThanZero().modulusLottoPrice()
 
+    fun validateJackpotNumbers(numbers: String): List<Int> {
+        val jackpotNumbers: ArrayList<Int> = arrayListOf()
+        numbers.withCommaToList().forEach { number ->
+            jackpotNumbers.add(number.toValidNumber().lessThanZero())
+        }
+        return jackpotNumbers.isCorrectLottoCount()
+    }
 
     internal fun Int.lessThanZero(): Int = require(this >= 0).let { return this }
     internal fun Int.modulusLottoPrice(): Int = require(this % LOTTO_PRICE == 0).let { return this }
+    internal fun String.toValidNumber(): Int = require(toIntOrNull() != null).let { return this.toInt() }
+    internal fun List<Int>.isCorrectLottoCount(): List<Int> = require(size == LOTTO_COUNT).let { return this }
 
 
     companion object {
         private const val LOTTO_PRICE = 1000
+        private const val LOTTO_COUNT = 6
     }
 }
