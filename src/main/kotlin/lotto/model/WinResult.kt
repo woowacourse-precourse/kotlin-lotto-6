2977e2926012b6ec51c3cbdc565ed227511051db
jaehan4707 <jahan4707@naver.com>
2023-11-04 18:47:41 +0900

    feat : 수익률 계산 구현

--- src/main/kotlin/lotto/model/WinResult.kt
+++ src/main/kotlin/lotto/model/WinResult.kt
@@ -3,7 +3,8 @@ package lotto.model
 class WinResult(private val user : User, private val winningLotto : WinningLotto) {
     private var _placeResult =  hashMapOf<Place,Int>()
     val placeResult get() = _placeResult
-
+    private var _earningRate = 0.0
+    val earningRate get() = _earningRate
 
     fun calculateResult(){
         for(lotto in user.lottoes){
@@ -18,5 +19,13 @@ class WinResult(private val user : User, private val winningLotto : WinningLotto
         val matchNumbers = lottoNumbers.count{lottoNumber -> lottoNumber in luckyNumbers}
         val matchBonus = lottoNumbers.contains(bonusNumber)
         return Place.decidePlace(matchNumbers,matchBonus)
+    }
+
+    fun calculateEarningRate(price : Int) : Double{
+        var earnMoney = 0
+        for(place in _placeResult){
+            earnMoney += place.key.price * place.value
+        }
+        return (earnMoney / price.toDouble()) * 100
     }
 }
