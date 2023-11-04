1a150d29b404d59884753aeed2eaac46c453cfd7
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:03:44 +0900

    feat: 당첨 번호 입력을 요청하는 기능

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,7 +1,9 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.model.Lotto
 import lotto.utils.Exceptions.inputPurchaseAmountException
+import lotto.utils.Exceptions.inputWinningNumberException
 
 
 class InputView {
@@ -9,6 +11,14 @@ class InputView {
     fun inputPurchaseAmount(): Int {
         while (true) {
             inputPurchaseAmountException(Console.readLine().trim())
+                .onSuccess { return it }
+                .onFailure { println(it) }
+        }
+    }
+
+    fun inputWinningNumber(): Lotto {
+        while (true) {
+            inputWinningNumberException(Console.readLine().trim().split(","))
                 .onSuccess { return it }
                 .onFailure { println(it) }
         }
