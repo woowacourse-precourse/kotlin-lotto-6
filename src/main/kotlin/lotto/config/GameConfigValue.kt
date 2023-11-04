589b9df45afc2eb556b91facf7b9066cdd1549f7
applepepsi <dlaejrtnz@gmail.com>
2023-11-05 02:09:22 +0900

    feat(lotto/config): Create exception handling messages and game settings values

---
+++ src/main/kotlin/lotto/config/GameConfigValue.kt
@@ -0,0 +1,8 @@
+package lotto.config
+
+object GameConfigValue {
+    const val LOTTO_DEFAULT_DIGIT=6
+    const val PURCHASE_AMOUNT_UNIT=1000
+    const val MINIMUM_LOTTO_NUMBER=1
+    const val MAXIMUM_LOTTO_NUMBER=45
+}
