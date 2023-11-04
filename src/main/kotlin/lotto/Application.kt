670b8eb396cb87ab9ef500e5048c73a6d2e1b207
Nohsein <si2238@naver.com>
2023-11-05 01:39:42 +0900

    feat: 로또 금액 입력
    1000원 단위 및 1000원 이상 아닐 시 에러 메시지 후 재입력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,30 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.Constants.AMOUNT_ERROR
+import lotto.Constants.ASK_AMOUNT
+import lotto.Constants.PRICE
+
 fun main() {
-    TODO("프로그램 구현")
+    askAmount()
+}
+
+fun askAmount() {
+    try {
+        println(ASK_AMOUNT)
+        validAmount()
+    } catch (e: Exception) {
+        println(AMOUNT_ERROR)
+        askAmount()
+    }
+}
+
+fun validAmount(): Int {
+    try {
+        val cost = Console.readLine().toInt()
+        require(cost % PRICE == 0 && cost >= PRICE) { AMOUNT_ERROR }
+        return cost
+    } catch (e: Exception) {
+        throw IllegalArgumentException(AMOUNT_ERROR)
+    }
 }
---
+++ src/main/kotlin/lotto/Constants.kt
@@ -0,0 +1,8 @@
+package lotto
+
+object Constants {
+    const val PRICE = 1000
+
+    const val ASK_AMOUNT = "구입금액을 입력해 주세요."
+    const val AMOUNT_ERROR = "[ERROR] 1000원 단위의 숫자를 입력하세요.\n"
+}
