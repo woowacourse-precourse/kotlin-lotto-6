daf76027cabc707fa845d08b8df9514413d86ffa
Sheep1sik <bohg6799@naver.com>
2023-11-04 19:44:04 +0900

    feat: 티켓의 수만큼 반복 해주는 기능 구현
    - 티켓의 수만큼 랜덤 로또 리스트 안에 입력한 로또 번호와 보너스 번호가 포함되어 있는지 확인

--- src/main/kotlin/lotto/LottoGameService.kt
+++ src/main/kotlin/lotto/LottoGameService.kt
@@ -21,6 +21,20 @@ class LottoGameService {
         }
         return purchaseLottoLists
     }
+    // 랜덤 로또 번호 리스트와 입력한 로또 번호를 비교해주는 함수
+    fun calculateWinningStatistics(
+        lotto: List<Int>,
+        bonus: Int,
+        randomLottoLists: MutableMap<Int, List<Int>>,
+        ticket: Int
+    ) {
+        for (ticketCheck in 0 until ticket){
+            val winningLottoCheck = checkWinningLottoNumber(lotto, randomLottoLists, ticketCheck)
+            val bonusNumberCheck = checkWinningBonusNumber(bonus, randomLottoLists, ticketCheck)
+            saveWinningStatsToRank(winningLottoCheck,bonusNumberCheck,ticketCheck)
+        }
+    }
+    // 랜덤 로또 리스트에 입력한 로또 번호가 몇개 포함되어 있는지 확인하는 함수
     private fun checkWinningLottoNumber(lotto: List<Int>, randomLottoLists: MutableMap<Int, List<Int>>, ticketCheck: Int): Int {
         return randomLottoLists[ticketCheck]!!.intersect(lotto.toSet()).count()
     }
