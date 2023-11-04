932ba1ccd33af1697fb86a5de31b515dbef9bee4
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:02:35 +0900

    feat: 당첨 번호 입력을 예외 처리하는 기능

--- src/main/kotlin/lotto/utils/Exceptions.kt
+++ src/main/kotlin/lotto/utils/Exceptions.kt
@@ -1,8 +1,13 @@
 package lotto.utils
 
+import lotto.model.Lotto
 import lotto.utils.Constant.AMOUNT_NOT_DIVISIBLE_ERROR_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE
+import lotto.utils.Constant.WINNING_NUMBER_RANGE_ERROR_MESSAGE
+import lotto.utils.Constant.INVALID_WINNING_NUMBER_ERROR_MESSAGE
 import lotto.utils.Constant.LOTTO_COST
+import lotto.utils.Constant.MAX_LOTTO_NUMBER
+import lotto.utils.Constant.MIN_LOTTO_NUMBER
 
 
 object Exceptions {
@@ -15,6 +20,21 @@ object Exceptions {
         }.recoverCatching { e ->
             when (e) {
                 is NumberFormatException -> throw NumberFormatException(PURCHASE_AMOUNT_INPUT_ERROR_MESSAGE)
+                else -> throw e
+            }
+        }
+    }
+
+    fun inputWinningNumberException(winningNumber: List<String>): Result<Lotto> {
+        return runCatching {
+            Lotto(winningNumber.map {
+                val intValue = it.toInt()
+                require(intValue in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { WINNING_NUMBER_RANGE_ERROR_MESSAGE }
+                intValue
+            })
+        }.recoverCatching { e ->
+            when (e) {
+                is NumberFormatException -> throw NumberFormatException(INVALID_WINNING_NUMBER_ERROR_MESSAGE)
                 else -> throw e
             }
         }
