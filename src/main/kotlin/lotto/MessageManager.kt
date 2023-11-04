e01921c2d8f5bb70ba887e14815183e59a1bdb54
Dongsu Lee <dongsu0717@nate.com>
2023-11-05 05:19:34 +0900

    기본 코멘트들 object로 저장

---
+++ src/main/kotlin/lotto/MessageManager.kt
@@ -0,0 +1,8 @@
+package lotto
+
+object MessageManager {
+    const val COMMENTS_HOW_MANY_BUY = "구입금액을 입력해 주세요."
+    const val COMMENTS_MAIN_NUMBER = "당첨 번호를 입력해 주세요."
+    const val COMMENTS_SUB_NUMBER = "보너스 번호를 입력해 주세요."
+    const val COMMENTS_LOTTO_RESULT = "당첨 통계\n" + "---"
+}
