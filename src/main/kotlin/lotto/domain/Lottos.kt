cd7bec851c52a24f8ea635fb4808eb3b288bbc13
junyong <junyong008@gmail.com>
2023-11-04 23:41:05 +0900

    feat(Lottos): forEach 및 size 메서드 추가

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -1,4 +1,9 @@
 package lotto.domain
 
 class Lottos(private val lottos: List<Lotto>) {
+    fun <T> forEach(action: (T) -> Unit) {
+        lottos.forEach { action }
+    }
+
+    fun size(): Int = lottos.size
 }
