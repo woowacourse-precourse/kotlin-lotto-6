fb723a7576eba24e2c77673d93acc51240077637
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2023-11-04 15:35:14 +0900

    refactor(Lotto): move number range validator to extensions

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,17 +4,10 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == SIZE)
         require(numbers.isUnique())
-        requireInRange(numbers)
-    }
-
-    private fun requireInRange(numbers: List<Int>) {
-        numbers.forEach { number ->
-            require(number in NUMBER_RANGE)
-        }
+        require(numbers.isInLottoNumberRange())
     }
 
     companion object {
         private const val SIZE = 6
-        private val NUMBER_RANGE = 1..45
     }
 }
