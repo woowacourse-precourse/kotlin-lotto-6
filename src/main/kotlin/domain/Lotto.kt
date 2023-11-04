6b328e643db91665827757c7ae9a9790d968c1f1
jiwon2724 <stopone3119@naver.com>
2023-11-04 22:31:21 +0900

    [chore] 함수 반환 타입 수정 및 상수화

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,9 +1,13 @@
 package domain
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(private val numbers: Set<Int>) {
     init {
-        require(numbers.size == 6)
-        require(numbers.distinct().size != 6)
+        require(numbers.size == LOTTO_COUNT)
+        require(numbers.distinct().size != LOTTO_COUNT)
     }
-    fun getNumbers() = numbers
+    fun getNumbers() = numbers.sorted()
+
+    companion object {
+        private const val LOTTO_COUNT = 6
+    }
 }
