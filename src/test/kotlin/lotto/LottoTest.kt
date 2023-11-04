562f7c2570ebf2988c07eb8f8f2d0093fd0f2557
stopstone <wltjr6432@naver.com>
2023-11-04 14:35:52 +0900

    test: 구입금액에 따라 로또가 발행되는지 확인한다

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,8 @@
 package lotto
 
+import lotto.Controller.LottoManager
+import lotto.view.InputView
+import lotto.view.OutputView
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,4 +24,9 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `구입금액에 따라 티켓이 변경되는지 확인한다`() {
+        val Manager = LottoManager(InputView(), OutputView())
+            Manager.calculateTicket(1000).equals(1)
+    }
 }
