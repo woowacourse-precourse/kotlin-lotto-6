de9ee28c8a0f88c9c409a917562c37c58d67d731
applepepsi <dlaejrtnz@gmail.com>
2023-11-05 06:56:04 +0900

    feat(model/Lotto): Implementing exception handling

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,34 @@
 package lotto.model
 
+import lotto.config.ExceptionMessage.LOTTO_RANGE_ERROR
+import lotto.config.GameConfigValue.MINIMUM_LOTTO_NUMBER
+import lotto.config.GameConfigValue.MAXIMUM_LOTTO_NUMBER
+import lotto.config.ExceptionMessage.LOTTO_DEFAULT_DIGIT_ERROR
+import lotto.config.GameConfigValue.LOTTO_DEFAULT_DIGIT
+import lotto.config.ExceptionMessage.DUPLICATE_VALUES
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        checkLottoNumberSize()
+        lottoNumberRangeCheck()
+        duplicateNumberCheck()
     }
 
-    // TODO: 추가 기능 구현
+    private fun checkLottoNumberSize() {
+        if (numbers.size != LOTTO_DEFAULT_DIGIT) {
+            throw IllegalArgumentException(LOTTO_DEFAULT_DIGIT_ERROR)
+        }
+    }
+    private fun lottoNumberRangeCheck() {
+        numbers.forEach {
+            if (it !in (MINIMUM_LOTTO_NUMBER)..<MAXIMUM_LOTTO_NUMBER) {
+                throw IllegalArgumentException(LOTTO_RANGE_ERROR)
+            }
+        }
+    }
+    private fun duplicateNumberCheck() {
+        if (numbers.distinct().size != numbers.size) {
+            throw IllegalArgumentException(DUPLICATE_VALUES)
+        }
+    }
 }
