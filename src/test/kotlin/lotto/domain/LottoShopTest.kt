2e37775609d29f1abb4596a548d427edfeccedd1
junyong <junyong008@gmail.com>
2023-11-04 23:43:58 +0900

    test(LottoShop): 로또 구매 테스트 추가

---
+++ src/test/kotlin/lotto/domain/LottoShopTest.kt
@@ -0,0 +1,37 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LottoShopTest {
+    @Test
+    fun `로또 구매 - 정상 입력`() {
+        // given
+        val input = Money(5000)
+        // when
+        val result = LottoShop.buyLottos(input)
+        // then
+        assertEquals(result.size(), 5)
+    }
+
+    @Test
+    fun `로또 구매 - 1,000으로 나뉘어 지지 않는 값 예외 처리`() {
+        // given
+        val input = Money(1001)
+        // when & then
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { LottoShop.buyLottos(input) }
+            .withMessage("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.")
+    }
+
+    @Test
+    fun `로또 구매 - 1,000 미만 값 예외 처리`() {
+        // given
+        val input = Money(500)
+        // when & then
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { LottoShop.buyLottos(input) }
+            .withMessage("[ERROR] 구입금액은 최소 1000원 이상 입력해야 합니다.")
+    }
+}
