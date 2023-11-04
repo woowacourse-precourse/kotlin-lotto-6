d05721072188ad803327bf28a6c91bfbac04cc85
jiwon2724 <stopone3119@naver.com>
2023-11-04 12:26:09 +0900

    [feat] 당첨 번호 입력 구현

--- src/main/kotlin/ui/InputManager.kt
+++ src/main/kotlin/ui/InputManager.kt
@@ -6,10 +6,19 @@ import validation.ValidationManager
 class InputManager(private val validationManager: ValidationManager = ValidationManager()) {
     fun inputLottoPurchaseAmount(errorMessage: () -> Unit): Int {
         return try {
-            validationManager.validateAmount(Console.readLine())
+            validationManager.validateAmount(Console.readLine().toInt())
         } catch (exception: IllegalArgumentException) {
             errorMessage()
             inputLottoPurchaseAmount(errorMessage)
+        }
+    }
+
+    fun inputJackpotNumbers(errorMessage: () -> Unit): List<Int> {
+        return try {
+            validationManager.validateJackpotNumbers(Console.readLine())
+        } catch (exception: IllegalArgumentException) {
+            errorMessage()
+            inputJackpotNumbers(errorMessage)
         }
     }
 }
