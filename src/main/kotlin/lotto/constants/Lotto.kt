9ebe6d77a96f8c5af1654552b5e1d9d3ae8372fc
민대인 <bamin0422@gmail.com>
2023-11-04 14:14:19 +0900

    feat: Lotto 상수 정의

---
+++ src/main/kotlin/lotto/constants/Lotto.kt
@@ -0,0 +1,9 @@
+package lotto.constants
+
+object Lotto {
+    const val MINIMUM_NUMBER = 1
+    const val MAXIMUM_NUMBER = 45
+    const val LOTTO_PRICE = 1000
+    const val LOTTO_SIZE = 6
+    val LOTTO_RANGE = MINIMUM_NUMBER..MAXIMUM_NUMBER
+}
--- src/main/kotlin/lotto/model/LottoService.kt
+++ src/main/kotlin/lotto/model/LottoService.kt
@@ -1,12 +1,12 @@
 package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.constants.Lotto.LOTTO_PRICE
+import lotto.constants.Lotto.LOTTO_SIZE
+import lotto.constants.Lotto.MAXIMUM_NUMBER
+import lotto.constants.Lotto.MINIMUM_NUMBER
 
 object LottoService {
-    private const val MINIMUM_NUMBER = 1
-    private const val MAXIMUM_NUMBER = 45
-    private const val LOTTO_PRICE = 1000
-    private const val LOTTO_SIZE = 6
     fun buyLotto(money: Int): List<List<Int>> =
         List(money / LOTTO_PRICE){ generateLotto() }
 
