8eff375e05342b3260e1131d75a99ec143c3c6ce
dpcks0509 <dpcks0509@gmail.com>
2023-11-04 22:44:59 +0900

    feat: 로또 금액 단위를 나타내는 상수 추가 및 기존 상수명 디테일 추가
    로또 금액 단위를 나타내는 LOTTO_AMOUNT_UNIT 상수를 추가하였다.
    그리고 기존 상수명들에 디테일을 추가하였다.

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -1,7 +1,8 @@
 package util
 
 object Constants {
-    const val LOTTO_START = 1
-    const val LOTTO_END = 45
-    const val LOTTO_NUMBER = 6
+    const val LOTTO_NUMBER_START = 1
+    const val LOTTO_NUMBER_END = 45
+    const val LOTTO_TOTAL_NUMBER = 6
+    const val LOTTO_AMOUNT_UNIT = 1000
 }
