9928b822b8e0d7d1a217dc9f6dd3d745a7de1b63
lh99j <grand2181@gmail.com>
2023-11-04 14:07:18 +0900

    test(Validator): 정수 리스트인지 검증 기능 테스트 케이스 작성

--- src/test/kotlin/lotto/util/ValidatorTest.kt
+++ src/test/kotlin/lotto/util/ValidatorTest.kt
@@ -62,4 +62,12 @@ class ValidatorTest {
             }
         }
     }
+
+    @Test
+    @DisplayName("로또 당첨 번호에 숫자가 아닌 값이 들어오면 예외가 발생한다.")
+    fun validateIntegerList() {
+        assertThrows<IllegalArgumentException> {
+            validateLottoInteger(listOf("a", "b", "c"))
+        }
+    }
 }
