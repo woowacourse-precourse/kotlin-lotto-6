eed45a243dc60acb05139e4a2d23d483d1182d2f
oh in seong <superohinsung@naver.com>
2023-11-04 21:38:31 +0900

    [feat] Add the necessary instructions for the game to Message.kt

--- src/main/kotlin/lotto/Message.kt
+++ src/main/kotlin/lotto/Message.kt
@@ -1,4 +1,11 @@
 package lotto
 
 object Message {
+    const val ENTER_THE_PURCHASE = "구입금액을 입력해 주세요."
+    const val BOUGHT_N_PIECES = "개를 구매했습니다."
+    const val ENTER_YOUR_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+    const val ENTER_THE_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val WINNING_STATISTICS = "당첨 통계"
+    const val COMMA = "---"
+
 }
