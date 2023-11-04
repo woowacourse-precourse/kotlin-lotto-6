c4cc75ba6099a20a21fd5ed5081e7aa314ddf71a
dpcks0509 <dpcks0509@gmail.com>
2023-11-04 22:46:29 +0900

    feat: 로또 번호 출력을 위한 toString 함수 오버라이딩
    로또 번호를 출력하기 위해 toString 함수를 오버라이딩 하였다.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,4 +4,8 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
