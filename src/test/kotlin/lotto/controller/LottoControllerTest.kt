1914b6618e826db35d668ba3a1428b5ff27172dd
Minjun Kim <damon911@naver.com>
2023-11-04 22:44:57 +0900

    refactor: LottoController 코드 작성에 따라 테스트 코드 수정

--- src/test/kotlin/lotto/controller/LottoControllerTest.kt
+++ src/test/kotlin/lotto/controller/LottoControllerTest.kt
@@ -1,29 +1,37 @@
 package lotto.controller
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoControllerTest {
 
+    private lateinit var controller: LottoController
+
+    @BeforeEach
+    fun setUp() {
+        controller = LottoController()
+    }
+
     @Test
     fun `정상 범위 내의 1000원 단위인 로또 구입 금액 입력`() {
         assertDoesNotThrow {
-            inputLottoAmount(14000)
+            controller.inputLottoAmount("14000")
         }
     }
 
     @Test
     fun `정상 범위 내의 1000원 단위가 아닌 로또 구입 금액 입력`() {
         assertThrows<IllegalArgumentException> {
-            inputLottoAmount(14500)
+            controller.inputLottoAmount("14500")
         }
     }
 
     @Test
     fun `정상 범위 밖의 1000원 단위인 로또 구입 금액 입력`() {
         assertThrows<IllegalArgumentException> {
-            inputLottoAmount(4_611_686_019_000)
+            controller.inputLottoAmount("4611686019000")
         }
     }
 }
