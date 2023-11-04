9c6c5573830b1a896d97e898006e06f6ec247a19
jiwon2724 <stopone3119@naver.com>
2023-11-05 02:04:41 +0900

    [chore] 당첨 정보 enum class toString 함수 수정

--- src/main/kotlin/model/Winning.kt
+++ src/main/kotlin/model/Winning.kt
@@ -12,7 +12,7 @@ const val MATCH_FOUR_NUMBERS = 4
 const val MATCH_FIVE_NUMBERS = 5
 const val MATCH_SIX_NUMBERS = 6
 
-enum class Winning(val lottoWinnings: String, val match: Int = 0) {
+enum class Winning(val lottoWinnings: String, val match: Int = 0, val count: Int = 0) {
     FIFTH_PLACE(lottoWinnings = WINNING_FIFTH_PLACE, match = MATCH_THREE_NUMBERS),
     FOURTH_PLACE(lottoWinnings = WINNING_FOURTH_PLACE, match = MATCH_FOUR_NUMBERS),
     THIRD_PLACE(lottoWinnings = WINNING_THIRD_PLACE, match = MATCH_FIVE_NUMBERS),
@@ -20,5 +20,7 @@ enum class Winning(val lottoWinnings: String, val match: Int = 0) {
     FIRST_PLACE(lottoWinnings = WINNING_FIRST_PLACE, match = MATCH_SIX_NUMBERS),
     NO_LUCK(WINNING_NO_LUCK);
 
-    override fun toString(): String = "${match}개 일치 ($lottoWinnings) - "
+    override fun toString() =
+        if (lottoWinnings != WINNING_SECOND_PLACE) "${match}개 일치 ($lottoWinnings) - ${count}개"
+        else "${match}개 일치, 보너스 볼 일치 ($lottoWinnings) - ${count}개"
 }
