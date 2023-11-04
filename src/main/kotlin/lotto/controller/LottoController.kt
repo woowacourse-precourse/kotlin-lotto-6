94ab43f1ec7a3dd707d5da4d94b84c1e634a29f9
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-04 22:52:40 +0900

    feat(Add input value exception handling)
    사용자가 1000원단위로 입력하는 값의 예외처리를 추가하였습니다.

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -2,6 +2,7 @@ package lotto.controller
 
 import camp.nextstep.edu.missionutils.Console.readLine
 import lotto.model.LottoModel
+import lotto.enums.Exception
 import lotto.view.LottoView
 
 class LottoController(private val lottoModel: LottoModel,private val lottoView: LottoView) {
@@ -9,10 +10,25 @@ class LottoController(private val lottoModel: LottoModel,private val lottoView: 
     fun stratLotto() {
         lottoView.printEnterPurchaseMessage()
         inputPurchaseAmount()
-
     }
 
     private fun inputPurchaseAmount() {
-        val purchaseAmunt = readLine()
+        while (true) {
+            try {
+                lottoModel.returnValue(checkPurchaseAmount(readLine()))
+                break
+            } catch (e: IllegalArgumentException) {
+                println("${Exception.ERROR_HEADER.message} ${e.message}")
+                lottoView.printEnterPurchaseMessage()
+            }
+        }
+    }
+
+    private fun checkPurchaseAmount(purchaseAmount: String): Int {
+        require(purchaseAmount.isNotBlank() && purchaseAmount.isNotEmpty()) { Exception.INPUT_IS_BLANK.message }
+        require(purchaseAmount.all { it.isDigit() }) { Exception.NOT_NUMBER.message }
+        val amount = purchaseAmount.toInt()
+        require(amount > 0 && amount % 1000 == 0) { Exception.AMOUNT_MUST_BE_THOUSAND_WON.message }
+        return amount
     }
 }
