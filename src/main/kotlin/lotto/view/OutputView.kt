234e9480236be1e205c4c64c60bc28c2db96380d
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:07:58 +0900

    feat: 구매한 로또 수를 출력하는 기능

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,9 +1,12 @@
 package lotto.view
 
+import lotto.utils.Constant.LOTTO_QUANTITY_MESSAGE
 import lotto.utils.Constant.PURCHASE_AMOUNT_MESSAGE
 
 class OutputView {
 
     fun printAmountMessage() = println(PURCHASE_AMOUNT_MESSAGE)
+
+    fun printLottoQuantity(quantity: Int) = println(LOTTO_QUANTITY_MESSAGE.format(quantity))
 
 }
