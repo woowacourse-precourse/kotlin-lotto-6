51da322e7b9cb678d0242ea727cdb5b4e3c17e4e
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:19:36 +0900

    feat: 로또 게임 실행 및 결과를 계산하는 기능

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -15,5 +15,31 @@ class LottoGameController(
         outputView.printAmountMessage()
         val purchaseAmount = inputView.inputPurchaseAmount()
 
+        playLottoGame(purchaseAmount = purchaseAmount) { lottoNumbers, winningNumber, bonusNumber ->
+            val lottoResults = lottoGame.getLottoResults(
+                lottoNumbers = lottoNumbers,
+                winningNumber = winningNumber,
+                bonusNumber = bonusNumber,
+            )
+            val lottoMatchResult = lottoGame.getLottoMatchResult(lottoResults = lottoResults)
+            val rate = lottoGame.calculateRate(lottoMatchResult = lottoMatchResult, purchaseAmount = purchaseAmount)
+            outputView.printLottoWinningResults(lottoMatchResult = lottoMatchResult, rate = rate)
+        }
+    }
+
+    private inline fun playLottoGame(purchaseAmount: Int, action: (List<Lotto>, Lotto, Int) -> Unit) {
+        val quantity = lottoGame.getQuantity(purchaseAmount = purchaseAmount)
+        outputView.printLottoQuantity(quantity = quantity)
+
+        val lottoNumbers = lottoGame.createRandomLottoNumbers(quantity = quantity)
+        outputView.printLottoNumbers(lottoNumbers = lottoNumbers)
+
+        outputView.printLottoWinningNumber()
+        val winningNumber = inputView.inputWinningNumber()
+
+        outputView.printBonusNumber()
+        val bonusNumber = inputView.inputBonusNumber(winningNumber = winningNumber)
+
+        action(lottoNumbers, winningNumber, bonusNumber)
     }
 }
