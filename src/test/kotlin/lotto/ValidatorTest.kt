43f9031ee431d988dd95a7580a936c8bb8cb1f90
getupminaaa <getupmina@gmail.com>
2023-11-04 17:39:36 +0900

    test: 보너스 번호 입력값이 유효한지에 대한 테스트 작성

--- src/test/kotlin/lotto/ValidatorTest.kt
+++ src/test/kotlin/lotto/ValidatorTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import lotto.util.Validator.isValidBonusNum
 import lotto.util.Validator.isValidPurchaseAmount
 import lotto.util.Validator.isValidWinningNums
 import org.junit.jupiter.api.DisplayName
@@ -54,6 +55,31 @@ class ValidatorTest {
             }
             assertThrows<IllegalArgumentException> {
                 isValidWinningNums("0,2,3,4,47,100")
+            }
+        }
+    }
+    @Test
+    @DisplayName("보너스 번호 입력이 유효하지 않는다면 예외를 발생한다")
+    fun validateBonusNum() {
+        assertSimpleTest {
+            val winningNums = listOf(1,23,13,44,5,36)
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum("",winningNums)
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum(" ",winningNums)
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum("A",winningNums)
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum("46",winningNums)
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum("1,1,2,3,4,5",winningNums)
+            }
+            assertThrows<IllegalArgumentException> {
+                isValidBonusNum("2,3,4,44,7,40",winningNums)
             }
         }
     }
