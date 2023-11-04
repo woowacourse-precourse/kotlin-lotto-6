3114a4e285135640b8fde8b98c1d1e89b3c0fc59
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-05 02:52:54 +0900

    test: 숫자가 아닌 문자가 있을 경우 예외 발생 테스트 코드 수정

--- src/test/kotlin/lotto/BuyingPriceTest.kt
+++ src/test/kotlin/lotto/BuyingPriceTest.kt
@@ -2,12 +2,13 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import util.Validator.containsOnlyDigits
 
 class BuyingPriceTest {
     @Test
     fun `숫자가 아닌 문자가 있을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            require("1234567890".all { it.isDigit() })
+            containsOnlyDigits("1234567980.0")
         }
     }
 }
