22442f58af49eb251c1b862ca98f175c0b776671
JunJangE <wnswkd486@gmail.com>
2023-11-04 16:14:06 +0900

    feat: 랜덤 로또 번호를 생성하는 기능

--- src/main/kotlin/lotto/domain/LottoGameImpl.kt
+++ src/main/kotlin/lotto/domain/LottoGameImpl.kt
@@ -1,8 +1,24 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.model.Lotto
 import lotto.utils.Constant.LOTTO_COST
+import lotto.utils.Constant.LOTTO_SIZE
+import lotto.utils.Constant.MAX_LOTTO_NUMBER
+import lotto.utils.Constant.MIN_LOTTO_NUMBER
 
 class LottoGameImpl : LottoGame {
 
     override fun getQuantity(purchaseAmount: Int): Int = purchaseAmount / LOTTO_COST
+
+    override fun createRandomLottoNumbers(quantity: Int): List<Lotto> = List(quantity) {
+        Lotto(
+            Randoms.pickUniqueNumbersInRange(
+                MIN_LOTTO_NUMBER,
+                MAX_LOTTO_NUMBER,
+                LOTTO_SIZE,
+            ).sorted()
+        )
+    }
+
 }
