c1116584dd205c6092a980ed55446eda59a5e2cd
민대인 <bamin0422@gmail.com>
2023-11-04 13:54:08 +0900

    feat: printInputMoney 메서드 구현

--- src/main/kotlin/lotto/util/OutputUtil.kt
+++ src/main/kotlin/lotto/util/OutputUtil.kt
@@ -1,4 +1,9 @@
 package lotto.util
 
-class OutputUtil {
+object OutputUtil {
+    private const val MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요."
+
+    fun printInputMoney() {
+        println(MESSAGE_INPUT_MONEY)
+    }
 }
