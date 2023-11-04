c53efa172f42c4201372a0a6373377222f56f203
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-11-05 01:44:10 +0900

    Add: "로또 금액 입력 받기 전 문구 출력"

--- src/main/kotlin/lotto/LottoGameManager.kt
+++ src/main/kotlin/lotto/LottoGameManager.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object LottoGameManager {
     private var lottoCost = 0
     fun startLottoGame(){
+        println("구입금액을 입력해 주세요.")
         getLottoCost()
     }
 
