76c93bc3535e85bc1c4b28db5a7b9f86af7758af
minuxx <minux8905@gmail.com>
2023-11-05 04:12:11 +0900

    feat: 유효하지 않은 금액 단위 예외 발생 기능 추가

--- src/main/kotlin/lotto/User.kt
+++ src/main/kotlin/lotto/User.kt
@@ -14,17 +14,21 @@ class User {
     private fun readAndTrimInput() = Console.readLine().trim()
 
     private fun setAmountFromInput(input: String) {
-        require(isValidAmount(input)) { INVALID_DIGIT_AMOUNT }
+        require(isValidAmount(input)) { INVALID_AMOUNT_ERROR_MESSAGE }
         _amount = input.toInt()
     }
 
-    private fun isValidAmount(input: String) = isNotEmpty(input) && isInputDigitsOnly(input)
+    private fun isValidAmount(input: String) =
+        isNotEmpty(input) && isInputDigitsOnly(input) && isAmountInUnit(input.toInt())
 
     private fun isNotEmpty(input: String) = input.isNotEmpty()
 
-    private fun isInputDigitsOnly(input: String) =  input.all { it.isDigit() }
+    private fun isInputDigitsOnly(input: String) = input.all { it.isDigit() }
+
+    private fun isAmountInUnit(amount: Int) = amount >= AMOUNT_UNIT && amount % AMOUNT_UNIT == 0
 
     companion object {
-        private const val INVALID_DIGIT_AMOUNT = "[ERROR] 금액은 숫자만 입력이 가능합니다."
+        const val AMOUNT_UNIT = 1000
+        const val INVALID_AMOUNT_ERROR_MESSAGE = "[ERROR] 금액은 ${AMOUNT_UNIT} 단위의 숫자만 입력이 가능합니다."
     }
 }
