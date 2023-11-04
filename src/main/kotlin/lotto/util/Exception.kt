3ad07e612d2e06fc124233d1d44f860ad1356c24
jaehan4707 <jahan4707@naver.com>
2023-11-04 19:14:20 +0900

    refactor : 예외처리 require, requireNotNull 사용

--- src/main/kotlin/lotto/util/Exception.kt
+++ src/main/kotlin/lotto/util/Exception.kt
@@ -7,38 +7,31 @@ import lotto.util.Constant.INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE
 import lotto.util.Constant.INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE
 import lotto.util.Constant.INPUT_PRICE_NOT_NUMBER_ERROR_MESSAGE
 import lotto.util.Constant.INPUT_PRICE_UNIT_ERROR_MESSAGE
+import lotto.util.Constant.LOTTO_MAX_RANDOM_NUMBER
+import lotto.util.Constant.LOTTO_MIN_RANDOM_NUMBER
 import lotto.util.Constant.LOTTO_NUMBER_SIZE
 import lotto.util.Constant.UNIT_PRICE
 import java.lang.IllegalArgumentException
 
 object Exception {
 
-    fun validateInputPrice(price : String){
-        if(price.toIntOrNull()==null){
-            throw IllegalArgumentException(INPUT_PRICE_NOT_NUMBER_ERROR_MESSAGE)
-        }
-        if(price.toInt() % UNIT_PRICE !=0){
-            throw IllegalArgumentException(INPUT_PRICE_UNIT_ERROR_MESSAGE)
-        }
+    fun validateInputPrice(price: String) {
+        requireNotNull(price.toIntOrNull()) { INPUT_PRICE_NOT_NUMBER_ERROR_MESSAGE }
+        require(price.toInt() % UNIT_PRICE == 0) { INPUT_PRICE_UNIT_ERROR_MESSAGE }
     }
-    fun validateInputLuckyNumber(numbers:String){
+
+    fun validateInputLuckyNumber(numbers: String) {
         val luckyNumbers = numbers.split(",")
-        if(luckyNumbers.size!= LOTTO_NUMBER_SIZE){
-            throw IllegalArgumentException(INPUT_LUCKY_NUMBER_LENGTH_ERROR_MESSAGE)
-        }
-        if(luckyNumbers.count{ luckynumber -> luckynumber.toIntOrNull()==null} > 0){
-            throw IllegalArgumentException(INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE)
-        }
-        if(luckyNumbers.toSet().size != LOTTO_NUMBER_SIZE){
-            throw  IllegalArgumentException(INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE)
-        }
+        require(luckyNumbers.size == LOTTO_NUMBER_SIZE) { INPUT_LUCKY_NUMBER_LENGTH_ERROR_MESSAGE }
+        require(luckyNumbers.count {
+            luckyNumber -> luckyNumber.toIntOrNull() == null || luckyNumber.toInt() !in LOTTO_MIN_RANDOM_NUMBER..LOTTO_MAX_RANDOM_NUMBER } == 0)
+        { INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
+        require(luckyNumbers.toSet().size == LOTTO_NUMBER_SIZE) { INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE }
     }
-    fun validateInputBonusNumber(number : String, luckyNumbers : List<Int>){
-        if(number.toIntOrNull()==null){
-            throw  IllegalArgumentException(INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE)
-        }
-        if(luckyNumbers.contains(number.toInt())){
-            throw  IllegalArgumentException(INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE)
-        }
+
+    fun validateInputBonusNumber(number: String, luckyNumbers: List<Int>) {
+        requireNotNull(number.toIntOrNull()) { INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
+        require(number.toInt() in LOTTO_MIN_RANDOM_NUMBER .. LOTTO_MAX_RANDOM_NUMBER) { INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE  }
+        require(!luckyNumbers.contains(number.toInt())) { INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE }
     }
 }
