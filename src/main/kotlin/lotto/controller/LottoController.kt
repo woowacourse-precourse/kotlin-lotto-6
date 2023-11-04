fe402155e98b578a8d1eda089b56d112c936a890
dpcks0509 <dpcks0509@gmail.com>
2023-11-04 22:50:46 +0900

    refactor: 로또 구매 개수 구하는 함수 추가
    로또의 구매 개수를 구하는 함수를 추가하였다.

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,11 +1,26 @@
 package lotto.controller
 
-import lotto.view.LottoView
+import lotto.view.InputView
+import lotto.view.OutputView
+import util.Constants.LOTTO_AMOUNT_UNIT
+import util.Validator.validatePurchaseAmount
 
 class LottoController {
-    private val lottoView = LottoView()
+    private val inputView = InputView()
+    private val outputView = OutputView()
 
     fun run() {
-        lottoView.getPurchaseAmount()
+        val numberOfPurchase = getNumberOfPurchase()
+    }
+
+    fun getNumberOfPurchase(): Int {
+        outputView.printPurchaseAmountInstruction()
+        return try {
+            val purchaseAmount = inputView.inputPurchaseAmount()
+            validatePurchaseAmount(purchaseAmount) / LOTTO_AMOUNT_UNIT
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            outputView.printPurchaseAmountErrorMessage(illegalArgumentException.message.toString())
+            getNumberOfPurchase()
+        }
     }
 }
