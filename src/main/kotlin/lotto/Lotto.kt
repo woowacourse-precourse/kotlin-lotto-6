529c9c44a1bb3c6ff230c536d3198f012e666930
book3039 <book3039@knou.ac.kr>
2023-11-04 12:01:37 +0900

    feat: 로또 숫자 6자리가 1 ~ 45의 범위가 아니면 예외 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,6 +6,7 @@ class Lotto(private val numbers: List<Int>) {
             numbers.distinct()
                 .size == 6
         )
+        require(numbers.all { it in 1..45 })
     }
 
     internal fun getAscendingList() = numbers.sorted()
