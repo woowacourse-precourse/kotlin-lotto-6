6f07fd584cea6b398db21c51563cb0044521fa07
jaehan4707 <jahan4707@naver.com>
2023-11-04 19:23:36 +0900

    refactor : 당첨번호 예외처리 고차함수 분리

--- src/main/kotlin/lotto/util/Exception.kt
+++ src/main/kotlin/lotto/util/Exception.kt
@@ -23,15 +23,16 @@ object Exception {
     fun validateInputLuckyNumber(numbers: String) {
         val luckyNumbers = numbers.split(",")
         require(luckyNumbers.size == LOTTO_NUMBER_SIZE) { INPUT_LUCKY_NUMBER_LENGTH_ERROR_MESSAGE }
-        require(luckyNumbers.count {
-            luckyNumber -> luckyNumber.toIntOrNull() == null || luckyNumber.toInt() !in LOTTO_MIN_RANDOM_NUMBER..LOTTO_MAX_RANDOM_NUMBER } == 0)
-        { INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
+        for (luckyNumber in luckyNumbers){
+            requireNotNull(luckyNumber.toIntOrNull()) {INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
+            require(luckyNumber.toInt() in LOTTO_MIN_RANDOM_NUMBER .. LOTTO_MAX_RANDOM_NUMBER){ INPUT_LUCKY_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
+        }
         require(luckyNumbers.toSet().size == LOTTO_NUMBER_SIZE) { INPUT_LUCKY_NUMBER_OVERLAP_ERROR_MESSAGE }
     }
 
     fun validateInputBonusNumber(number: String, luckyNumbers: List<Int>) {
         requireNotNull(number.toIntOrNull()) { INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
-        require(number.toInt() in LOTTO_MIN_RANDOM_NUMBER .. LOTTO_MAX_RANDOM_NUMBER) { INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE  }
+        require(number.toInt() in LOTTO_MIN_RANDOM_NUMBER..LOTTO_MAX_RANDOM_NUMBER) { INPUT_BONUS_NUMBER_NOT_NUMBER_ERROR_MESSAGE }
         require(!luckyNumbers.contains(number.toInt())) { INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE }
     }
 }
