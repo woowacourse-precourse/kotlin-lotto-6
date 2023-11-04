94d3680318d84f0ed05184c83139646b0bed89e1
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 00:24:25 +0900

    feat: 로또 당첨 번호 입력 예외처리 에러 메세지 추가
    로또 당첨 번호 입력 예외처리에 필요한 에러 메세지를 추가하였다.

--- src/main/kotlin/util/ErrorMessage.kt
+++ src/main/kotlin/util/ErrorMessage.kt
@@ -1,7 +1,10 @@
 package util
 
 enum class ErrorMessage(val message: String) {
-    INVALID_INPUT("공백 또는 숫자 이외의 입력은 불가능합니다."),
+    INVALID_INPUT("공백을 입력하거나 숫자 이외의 값을 입력하는 것은 불가능합니다."),
     INSUFFICIENT_PURCHASE_AMOUNT("로또 구입 금액은 1,000원 이상부터 입력 가능합니다."),
     INVALID_PURCHASE_AMOUNT_UNIT("로또 구입 금액은 1,000원 단위로 입력 가능합니다."),
+    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45 사이의 숫자만 가능합니다."),
+    DUPLICATED_LOTTO_NUMBER("로또 번호는 중복되지 않는 숫자만 가능합니다."),
+    INVALID_LOTTO_COUNT("로또 번호는 6개의 숫자만 가능합니다.")
 }
