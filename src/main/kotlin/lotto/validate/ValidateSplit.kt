aefb5e83953e581782d1f42c06d041effcebf21c
stopstone <wltjr6432@naver.com>
2023-11-04 15:53:09 +0900

    feat: split한 List안에 문자 포함되어있는경우

--- src/main/kotlin/lotto/validate/ValidateSplit.kt
+++ src/main/kotlin/lotto/validate/ValidateSplit.kt
@@ -1,11 +1,18 @@
 package lotto.validate
 
+import lotto.utils.Messages
+
 class ValidateSplit {
     fun validateSplitMyNumbers(myNumbers: List<String>): List<String> {
         myNumbers.forEach {
             validateInputZero(it)
+            validateInputNotNumber(it)
         }
         return myNumbers
+    }
+
+    private fun validateInputNotNumber(myNumbers: String) {
+        require(myNumbers.all { it.isDigit() }) {"${Messages.ERROR_MESSAGE} ${Messages.INVALID_INPUT}" }
     }
 
     private fun validateInputZero(myNumbers: String) {
