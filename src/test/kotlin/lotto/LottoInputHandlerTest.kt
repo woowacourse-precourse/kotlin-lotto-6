a60bc5e8dfd316612963e509dbe79920cebaa484
SeongWonYoon <dbswjdqhd12@gmail.com>
2023-11-04 18:48:57 +0900

    Test: "추가 InputHandler 테스트코드"
    로또 구입 금액이 1000원 단위가 아닌 경우와 로또 구입 금액이 자연수가 아닌 경우 테스트 코드 추가

---
+++ src/test/kotlin/lotto/LottoInputHandlerTest.kt
@@ -0,0 +1,22 @@
+package lotto
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoInputHandlerTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["1400","1","234","1005","1010"])
+    fun `로또 구입 금액이 1000원 단위가 아닌 경우`(string : String){
+        assertFalse { LottoInputHandler.isValidCost(string.toIntOrNull()) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["c","c12","-1","-1c","0"])
+    fun `로또 구입 금액이 자연수가 아닌 경우`(string : String){
+        assertFalse { LottoInputHandler.isValidCost(string.toIntOrNull()) }
+    }
+
+}
