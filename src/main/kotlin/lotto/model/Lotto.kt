81e62964db542d2569929adb8fe5c66aa85983a2
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 04:13:37 +0900

    feat: 로또 중복 숫자 예외처리 추가
    로또 중복 숫자 예외처리를 추가하였다.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -3,6 +3,7 @@ package lotto.model
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.size == numbers.toSet().size)
     }
 
     override fun toString(): String {
