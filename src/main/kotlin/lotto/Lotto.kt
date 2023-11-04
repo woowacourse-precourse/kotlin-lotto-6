325a3bd4780e12a5f227a6228353f121e37a1999
stopstone <wltjr6432@naver.com>
2023-11-04 16:03:57 +0900

    style: 상수로 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,10 +1,11 @@
 package lotto
 
+import lotto.utils.Constants
 import lotto.utils.Messages
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) {"${Messages.ERROR_MESSAGE} ${Messages.MY_NUMBERS_SIZE_MESSAGE}"}
+        require(numbers.size == Constants.LOTTO_SIZE) {"${Messages.ERROR_MESSAGE} ${Messages.MY_NUMBERS_SIZE_MESSAGE}"}
     }
 
     // TODO: 추가 기능 구현
