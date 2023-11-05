e4074ec33032374fb303342ee38a9b2d08eaa705
j2j2 <fineimagination@gmail.com>
2023-11-05 19:42:29 +0900

    feat(lotto): 로또 당첨 번호를 입력하는 기능 구현

--- src/main/kotlin/lotto/domain/WinningNumber.kt
+++ src/main/kotlin/lotto/domain/WinningNumber.kt
@@ -5,9 +5,36 @@ import camp.nextstep.edu.missionutils.Console
 class WinningNumber {
     private lateinit var winningNumbers: List<Int>
 
-    fun decideWinningNumber() {
+    fun decideWinningNumbers() {
         println("당첨 번호를 입력해 주세요.")
-        winningNumbers = Console.readLine().split("")
-            .map { it.toInt() }
+        winningNumbers = validateNumbers()
+    }
+
+    fun getWinningNumbers(): List<Int> {
+        if (!::winningNumbers.isInitialized) {
+            throw UninitializedPropertyAccessException("Winning numbers have not been decided yet.")
+        }
+        return winningNumbers
+    }
+
+    private fun validateNumbers(): List<Int> {
+        while (true) {
+            try {
+                val enterNumbers = Console.readLine().split(',').map { it.toIntOrNull() }
+                require(enterNumbers.all { it != null }) { ValidatorNumbers.INVALID_FORMAT.message }
+                require(enterNumbers.size == 6) { ValidatorNumbers.INVALID_SIZE.message }
+                require(enterNumbers.all { it in 1..45 }) { ValidatorNumbers.INVALID_RANGE.message }
+                require(enterNumbers.distinct().size == enterNumbers.size) { ValidatorNumbers.INVALID_DUPLICATE.message }
+
+                return enterNumbers.map { it!! }
+
+            } catch (e: IllegalArgumentException) {
+                printErrorMessage(e.message ?: "[ERROR] Invalid input")
+            }
+        }
+    }
+
+    private fun printErrorMessage(message: String) {
+        println(message)
     }
 }
