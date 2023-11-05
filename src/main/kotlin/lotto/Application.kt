e28822138be4ed944224a70a461170b64060514c
Nohsein <si2238@naver.com>
2023-11-05 21:11:28 +0900

    feat: 당첨 번호 입력
    공백 제거 후 쉼표 기준으로 입력 받기
    6개 숫자 아니면 에러 처리 후 재입력
    1~45 범위의 숫자 아니면 에러 처리 후 재입력
    중복된 숫자면 에러 처리 후 입력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,10 +3,16 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import lotto.Constants.AMOUNT_ERROR
 import lotto.Constants.ASK_AMOUNT
+import lotto.Constants.ASK_WIN_NUM
+import lotto.Constants.COUNT
+import lotto.Constants.END
 import lotto.Constants.PRICE
+import lotto.Constants.START
+import lotto.Constants.WIN_NUM_ERROR
 
 fun main() {
     Purchase(askAmount()).lottoNum()
+    Lotto(askWinNum())
 }
 
 fun askAmount(): Int {
@@ -28,3 +34,27 @@ fun validAmount(): Int {
         throw IllegalArgumentException(AMOUNT_ERROR)
     }
 }
+
+fun askWinNum(): List<Int> {
+    try {
+        println("\n" + ASK_WIN_NUM)
+        return validWinNum()
+    } catch (e: Exception) {
+        println(WIN_NUM_ERROR)
+        return askWinNum()
+    }
+}
+
+fun validWinNum(): List<Int> {
+    try {
+        val winNum = Console.readLine().trim().split(",")
+        val numbers = winNum.map { it.toInt() }
+        require(numbers.size == COUNT && numbers.toSet().size == COUNT) { WIN_NUM_ERROR }
+        require(numbers.all { it in START..END }) { WIN_NUM_ERROR }
+        return numbers
+    } catch (e: Exception) {
+        throw IllegalArgumentException(WIN_NUM_ERROR)
+    }
+}
+
+
--- src/main/kotlin/lotto/Constants.kt
+++ src/main/kotlin/lotto/Constants.kt
@@ -9,4 +9,6 @@ object Constants {
     const val ASK_AMOUNT = "구입금액을 입력해 주세요."
     const val AMOUNT_ERROR = "[ERROR] 1000원 단위의 숫자를 입력하세요.\n"
     const val PURCHASED = "개를 구매했습니다."
+    const val ASK_WIN_NUM = "당첨 번호를 입력해 주세요."
+    const val WIN_NUM_ERROR = "[ERROR] 1~45 범위의 숫자를 6개 입력해 주세요."
 }
