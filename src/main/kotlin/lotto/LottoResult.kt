f0e3ad289b77f465cf5921e01777f0b4d91e4fe6
book3039 <book3039@knou.ac.kr>
2023-11-05 23:17:09 +0900

    refactor: calculateWinLottos depth를 줄임

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -11,15 +11,18 @@ class LottoResult(val winLotto: Lotto, val bonus: Int) {
     fun hasBonus(lotto: Lotto): Boolean = lotto.toAscendingList().contains(bonus)
 
     fun calculateWinLottos(lottos: MutableList<Lotto>): LottoResult {
-        for (lotto in lottos) {
-            when (countWinNumbers(lotto)) {
-                LottoPrize.FIFTH_PRIZE.value -> LottoPrize.FIFTH_PRIZE.count++
-                LottoPrize.FOURTH_PRIZE.value -> LottoPrize.FOURTH_PRIZE.count++
-                LottoPrize.THIRD_PRIZE.value -> countSecondOrThird(lotto)
-                LottoPrize.JACKPOT.value -> LottoPrize.JACKPOT.count++
-            }
-        }
+        for (lotto in lottos) countPrizeRank(lotto)
+
         return this
+    }
+
+    private fun countPrizeRank(lotto: Lotto) {
+        when (countWinNumbers(lotto)) {
+            LottoPrize.FIFTH_PRIZE.value -> LottoPrize.FIFTH_PRIZE.count++
+            LottoPrize.FOURTH_PRIZE.value -> LottoPrize.FOURTH_PRIZE.count++
+            LottoPrize.THIRD_PRIZE.value -> countSecondOrThird(lotto)
+            LottoPrize.JACKPOT.value -> LottoPrize.JACKPOT.count++
+        }
     }
     private fun countSecondOrThird(lotto: Lotto) {
         if (hasBonus(lotto)) {
