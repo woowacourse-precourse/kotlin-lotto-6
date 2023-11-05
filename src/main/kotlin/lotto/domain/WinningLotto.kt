c762011a05f7eddd6108d93a06d7f50f7b2eee22
MinhoiKoo <minlove2013@gmail.com>
2023-11-05 23:14:01 +0900

    feat: 플레이어가 구입한 로또와 비교하여 당첨 여부 확인 함수 추가
    -5개 맞췄을 때만 보너스 번호까지 맞는지 확인

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -8,6 +8,25 @@ class WinningLotto(private val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_SIZE_EXCEPTION_MESSAGE }
     }
-}
+
+    fun determineWinner(playerLotto: Lotto, bonusNumber: Int): WinningRank {
+        val winningNumbers = playerLotto.filter { it in numbers }
+        return when (winningNumbers.size) {
+            6 -> WinningRank.SIX_MATCHES
+            5 -> isBonusNumberMatched(winningNumbers, LottoNumber(bonusNumber))
+            4 -> WinningRank.FOUR_MATCHES
+            3 -> WinningRank.THREE_MATCHES
+            else -> WinningRank.NO_MATCHES
+        }
+    }
+
+        private fun isBonusNumberMatched(lotto: List<LottoNumber>, bonusNumber: LottoNumber): WinningRank {
+            if (lotto.contains(bonusNumber)) {
+                return WinningRank.FIVE_MATCHES_WITH_BONUS_NUMBER
+            }
+            return WinningRank.FIVE_MATCHES
+        }
+
+    }
 
 
