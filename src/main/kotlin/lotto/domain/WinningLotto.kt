95f01ea00a9a1ed2f81d1d78577ede6cf2f47f88
MinhoiKoo <minlove2013@gmail.com>
2023-11-05 16:47:51 +0900

    feat: 정답 로또 클래스 생성

---
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -0,0 +1,13 @@
+package lotto.domain
+
+import lotto.utils.Constant.INVALID_SIZE_EXCEPTION_MESSAGE
+import lotto.utils.Constant.LOTTO_NUMBER_SIZE
+
+class WinningLotto(private val numbers: List<Int>) {
+
+    init {
+        require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_SIZE_EXCEPTION_MESSAGE }
+    }
+}
+
+
