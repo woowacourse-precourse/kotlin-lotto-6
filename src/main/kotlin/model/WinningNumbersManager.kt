05d64c8aa451049eaad4b7d6171623f5e9ad56b1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 07:21:21 +0900

    feat: 당첨번호 내 정상 범위 이외의 숫자가 있을 경우 예외 발생 기능 작성

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -3,9 +3,11 @@ package model
 class WinningNumbersManager(number: List<String>) {
     init {
         require(number.toSet().size == number.size) { Error.UNIQUE_LOTTO_NUMBERS.message }
+        require(number.all { it.toInt() in 1..45 }) { Error.LOTTO_NUMBER_RANGE.message }
     }
 
     private enum class Error(val message: String) {
         UNIQUE_LOTTO_NUMBERS("로또 번호는 중복되어선 안됩니다."),
+        LOTTO_NUMBER_RANGE("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
     }
 }
