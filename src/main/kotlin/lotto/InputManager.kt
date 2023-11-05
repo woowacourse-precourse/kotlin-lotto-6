797c2d72dca38ea51eacf5170df05a0025198ae5
hongjeZZ <ajyong316@gmail.com>
2023-11-05 20:50:01 +0900

    feat: 예외 발생 후 다시 당첨 번호를 입력받는 기능 구현

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -1,11 +1,13 @@
 package lotto
 
 import lotto.validator.InputMoneyValidator
+import lotto.validator.WinningNumbersValidator
 import lotto.view.InputView
 
 class InputManager {
     private val inputView = InputView()
     private val inputMoneyValidator = InputMoneyValidator()
+    private val winningNumbersValidator = WinningNumbersValidator()
 
     fun getInputMoney(): String {
         return try {
@@ -15,6 +17,17 @@ class InputManager {
         } catch (e: IllegalArgumentException) {
             println(e.message)
             getInputMoney()
+        }
+    }
+
+    fun getWinningNumbers(): List<Int> {
+        return try {
+            val inputMoney = inputView.readWinningNumbersFromUser()
+            winningNumbersValidator.validate(inputMoney)
+            inputMoney.split(",").map { it.trim().toInt() }
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            getWinningNumbers()
         }
     }
 }
