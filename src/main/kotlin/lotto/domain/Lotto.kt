9520205e61daa8e68e66b701e20188667961ea16
hongjeZZ <ajyong316@gmail.com>
2023-11-06 00:41:50 +0900

    feat: 모든 로또를 당첨 번호, 보너스 번호와 비교하는 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -5,6 +5,10 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
+    fun contains(number: Int): Boolean = numbers.contains(number)
+
+    fun getNumbers(): List<Int> = numbers
+
     override fun toString(): String {
         return numbers.sorted().toString()
     }
