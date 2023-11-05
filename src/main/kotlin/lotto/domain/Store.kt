832e56598677e9c2f6092ceb88d7d84bf9025cda
j2j2 <fineimagination@gmail.com>
2023-11-05 17:28:26 +0900

    feat(lotto): 로또 구입 금액을 입력 받는 기능 구현

--- src/main/kotlin/lotto/domain/Store.kt
+++ src/main/kotlin/lotto/domain/Store.kt
@@ -6,6 +6,31 @@ class Store {
     private var cost: Int = 0
     fun payToBuy() {
         println("구입금액을 입력해 주세요.")
-        cost = Console.readLine().toInt()
+        cost = validatePayment()
+    }
+
+    private fun validatePayment(): Int {
+        while (true) {
+            val payMoney = Console.readLine()
+            val validationResult = ValidatorPayment.checkMoney(payMoney)
+            if (validationResult.isValid) {
+                return payMoney.toInt().also {
+                    printSuccessMessage(howManyLotto(it))
+                }
+            }
+            printErrorMessage(validationResult.message)
+        }
+    }
+
+    private fun printSuccessMessage(count: Int) {
+        println("${count}개를 구매했습니다.")
+    }
+
+    private fun printErrorMessage(message: String) {
+        println(message)
+    }
+
+    private fun howManyLotto(money: Int): Int {
+        return money / 1000
     }
 }
