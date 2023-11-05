ce5648e1b3bf4ed8f257b6ca5fb79719165e5780
oh in seong <superohinsung@naver.com>
2023-11-06 08:18:11 +0900

    [feat] Add ability to verify valid lotto number generation

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,33 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        requireDuplicateLottoNumber()
+        requireValidRange()
+        requireAscendingOrder()
     }
 
-    // TODO: 추가 기능 구현
+    private fun requireDuplicateLottoNumber() {
+        val uniqueNumbers = HashSet<Int>()
+
+        for (number in numbers) {
+            require(uniqueNumbers.add(number))
+        }
+    }
+
+    private fun requireValidRange() {
+        for (number in numbers) {
+            require(number in VALID_RANGE)
+        }
+    }
+
+    private fun requireAscendingOrder() {
+        for (i in 1 until numbers.size) {
+            require(numbers[i] > numbers[i - 1])
+        }
+    }
+
+    companion object {
+        val VALID_RANGE = 1..46
+    }
+
 }
