539fda2e5532676e17ca56cff5690d247335d750
lee989898 <wnsrud1352@gmail.com>
2023-11-06 00:02:46 +0900

    [test] "1"을 ,로 split 했을 때 1만 포함하는 배열이 반환되는지 확인 테스트 구현

--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -7,13 +7,19 @@ class StringTest {
 
     @Test
     fun `"1,2"를 ,로 split 했을 때 1과 2로 잘 분리되었는지 확인`() {
-        // Given
         val given = "1,2"
 
-        // When
         val result = given.split(",")
 
-        // Then
         assertThat(result).contains("1", "2")
+    }
+
+    @Test
+    fun `"1"을 ,로 split 했을 때 1만 포함하는 배열이 반환되는지 확인`() {
+        val given = "1"
+
+        val result = given.split(",")
+
+        assertThat(result).containsExactly("1")
     }
 }
