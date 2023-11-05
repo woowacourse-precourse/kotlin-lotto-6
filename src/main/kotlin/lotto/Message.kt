6fa425573098a75b543eac867a3a12b22246c0bb
oh in seong <superohinsung@naver.com>
2023-11-06 07:45:11 +0900

    [feat] Add Messages

--- src/main/kotlin/lotto/Message.kt
+++ src/main/kotlin/lotto/Message.kt
@@ -1,11 +1,16 @@
 package lotto
 
 object Message {
-    const val ENTER_THE_PURCHASE = "구입금액을 입력해 주세요."
+
+    const val ENTER_PURCHASE = "구입금액을 입력해 주세요."
     const val BOUGHT_N_PIECES = "개를 구매했습니다."
-    const val ENTER_YOUR_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
-    const val ENTER_THE_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val ENTER_LOTTO_NUMBER = "당첨 번호를 입력해 주세요."
+    const val ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
     const val WINNING_STATISTICS = "당첨 통계"
     const val COMMA = "---"
+
+    const val ERROR = "[ERROR]"
+    const val ERROR_LOTTO_DUPLICATION = "중복된 로또번호가 입력되었습니다."
+    const val ERROR_BONUS_DUPLICATION = "앞서 입력된 로또번호와 중복되었습니다."
 
 }
