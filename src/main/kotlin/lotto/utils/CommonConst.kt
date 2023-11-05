e86744a582c92a9235a976b1a0fe7819efd7e4d9
추민수 <devsoftychoo@gmail.com>
2023-11-05 19:19:32 +0900

    [Feat] : PrizeType EnumClass
    1. 상금의 정보를 저장하고 관리할 Enum Class구현
    2. getPrice함수로 이름이 일치하는지 확인하고 상금을 Return
    3. uppercase로 소문자가 들어왔을 경우에도 정상 작동하게 구현
    4. 일치하는 이름이 없을 경우 0 리턴

--- src/main/kotlin/lotto/utils/CommonConst.kt
+++ src/main/kotlin/lotto/utils/CommonConst.kt
@@ -18,32 +18,26 @@ object CommonConst {
     const val MESSAGE_LOTTO_WINNING_DETAILS_6_MATCHES = "6개 일치 (2,000,000,000원) - %d개"
     const val MESSAGE_TOTAL_PRIZE_RATE = "총 수익률은 %.1f"
     const val MESSAGE_TOTAL_PRIZE_RATE_THE_REST = "%입니다."
-    
+
     // generateLottoNumbers()
     const val GENERATE_LOTTO_NUMBERS_MIN = 1
     const val GENERATE_LOTTO_NUMBERS_MAX = 45
     const val GENERATE_LOTTO_NUMBERS_COUNT = 6
 
-    // prize
-    const val FIRST_PRIZE = 2000000000
-    const val SECOND_PRIZE = 30000000
-    const val THIRD_PRIZE = 1500000
-    const val FOURTH_PRIZE = 50000
-    const val FIFTH_PRIZE = 5000
-
     // exceptions
     // purchase
     const val ILLEGAL_ARGUMENT_EXCEPTION_PURCHASE_IS_NOT_DIGIT = "[ERROR] 숫자가 아닌 값을 입력하셨습니다. 다시 입력해주세요."
     const val ILLEGAL_ARGUMENT_EXCEPTION_PURCHASE = "[ERROR] 유효하지 않은 금액입니다. 1,000원 단위로 입력해주세요."
+
     // winning numbers
     const val ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS_IS_NOT_DIGIT = "[ERROR] 숫자가 아닌 값을 입력하셨습니다. 다시 입력해주세요."
     const val ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS = "[ERROR] 유효하지 않은 번호입니다. 6자리의 서로다른 번호를 입력해주세요"
+    const val ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS_OUT_OF_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+
     // bonus number
     const val ILLEGAL_ARGUMENT_EXCEPTION_BONUS_NUMBER_CONTAIN_WINNING_NUMBER = "[ERROR] 보너스 숫자는 추첨 번호와 다른 값을 입력해주세요."
     const val ILLEGAL_ARGUMENT_EXCEPTION_BONUS_NUMBER_IS_NOT_DIGIT = "[ERROR] 숫자가 아닌 값을 입력하셨습니다. 다시 입력해주세요."
     const val ILLEGAL_ARGUMENT_EXCEPTION_BONUS_NUMBER = "[ERROR] 유효하지 않은 번호입니다. 1자리의 정수를 입력해주세요"
-
-    const val ILLEGAL_ARGUMENT_EXCEPTION_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
 }
 
 
