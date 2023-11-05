b5a1a8d81ab6045f8b3025e2881b69d3cf9abedd
MinhoiKoo <minlove2013@gmail.com>
2023-11-06 03:59:08 +0900

    refactor: override equals, hashcode, toString

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -10,4 +10,14 @@ class LottoNumber(val number: Int) {
     init {
         require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { WRONG_NUMBER_RANGE_EXCEPTION_MESSAGE }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is LottoNumber) return other.number == this.number
+        return false
+    }
+
+    override fun hashCode() = number
+
+    override fun toString() = number.toString()
+
 }
