d5330bf100a0dd8ea77cda86a68db58db4e24af9
jaehan4707 <jahan4707@naver.com>
2023-11-05 19:45:38 +0900

    feat : WinningLotto, WinningLottoService 분리

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -4,21 +4,13 @@ package lotto.model
 import lotto.util.Constant.INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE
 import lotto.util.Exception
 
-class WinningLotto {
+class WinningLotto(private val luckyNumbers: List<Int>, private val bonusNumber: Int) {
 
-    private var _luckyNumbers = listOf<Int>()
-    val luckyNumbers get() = _luckyNumbers
-    private var _bonusNumber = 0
-    val bonusNumber get() = _bonusNumber
-
-    fun setLuckyNumbers(luckyNumbers : List<Int>) {
+    init{
         Exception.validateInputLuckyNumber(luckyNumbers)
-        _luckyNumbers = luckyNumbers
+        Exception.validateInputBonusNumber(bonusNumber)
     }
 
-    fun setBonusNumber(bonusNumber : Int){
-        Exception.validateInputBonusNumber(bonusNumber)
-        require(!luckyNumbers.contains(bonusNumber)) { INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE}
-        _bonusNumber = bonusNumber
-    }
+    fun getLuckyNumbers() = luckyNumbers
+    fun getBonusNumber() = bonusNumber
 }
---
+++ src/main/kotlin/lotto/service/WinningLottoService.kt
@@ -0,0 +1,43 @@
+package lotto.service
+
+import lotto.model.WinningLotto
+import lotto.util.Constant
+import lotto.util.Constant.INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE
+import lotto.util.Exception
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class WinningLottoService(private val inputView: InputView = InputView(), private val outputView: OutputView = OutputView()){
+
+    private var luckyNumbers = listOf<Int>()
+    private var bonusNumber = 0
+
+    fun makeWinningLotto()  : WinningLotto {
+        outputView.printInputLuckyNumber()
+        inputLuckyNumber()
+        outputView.printInputBonusNumber()
+        inputBonusNumber()
+        return WinningLotto(luckyNumbers,bonusNumber)
+    }
+
+    private fun inputLuckyNumber() {
+        try {
+            luckyNumbers = inputView.inputLuckyNumber()
+            Exception.validateInputLuckyNumber(luckyNumbers)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            inputLuckyNumber()
+        }
+    }
+
+    private fun inputBonusNumber() {
+        try {
+            bonusNumber = inputView.inputBonusNumber()
+            Exception.validateInputBonusNumber(bonusNumber)
+            require(!luckyNumbers.contains(bonusNumber)) { INPUT_BONUS_NUMBER_OVERLAP_ERROR_MESSAGE}
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            inputBonusNumber()
+        }
+    }
+}
