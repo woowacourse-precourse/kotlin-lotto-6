f8c91b27d6d3133d95e86aea31f05b6dfb737915
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-06 02:35:45 +0900

    [Feat] 구입 금액 입력 함수 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,14 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    print(inputPurchaseAmount())
 }
+
+fun inputPurchaseAmount(): Int {
+    println("구입금액을 입력해 주세요.")
+    return Console.readLine().toInt()
+}
+
+
