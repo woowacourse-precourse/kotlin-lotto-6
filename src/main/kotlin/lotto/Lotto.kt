910e3f5d026afb1ca2ad62fc83735e58eea58f70
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-05 16:19:50 +0900

    feat) input buynumber

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,9 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
+    fun getList(): List<Int> {
+        return numbers
+    }
 
 
 }
--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -3,16 +3,37 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+
 class LottoGame {
     private val error = Error()
     private val numbers = mutableListOf<Int>()
     private var bonus = 0
+    private val buyNumbers = mutableListOf<List<Int>>()
     fun start(){
         inputPrice()
         inputPrizeNumber()
         inputBonusNumber()
+        showResult()
+    }
+    private fun showResult(){
+        showText()
+        println("3개 일치 (5,000원) - ${bonus}개")
+        println("4개 일치 (50,000원) - ${bonus}개")
+        println("5개 일치 (1,500,000원) - ${bonus}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${bonus}개")
+        println("6개 일치 (2,000,000,000원) - ${bonus}개")
+        showProfit(62.5)
+    }
+    private fun showProfit(profit: Double){
+        println("총 수익률은 ${profit}%입니다.")
+    }
+    private fun showText(){
+        println("\n당첨 통계")
+        println("___")
     }
     private fun inputBonusNumber(){
+        println()
+        println("보너스 번호를 입력해 주세요.")
         val bonusNum = Console.readLine().toInt()
         bonus = bonusNum
 
@@ -40,8 +61,9 @@ class LottoGame {
     }
     private fun printNumber(line: Int) {
         for(i in 0 until line){
-            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+            val numbers : List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
             println(numbers)
+            buyNumbers.add(numbers)
         }
         println()
     }
