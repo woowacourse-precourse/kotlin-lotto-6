454ab8e48fad8d84584527b9035dd01bee746925
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-06 04:07:31 +0900

    feat: constant 패키지 안에 결과 출력 메시지 추가

---
+++ src/main/kotlin/exception/Message.kt
@@ -0,0 +1,19 @@
+package exception
+
+class Message {
+    companion object {
+        const val MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요."
+        const val MESSAGE_PRINT_TICKET_AMOUNT = "개를 구매했습니다."
+        const val MESSAGE_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+        const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+        const val MESSAGE_WINNING_STATISTICS = "당첨 통계\n---"
+        const val MESSAGE_PRINT_FIFTH = "3개 일치 (5,000원)"
+        const val MESSAGE_PRINT_FOURTH = "4개 일치 (50,000원)"
+        const val MESSAGE_PRINT_THIRD = "5개 일치 (1,500,000원)"
+        const val MESSAGE_PRINT_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원)"
+        const val MESSAGE_PRINT_FIRST = "6개 일치 (2,000,000,000원)"
+        const val MESSAGE_PRINT_AMOUNT = "개"
+        const val MESSAGE_PRINT_EARN = "총 수익률은 "
+        const val MESSAGE_PRINT_EARN_RATE = "%입니다."
+    }
+}
