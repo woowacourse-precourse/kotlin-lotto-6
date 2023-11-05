a3ad1134e47aef015662fe961ad68ecdb3e4e063
eunma <aa39043533@gmail.com>
2023-11-05 21:48:45 +0900

    로또 구입 금액 입력받기 및 구매금액에 따른 $n장 수 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,11 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해 주세요.")
+    val money = readLine()!!.toInt()
+    val count = money / 1000
+    println("${count}개를 구매했습니다.")
+
 }
