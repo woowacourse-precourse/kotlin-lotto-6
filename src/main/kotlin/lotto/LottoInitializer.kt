f1238277379bc7758a1160b09bd2f7cf0ac8dfc7
yeji0214 <1225yj50@naver.com>
2023-11-05 14:06:50 +0900

    LottoInitializer.kt 메서드 작게 나누기

--- src/main/kotlin/lotto/LottoInitializer.kt
+++ src/main/kotlin/lotto/LottoInitializer.kt
@@ -3,31 +3,38 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class LottoInitializer {
+    var purchaseAmount: Int = 0
+    val prizeCounts = IntArray(5)
+    var lottoPurchaseCount: Int = 0
     fun initializeLotto(): Tuple {
-        var amount: Int
-        var lottoPurchaseCount: Int
-        val prizeCounts = IntArray(5)
+        inputPurchaseAmount()
 
+        lottoPurchaseCount = purchaseAmount / 1000
+        println("\n${lottoPurchaseCount}${MessageConstants.PURCHASE_COMPLETED}")
+        return Tuple(purchaseAmount, lottoPurchaseCount, prizeCounts)
+    }
+
+    fun inputPurchaseAmount() {
         while (true) {
             println(MessageConstants.INPUT_PURCHASE_AMOUNT)
             try {
-                amount = Console.readLine().toInt()
-                if (amount < 1000) {
-                    throw IllegalArgumentException(MessageConstants.ERROR_AMOUNT_LESS_THAN_1000)
-                } else if (amount % 1000 != 0) {
-                    throw IllegalArgumentException(MessageConstants.ERROR_NOT_A_MULTIPLE_OF_1000)
-                } else {
+                purchaseAmount = Console.readLine().toInt()
+                if (validatePurchaseAmount(purchaseAmount))
                     break
-                }
             } catch (e: NumberFormatException) {
                 println(MessageConstants.ERROR_NOT_A_NUMBER)
             } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
         }
+    }
 
-        lottoPurchaseCount = amount / 1000
-        println("\n${lottoPurchaseCount}${MessageConstants.PURCHASE_COMPLETED}")
-        return Tuple(amount, lottoPurchaseCount, prizeCounts)
+    fun validatePurchaseAmount(purchaseAmount: Int) : Boolean {
+        if (purchaseAmount < 1000) {
+            throw IllegalArgumentException(MessageConstants.ERROR_AMOUNT_LESS_THAN_1000)
+        } else if (purchaseAmount % 1000 != 0) {
+            throw IllegalArgumentException(MessageConstants.ERROR_NOT_A_MULTIPLE_OF_1000)
+        }
+        return true
     }
 }
