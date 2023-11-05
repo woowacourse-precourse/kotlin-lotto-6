d69ecc8a48ede4e770fd7716ec4d15fe349f579c
book3039 <book3039@knou.ac.kr>
2023-11-05 21:42:38 +0900

    refactor: MatchNumber enum class를 LottoPrize로 변경하고 상금 정보 추가

--- src/main/kotlin/lotto/LottoPrize.kt
+++ src/main/kotlin/lotto/LottoPrize.kt
@@ -1,10 +1,9 @@
 package lotto
 
-enum class LottoPrize(val prizeMoney: Int) {
-    JACKPOT(2_000_000_000),
-    SECOND_PRIZE(30_000_000),
-    THIRD_PRIZE(1_500_000),
-    FOUTRH_PRIZE(50_000),
-    FIFTH_PRIZE(5_000),
-    BLACNK(0);
+enum class LottoPrize(val value: Int, var count: Int, val prizeMoney: Int) {
+    JACKPOT(6, 0, 2_000_000_000),
+    SECOND_PRIZE(5, 0,30_000_000),
+    THIRD_PRIZE(5, 0,1_500_000),
+    FOUTRH_PRIZE(4, 0,50_000),
+    FIFTH_PRIZE(3, 0,5_000)
 }
