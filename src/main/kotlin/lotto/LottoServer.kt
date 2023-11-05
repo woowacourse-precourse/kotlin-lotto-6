10c6a4679a427c02dc7b15cd6401e2f8684b1860
Sihwan Kim <vkn123@naver.com>
2023-11-05 12:12:31 +0900

    feat: 수익률 계산기능 구현

--- src/main/kotlin/lotto/LottoServer.kt
+++ src/main/kotlin/lotto/LottoServer.kt
@@ -15,9 +15,21 @@ class LottoServer {
         val bonusNumber = client.inputBonusNumber()
         val winPrizeList = computeWinPrize(winNumberList, bonusNumber)
         client.outputWinPrize(winPrizeList)
+        val rateOfReturn = computeRateOfReturn(winPrizeList, count)
     }
 
 
+    private fun computeRateOfReturn(winPrizeList: List<WinState>, count: Int): Int {
+        val userMoney = count * 1000
+        val totalReturn =
+            THREE_PRIZE_MONEY * winPrizeList.count { it == WinState.THREE }
+        +FOUR_PRIZE_MONEY * winPrizeList.count { it == WinState.THREE }
+        +FIVE_PRIZE_MONEY * winPrizeList.count { it == WinState.FIVE }
+        +FIVE_WITH_BONUS_PRIZE_MONEY * winPrizeList.count { it == WinState.FIVEPLUSBONUS }
+        +SIX_PRIZE_MONEY * winPrizeList.count { it == WinState.SIX }
+
+        return (totalReturn / userMoney) * 100
+    }
 
     private fun computeWinPrize(winNumberList: List<Int>, bonusNumber: Int): List<WinState> {
         return lottoList.map { lotto ->
