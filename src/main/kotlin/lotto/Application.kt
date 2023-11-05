5d75ac32e83fbede6f0e0e3e8a96b65c5fd7f669
3uomlkh <cw1205@duksung.ac.kr>
2023-11-06 00:50:41 +0900

    feat: 안내 메세지 출력 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -9,3 +9,24 @@ fun main() {
 fun inputMessage(): String? {
     return Console.readLine()
 }
+
+fun printStartMessage() {
+    println("구입금액을 입력해 주세요.")
+}
+
+fun printPurchaseTotal(n: Int) {
+    println("${n}개를 구매했습니다.")
+}
+
+fun printWinningMessage() {
+    println("당첨 번호를 입력해 주세요.")
+}
+
+fun printBonusMessage() {
+    println("보너스 번호를 입력해 주세요.")
+}
+
+fun printWinningReport() {
+    println("당첨 통계")
+    println("---")
+}
