23a6e58bb967f4639fe866044ee087c6e110a220
MinhoiKoo <minlove2013@gmail.com>
2023-11-05 21:50:17 +0900

    refactor: Player Class 에서 처리하던 로직 NumberIssuer로 분리
    -Player는 구매할 돈을 지불한다
    -NumberIssuer는 돈을 받고 로또 개수만큼 숫자를 발행한 후 반환한다

--- src/main/kotlin/lotto/domain/NumberIssuer.kt
+++ src/main/kotlin/lotto/domain/NumberIssuer.kt
@@ -6,8 +6,16 @@ import lotto.utils.Constant.MAX_LOTTO_NUMBER
 import lotto.utils.Constant.MIN_LOTTO_NUMBER
 
 object NumberIssuer {
-    fun issueNumbers(): List<LottoNumber> {
-        val numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_SIZE).map {it -> LottoNumber(it)}
-        return numbers
+    fun issueNumbers(money: Int): List<Lotto> {
+        val issuedLotto = mutableListOf<Lotto>()
+
+        val quantity = money / 1000
+        repeat(quantity) {
+            val numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_SIZE)
+                .map { LottoNumber(it) }
+            issuedLotto.add(Lotto(numbers))
+        }
+        return issuedLotto
     }
+
 }
--- src/main/kotlin/lotto/domain/Player.kt
+++ src/main/kotlin/lotto/domain/Player.kt
@@ -3,13 +3,8 @@ package lotto.domain
 class Player(private var money: Int) {
 
     fun purchaseLotto(): List<Lotto> {
-        val lottoNumbers = mutableListOf<Lotto>()
-        val quantity = money / 1000
-        repeat(quantity) {
-            val numbers = Lotto(NumberIssuer.issueNumbers())
-            lottoNumbers.add(numbers)
-        }
-        return lottoNumbers
+        val boughtLotto = NumberIssuer.issueNumbers(money)
+        return boughtLotto
     }
 
     fun getReturnMoney(returnMoney: Int) {
