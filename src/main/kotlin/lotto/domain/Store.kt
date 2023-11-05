e9aa75510153f2b80ae773fcbc3a6b17cfce8c86
DoDoBest <send1684@gmail.com>
2023-11-06 01:53:37 +0900

    test: Store 테스트 코드 작성 및 테스트가 통과하도록 로직 수정

--- src/main/kotlin/lotto/domain/Store.kt
+++ src/main/kotlin/lotto/domain/Store.kt
@@ -10,8 +10,8 @@ class Store(
         val chargeAmount: UInt = io.getPurchaseAmount()
         val (quantity, change) = pos.getQuotientAndRemainder(chargeAmount, LOTTO_PRICE)
 
-        require(change != NO_CHANGE) {
-            SHOULD_BE_NO_CHANGE.format(LOTTO_PRICE)
+        require(change == NO_CHANGE) {
+            SHOULD_BE_NO_CHANGE.format(LOTTO_PRICE.toLong())
         }
         return pos.issueLotto(quantity).apply {
             io.showIssuedLotto(this)
@@ -21,7 +21,7 @@ class Store(
     companion object {
         const val LOTTO_PRICE = 1_000u
         private const val NO_CHANGE = 0
-        private const val SHOULD_BE_NO_CHANGE = "[ERROR] %d원 단위로 지불하셔야 합니다."
+        const val SHOULD_BE_NO_CHANGE = "[ERROR] %d원 단위로 지불하셔야 합니다."
 
     }
 }
