a727c353097c7ebf8e4eb9022ae13c81125b0c82
sunkk8482 <sunkk8482@naver.com>
2023-11-05 22:00:45 +0900

    Style : 에러 상수 정의

--- src/main/kotlin/lotto/domain/Errorcheck.kt
+++ src/main/kotlin/lotto/domain/Errorcheck.kt
@@ -1,20 +1,22 @@
 package lotto.domain
 
+import lotto.constants.*
+
 fun blankCheck(input: String) {
     if (input.isBlank()) {
-        throw IllegalArgumentException("공백은 안됩니다")
+        throw IllegalArgumentException(BLANK_ERROR)
     }
 }
 
 fun numberCheck(input: String) {
     if (input.toIntOrNull() == null) {
-        throw IllegalArgumentException("숫자가 아닌값이 있습니다")
+        throw IllegalArgumentException(NUMBER_ERROR)
     }
 }
 
 fun moneyCheck(money: Int) {
     if (money % 1000 != 0) {
-        throw IllegalArgumentException("1000으로 나누어떨어져야합니다")
+        throw IllegalArgumentException(MONEY_ERROR)
     }
 }
 
@@ -31,35 +33,35 @@ fun winningNumberErrorCheck(input: String, splitInput: List<String>): List<Int> 
 fun winNumberCheck(splitInput: List<String>) {
     splitInput.forEach {
         runCatching { it.toInt() }
-            .onFailure { throw IllegalArgumentException("숫자가 아닌") }
+            .onFailure { throw IllegalArgumentException(NUMBER_ERROR) }
     }
 }
 
 fun winBlankCheck(splitInput: List<String>) {
     for(i in splitInput){
         if(i==""){
-            throw IllegalArgumentException("공백 노")
+            throw IllegalArgumentException(BLANK_ERROR)
         }
     }
 }
 
 fun winNumberIndexCheck(splitInput: List<String>) {
     if (splitInput.size != 6) {
-        throw IllegalArgumentException("입력 값은 6개여야 합니다")
+        throw IllegalArgumentException(INDEX_ERROR)
     }
 }
 
 fun winValidNumberCheck(changeNumbers: List<Int>) {
     val validNumber= changeNumbers.filter { it in 1..45 }
     if (validNumber.size != 6) {
-        throw IllegalArgumentException("범위를 초과한 수가 있습니다")
+        throw IllegalArgumentException(RANGE_ERROR)
 
     }
 }
 
 fun winChangeNumberCheck(changeNumbers: List<Int>) {
     if (changeNumbers.distinct().size != changeNumbers.size) {
-        throw IllegalArgumentException("중복된 값이 있습니다. 다시 입력하세요.")
+        throw IllegalArgumentException(DUPLICATE_ERROR)
     }
 }
 
@@ -75,24 +77,24 @@ fun bonusNumberErrorCheck(winningNumber: List<Int>, bonus: String): Int {
 
 fun bonusBlankCheck(bonus: String) {
     if (bonus.equals("")) {
-        throw IllegalArgumentException("공백은 안됩니다")
+        throw IllegalArgumentException(BLANK_ERROR)
     }
 }
 
 fun bonusRangeCheck(bonusNumber: Int) {
     if (bonusNumber < 1 || bonusNumber > 45) {
-        throw IllegalArgumentException("범위를 초과한 수가 있습니다")
+        throw IllegalArgumentException(RANGE_ERROR)
     }
 }
 
 fun bonusWinningDuplicateCheck(winningNumber: List<Int>, bonusNumber: Int) {
     if (bonusNumber in winningNumber) {
-        throw IllegalArgumentException("이미 당첨번호에 있습니다.")
+        throw IllegalArgumentException(DUPLICATE_ERROR)
     }
 }
 
 fun bonusNumberCheck(bonus: String){
     if(bonus.toIntOrNull()==null){
-        throw IllegalArgumentException("숫자가 아님")
+        throw IllegalArgumentException(NUMBER_ERROR)
     }
 }
