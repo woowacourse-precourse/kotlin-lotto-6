6a2c5a9468a30539a2fb1e5405b944330873c031
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-06 06:40:05 +0900

    feat: 구입 금액에 해당하는 로또의 갯수를 출력하는 기능 추가

--- src/main/kotlin/ui/Output.kt
+++ src/main/kotlin/ui/Output.kt
@@ -1,4 +1,10 @@
 package ui
 
+import exception.Message
+import exception.Message.Companion.MESSAGE_PRINT_TICKET_AMOUNT
+
 object Output  {
+    fun printLottoCount(count: Int) {
+        println("${count}"+ MESSAGE_PRINT_TICKET_AMOUNT)
+    }
 }
