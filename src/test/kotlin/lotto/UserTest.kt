b2343c04c743ccf3fbf90148d9cae38560f4fa83
jaehan4707 <jahan4707@naver.com>
2023-11-05 19:34:41 +0900

    test : UserTest 코드 수정

--- src/test/kotlin/lotto/UserTest.kt
+++ src/test/kotlin/lotto/UserTest.kt
@@ -1,27 +1,26 @@
 package lotto
 
+import lotto.model.Lotto
 import lotto.model.User
+import lotto.service.UserService
 import lotto.util.Constant.UNIT_PRICE
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class UserTest {
 
-    private val user = User()
-
     @Test
     fun `구입 금액이 1000원 단위가 아닐 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            user.setPrice(12500)
+            User(1200, listOf(Lotto(listOf(1, 2, 3, 4, 5, 6))))
         }
     }
-
     @Test
-    fun `사용자는 구입 금액만큼 로또를 발행 받아야한다`() {
-        val price = 10000
-        user.setPrice(price)
-        user.buyLotto()
-        assertEquals(user.lottoes.size, price / UNIT_PRICE)
+    fun `구입 금액 만큼 로또를 구매하지 않을 경우 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            User(3000, listOf(Lotto(listOf(1,2,3,4,5,6)),Lotto(listOf(7,8,9,10,11,12))))
+        }
     }
 }
