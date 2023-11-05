961208a1bcbd790b3abaa460b9d7253f8ece2170
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 15:46:21 +0900

    refactor: 유효성 검증 함수 구조 변경
    유효성 검증하는 함수의 구조를 변경하여 코드를 간결화 했다.

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -2,41 +2,30 @@ package util
 
 object Validator {
     fun validatePurchaseAmount(purchaseAmount: String): Int {
-        try {
-            val validPurchaseAmount = purchaseAmount.toInt()
-            require(validPurchaseAmount >= 1000) { ErrorMessage.INSUFFICIENT_PURCHASE_AMOUNT.message }
-            require(validPurchaseAmount % 1000 == 0) { ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.message }
-            return validPurchaseAmount
-        } catch (numberFormatException: NumberFormatException) {
-            throw IllegalArgumentException(ErrorMessage.INVALID_INPUT.message)
-        }
+        val validPurchaseAmount = purchaseAmount.toIntOrNull()
+        requireNotNull(validPurchaseAmount) { ErrorMessage.INVALID_INPUT.message }
+        require(validPurchaseAmount >= 1000) { ErrorMessage.INSUFFICIENT_PURCHASE_AMOUNT.message }
+        require(validPurchaseAmount % 1000 == 0) { ErrorMessage.INVALID_PURCHASE_AMOUNT_UNIT.message }
+        return validPurchaseAmount
     }
 
     fun validateWinningNumbers(winningNumbers: String): List<Int> {
-        try {
-            val validWinningNumbers = winningNumbers.split(",").map { winningNumber ->
-                val validWinningNumber = winningNumber.toInt()
-                require(validWinningNumber in 1..45) { ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.message }
-                validWinningNumber
-            }
-            require(validWinningNumbers.size == 6) { ErrorMessage.INVALID_LOTTO_COUNT.message }
-            require(validWinningNumbers.size == validWinningNumbers.toSet().size) { ErrorMessage.DUPLICATED_LOTTO_NUMBER.message }
-            return validWinningNumbers
-        } catch (numberFormatException: NumberFormatException) {
-            throw IllegalArgumentException(ErrorMessage.INVALID_INPUT.message)
+        val validWinningNumbers = winningNumbers.split(",").map { winningNumber ->
+            val validWinningNumber = winningNumber.toIntOrNull()
+            requireNotNull(validWinningNumber) { ErrorMessage.INVALID_INPUT.message }
+            validWinningNumber
         }
+        require(validWinningNumbers.all { winningNumber -> winningNumber in 1..45 }) { ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.message }
+        require(validWinningNumbers.size == 6) { ErrorMessage.INVALID_LOTTO_COUNT.message }
+        require(validWinningNumbers.toSet().size == 6) { ErrorMessage.DUPLICATED_LOTTO_NUMBER.message }
+        return validWinningNumbers
     }
 
     fun validateBonusNumber(bonusNumber: String, winningNumbers: List<Int>): Int {
-        try {
-            val validBonusNumber = bonusNumber.toInt()
-            require(validBonusNumber in 1..45) { ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.message }
-            for (winningNumber in winningNumbers) {
-                require(validBonusNumber != winningNumber) { ErrorMessage.DUPLICATED_BONUS_NUMBER.message }
-            }
-            return validBonusNumber
-        } catch (numberFormatException: NumberFormatException) {
-            throw IllegalArgumentException(ErrorMessage.INVALID_INPUT.message)
-        }
+        val validBonusNumber = bonusNumber.toIntOrNull()
+        requireNotNull(validBonusNumber) { ErrorMessage.INVALID_INPUT.message }
+        require(validBonusNumber in 1..45) { ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.message }
+        require(validBonusNumber !in winningNumbers) { ErrorMessage.DUPLICATED_BONUS_NUMBER.message }
+        return validBonusNumber
     }
 }
