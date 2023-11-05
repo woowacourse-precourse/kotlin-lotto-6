c47885b188925b350c5af9ca0b816475992cfe4c
jaehan4707 <jahan4707@naver.com>
2023-11-05 16:37:58 +0900

    fix : 숫자 범위 검사 코드 수정

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -10,7 +10,7 @@ import lotto.util.Constant.LOTTO_NUMBER_OVERLAP_ERROR_MESSAGE
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) {LOTTO_NUMBER_NUMBER_LENGTH_ERROR_MESSAGE}
-        require(numbers.max() <= LOTTO_MAX_RANDOM_NUMBER || numbers.min() >= LOTTO_MIN_RANDOM_NUMBER) { LOTTO_NUMBER_ERROR_MESSAGE}
+        require(numbers.max() <= LOTTO_MAX_RANDOM_NUMBER && numbers.min() >= LOTTO_MIN_RANDOM_NUMBER) { LOTTO_NUMBER_ERROR_MESSAGE}
         require(numbers.toSet().size==6) { LOTTO_NUMBER_OVERLAP_ERROR_MESSAGE}
     }
 
