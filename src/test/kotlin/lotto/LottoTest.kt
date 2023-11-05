bf46aac09608ea3506f66c0d5809831106d4c2f5
JoYehyun <jo990714@naver.com>
2023-11-06 01:35:14 +0900

    test: 로또 번호 비교 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -20,5 +19,24 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun When_6MatchNumbers_Expect_6() {
+        val input = Lotto(listOf(1,2,3,4,5,6))
+        val expected = 6
+        assert(input.countSameNumber(listOf(1,2,3,4,5,6)) == expected)
+    }
+
+    @Test
+    fun When_3MatchNumbers_Expect_3() {
+        val input = Lotto(listOf(1,2,3,4,5,6))
+        val expected = 3
+        assert(input.countSameNumber(listOf(1,2,3,7,8,9)) == expected)
+    }
+
+    @Test
+    fun When_0MatchNumber_Expect_0() {
+        val input = Lotto(listOf(1,2,3,4,5,6))
+        val expected = 0
+        assert(input.countSameNumber(listOf(7,8,9,10,11,12)) == expected)
+    }
 }
