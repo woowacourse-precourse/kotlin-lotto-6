90d52413be37126516fcbe4b7d04c9dc614be1b1
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 18:39:54 +0900

    refator: 문자열 StringConstants 상수로 대체
    문자열을 StringConstants 상수로 대체하였다.

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,24 +1,35 @@
 package lotto.view
 
 import lotto.model.Lotto
-import util.Constants.FIFTH_RANK
-import util.Constants.FIRST_RANK
-import util.Constants.FOURTH_RANK
-import util.Constants.SECOND_RANK
-import util.Constants.THIRD_RANK
+import util.NumberConstants.FIFTH_RANK
+import util.NumberConstants.FIRST_RANK
+import util.NumberConstants.FOURTH_RANK
+import util.NumberConstants.SECOND_RANK
+import util.NumberConstants.THIRD_RANK
+import util.StringConstants.BONUS_NUMBER_INSTRUCTION
+import util.StringConstants.ERROR_MESSAGE
+import util.StringConstants.NUMBER_OF_PURCHASES
+import util.StringConstants.PURCHASE_AMOUNT_INSTRUCTION
+import util.StringConstants.RATE_OF_RETURN
+import util.StringConstants.WINNING_FIFTH_RANK
+import util.StringConstants.WINNING_FIRST_RANK
+import util.StringConstants.WINNING_FOURTH_RANK
+import util.StringConstants.WINNING_NUMBER_INSTRUCTION
+import util.StringConstants.WINNING_SECOND_RANK
+import util.StringConstants.WINNING_STATISTICS_INSTRUCTION
+import util.StringConstants.WINNING_THIRD_RANK
 
 class OutputView {
     fun printPurchaseAmountInstruction() {
-        println("구입금액을 입력해 주세요.")
+        println(PURCHASE_AMOUNT_INSTRUCTION)
     }
 
     fun printErrorMessage(errorMessage: String) {
-        println("[ERROR] $errorMessage")
+        println(ERROR_MESSAGE.format(errorMessage))
     }
 
     fun printNumberOfPurchases(numberOfPurchase: Int) {
-        println()
-        println("${numberOfPurchase}개를 구매했습니다.")
+        println(NUMBER_OF_PURCHASES.format(numberOfPurchase))
     }
 
     fun printLotto(lotto: Lotto) {
@@ -26,31 +37,26 @@ class OutputView {
     }
 
     fun printWinningNumbersInstruction() {
-        println()
-        println("당첨 번호를 입력해 주세요.")
+        println(WINNING_NUMBER_INSTRUCTION)
     }
 
     fun printBonusNumberInstruction() {
-        println()
-        println("보너스 번호를 입력해 주세요.")
+        println(BONUS_NUMBER_INSTRUCTION)
     }
 
     fun printWinningStatisticsInstruction() {
-        println()
-        println("당첨 통계")
-        println("---")
+        println(WINNING_STATISTICS_INSTRUCTION)
     }
 
     fun printWinningStatistics(winningRanks: List<Int>) {
-        println("3개 일치 (5,000원) - ${winningRanks[FIFTH_RANK]}개")
-        println("4개 일치 (50,000원) - ${winningRanks[FOURTH_RANK]}개")
-        println("5개 일치 (1,500,000원) - ${winningRanks[THIRD_RANK]}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningRanks[SECOND_RANK]}개")
-        println("6개 일치 (2,000,000,000원) - ${winningRanks[FIRST_RANK]}개")
+        println(WINNING_FIFTH_RANK.format(winningRanks[FIFTH_RANK]))
+        println(WINNING_FOURTH_RANK.format(winningRanks[FOURTH_RANK]))
+        println(WINNING_THIRD_RANK.format(winningRanks[THIRD_RANK]))
+        println(WINNING_SECOND_RANK.format(winningRanks[SECOND_RANK]))
+        println(WINNING_FIRST_RANK.format(winningRanks[FIRST_RANK]))
     }
 
     fun printRateOfReturn(rateOfReturn: Double) {
-        val formattedRateOfReturn = String.format("%.1f", rateOfReturn)
-        println("총 수익률은 ${formattedRateOfReturn}%입니다.")
+        println(RATE_OF_RETURN.format(rateOfReturn))
     }
 }
