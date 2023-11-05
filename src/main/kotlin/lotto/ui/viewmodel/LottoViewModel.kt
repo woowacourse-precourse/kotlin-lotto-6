d3a72af5e1ce063064c6cebd93b5d25975a96945
추민수 <devsoftychoo@gmail.com>
2023-11-05 19:34:45 +0900

    [Feat] : 로또 생성 및 최종통계 ViewModel에 연결
    view에서 요청받아 레포지토리에게 요청하기 위한 함수 구현

--- src/main/kotlin/lotto/ui/viewmodel/LottoViewModel.kt
+++ src/main/kotlin/lotto/ui/viewmodel/LottoViewModel.kt
@@ -6,5 +6,16 @@ import lotto.ui.repository.LottoRepository
 
 class LottoViewModel(private val repository: LottoRepository) {
 
- 
+    fun generateRandomLotto(purchaseAmount: Int): List<List<Int>> {
+        repository.generateRandomLotto(purchaseAmount).also {
+            return it.lottoTickets
+        }
+    }
+
+    fun checkLottoStatistics(numbers: List<Int>, bonusNumber: Int): UserLottoState {
+        repository.checkStatistics(LottoWinningNumber(lottoNumbers = numbers, bonusNumber = bonusNumber)).also {
+            return it
+        }
+    }
+
 }
