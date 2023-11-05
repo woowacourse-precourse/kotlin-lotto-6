93930d512e15ca5525dc95f22e88ab8e025acfc7
oh in seong <superohinsung@naver.com>
2023-11-06 07:43:59 +0900

    [feat] Calculate the number of lottoes

--- src/main/kotlin/lotto/Calculator.kt
+++ src/main/kotlin/lotto/Calculator.kt
@@ -1,4 +1,11 @@
 package lotto
 
-class Calculator {
+class Calculator() {
+
+    fun calculateLottoAvailableForPurchase(cost: String): Int {
+        val number = cost.toIntOrNull() ?: throw IllegalArgumentException()
+        require(number > 0)
+        return (number / 1000)
+    }
+
 }
