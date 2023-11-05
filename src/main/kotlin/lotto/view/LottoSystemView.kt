4fb2eb77a688777799a8f5d4b5a216d066351612
seoyeon <seoyeonn051@gmail.com>
2023-11-06 00:46:12 +0900

    fix: 로또 시스템 뷰 출력 기능 생성

--- src/main/kotlin/lotto/view/LottoSystemView.kt
+++ src/main/kotlin/lotto/view/LottoSystemView.kt
@@ -1,6 +1,41 @@
 package lotto.view
 
+import lotto.model.LottoNum
+import lotto.model.LottoPaper
+import lotto.util.inputHandler
+
 class LottoSystemView {
+    fun getLottoPurchaseMount(): String {
+        println(ENTER_LOTTO_PURCHASE_AMOUNT_MESSAGE)
+        return inputHandler()
+    }
+
+    fun getWinningLottoNum(): String {
+        println(REQUEST_WINNING_LOTTO_NUM_MESSAGE)
+        return inputHandler()
+    }
+
+    fun getBonusLottoNum(): String {
+        println(REQUEST_BONUS_LOTTO_NUM_MESSAGE)
+        return inputHandler()
+    }
+
+    fun printLottoNumList(lottoNumList: LottoPaper) {
+        println("${lottoNumList.getLottoPaper().size}$PURCHASED_LOTTO_COUNT_MESSAGE")
+        for (lottoNums in lottoNumList.getLottoPaper()) {
+            println(lottoNums)
+        }
+    }
+
+    fun printWinningStatistics() {
+        println(WINNING_STATISTICS_MESSAGE)
+        println(DIVIDER_LINE)
+        // 당첨 내역 출력
+    }
+
+    fun printRateOfReturn(profitPercentage: Double){
+        println(RATE_OF_RETURN_MESSAGE.replace("profitPercentage", "$profitPercentage"))
+    }
 
     companion object {
         const val ENTER_LOTTO_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해주세요."
@@ -9,5 +44,7 @@ class LottoSystemView {
         const val REQUEST_BONUS_LOTTO_NUM_MESSAGE = "보너스 번호를 입력해 주세요."
         const val WINNING_STATISTICS_MESSAGE = "당첨 통계"
         const val COINCIDE_NUM_MESSAGE = "개 일치"
+        const val DIVIDER_LINE = "---"
+        const val RATE_OF_RETURN_MESSAGE = "총 수익률은 profitPercentage%입니다"
     }
 }
