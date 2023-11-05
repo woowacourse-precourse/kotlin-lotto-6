b595dcf474bb3ece87349dac73a7eed9064ce0f4
jiwon2724 <stopone3119@naver.com>
2023-11-06 00:01:33 +0900

    [chore] Winning enum class 수정

--- src/main/kotlin/model/Winning.kt
+++ src/main/kotlin/model/Winning.kt
@@ -1,6 +1,5 @@
 package model
 
-private const val WINNING_NO_LUCK = "0원"
 private const val WINNING_FIFTH_PLACE = "5,000원"
 private const val WINNING_FOURTH_PLACE = "50,000원"
 private const val WINNING_THIRD_PLACE = "1,500,000원"
@@ -12,15 +11,14 @@ const val MATCH_FOUR_NUMBERS = 4
 const val MATCH_FIVE_NUMBERS = 5
 const val MATCH_SIX_NUMBERS = 6
 
-enum class Winning(val lottoWinnings: String, val match: Int = 0, val count: Int = 0) {
+enum class Winning(val lottoWinnings: String, val match: Int = 0) {
     FIFTH_PLACE(lottoWinnings = WINNING_FIFTH_PLACE, match = MATCH_THREE_NUMBERS),
     FOURTH_PLACE(lottoWinnings = WINNING_FOURTH_PLACE, match = MATCH_FOUR_NUMBERS),
     THIRD_PLACE(lottoWinnings = WINNING_THIRD_PLACE, match = MATCH_FIVE_NUMBERS),
     SECOND_PLACE(lottoWinnings = WINNING_SECOND_PLACE, match = MATCH_FIVE_NUMBERS),
-    FIRST_PLACE(lottoWinnings = WINNING_FIRST_PLACE, match = MATCH_SIX_NUMBERS),
-    NO_LUCK(WINNING_NO_LUCK);
+    FIRST_PLACE(lottoWinnings = WINNING_FIRST_PLACE, match = MATCH_SIX_NUMBERS);
 
     override fun toString() =
-        if (lottoWinnings != WINNING_SECOND_PLACE) "${match}개 일치 ($lottoWinnings) - ${count}개"
-        else "${match}개 일치, 보너스 볼 일치 ($lottoWinnings) - ${count}개"
+        if (lottoWinnings != WINNING_SECOND_PLACE) "${match}개 일치 ($lottoWinnings) - "
+        else "${match}개 일치, 보너스 볼 일치 ($lottoWinnings) - "
 }
