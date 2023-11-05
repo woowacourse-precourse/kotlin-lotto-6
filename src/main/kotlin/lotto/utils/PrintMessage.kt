85dbe6552cbab5b54aaa31877ff7f66dab078e09
YEOMHYERA <hyera101511@gmail.com>
2023-11-05 16:27:06 +0900

    Added : utils - 상수명 및 출력문 정의

---
+++ src/main/kotlin/lotto/utils/PrintMessage.kt
@@ -0,0 +1,17 @@
+package lotto.utils
+
+object PrintMessage {
+    const val PRINT_REQUEST_COST = "구입금액을 입력해 주세요.\n"
+    const val PRINT_REQUEST_WIN_NUMBER = "당첨 번호를 입력해 주세요.\n"
+    const val PRINT_REQUEST_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n"
+    const val PRINT_TOTAL_RESULT = "당첨 통계\n"
+    const val PRINT_TOTAL_AREA = "---\n"
+    const val WINNER_REWARD_1TH = "2,000,000,000원"
+    const val WINNER_REWARD_2TH = "30,000,000원"
+    const val WINNER_REWARD_3TH = "1,500,000원"
+    const val WINNER_REWARD_4TH = "50,000원"
+    const val WINNER_REWARD_5TH = "5,000원"
+    const val PRINT_TOTAL_RATE ="총 수익률은 "
+    const val PRINT_TOTAL_END_MESSAGE ="%입니다."
+
+}
---
+++ src/main/kotlin/lotto/utils/Value.kt
@@ -0,0 +1,16 @@
+package lotto.utils
+
+object Value {
+    const val LOTTO_COST = 1000
+    const val LOTTO_NUMBER_COUNT = 6
+    const val MINIMUM_LOTTO_NUMBER_BOUNDARY = 1
+    const val MAXIMUM_LOTTO_NUMBER_BOUNDARY = 45
+    const val WINNER_CASE = 5
+    const val WINNER_NUMBER_MATCH_1TH = 6
+    const val WINNER_NUMBER_MATCH_2TH = 5
+    const val WINNER_NUMBER_MATCH_2TH_BONUS = 1
+    const val WINNER_NUMBER_MATCH_3TH = 5
+    const val WINNER_NUMBER_MATCH_4TH = 4
+    const val WINNER_NUMBER_MATCH_5TH = 3
+
+}
