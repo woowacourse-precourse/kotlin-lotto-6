f2648eb416eda4aae6cce51ad2654b6cbfa48b34
j2j2 <fineimagination@gmail.com>
2023-11-05 20:58:25 +0900

    feat(lotto): 구매한 로또와 당첨 번호를 비교하는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,9 +7,20 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    fun compareNumber(winningNumber: WinningNumber) {}
+    override fun toString(): String {
+        return numbers.joinToString(separator = ", ", prefix = "[", postfix = "]")
+    }
+
+    fun compareNumber(winningNumber: WinningNumber): Pair<Int, Boolean> {
+        val comparisonNumbers = winningNumber.getWinningNumbers()
+        val matchNumbers = numbers.intersect(comparisonNumbers.toSet()).size
+        val comparisonBonus = winningNumber.getBonusNumber()
+        val matchBonus = numbers.contains(comparisonBonus)
+        return Pair(matchNumbers, matchBonus)
+    }
 
     fun printLottoNumber() {}
 
-    fun printResultOfLotto() {}
+    fun resultOfLotto(result: Pair<Int, Boolean>) {}
+
 }
