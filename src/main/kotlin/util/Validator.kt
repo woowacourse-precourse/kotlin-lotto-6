68cbe8568b3efa406338990eff784f1651a30f6e
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 02:42:23 +0900

    feat: 1회 최대 구매 가능 금액보다 높은 금액일 경우 예외 발생 기능 작성

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -17,10 +17,15 @@ object Validator {
         require(price.first() != '0') { Error.NUMBER_FORMAT_ERROR.message }
     }
 
+    fun isAmountOverMaxLimit(price: String) {
+        require(price.toInt() <= 100_000) { Error.AMOUNT_OVER_MAX_LIMIT.message }
+    }
+
     private enum class Error(val message: String) {
         INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
         PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
         PURCHASE_AMOUNT_EMPTY("[ERROR] 구매 금액이 입력되지 않았습니다."),
         NUMBER_FORMAT_ERROR("[ERROR] 올바른 숫자 형태가 아닙니다."),
+        AMOUNT_OVER_MAX_LIMIT("[ERROR] 최대 100,000원 까지 구매가 가능합니다."),
     }
 }
