7d309a2955b35e2d058f80edf2b15820305a6688
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 21:25:17 +0900

    test: 테스트를 위한 view,controller 세팅
    테스트에 필요한 view,controller를 세팅하였다.

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,11 +1,17 @@
 package lotto
 
+import lotto.controller.LottoController
 import lotto.model.Lotto
+import lotto.view.InputView
+import lotto.view.OutputView
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-
 class LottoTest {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val lottoController = LottoController()
+
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
