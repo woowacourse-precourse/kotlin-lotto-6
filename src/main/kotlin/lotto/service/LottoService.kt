62b8c9e67d3d405dedb5ec8cbb2994bba7583566
jaehan4707 <jahan4707@naver.com>
2023-11-05 21:01:46 +0900

    chore : Service 패키지로 이동, 파일 이름 변경

--- src/main/kotlin/lotto/service/LottoService.kt
+++ src/main/kotlin/lotto/service/LottoService.kt
@@ -1,11 +1,11 @@
-package lotto.model
+package lotto.service
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.util.Constant.LOTTO_MAX_RANDOM_NUMBER
 import lotto.util.Constant.LOTTO_MIN_RANDOM_NUMBER
 import lotto.util.Constant.LOTTO_NUMBER_SIZE
 
-object LottoNumberGenerator {
+object LottoService {
 
     fun makeLottoNumber(): List<Int> = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_RANDOM_NUMBER,LOTTO_MAX_RANDOM_NUMBER,LOTTO_NUMBER_SIZE).sorted()
 }
