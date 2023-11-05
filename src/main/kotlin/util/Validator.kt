fb3b7557d520683ebd41b9a04c603f07adddffe8
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 01:48:58 +0900

    feat: 값을 입력하지 않았을 경우 예외 발생 기능 작성

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -9,8 +9,13 @@ object Validator {
         require(price.toInt() % 1000 == 0) { Error.PURCHASE_IN_THOUSAND_WON.message }
     }
 
+    fun isEmptyValue(price: String) {
+        require(price.isNotBlank()) { Error.PURCHASE_AMOUNT_EMPTY.message }
+    }
+
     private enum class Error(val message: String) {
         INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
         PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
+        PURCHASE_AMOUNT_EMPTY("[ERROR] 구매 금액이 입력되지 않았습니다."),
     }
 }
