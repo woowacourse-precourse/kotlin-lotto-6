74e095463be73d42b7082c9378e95b1a780e3a87
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 02:03:35 +0900

    feat: 올바른 숫자 형태가 아닐 경우 예외 발생 기능 작성

--- src/main/kotlin/util/Validator.kt
+++ src/main/kotlin/util/Validator.kt
@@ -13,9 +13,14 @@ object Validator {
         require(price.isNotBlank()) { Error.PURCHASE_AMOUNT_EMPTY.message }
     }
 
+    fun isValidNumberFormat(price: String) {
+        require(price.first() != '0') { Error.NUMBER_FORMAT_ERROR.message }
+    }
+
     private enum class Error(val message: String) {
         INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
         PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
         PURCHASE_AMOUNT_EMPTY("[ERROR] 구매 금액이 입력되지 않았습니다."),
+        NUMBER_FORMAT_ERROR("[ERROR] 올바른 숫자 형태가 아닙니다.")
     }
 }
