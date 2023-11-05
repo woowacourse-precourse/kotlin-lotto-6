a770955c42141a2de56661611042936018945012
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 03:06:43 +0900

    test: 입력받은 금액에 대한 테스트 코드 수정

--- src/test/kotlin/lotto/BuyingPriceTest.kt
+++ src/test/kotlin/lotto/BuyingPriceTest.kt
@@ -1,46 +1,42 @@
 package lotto
 
+import model.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import util.Validator.containsOnlyDigits
-import util.Validator.isAmountInThousandWon
-import util.Validator.isAmountOverMaxLimit
-import util.Validator.isEmptyValue
-import util.Validator.isValidNumberFormat
 
 class BuyingPriceTest {
     @Test
     fun `숫자가 아닌 문자가 있을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            containsOnlyDigits("1234567980.0")
+            Money("1234567980.0")
         }
     }
 
     @Test
     fun `1,000원 단위가 아닐 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            isAmountInThousandWon("28800")
+            Money("28800")
         }
     }
 
     @Test
     fun `값을 입력하지 않을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            isEmptyValue("")
+            Money("")
         }
     }
 
     @Test
     fun `올바른 숫자 형태를 입력하지 않았을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            isValidNumberFormat("000123")
+            Money("000123")
         }
     }
 
     @Test
     fun `1회 최대 구매 가능 금액보다 높은 금액일 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            isAmountOverMaxLimit("120000")
+            Money("120000")
         }
     }
 }
