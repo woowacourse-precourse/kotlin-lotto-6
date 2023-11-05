6935b292fe56b7b5a7d17190fd11a9238623275a
Tnalxmsk <alstnwkd990@naver.com>
2023-11-05 16:36:19 +0900

    feat(lotto): Lotto 클래스 메서드 추가
    자신의 로또 번호와 당첨 번호를 비교하는 기능,
    자신의 로또 번호 보너스 숫자를 비교하는 기능

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -5,11 +5,18 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    fun getNumbers(): List<Int> {
-        return this.numbers
-    }
-
     fun printNumbers() {
         println(this.numbers)
+    }
+
+    fun compareLottoNumber(winningNumber: WinningNumber): Int {
+        val result = winningNumber.getWinningNumbers().filter {
+            this.numbers.contains(it)
+        }
+        return result.size
+    }
+
+    fun compareBonusNumber(bonusNumber: Int): Boolean {
+        return this.numbers.contains(bonusNumber)
     }
 }
