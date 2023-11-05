e89b8b8bed12f43ac6d59b6a99ae572c0e601c27
추민수 <devsoftychoo@gmail.com>
2023-11-05 19:47:21 +0900

    [Feat] : 당첨 번호 예외처리
    당첨 번호에 대한 예외처리를 해줄 validateWinningNumbers 함수 구현

--- src/main/kotlin/lotto/utils/Exceptions.kt
+++ src/main/kotlin/lotto/utils/Exceptions.kt
@@ -18,6 +18,22 @@ object Exceptions {
         return input?.toInt()
     }
 
+    // 당첨 번호 예외처리
+    fun validateWinningNumbers(input: String?): String? {
+        when {
+            input == null || GameUtils.parseToInt(input).size != 6 -> {
+                throwIllegalArgumentException(CommonConst.ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS)
+            }
+            input.replace(",", "").any() { !it.isDigit() } -> {
+                throwIllegalArgumentException(CommonConst.ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS_IS_NOT_DIGIT)
+            }
+            !input.split(",").all { it.toInt() in 1..45 } -> {
+                throwIllegalArgumentException(CommonConst.ILLEGAL_ARGUMENT_EXCEPTION_WINNING_NUMBERS_OUT_OF_RANGE)
+            }
+        }
+        return input
+    }
+
     private fun throwIllegalArgumentException(message: String) {
         throw IllegalArgumentException(message)
     }
