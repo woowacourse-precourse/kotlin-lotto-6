1291aaa19705fbac30d8e3f995c46320d784ce46
민대인 <bamin0422@gmail.com>
2023-11-06 02:48:34 +0900

    test: checkWinningNumberSize - 중복되는 수가 입력될 시 예외가 발생한다 메서드 구현 및 테스팅

--- src/test/kotlin/lotto/util/LottoValidatorUtilTest.kt
+++ src/test/kotlin/lotto/util/LottoValidatorUtilTest.kt
@@ -1,11 +1,29 @@
 package lotto.util
 
+import lotto.constants.Error.EXCEPTION_MESSAGE_WINNING_NUMBER_SIZE_INVALID
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class LottoValidatorUtilTest {
 
-    @Test
-    fun checkWinningNumberSize() {
+    @DisplayName("중복되는 수 입력 검증")
+    @ParameterizedTest
+    @MethodSource("winningNumberSizeInvalidProvider")
+    fun `checkWinningNumberSize - 중복되는 수가 입력될 시 예외가 발생한다`(winningNumber: List<Int>) {
+        // given
+        val exception = assertThrows<java.lang.IllegalArgumentException> {
+            LottoValidatorUtil.checkWinningNumberSize(winningNumber)
+        }
+        // when
+        val expectedExceptionMessage = EXCEPTION_MESSAGE_WINNING_NUMBER_SIZE_INVALID
+        // then
+        Assertions.assertEquals(expectedExceptionMessage, exception.message)
     }
 
     @Test
@@ -22,5 +40,17 @@ internal class LottoValidatorUtilTest {
 
     @Test
     fun checkNumberOverlap() {
+    }
+
+    companion object {
+        @JvmStatic
+        fun winningNumberSizeInvalidProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(listOf(1,2)),
+                Arguments.of(listOf(1,2,3,9)),
+                Arguments.of(listOf(1,2,3,4,5,6,7)),
+                Arguments.of(listOf(1,2,3,4)),
+            )
+        }
     }
 }
