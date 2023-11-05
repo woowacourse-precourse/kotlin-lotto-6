28374bacc8bb7c8634b446ed953ba481c9cd0d0c
yeonghyeon <agilecatch@gmail.com>
2023-11-05 17:42:51 +0900

    [Feat] : 사용자로부터 구입 금액을 입력받기
    - 입력 받은 값은 null이 아니고 숫자만 입력할 수 있도록 예외처리
    - 1000원 단위로 입력할 수 있도록 예외처리

--- src/main/kotlin/lotto/LottoView.kt
+++ src/main/kotlin/lotto/LottoView.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 //사용자로부터 입력을 받고, 결과를 출력하는 역할을 합니다.
 class LottoView {
+
+    fun getPurchaseAmountFromUser(): Int {
+        while (true) {
+            println("구입 금액을 입력해 주세요.")
+            val input = Console.readLine()
+            return Exception.purchaseAmountEntryException(input) ?: continue
+        }
+    }
+
 }
