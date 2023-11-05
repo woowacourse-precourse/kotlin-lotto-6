0b59f459e61d32d7cf6c52e05fd65d57d77e8ace
hugesilver <xor3160@naver.com>
2023-11-06 04:22:08 +0900

    fix prize of 5 matches and bonus

--- src/main/kotlin/lotto/LottoPrize.kt
+++ src/main/kotlin/lotto/LottoPrize.kt
@@ -4,7 +4,7 @@ enum class LottoPrize(val matchedNumbers: Int, val matchedBonus: Boolean, val pr
     MATCHED3(3, false, 5000, 0),
     MATCHED4(4, false, 50000, 0),
     MATCHED5(5, false, 1500000, 0),
-    MATCHED5_AND_BONUS(5, true, 3000000, 0),
+    MATCHED5_AND_BONUS(5, true, 30000000, 0),
     MATCHED6(6, false, 2000000000, 0);
 
     fun incrementCount() {
