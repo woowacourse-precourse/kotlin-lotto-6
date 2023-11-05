026993f6675926ffbc14e9f1cb3a2a5c0f145b27
applepepsi <dlaejrtnz@gmail.com>
2023-11-06 05:29:07 +0900

    test: Create exception handling test code

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,8 @@
 package lotto
 
+import lotto.model.BonusNumberVerification
 import lotto.model.Lotto
+import lotto.model.PurchaseMoneyVerification
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -13,7 +15,7 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -21,5 +23,25 @@ class LottoTest {
         }
     }
 
+    @Test
+    fun `로또 구입 금액이 천원 단위가 아니면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            PurchaseMoneyVerification().inputMoneyCheck(2500)
+        }
+    }
+
+    @Test
+    fun `보너스 번호는 당첨 번호와 중복되면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            BonusNumberVerification(5,listOf(1,2,3,4,5,6))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 1이상 45이하가 아니라면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
     // 아래에 추가 테스트 작성 가능
 }
