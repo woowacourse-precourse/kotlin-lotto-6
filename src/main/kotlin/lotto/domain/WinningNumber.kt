ce02523f36a7d8fb03b77693fd1d3be56441880c
j2j2 <fineimagination@gmail.com>
2023-11-05 20:04:32 +0900

    feat(lotto): 로또 보너스 번호를 입력하는 기능 구현

--- src/main/kotlin/lotto/domain/WinningNumber.kt
+++ src/main/kotlin/lotto/domain/WinningNumber.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 
 class WinningNumber {
     private lateinit var winningNumbers: List<Int>
+    private var bonusNumber: Int = -1
 
     fun decideWinningNumbers() {
         println("당첨 번호를 입력해 주세요.")
@@ -12,9 +13,21 @@ class WinningNumber {
 
     fun getWinningNumbers(): List<Int> {
         if (!::winningNumbers.isInitialized) {
-            throw UninitializedPropertyAccessException("Winning numbers have not been decided yet.")
+            throw UninitializedPropertyAccessException("당첨 번호들이 결정되지 않았습니다.")
         }
         return winningNumbers
+    }
+
+    fun decideBonusNumber() {
+        println("보너스 번호를 입력해 주세요.")
+        bonusNumber = validateBonus()
+    }
+
+    fun getBonusNumber(): Int {
+        if (bonusNumber == -1) {
+            throw UninitializedPropertyAccessException("보너스 번호가 결정되지 않았습니다.")
+        }
+        return bonusNumber
     }
 
     private fun validateNumbers(): List<Int> {
@@ -28,6 +41,21 @@ class WinningNumber {
 
                 return enterNumbers.map { it!! }
 
+            } catch (e: IllegalArgumentException) {
+                printErrorMessage(e.message ?: "[ERROR] Invalid input")
+            }
+        }
+    }
+
+    private fun validateBonus(): Int {
+        while (true) {
+            try {
+                val enterNumber = Console.readLine().toIntOrNull()
+                require(enterNumber != null) { ValidatorBonus.INVALID_FORMAT.message }
+                require(enterNumber in 1..45) { ValidatorBonus.INVALID_RANGE.message }
+                require(enterNumber !in winningNumbers) { ValidatorBonus.INVALID_DUPLICATE.message }
+
+                return enterNumber
             } catch (e: IllegalArgumentException) {
                 printErrorMessage(e.message ?: "[ERROR] Invalid input")
             }
