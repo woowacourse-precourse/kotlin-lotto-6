a86d5824bfaba2663ce967f7615a7d9e89b872e9
jinuemong <jinwoo3661@naver.com>
2023-11-05 17:57:47 +0900

    Feat: 메시지 출력을 담당하는 MessageManager 기능 구현

---
+++ src/main/kotlin/lotto/manager/MessageManager.kt
@@ -0,0 +1,23 @@
+package lotto.manager
+
+class MessageManager {
+
+    fun printInputPrice() = println(ENTER_PURCHASE_PRICE)
+
+    fun printPurchaseAmount(amount: String) = println("$amount$COUNT_PURCHASE_AMOUNT")
+
+    fun printInputLottoNumber() = println(ENTER_LOTTO_NUMBER)
+
+    fun printInputBonusNumber() = println(ENTER_BONUS_NUMBER)
+
+    fun printLottoResult() = println("$RECORD_RESULT\n$RECORD_LINE")
+
+    companion object {
+        private const val ENTER_PURCHASE_PRICE = "구입금액을 입력해 주세요."
+        private const val COUNT_PURCHASE_AMOUNT = "개를 구매했습니다."
+        private const val ENTER_LOTTO_NUMBER = "당첨 번호를 입력해 주세요."
+        private const val ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+        private const val RECORD_RESULT = "당첨 통계"
+        private const val RECORD_LINE = "---"
+    }
+}
