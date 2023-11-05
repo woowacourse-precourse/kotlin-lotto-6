9fdd9cd76b5b934535eacd7c1f36306f169a12f7
조형석 <chs9902@naver.com>
2023-11-05 17:07:04 +0900

    refactor: controller 구조 조정

--- src/main/kotlin/lotto/controller/LottoMachine.kt
+++ src/main/kotlin/lotto/controller/LottoMachine.kt
@@ -5,31 +5,27 @@ import lotto.view.Input
 import lotto.view.Output
 
 class LottoMachine {
-    fun start(){
+    fun start() {
         val money = moneyInput()
         val count = Count().calculate(money)
         val userLotto = purchaseNumberLotto(count)
         val answers = userAnswer()
         val bonus = userBonus(answers)
-        val jackpot = Jackpot().discriminate(answers,bonus,userLotto)
-        val profit = Profit().calculateProfit(money,jackpot)
-        Output().printAnswerCompare(jackpot,profit)
-    }
-    fun userBonus(answers: List<Int>):Int{
-        Output().printWriteBonus()
-        val bonus = Input().write()
-        Bonus().BonusFomatValidate(bonus)
-        Bonus().BonuDuplicationValidate(bonus.toInt(),answers)
-        return bonus.toInt()
-    }
-    fun userAnswer():List<Int>{
-        Output().printWriteAnswer()
-        val answers = Input().write()
-        val userAnswerInput = UserLottoNumber().convert(answers)
-        return userAnswerInput
+        val jackpot = Jackpot().discriminate(answers, bonus, userLotto)
+        val profit = Profit().calculateProfit(money, jackpot)
+        Output().printAnswerCompare(jackpot, profit)
     }
 
-    fun purchaseNumberLotto(count:Int) :List<List<Int>>{
+    fun moneyInput(): Int {
+        Output().printWriteMoney()
+        val money = Input().write()
+        Money().MoneyFomatValidate(money)
+        Money().MoneyRangeValidate(money)
+        Money().MoneyChangesValidate(money)
+        return money.toInt()
+    }
+
+    fun purchaseNumberLotto(count: Int): List<List<Int>> {
         Output().printPurchase(count)
         val userLotto: MutableList<List<Int>> = mutableListOf()
 
@@ -40,12 +36,21 @@ class LottoMachine {
         }
         return userLotto
     }
-    fun moneyInput():Int{
-        Output().printWriteMoney()
-        val money = Input().write()
-        Money().MoneyFomatValidate(money)
-        Money().MoneyRangeValidate(money)
-        Money().MoneyChangesValidate(money)
-        return money.toInt()
+
+    fun userAnswer(): List<Int> {
+        Output().printWriteAnswer()
+        val answers = Input().write()
+        val userAnswerInput = UserLottoNumber().convert(answers)
+        return userAnswerInput
     }
+
+    fun userBonus(answers: List<Int>): Int {
+        Output().printWriteBonus()
+        val bonus = Input().write()
+        Bonus().BonusFomatValidate(bonus)
+        Bonus().BonuDuplicationValidate(bonus.toInt(), answers)
+        return bonus.toInt()
+    }
+
+
 }
