e7f857a9e1ee99c909acfad70c79bc7fe0c45e14
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 06:34:14 +0900

    style: 함수 선언 위치 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,9 +7,10 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.all { it in 1..45 }) { Error.LOTTO_NUMBER_RANGE.message }
     }
 
+    // TODO: 추가 기능 구현
+
     fun getLottoNumberInfo() = numbers.sorted()
 
-    // TODO: 추가 기능 구현
     private enum class Error(val message: String) {
         REQUIRED_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
         UNIQUE_LOTTO_NUMBERS("로또 번호는 중복되어선 안됩니다."),
