23985ed25a2499471070bd9a86795528cb4371a5
DoDoBest <send1684@gmail.com>
2023-11-05 15:02:05 +0900

    test: 로또 등수 계산 함수에 대한 테스트 코드 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,14 @@
 package lotto
 
+import lotto.data.GRADE
+import lotto.data.Lotto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 
 class LottoTest {
@@ -20,5 +27,120 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @ParameterizedTest(name = "check {0}")
+    @MethodSource("generateLottoAndExpectedGrade")
+    @DisplayName("Lotto : checkGrade()")
+    fun `로또 번호의 등수를 반환한다`(argv: LottoAndExpectedGrade) {
+        // given
+        val lotto = argv.lotto
+        val win = argv.winningLotto
+        val bonus = argv.bonus
+
+        // when
+        val actual = lotto.checkGrade(win, bonus)
+
+        // then
+        val expected = argv.grade
+        assertThat(actual).isEqualTo(expected)
+    }
+
+
+    companion object {
+        @JvmStatic
+        private fun generateLottoAndExpectedGrade(): List<LottoAndExpectedGrade> {
+            return listOf(
+                LottoAndExpectedGrade(
+                    // 1등
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    7,
+                    GRADE.ONE,
+                ),
+                LottoAndExpectedGrade(
+                    // 2등
+                    Lotto(listOf(1, 2, 3, 4, 5, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    45,
+                    GRADE.TWO,
+                ),
+                LottoAndExpectedGrade(
+                    // 3등
+                    Lotto(listOf(1, 2, 3, 4, 5, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    7,
+                    GRADE.THREE,
+                ),
+                LottoAndExpectedGrade(
+                    // 4등 + 보너스 당첨
+                    Lotto(listOf(1, 2, 3, 4, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    15,
+                    GRADE.FOUR,
+                ),
+                LottoAndExpectedGrade(
+                    // 4등 + 보너스 미당첨
+                    Lotto(listOf(1, 2, 3, 4, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    7,
+                    GRADE.FOUR,
+                ),
+                LottoAndExpectedGrade(
+                    // 5등 + 보너스 당첨
+                    Lotto(listOf(1, 2, 3, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    10,
+                    GRADE.FIVE,
+                ),
+                LottoAndExpectedGrade(
+                    // 5등 + 보너스 미당첨
+                    Lotto(listOf(1, 2, 3, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    25,
+                    GRADE.FIVE,
+                ),
+                LottoAndExpectedGrade(
+                    // 2개 맞춤 + 보너스 당첨
+                    Lotto(listOf(1, 2, 7, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    10,
+                    GRADE.LOSE,
+                ),
+                LottoAndExpectedGrade(
+                    // 2개 맞춤 + 보너스 미당첨
+                    Lotto(listOf(1, 2, 7, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    8,
+                    GRADE.LOSE,
+                ),
+                LottoAndExpectedGrade(
+                    // 1개 맞춤 + 보너스 당첨
+                    Lotto(listOf(1, 7, 8, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    7,
+                    GRADE.LOSE,
+                ),
+                LottoAndExpectedGrade(
+                    // 1개 맞춤 + 보너스 미당첨
+                    Lotto(listOf(1, 7, 8, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    25,
+                    GRADE.LOSE,
+                ),
+                LottoAndExpectedGrade(
+                    // 0개 맞춤 + 보너스 당첨
+                    Lotto(listOf(7, 8, 9, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    7,
+                    GRADE.LOSE,
+                ),
+                LottoAndExpectedGrade(
+                    // 0개 맞춤 + 보너스 미당첨
+                    Lotto(listOf(7, 8, 9, 10, 15, 45)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    25,
+                    GRADE.LOSE,
+                ),
+            )
+        }
+    }
 }
