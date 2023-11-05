ff02c3dbe254fe42b239dc82cf4fa9d1fcc3c129
jihyeonbaem <jihyeonbae424@gmail.com>
2023-11-05 20:25:43 +0900

    refactor(Lotto.kt): 중복 검증 추가 및 상수화

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,10 +2,15 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == NUMBER_SIZE)
+        require(numbers.toSet().size == NUMBER_SIZE)
     }
 
     fun sort(): List<Int> {
         return numbers.sorted()
+    }
+
+    companion object {
+        const val NUMBER_SIZE = 6
     }
 }
