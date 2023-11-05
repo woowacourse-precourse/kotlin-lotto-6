3fe85df52fc81ee1bef5cffd6a8798178a934c4c
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-06 03:06:55 +0900

    [Feat] 예외 처리 : 1000원 단위가 아닐 경우

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,7 +8,13 @@ fun main() {
 
 fun inputPurchaseAmount(): Int {
     println("구입금액을 입력해 주세요.")
-    return Console.readLine().toInt()
+    val amount = Console.readLine().toInt()
+    validatePurchaseAmount(amount)
+    return amount
+}
+
+fun validatePurchaseAmount(amount: Int) {
+    require(amount % 1000 == 0) { throw IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.") }
 }
 
 
