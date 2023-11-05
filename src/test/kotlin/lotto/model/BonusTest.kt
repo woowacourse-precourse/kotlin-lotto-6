0adcbdbf1af2e1c0859c9ac7b5cc5409a76c791c
lh99j <grand2181@gmail.com>
2023-11-05 14:22:41 +0900

    test(BonusTest): Bonus 모델 테스트케이스 작성

---
+++ src/test/kotlin/lotto/model/BonusTest.kt
@@ -0,0 +1,36 @@
+package lotto.model
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BonusTest {
+    @Test
+    @DisplayName("보너스 숫자가 올바르지 않은 번호면 예외가 발생한다.")
+    fun bonusConstructTest(){
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> {
+                val bonus = Bonus(-1)
+            }
+
+            assertThrows<IllegalArgumentException> {
+                val bonus = Bonus(46)
+            }
+
+            assertThrows<IllegalArgumentException> {
+                val purchase = Purchase(0)
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("보너스 숫자가 주어진 리스트에 중복된 숫자면 예외가 발생한다.")
+    fun checkUniqueNumberTest(){
+        val input = listOf(1, 2, 3, 4, 5, 6)
+        val bonus = Bonus(1)
+        assertThrows<IllegalArgumentException> {
+            bonus.checkUniqueNumber(input)
+        }
+    }
+}
