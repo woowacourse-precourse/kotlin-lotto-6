9475c27c4f9f6d808b7c268e0e2cfb1517a05bb1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 06:54:38 +0900

    feat: 당첨번호에 번호가 중복되어 있을 경우 예외 발생 기능 작성

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -1,4 +1,11 @@
 package model
 
 class WinningNumbersManager(number: List<String>) {
+    init {
+        require(number.toSet().size == number.size) { Error.UNIQUE_LOTTO_NUMBERS.message }
+    }
+
+    private enum class Error(val message: String) {
+        UNIQUE_LOTTO_NUMBERS("로또 번호는 중복되어선 안됩니다."),
+    }
 }
