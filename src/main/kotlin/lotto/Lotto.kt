a52ac015df4a10fed5f6f379ee3bfaedf6f7c5e0
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 05:03:58 +0900

    feat: 로또번호가 6개가 아닐 경우 예외 메세지 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,11 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { Error.REQUIRED_LOTTO_NUMBER_COUNT.message }
     }
 
     // TODO: 추가 기능 구현
+    private enum class Error(val message: String) {
+        REQUIRED_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+    }
 }
