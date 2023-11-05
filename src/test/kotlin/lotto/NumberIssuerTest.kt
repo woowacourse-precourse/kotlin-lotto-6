7f38a66860ffa76712d85cda858220489594e775
MinhoiKoo <minlove2013@gmail.com>
2023-11-06 04:35:24 +0900

    test: 로또 숫자 발행 Test 작성
    -숫자 6개
    -서로 다른 숫자 확인
    -구입한 로또의 갯수만큼 숫자를 발행하는지 확인

---
+++ src/test/kotlin/lotto/NumberIssuerTest.kt
@@ -0,0 +1,31 @@
+package lotto
+
+import lotto.domain.NumberIssuer
+import lotto.utils.Constant
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+private const val FIVE_LOTTOS = 5
+
+class NumberIssuerTest {
+    val money = 5000
+
+    @Test
+    fun `발행된 숫자 List의 size는 6이어야 한다`() {
+        val numbers = NumberIssuer.issueNumbers(money)
+        Assertions.assertThat(Constant.LOTTO_NUMBER_SIZE.equals(numbers.size))
+    }
+
+    @Test
+    fun `발행된 숫자는 서로 다른 숫자여야 한다`() {
+        val numbers = NumberIssuer.issueNumbers(money)
+        Assertions.assertThat(numbers.size.equals(numbers.toSet().size))
+    }
+
+    @Test
+    fun `구매한 로또의 갯수만큼 로또를 발행해 줘야 한다`() {
+        Assertions.assertThat(FIVE_LOTTOS).isEqualTo(NumberIssuer.issueNumbers(5000))
+    }
+
+
+}
