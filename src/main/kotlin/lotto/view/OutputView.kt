4223d6c35ec5784e7c59ecfcb24c4c7417933a86
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 15:42:43 +0900

    refactor: 로또 등수 상수로 변경
    로또 등수를 포현하는 코드를 상수로 변경하였다.

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,6 +1,11 @@
 package lotto.view
 
 import lotto.model.Lotto
+import util.Constants.FIFTH_RANK
+import util.Constants.FIRST_RANK
+import util.Constants.FOURTH_RANK
+import util.Constants.SECOND_RANK
+import util.Constants.THIRD_RANK
 
 class OutputView {
     fun printPurchaseAmountInstruction() {
@@ -37,11 +42,11 @@ class OutputView {
     }
 
     fun printWinningStatistics(winningRanks: List<Int>) {
-        println("3개 일치 (5,000원) - ${winningRanks[5]}개")
-        println("4개 일치 (50,000원) - ${winningRanks[4]}개")
-        println("5개 일치 (1,500,000원) - ${winningRanks[3]}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningRanks[2]}개")
-        println("6개 일치 (2,000,000,000원) - ${winningRanks[1]}개")
+        println("3개 일치 (5,000원) - ${winningRanks[FIFTH_RANK]}개")
+        println("4개 일치 (50,000원) - ${winningRanks[FOURTH_RANK]}개")
+        println("5개 일치 (1,500,000원) - ${winningRanks[THIRD_RANK]}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningRanks[SECOND_RANK]}개")
+        println("6개 일치 (2,000,000,000원) - ${winningRanks[FIRST_RANK]}개")
     }
 
     fun printRateOfReturn(rateOfReturn: Double) {
