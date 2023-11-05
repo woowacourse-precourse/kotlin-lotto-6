401621d50401612d97eb9a63b2374de6eadba75a
추민수 <devsoftychoo@gmail.com>
2023-11-05 20:08:01 +0900

    [Docs] : 누락된 설명 주석으로 추가

--- src/main/kotlin/lotto/ui/Lotto.kt
+++ src/main/kotlin/lotto/ui/Lotto.kt
@@ -11,14 +11,14 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun startLottoDraw(bonusNumber: Int, viewModel: LottoViewModel) {
-
-        viewModel.checkLottoStatistics(numbers,bonusNumber).also {
+        //사용자에게 최종 통계 출력
+        viewModel.checkLottoStatistics(numbers, bonusNumber).also {
             println(CommonConst.MESSAGE_LOTTO_WINNING_DETAILS_3_MATCHES.format(it.fifthPrizeCount))
             println(CommonConst.MESSAGE_LOTTO_WINNING_DETAILS_4_MATCHES.format(it.fourthPrizeCount))
             println(CommonConst.MESSAGE_LOTTO_WINNING_DETAILS_5_MATCHES.format(it.thirdPrizeCount))
             println(CommonConst.MESSAGE_LOTTO_WINNING_DETAILS_5_MATCHES_BONUS.format(it.secondPrizeCount))
             println(CommonConst.MESSAGE_LOTTO_WINNING_DETAILS_6_MATCHES.format(it.firstPrizeCount))
-            println(CommonConst.MESSAGE_TOTAL_PRIZE_RATE.format(it.totalPrizeRate)+CommonConst.MESSAGE_TOTAL_PRIZE_RATE_THE_REST)
+            println(CommonConst.MESSAGE_TOTAL_PRIZE_RATE.format(it.totalPrizeRate) + CommonConst.MESSAGE_TOTAL_PRIZE_RATE_THE_REST)
         }
     }
 }
