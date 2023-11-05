ad132eba13251397217342bfc2102d527668e732
aprilgom <aprilgom@users.noreply.github.com>
2023-11-05 17:29:07 +0900

    feat: 로또 중복 숫자 예외 처리
    (cherry picked from commit d3af6d34715152a69333a726c25f92dcb6e63a61)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct().size == 6)
     }
 
     override fun toString(): String {
