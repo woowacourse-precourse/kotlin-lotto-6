4046b7b23975eee5b354a67c2872f64546f28ae9
JoYehyun <jo990714@naver.com>
2023-11-05 18:01:43 +0900

    test: 당첨 번호 입력 예외 처리 테스트

---
+++ src/test/kotlin/utils/WinningNumbersValidatorTest.kt
@@ -0,0 +1,41 @@
+package utils
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class WinningNumbersValidatorTest {
+
+    private val winningNumbersValidator = WinningNumbersValidator()
+
+    @Test
+    fun When_Input_4Numbers_Expect_InValid() {
+        val input = "1,2,3,4".split(',')
+        assertThrows<IllegalArgumentException> { winningNumbersValidator.validateNumbersCount(input) }
+    }
+
+    @Test
+    fun When_Input_6Numbers_Expect_Valid() {
+        val input = "11,22,33,44,55,66".split(',')
+        assertDoesNotThrow { winningNumbersValidator.validateNumbersCount(input) }
+    }
+
+    @Test
+    fun When_InClude_EmptySpace_Expect_InValid() {
+        val input = ",1,2,3,4,5".split(',')
+        assertThrows<IllegalArgumentException> { winningNumbersValidator.validateEmptySpace(input) }
+    }
+
+    @Test
+    fun When_InClude_OutOfRange_Expect_InValid() {
+        val input = "1,2,3,4,5,50".split(',').map { it.toInt() }
+        assertThrows<IllegalArgumentException> { winningNumbersValidator.validateOutOfRange(input) }
+    }
+
+    @Test
+    fun When_6Numbers_InValidRange_Expect_Valid() {
+        val input = "1,2,3,4,5,6".split(',')
+        assert(winningNumbersValidator.checkInputValidation(input))
+    }
+}
