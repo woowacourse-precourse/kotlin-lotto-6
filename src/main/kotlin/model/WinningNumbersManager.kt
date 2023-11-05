875ee1e955028eabb36de4eec9092417a925d508
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 07:29:22 +0900

    feat: 당첨번호에 숫자가 아닌 문자가 포함되어 있을 경우 예외 발생 기능 작성

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -2,12 +2,14 @@ package model
 
 class WinningNumbersManager(number: List<String>) {
     init {
+        require(number.all { it.matches(Regex("[^0-9]")) }) { Error.INPUT_ONLY_NUMBER.message }
         require(number.toSet().size == number.size) { Error.UNIQUE_LOTTO_NUMBERS.message }
         require(number.all { it.toInt() in 1..45 }) { Error.LOTTO_NUMBER_RANGE.message }
     }
 
     private enum class Error(val message: String) {
-        UNIQUE_LOTTO_NUMBERS("로또 번호는 중복되어선 안됩니다."),
-        LOTTO_NUMBER_RANGE("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
+        INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
+        UNIQUE_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복되어선 안됩니다."),
+        LOTTO_NUMBER_RANGE("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
     }
 }
