afbdf96af10658e0657eccb4c12e805df44f5452
Joontae <chlwnsxo0@gmail.com>
2023-11-06 02:16:51 +0900

    feat(Lottos): Lottos class completed

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -10,10 +10,20 @@ class Lotto(private val numbers: List<Int>) {
     fun sortNumbers(): List<Int> {
         return numbers.sorted()
     }
-
    fun isDuplicate(){
         if(numbers.distinct().size!=6){
             throw IllegalArgumentException(isDuplicateErrorMessage)
         }
+    }
+    fun countHit(winningNumbers:List<Int>):Int{
+        return numbers.intersect(winningNumbers).size
+    }
+    fun checkBonusHit(bonusNumber:Int):Boolean{
+        return numbers.contains(bonusNumber)
+    }
+    fun getRank(winningNumbers:List<Int>, bonusNumber:Int):Rank{
+        val hit = countHit(winningNumbers)
+        val bonusHit = checkBonusHit(bonusNumber)
+        return Rank.of(hit, bonusHit)
     }
 }
--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -1,9 +1,25 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 
 class Lottos(amount:Int) {
     private val lottos = ArrayList<Lotto>()
-    private fun generateNumber() {
+    private fun generateNumber() :List<Int>{
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    }
+    fun generateLottos(amount:Int){
+        for(i in 1 .. amount){
+            lottos.add(Lotto(generateNumber()))
+        }
+    }
 
+    fun getLottoRanks(winningNumber:List<Int>,bonusNumber:Int):Map<Rank,Int>{
+        val rankCounts = Rank.values().associateWith { 0 }.toMutableMap()
+        for (lotto in lottos) {
+            val rank = lotto.getRank(winningNumber, bonusNumber)
+            rankCounts[rank] = rankCounts[rank]!! + 1
+        }
+        return rankCounts
     }
 }
