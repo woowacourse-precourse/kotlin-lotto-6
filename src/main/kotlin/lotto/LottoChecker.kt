1d15d551e1b999f04bde98b0044bc80a89e28c5b
aprilgom <aprilgom@users.noreply.github.com>
2023-11-06 06:38:25 +0900

    fix: 등수 조건 수정

--- src/main/kotlin/lotto/LottoChecker.kt
+++ src/main/kotlin/lotto/LottoChecker.kt
@@ -12,7 +12,7 @@ class LottoChecker(
         val resultNum = howMuchNumbersAreCorrect(userNumber)
         return when {
             resultNum == LOTTO_BONUS_COUNT -> Position.First
-            resultNum == LOTTO_BONUS_COUNT && userNumber.contains(bonusNumber) -> Position.Second
+            resultNum == LOTTO_BONUS_COUNT - 1 && userNumber.contains(bonusNumber) -> Position.Second
             resultNum == LOTTO_BONUS_COUNT - 1 -> Position.Third
             resultNum == LOTTO_BONUS_COUNT - 2 -> Position.Fourth
             resultNum == LOTTO_BONUS_COUNT - 3 -> Position.Fifth
