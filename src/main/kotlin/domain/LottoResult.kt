2428dd29632a3ee74aa1250b8206e54fc731835f
jiwon2724 <stopone3119@naver.com>
2023-11-05 13:56:02 +0900

    [feat] 로또 결과 클래스 및 함수 구현

---
+++ src/main/kotlin/domain/LottoResult.kt
@@ -0,0 +1,39 @@
+package domain
+
+class LottoResult(
+    val checkPrize: Array<Int> = Array(PRIZE_COUNT) { 0 },
+    private val amount: Int
+) {
+    fun rateOfReturn(): String = getTotalPrize().calculateRateOfReturn()
+
+    private fun getTotalPrize(): Int {
+        var myTotalPrize = 0
+        checkPrize.forEachIndexed { index, prize ->
+            myTotalPrize += when (index) {
+                0 -> WINNING_FIRST_PLACE * prize
+                1 -> WINNING_SECOND_PLACE * prize
+                2 -> WINNING_THIRD_PLACE * prize
+                3 -> WINNING_FOURTH_PLACE * prize
+                else -> WINNING_FIFTH_PLACE * prize
+            }
+        }
+        return myTotalPrize
+    }
+
+    private fun Int.calculateRateOfReturn(): String {
+        return "${ROUND_TO_TWO_DECIMALS.format ((this.toFloat() / amount.toFloat()) * 100)}$PERCENTAGE"
+    }
+
+    companion object {
+        private const val PRIZE_COUNT = 5
+
+        private const val WINNING_FIRST_PLACE = 2000000000
+        private const val WINNING_SECOND_PLACE = 30000000
+        private const val WINNING_THIRD_PLACE = 1500000
+        private const val WINNING_FOURTH_PLACE = 50000
+        private const val WINNING_FIFTH_PLACE = 5000
+
+        private const val ROUND_TO_TWO_DECIMALS = "%.1f"
+        private const val PERCENTAGE = "%"
+    }
+}
