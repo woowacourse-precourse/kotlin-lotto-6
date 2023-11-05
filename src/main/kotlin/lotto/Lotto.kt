437973a4c75f77175801bf015dc8ea04f73a4356
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 05:08:36 +0900

    feat: 로또번호에 중복된 숫자가 있을 경우 예외 발생 기능 작성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,10 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { Error.REQUIRED_LOTTO_NUMBER_COUNT.message }
+        require(numbers.toSet().size == 6) { Error.UNIQUE_LOTTO_NUMBERS.message }
     }
 
     // TODO: 추가 기능 구현
     private enum class Error(val message: String) {
         REQUIRED_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
+        UNIQUE_LOTTO_NUMBERS("로또 번호는 중복되어선 안됩니다."),
     }
 }
