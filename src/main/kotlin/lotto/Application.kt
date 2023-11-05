56341fd25ccade4e3eaa2d7030f88b79dfb1273b
yuhyun <dlugus@naver.com>
2023-11-05 18:08:47 +0900

    feat: 구입금액 입력받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun userInputMoney() {
+    print("구입금액을 입력해 주세요.")
+    val userInput = Console.readLine()
+    
+    return userInput
 }
