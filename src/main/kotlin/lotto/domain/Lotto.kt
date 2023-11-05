69fd55f91af1c617010ef90bb8d5212047b11413
MinhoiKoo <minlove2013@gmail.com>
2023-11-05 21:47:25 +0900

    feat: List 내장 메서드 사용하기 위해 numbers에 List 위임

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,7 +3,7 @@ package lotto.domain
 import lotto.utils.Constant.INVALID_SIZE_EXCEPTION_MESSAGE
 import lotto.utils.Constant.LOTTO_NUMBER_SIZE
 
-class Lotto(private val numbers: List<LottoNumber>) {
+class Lotto(private val numbers: List<LottoNumber>) : List<LottoNumber> by numbers {
     init {
         require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_SIZE_EXCEPTION_MESSAGE }
     }
