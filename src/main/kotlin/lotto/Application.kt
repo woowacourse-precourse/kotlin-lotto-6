516e84240f6b9042f9e0ceb7d6047015c3d166c4
book3039 <book3039@knou.ac.kr>
2023-11-05 16:05:38 +0900

    feat: Application.kt 로또 당첨 번호 및 보너스 번호 입력 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,7 +6,12 @@ import ui.UserOutput
 fun main() {
     val lottoGenerator = LottoGenerator(UserInput.readMoney())
     val lottos = lottoGenerator.create()
+
     UserOutput.printPurchaseResult(lottos.size)
     for (lotto in lottos) println(lotto.toAscendingList())
+
+    val lottoResult = LottoResult(lottos)
+    lottoResult.winLotto = Lotto(UserInput.readWinNumbers())
+    lottoResult.bonus = UserInput.readBonusNumber()
 
 }
