3022f64ad1c18b75b7733ca4929ad6e581022631
eunma <aa39043533@gmail.com>
2023-11-05 21:19:23 +0900

    로또클래스 번호정렬 기능 번호 중복 확인

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,15 @@
 package lotto
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(numbers: List<Int>) {
+    val sortedNumbers: List<Int> = numbers.sorted()
+
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == 6)
+    }
+
+    fun matchCount(winningNumbers: List<Int>): Int {
+        return sortedNumbers.intersect(winningNumbers).size
     }
 
     // TODO: 추가 기능 구현
