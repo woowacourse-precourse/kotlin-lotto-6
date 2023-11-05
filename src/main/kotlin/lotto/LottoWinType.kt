61884becef985934f0bca319bd157e1f9addf591
oh in seong <superohinsung@naver.com>
2023-11-06 02:20:11 +0900

    [refactor] add variable winsNumber

--- src/main/kotlin/lotto/LottoWinType.kt
+++ src/main/kotlin/lotto/LottoWinType.kt
@@ -1,11 +1,11 @@
 package lotto
 
-enum class LottoWinType(val prize: Long) {
+enum class LottoWinType(val prize: Long, var winsNumber: Int) {
 
-    THREE_MATCH(5000),
-    FOUR_MATCH(50000),
-    FIVE_MATCH(1500000),
-    FIVE_MATCH_WITH_BONUS(30000000),
-    SIX_MATCH(2000000000);
+    THREE_MATCH(5000, 0),
+    FOUR_MATCH(50000, 0),
+    FIVE_MATCH(1500000, 0),
+    FIVE_MATCH_WITH_BONUS(30000000, 0),
+    SIX_MATCH(2000000000, 0);
 
 }
