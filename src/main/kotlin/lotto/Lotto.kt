d56b474949eae0da116e3ebf9ecc94c60c343212
eunma <aa39043533@gmail.com>
2023-11-05 23:02:47 +0900

    enumclass를 적용하여 로또타게팅을 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,11 +6,39 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.toSet().size == 6)
     }
 
-    fun getSortedNumbers(): List<Int> {
-        return numbers.sorted()
+    fun calculateResult(winningNumbers: List<Int>, bonusNumber: Int): Rank {
+        val matchCount = numbers.intersect(winningNumbers).size
+        val bonusMatch = numbers.contains(bonusNumber)
+
+        return Rank.findRank(matchCount, bonusMatch)
+
+        fun getSortedNumbers(): List<Int> {
+            return numbers.sorted()
+        }
+
+        fun matchCount(winningNumbers: List<Int>): Int {
+            return getSortedNumbers().intersect(winningNumbers).size
+        }
     }
 
-    fun matchCount(winningNumbers: List<Int>): Int {
-        return getSortedNumbers().intersect(winningNumbers).size
+    enum class Rank(
+        val matchCount: Int,
+        val prizeMoney: Int,
+        val needBonus: Boolean = false
+    ) {
+        FIRST(6, 2_000_000_000),
+        SECOND(5, 30_000_000, true),
+        THIRD(5, 1_500_000),
+        FOURTH(4, 50_000),
+        FIFTH(3, 5_000),
+        MISS(0, 0);
+
+        companion object {
+            fun findRank(matchCount: Int, bonusMatch: Boolean): Rank {
+                return values().find { it.matchCount == matchCount && it.needBonus == bonusMatch }
+                    ?: values().find { it.matchCount == matchCount }
+                    ?: MISS
+            }
+        }
     }
 }
