09063900ad0df44a5a999aff2d6e858815ac5dba
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 02:50:49 +0900

    feat: 입력 받은 로또 구입금액을 관리하는 객체 생성

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -1,14 +1,23 @@
 package controller
 
+import model.Money
 import view.InputView
 import view.OutputView
 
-class LottoController(
-    private val inputView: InputView,
-    private val outputView: OutputView,
-) {
+class LottoController(private val inputView: InputView, private val outputView: OutputView) {
+
+    private lateinit var money: Money
     fun run() {
-        outputView.printPurchaseAmount()
-        val purchaseAmount = inputView.getValue()
+        do {
+            outputView.printPurchaseAmount()
+            val inputPrice = inputView.getValue()
+            try {
+                money = Money(inputPrice)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                continue
+            }
+            break
+        } while (true)
     }
 }
---
+++ src/main/kotlin/model/Money.kt
@@ -0,0 +1,17 @@
+package model
+
+import util.Validator.containsOnlyDigits
+import util.Validator.isAmountInThousandWon
+import util.Validator.isAmountOverMaxLimit
+import util.Validator.isEmptyValue
+import util.Validator.isValidNumberFormat
+
+class Money(private val price: String) {
+    init {
+        containsOnlyDigits(price)
+        isEmptyValue(price)
+        isValidNumberFormat(price)
+        isAmountInThousandWon(price)
+        isAmountOverMaxLimit(price)
+    }
+}
