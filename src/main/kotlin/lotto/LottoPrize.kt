c99bbf4924427598661b73d99b666c440f36938d
book3039 <book3039@knou.ac.kr>
2023-11-05 22:06:05 +0900

    feat: 로또 당첨 통계 기능 일부 구현

--- src/main/kotlin/lotto/LottoPrize.kt
+++ src/main/kotlin/lotto/LottoPrize.kt
@@ -1,9 +1,32 @@
 package lotto
 
-enum class LottoPrize(val value: Int, var count: Int, val prizeMoney: Int) {
-    JACKPOT(6, 0, 2_000_000_000),
-    SECOND_PRIZE(5, 0,30_000_000),
-    THIRD_PRIZE(5, 0,1_500_000),
-    FOUTRH_PRIZE(4, 0,50_000),
-    FIFTH_PRIZE(3, 0,5_000)
+import util.Constants.MONEY_FIFTH_PRIZE
+import util.Constants.MONEY_FOURTH_PRIZE
+import util.Constants.MONEY_JACKPOT
+import util.Constants.MONEY_SECOND_PRIZE
+import util.Constants.MONEY_THIRD_PRIZE
+
+enum class LottoPrize(val value: Int, var count: Int) {
+    JACKPOT(6, 0) {
+        override fun printPrizeData() =
+            println("${JACKPOT.value}개 일치 (${MONEY_JACKPOT}) - ${JACKPOT.count}개")
+    },
+    SECOND_PRIZE(5, 0){
+        override fun printPrizeData() =
+            println("${SECOND_PRIZE.value}개 일치 (${MONEY_SECOND_PRIZE}) - ${SECOND_PRIZE.count}개")
+    },
+    THIRD_PRIZE(5, 0){
+        override fun printPrizeData() =
+            println("${THIRD_PRIZE.value}개 일치 (${MONEY_THIRD_PRIZE}) - ${THIRD_PRIZE.count}개")
+    },
+    FOURTH_PRIZE(4, 0){
+        override fun printPrizeData() =
+            println("${FOURTH_PRIZE.value}개 일치 (${MONEY_FOURTH_PRIZE}) - ${FOURTH_PRIZE.count}개")
+    },
+    FIFTH_PRIZE(3, 0){
+        override fun printPrizeData() =
+            println("${FIFTH_PRIZE.value}개 일치 (${MONEY_FIFTH_PRIZE}) - ${FIFTH_PRIZE.count}개")
+    };
+
+    abstract fun printPrizeData()
 }
