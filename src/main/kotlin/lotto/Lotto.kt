787918bb92ccd9265dcbe14e0f7df51680162f30
jklom0326 <jklom0326@gmail.com>
2023-11-05 21:32:27 +0900

    feat: 로또 번호를 생성하는 기능 구현
    - 로또 번호에 대한 예외 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,16 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "[ERROR]: 번호를 6개만 입력해주세요" }
+        require(numbers.distinct().size == 6) { "[ERROR]: 중복된 번호가 있습니다." }
+        numbers.forEach {
+            require(it in 1..45) { "[ERROR]: 1부터 45 사이의 수를 입력해주세요." }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    fun getLottoNumbers(): List<Int> {
+        return numbers
+    }
 }
