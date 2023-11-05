5821a36e6750512c986d268c1a51cedc757808e6
book3039 <book3039@knou.ac.kr>
2023-11-05 14:43:16 +0900

    refactor: Vaildator 클래스 분리, Constants 클래스 생성, Lotto init 함수 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,13 +1,11 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
+import util.Validator
 
 class Lotto(private val numbers: List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)) {
     init {
-        require(
-            numbers.distinct()
-                .size == 6
-        )
+        Validator.checkProperNumbersSize(numbers)
         require(numbers.all { it in 1..45 })
     }
     fun toAscendingList() = numbers.sorted()
--- src/main/kotlin/ui/UserInput.kt
+++ src/main/kotlin/ui/UserInput.kt
@@ -1,26 +1,23 @@
 package ui
 
 import camp.nextstep.edu.missionutils.Console
+import util.Constants.MSG_INPUT_MONEY
+import util.Constants.MSG_INPUT_WIN_NUMBERS
+import util.Validator
 
-const val MSG_INPUT_MONEY = "구입금액을 입력해 주세요."
-const val MSG_INPUT_WIN_NUMBERS = "당첨 번호를 입력해 주세요."
-const val MSG_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
 
-const val MAX_PURCHASE_MONEY = 100_000
-const val MIN_PURCHASE_MONEY = 1000
-const val PROPER_SIZE = 6
 
 object UserInput {
 
     fun readMoney(): Int {
         println(MSG_INPUT_MONEY)
         val input = Console.readLine()
-        InputValidator
+        Validator
             .checkIsDigit(input)
             .checkIsEmptyString(input)
 
         val money = input.toInt()
-        InputValidator.checkPurchaseRange(money)
+        Validator.checkPurchaseRange(money)
 
         return money
     }
@@ -28,12 +25,12 @@ object UserInput {
     fun readWinNumbers(): List<Int> {
         println(MSG_INPUT_WIN_NUMBERS)
         val input = Console.readLine()
-        InputValidator
+        Validator
             .checkIsDigit(input)
             .checkIsEmptyString(input)
 
         val winNumbers = inputToNumbersByComma(input)
-        InputValidator.checkProperNumbersSize(winNumbers)
+        Validator.checkProperNumbersSize(winNumbers)
 
         return winNumbers
     }
@@ -43,27 +40,3 @@ private fun inputToNumbersByComma(input: String) =
     input.split(",")
         .map { it.toInt() }
         .distinct()
-
-object InputValidator {
-
-    fun checkIsDigit(input: String): InputValidator {
-        require(input.all{ it.isDigit() })
-        return this
-    }
-
-    fun checkIsEmptyString(input: String): InputValidator {
-        require(input.isNotEmpty())
-        return this
-    }
-
-    fun checkPurchaseRange(money: Int): InputValidator {
-        require(money in MIN_PURCHASE_MONEY..MAX_PURCHASE_MONEY)
-        return this
-    }
-
-    fun checkProperNumbersSize(numbers: List<Int>) =
-        require(
-            numbers.distinct()
-                .size == 6
-        )
-}
