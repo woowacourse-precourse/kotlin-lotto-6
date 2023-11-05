2c435e75f5be1c6fe7ae7d6fa5d43f517125f374
oh in seong <superohinsung@naver.com>
2023-11-06 07:44:32 +0900

    [feat] Enter purchase amount

--- src/main/kotlin/lotto/Game.kt
+++ src/main/kotlin/lotto/Game.kt
@@ -1,4 +1,15 @@
 package lotto
 
-class Game {
+import camp.nextstep.edu.missionutils.Console
+class Game private constructor(
+    private val calculator: Calculator = Calculator(),
+    private val printer: Printer = Printer()
+) {
+
+    fun purchaseLotto(): Int {
+        printer.printEnterPurchase()
+        val input = Console.readLine()
+        return calculator.calculateLottoAvailableForPurchase(input)
+    }
+
 }
