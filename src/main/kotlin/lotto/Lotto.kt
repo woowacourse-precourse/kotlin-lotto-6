5189154136526fc8007ffaf40a6a20678832bd57
yeonghyeon <agilecatch@gmail.com>
2023-11-05 18:01:22 +0900

    [Feat] : 입력받은 구입 금액에 해당하는 만큼 로또를 발행
    - 입력한 금액에 맞게 로또 티켓을 생성 합니다.
    - 각 티켓은 랜덤하게 선택된 6개의 숫자를 가지도록 하였습니다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 //로또 번호를 생성하고, 입력된 로또 번호와 당첨 번호를 비교하여 당첨 정보를 반환하는 역할을 합니다.
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+    }
+
+    fun generateLottoNumbers(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
     }
 
     // TODO: 추가 기능 구현
--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -2,5 +2,14 @@ package lotto
 
 //사용자 입력을 받아 모델에 전달하고, 모델로부터 받은 결과를 뷰에 전달합니다. 뷰와 모델을 연결하는 역할을 합니다.
 class LottoController(private val lottoView: LottoView, private val lotto: Lotto) {
+    fun generateLottoTickets(purchaseAmount: Int) {
+        val numOfTickets = lottoView.getPurchaseAmountFromUser() / 1000
+        val lottoTickets = mutableListOf<Lotto>()
 
+        repeat(numOfTickets) {
+            val numbers = lotto.generateLottoNumbers()
+            val lottoTicket = Lotto(numbers)
+            lottoTickets.add(lottoTicket)
+        }
+    }
 }
