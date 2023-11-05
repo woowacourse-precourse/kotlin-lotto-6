56bdb8f82cff9cc354f1bb1b9ce0eb1b0f3c6f51
DoDoBest <send1684@gmail.com>
2023-11-05 14:37:18 +0900

    refactor: intersect 함수를 사용해서 로또 등수를 구하는 코드 길이 줄이기

--- src/main/kotlin/lotto/data/Lotto.kt
+++ src/main/kotlin/lotto/data/Lotto.kt
@@ -8,27 +8,9 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun checkGrade(win: Lotto, bonusNum: Int): GRADE {
-        val (countOfSame, isBonusContain) = count(win.numbers, bonusNum)
+        val countOfSame = numbers.intersect(win.numbers.toSet()).size
+        val isBonusContain = bonusNum in numbers
         return GRADE.from(countOfSame, isBonusContain)
-    }
-
-    private fun count(winningNums: List<Int>, bonus: Int): Pair<Int, Boolean> {
-        var countOfSame = 0
-        var numIdx = 0
-        var winIdx = 0
-        val isBonusContain = bonus in numbers
-
-        while (numIdx < LENGTH_OF_NUM && winIdx < LENGTH_OF_NUM) {
-            if (numbers[numIdx] == winningNums[winIdx]) {
-                countOfSame++
-                numIdx++
-            } else if (numbers[numIdx] < winningNums[winIdx]) {
-                numIdx++
-                continue
-            }
-            winIdx++
-        }
-        return countOfSame to isBonusContain
     }
 
     override fun toString(): String {
