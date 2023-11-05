f2d58bf4122b1706af5e2e4dec5124c76b16c58e
민대인 <bamin0422@gmail.com>
2023-11-06 02:53:42 +0900

    test: checkWinningNumberSize - 당첨번호에 중복되는 숫자가 존재하면 예외가 발생한다 메서드 구현 및 테스팅

--- src/test/kotlin/lotto/util/LottoValidatorUtilTest.kt
+++ src/test/kotlin/lotto/util/LottoValidatorUtilTest.kt
@@ -1,5 +1,6 @@
 package lotto.util
 
+import lotto.constants.Error.EXCEPTION_MESSAGE_WINNING_NUMBER_DUPLICATED_NUMBER_EXIST
 import lotto.constants.Error.EXCEPTION_MESSAGE_WINNING_NUMBER_SIZE_INVALID
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
@@ -12,7 +13,7 @@ import java.util.stream.Stream
 
 internal class LottoValidatorUtilTest {
 
-    @DisplayName("중복되는 수 입력 검증")
+    @DisplayName("당첨번호 갯수 검증")
     @ParameterizedTest
     @MethodSource("winningNumberSizeInvalidProvider")
     fun `checkWinningNumberSize - 당첨번호의 갯수가 유효하지 않으면 예외가 발생한다`(winningNumber: List<Int>) {
@@ -22,6 +23,20 @@ internal class LottoValidatorUtilTest {
         }
         // when
         val expectedExceptionMessage = EXCEPTION_MESSAGE_WINNING_NUMBER_SIZE_INVALID
+        // then
+        Assertions.assertEquals(expectedExceptionMessage, exception.message)
+    }
+
+    @DisplayName("중복되는 수 입력 검증")
+    @ParameterizedTest
+    @MethodSource("winningNumberOverlappedProvider")
+    fun `checkWinningNumberSize - 당첨번호에 중복되는 숫자가 존재하면 예외가 발생한다`(winningNumber: List<Int>) {
+        // given
+        val exception = assertThrows<java.lang.IllegalArgumentException> {
+            LottoValidatorUtil.checkWinningNumberSize(winningNumber)
+        }
+        // when
+        val expectedExceptionMessage = EXCEPTION_MESSAGE_WINNING_NUMBER_DUPLICATED_NUMBER_EXIST
         // then
         Assertions.assertEquals(expectedExceptionMessage, exception.message)
     }
@@ -50,6 +65,16 @@ internal class LottoValidatorUtilTest {
                 Arguments.of(listOf(1,2,3,9)),
                 Arguments.of(listOf(1,2,3,4,5,6,7)),
                 Arguments.of(listOf(1,2,3,4)),
+            )
+        }
+
+        @JvmStatic
+        fun winningNumberOverlappedProvider(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(listOf(1,2,2,2,2,3)),
+                Arguments.of(listOf(1,2,3,9,9,4)),
+                Arguments.of(listOf(6,2,3,4,5,6)),
+                Arguments.of(listOf(1,2,3,4,3,2)),
             )
         }
     }
