b71a0b0dfdf4e46426e4fcc54e727956b7b07507
hugesilver <xor3160@naver.com>
2023-11-05 23:29:36 +0900

    update input money and exceptions

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,39 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
+var MONEY: Int? = null
+
 fun main() {
-    TODO("프로그램 구현")
+    initialize()
+    while (MONEY == null) {
+        inputMoney()
+    }
+}
+
+fun initialize() {
+    MONEY = null
+}
+
+fun inputMoney() {
+    println("구입금액을 입력해 주세요.")
+    val input = Console.readLine()
+    try {
+        checkMoney(input)
+
+        MONEY = input.toInt()
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+}
+
+fun checkMoney(tempMoney: String) {
+    // Int형이거나, 1000 이상이거나, 1000으로 나누어 떨어지는지 확인
+    if (tempMoney.toIntOrNull() == null || tempMoney.toInt() < 1000 || tempMoney.toInt() % 1000 != 0) {
+        throwErrorMessage("1000원 이상 입력 및 1000원으로 나누어 떨어지게 하세요.")
+    }
+}
+
+fun throwErrorMessage(text: String) {
+    throw IllegalArgumentException("[ERROR] $text")
 }
