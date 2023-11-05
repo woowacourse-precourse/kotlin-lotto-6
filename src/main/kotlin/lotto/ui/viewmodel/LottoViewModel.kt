9af7e5083f98352032dbb0ee680d19752e25cdbf
추민수 <devsoftychoo@gmail.com>
2023-11-05 19:33:29 +0900

    [Feat] : viewModel 기능을 할 클래스 구현
    - 게임과 관련된 비즈니스 로직을 처리하는 ViewModel 클래스
    - 비즈니스 로직과 UI를 분리하여 유지보수성, 확장성, 테스트 용이성 등을 높임

---
+++ src/main/kotlin/lotto/ui/viewmodel/LottoViewModel.kt
@@ -0,0 +1,10 @@
+package lotto.ui.viewmodel
+
+import lotto.data.model.LottoWinningNumber
+import lotto.data.model.UserLottoState
+import lotto.ui.repository.LottoRepository
+
+class LottoViewModel(private val repository: LottoRepository) {
+
+ 
+}
