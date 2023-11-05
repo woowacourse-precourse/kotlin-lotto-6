2d15cb8ec60ce160b2c28ba9996e6867f28bd192
MinhoiKoo <minlove2013@gmail.com>
2023-11-05 17:14:51 +0900

    feat: 공통으로 사용하는 상수 추가
    -로또의 숫자 개수가 6개가 아닐 때 출력하는 에러 메세지

--- src/main/kotlin/lotto/utils/Constant.kt
+++ src/main/kotlin/lotto/utils/Constant.kt
@@ -4,4 +4,7 @@ object Constant {
     const val LOTTO_NUMBER_SIZE = 6
     const val MIN_LOTTO_NUMBER = 1
     const val MAX_LOTTO_NUMBER = 45
+
+    const val INVALID_SIZE_EXCEPTION_MESSAGE = "[ERROR] 로또의 숫자 갯수는 6개여야 합니다!"
+
 }
