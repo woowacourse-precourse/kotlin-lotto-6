f82f1108f80b6817a52e9d49f8f16f8cce0baf94
aprilgom <aprilgom@users.noreply.github.com>
2023-11-05 17:58:16 +0900

    feat: 로또 번호와 번호 겹치는 숫자 몇 개인지 알아내는 기능 구현
    (cherry picked from commit 27cf09ec4a901e704f054d3a1f151e22d676d4ad)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,21 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
+    private val numbersSet = numbers.toSet()
     init {
         require(numbers.size == 6)
-        require(numbers.distinct().size == 6)
+        require(numbersSet.size == 6)
     }
 
     override fun toString(): String {
         return numbers.joinToString(", ", "[", "]")
+    }
+    companion object {
+        fun getHowMuchNumbersIntersect(a: Lotto, b: Lotto): Int {
+            val aSet = a.numbersSet
+            val bSet = b.numbersSet
+            return (aSet intersect bSet).size
+        }
     }
 }
