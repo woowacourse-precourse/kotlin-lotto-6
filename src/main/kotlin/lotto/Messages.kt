8de11ea26ecad605c5b4b0ddb9c9937f0bc1d94a
Minjun Kim <damon911@naver.com>
2023-11-05 12:52:57 +0900

    refactor: 구입 금액의 형식 오류와 범위 오류의 에러 메세지를 따로 설정

--- src/main/kotlin/lotto/Messages.kt
+++ src/main/kotlin/lotto/Messages.kt
@@ -5,6 +5,7 @@ enum class Messages(val message: String) {
     TEXT_INPUT_LOTTO_AMOUNT("구입 금액을 입력해 주세요."),
 
     // EXCEPTION
+    EXCEPTION_WRONG_FORMAT("[ERROR] 구입 금액의 형식이 잘못되었습니다."),
     EXCEPTION_WRONG_RANGE("[ERROR] 구입 금액의 범위는 1,000원 이상 4,611,686,018,000원 이하입니다."),
     EXCEPTION_WRONG_MONEY_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다."),
 }
--- src/main/kotlin/lotto/Validation.kt
+++ src/main/kotlin/lotto/Validation.kt
@@ -3,7 +3,7 @@ package lotto
 object Validation {
 
     fun validateOutOfRange(amount: String): Long {
-        val num = amount.toLongOrNull() ?: throw IllegalArgumentException(Messages.EXCEPTION_WRONG_RANGE.message)
+        val num = amount.toLongOrNull() ?: throw IllegalArgumentException(Messages.EXCEPTION_WRONG_FORMAT.message)
         require(num in 1000..4_611_686_018_000) { Messages.EXCEPTION_WRONG_RANGE.message }
         return num
     }
