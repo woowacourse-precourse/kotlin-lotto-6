c601898d701a2264f7ab7fec83f6a478dc002318
seongsan <tjdtks030@naver.com>
2023-11-06 04:24:06 +0900

    test: InputValidator 공통 및 로또 구매 금액 입력 관련 테스트 코드 작성
    입력값 공백 및 정수 여부, 로또 구매 금액 관련 테스트 코드 구현

---
+++ src/test/kotlin/lotto/InputValidatorTest.kt
@@ -0,0 +1,47 @@
+package lotto
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import lotto.InputValidator as Validator
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class InputValidatorTest {
+
+    @Test
+    @DisplayName("공백 여부")
+    fun `checkIsNotBlank 메서드 사용 시 입력값이 공백일 때 예외 발생`() {
+        val input = ""
+
+        assertThrows<IllegalArgumentException> { Validator.checkIsNotBlank(input) }
+    }
+
+    @Test
+    @DisplayName("정수 여부")
+    fun `checkHasNotWord 메서드 사용 시 입력값이 정수가 아닐 때 예외 발생`() {
+        val input = "11.5"
+
+        assertThrows<IllegalArgumentException> { Validator.checkIsNotNumber(input) }
+    }
+
+    @Nested
+    @DisplayName("로또 구매 금액")
+    class Money {
+
+        @Test
+        @DisplayName("예외1")
+        fun `checkDividedAsThousand 메서드 사용 시 입력값이 1000 으로 나누어 떨어지지 않을 때 예외 발생`() {
+            val input = "1200"
+
+            assertThrows<IllegalArgumentException> { Validator.checkDividedAsThousand(input) }
+        }
+
+        @Test
+        @DisplayName("예외2")
+        fun `checkIsNotZero 메서드 사용 시 입력값이 0 일 때 예외 발생`() {
+            val input = "0"
+
+            assertThrows<IllegalArgumentException> { Validator.checkIsNotZero(input) }
+        }
+    }
+}
