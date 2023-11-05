c40ebffa5e9121173b882019bb9ec09e3ebfab65
민대인 <bamin0422@gmail.com>
2023-11-06 02:27:09 +0900

    test: LottoValidatorUtilTest 생성

---
+++ src/test/kotlin/lotto/util/LottoValidatorUtilTest.kt
@@ -0,0 +1,26 @@
+package lotto.util
+
+import org.junit.jupiter.api.Test
+
+internal class LottoValidatorUtilTest {
+
+    @Test
+    fun checkWinningNumberSize() {
+    }
+
+    @Test
+    fun checkNumberInRange() {
+    }
+
+    @Test
+    fun checkMoneyAvailable() {
+    }
+
+    @Test
+    fun checkWinningNumberAvailable() {
+    }
+
+    @Test
+    fun checkNumberOverlap() {
+    }
+}
