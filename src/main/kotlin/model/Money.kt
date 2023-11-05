e371cb829f2174fd5e4689c31c4a0d11bb8016ef
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 05:35:21 +0900

    refactor: 기존 최대 금액을 넘을 경우 예외 발생기능에 로또 금액보다 낮은 금액을 입력하였을 경우 예외 발생기능 추가

--- src/main/kotlin/model/Money.kt
+++ src/main/kotlin/model/Money.kt
@@ -5,8 +5,8 @@ class Money(private val price: String) {
         require(price.all { it.isDigit() }) { Error.INPUT_ONLY_NUMBER.message }
         require(price.isNotBlank()) { Error.PURCHASE_AMOUNT_EMPTY.message }
         require(price.first() != '0') { Error.NUMBER_FORMAT.message }
-        require(price.toInt() % 1000 == 0) { Error.PURCHASE_IN_THOUSAND_WON.message }
-        require(price.toInt() <= 100_000) { Error.AMOUNT_OVER_MAX_LIMIT.message }
+        require(price.toInt() in 1_000..100_000) { Error.AMOUNT_OVER_MAX_LIMIT.message }
+        require(price.toInt() % 1_000 == 0) { Error.PURCHASE_IN_THOUSAND_WON.message }
     }
 
     fun getPurchasableLottoTicketCount() = price.toInt() / 1000
@@ -15,7 +15,7 @@ class Money(private val price: String) {
         INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
         PURCHASE_AMOUNT_EMPTY("[ERROR] 구매 금액이 입력되지 않았습니다."),
         NUMBER_FORMAT("[ERROR] 올바른 숫자 형태가 아닙니다."),
+        AMOUNT_OVER_MAX_LIMIT("[ERROR] 최소 1,000원 부터 최대 100,000원 까지 구매가 가능합니다."),
         PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
-        AMOUNT_OVER_MAX_LIMIT("[ERROR] 최대 100,000원 까지 구매가 가능합니다."),
     }
 }
