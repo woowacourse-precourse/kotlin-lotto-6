c18656baa09791b0fe6de4a4827164863f3383e8
dpcks0509 <dpcks0509@gmail.com>
2023-11-05 14:51:27 +0900

    style: 당첨 순위 상수명 변경
    당첨 순위를 나타내는 상수명을 prize에서 rank로 변경하였다.

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -5,9 +5,16 @@ object Constants {
     const val LOTTO_MAX_NUMBER = 45
     const val LOTTO_TOTAL_NUMBER = 6
     const val LOTTO_AMOUNT_UNIT = 1000
+    const val FIRST_RANK = 1
+    const val SECOND_RANK = 2
+    const val THIRD_RANK = 3
+    const val FOURTH_RANK = 4
+    const val FIFTH_RANK = 5
+    const val NO_RANK = 0
     const val FIRST_PRIZE_AMOUNT = 2000000000
     const val SECOND_PRIZE_AMOUNT = 30000000
     const val THIRD_PRIZE_AMOUNT = 1500000
     const val FOURTH_PRIZE_AMOUNT = 50000
     const val FIFTH_PRIZE_AMOUNT = 5000
+    const val NO_PRIZE_AMOUNT = 0
 }
