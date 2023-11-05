8697b94456cdcd4229008b855627bd341e74305d
hugesilver <xor3160@naver.com>
2023-11-06 00:12:37 +0900

    update make random numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 var MONEY: Int? = null
 
@@ -9,6 +10,7 @@ fun main() {
     while (MONEY == null) {
         inputMoney()
     }
+    makeLottoes()
 }
 
 fun initialize() {
@@ -20,8 +22,8 @@ fun inputMoney() {
     val input = Console.readLine()
     try {
         checkMoney(input)
-
         MONEY = input.toInt()
+        println()
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
@@ -31,6 +33,14 @@ fun checkMoney(tempMoney: String) {
     // Int형이거나, 1000 이상이거나, 1000으로 나누어 떨어지는지 확인
     if (tempMoney.toIntOrNull() == null || tempMoney.toInt() < 1000 || tempMoney.toInt() % 1000 != 0) {
         throwErrorMessage("1000원 이상 입력 및 1000원으로 나누어 떨어지게 하세요.")
+    }
+}
+
+fun makeLottoes() {
+    println("${MONEY!! / 1000}개를 구매했습니다.")
+    for(i in 0 until (MONEY!! / 1000)) {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        println(numbers)
     }
 }
 
