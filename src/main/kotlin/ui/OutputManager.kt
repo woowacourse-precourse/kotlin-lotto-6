ce30bc64619b1e700586058ec397d3472f71a57e
jiwon2724 <stopone3119@naver.com>
2023-11-06 00:01:58 +0900

    [chore] 함수 수정 및 사용 안되는 함수 제거

--- src/main/kotlin/ui/OutputManager.kt
+++ src/main/kotlin/ui/OutputManager.kt
@@ -6,17 +6,12 @@ class OutputManager {
     fun invalidLottoNumbers() = println("${OutputMessages.INVALID_LOTTO_NUMBERS} ${OutputMessages.RE_TRY}")
     fun invalidBonusNumber() = println("${OutputMessages.INVALID_BONUS_NUMBER} ${OutputMessages.RE_TRY}")
     fun purchaseLottoCount(lottoCount: Int) = println(OutputMessages.PURCHASE_LOTTO_COUNT.toString().format(lottoCount))
-    fun lottoNumbers(numbers: List<Int>) = println(numbers)
+    fun lottoNumbers(lottoNumber: List<Int>) = println(lottoNumber)
     fun promptJackpotNumbers() = println(OutputMessages.PROMPT_JACKPOT_NUMBERS)
     fun promptBonusNumber() = println(OutputMessages.PROMPT_BONUS_NUMBER)
     fun lottoStats() = println(OutputMessages.LOTTO_STATS)
-    fun retry() = println(OutputMessages.RE_TRY)
     fun line() = println(OutputMessages.LINE)
-    fun lottoResult(
-        correctLottoNumbers: Int,
-        winningAmount: String,
-        winningCount: Int
-    ) = println(OutputMessages.LOTTO_RESULT.toString().format(correctLottoNumbers, winningAmount, winningCount))
+    fun rateOfReturn(lottoRateOfReturn: String) = println(OutputMessages.RATE_OF_RETURN.toString().format(lottoRateOfReturn))
 }
 
 enum class OutputMessages(private val message: String) {
@@ -25,13 +20,13 @@ enum class OutputMessages(private val message: String) {
     INVALID_LOTTO_NUMBERS("$ERROR 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     INVALID_BONUS_NUMBER("$ERROR 보너스 번호는 당첨번호와 중복되면 안됩니다."),
     PROMPT_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
-    PROMPT_JACKPOT_NUMBERS("당첨 번호를 입력해 주세요."),
-    PROMPT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    PURCHASE_LOTTO_COUNT("%d개를 구매했습니다."),
-    LOTTO_RESULT("%d개 일치 (%s원) - %d개"),
+    PROMPT_JACKPOT_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    PROMPT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    PURCHASE_LOTTO_COUNT("\n%d개를 구매했습니다."),
     RE_TRY("다시 입력해주세요."),
     LINE("---"),
-    LOTTO_STATS("당첨 통계");
+    LOTTO_STATS("\n당첨 통계"),
+    RATE_OF_RETURN("총 수익률은 %s입니다.");
 
     override fun toString(): String = message
 }
