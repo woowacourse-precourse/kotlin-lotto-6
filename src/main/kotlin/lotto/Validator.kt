7200206e97d6d52d76dce949f1b48f69376f2b0e
Sheep1sik <bohg6799@naver.com>
2023-11-05 23:39:33 +0900

    feat: 공백 입력시 예외처리 기능 구현
    - purchaseAmount, winningNumbers, bonus 예외처리 추가

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -2,6 +2,7 @@ package lotto
 
 import lotto.Constants.Companion.ERROR_DUPLICATE_WINNING_AND_BONUS_NUMBER_MESSAGE
 import lotto.Constants.Companion.ERROR_DUPLICATE_WINNING_NUMBER_MESSAGE
+import lotto.Constants.Companion.ERROR_EMPTY_WINNING_NUMBER_MESSAGE
 import lotto.Constants.Companion.ERROR_INVALID_LOTTO_AMOUNT_1000_EXCEPTION_MESSAGE
 import lotto.Constants.Companion.ERROR_INVALID_NOT_NUMBER_LOTTO_AMOUNT_MESSAGE
 import lotto.Constants.Companion.ERROR_INVALID_NUMBER_COUNT_MESSAGE
@@ -16,6 +17,10 @@ import lotto.Constants.Companion.MIN_NUMBER
 class Validator {
 
     fun isUserPurchaseAmountCheck(purchaseAmount: String) {
+        if (isEmptyNumber(purchaseAmount)) {
+            println(ERROR_EMPTY_WINNING_NUMBER_MESSAGE)
+            throw IllegalArgumentException()
+        }
         if (isNumeric(purchaseAmount)) {
             println(ERROR_INVALID_NOT_NUMBER_LOTTO_AMOUNT_MESSAGE)
             throw IllegalArgumentException()
@@ -25,19 +30,25 @@ class Validator {
             throw IllegalArgumentException()
         }
     }
+
     private fun isDivisibleBy1000(purchaseAmount: String): Boolean {
         return purchaseAmount.toInt() % LOTTO_PRICE != MIN_NUMBER
     }
+
     private fun isNumeric(number: String): Boolean {
         return number.toIntOrNull() == null
     }
 
-    fun isUserWinningNumbersCheck(winningNumbers: List<Int>){
+    fun isUserWinningNumbersCheck(winningNumbers: List<Int>) {
+        if (isEmptyWinningNumbers(winningNumbers)) {
+            println(ERROR_EMPTY_WINNING_NUMBER_MESSAGE)
+            throw IllegalArgumentException()
+        }
         if (isTooManyWinningNumbers(winningNumbers)) {
             println(ERROR_INVALID_NUMBER_COUNT_MESSAGE)
             throw IllegalArgumentException()
         }
-        if(isOutOfRangeWinningNumbers(winningNumbers)) {
+        if (isOutOfRangeWinningNumbers(winningNumbers)) {
             println(ERROR_OUT_OF_RANGE_NUMBER_MESSAGE)
             throw IllegalArgumentException()
         }
@@ -54,22 +65,33 @@ class Validator {
     private fun isTooManyWinningNumbers(winningNumbers: List<Int>): Boolean {
         return winningNumbers.size > MAX_NUMBER
     }
+
     private fun isOutOfRangeWinningNumbers(winningNumbers: List<Int>): Boolean {
         return winningNumbers.any { it < MIN_LOTTO_NUMBER || it > MAX_LOTTO_NUMBER }
     }
+
     private fun isDuplicateLottoNumbers(winningNumbers: List<Int>): Boolean {
         return winningNumbers.size != winningNumbers.toSet().size
     }
+
     private fun isPositiveWinningNumbers(winningNumbers: List<Int>): Boolean {
         return !winningNumbers.all { it > 0 }
     }
 
+    private fun isEmptyWinningNumbers(winningNumbers: List<Int>): Boolean {
+        return winningNumbers.isEmpty()
+    }
+
     fun isUserBonusNumberCheck(lotto: List<Int>, bonus: String) {
+        if (isEmptyNumber(bonus)) {
+            println(ERROR_EMPTY_WINNING_NUMBER_MESSAGE)
+            throw IllegalArgumentException()
+        }
         if (isNumeric(bonus)) {
             println(ERROR_INVALID_NOT_NUMBER_LOTTO_AMOUNT_MESSAGE)
             throw IllegalArgumentException()
         }
-        if (isLottoAndBonusDuplicates(lotto,bonus)) {
+        if (isLottoAndBonusDuplicates(lotto, bonus)) {
             println(ERROR_DUPLICATE_WINNING_AND_BONUS_NUMBER_MESSAGE)
             throw IllegalArgumentException()
         }
@@ -82,14 +104,21 @@ class Validator {
             throw IllegalArgumentException()
         }
     }
+
     private fun isLottoAndBonusDuplicates(lotto: List<Int>, bonus: String): Boolean {
         return lotto.contains(bonus.toInt())
     }
+
     private fun isPositiveBonusNumber(bonus: String): Boolean {
         return bonus.toInt() < MIN_NUMBER
     }
+
     private fun isOutOfRangeBonusNumber(bonus: String): Boolean {
         return bonus.toInt() < MIN_LOTTO_NUMBER || bonus.toInt() > MAX_LOTTO_NUMBER
+    }
+
+    private fun isEmptyNumber(bonus: String): Boolean {
+        return bonus.isEmpty()
     }
 
 }
