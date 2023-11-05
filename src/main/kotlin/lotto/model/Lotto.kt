a86adad6f6dc890b732748c94945da48eac80336
jaehan4707 <jahan4707@naver.com>
2023-11-05 16:13:33 +0900

    feat : Lotto 예외 처리 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,8 +1,17 @@
 package lotto.model
 
+import lotto.util.Constant
+import lotto.util.Constant.LOTTO_MAX_RANDOM_NUMBER
+import lotto.util.Constant.LOTTO_MIN_RANDOM_NUMBER
+import lotto.util.Constant.LOTTO_NUMBER_ERROR_MESSAGE
+import lotto.util.Constant.LOTTO_NUMBER_NUMBER_LENGTH_ERROR_MESSAGE
+import lotto.util.Constant.LOTTO_NUMBER_OVERLAP_ERROR_MESSAGE
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {LOTTO_NUMBER_NUMBER_LENGTH_ERROR_MESSAGE}
+        require(numbers.max() <= LOTTO_MAX_RANDOM_NUMBER || numbers.min() >= LOTTO_MIN_RANDOM_NUMBER) { LOTTO_NUMBER_ERROR_MESSAGE}
+        require(numbers.toSet().size==6) { LOTTO_NUMBER_OVERLAP_ERROR_MESSAGE}
     }
 
     // TODO: 추가 기능 구현
