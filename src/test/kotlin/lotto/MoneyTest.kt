2c476e4af97234a38a212472463e866140e85519
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 03:30:57 +0900

    rename: 테스트 파일명 수정, BuyingPriceTest -> MoneyTest

--- src/test/kotlin/lotto/MoneyTest.kt
+++ src/test/kotlin/lotto/MoneyTest.kt
@@ -4,7 +4,7 @@ import model.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class BuyingPriceTest {
+class MoneyTest {
     @Test
     fun `숫자가 아닌 문자가 있을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
