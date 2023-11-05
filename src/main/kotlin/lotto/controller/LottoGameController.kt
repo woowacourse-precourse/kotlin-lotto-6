2ac94745b18122c813f8a2b43cc402805fde2bb9
Tnalxmsk <alstnwkd990@naver.com>
2023-11-05 17:24:32 +0900

    feat(lotto): LottoGameController 기능 수정 및 실행 구현
    createWinningNumber() 코드 개선
    로또 게임 실행 구현

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -1,34 +1,47 @@
 package lotto.controller
 
+import lotto.model.Grade
 import lotto.view.Input
 import lotto.model.Lotto
 import lotto.model.Seller
 import lotto.model.WinningNumber
+import lotto.view.LottoResultView
 
 class LottoGameController {
     private val seller = Seller()
     private val input = Input()
+    private val grade = Grade()
+    private val lottoResultView = LottoResultView()
 
     fun startLottoGame() {
         val money = input.inputMoney()
         val lottoPapers = seller.sellLotto(money)
+
         printLotto(lottoPapers)
 
         val winningNumber = createWinningNumber()
+        lottoPapers.forEach { myLotto ->
+            grade.decideGrade(myLotto, winningNumber)
+        }
+
+        val gradeResult = grade.getLottoGradeResult()
+        lottoResultView.printLottoStatistics(grade.getLottoGradeResult())
+        lottoResultView.printRateReturn(gradeResult, money)
     }
 
+    private fun createWinningNumber(): WinningNumber = WinningNumber(
+        input.inputWinningNumber(),
+        input.inputBonusNumber()
+    )
+
     private fun printLotto(lottoPaper: List<Lotto>) {
-        println("\n${getLottoCount(lottoPaper)}$USER_LOTTO")
+        println("\n${USER_LOTTO.format(lottoPaper.size)}")
         lottoPaper.forEach {
             it.printNumbers()
         }
     }
 
-    private fun createWinningNumber(): WinningNumber = WinningNumber(input.inputWinningNumber(), input.inputBonusNumber())
-
     companion object {
-        private var USER_LOTTO = "개를 구매했습니다."
-        fun getLottoCount(lottoPaper: List<Lotto>): Int = lottoPaper.size
-
+        private const val USER_LOTTO = "%d개를 구매했습니다."
     }
 }
