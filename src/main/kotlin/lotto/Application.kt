b846948e1d1331ea3585361a7c3fb7428729c0c5
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-06 22:46:31 +0900

    로또 구입 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,7 +1,10 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
 fun main() {
-
+    val purchaseAmount = getValidPurchaseAmount()
+    val tickets = buyLottoTickets(purchaseAmount)
+    tickets.forEach { println(it) }
 }
 
 fun buyLottoTickets(purchaseAmount: Int): List<Lotto> {
@@ -9,5 +12,26 @@ fun buyLottoTickets(purchaseAmount: Int): List<Lotto> {
     return List(ticketCount) {
         val numbers = generateLottoNumbers()
         Lotto(numbers)
+    }
+}
+fun inputPurchaseAmount(): String {
+    println("구입금액을 입력해 주세요.")
+    return readLine().orEmpty()
+}
+
+fun getValidPurchaseAmount(): Int {
+    while (true) {
+        val input = inputPurchaseAmount()
+        try {
+            val amount = input.toInt()
+            if (amount % 1000 != 0) {
+                throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위로 입력해야 합니다.")
+            }
+            return amount
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 로또 구입 금액은 숫자로 입력해야 합니다.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
     }
 }
