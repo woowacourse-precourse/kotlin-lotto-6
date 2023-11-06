415003113c6563990ec09dac3af40e7f7e7f6d2e
hongjeZZ <ajyong316@gmail.com>
2023-11-06 23:58:26 +0900

    refactor: 당첨 결과 계산하는 메서드 분리

--- src/main/kotlin/lotto/domain/ResultCalculator.kt
+++ src/main/kotlin/lotto/domain/ResultCalculator.kt
@@ -4,15 +4,25 @@ class ResultCalculator(
     private val lottos: List<Lotto>,
     private val winningLotto: WinningLotto
 ) {
+    private var matchingCount = 0
+    private var bonusMatch = false
+
     fun calculateResult(): LottoResult {
         val result = LottoResult()
 
         for (lotto in lottos) {
-            val count = winningLotto.calculateMatchingCount(lotto)
-            val bonus = winningLotto.calculateBonusMatch(lotto)
-            val rank = Rank.checkRank(count, bonus)
-            result.addCount(rank)
+            calculateCount(lotto)
+            isBonusMatch(lotto)
+            result.addCount(Rank.checkRank(matchingCount, bonusMatch))
         }
         return result
+    }
+
+    private fun calculateCount(lotto: Lotto) {
+        matchingCount = winningLotto.checkCountMatched(lotto)
+    }
+
+    private fun isBonusMatch(lotto: Lotto) {
+        bonusMatch = winningLotto.checkBonusMatch(lotto)
     }
 }
