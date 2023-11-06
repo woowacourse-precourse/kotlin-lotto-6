390d18e8c817e053720ac3b559e7e906e7ed6f3f
sunkk8482 <sunkk8482@naver.com>
2023-11-06 13:30:33 +0900

    Refactor : 상금 비율 계산 분리 및 이름 변경

--- src/main/kotlin/lotto/domain/Prize.kt
+++ src/main/kotlin/lotto/domain/Prize.kt
@@ -3,18 +3,18 @@ package lotto.domain
 import kotlin.math.roundToInt
 
 class Prize {
-    fun checkPrize(grades: List<Grade>, money: Int): Double {
+    fun getPrizeRatio(grades: List<Grade>, money: Int): Double {
         val totalPrize = countPrize(grades)
-        val ratio = (totalPrize.toDouble() / money.toDouble()) * 100
+        val ratio = calculateRatio(totalPrize,money)
         return (ratio * 100).roundToInt() / 100.0
     }
 
-    private fun countPrize(grades: List<Grade>) : Long {
-        var totalPrize: Long = 0
-        for (reward in grades) {
-            totalPrize+=reward.rewardMoney
-        }
-        return totalPrize
+    private fun countPrize(grades: List<Grade>): Long {
+        return grades.sumOf { it.rewardMoney.toLong() }
+    }
+
+    private fun calculateRatio(totalPrize : Long, money : Int):Double{
+        return (totalPrize.toDouble() / money.toDouble()) * 100
     }
 
 
