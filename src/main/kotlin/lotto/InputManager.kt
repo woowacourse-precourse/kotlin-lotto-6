4e0b71c4528837c5c32a552fe71d5c54e632bfb6
LEE <tpqms775@naver.com>
2023-11-06 13:43:03 +0900

    feat : 구입금액, 당첨번호, 보너스 번호 입력받기

---
+++ src/main/kotlin/lotto/InputManager.kt
@@ -0,0 +1,19 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputManager {
+    fun inputMoney(): String? {
+        println("구입금액을 입력해 주세요.")
+        return Console.readLine()
+    }
+    fun inputWinningNumber(): String? {
+        println("당첨 번호를 입력해 주세요.")
+        return Console.readLine()
+    }
+    fun inputBonusNumber(): String? {
+        println("보너스 번호를 입력해 주세요.")
+        return Console.readLine()
+    }
+
+}
