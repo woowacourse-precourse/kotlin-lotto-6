7b558cdc7ff22cea94b039f33dadbaa8496c017a
unso99 <unso99@naver.com>
2023-11-06 20:47:31 +0900

    :sparkles: Lotto 파일 check 기능 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto.model
 
+import lotto.util.Check.checkWinningNumbers
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-        //todo 서로다른 숫자 인지, 1~45까지의 숫자인지 체크필요
+        checkWinningNumbers(numbers)
     }
 
     fun getNumbers() = numbers
