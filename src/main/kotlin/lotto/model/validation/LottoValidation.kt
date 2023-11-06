2e73b9cd86f20ea2ec0ba5a6276ef4a08071e6b2
seoyeon <seoyeonn051@gmail.com>
2023-11-06 10:23:52 +0900

    Refactor: LottoValidation과 InputValidation 기능 분리

---
+++ src/main/kotlin/lotto/model/validation/LottoValidation.kt
@@ -0,0 +1,28 @@
+package lotto.model
+
+class LottoNumValidation {
+    fun validateLottoNum(input: List<Int>) {
+        checkLottoNumSize(input)
+        checkForDuplicateLottoNumbers(input)
+        checkLottoNumRange(input)
+    }
+
+    private fun checkLottoNumSize(input: List<Int>) {
+        require(input.size == 6) { LOTTO_NUM_SIZE_ERROR }
+    }
+
+    private fun checkForDuplicateLottoNumbers(input: List<Int>) {
+        require(input.size == input.distinct().count()) { DUPLICATE_LOTTO_NUM_ERROR }
+    }
+
+    private fun checkLottoNumRange(input: List<Int>) {
+        require(input.all { it in LottoNumbersFactory.LOTTO_START_NUM..LottoNumbersFactory.LOTTO_START_NUM }) { LOTTO_NUM_RANGE_ERROR }
+    }
+
+
+    companion object {
+        const val LOTTO_NUM_SIZE_ERROR = "[ERROR] 로또 번호의 숫자의 개수는 6개입니다."
+        const val DUPLICATE_LOTTO_NUM_ERROR = "[ERROR] 로또 번호는 중복될 수 없습니다."
+        const val LOTTO_NUM_RANGE_ERROR = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    }
+}
