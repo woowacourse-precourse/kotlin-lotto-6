dc776565b5e4aeee0bff93a3fb702d220ad76823
applepepsi <dlaejrtnz@gmail.com>
2023-11-07 08:36:27 +0900

    feat(config/GameConfigValue): Add the prize money

--- src/main/kotlin/lotto/config/GameConfigValue.kt
+++ src/main/kotlin/lotto/config/GameConfigValue.kt
@@ -5,8 +5,13 @@ object GameConfigValue {
     const val PURCHASE_AMOUNT_UNIT=1000
     const val MINIMUM_LOTTO_NUMBER=1
     const val MAXIMUM_LOTTO_NUMBER=45
-    const val SIX_NUMBER_CORRECT=6
-    const val FIVE_NUMBER_CORRECT=5
-    const val FOUR_NUMBER_CORRECT=4
-    const val THREE_NUMBER_CORRECT=3
+    const val SIX_NUMBER=6
+    const val FIVE_NUMBER=5
+    const val FOUR_NUMBER=4
+    const val THREE_NUMBER=3
+    const val SIX_NUMBER_NUMBER_PRICE=2000000000
+    const val FIVE_NUMBER_AND_BONUS_NUMBER_PRICE=30000000
+    const val FIVE_NUMBER_NUMBER_PRICE=1500000
+    const val FOUR_NUMBER_NUMBER_PRICE=50000
+    const val THREE_NUMBER_NUMBER_PRICE=5000
 }
