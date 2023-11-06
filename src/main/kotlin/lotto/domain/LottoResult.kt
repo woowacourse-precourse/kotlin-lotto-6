429657aa34ef367590e9ef0bc11f295538a0417c
j2j2 <fineimagination@gmail.com>
2023-11-06 23:29:00 +0900

    feat(lotto): 로또 결과 확인 기능 구현

---
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -0,0 +1,34 @@
+package lotto.domain
+
+import lotto.Lotto
+
+class LottoResult(private val lottos: List<Lotto>, private val winningNumbers: WinningNumber) {
+    private var resultLottos: MutableMap<LottoRank, Int> = mutableMapOf()
+    private var totalPrize: Long = 0L
+    private var profit: Double = 0.0
+
+    init {
+        calculateResult()
+    }
+
+    private fun calculateResult() {
+        LottoRank.entries.forEach { resultLottos[it] = 0 }
+        for (lotto in lottos) {
+            val compareResult = lotto.compareNumber(winningNumbers)
+            val lottoRank = LottoRank.fromMatches(compareResult.first, compareResult.second)
+            if (lottoRank != LottoRank.NO_MATCH) {
+                resultLottos[lottoRank] = resultLottos.getOrDefault(lottoRank, 0) + 1
+                totalPrize += lottoRank.prize.toLong()
+            }
+        }
+    }
+
+    fun printResultStatistics() {
+        println("\n--- 당첨 통계 ---")
+        resultLottos.filterKeys { it != LottoRank.NO_MATCH }.forEach { (key, value) ->
+            println("${key.toString()} - ${value}개")
+        }
+    }
+
+
+}
