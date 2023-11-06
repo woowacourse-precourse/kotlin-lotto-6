8daaaded881ccd570df873dbad451e2cf87ee8fc
jinuemong <jinwoo3661@naver.com>
2023-11-06 12:40:24 +0900

    Feat: 로또 번호를 생성하는 LottoManager 기능 구현

---
+++ src/main/kotlin/lotto/domain/LottoManager.kt
@@ -0,0 +1,20 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.model.Lotto
+
+class LottoManager {
+
+    fun makeLotto(): Lotto = Lotto(makeRandomNumbers())
+
+    private fun makeRandomNumbers(): List<Int> =
+        Randoms.pickUniqueNumbersInRange(
+            LOTTO_START_INCLUSIVE, LOTTO_END_INCLUSIVE, LOTTO_COUNT
+        )
+
+    companion object {
+        const val LOTTO_START_INCLUSIVE = 1
+        const val LOTTO_END_INCLUSIVE = 1
+        const val LOTTO_COUNT = 1
+    }
+}
