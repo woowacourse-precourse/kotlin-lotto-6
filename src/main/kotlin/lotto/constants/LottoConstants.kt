a2ed72754172cba1790bd1396ed56c51d20b25cc
sangil <fhemflrh60@gmail.com>
2023-11-07 00:27:57 +0900

    refactor: Constant 수정

--- src/main/kotlin/lotto/constants/LottoConstants.kt
+++ src/main/kotlin/lotto/constants/LottoConstants.kt
@@ -2,4 +2,6 @@ package lotto.constants
 
 object LottoConstants {
     const val LOTTO_PRICE = 1000
+    const val MIN_LOTTO_NUMBER = 1
+    const val MAX_LOTTO_NUMBER = 45
 }
--- src/main/kotlin/lotto/util/Printer.kt
+++ src/main/kotlin/lotto/util/Printer.kt
@@ -1,10 +1,14 @@
 package lotto.util
 
-import lotto.constants.LottoConstants
+import lotto.constants.LottoConstants.LOTTO_PRICE
+import lotto.constants.LottoConstants.MIN_LOTTO_NUMBER
+import lotto.constants.LottoConstants.MAX_LOTTO_NUMBER
 
 object Printer {
-    private const val MESSAGE_INPUT_MONEY = "로또를 구입할 금액을 입력해 주세요. 로또는 개당 ${LottoConstants.LOTTO_PRICE}원 입니다."
-    private const val MESSAGE_INPUT_NUMBERS = "로또 당첨 번호로 사용할 6개의 숫자를 쉼표(,)로 구분해 입력해 주세요."
+    private const val MESSAGE_INPUT_MONEY = "로또를 구입할 금액을 숫자만 입력해 주세요. 로또는 개당 ${LOTTO_PRICE}원 입니다."
+    private const val MESSAGE_INPUT_NUMBERS =
+        "로또 당첨 번호로 사용할 $MIN_LOTTO_NUMBER ~ $MAX_LOTTO_NUMBER 사이의 숫자 6개를 쉼표(,)로 구분해 입력해 주세요."
+    private const val MESSAGE_INPUT_BONUS = "보너스 당첨 번호를 $MIN_LOTTO_NUMBER ~ $MAX_LOTTO_NUMBER 사이에서 입력해 주세요."
 
     fun printInputMoney() {
         println(MESSAGE_INPUT_MONEY)
@@ -12,5 +16,9 @@ object Printer {
 
     fun printInputNumbers() {
         println(MESSAGE_INPUT_NUMBERS)
+    }
+
+    fun printInputBonus() {
+        println(MESSAGE_INPUT_BONUS)
     }
 }
