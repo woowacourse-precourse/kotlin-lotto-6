1607a21fc21843f29376099978988619f92d6f43
Jbrochan <hyeongchanj@gmail.com>
2023-11-06 17:58:25 +0900

    feat: 구입 금액 단위 예외 처리 기능 구현
    - 구입 금액 1,000원 단위 미입력 예외 처리 기능 구현
    - 구입 금액 입력 부분 주석 작성

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -3,12 +3,25 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class InputManager {
+    // 구입 금액 입력
     private fun inputMoney(): String {
         println("구입금액을 입력해 주세요.")
         return Console.readLine()
     }
 
+    // 구입 금액 처리 및 반환
     fun getMoney(): Int {
-        return inputMoney().toInt()
+        val inputMoney = inputMoney().toInt()
+
+        handleMoneyUnitException(inputMoney)
+
+        return inputMoney
+    }
+
+    // 1,000원 단위 입력 예외 처리
+    private fun handleMoneyUnitException(inputMoney: Int) {
+        if (inputMoney % 1000 != 0) {
+            throw IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.")
+        }
     }
 }
