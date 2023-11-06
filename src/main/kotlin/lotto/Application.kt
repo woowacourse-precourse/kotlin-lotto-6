eb993f1d2ce507500b8e9f361616a7d13202e13e
stopkite <stopkite98@gmail.com>
2023-11-06 18:59:24 +0900

    feat(Application): 로또 머신 클래스 생성 및 함수 호출

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,9 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    with(LottoMachine()) {
+        generateLottoTickets()
+        generateWinningNumbers()
+        showStaticsResult()
+    }
 }
