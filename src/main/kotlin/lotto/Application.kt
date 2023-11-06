78831711cb51bda1f0dad0b43f75eb4b44753674
eunji <nagosoo@kakao.com>
2023-11-06 22:48:34 +0900

    feat: 당첨 번호를 입력 받는다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,17 +7,19 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     var numberOfLottoTickets = 0
     val lottos = mutableListOf<Lotto>()
-    println("구입금액을 입력해 주세요.")
     validateAndThrowExceptionIfError { numberOfLottoTickets = getLottoPurchaseAmount() }
     println("${numberOfLottoTickets}개를 구매했습니다.")
-    lottos.addAll(getLottoWinningNumbers(lottos, numberOfLottoTickets))
+    getLottoWinningNumbers(lottos, numberOfLottoTickets)
     lottos.forEach {
         it.printWinningNumbers()
     }
+    validateAndThrowExceptionIfError { getUserLottoNumbers() }
 }
 
 fun getLottoPurchaseAmount(): Int {
+    println("구입금액을 입력해 주세요.")
     val lottoPurchaseAmount = Console.readLine()
+    println()
     val numberOfLottoTickets = lottoPurchaseAmount.toInt()
     if (numberOfLottoTickets % 1000 == 0) {
         return numberOfLottoTickets / 1000
@@ -29,7 +31,7 @@ fun getLottoPurchaseAmount(): Int {
 fun getLottoWinningNumbers(
     lottos: MutableList<Lotto>,
     numberOfLottoTickets: Int
-): MutableList<Lotto> {
+) {
     repeat(numberOfLottoTickets) {
         val numbers = Randoms.pickUniqueNumbersInRange(
             minLottoWinningNumber,
@@ -40,5 +42,18 @@ fun getLottoWinningNumbers(
             lottos.add(this)
         }
     }
-    return lottos
 }
+
+fun getUserLottoNumbers(): List<Int> {
+    println("당첨 번호를 입력해 주세요.")
+    val userInput = Console.readLine()
+    val userLottoNumbers = userInput.split(",").map {
+        it.toInt()
+    }
+    if (userLottoNumbers.size == 6) {
+        return userLottoNumbers
+    } else {
+        throw IllegalArgumentException("$errorPrefix 당첨 번호는 1~45 사이의 중복되지 않는 숫자를 , 로 구분하여 입력해야 합니다.")
+    }
+}
+
