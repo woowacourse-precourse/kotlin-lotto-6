962260b34f6520976515394d89f5a1e5396d3e92
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-07 01:39:31 +0900

    [Feat] 예외 처리 : 값이 1~45 범위가 아닐 경우(보너스 번호)

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -17,12 +17,18 @@ class LottoMachine {
 
     fun inputBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
-        return Console.readLine().toInt()
+        val bonusNumber = Console.readLine().toInt()
+        validateBonusNumber(bonusNumber)
+        return bonusNumber
     }
 
     private fun validateWinningNumbers(winningNumbers: List<Int>) {
         require(winningNumbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER }) { throw IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.") }
         require(winningNumbers.size == CNT_LOTTO_NUMBER) { throw IllegalArgumentException("[ERROR] 당첨 번호 6개를 입력해야 합니다.") }
         require(winningNumbers.toSet().size == CNT_LOTTO_NUMBER) { throw IllegalArgumentException("[ERROR] 중복된 번호가 존재합니다.") }
+    }
+
+    private fun validateBonusNumber(bonusNumber: Int) {
+        require(bonusNumber in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) { throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.") }
     }
 }
