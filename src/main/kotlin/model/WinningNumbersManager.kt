4cb6b84daf517e0637234473f7c2bb4458b5a8f1
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 14:33:34 +0900

    feat: 당첨번호 입력시 올바르지 못한 숫자 형식을 가지고 있을 겨웅 예외 발생 기능 작성

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -8,6 +8,7 @@ class WinningNumbersManager(private val numbers: List<String>) {
     private lateinit var winningNumbers: List<Int>
     init {
         numbers.forEach { InputValidation.ONLY_NUMBER.isValid(it) }
+        numbers.forEach { InputValidation.NUMBER_FORMAT.isValid(it) }
         winningNumbers = numbers.map { it.toInt() }
         LottoValidation.UNIQUE_LOTTO_NUMBERS.isValid(winningNumbers)
         LottoValidation.LOTTO_NUMBER_RANGE.isValid(winningNumbers)
