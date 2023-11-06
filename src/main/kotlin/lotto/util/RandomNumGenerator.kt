5777858234aa5aa1f33c00289e270a9bc4f4cc05
seoyeon <seoyeonn051@gmail.com>
2023-11-06 10:09:13 +0900

    feat: RandomNumGenerator.kt 생성

---
+++ src/main/kotlin/lotto/util/RandomNumGenerator.kt
@@ -0,0 +1,7 @@
+package lotto.util
+
+import camp.nextstep.edu.missionutils.Randoms
+
+fun randomNumberGenerator(startNum: Int, lastNum: Int): Int {
+    return Randoms.pickNumberInRange(startNum, lastNum)
+}
