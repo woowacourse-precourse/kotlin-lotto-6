2d3f522e9d26fb21b293ac58205caeedd7d36f9f
DJ_Lumiere <djminstar573@gmail.com>
2023-11-06 14:08:49 +0900

    Update Application.kt
    로또 숫자 고르는 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,12 @@
 package lotto
 
+fun pickLottoNumbers(): Lotto {
+    val numbers = pickUniqueNumbersInRange(
+        LottoConstraints.NUMBER_START, LottoConstraints.NUMBER_END, LottoConstraints.NUMBER_COUNT
+    )
+    return Lotto(numbers)
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
