05928c46a3aa96db541140c7fd449584960f68be
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 02:44:15 +0900

    refactor: 출력시 배열의 순서를 거꾸로 넣은것 수정

--- src/main/kotlin/ui/Output.kt
+++ src/main/kotlin/ui/Output.kt
@@ -4,14 +4,14 @@ import exception.Message
 import exception.Message.Companion.MESSAGE_PRINT_TICKET_AMOUNT
 import lotto.Lottos
 
-object Output  {
+object Output {
     fun printLottoCount(count: Int) {
-        println("${count}"+ MESSAGE_PRINT_TICKET_AMOUNT)
+        println("${count}" + MESSAGE_PRINT_TICKET_AMOUNT)
     }
 
     fun printUserLottos(userLottos: Lottos) {
         userLottos.getUserLotto().forEach {
-            println(it.getNumbers())
+            println(it.getNumbers().sorted())
         }
     }
 
@@ -22,14 +22,16 @@ object Output  {
     fun printInputBonusNumber() {
         println(Message.MESSAGE_INPUT_BONUS_NUMBER)
     }
+
     fun printMatchResult(matchResult: MutableList<Int>) {
         println(Message.MESSAGE_WINNING_STATISTICS)
-        println(Message.MESSAGE_PRINT_FIFTH + " - " + matchResult[0] + Message.MESSAGE_PRINT_AMOUNT)
-        println(Message.MESSAGE_PRINT_FOURTH + " - " + matchResult[1] + Message.MESSAGE_PRINT_AMOUNT)
+        println(Message.MESSAGE_PRINT_FIFTH + " - " + matchResult[4] + Message.MESSAGE_PRINT_AMOUNT)
+        println(Message.MESSAGE_PRINT_FOURTH + " - " + matchResult[3] + Message.MESSAGE_PRINT_AMOUNT)
         println(Message.MESSAGE_PRINT_THIRD + " - " + matchResult[2] + Message.MESSAGE_PRINT_AMOUNT)
-        println(Message.MESSAGE_PRINT_SECOND + " - " + matchResult[3] + Message.MESSAGE_PRINT_AMOUNT)
-        println(Message.MESSAGE_PRINT_FIRST + " - " + matchResult[4] + Message.MESSAGE_PRINT_AMOUNT)
+        println(Message.MESSAGE_PRINT_SECOND + " - " + matchResult[1] + Message.MESSAGE_PRINT_AMOUNT)
+        println(Message.MESSAGE_PRINT_FIRST + " - " + matchResult[0] + Message.MESSAGE_PRINT_AMOUNT)
     }
+
     fun printEarnRate(earnRate: Double) {
         println(Message.MESSAGE_PRINT_EARN + earnRate + Message.MESSAGE_PRINT_EARN_RATE)
     }
