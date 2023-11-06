b755fb162d7feb6bd7ccf1164296920f978b8606
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 06:59:12 +0900

    refactor: UserLottoTest 수정

---
+++ src/test/kotlin/lotto/domain/UserLottoTest.kt
@@ -0,0 +1,46 @@
+package lotto.domain
+
+
+import lotto.UserLotto
+import org.junit.jupiter.api.Test
+import lotto.checkValidUserLotto
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
+
+
+class UserLottoTest {
+    @Test
+    fun `유저 로또 생성시 예외처리 테스트(미달)`() {
+        val invalidInput = mutableListOf(1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> {
+            checkValidUserLotto(invalidInput)
+        }
+    }
+
+    @Test
+    fun `유저 로또 생성시 예외처리 테스트(초과)`() {
+        val invalidInput = mutableListOf(1, 2, 3, 4, 5, 6, 7)
+        assertThrows<IllegalArgumentException> {
+            checkValidUserLotto(invalidInput)
+        }
+    }
+
+    @Test
+    fun `유저 로또 생성시 예외처리 테스트(중복)`() {
+        val invalidInput = mutableListOf(1, 2, 3, 4, 5, 5)
+        assertThrows<IllegalArgumentException> {
+            checkValidUserLotto(invalidInput)
+        }
+    }
+
+    @Test
+    fun `유저 로또 생성시 예외처리 테스트(범위)`() {
+        val invalidInput = mutableListOf(1, 2, 3, 4, 5, 46)
+        assertThrows<IllegalArgumentException> {
+            checkValidUserLotto(invalidInput)
+        }
+    }
+
+
+}
+
