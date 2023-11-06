9970dab42c919f2349c5fccbebc1abbc4432691d
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-07 01:13:03 +0900

    [Feat] 예외 처리 : 중복된 값이 있을 경우

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,6 +2,7 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 
+private const val CNT_LOTTO_NUMBER = 6
 private const val MIN_LOTTO_NUMBER = 1
 private const val MAX_LOTTO_NUMBER = 45
 
@@ -16,5 +17,6 @@ class LottoMachine {
 
     private fun validateWinningNumbers(winningNumbers: List<Int>) {
         require(winningNumbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER }) { throw IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.") }
+        require(winningNumbers.toSet().size == CNT_LOTTO_NUMBER) { throw IllegalArgumentException("[ERROR] 중복된 번호가 존재합니다.") }
     }
 }
