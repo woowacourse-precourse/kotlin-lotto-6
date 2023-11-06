022b700e42f66c7cd5cd30182b9163792516271f
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-07 00:57:10 +0900

    feat: 로또 등수 반환 기능 작성

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -14,6 +14,16 @@ class Lotto(private val numbers: List<Int>) {
     fun getLottoNumberInfo() = numbers.sorted()
 
     fun getLottoRank(winningNumber: List<Int>, bonusNumber: Int): Int {
+        when (winningNumber.count { numbers.contains(it) }) {
+            3 -> return 5
+            4 -> return 4
+            5 -> {
+                if (numbers.contains(bonusNumber)) return 2
+                return 3
+            }
+
+            6 -> return 1
+        }
         return 0
     }
 }
