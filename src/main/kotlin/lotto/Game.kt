0981ef1bb824f6a56ce9da1519645990c946c70f
oh in seong <superohinsung@naver.com>
2023-11-06 13:00:00 +0900

    Revert "[feat] add Sorting Lotto Numbers"
    This reverts commit 936263b5012e39830fd2355f36282495cdfccbf3.

--- src/main/kotlin/lotto/Game.kt
+++ src/main/kotlin/lotto/Game.kt
@@ -20,16 +20,12 @@ class Game private constructor(
 
         val lottery = mutableListOf<Lotto>()
         repeat(purchaseLottoNumber) {
-            val numbers = sortLotteryRandomNumber(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
             lottery.add(Lotto(numbers))
         }
 
         printer.printLottoNumber(lottery)
         return lottery
-    }
-
-    fun sortLotteryRandomNumber(numbers: List<Int>): List<Int> {
-        return numbers.sorted()
     }
 
 }
