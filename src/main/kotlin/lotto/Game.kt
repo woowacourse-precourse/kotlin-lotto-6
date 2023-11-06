a6b5eecbb569926721b25ee632f53db7c2721e08
oh in seong <superohinsung@naver.com>
2023-11-06 14:16:21 +0900

    [feat] Add ability for the user to receive 6 winning numbers

--- src/main/kotlin/lotto/Game.kt
+++ src/main/kotlin/lotto/Game.kt
@@ -3,33 +3,46 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-class Game private constructor(
-    private val calculator: Calculator = Calculator(), private val printer: Printer = Printer()
-) {
+class Game {
 
-    fun purchaseLotto(): Int {
-        printer.printEnterPurchase()
-        val input = Console.readLine()
-        return calculator.calculateLottoAvailableForPurchase(input)
+    fun purchaseLotto(input: String): Int {
+        val calculator = Calculator()
+        requireIsInt(input)
+        return calculator.calculateLottoAvailableForPurchase(input.toInt())
     }
 
-    fun createLotteryRandomNumber(): List<Lotto> {
-        val purchaseLottoNumber = purchaseLotto()
-        print(purchaseLottoNumber)
-        printer.printBuyNPieces()
-
+    fun createLotteryRandomNumber(purchaseLottoNumber: Int): List<Lotto> {
         val lottery = mutableListOf<Lotto>()
         repeat(purchaseLottoNumber) {
             val numbers = sortLotteryRandomNumber(Randoms.pickUniqueNumbersInRange(1, 45, 6))
             lottery.add(Lotto(numbers))
         }
-
-        printer.printLottoNumber(lottery)
         return lottery
     }
 
     fun sortLotteryRandomNumber(numbers: List<Int>): List<Int> {
         return numbers.sorted()
+    }
+
+    fun inputUserPickNumbersS(input: String): Lotto {
+        val splitNumber = input.split(",")
+        val numbers = mutableListOf<Int>()
+        splitNumber.forEach {
+            requireIsInt(it)
+            numbers.add(it.toInt())
+        }
+
+        return Lotto(numbers)
+    }
+
+    fun inputBonusNumber(input: String): Int {
+        requireIsInt(input)
+        return input.toInt()
+    }
+
+    private fun requireIsInt(input: String) {
+        val number = input.toIntOrNull() ?: throw IllegalArgumentException()
+        require(number > 0)
     }
 
 }
