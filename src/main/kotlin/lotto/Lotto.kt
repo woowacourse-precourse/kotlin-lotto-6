1e233d141bda1c6cbdf054db3c323160f93cbcc2
LEE <tpqms775@naver.com>
2023-11-06 16:40:46 +0900

    feat : 당첨번호와 구매번호 비교하여 같은 수 있을 시 같은 개수 반환 compareNumber()

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,4 +6,13 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     // TODO: 추가 기능 구현
+
+    fun compareNumber(nums: List<Int>): Int {
+
+        val myNumbers = numbers.toSet()
+        val winNumbers = nums.toSet()
+
+        val intersection = myNumbers.intersect(winNumbers)
+        return intersection.size
+    }
 }
