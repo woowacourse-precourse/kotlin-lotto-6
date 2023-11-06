42d6b8ecc4291b0662b8c15e67c42e0cdca5b6fa
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 00:35:54 +0900

    feat: 입력받은 숫자 예외처리 테스트 작성

---
+++ src/test/kotlin/lotto/UserLottoTest.kt
@@ -0,0 +1,26 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class UserLottoTest {
+    @Test
+    fun `입력한 번호 예외처리(1~45의 숫자가 아닐경우)`() {
+        var userLotto = UserLotto()
+        var input = mutableListOf(1, 2, 3, 4, 5, 46)
+        assertThrows<IllegalArgumentException> { userLotto.checkValidUserLotto(input) }
+    }
+
+    @Test
+    fun `입력한 번호 예외처리(중복되는 숫자가 있을경우)`() {
+        var userLotto = UserLotto()
+        var input = mutableListOf(1, 2, 3, 4, 5, 5)
+        assertThrows<IllegalArgumentException> { userLotto.checkValidUserLotto(input) }
+    }
+    @Test
+    fun `입력한 번호 예외처리(6개의 숫자가 아닐경우)`() {
+        var userLotto = UserLotto()
+        var input = mutableListOf(1, 2, 3, 4, 5)
+        assertThrows<IllegalArgumentException> { userLotto.checkValidUserLotto(input) }
+    }
+}
