272e7f599c77690332fdcca9bb31a4e744c8a0fe
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 14:40:22 +0900

    feat: 당첨 번호가 6개가 아닐 경우 예외 발생 기능 작성

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -10,6 +10,7 @@ class WinningNumbersManager(private val numbers: List<String>) {
         numbers.forEach { InputValidation.ONLY_NUMBER.isValid(it) }
         numbers.forEach { InputValidation.NUMBER_FORMAT.isValid(it) }
         winningNumbers = numbers.map { it.toInt() }
+        LottoValidation.REQUIRED_LOTTO_NUMBER_COUNT.isValid(winningNumbers)
         LottoValidation.UNIQUE_LOTTO_NUMBERS.isValid(winningNumbers)
         LottoValidation.LOTTO_NUMBER_RANGE.isValid(winningNumbers)
     }
