5c2c0d8ec20bc436bc75f608e6e7771d67e1f5b5
oh in seong <superohinsung@naver.com>
2023-11-06 17:11:37 +0900

    [feat] Add guidance output function

--- src/main/kotlin/lotto/Printer.kt
+++ src/main/kotlin/lotto/Printer.kt
@@ -2,11 +2,17 @@ package lotto
 
 class Printer {
 
-    fun printEnterPurchase() = println(Message.ENTER_PURCHASE)
+    fun printEnterPurchaseAnnouncement() = println(Message.ENTER_PURCHASE)
 
-    fun printBuyNPieces() = println(Message.BOUGHT_N_PIECES)
+    fun printBuyNPiecesAnnouncement() = println(Message.BOUGHT_N_PIECES)
 
-    fun printEnterLottoNumber() = println(Message.ENTER_USER_PICK_NUMBERS)
+    fun printEnterUserPickNumbersAnnouncement() = println(Message.ENTER_USER_PICK_NUMBERS)
+
+    fun printEnterBonusNumberAnnouncement() = println(Message.ENTER_BONUS_NUMBER)
+
+    fun printWinningStatisticsAnnouncement() = println(Message.WINNING_STATISTICS)
+
+    fun printCommaAnnouncement() = println(Message.COMMA)
 
     fun printLottoNumber(lottery: List<Lotto>) {
         repeat(lottery.size) {
