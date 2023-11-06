3d0fcbb727a6488ea49cef938105fcfedb579ab2
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 02:38:41 +0900

    refactor: 예외처리시 문구 출력되게 수정

--- src/main/kotlin/lotto/UserLotto.kt
+++ src/main/kotlin/lotto/UserLotto.kt
@@ -16,12 +16,15 @@ class UserLotto {
 
     fun checkValidUserLotto(userLotto: MutableList<Int>) {
         if (userLotto.size != 6) {
+            println(Exception.EXCEPTION_INVALID_SIZE)
             throw IllegalArgumentException(Exception.EXCEPTION_INVALID_SIZE)
         }
         if (userLotto.any { it !in 1..45 }) {
+            println(Exception.EXCEPTION_INVALID_NUMBER)
             throw IllegalArgumentException(Exception.EXCEPTION_INVALID_NUMBER)
         }
         if (userLotto.distinct().size != 6) {
+            println(Exception.EXCEPTION_DUPLICATE_NUMBER)
             throw IllegalArgumentException(Exception.EXCEPTION_DUPLICATE_NUMBER)
         }
     }
