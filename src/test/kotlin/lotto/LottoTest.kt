54af09e59e8786ae35920ee743fd54fd052bc6a2
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-07 01:09:57 +0900

    테스트 코드 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,8 +1,8 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
 
 class LottoTest {
     @Test
@@ -12,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -20,5 +19,20 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `일치하는 번호의 개수와 보너스 번호 여부에 따른 결과를 올바르게 반환해야 한다`() {
+        val resultWithBonus = LottoResult.fromMatchCount(5, true)
+        val resultWithoutBonus = LottoResult.fromMatchCount(5, false)
+
+        assertThat(resultWithBonus).isEqualTo(LottoResult.FIVE_MATCHES_AND_BONUS)
+        assertThat(resultWithoutBonus).isEqualTo(LottoResult.FIVE_MATCHES)
+    }
+
+    @Test
+    fun `상금을 올바르게 계산해야 한다`() {
+        val result = LottoResult.FIVE_MATCHES
+        val expectedPrize = 1500000
+
+        assertThat(result.prize).isEqualTo(expectedPrize)
+    }
 }
