75f4dc888db006b46c489f285a662be6ed9b185a
oh in seong <superohinsung@naver.com>
2023-11-06 17:11:01 +0900

    [feat] Add Fuction to calculate returns

--- src/main/kotlin/lotto/Calculator.kt
+++ src/main/kotlin/lotto/Calculator.kt
@@ -6,4 +6,12 @@ class Calculator {
         return (number / 1000)
     }
 
+    fun calculateYieldResult(purchaseCost: Int, types: List<LottoWinType>): String {
+        val profits = types.sumOf { it.prize }
+        val yield = (profits.toDouble() / purchaseCost * 100).takeIf { it.isFinite() } ?: 0.0
+        val formattedYield = String.format("%.2f", yield)
+
+        return "${formattedYield}%"
+    }
+
 }
