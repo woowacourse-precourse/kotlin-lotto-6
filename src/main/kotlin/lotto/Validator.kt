6d246d2b07797d82b2603c0bd1a26711e2c9a408
Junyoung-WON <wjyo60@gmail.com>
2023-11-06 16:26:41 +0900

    FEAT: 입력받은 로또 구매 금액이 1,000원으로 나누어 떨어지는지 검사
    구매 금액을 1,000으로 나눈 나머지가 0인지 확인하여 나누어 떨어지면 true, 그렇지 않으면 false를 반환한다.

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -3,8 +3,7 @@ package lotto
 class Validator {
     fun validatePurchaseMoney(purchaseMoney: String): Boolean {
         return try {
-            purchaseMoney.toInt()
-            true
+            isNumber(purchaseMoney) && isDividedUpThousand(purchaseMoney.toInt())
         } catch (e: IllegalArgumentException) {
             println("[ERROR] 구입 금액을 올바르게 입력해주세요.")
             false
@@ -13,5 +12,14 @@ class Validator {
 
     fun isNumber(input: String): Boolean{
         return input.toIntOrNull() != null
+    }
+
+    fun isDividedUpThousand(money: Int): Boolean{
+        return if (money%1000 == 0) {
+            true
+        } else{
+            println("[ERROR] 구입 금액이 1000원으로 나누어 떨어지지 않습니다.")
+            false
+        }
     }
 }
