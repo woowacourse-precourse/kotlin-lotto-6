cc27ae019a7c86141c9d44770f5ef6a178b45a47
bak <bak4017@gmail.com>
2023-11-06 14:49:23 +0900

    refactor(lotto): issueTickets 함수 삭제

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,12 +1,6 @@
 package lotto.domain
 
 class LottoMachine {
-    // 로또 티켓 발행과 당첨 번호 추첨, 보너스 번호 추첨의 기능을 가진 클래스
-
-    fun issueTickets(numberOfTickets: Int) {
-
-    }
-
     fun drawWinNumbers(): Set<Int> {
         return setOf()
     }
