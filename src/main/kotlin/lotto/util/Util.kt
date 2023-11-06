641b4b5fc1f1622246e78d33e324aabe5aa17e3a
unso99 <unso99@naver.com>
2023-11-06 20:25:59 +0900

    :sparkles: Util 파일 추가 및 기능 추가

---
+++ src/main/kotlin/lotto/util/Util.kt
@@ -0,0 +1,52 @@
+package lotto.util
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.view.Message
+import kotlin.math.roundToInt
+
+object Util {
+
+    fun requestMoney(): Int {
+
+        val purchasePrice = Console.readLine().toIntOrNull()
+
+        //todo check하는 기능 추가
+
+        return purchasePrice!!
+
+    }
+
+    fun requestWinningNumbers(): MutableList<Int?> {
+        println(Message.REQUEST_WINNING_NUMBERS.message)
+
+        val input = Console.readLine()
+
+        val winningNumbersList = input.replace(" ", "").split(",").map { it.toIntOrNull() }.toMutableList()
+
+        //todo check하는 기능 추가
+
+        return winningNumbersList
+    }
+
+    fun requestBonusNumber(): Int {
+        println(Message.REQUEST_BONUS_NUMBER.message)
+
+        val bonusNumber = Console.readLine().toIntOrNull()
+
+        //todo check하는 기능 추가
+
+        return bonusNumber!!
+    }
+
+    fun getLottoTicket(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+
+
+    fun getRoi(investment: Int, prizeMoney: Int): Double {
+        val roi = (prizeMoney.toDouble() / investment) * 100
+
+        return (roi * 10.0).roundToInt() / 10.0
+
+    }
+
+}
