3b76e7abfa28824b8f14b6a05f89c43f46b30985
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 13:20:06 +0900

    refactor: Money 객체 생성 시 유효성 검사 로직 변경

--- src/main/kotlin/model/Money.kt
+++ src/main/kotlin/model/Money.kt
@@ -1,21 +1,15 @@
 package model
 
+import util.InputValidation
+
 class Money(private val price: String) {
     init {
-        require(price.all { it.isDigit() }) { Error.INPUT_ONLY_NUMBER.message }
-        require(price.isNotBlank()) { Error.PURCHASE_AMOUNT_EMPTY.message }
-        require(price.first() != '0') { Error.NUMBER_FORMAT.message }
-        require(price.toInt() in 1_000..100_000) { Error.AMOUNT_OVER_MAX_LIMIT.message }
-        require(price.toInt() % 1_000 == 0) { Error.PURCHASE_IN_THOUSAND_WON.message }
+        InputValidation.ONLY_NUMBER.isValid(price)
+        InputValidation.PURCHASE_AMOUNT_EMPTY.isValid(price)
+        InputValidation.NUMBER_FORMAT.isValid(price)
+        InputValidation.VALID_RANGE.isValid(price)
+        InputValidation.PURCHASE_IN_THOUSAND_WON.isValid(price)
     }
 
     fun getPurchasableLottoTicketCount() = price.toInt() / 1000
-
-    private enum class Error(val message: String) {
-        INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
-        PURCHASE_AMOUNT_EMPTY("[ERROR] 구매 금액이 입력되지 않았습니다."),
-        NUMBER_FORMAT("[ERROR] 올바른 숫자 형태가 아닙니다."),
-        AMOUNT_OVER_MAX_LIMIT("[ERROR] 최소 1,000원 부터 최대 100,000원 까지 구매가 가능합니다."),
-        PURCHASE_IN_THOUSAND_WON("[ERROR] 1,000원 단위로 구매가 가능합니다."),
-    }
 }
