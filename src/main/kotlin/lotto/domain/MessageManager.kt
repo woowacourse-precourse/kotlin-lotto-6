bcbdac0b1bab55d8f5d0a2cd578e327419d32ee0
jinuemong <jinwoo3661@naver.com>
2023-11-06 12:07:32 +0900

    File: 파일명 manager -> domain으로 변경, 파일 이동

--- src/main/kotlin/lotto/domain/MessageManager.kt
+++ src/main/kotlin/lotto/domain/MessageManager.kt
@@ -1,4 +1,4 @@
-package lotto.manager
+package lotto.domain
 
 class MessageManager {
 
--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,8 @@
 package lotto.model
 
+// 사용자 구매 로또
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
-
-    // TODO: 추가 기능 구현
 }
