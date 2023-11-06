6f6be4d9e5eab3352240d59b62fcfc8d2ae3d64e
jiwon2724 <stopone3119@naver.com>
2023-11-06 21:37:05 +0900

    [test] 당첨 번호에 보너스 번호가 포함되어 있는지 확인하는 테스트 코드 작성

--- src/test/kotlin/lotto/LottoPrizeCheckTest.kt
+++ src/test/kotlin/lotto/LottoPrizeCheckTest.kt
@@ -12,9 +12,14 @@ class LottoPrizeCheckTest {
         assertEquals(LottoPrizeCheck(JACKPOT_NUMBERS).checkPrize(lotto), FOURTH_PLACE_INDEX)
     }
 
+    @Test
+    fun `로또 번호에 보너스 번호가 있는지 확인`() {
+        assertTrue(LOTTO_NUMBERS.contains(BONUS_NUMBER))
+    }
+
     companion object {
-        private val JACKPOT_NUMBERS = listOf(1, 2, 3, 4, 5, 6)
-        private val LOTTO_NUMBERS = listOf(3, 4, 5, 21, 22, 23)
+        private val JACKPOT_NUMBERS = listOf(1, 2, 3, 4, 7, 44)
+        private val LOTTO_NUMBERS = listOf(3, 4, 5, 21, 22, 7)
         private const val BONUS_NUMBER = 7
         private const val FOURTH_PLACE_INDEX = 0
     }
