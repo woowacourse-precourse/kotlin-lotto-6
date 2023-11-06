52f945f6cfd632a8dd5d49cc8cec1abc7c1a8bab
junyong <junyong008@gmail.com>
2023-11-06 16:43:16 +0900

    feat(InputView): 보너스 번호 입력 기능 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,6 +6,7 @@ object InputView {
 
     private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요."
+    private const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
 
     fun inputPurchaseAmount(): String {
         println(INPUT_PURCHASE_AMOUNT)
@@ -15,5 +16,10 @@ object InputView {
     fun inputWinningLotto(): List<String> {
         println(System.lineSeparator() + INPUT_WINNING_LOTTO)
         return Console.readLine().split(",")
+    }
+
+    fun inputBonusNumber(): String {
+        println(INPUT_BONUS_NUMBER)
+        return Console.readLine()
     }
 }
