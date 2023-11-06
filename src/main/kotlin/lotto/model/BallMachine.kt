99868420650939bd1e7e54f604f206d788a95f9c
unso99 <unso99@naver.com>
2023-11-06 20:12:29 +0900

    :sparkles: BallMachine 파일 추가 및 기능 추가

---
+++ src/main/kotlin/lotto/model/BallMachine.kt
@@ -0,0 +1,10 @@
+package lotto.model
+
+class BallMachine() {
+
+    private var _winningNumber = mutableListOf<Int>()
+    var winningNumber = _winningNumber
+
+    private var _bonusNumber = 0
+    var bonusNumber = _bonusNumber
+}
