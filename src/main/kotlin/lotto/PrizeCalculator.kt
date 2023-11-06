d7bcf4ba60c81b488a69a38c61e2507c682617ef
aprilgom <aprilgom@users.noreply.github.com>
2023-11-07 02:34:07 +0900

    feat: 로또 등수별 당첨금 분류

--- src/main/kotlin/lotto/PrizeCalculator.kt
+++ src/main/kotlin/lotto/PrizeCalculator.kt
@@ -1,6 +1,9 @@
 package lotto
 
 class PrizeCalculator(private val lottoList: List<Lotto>, private val lottoChecker: LottoChecker) {
+    fun getWinningCountMap() = lottoList.groupingBy {
+        lottoChecker.checkLotto(it)
+    }.eachCount()
     fun getSumOfWinningPrize() = lottoList.sumOf {
         val position = lottoChecker.checkLotto(it)
         position.winningPrize
