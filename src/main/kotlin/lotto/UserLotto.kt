5cd36f0e309c8a341ef25bfe90e69cec63afa91c
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-06 23:27:59 +0900

    feat: 당첨 번호를 입력받고 번호의 유효성을 검사하는 기능 추가

--- src/main/kotlin/lotto/UserLotto.kt
+++ src/main/kotlin/lotto/UserLotto.kt
@@ -1,4 +1,27 @@
 package lotto
 
+import ui.Input
+import exception.Exception
+
 class UserLotto {
+    init {
+        Input.getInputWinningNumber()
+    }
+
+    fun createUserLotto(): Lotto {
+        val userLotto = Input.getInputWinningNumber()
+        return Lotto(userLotto.sorted())
+    }
+
+    fun checkValidUserLotto(userLotto: MutableList<Int>) {
+        if (userLotto.size != 6) {
+            throw IllegalArgumentException(Exception.EXCEPTION_INVALID_SIZE)
+        }
+        if (userLotto.any { it !in 1..45 }) {
+            throw IllegalArgumentException(Exception.EXCEPTION_INVALID_NUMBER)
+        }
+        if (userLotto.distinct().size != 6) {
+            throw IllegalArgumentException(Exception.EXCEPTION_DUPLICATE_NUMBER)
+        }
+    }
 }
