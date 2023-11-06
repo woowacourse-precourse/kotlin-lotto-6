75ff07d34e3cb22caac4d6758f3ec479818c22a2
hongjeZZ <ajyong316@gmail.com>
2023-11-06 22:20:38 +0900

    chore: LottoMachine 클래스 테스트 코드 작성

---
+++ src/test/kotlin/lotto/domain/LottoMachineTest.kt
@@ -0,0 +1,38 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest {
+    private val lottoMachine = LottoMachine()
+
+    @Test
+    fun `구입금액에 따라 로또를 생성하는 기능 테스트 1`() {
+        val inputMoney = 5000
+        val lottos = lottoMachine.issueLottos(inputMoney)
+        val expectedLottosSize = 5
+        assertThat(expectedLottosSize).isEqualTo(lottos.size)
+    }
+
+    @Test
+    fun `구입금액에 따라 로또를 생성하는 기능 테스트 2`() {
+        val inputMoney = 9595000
+        val lottos = lottoMachine.issueLottos(inputMoney)
+        val expectedLottosSize = 9595
+        assertThat(expectedLottosSize).isEqualTo(lottos.size)
+    }
+
+    @Test
+    fun `pickUniqueNumbersInRange 메서드 테스트`() {
+        repeat(10000) {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            val numbersSet = numbers.toSet()
+            assertThat(numbers).hasSize(6)
+            assertThat(numbers.size).isEqualTo(numbersSet.size)
+            assertThat(numbers).allMatch { number ->
+                number in 1..45
+            }
+        }
+    }
+}
