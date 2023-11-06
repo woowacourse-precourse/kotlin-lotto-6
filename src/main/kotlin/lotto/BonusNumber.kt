55be22dd02529e42c2914dd4403491c56da67f5a
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 02:17:52 +0900

    fix: createBonusNumber함수 파라미터 추가

--- src/main/kotlin/lotto/BonusNumber.kt
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -4,21 +4,26 @@ import camp.nextstep.edu.missionutils.Console
 import exception.Exception
 import exception.Message
 import ui.Input
+import ui.Output
 
 class BonusNumber {
     init {
-        Message.MESSAGE_INPUT_BONUS_NUMBER
+        Output.printInputBonusNumber()
     }
 
-    fun createBonusNumber(): Int {
+    fun createBonusNumber(userLotto: Lotto): Int {
         val bonusNumber = Input.inputBonusNumber()
         checkValidBonusNumber(bonusNumber)
-        return bonusNumber
+        return bonusNumber.toInt()
     }
 
-    fun checkValidBonusNumber(bonusNumber: Int) {
-        if (bonusNumber !in 1..45) {
+    fun checkValidBonusNumber(bonusNumber: String) {
+        if (bonusNumber.toInt() !in 1..45) {
             throw IllegalArgumentException(Exception.EXCEPTION_INVALID_BONUS_NUMBER)
         }
+        if (!bonusNumber.all { Character.isDigit(it) }) {
+            throw IllegalArgumentException(Exception.EXCEPTION_INVALID_NUMBER)
+        }
     }
+
 }
