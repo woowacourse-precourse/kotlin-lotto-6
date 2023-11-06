80898d4a938b3e75f548a71a848999af31595052
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 05:22:25 +0900

    Feat: 로또 구입 금액 입력 유효성 검사(1000원 단위로 나누어 떨어지지 않을 때)

--- src/main/kotlin/lotto/Amount.kt
+++ src/main/kotlin/lotto/Amount.kt
@@ -5,6 +5,7 @@ class Amount(private val amount: String) {
     init {
         require(isNumber()) { throw IllegalArgumentException("$ERROR $INPUT_ONLY_NUMBER") }
         require(isBelowZero()) {throw IllegalArgumentException("$ERROR $INPUT_ONLY_MORE_THAN_ZERO")}
+        require(isDivideThousandUnit()) { throw IllegalArgumentException("$ERROR $INPUT_ONLY_UNIT_OF_THOUSAND") }
     }
 
     private fun isNumber(): Boolean {
@@ -16,10 +17,16 @@ class Amount(private val amount: String) {
         return value != null && value > 0
     }
 
+    private fun isDivideThousandUnit(): Boolean {
+        val value = amount.toInt() % 1000
+        return value == 0
+    }
+
     companion object {
         const val ERROR = "[ERROR]"
         const val INPUT_ONLY_NUMBER = "숫자만 입력하세요."
         const val INPUT_ONLY_MORE_THAN_ZERO = "구입 금액으로 0 이하의 숫자는 입력할 수 없습니다."
+        const val INPUT_ONLY_UNIT_OF_THOUSAND = "구입 금액은 1000원 단위로 나누어 떨어지는 수여야 합니다."
     }
 
 }
