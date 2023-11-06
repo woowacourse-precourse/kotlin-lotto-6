d9db5a29887d23c2041998928941b3ea8d8a3c17
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-06 22:50:07 +0900

    test: 구매한 로또를 출력하는지 테스트

--- src/test/kotlin/lotto/BuyTest.kt
+++ src/test/kotlin/lotto/BuyTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import ui.Output
 
 class BuyTest {
     @Test
@@ -15,5 +16,25 @@ class BuyTest {
         assertThrows<IllegalArgumentException> {
             Buy().validateMoney("abc")
         }
+    }
+
+    @Test
+    fun `구매한 로또를 출력하는지 확인`() {
+        val buy = Buy()
+        val lotto1 = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val lotto2 = Lotto(listOf(11, 12, 13, 14, 15, 16))
+        val lotto3 = Lotto(listOf(21, 22, 23, 24, 25, 26))
+        val lotto4 = Lotto(listOf(31, 32, 33, 34, 35, 36))
+        val lotto5 = Lotto(listOf(41, 42, 43, 44, 45, 40))
+
+
+        val lottos = UserLotto()
+        lottos.addUserLotto(lotto1)
+        lottos.addUserLotto(lotto2)
+        lottos.addUserLotto(lotto3)
+        lottos.addUserLotto(lotto4)
+        lottos.addUserLotto(lotto5)
+
+        Output.printUserLottos(lottos)
     }
 }
