6b8468dd7115e99cc81af7894b466309d01cc2a0
3uomlkh <cw1205@duksung.ac.kr>
2023-11-06 16:36:08 +0900

    feat: 로또 구매 금액 입력 시 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,10 +3,19 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
+    try {
+        printStartMessage()
+        var input = inputMessage().trim()
+        purchaseAmountEmpty(input)
+        println(input)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+        main()
+    }
 
 }
 
-fun inputMessage(): String? {
+fun inputMessage(): String {
     return Console.readLine()
 }
 
@@ -29,4 +38,21 @@ fun printBonusMessage() {
 fun printWinningReport() {
     println("당첨 통계")
     println("---")
+}
+
+// Validator
+// 로또 구매 금액이 정수가 아닌 경우
+fun purchaseAmount(num: String): Int{
+    require (num.all { it.isDigit() }) {"[ERROR] 로또 금액은 정수로 입력해주세요."}
+    return num.toInt()
+}
+// 로또 구매 금액이 공백인 경우
+fun purchaseAmountEmpty(num: String): Int {
+    require (num.isNotEmpty() && num.isNotBlank()) {"[ERROR] 공백은 포함할 수 없습니다."}
+    return num.toInt()
+}
+
+fun purchaseAmountEmpty(num: Int): Int {
+    require (num % 1000 != 0) {"[ERROR] 1,000원 단위로 입력해주세요."}
+    return num
 }
