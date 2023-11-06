4607887cbadca2469fc2fe1fa0de591973793b5d
junyong <junyong008@gmail.com>
2023-11-06 14:47:30 +0900

    feat(InputView): 당첨 번호 입력 기능 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
 
     private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+    private const val INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요."
 
     fun inputPurchaseAmount(): String {
         println(INPUT_PURCHASE_AMOUNT)
         return Console.readLine()
+    }
+
+    fun inputWinningLotto(): List<String> {
+        println(System.lineSeparator() + INPUT_WINNING_LOTTO)
+        return Console.readLine().split(",")
     }
 }
