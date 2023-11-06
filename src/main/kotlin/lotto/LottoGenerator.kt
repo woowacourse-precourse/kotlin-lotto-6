c5c5fe2d9ccd8c9a7dbe0d193eee5eba1b191c9f
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-06 22:13:41 +0900

    [Refactor] 로또 랜덤 생성 조건을 상수로 정의

--- src/main/kotlin/lotto/LottoGenerator.kt
+++ src/main/kotlin/lotto/LottoGenerator.kt
@@ -2,10 +2,14 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
+private const val CNT_LOTTO_NUMBER = 6
+private const val MIN_LOTTO_NUMBER = 1
+private const val MAX_LOTTO_NUMBER = 45
+
 class LottoGenerator(private val lottoGenerateCount: Int) {
 
     private fun generateRandomLotto(): List<Int> {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, CNT_LOTTO_NUMBER).sorted()
     }
 
     fun printRandomLotto() {
