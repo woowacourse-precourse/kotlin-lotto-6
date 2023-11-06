f411fddb6628c463340d0a60dec9fd1e1b8e2847
DoDoBest <send1684@gmail.com>
2023-11-07 00:31:22 +0900

    refactor: Lotto 클래스에 에러 메시지 추가

--- src/main/kotlin/lotto/data/Lotto.kt
+++ src/main/kotlin/lotto/data/Lotto.kt
@@ -2,9 +2,15 @@ package lotto.data
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == LENGTH_OF_NUM)
-        require(numbers.toSet().size == LENGTH_OF_NUM)
-        require(numbers.sorted() == numbers)
+        require(numbers.size == LENGTH_OF_NUM) {
+            LENGTH_IS_NOT_CORRECT
+        }
+        require(numbers.toSet().size == LENGTH_OF_NUM) {
+            DUPLICATE_IS_NOT_ALLOWED
+        }
+        require(numbers.sorted() == numbers) {
+            NUMBER_SHOULD_BE_ORDERED
+        }
     }
 
     fun checkGrade(win: Lotto, bonusNum: Int): GRADE {
@@ -21,5 +27,8 @@ class Lotto(private val numbers: List<Int>) {
         const val LENGTH_OF_NUM = 6
         const val START_NUM = 1
         const val END_NUM = 45
+        private const val LENGTH_IS_NOT_CORRECT = "[ERROR] 여섯 개의 숫자를 입력하세요."
+        private const val DUPLICATE_IS_NOT_ALLOWED = "[ERROR] 번호는 중복될 수 없습니다."
+        private const val NUMBER_SHOULD_BE_ORDERED = "[ERROR] 오름차순으로 정렬해서 입력하세요."
     }
 }
