1a0bcb3f5267e3ba061044e9070f0bea00a99e90
crAy <craycoding@gmail.com>
2023-11-06 21:38:39 +0900

    feat : Add lotto checker, lotto award enum class

--- src/main/kotlin/lotto/LottoInspector.kt
+++ src/main/kotlin/lotto/LottoInspector.kt
@@ -2,8 +2,24 @@ package lotto
 
 class LottoInspector {
 
-    private fun checkLotto(lotto: Lotto, winningNumber: List<Int>, bonusNumber: Int) {
+    private fun checkLotto(lotto: Lotto, winningNumber: List<Int>, bonusNumber: Int): Int {
         val lottoNumber = lotto.getLottoNumber()
+        val lottoResult = compareLotto(lottoNumber, winningNumber)
+        when (lottoResult) {
+            LottoAward.SIX.matchedNumbers -> return 1
+            LottoAward.FIVE.matchedNumbers -> return bonusCheck(lotto, bonusNumber)
+            LottoAward.FOUR.matchedNumbers -> return 4
+            LottoAward.THREE.matchedNumbers -> return 5
+        }
+        return 0
+    }
+
+    private fun bonusCheck(lotto: Lotto, bonusNumber: Int): Int {
+        val lottoNumbers = lotto.getLottoNumber()
+        if (lottoNumbers.contains(bonusNumber)) {
+            return 2
+        }
+        return 3
     }
 
     private fun compareLotto(lottoNumber: List<Int>, winningNumber: List<Int>): Int {
