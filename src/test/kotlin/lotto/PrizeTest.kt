6978f5ab00917c4c7b589350a1cb55cdcfe7bc2a
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 01:34:28 +0900

    feat: 수익률 출력 테스트 작성

--- src/test/kotlin/lotto/PrizeTest.kt
+++ src/test/kotlin/lotto/PrizeTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
+import ui.Output
 
 class PrizeTest {
     @Test
@@ -11,5 +12,11 @@ class PrizeTest {
         var prize = mutableListOf<Prize>(Prize.MISS, Prize.FOURTH, Prize.FIRST, Prize.FIRST, Prize.FIFTH, Prize.FIFTH)
         var prizeList = getPrizeList(prize)
         assertThat(prizeList).containsExactly(2,0,0,1,2,1)
+    }
+    @Test
+    fun `수익률 계산 테스트`() {
+        var prize = mutableListOf<Prize>(Prize.MISS, Prize.FOURTH, Prize.FIRST, Prize.FIRST, Prize.FIFTH, Prize.FIFTH)
+        var prizeList = getEarningRate(prize, 10)
+        Output.printEarnRate(prizeList)
     }
 }
