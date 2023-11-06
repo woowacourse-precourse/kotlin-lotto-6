20839989f9e63d86fd205d0f673e9682ddab4382
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-06 18:13:54 +0900

    Feat(Add new output message Enum
    당첨번호 입력해주세요와 보너스번호 입력해주세요 라는 메세지를 추가하였습니다

--- src/main/kotlin/lotto/enums/OutPut.kt
+++ src/main/kotlin/lotto/enums/OutPut.kt
@@ -2,6 +2,8 @@ package lotto.enums
 
 enum class OutPut(val message: String) {
     PLEASE_INPUT_AMOUNT("구입금액을 입력해 주세요."),
+    PLEASE_INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
+    PLEASE_INPUT_BOUNS_NUMBER("보너스 번호를 입력해 주세요."),
     PURCHASED("개를 구매했습니다")
     ;
 }
