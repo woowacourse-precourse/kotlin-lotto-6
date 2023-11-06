2bce5df3273627169695852feb0cafc3943997a8
BBIYACK-0834 <erty0834@naver.com>
2023-11-07 03:44:15 +0900

    게임 실행

---
+++ src/main/kotlin/lotto/game.kt
@@ -0,0 +1,13 @@
+package lotto
+
+class Game{
+    fun start(){
+
+        //1. 초기 설정
+        InitSetting.initSet()
+        //2.
+        InitSetting.printrandomlotto()
+        //3.
+        InitSetting.initSetans()
+    }
+}
