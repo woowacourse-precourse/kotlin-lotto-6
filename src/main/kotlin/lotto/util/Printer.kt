3a9a7da80b90f1567386c88797fc457f90e63ae6
sangil <fhemflrh60@gmail.com>
2023-11-06 16:44:12 +0900

    feat: Lotto Input Output 구축

--- src/main/kotlin/lotto/util/Printer.kt
+++ src/main/kotlin/lotto/util/Printer.kt
@@ -1,4 +1,12 @@
 package lotto.util
 
 class Printer {
+
+    fun printInputMoney() {
+        println(MESSAGE_INPUT_MONEY)
+    }
+
+    companion object {
+        const val MESSAGE_INPUT_MONEY = "구입 금액을 입력해 주세요."
+    }
 }
--- src/main/kotlin/lotto/util/Reader.kt
+++ src/main/kotlin/lotto/util/Reader.kt
@@ -1,4 +1,18 @@
 package lotto.util
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.model.Lotto
+
 class Reader {
+
+    // Todo: 입력 메서드 수정
+    fun readLottoPrice(): Int {
+        val readPrice = Console.readLine()
+        return 0
+    }
+    // Todo: 입력 메서드 수정
+    fun readLottoNumber(): Lotto {
+        val readNumberList = Console.readLine()
+        return Lotto(listOf(0, 0, 0, 0, 0, 0))
+    }
 }
