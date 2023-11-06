76d7327991c018868eceee5068994c2db06452d0
oh in seong <superohinsung@naver.com>
2023-11-06 12:54:14 +0900

    [feat] Add ability to print lotto numbers

--- src/main/kotlin/lotto/Printer.kt
+++ src/main/kotlin/lotto/Printer.kt
@@ -4,6 +4,15 @@ class Printer {
 
     fun printEnterPurchase() = println(Message.ENTER_PURCHASE)
 
-    fun printEnterLottoNumber() = println(Message.ENTER_LOTTO_NUMBER)
+    fun printBuyNPieces() = println(Message.BOUGHT_N_PIECES)
+
+    fun printEnterLottoNumber() = println(Message.ENTER_USER_PICK_NUMBERS)
+
+    fun printLottoNumber(lottery: List<Lotto>) {
+        repeat(lottery.size) {
+            println(lottery[it].formatNumbers())
+        }
+    }
+
 
 }
