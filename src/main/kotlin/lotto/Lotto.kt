16321bbb34e589f373d1c5faedb7e85b1dedeaf9
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 06:53:37 +0900

    Feat: 로또 번호 유효성 검사 기능(6자리가 아닐 때)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,14 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "$ERROR $LOTTO_NUMBER_ONLY_SIX_DIGITS" }
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        const val ERROR = "[ERROR]"
+        const val LOTTO_NUMBER_ONLY_SIX_DIGITS = "로또 번호는 6자리여야 합니다."
+    }
+
 }
