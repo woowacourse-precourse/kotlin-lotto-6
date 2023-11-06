5ac82d31234ddf4c539c49ed3474716d5963efe7
bak <bak4017@gmail.com>
2023-11-07 01:06:12 +0900

    refactor(lotto): 당첨 번호 및 보너스 번호 추첨 함수 LottoMachine 클래스로 이동

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,5 +1,6 @@
 package lotto.controller
 
+import lotto.domain.LottoMachine
 import lotto.domain.LottoStore
 import lotto.validator.BonusNumberValidator
 import lotto.validator.WinNumbersValidator
@@ -8,10 +9,10 @@ import lotto.view.OutputView
 
 class LottoController {
     fun run() {
-        val lottoStore = inputPrice()
+        val lottoStore: LottoStore = inputPrice()
         printTickets(lottoStore)
-        val winNumbers = drawWinNumbers()
-        val bonusNumbers = drawBonusNumber()
+        val winNumbers: List<String> = LottoMachine().drawWinNumbers()
+        val bonusNumber: String = LottoMachine().drawBonusNumber()
     }
 
     private fun inputPrice(): LottoStore {
@@ -20,38 +21,6 @@ class LottoController {
             try {
                 val purchasePrice = InputView.promptForPurchasePrice().also { println() }
                 return LottoStore(purchasePrice)
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
-                attempts++
-            }
-        }
-
-        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
-    }
-
-    private fun drawWinNumbers(): List<String> {
-        var attempts = 0
-        while (attempts <= 5) {
-            try {
-                val winNumbers = InputView.promptForWinNumbers().also { println() }
-                WinNumbersValidator(winNumbers)
-                return winNumbers
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
-                attempts++
-            }
-        }
-
-        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
-    }
-
-    private fun drawBonusNumber(): String {
-        var attempts = 0
-        while (attempts <= 5) {
-            try {
-                val bonusNumber = InputView.promptForBonusNumber().also { println() }
-                BonusNumberValidator(bonusNumber)
-                return bonusNumber
             } catch (e: IllegalArgumentException) {
                 println(e.message)
                 attempts++
--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,12 +1,40 @@
 package lotto.domain
 
+import lotto.validator.BonusNumberValidator
+import lotto.validator.WinNumbersValidator
+import lotto.view.InputView
+
 class LottoMachine {
-    fun drawWinNumbers(): Set<Int> {
-        return setOf()
+    fun drawWinNumbers(): List<String> {
+        var attempts = 0
+        while (attempts <= 5) {
+            try {
+                val winNumbers = InputView.promptForWinNumbers().also { println() }
+                WinNumbersValidator(winNumbers)
+                return winNumbers
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                attempts++
+            }
+        }
+
+        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
     }
 
-    fun drawBonusNumber(): Int {
-        return 0
+    fun drawBonusNumber(): String {
+        var attempts = 0
+        while (attempts <= 5) {
+            try {
+                val bonusNumber = InputView.promptForBonusNumber().also { println() }
+                BonusNumberValidator(bonusNumber)
+                return bonusNumber
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                attempts++
+            }
+        }
+
+        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
     }
 
 }
