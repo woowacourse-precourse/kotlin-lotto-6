9de7c7e64f6248675b81116c905c02a7300728af
lee989898 <wnsrud1352@gmail.com>
2023-11-06 18:12:18 +0900

    [test] numbers에 1,2,3이 포함되는지 확인 테스트 구현

--- src/test/kotlin/study/SetTest.kt
+++ src/test/kotlin/study/SetTest.kt
@@ -1,6 +1,8 @@
 package study
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class SetTest {
     private lateinit var numbers: HashSet<Int>
@@ -12,5 +14,12 @@ class SetTest {
         numbers.add(1)
         numbers.add(2)
         numbers.add(3)
+    }
+
+    @Test
+    fun contains() {
+        assertThat(numbers.contains(1)).isTrue()
+        assertThat(numbers.contains(2)).isTrue()
+        assertThat(numbers.contains(3)).isTrue()
     }
 }
