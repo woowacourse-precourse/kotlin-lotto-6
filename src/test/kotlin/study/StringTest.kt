4a42e1047cef235e695287fc14f562303ea00612
lee989898 <wnsrud1352@gmail.com>
2023-11-06 17:24:21 +0900

    [test] "abc"에서 범위를 벗어난 인덱스를 get()으로 가져올 때 예외를 던지는지 확인 테스트 구현

--- src/test/kotlin/study/StringTest.kt
+++ src/test/kotlin/study/StringTest.kt
@@ -2,6 +2,9 @@ package study
 
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.DisplayName
 
 class StringTest {
 
@@ -27,8 +30,24 @@ class StringTest {
     fun `"(1,2)"를 substring()로 ()을 제거했을때 "1,2"를 반환하는지 확인`() {
         val given = "(1,2)"
 
-        val result = given.substring(1,4)
+        val result = given.substring(1, 4)
 
         assertThat(result).isEqualTo("1,2")
+    }
+
+    @Test
+    @DisplayName("\"abc\"에서 범위를 벗어난 인덱스를 get()으로 가져올 때 예외를 던지는지 확인")
+    fun `"abc" use get(3) throw IndexOutOfBoundsException`() {
+        val given = "abc"
+
+        assertThatExceptionOfType(IndexOutOfBoundsException::class.java)
+            .isThrownBy {
+                given.get(3)
+            }.withMessage("String index out of range: 3")
+
+        assertThatThrownBy {
+            given.get(3)
+        }.isInstanceOf(IndexOutOfBoundsException::class.java)
+            .hasMessageContaining("String index out of range: 3")
     }
 }
