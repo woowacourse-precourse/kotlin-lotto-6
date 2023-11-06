a325967114792f46df0998c8fc862e895c6b74ae
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-07 08:43:03 +0900

    feat: lotto game v1 - RandomNums(6) - No Duplicated Number -

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,45 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    val money = purchaseMoney() // 로또 구매 금 입력
+    val lotto = purchaseLotto(money / 1000) // 로또 생성
+    printLotto(lotto) // 구매한 로또 출력
+
+//    val winningNums = winningNumber()
+//    val bonusNum = bonusNumber()
+//    print("구입금액을 입력해 주세요.")
+//
+//    print("당첨 번호를 입력해 주세요.")
+//    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+//
+//    print("보너스 번호를 입력해 주세요.")
+
 }
+
+// 사용자가 구입 금액 입력 함수
+fun purchaseMoney(): Int {
+    println("구입금액을 입력해 주세요.")
+    val money = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+    if (money < 1000 || money % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+    }
+    return money
+}
+
+// 구매한 로또 개수 입력 함수
+fun purchaseLotto(lottoCnt: Int): List<List<Int>> {
+    return List(lottoCnt) {
+        Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+    }
+}
+
+// 구매한 로또 개수 출력하기
+fun printLotto(lotto: List<List<Int>>) {
+    println("${lotto.size}개를 구매했습니다.")
+    lotto.forEach { println(it) }
+}
+
+
