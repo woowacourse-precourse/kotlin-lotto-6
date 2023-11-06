57833f530577e2b629b2a0901f4d2efd86e6807d
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 14:24:41 +0900

    refactor: Lotto 생성 시 예외 발생할 경우 예외 메세지 변경

--- src/main/kotlin/util/LottoValidation.kt
+++ src/main/kotlin/util/LottoValidation.kt
@@ -2,15 +2,15 @@ package util
 
 enum class LottoValidation(val message: String, val isValid: (List<Int>) -> Unit) {
     REQUIRED_LOTTO_NUMBER_COUNT(
-        "[ERROR] 로또 번호는 6개여야 합니다.",
+        "[ERROR] 6개의 번호가 정확히 입력되지 않았습니다.",
         fun(numbers: List<Int>) = require(numbers.size == 6) { REQUIRED_LOTTO_NUMBER_COUNT.message },
     ),
     UNIQUE_LOTTO_NUMBERS(
-        "[ERROR] 로또 번호는 중복되어선 안됩니다.",
+        "[ERROR] 중복된 번호가 존재합니다.",
         fun(numbers: List<Int>) = require(numbers.toSet().size == 6) { UNIQUE_LOTTO_NUMBERS.message },
     ),
     LOTTO_NUMBER_RANGE(
-        "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+        "[ERROR] 1 ~ 45 범위에 포함되지 않는 숫자가 존재합니다.",
         fun(numbers: List<Int>) = require(numbers.all { it in 1..45 }) { LOTTO_NUMBER_RANGE.message },
     ),
 }
