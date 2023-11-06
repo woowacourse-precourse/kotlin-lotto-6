1232d8eed37528a4539a28c10f527df704ef5044
Minjun Kim <damon911@naver.com>
2023-11-06 20:13:28 +0900

    test: 로또 번호와 당첨 번호 비교하여 올바른 당첨 결과를 리턴하는지 테스트

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -23,5 +24,35 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 100))
         }
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 비교하면 3개가 일치`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.compareLotto(listOf(1, 2, 3, 14, 15, 16), 45)).isEqualTo(3)
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 비교하면 4개가 일치`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.compareLotto(listOf(1, 2, 3, 4, 15, 16), 45)).isEqualTo(4)
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 비교하면 5개가 일치`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.compareLotto(listOf(1, 2, 3, 4, 5, 16), 45)).isEqualTo(5)
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 비교하면 5개와 보너스 번호가 일치`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.compareLotto(listOf(1, 2, 3, 4, 5, 16), 6)).isEqualTo(50)
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 비교하면 6개가 일치`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.compareLotto(listOf(1, 2, 3, 4, 5, 6), 45)).isEqualTo(6)
     }
 }
