71c50b1d303fa258524c80d97e875a0a7141c1ae
Junyoung-WON <wjyo60@gmail.com>
2023-11-06 16:54:42 +0900

    FEAT: 사용자로부터 구입 금액을 입력받는 기능 구현
    - User 클래스는 구입 금액인 purchaseMoney와 입력값을 검사하는 validator
    객체를 인스턴스로 가진다.
    - inputPurchaseMoney() 메소드는 사용자에게 구입 금액을 입력받고, 예외 발생
    시 올바른 입력 값을 받을 때 까지 반복하여 입력 받는다. 입력받은 문자열은
    Int형으로 변환하여 purchaseMoney에 저장한다.

--- src/main/kotlin/lotto/User.kt
+++ src/main/kotlin/lotto/User.kt
@@ -1,4 +1,19 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 class User {
+    var purchaseMoney: Int = 0
+    val validator = Validator()
+    fun inputPurchaseMoney() {
+        var validation: Boolean = false
+        var money: String = ""
+        while (!validation) {
+            println("구입금액을 입력해 주세요.")
+            money = Console.readLine()
+            validation = validator.validatePurchaseMoney(money)
+        }
+        purchaseMoney = money.toInt()
+    }
+    
 }
