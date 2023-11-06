c878f986c9436e66d29e285cc41249edc5f7b5ea
sunkk8482 <sunkk8482@naver.com>
2023-11-06 13:51:18 +0900

    Test : 반올림에서 올림 테스트

--- src/test/kotlin/lotto/domain/PrizeTest.kt
+++ src/test/kotlin/lotto/domain/PrizeTest.kt
@@ -24,4 +24,15 @@ class PrizeTest {
         val testResult = testPrize.getPrizeRatio(testList, testMoney)
         Assertions.assertThat(testResult).isEqualTo(1833.3)
     }
+
+    @Test
+    fun prizeRatioRoundUpTest() {
+        val testPrize = Prize()
+        val testList = mutableListOf<Grade>()
+        testList.add(Grade.FIFTH)
+        testList.add(Grade.FOURTH)
+        val testMoney = 6000
+        val testResult = testPrize.getPrizeRatio(testList, testMoney)
+        Assertions.assertThat(testResult).isEqualTo(916.7)
+    }
 }
