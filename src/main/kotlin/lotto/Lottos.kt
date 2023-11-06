d56a073268e8bfdf73d60f1f1c573b787a645dec
LEE <tpqms775@naver.com>
2023-11-06 20:43:28 +0900

    feat : 구매 개수와 구매한 로또 출력

--- src/main/kotlin/lotto/Lottos.kt
+++ src/main/kotlin/lotto/Lottos.kt
@@ -3,11 +3,16 @@ package lotto
 class Lottos {
     fun makeLotto(): List<Lotto> {
         val vendor = Vendor()
+        val outputManager = OutputManager()
         val numberCreator = NumberCreator()
         val lottos = mutableListOf<Lotto>()
+        val lottoNum = vendor.lottoNum()
 
-        repeat(vendor.lottoNum()){
+        outputManager.lottos(lottoNum)
+
+        repeat(lottoNum){
             val lottoNumber = numberCreator.randoms()
+            println("$lottoNumber")
             val lotto = Lotto(lottoNumber)
             lottos.add(lotto)
         }
---
+++ src/main/kotlin/lotto/OutputManager.kt
@@ -0,0 +1,9 @@
+package lotto
+
+
+class OutputManager {
+    fun lottos(lottosNum : Int){
+        println()
+        println("$lottosNum 개를 구매했습니다.")
+    }
+}
