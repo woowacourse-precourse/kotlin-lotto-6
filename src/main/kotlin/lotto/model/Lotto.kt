3e0f0a6dee9e8a3aa61fa24ee8bf40fb1e0db34d
sh1mj1 <7wlgns@gmail.com>
2023-11-06 21:06:11 +0900

    feat(Lotto): validate Lotto Numbers
    * 로또 번호는 6개.
    * 로또 번호에 중복 없음.
    * 로또 번호는 1 ~ 45 의 숫자로 이루어짐.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,7 +2,17 @@ package lotto.model
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            "[ERROR] 로또 번호는 6개의 숫자를 가집니다."
+        }
+        require(numbers.size == numbers.distinct().size) {
+            "[ERROR] 로또 번호는 중복되면 안 됩니다."
+        }
+        numbers.forEach {
+            require(it in 1..45) {
+                "[ERROR] 로또 번호는 1 ~ 45 이어야 합니다."
+            }
+        }
     }
 
     // TODO: 추가 기능 구현
