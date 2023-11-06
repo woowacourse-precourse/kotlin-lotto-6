831537c72aa8dc39e2b231ac6c19b0d0e9c22875
Junyoung-WON <wjyo60@gmail.com>
2023-11-06 11:38:49 +0900

    FEAT: 입력된 문자열이 정수인지 검증하는 함수
    입력된 문자열을 정수로 변환할 때 예외가 발생한다면 에러 메세지를 출력
    후 false를 반환한다.
    정상적으로 변환된다면 true를 반환한다.

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -1,5 +1,13 @@
 package lotto
 
 class Validator {
-    
+    fun validatePurchaseMoney(purchaseMoney: String): Boolean {
+        return try {
+            purchaseMoney.toInt()
+            true
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] 구입 금액을 올바르게 입력해주세요.")
+            false
+        }
+    }
 }
