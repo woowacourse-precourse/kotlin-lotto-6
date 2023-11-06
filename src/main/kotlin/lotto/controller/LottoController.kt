8edb30e8ea6291641684e9fe3628ea36442e4beb
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-06 17:46:49 +0900

    Feat(Added function to input winning numbers)
    당첨숫자를 입력받는 기능을 추가하였습니다

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -11,6 +11,7 @@ class LottoController(private val lottoModel: LottoModel, private val lottoView:
     fun startLotto() {
         lottoView.printEnterPurchaseMessage()
         inputPurchaseAmount()
+        inputWinningNumbers()
     }
 
     private fun inputPurchaseAmount() {
@@ -26,11 +27,39 @@ class LottoController(private val lottoModel: LottoModel, private val lottoView:
         }
     }
 
+    private fun inputWinningNumbers() {
+        while (true) {
+            try {
+                lottoModel.setWinningNumbers(checkWinningNumbers(readLine()))
+                break
+            } catch (e: IllegalArgumentException) {
+                println("${Exception.ERROR_HEADER.message} ${e.message}")
+                lottoView.printEnterPurchaseMessage()
+            }
+        }
+    }
+
     private fun checkPurchaseAmount(purchaseAmount: String): Int {
         require(purchaseAmount.isNotBlank() && purchaseAmount.isNotEmpty()) { Exception.INPUT_IS_BLANK.message }
-        require(purchaseAmount.all { it.isDigit() }) { Exception.NOT_NUMBER.message }
+        require(purchaseAmount.isDigit()) { Exception.NOT_NUMBER.message }
         val amount = purchaseAmount.toInt()
         require(amount > 0 && amount % 1000 == 0) { Exception.AMOUNT_MUST_BE_THOUSAND_WON.message }
+
         return amount
+    }
+
+    private fun checkWinningNumbers(winningNumbers: String): String {
+        val splitWinningNumbers = winningNumbers.split(",")
+        require(splitWinningNumbers.size == 6) { Exception.INVALID_COUNT.message }
+        require(splitWinningNumbers.all { it.isNotBlank() && it.isNotEmpty() }) { Exception.INPUT_IS_BLANK.message }
+        require(splitWinningNumbers.all { it.isDigit() }) { Exception.NOT_NUMBER.message }
+        require(splitWinningNumbers.toSet().size == 6) { Exception.DUPLICATED_NUMBER.message }
+        require(splitWinningNumbers.all { it.toInt() in 1..44 }) { Exception.INVALID_RANGE_NUMBER.message }
+
+        return winningNumbers
+    }
+
+    private fun String.isDigit(): Boolean {
+        return all { it.isDigit() }
     }
 }
--- src/main/kotlin/lotto/model/LottoModel.kt
+++ src/main/kotlin/lotto/model/LottoModel.kt
@@ -4,12 +4,19 @@ import camp.nextstep.edu.missionutils.Randoms
 import java.lang.NumberFormatException
 
 class LottoModel {
+    private val winningAndBounsNumbers = mutableListOf<String>()
 
     fun generateLottoNumbers(amount: Int): MutableList<List<Int>> {
         val lottoNumbers = mutableListOf<List<Int>>()
         repeat(amount / 1000) { lottoNumbers.add(createRandomNumber()) }
 
         return lottoNumbers
+    }
+
+    fun setWinningNumbers(winningNumbers: String) {
+        winningNumbers
+            .split(",")
+            .forEach { winningAndBounsNumbers.add(it) }
     }
 
     private fun createRandomNumber(): List<Int> {
