c8b78b0bfa36acccb061fa521d941846ecf198ee
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 05:02:05 +0900

    Feat: 로또 구입 금액 입력 유효성 검사(0 이하의 숫자를 입력했을 때)

--- src/main/kotlin/lotto/Amount.kt
+++ src/main/kotlin/lotto/Amount.kt
@@ -4,15 +4,22 @@ class Amount(private val amount: String) {
 
     init {
         require(isNumber()) { throw IllegalArgumentException("$ERROR $INPUT_ONLY_NUMBER") }
+        require(isBelowZero()) {throw IllegalArgumentException("$ERROR $INPUT_ONLY_MORE_THAN_ZERO")}
     }
 
     private fun isNumber(): Boolean {
         return amount.all { it.isDigit() }
     }
 
+    private fun isBelowZero(): Boolean {
+        val value = amount.toIntOrNull()
+        return value != null && value > 0
+    }
+
     companion object {
         const val ERROR = "[ERROR]"
         const val INPUT_ONLY_NUMBER = "숫자만 입력하세요."
+        const val INPUT_ONLY_MORE_THAN_ZERO = "구입 금액으로 0 이하의 숫자는 입력할 수 없습니다."
     }
 
 }
