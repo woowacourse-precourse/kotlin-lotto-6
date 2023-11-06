937b8b6e0820be6c32b096e01a4ea5580d332c8c
sseung416 <choiseungyeon416@gmail.com>
2023-11-06 11:49:42 +0900

    refactor: Validator 오류 메시지 ValidatorError enum class로 상수화

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -1,13 +1,18 @@
 package lotto
 
+enum class ValidatorError(val message: String) {
+    NotNumber("문자는 입력할 수 없습니다."),
+    NotPositiveNumber("음수나 0 값은 입력할 수 없습니다.")
+}
+
 fun <T> T.requireAndReturn(value: Boolean, message: String, containsErrorMessage: Boolean = true): T {
     require(value) { if (containsErrorMessage) message.toErrorMessage() else message }
     return this
 }
 
-fun String.toValidInt(): Int = requireAndReturn(this.isInt(), "문자는 입력할 수 없습니다.").toInt()
+fun String.toValidInt(): Int = requireAndReturn(this.isInt(), ValidatorError.NotNumber.message).toInt()
 
-fun Int.validPositiveNumber(): Int = requireAndReturn(this.isPositiveNumber(), "음수나 0 값은 입력할 수 없습니다.")
+fun Int.validPositiveNumber(): Int = requireAndReturn(this.isPositiveNumber(), ValidatorError.NotPositiveNumber.message)
 
 fun String.isInt(containsSign: Boolean = false): Boolean {
     if (isBlank()) return false
