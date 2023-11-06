cc51f740bd267ed52daf1e3977552e4895bb0d90
Kim Gimun <kmkim7575@gmail.com>
2023-11-06 19:39:14 +0900

    test: 입력받은 구매 금액에 따른 발행 가능 휫수 출력 테스트 케이스 추가

---
+++ src/test/kotlin/lotto/view/ResultViewTest.kt
@@ -0,0 +1,15 @@
+package lotto.view
+
+import lotto.domain.purchase.Money
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ResultViewTest {
+
+    @Test
+    fun `입력받은 구매 금액에 따른 발행 가능 횟수 출력`() {
+        val money = Money(5000)
+        val chance = Chance(money)
+        assertThat(chance.times).isEqualTo(5)
+    }
+}
