1d79b349f60ef487e9d1dc4dff7d2e57b216156a
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 14:53:19 +0900

    refactor: 당첨번호 확인로직 중 중복 로직 제거

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -6,10 +6,13 @@ import util.LottoValidation
 class WinningNumbersManager(private val numbers: List<String>) {
 
     private lateinit var winningNumbers: List<Int>
+
     init {
-        numbers.forEach { InputValidation.NUMBER_EMPTY.isValid(it) }
-        numbers.forEach { InputValidation.ONLY_NUMBER.isValid(it) }
-        numbers.forEach { InputValidation.NUMBER_FORMAT.isValid(it) }
+        numbers.forEach {
+            InputValidation.NUMBER_EMPTY.isValid(it)
+            InputValidation.ONLY_NUMBER.isValid(it)
+            InputValidation.NUMBER_FORMAT.isValid(it)
+        }
         winningNumbers = numbers.map { it.toInt() }
         LottoValidation.REQUIRED_LOTTO_NUMBER_COUNT.isValid(winningNumbers)
         LottoValidation.UNIQUE_LOTTO_NUMBERS.isValid(winningNumbers)
