d629312685b0cde7db8089edb80aa83376d7a41e
stopstone <wltjr6432@naver.com>
2023-11-07 00:45:52 +0900

    refactor: LottoManager - run() 기능 분리

--- src/main/kotlin/lotto/controller/LottoManager.kt
+++ src/main/kotlin/lotto/controller/LottoManager.kt
@@ -2,39 +2,64 @@ package lotto.controller
 
 import lotto.Lotto
 import lotto.domain.*
+import lotto.utils.Constants
 import lotto.view.OutputView
 
 class LottoManager(private val outputView: OutputView) {
     fun run() {
+        val ticket = purchaseLotto()
+        val lottoList = answerLotto(ticket)
+        val winningNumbers = userWinningLotto()
+        val bonusNumber = bonus(winningNumbers)
+        val lotto = Lotto(winningNumbers)
+        val results = rankOfLotto(lotto, lottoList, bonusNumber)
+        val profitPercentage = yieldLotto(results, ticket)
+        outputResults(results, profitPercentage)
+    }
+
+    private fun purchaseLotto(): Int {
         val lottoPurchase = LottoPurchase()
         val amount = lottoPurchase.buyLotto()
         val ticket = lottoPurchase.calculateTicket(amount)
         outputView.showBuyTicketMessage(ticket)
 
+        return ticket
+    }
+
+    private fun answerLotto(ticket: Int): MutableList<Lotto> {
         val lottoWrapper = LottoWrapper()
         val lottoList = lottoWrapper.repeatLottoNumbers(ticket)
 
         outputView.showLottoNumbers(lottoList)
+        return lottoList
+    }
 
+    private fun userWinningLotto(): List<Int> {
         val lottoWinning = LottoWinning()
-        val winningNumbers = lottoWinning.createWinningLotto()
+        return lottoWinning.createWinningLotto()
+    }
 
-        val lotto = Lotto(winningNumbers)
-
+    private fun bonus(winningNumbers: List<Int>): Int {
         val bonus = LottoBonus()
-        val bonusNumber = bonus.createBonusNumber(winningNumbers)
+        return bonus.createBonusNumber(winningNumbers)
+    }
 
-
+    private fun rankOfLotto(lotto: Lotto, lottoList: MutableList<Lotto>, bonusNumber: Int): MutableMap<Prize, Int> {
         outputView.showWinningStatisticsMessages()
         val ranks = LottoRanks()
-
         var results = mutableMapOf<Prize, Int>()
         lottoList.forEach {
             results = ranks.rank(it.getLotto(), lotto, bonusNumber)
         }
-        val yield = LottoYield()
-        val profitPercentage = yield.calculateLottoYield(results, amount)
+        return results
+    }
 
+    private fun yieldLotto(results: MutableMap<Prize, Int>, ticket: Int): Double {
+        val yield = LottoYield()
+        return yield.calculateLottoYield(results, ticket * Constants.THOUSAND_PRICE)
+    }
+
+    private fun outputResults(results: MutableMap<Prize, Int>, profitPercentage: Double) {
         outputView.printResults(results)
         outputView.printProfitPercentage(profitPercentage)
     }
