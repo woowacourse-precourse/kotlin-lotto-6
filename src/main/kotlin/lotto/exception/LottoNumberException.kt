779048f8bd8ee14adf112d1fd7e444f4786e0b0f
junyong <junyong008@gmail.com>
2023-11-06 17:42:47 +0900

    style(LottoNumber): 숫자 범위 초과 에러 메시지 내용 수정

--- src/main/kotlin/lotto/exception/LottoNumberException.kt
+++ src/main/kotlin/lotto/exception/LottoNumberException.kt
@@ -1,7 +1,7 @@
 package lotto.exception
 
 enum class LottoNumberException(val message: String) {
-    OUT_RANGE("[ERROR] 로또 숫자 범위는 %d부터 %d까지 입니다.");
+    OUT_RANGE("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.");
 
     fun getLottoNumberRange(min: Int, max: Int): String = message.format(min, max)
 }
