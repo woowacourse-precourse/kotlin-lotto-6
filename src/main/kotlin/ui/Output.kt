68b4737c06442f3da6faf57ba23a4311828ffb54
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-06 22:44:49 +0900

    feat: 구입한 로또들의 번호를 출력하는 기능 추가

--- src/main/kotlin/ui/Output.kt
+++ src/main/kotlin/ui/Output.kt
@@ -2,9 +2,16 @@ package ui
 
 import exception.Message
 import exception.Message.Companion.MESSAGE_PRINT_TICKET_AMOUNT
+import lotto.UserLotto
 
 object Output  {
     fun printLottoCount(count: Int) {
         println("${count}"+ MESSAGE_PRINT_TICKET_AMOUNT)
+    }
+
+    fun printUserLottos(userLottos: UserLotto) {
+        userLottos.getUserLotto().forEach {
+            println(it.getNumbers())
+        }
     }
 }
