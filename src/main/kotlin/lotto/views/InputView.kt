9b4ac66ee94cac25230f3be3340bc38b0aae3abe
sunkk8482 <sunkk8482@naver.com>
2023-11-06 14:13:28 +0900

    Refactor : 재입력을 재귀함수가 아닌 while로 수정

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -5,34 +5,37 @@ import lotto.domain.*
 
 object InputView {
     fun enterMoney(): Int {
-        val input = Console.readLine()
-        try {
-            moneyErrorCheck(input)
-            return input.toInt()
-        } catch (e: IllegalArgumentException) {
-            println("[ERROR] ${e.message}")
+        while(true) {
+            val input = Console.readLine()
+            try {
+                moneyErrorCheck(input)
+                return input.toInt()
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ${e.message}")
+            }
         }
-        return enterMoney()
     }
 
     fun enterWinningNumber(): List<Int> {
-        val input = Console.readLine()
-        val splitInput = input.split(",")
-        try {
-            return winningNumberErrorCheck(input, splitInput)
-        } catch (e: IllegalArgumentException) {
-            println("[ERROR] ${e.message}")
+        while(true) {
+            val input = Console.readLine()
+            val splitInput = input.split(",")
+            try {
+                return winningNumberErrorCheck(input, splitInput)
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ${e.message}")
+            }
         }
-        return enterWinningNumber()
     }
 
     fun enterBonusNumber(winningNumber: List<Int>): Int {
-        val bonus = Console.readLine()
-        try {
-            return bonusNumberErrorCheck(winningNumber, bonus)
-        } catch (e: IllegalArgumentException) {
-            println("[ERROR] ${e.message}")
+        while(true) {
+            val bonus = Console.readLine()
+            try {
+                return bonusNumberErrorCheck(winningNumber, bonus)
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ${e.message}")
+            }
         }
-        return enterBonusNumber(winningNumber)
     }
 }
