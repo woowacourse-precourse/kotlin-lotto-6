71033b2a584ac8248e6289a26152370a29f2f03a
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2023-11-06 16:26:43 +0900

    feat : 발행한 로또 수량 출력

--- src/main/kotlin/lotto/controller/LottoGame.kt
+++ src/main/kotlin/lotto/controller/LottoGame.kt
@@ -3,15 +3,20 @@ package lotto.controller
 import lotto.util.Constants.INPUT_MONEY
 import lotto.util.Validation
 import lotto.view.Input
+import lotto.view.Output
 
 class LottoGame {
 
     private val input = Input()
+    private val output = Output()
     private val validate = Validation()
 
     fun play() {
         println(INPUT_MONEY)
         val amount = checkInputAmount()
+        println()
+
+        output.outputNumber(amount)
     }
 
     private fun checkInputAmount(): Int {
--- src/main/kotlin/lotto/util/Constants.kt
+++ src/main/kotlin/lotto/util/Constants.kt
@@ -3,4 +3,5 @@ package lotto.util
 object Constants {
 
     const val INPUT_MONEY = "구입금액을 입력해 주세요."
+    const val OUTPUT_NUMBER = "개를 구매했습니다."
 }
