0ba006ec35c31ac53b834560d4e9ea90bb9e87f8
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-11-06 18:56:55 +0900

    [feat] 로또 구입 금액 입력 예외처리

--- src/main/kotlin/lotto/domain/PurchaseLotto.kt
+++ src/main/kotlin/lotto/domain/PurchaseLotto.kt
@@ -13,4 +13,11 @@ class PurchaseLotto {
         validateAmount(amount)
         return amount.toInt()
     }
+
+    private fun validateAmount(amount: String) {
+        for(c in amount){
+            if(c !in '0' until '9') throw IllegalArgumentException(MESSAGE_ONLY_NUMBER)
+        }
+        if(amount.toInt() % AMOUNT_UNIT != 0) throw IllegalArgumentException(MESSAGE_NOT_FIT_UNIT_1000)
+    }
 }
