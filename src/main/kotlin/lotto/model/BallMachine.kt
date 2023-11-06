2fd78deac355e00ac8fd92bd6d0816260c0e44c5
unso99 <unso99@naver.com>
2023-11-06 21:18:16 +0900

    :hammer: BallMachine 파일 변수명 변경

--- src/main/kotlin/lotto/model/BallMachine.kt
+++ src/main/kotlin/lotto/model/BallMachine.kt
@@ -2,8 +2,8 @@ package lotto.model
 
 class BallMachine() {
 
-    private var _winningNumber = mutableListOf<Int>()
-    var winningNumber = _winningNumber
+    private var _winningNumbers = mutableListOf<Int>()
+    var winningNumbers = _winningNumbers
 
     private var _bonusNumber = 0
     var bonusNumber = _bonusNumber
