bd82a6f194b1fd3f77f97d83b148cb4e8cc6b086
sh1mj1 <7wlgns@gmail.com>
2023-11-06 21:41:20 +0900

    feat(WinningRank): implement rank of the winning
    * 로또 당첨에 대한 순위와 상금을 구현한다.

--- src/main/kotlin/lotto/model/WinningRank.kt
+++ src/main/kotlin/lotto/model/WinningRank.kt
@@ -1,5 +1,10 @@
 package lotto.model
 
-enum class WinningRank(val matchingCount: Int, prize: Int) {
-
+enum class WinningRank(val prize: Int) {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5_000),
+    FAILURE(0)
 }
