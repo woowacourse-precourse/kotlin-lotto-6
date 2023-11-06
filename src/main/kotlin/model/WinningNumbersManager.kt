8c5a069e38d0e9b47251b470152d17c62884dad0
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 14:32:33 +0900

    refactor: 당첨번호 관리 객체 유효성 검사 로직 수정

--- src/main/kotlin/model/WinningNumbersManager.kt
+++ src/main/kotlin/model/WinningNumbersManager.kt
@@ -1,15 +1,15 @@
 package model
 
-class WinningNumbersManager(number: List<String>) {
-    init {
-        require(number.all { it.matches(Regex("[^0-9]")) }) { Error.INPUT_ONLY_NUMBER.message }
-        require(number.toSet().size == number.size) { Error.UNIQUE_LOTTO_NUMBERS.message }
-        require(number.all { it.toInt() in 1..45 }) { Error.LOTTO_NUMBER_RANGE.message }
-    }
+import util.InputValidation
+import util.LottoValidation
 
-    private enum class Error(val message: String) {
-        INPUT_ONLY_NUMBER("[ERROR] 숫자만 입력이 가능합니다."),
-        UNIQUE_LOTTO_NUMBERS("[ERROR] 로또 번호는 중복되어선 안됩니다."),
-        LOTTO_NUMBER_RANGE("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
+class WinningNumbersManager(private val numbers: List<String>) {
+
+    private lateinit var winningNumbers: List<Int>
+    init {
+        numbers.forEach { InputValidation.ONLY_NUMBER.isValid(it) }
+        winningNumbers = numbers.map { it.toInt() }
+        LottoValidation.UNIQUE_LOTTO_NUMBERS.isValid(winningNumbers)
+        LottoValidation.LOTTO_NUMBER_RANGE.isValid(winningNumbers)
     }
 }
