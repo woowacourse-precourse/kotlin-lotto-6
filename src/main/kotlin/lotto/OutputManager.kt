336755c63a823d82bbc09111cf6666acda3f544c
YuBeen-Park <park_ubin@naver.com>
2023-11-07 01:49:22 +0900

    mod(OutputManager) : companion object 상수화

--- src/main/kotlin/lotto/OutputManager.kt
+++ src/main/kotlin/lotto/OutputManager.kt
@@ -11,9 +11,9 @@ class OutputManager {
     fun printInputBonusNum() = println(INPUT_BONUS_NUM_MSG)
 
     companion object {
-        private val INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요."
-        private val PURCHASED_LOTTO_AMOUNT_MSG = "개를 구매했습니다."
-        private val INPUT_PRIZE_NUM_MSG = "당첨 번호를 입력해 주세요."
-        private val INPUT_BONUS_NUM_MSG = "보너스 번호를 입력해 주세요."
+        private const val INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요."
+        private const val PURCHASED_LOTTO_AMOUNT_MSG = "개를 구매했습니다."
+        private const val INPUT_PRIZE_NUM_MSG = "당첨 번호를 입력해 주세요."
+        private const val INPUT_BONUS_NUM_MSG = "보너스 번호를 입력해 주세요."
     }
 }
