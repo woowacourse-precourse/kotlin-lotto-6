c7f28a8a31fcbab1e6a7fa76a11ee7d7696eb0f8
조형석 <chs9902@naver.com>
2023-11-07 01:03:23 +0900

    feat: 로또 번호 개수 유효성 검사 추가

--- src/main/kotlin/lotto/model/Constants.kt
+++ src/main/kotlin/lotto/model/Constants.kt
@@ -33,13 +33,14 @@ class Constants {
         val TOTAL_PROFIT = "총 수익률은 "
         val PERCENT = "%입니다."
 
+        val ERROR_LOTTO_SIZE = "[ERROR] 로또 번호는 6자리입니다."
         val ERROR_LOTTO_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
         val ERROR_MONEY_FORMAT = "[ERROR] 구매 금액은 숫자여야 합니다."
         val ERROR_NO_CHAGES = "[ERROR] 잔돈은 안받습니다."
         val ERROR_MONEY_RANGE = "[ERROR] 1000원 이상 돈을 주십시오."
         val ERROR_LOTTO_DUPlICATION = "[ERROR] 로또 번호 중복 발견."
         val ERROR_LOTTO_FORMAT = "[ERROR] 로또 번호는 숫자여야 합니다."
-        val ERROR_BONUS_DUPLICATION = "[ERROR] 보너스가 답과 중복 됐습니다."
+        val ERROR_BONUS_DUPLICATION = "[ERROR] 보너스가 답과 중복됐습니다."
         val ERROR_BONUS_FORMAT = "[ERROR] 보너스는 숫자여야 합니다."
     }
 }
--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -7,17 +7,27 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun duplicationValidate() {
-        if (numbers.toSet().size != numbers.size)
+        if (numbers.toSet().size != numbers.size){
+            println(Constants.ERROR_LOTTO_DUPlICATION)
             throw IllegalStateException(Constants.ERROR_LOTTO_DUPlICATION)
+        }
     }
 
     fun rangeValidate() {
         for (number in numbers){
             if (number > 45 || number < 1){
+                println(Constants.ERROR_LOTTO_RANGE)
                 throw IllegalStateException(Constants.ERROR_LOTTO_RANGE)
             }
         }
     }
+    fun sizeValidate() {
+        if (numbers.size != Constants.SIX){
+            println(Constants.ERROR_LOTTO_SIZE)
+            throw IllegalStateException(Constants.ERROR_LOTTO_SIZE)
+        }
+    }
+
 
     fun serve(): List<Int> {
         return numbers
