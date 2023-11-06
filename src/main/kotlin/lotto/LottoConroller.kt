9642eb09bdd4854a7e8fe20b07655839fa11b5df
park-yina <111878820+park-yina@users.noreply.github.com>
2023-11-06 11:23:38 +0900

    FEAT:당첨 개수에 따른 출력양식 만들기 DOCS:에러사항 추가

--- src/main/kotlin/lotto/LottoConroller.kt
+++ src/main/kotlin/lotto/LottoConroller.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lottoView.LottoOutPut
+import lottoView.LottoWinningStatics
 import lottoViewModel.CreateNumbers
 import lottoViewModel.ValidInput
 
@@ -30,6 +31,18 @@ class LottoConroller {
         catch (e:IllegalArgumentException){
             println(e.message)
             return printWinningMents()
+        }
+    }
+    fun calculateRank(winningNumbers:List<Int>,bonusNumber:String,purchaseLotto:List<List<Int>>){
+    }
+    private fun winningRank(matchNumber:Int,hasBonus:Boolean):LottoWinningStatics.NumberOfMatches{
+        return when{
+            matchNumber==6->LottoWinningStatics.NumberOfMatches.SIX_MATHCES
+            matchNumber==5&&hasBonus->LottoWinningStatics.NumberOfMatches.FIVE_BONUS_MATCHES
+            matchNumber==5->LottoWinningStatics.NumberOfMatches.FIVE_MATCHES
+            matchNumber==4->LottoWinningStatics.NumberOfMatches.FOUR_MATCHES
+            matchNumber==3->LottoWinningStatics.NumberOfMatches.THREE_MATCHES
+            else->LottoWinningStatics.NumberOfMatches.RESULT_RATE
         }
     }
 }
