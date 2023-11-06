7cbb02ae4b0df277170e6b856c546f84d861c030
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-07 08:58:45 +0900

    feat: lotto game v3 - winningNumber(당첨번호) - bonusNumber(보너스번호)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,9 +8,9 @@ fun main() {
     val lotto = purchaseLotto(money / 1000) // 로또 생성
     printLotto(lotto) // 구매한 로또 출력
 
-//    val winningNums = winningNumber()
-//    val bonusNum = bonusNumber()
-//    print("구입금액을 입력해 주세요.")
+    val winningNums = winningNumber()
+    val bonusNum = bonusNumber()
+    print("구입금액을 입력해 주세요.")
 //
 //    print("당첨 번호를 입력해 주세요.")
 //    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
@@ -22,7 +22,8 @@ fun main() {
 // 사용자가 구입 금액 입력 함수
 fun purchaseMoney(): Int {
     println("구입금액을 입력해 주세요.")
-    val money = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+    val money = Console.readLine().toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
     if (money < 1000 || money % 1000 != 0) {
         throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
     }
@@ -42,3 +43,17 @@ fun printLotto(lotto: List<List<Int>>) {
     lotto.forEach { println(it) }
 }
 
+// 당첨 번호 입력하는 함수
+fun winningNumber(): List<Int> {
+    println("당첨 번호를 입력해 주세요.")
+    return Console.readLine().split(",").map { it.trim().toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+    }
+}
+
+// 보너스 번호 입력하는 함수
+fun bonusNumber(): Int {
+    println("보너스 번호를 입력해 주세요.")
+    return Console.readLine().toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+}
