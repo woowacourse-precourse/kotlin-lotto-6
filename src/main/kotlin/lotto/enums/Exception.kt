23c903aa5db929809a53fb7100fc4514bdbe85ff
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-06 18:57:53 +0900

    Refactor(renaming)
    어색한 문구 수정

--- src/main/kotlin/lotto/enums/Exception.kt
+++ src/main/kotlin/lotto/enums/Exception.kt
@@ -9,5 +9,5 @@ enum class Exception(val message: String) {
     INVALID_COUNT("쉼표로 구분된 6개의 숫자를 입력해주세요."),
     INVALID_CHARACTER("잘못된 입력입니다."),
     INVALID_RANGE_NUMBER("1부터 45 사이의 숫자를 입력해주세요."),
-    DUPLICATED_BONUS_NUMBER("보너스 번호는 당첨 번호가 아니어야 합니다.");
+    DUPLICATED_BONUS_NUMBER("당첨번호와 중복 됩니다");
 }
