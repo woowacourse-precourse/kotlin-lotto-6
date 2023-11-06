1081cefbb9118070714e24149ae073584d26a029
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-06 18:14:34 +0900

    Feat(Add print message)
    당첨번호 입력해주세요와 보너스번호 입력해주세요 라는 메세지를 출력하는 기능을 추가하였습니다

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -7,6 +7,14 @@ class LottoView {
         println(OutPut.PLEASE_INPUT_AMOUNT.message)
     }
 
+    fun printEnterWinningNumberMessage() {
+        println(OutPut.PLEASE_INPUT_WINNING_NUMBER.message)
+    }
+
+    fun printEnterBonusNumberMessage() {
+        println(OutPut.PLEASE_INPUT_BOUNS_NUMBER.message)
+    }
+
     fun displayLottoNumbers(lottoNumbers: List<List<Int>>) {
         println("\n${lottoNumbers.size} ${OutPut.PURCHASED.message}")
         lottoNumbers.forEach { numbers ->
