7f3452d8269a7924c0f006d257d4da335b2368db
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 04:34:57 +0900

    Refactor: 구매 금액 입력 요구 문구 상수로 변경

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -3,7 +3,11 @@ package lotto
 class OutputView {
 
     fun printRequestInputPurchaseAmount() {
-        println("구입금액을 입력해 주세요.")
+        println(REQUEST_INPUT_PURCHASE_AMOUNT)
+    }
+
+    companion object {
+        const val REQUEST_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     }
 
 }
