5d43b855280e902d3bbb6b3e3be990eb35959789
Hogu59 <dbstjdguswkd@naver.com>
2023-11-07 01:45:32 +0900

    style: delete unused lines and imports

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -3,7 +3,6 @@ package lotto
 import camp.nextstep.edu.missionutils.test.Assertions
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
