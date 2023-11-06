ff606e1a64ba5c6f1e0ab53d4a6f5a87651dd009
hongjeZZ <ajyong316@gmail.com>
2023-11-06 20:57:32 +0900

    feat: 로또 번호 검증 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,6 +3,8 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == numbers.size)
+        require(numbers.all { number -> number in 1..45 })
     }
 
     fun hasNumber(number: Int): Boolean = numbers.contains(number)
