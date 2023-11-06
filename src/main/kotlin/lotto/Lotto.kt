99e45c28ff71f4c9ea7a94fcb216b323f303148b
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-11-06 23:41:43 +0900

    Feat : Lotto 객체 구현 로또 함수 - 당첨번호 개수 - 로또 출력
    예외처리
    -길이
    -중복

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,36 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
-        require(numbers.size == 6)
+        vaildateLength()
+        validateDuplication()
     }
 
-    // TODO: 추가 기능 구현
+    private fun vaildateLength() {
+        require(numbers.size == 6) { SIZEERROR }
+    }
+
+    private fun validateDuplication() {
+        require(numbers.size == numbers.distinct().count()) { ERRORDUPLICATION }
+    }
+
+    fun matchingNum(winningNumber: List<Int>): Int {
+        var result = 0
+        for (num in winningNumber) {
+            if (numbers.contains(num)) result++
+        }
+        return result
+    }
+
+    fun printLotto() {
+        println(numbers.sorted())
+    }
+
+    companion object {
+        const val SIZE = 6
+        const val SIZEERROR = "ERROR: 로또 번호를 ${SIZE} 보다 적거나 많이 입력했습니다.."
+        const val ERRORDUPLICATION = "ERROR: 입력하신 로또 번호는 중복이 있습니다."
+    }
+
 }
