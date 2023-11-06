581f0ac25ea40d4715df9443775d7901d6f10e71
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 07:09:25 +0900

    Feat: 로또 번호 유효성 검사 기능(중복된 숫자가 있을 때)

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,11 +4,17 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) { "$ERROR $LOTTO_NUMBER_ONLY_SIX_DIGITS" }
+        require(checkDuplicateNumbers()) { "$ERROR $LOTTO_NUMBER_CANNOT_DUPLICATED" }
+    }
+
+    private fun checkDuplicateNumbers(): Boolean {
+        return numbers.distinct().size == numbers.size
     }
 
     companion object {
         const val ERROR = "[ERROR]"
         const val LOTTO_NUMBER_ONLY_SIX_DIGITS = "로또 번호는 6자리여야 합니다."
+        const val LOTTO_NUMBER_CANNOT_DUPLICATED = "로또 번호는 중복될 수 없습니다."
     }
 
 }
