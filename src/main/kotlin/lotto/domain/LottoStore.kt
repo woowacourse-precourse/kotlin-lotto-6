e88837d98682e3e98c705254a46ed362b219a4d7
bak <bak4017@gmail.com>
2023-11-06 13:27:39 +0900

    test(lotto): LottoStore 테스트 추가

--- src/main/kotlin/lotto/domain/LottoStore.kt
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -1,12 +1,35 @@
 package lotto.domain
 
 import lotto.validator.PurchasePriceValidator
+import camp.nextstep.edu.missionutils.Randoms
 
 class LottoStore(private val value: String) {
     init {
         PurchasePriceValidator(value)
     }
+
+    fun sellTickets(): List<Lotto> {
+        val tickets = mutableListOf<Lotto>()
+        for (i in 0 until getNumberOfTickets()) {
+            tickets.add(Lotto(generateNumbers()))
+        }
+
+        return tickets
+    }
+
     fun getNumberOfTickets(): Int {
         return value.toInt() / 1000
+    }
+
+    private fun generateNumbers(): List<Int> {
+        val numbers = ArrayList<Int>()
+        Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT)
+        return numbers.sorted()
+    }
+
+    companion object {
+        const val START_INCLUSIVE = 1
+        const val END_INCLUSIVE = 9
+        const val COUNT = 6
     }
 }
--- src/test/kotlin/lotto/domain/LottoStoreTest.kt
+++ src/test/kotlin/lotto/domain/LottoStoreTest.kt
@@ -1,13 +1,22 @@
 package lotto.domain
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class LottoStoreTest {
     @Test
-    fun `티켓 수 반환 테스트`() {
+    fun `getNumberOfTickets가 티켓 수를 잘 반환하는지`() {
         val lottoStore = LottoStore("5000")
         val numberOfTickets = lottoStore.getNumberOfTickets()
         assertEquals(5, numberOfTickets)
+    }
+
+    @Test
+    fun `sellTickets가 티켓을 잘 만드는지`() {
+        val lottoStore = LottoStore("5000")
+        val tickets = lottoStore.sellTickets()
+
+        assertEquals(5, tickets.size)
     }
 }
