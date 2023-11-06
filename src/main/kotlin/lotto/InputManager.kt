50b975b11831e4563c53ed882f3d72119d0d30e8
LEE <tpqms775@naver.com>
2023-11-06 21:27:59 +0900

    refactor : 당첨번호 반환값 Int로 수정

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -7,9 +7,10 @@ class InputManager {
         println("구입금액을 입력해 주세요.")
         return Console.readLine()
     }
-    fun inputWinningNumber(): List<String> {
+    fun inputWinningNumber(): List<Int> {
         println("당첨 번호를 입력해 주세요.")
-        return Console.readLine().split(",")
+        val nums = Console.readLine().split(",")
+        return nums.map { it.toInt() }
     }
     fun inputBonusNumber(): String {
         println("보너스 번호를 입력해 주세요.")
