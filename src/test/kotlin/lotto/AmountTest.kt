4810694c2114799ae925326ab6e11c27f5d0f3c2
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 04:43:59 +0900

    Test: 구입 금액 입력 시 0 이하의 숫자를 입력했을 때 테스트 구현

--- src/test/kotlin/lotto/AmountTest.kt
+++ src/test/kotlin/lotto/AmountTest.kt
@@ -3,13 +3,21 @@ package lotto
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 class AmountTest {
     
     @ParameterizedTest
     @ValueSource(strings = ["asd", "2000ab"])
     fun `숫자가 아닌 문자를 입력했을 때`(amount: String) {
+        assertThrows<IllegalArgumentException> {
+            Amount(amount)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "-10000"])
+    fun `0이하의 숫자를 입력했을 때`(amount: String) {
         assertThrows<IllegalArgumentException> {
             Amount(amount)
         }
