8d7a713b9dd84f08516d00d422dd633f6239a78f
sunkk8482 <sunkk8482@naver.com>
2023-11-06 14:03:37 +0900

    Test : 각 등수별 테스트
    각 등수별로 적절한 값을 주었을 때 제대로 나오는지

--- src/test/kotlin/lotto/domain/WinningCheckTest.kt
+++ src/test/kotlin/lotto/domain/WinningCheckTest.kt
@@ -6,13 +6,47 @@ import org.assertj.core.api.Assertions.assertThat
 class WinningCheckTest {
 
     @Test
-    fun gradeCheck() {
+    fun gradeFirstCheck() {
         val testList = mutableListOf<Lotto>()
         testList.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
         val testCheck = WinningCheck()
         val testGrade = testCheck.numbersCheck(testList,listOf(1,2,3,4,5,6),7)
-        assertThat(testGrade[0].correct).isEqualTo(6)
+        assertThat(testGrade[0]).isEqualTo(Grade.FIRST)
     }
 
+    @Test
+    fun gradeSecondCheck() {
+        val testList = mutableListOf<Lotto>()
+        testList.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val testCheck = WinningCheck()
+        val testGrade = testCheck.numbersCheck(testList,listOf(1,2,3,4,5,8),6)
+        assertThat(testGrade[0]).isEqualTo(Grade.SECOND)
+    }
+
+    @Test
+    fun gradeThirdCheck() {
+        val testList = mutableListOf<Lotto>()
+        testList.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val testCheck = WinningCheck()
+        val testGrade = testCheck.numbersCheck(testList,listOf(1,2,3,4,5,8),9)
+        assertThat(testGrade[0]).isEqualTo(Grade.THIRD)
+    }
+    @Test
+    fun gradeFourthCheck() {
+        val testList = mutableListOf<Lotto>()
+        testList.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val testCheck = WinningCheck()
+        val testGrade = testCheck.numbersCheck(testList,listOf(1,2,3,4,9,10),8)
+        assertThat(testGrade[0]).isEqualTo(Grade.FOURTH)
+    }
+
+    @Test
+    fun gradeFifthCheck() {
+        val testList = mutableListOf<Lotto>()
+        testList.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
+        val testCheck = WinningCheck()
+        val testGrade = testCheck.numbersCheck(testList,listOf(1,2,3,10,11,12),7)
+        assertThat(testGrade[0]).isEqualTo(Grade.FIFTH)
+    }
 
 }
