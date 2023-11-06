f9ca9afe6a4cd8abafe076f5cf112aaed35100ba
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-06 18:12:46 +0900

    당첨 표기 방식 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,40 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == 6)
+        require(numbers.all { it in 1..45 })
     }
 
-    // TODO: 추가 기능 구현
+    fun calculateResult(winningNumbers: List<Int>, bonusNumber: Int): Rank {
+        val matchCount = numbers.intersect(winningNumbers).size
+        val bonusMatch = numbers.contains(bonusNumber)
+        return Rank.findRank(matchCount, bonusMatch)
+    }
+    fun getSortedNumbers(): List<Int> {
+        return numbers.sorted()
+    }
+
+    fun matchCount(winningNumbers: List<Int>): Int {
+        return getSortedNumbers().intersect(winningNumbers).size
+    }
+}
+
+enum class Rank(//등수 조건 설정
+    val matchCount: Int,
+    val prizeMoney: Int,
+    val needBonus: Boolean = false
+) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000), ;
+
+    companion object {
+        fun findRank(matchCount: Int, bonusMatch: Boolean): Rank {
+            return values().find { it.matchCount == matchCount && it.needBonus == bonusMatch }
+                ?: values().find { it.matchCount == matchCount && it != SECOND }
+                ?: FIFTH // 기본 값으로 FIFTH를 반환합니다.
+        }
+    }
 }
