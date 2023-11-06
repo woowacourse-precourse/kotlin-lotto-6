fd85e9ddcf309db6aa2df5d9e8abc16ab47dc7b2
seoyeon <seoyeonn051@gmail.com>
2023-11-06 10:54:51 +0900

    fix: validateInputLottoNum 기능 추가
    validateCommaSeparatedNumbers 함수 생성
    companion object 코드 위치 변경

--- src/main/kotlin/lotto/model/validation/InputValidation.kt
+++ src/main/kotlin/lotto/model/validation/InputValidation.kt
@@ -3,14 +3,6 @@ package lotto.model.validation
 import lotto.model.LottoNumValidation
 
 class InputValidation {
-
-    companion object {
-        private const val DIVISION_NUM = 1000
-        const val DIVISION_BY_THOUSAND_ERROR = "[ERRPR] 로또는 ${DIVISION_NUM}원 단위로 구매 가능합니다."
-        const val INPUT_ZERO_AMOUNT = "[ERRPR] 로또 구매 금액으로 0을 입력할 수 없습니다."
-        const val INPUT_NEGATIVE_AMOUNT = "[ERRPR] 로또 구매 금액으로 0을 입력할 수 없습니다."
-    }
-
     fun validateLottoPurchaseAmount(amount: Int) {
         isLottoPurchaseAmountDivisibleByThousand(amount)
         isLottoPurchaseAmountNotZero(amount)
@@ -22,10 +14,29 @@ class InputValidation {
     }
 
     private fun isLottoPurchaseAmountNotZero(amount: Int) {
-        require(amount != 0) { INPUT_ZERO_AMOUNT }
+        require(amount != 0) { INPUT_ZERO_AMOUNT_ERROR }
     }
 
     private fun isLottoPurchaseAmountNotPositive(amount: Int) {
-        require(amount > 0) { INPUT_NEGATIVE_AMOUNT }
+        require(amount > 0) { INPUT_NEGATIVE_AMOUNT_ERROR }
+    }
+
+    fun validateInputLottoNum(inputNumbers: String) {
+        validateCommaSeparatedNumbers(inputNumbers)
+    }
+
+    private fun validateCommaSeparatedNumbers(inputNumbers: String) {
+        val numbersRegex = Regex("^(\\d+,)*\\d+$")
+
+        require(numbersRegex.matches(inputNumbers)) { SEPARATE_BY_COMMA_ERROR }
+    }
+
+    companion object {
+        private const val DIVISION_NUM = 1000
+        const val DIVISION_BY_THOUSAND_ERROR = "[ERROR] 로또는 ${DIVISION_NUM}원 단위로 구매 가능합니다."
+        const val INPUT_ZERO_AMOUNT_ERROR = "[ERROR] 로또 구매 금액으로 0을 입력할 수 없습니다."
+        const val INPUT_NEGATIVE_AMOUNT_ERROR = "[ERROR] 로또 구매 금액으로 0을 입력할 수 없습니다."
+
+        const val SEPARATE_BY_COMMA_ERROR = "로또 당첨 숫자는 쉼표(,)로 구분되어야 합니다."
     }
 }
--- src/main/kotlin/lotto/model/validation/LottoValidation.kt
+++ src/main/kotlin/lotto/model/validation/LottoValidation.kt
@@ -19,7 +19,6 @@ class LottoNumValidation {
         require(input.all { it in LottoNumbersFactory.LOTTO_START_NUM..LottoNumbersFactory.LOTTO_START_NUM }) { LOTTO_NUM_RANGE_ERROR }
     }
 
-
     companion object {
         const val LOTTO_NUM_SIZE_ERROR = "[ERROR] 로또 번호의 숫자의 개수는 6개입니다."
         const val DUPLICATE_LOTTO_NUM_ERROR = "[ERROR] 로또 번호는 중복될 수 없습니다."
