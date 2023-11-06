d830d3c5f8074313610e508c10029e6547019d00
DJ_Lumiere <djminstar573@gmail.com>
2023-11-06 19:47:18 +0900

    Update Application.kt
    로또 구입 금액 입력받는 로직, 1000의 배수가 아니면 예외처리 후 다시 입력 받게 하는 로직 추기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,6 +7,30 @@ fun pickLottoNumbers(): Lotto {
     return Lotto(numbers)
 }
 
+fun isValidInteger(digits:String):Boolean{
+    return digits.all{it.isDigit()}
+}
+
+fun isPriceMultipleOf1000(value:Int):Boolean{
+    return value % 1000 == 0
+}
+
+fun isPriceValid(digits:String):Boolean{
+    if (!isValidInteger(digits)){
+        return false
+    }
+    val value = digits.toInt()
+    return isPriceMultipleOf1000(value)
+}
+
+fun lottoPayment():Int{
+    var digits = Console.readLine()
+    while (!isPriceValid(digits)){
+        digits = Console.readLine()
+    }
+    return digits.toInt()
+}
+
 fun main() {
     TODO("프로그램 구현")
 }
