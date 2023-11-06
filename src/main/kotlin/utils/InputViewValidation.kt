1a1cf11db99de665722a2a78ac496da70901c568
Kim Gimun <kmkim7575@gmail.com>
2023-11-06 18:49:28 +0900

    feat: add validation of money input

--- src/main/kotlin/utils/InputViewValidation.kt
+++ src/main/kotlin/utils/InputViewValidation.kt
@@ -2,4 +2,10 @@ package lotto.utils
 
 object InputViewValidation {
 
+    fun isUserAmountValid(userInput: String): Boolean = try {
+
+        true
+    } catch (e: NumberFormatException) {
+        false
+    }
 }
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,23 +1,37 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import constants.ExceptionMessages.EXCEPTION_PURCHASE_DIVISION
+import constants.ExceptionMessages.EXCEPTION_PURCHASE_MINUS
+import constants.ExceptionMessages.EXCEPTION_PURCHASE_STRING
 import constants.ExceptionMessages.EXCEPTION_UNEXPECTED
 import lotto.constants.Constants.INPUT_PURCHASE_AMOUNT
+import lotto.utils.InputViewValidation
 
 object InputView {
 
-    fun inputUserAmount(): Int {
+    fun inputPurchaseAmount(): Int {
         printStepMessage(INPUT_PURCHASE_AMOUNT)
         return getUserAmount()
     }
 
     private fun getUserAmount(): Int = try {
-        val amount = Console.readLine().toInt()
-
-        amount
+        val userInput = Console.readLine()
+        validateUserAmount(userInput)
+        userInput.toInt()
     } catch (e: IllegalArgumentException) {
         printErrorMessage(e.message ?: EXCEPTION_UNEXPECTED)
         getUserAmount()
+    }
+
+    private fun validateUserAmount(userInput: String) {
+        try {
+            val amount = userInput.toInt()
+            if (amount % 1000 != 0) throw IllegalArgumentException(EXCEPTION_PURCHASE_DIVISION)
+            if (amount < 0) throw IllegalArgumentException(EXCEPTION_PURCHASE_MINUS)
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(EXCEPTION_PURCHASE_STRING)
+        }
     }
 
     private fun printStepMessage(message: String) {
