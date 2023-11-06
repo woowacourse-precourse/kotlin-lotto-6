34178398a3bdbc5a367e09ce5d9bdba7eda4c0e8
hjw0623 <117283878+hjw0623@users.noreply.github.com>
2023-11-07 01:04:11 +0900

    feat: 당첨 여부와 등수를 판별하는 기능 추가

--- src/main/kotlin/lotto/Prize.kt
+++ src/main/kotlin/lotto/Prize.kt
@@ -1,5 +1,6 @@
 package lotto
 
+
 enum class Prize(val matchCount: Int, val prize: Int, val bonus: Boolean) {
     FIRST(6, 2000000_000, false),
     SECOND(5, 30000000, true),
@@ -7,4 +8,41 @@ enum class Prize(val matchCount: Int, val prize: Int, val bonus: Boolean) {
     FOURTH(4, 50000, false),
     FIFTH(3, 5000, false),
     MISS(0, 0, false);
+}
+
+
+
+fun getMatchCount(userLotto: Lotto, lotto: Lotto, bonusNumber: Int ): Pair<Int, Boolean> {
+    var matchCount = 0
+    var bonus = false
+    val numbers = userLotto.getNumbers()
+    val userLottoNumbers = lotto.getNumbers()
+    for (number in numbers) {
+        if (userLottoNumbers.contains(number)) {
+            matchCount++
+        }
+    }
+    if (numbers.contains(bonusNumber)) {
+        bonus = true
+    }
+    return Pair(matchCount, bonus)
+}
+fun getPrize(prizeList: Pair<Int,Boolean>): Prize {
+
+    if (prizeList.first == 3) {
+        return Prize.FIFTH
+    }
+    if (prizeList.first == 4) {
+        return Prize.FOURTH
+    }
+    if (prizeList.first == 5 && !prizeList.second) {
+        return Prize.THIRD
+    }
+    if (prizeList.first == 5 && prizeList.second) {
+        return Prize.SECOND
+    }
+    if (prizeList.first == 6) {
+        return Prize.FIRST
+    }
+    return Prize.MISS
 }
