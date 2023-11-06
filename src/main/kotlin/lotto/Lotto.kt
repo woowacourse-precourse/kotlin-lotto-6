ce00ce65a724dc191cfde495b012ccf1508c7751
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-11-06 22:41:01 +0900

    쉼표로 숫자 구분

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -8,4 +8,7 @@ class Lotto(private val numbers: List<Int>) {
     fun matchCount(winningNumbers: List<Int>): Int {
         return numbers.intersect(winningNumbers).size
     }
+    override fun toString(): String {
+        return numbers.joinToString(separator = ", ", prefix = "[", postfix = "]")
+    }
 }
