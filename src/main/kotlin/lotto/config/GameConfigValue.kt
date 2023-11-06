3a34ab2f2ea3f9da231d0df033294ba0c8a50319
applepepsi <dlaejrtnz@gmail.com>
2023-11-07 04:07:13 +0900

    feat(cinfig/GameConfigValue): Adding constants

--- src/main/kotlin/lotto/config/GameConfigValue.kt
+++ src/main/kotlin/lotto/config/GameConfigValue.kt
@@ -5,4 +5,8 @@ object GameConfigValue {
     const val PURCHASE_AMOUNT_UNIT=1000
     const val MINIMUM_LOTTO_NUMBER=1
     const val MAXIMUM_LOTTO_NUMBER=45
+    const val SIX_NUMBER_CORRECT=6
+    const val FIVE_NUMBER_CORRECT=5
+    const val FOUR_NUMBER_CORRECT=4
+    const val THREE_NUMBER_CORRECT=3
 }
