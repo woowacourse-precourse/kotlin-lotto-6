3ef571be9272a460599fb863b11bfabe8755623d
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-06 15:08:35 +0900

    feat: 당첨 번호 입력 메시지 출력 기능 작성

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -12,8 +12,11 @@ class OutputView {
 
     fun printAppendLine() = println()
 
+    fun printEnterWinningNumberMessage() = println(RequestType.ENTER_WINNING_NUMBERS.message)
+
     private enum class RequestType(val message: String) {
         PURCHASE_AMOUNT("구입금액을 입력해 주세요"),
         PURCHASED_N_ITEMS("개를 구매했습니다."),
+        ENTER_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     }
 }
