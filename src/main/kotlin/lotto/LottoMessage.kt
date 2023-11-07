bafe8355aa3d28f0856bfb8cdc25a1d52a7ff86a
jooyyoo <aliceyun9@naver.com>
2023-11-08 00:18:37 +0900

    [feat] 메세지 스트링 값 object 구현

---
+++ src/main/kotlin/lotto/LottoMessage.kt
@@ -0,0 +1,7 @@
+package lotto
+
+object LottoMessage {
+    const val PURCHASE_PRICE = "구입금액을 입력해 주세요."
+    const val INPUT_LOTTO_NUMBER =  "당첨 번호를 입력해 주세요."
+    const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+}
