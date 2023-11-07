76781c5996b01a9b884f3d22c9494b9fd3ebefed
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-08 08:14:23 +0900

    [feat] win rate && print result

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,11 +1,10 @@
 package lotto
 
-import kotlin.text.toInt
-import java.util.Random
 import lotto.Lotto
+import camp.nextstep.edu.missionutils.Randoms // pickUniqueNumbersInRange()
+import camp.nextstep.edu.missionutils.Console // readLine()
+import kotlin.math.*
 
-// import camp.nextstep.edu.missionutils.Randoms // pickUniqueNumbersInRange()
-// import camp.nextstep.edu.missionutils.Console // readLine()
 enum class Rank {
     FIRST,
     SECOND,
@@ -23,20 +22,28 @@ fun main() {
     while (price == 0) {
         try {
             println("구입금액을 입력해 주세요.")
-            inputPrice = readLine()!!
+            inputPrice = Console.readLine()
             price = priceCheck(inputPrice)
         } catch (e: IllegalArgumentException) {
             println(e)
         }
     }
+    println()
 
     var lottoList = mutableListOf<Lotto>()
-    val randomValues = listOf<Int>(1,2,3,4,5,6)
 
     for(i in 1..price / 1000) {
-        lottoList.add(Lotto(randomValues))
+        val randomPickNum = makeLottoNum()
+        randomPickNum.sorted()
+        lottoList.add(Lotto(randomPickNum))
     }
     
+    println("${price / 1000}개를 구매했습니다.")
+    for (lotto in lottoList) {
+        println(lotto.getNum())
+    }
+    println()
+
     lateinit var inputWinNum: String
     lateinit var inputBonusNum: String
     lateinit var winNumList: List<Int>
@@ -46,26 +53,57 @@ fun main() {
     while (checkWinNum == 0) {
         try {
             println("당첨 번호를 입력해 주세요.")
-            inputWinNum = readLine()!!
+            inputWinNum = Console.readLine()
             winNumList = winNumCheck(inputWinNum)
             checkWinNum = 1
+            println()
         } catch (e: IllegalArgumentException) {
             println(e)
-        }
+        } 
     }
     
     while (bonusNum == 0) {
         try {
             println("보너스 번호를 입력해 주세요.")
-            inputBonusNum = readLine()!!
-            bonusNum = bonusNumCheck(inputBonusNum)
+            inputBonusNum = Console.readLine()
+            bonusNum = bonusNumCheck(inputBonusNum,winNumList)
+            println()
         } catch (e: IllegalArgumentException) {
             println(e)
         }
     }
 
     val result = Array(6) { 0 }
+    
+    for (lotto in lottoList) {
+        result[lotto.compareNumber(winNumList, bonusNum)] += 1    
+    }
 
+    val totalRevenue = calRevenue(result)
+    
+    val rate = (totalRevenue * 100) / price
+
+    println("당첨통계\n---")
+    println("3개 일치 (5,000원) - ${result[4]}개")
+    println("4개 일치 (50,000원) - ${result[3]}개")
+    println("5개 일치 (1,500,000원) - ${result[2]}개")
+    println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[1]}개")
+    println("6개 일치 (2,000,000,000원) - ${result[0]}개")
+    println("총 수익률은 ${round(rate * 10) / 10}%입니다.")
+
+}
+
+fun calRevenue(result: Array<Int>): Double {
+
+    var total: Double = 0.0
+
+    total += result[0] * 2000000000
+    total += result[1] * 30000000
+    total += result[2] * 1500000
+    total += result[3] * 50000
+    total += result[4] * 5000
+
+    return total
 }
 
 fun winNumCheck(inputWinNum: String): List<Int> {
@@ -76,15 +114,23 @@ fun winNumCheck(inputWinNum: String): List<Int> {
         if (num < 1 || num > 45 || winNumList.size != 6) {
             throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
         }
+
+        if (winNumList.size != winNumList.toSet().size) {
+            throw IllegalArgumentException("[ERROR] 로또 번호가 중복되었습니다.")
+        }
     }
     
     return winNumList
 }
 
-fun bonusNumCheck(inputBonusNum: String): Int {
+fun bonusNumCheck(inputBonusNum: String, winNumList: List<Int>): Int {
+    try {
+        inputBonusNum.toInt()
+    } catch(e: NumberFormatException) {
+        throw IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
     val bonusNum = inputBonusNum.toInt()
-    
-    if (bonusNum < 1 || bonusNum > 45) {
+    if (bonusNum < 1 || bonusNum > 45 || winNumList.contains(bonusNum) ) {
         throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
     }
     
@@ -93,11 +139,42 @@ fun bonusNumCheck(inputBonusNum: String): Int {
 
 fun priceCheck(inputPrice: String): Int {
     
-    val result =inputPrice.toInt()
-    if (result % 1000 == 0) {
-        return result
+    try {
+        inputPrice.toInt()
+    } catch(e: NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 구입금액을 잘못 입력하였습니다.")
+    }
+
+    if (inputPrice.toInt() % 1000 == 0) {
+        return inputPrice.toInt()
     } else {
         throw IllegalArgumentException("[ERROR] 구입금액을 잘못 입력하였습니다.")
+    }
+}
+
+fun makeLottoNum(): List<Int> {
+    
+    lateinit var randomPickNum: List<Int>
+    var success = 0
+    while (success == 0) {
+        try {
+            randomPickNum = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            success = duplCheck(randomPickNum)
+            
+        } catch (e: IllegalStateException) {
+            println(e)
+        }
+        
+    }
+
+    return randomPickNum
+}
+
+fun duplCheck(randomPickNum: List<Int>): Int {
+    if (randomPickNum.size == randomPickNum.toSet().size) {
+        return 1
+    } else {
+        throw IllegalStateException("[ERROR] 로또번호가 중복되었습니다.")
     }
 }
 
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,17 +1,13 @@
 package lotto
 
 import lotto.Rank
-// val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-
-// 제공된 Lotto 클래스를 활용해 구현해야 한다.
-// numbers의 접근 제어자인 private을 변경할 수 없다.
-// Lotto에 필드를 추가할 수 없다.
-// Lotto의 패키지 변경은 가능하다.
-
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        if (numbers.size != numbers.toSet().size) {
+            throw IllegalArgumentException("[ERROR] 로또번호가 중복되었습니다.")
+        }
     }
 
     fun compareNumber(finalNum: List<Int>, bonusNum: Int): Int {
@@ -22,11 +18,10 @@ class Lotto(private val numbers: List<Int>) {
         when (compareSet.size) {
             9 -> return Rank.FIFTH.ordinal
             8 -> return Rank.FOURTH.ordinal
-            7 -> bonusCompare(numbers, bonusNum)
+            7 -> return bonusCompare(numbers, bonusNum)
             6 -> return Rank.FIRST.ordinal
             else -> return Rank.BOOM.ordinal
         }
-        return Rank.BOOM.ordinal
     }
 
     fun bonusCompare(numbers: List<Int>, bonusNum: Int): Int {
@@ -36,5 +31,9 @@ class Lotto(private val numbers: List<Int>) {
             return Rank.THIRD.ordinal
         }
         
+    }
+
+    fun getNum(): List<Int> {
+        return numbers;
     }
 }
