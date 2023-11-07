b143db3bcdb39f0e4e207a54a34aa085496ec769
gillyongs <khs20010327@gmail.com>
2023-11-08 00:05:48 +0900

    fix: 로또 객체에 중복 체크 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,15 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
+        val uniqueNumbers = HashSet<Int>()
+        for (number in numbers) {
+            require(number in 1..45) { "로또 번호는 1부터 45 사이의 숫자여야 합니다." }
+            require(uniqueNumbers.add(number)) { "로또 번호에 중복된 숫자가 있습니다." }
+        }
     }
 
-    // TODO: 추가 기능 구현
+    fun getNumbers(): List<Int> {
+        return numbers
+    }
 }
