14bf2b136cd79f30dd0cd037d6b4dd279cefc8be
lee989898 <wnsrud1352@gmail.com>
2023-11-07 17:56:42 +0900

    [feat] 로또 번호와 발행 로또 번호 비교하는 클래스 생성

---
+++ src/main/kotlin/lottonumbercomparator/LottoNumberComparatorImpl.kt
@@ -0,0 +1,11 @@
+package lottonumbercomparator
+
+import lotto.Lotto
+import lottoranking.LottoRanking
+import winningnumber.WinningNumber
+
+class LottoNumberComparatorImpl: LottoNumberComparator {
+    override fun compare(winningNumber: WinningNumber, lotto: Lotto): LottoRanking {
+        TODO("Not yet implemented")
+    }
+}
