7043cb344b79b874b4198d542212342624f9b0f7
wonjongin <wonggamggik@gmail.com>
2023-11-08 04:43:20 +0900

    코드 깔끔하게 만들기

--- src/main/kotlin/lotto/Calculator.kt
+++ src/main/kotlin/lotto/Calculator.kt
@@ -4,17 +4,17 @@ class Calculator() {
     fun calculateRateReturn(rank: List<Int>, money: Int): String {
 
         val returnMoney = (PriceHowManyMatch.일치3개.priceReturn() * rank[0]
-        + PriceHowManyMatch.일치4개.priceReturn() * rank[1]
-        + PriceHowManyMatch.일치5개.priceReturn() * rank[2]
-        + PriceHowManyMatch.일치5개보너스.priceReturn() * rank[3]
-        + PriceHowManyMatch.일치6개.priceReturn() * rank[4])
+                + PriceHowManyMatch.일치4개.priceReturn() * rank[1]
+                + PriceHowManyMatch.일치5개.priceReturn() * rank[2]
+                + PriceHowManyMatch.일치5개보너스.priceReturn() * rank[3]
+                + PriceHowManyMatch.일치6개.priceReturn() * rank[4])
 
-        return String.format("%.1f", returnMoney*100.0/money)
+        return String.format("%.1f", returnMoney * 100.0 / money)
     }
 
 }
 
-enum class PriceHowManyMatch(private val price:Int) {
+enum class PriceHowManyMatch(private val price: Int) {
     일치3개(5000),
     일치4개(50000),
     일치5개(1500000),
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -15,18 +15,18 @@ class Lotto(private val numbers: List<Int>) {
         return numbers.toSet().intersect(bonusNumber.bonusNumber.toSet()).size == 1
     }
 
-    fun divideStandard1to6(win: Int, bonus: Boolean) : Int {
+    fun divideStandard1to6(win: Int, bonus: Boolean): Int {
 
         val checkValue = listOf(win, bonus)
 
         return when (checkValue) {
-            listOf(3,false) -> 0
-            listOf(3,true) -> 0
-            listOf(3,false) -> 1
-            listOf(4,true) -> 1
-            listOf(5,false) -> 2
-            listOf(5,true) -> 3
-            listOf(6,false) -> 4
+            listOf(3, false) -> 0
+            listOf(3, true) -> 0
+            listOf(3, false) -> 1
+            listOf(4, true) -> 1
+            listOf(5, false) -> 2
+            listOf(5, true) -> 3
+            listOf(6, false) -> 4
             else -> 5
         }
     }
