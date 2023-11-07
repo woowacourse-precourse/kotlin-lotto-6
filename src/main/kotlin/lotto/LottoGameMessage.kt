dea1315d39e382e9d8a825cd212405f800707d2a
gretea5 <gretea4489@gmail.com>
2023-11-08 01:19:32 +0900

    feat: 입력 메시지 출력 테스트 구현

--- src/main/kotlin/lotto/LottoGameMessage.kt
+++ src/main/kotlin/lotto/LottoGameMessage.kt
@@ -5,7 +5,7 @@ class LottoGameMessage {
         const val PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요."
         const val LOTTO_NUMBER = "%d개를 구매했습니다."
         const val WINNING_NUMBER_INPUT = "당첨 번호를 입력해 주세요."
-        const val BONUS_NUMBER_INPUT = "보너스 번호를 입력해주세요."
+        const val BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요."
         const val WINNING_RESULT = "당첨 통계\n---"
         const val THREE_MATCHED = "3개 일치 (5,000원) - %d개"
         const val FOUR_MATCHED = "4개 일치 (50,000원) - %d개"
---
+++ src/test/kotlin/lotto/UserInputMessageTest.kt
@@ -0,0 +1,24 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class UserInputMessageTest: NsTest() {
+
+    @Test
+    fun `구입금액, 당첨 번호, 보너스번호 입력 메시지가 적절하게 나오는지 여부`() {
+        camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
+            run("5000", "1,2,3,4,5,6", "7")
+            assertThat(output()).contains(
+                "구입금액을 입력해 주세요.",
+                "당첨 번호를 입력해 주세요.",
+                "보너스 번호를 입력해 주세요.",
+            )
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+}
