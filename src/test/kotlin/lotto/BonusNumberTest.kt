fe94f8e8d0d22c0343194acbfda94a15bf8f42d9
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 14:13:03 +0900

    Test: 보너스 번호에 숫자가 아닌 문자를 입력했을 때 테스트 구현

---
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BonusNumberTest {
+
+    @Test
+    fun `보너스 번호에 숫자가 아닌 문자를 입력했을 때`() {
+        assertThrows<IllegalArgumentException> {
+            WinningNumber("a")
+        }
+    }
+
+}
