73daf7498dcd747ea8d1d502b83a76eac98655ce
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:40:32 +0900

    [refactor] LottoRanking rank -> correct로 프로퍼티명 변경

--- src/main/kotlin/lottoranking/LottoRanking.kt
+++ src/main/kotlin/lottoranking/LottoRanking.kt
@@ -1,9 +1,9 @@
 package lottoranking
 
-enum class LottoRanking(val rank: Int, val reward: Int, var count: Int) {
-    FIRST(rank = 1, reward = 2_000_000_000, count = 0),
-    SECOND(rank = 2, reward = 30_000_000, count = 0),
-    THIRD(rank = 3, reward = 1_500_000, count = 0),
-    FOURTH(rank = 4, reward = 50_000, count = 0),
-    FIFTH(rank = 5, reward = 5_000, count = 0),
+enum class LottoRanking(val correct: Int, val reward: Int, var count: Int) {
+    FIRST(correct = 6, reward = 2_000_000_000, count = 0),
+    SECOND(correct = 5, reward = 30_000_000, count = 0),
+    THIRD(correct = 5, reward = 1_500_000, count = 0),
+    FOURTH(correct = 4, reward = 50_000, count = 0),
+    FIFTH(correct = 3, reward = 5000, count = 0),
 }
