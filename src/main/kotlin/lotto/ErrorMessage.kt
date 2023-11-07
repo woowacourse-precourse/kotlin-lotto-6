becea4aac02c4c9a3cd046870a6aeb2139a57457
stopkite <stopkite98@gmail.com>
2023-11-08 08:32:00 +0900

    refactor(Validator): 유효성 검사 클래스 handler 인스턴스화

---
+++ src/main/kotlin/lotto/ErrorMessage.kt
@@ -0,0 +1,15 @@
+package lotto
+
+enum class ErrorMessage(val message: String) {
+    INVALID_PAYMENT_NUMBER("지불금액은 숫자로만 입력가능합니다."),
+    INVALID_MINIMUM_PAYMENT("최소 지불 금액은 1000원입니다."),
+    DIVIDE_TO_THOUSAND("지불 금액은 1000원 단위로 입력되어야 합니다."),
+    COMMA_START("처음에 콤마가 추가되었습니다. 콤마로 입력을 시작해서는 안됩니다."),
+    COMMA_END("콤마로 입력이 끝나서는 안됩니다."),
+    INVALID_WINNING_NUMBER("당첨번호는 숫자로만 입력되어야합니다."),
+    SAME_NUMBER("중복된 숫자가 있으면 안됩니다."),
+    LOTTO_NUMBER_COUNT("6개의 숫자가 입력되어야 합니다."),
+    NUMBER_RANGE("1과 45 사이의 숫자가 입력되어야 합니다."),
+    INVALID_BONUS_NUMBER("보너스 볼은 숫자로만 입력되어야합니다."),
+    SAME_WITH_WINNING_NUMBER("당첨 번호와 중복되어서는 안됩니다.")
+}
--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -1,9 +1,12 @@
 package lotto
 
 class Validator(private val input: String) {
-    fun isPaymentValid(): Boolean = ErrorHandler().checkWithExceptionHandler(this::validateIsNumber, INVALID_PAYMENT_NUMBER, ErrorType.NumberFormatException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateIsUpperToThousand, INVALID_MINIMUM_PAYMENT, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateIsCorrectUnit, DIVIDE_TO_THOUSAND, ErrorType.IllegalArgumentException)
+
+    private val errorHandler = ErrorHandler()
+
+    fun isPaymentValid(): Boolean = errorHandler.checkWithExceptionHandler(this::validateIsNumber, ErrorMessage.INVALID_PAYMENT_NUMBER.message, ErrorType.NumberFormatException) &&
+            errorHandler.checkWithExceptionHandler(this::validateIsUpperToThousand, ErrorMessage.INVALID_MINIMUM_PAYMENT.message, ErrorType.IllegalArgumentException) &&
+            errorHandler.checkWithExceptionHandler(this::validateIsCorrectUnit, ErrorMessage.DIVIDE_TO_THOUSAND.message, ErrorType.IllegalArgumentException)
 
     private fun validateIsNumber(): Boolean = input.toIntOrNull() is Int
 
@@ -18,12 +21,12 @@ class Validator(private val input: String) {
     }
 
     fun isWinningNumbersValid(): Boolean =
-            ErrorHandler().checkWithExceptionHandler(this::validateIsNotStartWithComma, COMMA_START_ERROR, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateIsNotEndWithComma, COMMA_END_ERROR, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateAreNumbers, INVALID_WINNING_NUMBER, ErrorType.NumberFormatException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateIsUnique, SAME_NUMBER_ERROR, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateNumberCount, LOTTO_NUMBER_COUNT_ERROR, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateNumbersRange, NUMBER_RANG_ERROR, ErrorType.IllegalArgumentException)
+        errorHandler.checkWithExceptionHandler(this::validateIsNotStartWithComma, ErrorMessage.COMMA_START.message, ErrorType.IllegalArgumentException) &&
+        errorHandler.checkWithExceptionHandler(this::validateIsNotEndWithComma, ErrorMessage.COMMA_END.message, ErrorType.IllegalArgumentException) &&
+        errorHandler.checkWithExceptionHandler(this::validateAreNumbers, ErrorMessage.INVALID_WINNING_NUMBER.message, ErrorType.NumberFormatException) &&
+        errorHandler.checkWithExceptionHandler(this::validateIsUnique, ErrorMessage.SAME_NUMBER.message, ErrorType.IllegalArgumentException) &&
+        errorHandler.checkWithExceptionHandler(this::validateNumberCount, ErrorMessage.LOTTO_NUMBER_COUNT.message, ErrorType.IllegalArgumentException) &&
+        errorHandler.checkWithExceptionHandler(this::validateNumbersRange, ErrorMessage.NUMBER_RANGE.message, ErrorType.IllegalArgumentException)
 
     private fun validateIsNotStartWithComma() :Boolean = !input.startsWith(",")
 
@@ -49,9 +52,9 @@ class Validator(private val input: String) {
         return winningNumbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER }
     }
 
-    fun isBonusBallValid(winningNumbers:List<Int>) = ErrorHandler().checkWithExceptionHandler(this::validateIsNumber, INVALID_BONUS_NUMBER, ErrorType.NumberFormatException) &&
-            ErrorHandler().checkWithExceptionHandler({validateIsBallSameWithWinningNumbers(winningNumbers)}, SAME_WITH_WINNING_NUMBER_ERROR, ErrorType.IllegalArgumentException) &&
-            ErrorHandler().checkWithExceptionHandler(this::validateBonusRange, NUMBER_RANG_ERROR, ErrorType.IllegalArgumentException)
+    fun isBonusBallValid(winningNumbers:List<Int>) = errorHandler.checkWithExceptionHandler(this::validateIsNumber, ErrorMessage.INVALID_BONUS_NUMBER.message, ErrorType.NumberFormatException) &&
+            errorHandler.checkWithExceptionHandler({validateIsBallSameWithWinningNumbers(winningNumbers)}, ErrorMessage.SAME_WITH_WINNING_NUMBER.message, ErrorType.IllegalArgumentException) &&
+            errorHandler.checkWithExceptionHandler(this::validateBonusRange, ErrorMessage.NUMBER_RANGE.message, ErrorType.IllegalArgumentException)
 
     private fun validateIsBallSameWithWinningNumbers(winningNumbers: List<Int>):Boolean {
         val bonusBall = input.toInt()
@@ -61,19 +64,8 @@ class Validator(private val input: String) {
     private fun validateBonusRange(): Boolean = input.toInt() in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
 
     companion object {
-        const val INVALID_PAYMENT_NUMBER = "지불금액은 숫자로만 입력가능합니다."
-        const val INVALID_MINIMUM_PAYMENT = "최소 지불 금액은 1000원입니다."
-        const val DIVIDE_TO_THOUSAND = "지불 금액은 1000원 단위로 입력되어야 합니다."
-        const val COMMA_START_ERROR = "처음에 콤마가 추가되었습니다. 콤마로 입력을 시작해서는 안됩니다."
-        const val COMMA_END_ERROR = "콤마로 입력이 끝나서는 안됩니다."
-        const val INVALID_WINNING_NUMBER = "당첨번호는 숫자로만 입력되어야합니다."
-        const val SAME_NUMBER_ERROR = "중복된 숫자가 있으면 안됩니다."
-        const val LOTTO_NUMBER_COUNT_ERROR = "6개의 숫자가 입력되어야 합니다."
-        const val NUMBER_RANG_ERROR = "1과 45 사이의 숫자가 입력되어야 합니다."
         const val MIN_LOTTO_NUMBER = 1
         const val MAX_LOTTO_NUMBER = 45
         const val LOTTO_NUMBER_COUNT = 6
-        const val INVALID_BONUS_NUMBER = "보너스 볼은 숫자로만 입력되어야합니다."
-        const val SAME_WITH_WINNING_NUMBER_ERROR = "당첨 번호와 중복되어서는 안됩니다."
     }
 }
