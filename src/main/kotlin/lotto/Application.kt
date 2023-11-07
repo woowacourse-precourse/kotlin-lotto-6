83dad7b22da017f28747d507ab4f1c115ff51764
juseong <wjdwntjd55@naver.com>
2023-11-07 22:55:14 +0900

    Feat : 로또 구입 금액 입력하는 기능

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,28 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("구입금액을 입력해 주세요.")
+    val purchaseAmount = readPurchaseAmount()
+    println()
+
 }
+
+fun readPurchaseAmount(): Int {
+    while (true) {
+        try {
+            val input = Console.readLine().toInt()
+            if (input % 1000 != 0) {
+                throw IllegalArgumentException("[ERROR] 유효한 금액을 입력하세요.")
+            }
+            return input
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 유효한 숫자를 입력하세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
