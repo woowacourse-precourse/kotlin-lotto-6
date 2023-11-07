fd96a4da8ddfc1b2d4341986955c1a4488a69569
bak <bak4017@gmail.com>
2023-11-07 16:31:48 +0900

    refactor(lotto): LottoRankTest 클래스 중복코드 제거

--- src/test/kotlin/lotto/domain/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -6,37 +6,36 @@ import org.junit.jupiter.api.Test
 class LottoRankTest {
     @Test
     fun `3개 미만 일치 시 없음`() {
-        val rank = LottoRank.getRank(2, false)
-        assertEquals(LottoRank.NONE, rank)
+        assertRank(2, false, LottoRank.NONE)
     }
 
     @Test
     fun `3개 일치 시 5등`() {
-        val rank = LottoRank.getRank(3, false)
-        assertEquals(LottoRank.FIFTH, rank)
+        assertRank(3, false, LottoRank.FIFTH)
     }
 
     @Test
     fun `4개 일치 시 4등`() {
-        val rank = LottoRank.getRank(4, false)
-        assertEquals(LottoRank.FOURTH, rank)
+        assertRank(4, false, LottoRank.FOURTH)
     }
 
     @Test
     fun `5개 일치하고 보너스볼 일치하지 않을 때 3등`() {
-        val rank = LottoRank.getRank(5, false)
-        assertEquals(LottoRank.THIRD, rank)
+        assertRank(5, false, LottoRank.THIRD)
     }
 
     @Test
     fun `5개 일치하고 보너스볼 일치할 때 2등`() {
-        val rank = LottoRank.getRank(5, true)
-        assertEquals(LottoRank.SECOND, rank)
+        assertRank(5, true, LottoRank.SECOND)
     }
 
     @Test
     fun `6개 일치 시 1등`() {
-        val rank = LottoRank.getRank(6, false)
-        assertEquals(LottoRank.FIRST, rank)
+        assertRank(6, false, LottoRank.FIRST)
+    }
+
+    private fun assertRank(matchCount: Int, matchBonus: Boolean, expectedRank: LottoRank) {
+        val rank = LottoRank.getRank(matchCount, matchBonus)
+        assertEquals(expectedRank, rank)
     }
 }
