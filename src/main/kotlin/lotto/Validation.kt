54dbadaa1d42c536733628c19449b157c193e50b
gaeun <geun5744@gmail.com>
2023-11-07 18:50:54 +0900

    [FEAT(Validation)] 당첨 번호 입력 검증 구현

--- src/main/kotlin/lotto/Validation.kt
+++ src/main/kotlin/lotto/Validation.kt
@@ -7,6 +7,8 @@ object Validation {
     private const val NUM_ERROR_MESSAGE = "숫자만 입력 가능합니다."
     private const val DIVIDE_1000_ERROR_MESSAGE = "1000원 단위로 로또 구매 가능합니다."
     private const val COMMA_ERROR_MESSAGE = "쉼표(,)를 기준으로 구분해주세요"
+    private const val NUM_AMOUNT_ERROR_MESSAGE = "6개의 숫자를 입력해주세요"
+    private const val NUM_RANGE_ERROR_MESSAGE  = "1에서 45 숫자만 입력 가능합니다."
 
     fun purchaseAmount(input: String) {
         checkIsDigit(input)
@@ -15,6 +17,13 @@ object Validation {
 
     fun answerNumber(input: String) {
         checkIsDigit(input)
+        checkContainComma(input)
+        checkNumAmount(input)
+        checkNumRange(input)
+    }
+
+    fun bonusNum(input: String) {
+
     }
 
     private fun checkIsDigit(input: String) {
@@ -31,8 +40,25 @@ object Validation {
         }
     }
 
-
-    fun bonusNum() {
-
+    private fun checkContainComma(input: String) {
+        require(!input.contains(",")) {
+            COMMA_ERROR_MESSAGE
+        }
     }
+
+    private fun checkNumAmount(input:String){
+        require(input.split(",").size!=6) {
+            NUM_AMOUNT_ERROR_MESSAGE
+        }
+    }
+
+    private fun checkNumRange(input: String) {
+        val answerNums = input.split(",").map { it.toInt() }
+        for(num in answerNums){
+            require(num !in 1..45){
+                NUM_RANGE_ERROR_MESSAGE
+            }
+        }
+    }
+
 }
