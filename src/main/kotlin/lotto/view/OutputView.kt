d8317be8d0a41966a6fd0973bac4c08b91e5a280
junyong <junyong008@gmail.com>
2023-11-07 10:30:55 +0900

    feat(OutputView): 당첨 결과 출력 기능 추가

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,14 +1,25 @@
 package lotto.view
 
 import lotto.domain.Lottos
+import lotto.domain.WinningRank
+import lotto.domain.WinningResult
 
 object OutputView {
 
     private const val OUTPUT_LOTTO_COUNT = "%d개를 구매했습니다."
+    private const val OUTPUT_TITLE_WINNING_RESULT = "당첨 통계"
+    private const val OUTPUT_WINNING_RESULT_SEPARATOR = "---"
 
     fun outputLottos(lottos: Lottos) {
         println()
         println(OUTPUT_LOTTO_COUNT.format(lottos.size()))
         println(lottos.joinToString(System.lineSeparator()))
+    }
+
+    fun outputWinningResult(winningResult: WinningResult) {
+        println()
+        println(OUTPUT_TITLE_WINNING_RESULT)
+        println(OUTPUT_WINNING_RESULT_SEPARATOR)
+        println(winningResult)
     }
 }
