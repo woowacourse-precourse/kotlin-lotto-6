23d0306bb6b0c2fddaaa6a427520feb4e6c98fa5
minuxx <minux8905@gmail.com>
2023-11-07 17:13:58 +0900

    test: 보너스 번호 범위 예외 처리 테스트 수정
    @ParameterizedTest, @ValueSource 적용
    - 범위 왼쪽, 오른쪽 테스트 케이스 모두 테스트할 수 있게끔 수정

--- src/test/kotlin/lotto/BonusTest.kt
+++ src/test/kotlin/lotto/BonusTest.kt
@@ -3,14 +3,14 @@ package lotto
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BonusTest {
 
-    @Test
-    fun `범위를 벗어난 보너스 번호인 경우 예외가 발생한다`() {
-        //given
-        val number = 46
-
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `범위를 벗어난 보너스 번호인 경우 예외가 발생한다`(number: Int) {
         //when
         val exception = assertThrows<IllegalArgumentException> { Bonus(number) }
 
