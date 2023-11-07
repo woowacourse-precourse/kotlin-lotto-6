03fe3d4a9d85aebde9b6905d209db4e699716e75
jklom0326 <jklom0326@gmail.com>
2023-11-08 00:18:15 +0900

    refactor: 당첨 등수를 확인하는 함수 분리
    - 매직넘버 상수화

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -7,10 +7,10 @@ class LottoMachine {
 
     fun start() {
         val payment = view.inputPayment()
-        val lottos = makeLotto(payment)
+        val lottos = makeLotto(payment.getPayment())
         view.printLotto(lottos)
         val inputLottoNumber = view.inputLottoNumber()
-        val inputBonusNumber = view.inputBonusLottoNumber()
+        val inputBonusNumber = view.inputBonusLottoNumber(inputLottoNumber)
 
         val prize = winningLotteryResult(lottos, inputLottoNumber, inputBonusNumber)
         val winRate = calculateWinRate(prize, lottos)
@@ -19,32 +19,42 @@ class LottoMachine {
         view.printWinRate(winRate)
     }
 
-    private fun winningLotteryResult(lottos: List<Lotto>, inputLottoNumber: Lotto, inputBonusNumber: Int): Prize {
+    private fun winningLotteryResult(lottos: List<Lotto>, inputLottoNumber: Lotto, inputBonusNumber: BonusNumber): Prize {
         val inputNumber = inputLottoNumber.getLottoNumbers()
         val prize = Prize()
         lottos.forEach { lotto ->
             val lottoNum = lotto.getLottoNumbers()
             val count = countMatchNumber(lottoNum, inputNumber)
-            when (count) {
-                6 -> prize.countPrize(Rank.FIRST_RANK)
-                5 -> {
-                    if (lottoNum.contains(inputBonusNumber)) {
-                        prize.countPrize(Rank.SECOND_RANK)
-                    } else {
-                        prize.countPrize(Rank.THIRD_RANK)
-                    }
-                }
-
-                4 -> prize.countPrize(Rank.FOURTH_RANK)
-                3 -> prize.countPrize(Rank.FIFTH_RANK)
-                else -> Rank.MISS
-            }
+            countPrize(count, prize, lottoNum, inputBonusNumber)
         }
         return prize
     }
 
+    private fun countPrize(
+        count: Int,
+        prize: Prize,
+        lottoNum: List<Int>,
+        inputBonusNumber: BonusNumber,
+    ) {
+        when (count) {
+            6 -> prize.countPrize(Rank.FIRST_RANK)
+            5 -> checkBonusNumber(lottoNum, inputBonusNumber.getBonusNumber(), prize)
+            4 -> prize.countPrize(Rank.FOURTH_RANK)
+            3 -> prize.countPrize(Rank.FIFTH_RANK)
+            else -> Rank.MISS
+        }
+    }
+
+    private fun checkBonusNumber(lottoNum: List<Int>, inputBonusNumber: Int, prize: Prize) {
+        if (lottoNum.contains(inputBonusNumber)) {
+            prize.countPrize(Rank.SECOND_RANK)
+        } else {
+            prize.countPrize(Rank.THIRD_RANK)
+        }
+    }
+
     private fun calculateWinRate(prize: Prize, lottos: List<Lotto>): Double {
-        return (prize.getPrizeMoney() / (lottos.size * 1000) * 100)
+        return (prize.getPrizeMoney() / (lottos.size * LOTTO_UNITS) * PERCENT)
     }
 
     private fun countMatchNumber(lottoNum: List<Int>, inputNumber: List<Int>): Int {
@@ -63,11 +73,19 @@ class LottoMachine {
     }
 
     private fun generateLotto(): Lotto {
-        val num = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        val num = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LOTTO_COUNT)
         return Lotto(num.sorted())
     }
 
     private fun countLotto(payment: Int): Int {
-        return payment / 1000
+        return payment / LOTTO_UNITS
+    }
+
+    companion object {
+        const val LOTTO_UNITS = 1000
+        const val PERCENT = 100
+        const val MIN_NUM = 1
+        const val MAX_NUM = 45
+        const val LOTTO_COUNT = 6
     }
 }
