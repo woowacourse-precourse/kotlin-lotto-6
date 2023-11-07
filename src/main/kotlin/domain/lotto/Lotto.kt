4552eed07e834fdfffd9e4ba3e916026dd6b6b41
Kim Gimun <kmkim7575@gmail.com>
2023-11-07 14:07:38 +0900

    feat: Lotto Test Case 충족을 위한 숫자 중복 방지 코드 추가

--- src/main/kotlin/domain/lotto/Lotto.kt
+++ src/main/kotlin/domain/lotto/Lotto.kt
@@ -4,6 +4,7 @@ class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6)
+        require(numbers.size == numbers.toSet().size)
     }
 
     fun sortedRandomNumbers()  = numbers.sorted().toMutableList()
