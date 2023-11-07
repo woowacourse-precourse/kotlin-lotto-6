3444328159ade7a7660206a66897a1e65b3f5197
junyong <junyong008@gmail.com>
2023-11-07 10:33:05 +0900

    refactor(InputView): 상수 이름 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
 
     private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
-    private const val INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요."
+    private const val INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요."
     private const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
 
     fun inputPurchaseAmount(): String {
@@ -13,8 +13,8 @@ object InputView {
         return Console.readLine()
     }
 
-    fun inputWinningLotto(): List<String> {
-        println(System.lineSeparator() + INPUT_WINNING_LOTTO)
+    fun inputWinningNumbers(): List<String> {
+        println(System.lineSeparator() + INPUT_WINNING_NUMBERS)
         return Console.readLine().split(",")
     }
 
