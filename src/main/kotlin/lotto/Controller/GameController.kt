9a107aab662af35e32cabf5484072e0105896e41
gillyongs <khs20010327@gmail.com>
2023-11-08 00:01:14 +0900

    feat: 로또 생성 구현

--- src/main/kotlin/lotto/Controller/GameController.kt
+++ src/main/kotlin/lotto/Controller/GameController.kt
@@ -8,6 +8,7 @@ import java.util.*
 class GameController {
     fun run() {
         val purchaseAmount = readPurchaseAmount()
+        val lottoTickets = LottoTicket.generateLottoTickets(purchaseAmount)
 
     }
 
@@ -26,4 +27,61 @@ class GameController {
         }
     }
 
+    private fun readWinningNumbers(): List<Int> {
+        while (true) {
+            try {
+                val winningNumberString = Input.readWinningNumbers()
+                val winningNumbers = ArrayList<Int>()
+                val uniqueNumbers = HashSet<Int>()
+                val trigger = validateWinningNumbers(winningNumberString, winningNumbers, uniqueNumbers)
+
+                if (winningNumbers.size == 6) {
+                    return winningNumbers
+                }
+                if (trigger == 1) {
+                    Error.notValidWinningNumberLength()
+                }
+            } catch (e: NumberFormatException) {
+                Error.NAN()
+            }
+        }
+    }
+
+    private fun validateWinningNumbers(
+            winningNumberString: String,
+            winningNumbers: MutableList<Int>,
+            uniqueNumbers: MutableSet<Int>
+    ): Int {
+        var trigger = 1
+        val winningNumberStringArray = winningNumberString.split(",".toRegex()).toTypedArray()
+        for (number in winningNumberStringArray) {
+            val num = number.toInt()
+            if (uniqueNumbers.contains(num)) {
+                Error.duplicateWinningNumber()
+                trigger = 0
+            } else if (num < 1 || num > 45) {
+                Error.notValidWinningNumber()
+                trigger = 0
+            } else {
+                winningNumbers.add(num)
+                uniqueNumbers.add(num)
+            }
+        }
+        return trigger
+    }
+
+    private fun readBonusNumber(): Int {
+        while (true) {
+            try {
+                val bonusNumberString = Input.readBonusNumber()
+                val bonusNumber = bonusNumberString.toInt()
+                if (1 <= bonusNumber && bonusNumber <= 45) {
+                    return bonusNumber
+                }
+                Error.notValidBonusNumber()
+            } catch (e: NumberFormatException) {
+                Error.NAN()
+            }
+        }
+    }
 }
