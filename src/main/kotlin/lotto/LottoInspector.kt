ff30cb2869f2a98b48872b5da7f4575f16648751
crAy <craycoding@gmail.com>
2023-11-07 13:32:07 +0900

    feat : add reward money enum class, lotto result print function

--- src/main/kotlin/lotto/LottoInspector.kt
+++ src/main/kotlin/lotto/LottoInspector.kt
@@ -15,6 +15,21 @@ class LottoInspector {
         return lottoResultList
     }
 
+    private fun printLottoResult(lottoResultList: Array<Int>) {
+        println("당첨 통계")
+        println("---")
+        println("${LottoMatched.THREE.matchedNumbers}개 일치 " +
+                "(${LottoRewardMoney.FIFTH.rewardMoney}) - ${lottoResultList[LottoAward.FIFTH.reward]}개")
+        println("${LottoMatched.FOUR.matchedNumbers}개 일치 " +
+                "(${LottoRewardMoney.FOURTH.rewardMoney}) - ${lottoResultList[LottoAward.FOURTH.reward]}개")
+        println("${LottoMatched.FIVE.matchedNumbers}개 일치 " +
+                "(${LottoRewardMoney.THIRD.rewardMoney}) - ${lottoResultList[LottoAward.THIRD.reward]}개")
+        println("${LottoMatched.FIVE.matchedNumbers}개 일치, 보너스 볼 일치 " +
+                "(${LottoRewardMoney.SECOND.rewardMoney}) - ${lottoResultList[LottoAward.SECOND.reward]}개")
+        println("${LottoMatched.SIX.matchedNumbers}개 일치 " +
+                "(${LottoRewardMoney.FIRST.rewardMoney}) - ${lottoResultList[LottoAward.FIRST.reward]}개")
+    }
+
     private fun checkLotto(lotto: Lotto, winningNumber: List<Int>, bonusNumber: Int): Int {
         val lottoNumber = lotto.getLottoNumber()
         val lottoResult = compareLotto(lottoNumber, winningNumber)
