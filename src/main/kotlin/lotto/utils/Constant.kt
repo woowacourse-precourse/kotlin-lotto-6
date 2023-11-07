16a37fc83aaf0e6a1a8c69148e6b2985ceb06c37
MinhoiKoo <minlove2013@gmail.com>
2023-11-07 12:38:35 +0900

    feat: 중복된 로또 숫자 존재시 예외 발생 로직 추가

--- src/main/kotlin/lotto/utils/Constant.kt
+++ src/main/kotlin/lotto/utils/Constant.kt
@@ -6,5 +6,6 @@ object Constant {
     const val MAX_LOTTO_NUMBER = 45
 
     const val INVALID_SIZE_EXCEPTION_MESSAGE = "[ERROR] 로또의 숫자 갯수는 6개여야 합니다!"
-
+    const val DUPLICATE_NUMBER_EXCEPTION_MESSAGE = "[ERROR] 중복된 숫자가 존재합니다"
+    const val NOT_DIGIT_EXCEPTION_MESSAGE = "[ERROR] 정수가 입력되지 않았습니다!"
 }
