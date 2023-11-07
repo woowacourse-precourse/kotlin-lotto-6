462fbbee69e948827e02cf844ad9fe2f008c9e03
stopkite <stopkite98@gmail.com>
2023-11-08 07:00:15 +0900

    feat(Lotto): 로또 클래스 검증로직 추가 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,8 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_NUMBER_COUNT) { LOTTO_NUMBER_ERROR }
+        require(numbers.toSet().size == LOTTO_NUMBER_COUNT) { SAME_NUMBER_ERROR }
+        require(numbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER }) { NUMBER_RANG_ERROR }
     }
 
     override fun toString(): String = numbers.joinToString(", ", "[", "]")
@@ -13,6 +15,10 @@ class Lotto(private val numbers: List<Int>) {
 
     companion object {
         const val LOTTO_NUMBER_ERROR = "로또에는 6개의 숫자가 입력되어야 합니다."
+        const val SAME_NUMBER_ERROR = "중복된 숫자가 있으면 안됩니다."
+        const val NUMBER_RANG_ERROR = "1과 45 사이의 숫자가 입력되어야 합니다."
         const val LOTTO_NUMBER_COUNT = 6
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
     }
 }
