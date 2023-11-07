8e53aad789f5e6f0083d3c4beb63d4242a689cf8
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:03:59 +0900

    [feat] "{발행한 로또 수량}개를 구매했습니다." 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,8 +2,13 @@ package view
 
 object OutputView {
     private const val PLEASE_ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+    private const val BOUGHT_NUMBER_OF_THEM = "개를 구매했습니다."
 
     fun pleaseEnterPurchaseAmount() {
         println(PLEASE_ENTER_PURCHASE_AMOUNT)
+    }
+
+    fun purchasedIssuedLottoTickets(lottoPurchaseAmount: Int) {
+        println("$lottoPurchaseAmount$BOUGHT_NUMBER_OF_THEM")
     }
 }
