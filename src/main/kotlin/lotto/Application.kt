fff71fbbb2523f1739782132bec8b0884cbb41b4
ii2001 <iicks2001@gmail.com>
2023-11-07 13:47:50 +0900

    feat: 당첨 번호 입력 함수 제작

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,6 +6,17 @@ fun main() {
     println("구입금액을 입력해 주세요.")
 
     val purchaseAmount = getPurchaseAmount()
+
+    val lottoNumbers = generateLottoNumbers(purchaseAmount)
+
+    println("${lottoNumbers.size}개를 구매했습니다.")
+    for (lottoNumber in lottoNumbers) {
+        println(lottoNumber)
+    }
+    println("당첨 번호를 입력해 주세요.")
+    getWinningNumbers()
+
+
 }
 
 fun generateLottoNumbers(purchaseAmount: Int): List<Lotto> {
@@ -43,4 +54,38 @@ fun inputPurchase(amount: Int): Int {
         return amount
     }
     return mutableAmount
+}
+
+fun getWinningNumbers(): List<Int> {
+    var numbers=listOf(0)
+    println("당첨 번호를 입력해 주세요.")
+    while (numbers==listOf(0)) {
+        numbers=inputWinningNumbers()
+    }
+    return numbers
+}
+
+fun inputWinningNumbers(): List<Int> {
+    try {
+        var numbers: List<Int>
+        val input = readLine()
+        numbers = input?.split(",")!!.mapNotNull { it.trim().toIntOrNull() }
+        validateLottoNumbers(numbers)
+        return numbers // 유효한 입력이 들어왔으면 루프를 종료
+    } catch (e: NumberFormatException) {
+        println("[ERROR] 잘못된 입력 형식입니다.")
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+    return listOf(0)
+}
+fun validateLottoNumbers(numbers: List<Int>) {
+    if (numbers.any { it < 1 || it > 45 } || numbers.size != 6) {
+        throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자 6개를 입력해야 합니다.")
+    }
+
+    val uniqueNumbers = numbers.toSet()
+    if (uniqueNumbers.size != 6) {
+        throw IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 포함되어 있습니다.")
+    }
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,12 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+    private val lottoNumbers = numbers
     init {
         require(numbers.size == 6)
     }
-
+    override fun toString(): String {
+        return lottoNumbers.toString()
+    }
     // TODO: 추가 기능 구현
 }
