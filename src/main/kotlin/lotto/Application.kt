a6449aeb498bebd238a0e24c5e43656b0aae1ddd
Nohsein <si2238@naver.com>
2023-11-07 12:41:37 +0900

    fix: 에외 테스트 실패
    잘못된 입력 허용 횟수 지정 후 초과 시 에러 메세지 후 종료
    모든 사용자 입력에 대해 적용

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.Constants.ALLOWED_ATTEMPTS_EXCEEDED
 import lotto.Constants.AMOUNT_ERROR
 import lotto.Constants.ASK_AMOUNT
 import lotto.Constants.ASK_BONUS_NUM
@@ -8,28 +9,35 @@ import lotto.Constants.ASK_WIN_NUM
 import lotto.Constants.BONUS_ERROR
 import lotto.Constants.COUNT
 import lotto.Constants.END
+import lotto.Constants.MAX_ATTEMPT
 import lotto.Constants.PRICE
 import lotto.Constants.START
 import lotto.Constants.WIN_NUM_ERROR
 
 fun main() {
-    val amount = askAmount()
-    val number = Purchase(amount).lottoNum()
-    val winNum = askWinNum()
-    val bonusNum = askBonusNum(winNum)
-    val winStat = mutableListOf<Int>(0, 0, 0, 0, 0, 0)
-    number.map { winStat[Lotto(it).isWin(winNum, bonusNum)]++ }
-    Statistics(winStat).stat(amount)
-
+    try {
+        val amount = askAmount(MAX_ATTEMPT)
+        val number = Purchase(amount).lottoNum()
+        val winNum = askWinNum(MAX_ATTEMPT)
+        val bonusNum = askBonusNum(winNum, MAX_ATTEMPT)
+        val winStat = mutableListOf<Int>(0, 0, 0, 0, 0, 0)
+        number.map { winStat[Lotto(it).isWin(winNum, bonusNum)]++ }
+        Statistics(winStat).stat(amount)
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
 
-fun askAmount(): Int {
+fun askAmount(chance: Int): Int {
     try {
         println(ASK_AMOUNT)
         return validAmount()
     } catch (e: Exception) {
-        println(AMOUNT_ERROR)
-        return askAmount()
+        if (chance > 1) {
+            println(e.message)
+            return askAmount(chance - 1)
+        }
+        return throw IllegalArgumentException(ALLOWED_ATTEMPTS_EXCEEDED)
     }
 }
 
@@ -43,13 +51,16 @@ fun validAmount(): Int {
     }
 }
 
-fun askWinNum(): List<Int> {
+fun askWinNum(chance: Int): List<Int> {
     try {
         println("\n" + ASK_WIN_NUM)
         return validWinNum()
-    } catch (e: Exception) {
-        println(WIN_NUM_ERROR)
-        return askWinNum()
+    } catch (e: IllegalArgumentException) {
+        if (chance > 1) {
+            println(e.message)
+            return askWinNum(chance - 1)
+        }
+        return throw IllegalArgumentException(ALLOWED_ATTEMPTS_EXCEEDED)
     }
 }
 
@@ -65,13 +76,16 @@ fun validWinNum(): List<Int> {
     }
 }
 
-fun askBonusNum(winNum: List<Int>): Int {
+fun askBonusNum(winNum: List<Int>, chance: Int): Int {
     try {
         println("\n" + ASK_BONUS_NUM)
         return validBonusNum(winNum)
-    } catch (e: Exception) {
-        println(BONUS_ERROR)
-        return askBonusNum(winNum)
+    } catch (e: IllegalArgumentException) {
+        if (chance > 1) {
+            println(e.message)
+            return askBonusNum(winNum, chance - 1)
+        }
+        return throw IllegalArgumentException(ALLOWED_ATTEMPTS_EXCEEDED)
     }
 }
 
--- src/main/kotlin/lotto/Constants.kt
+++ src/main/kotlin/lotto/Constants.kt
@@ -5,6 +5,7 @@ object Constants {
     const val START = 1
     const val END = 45
     const val COUNT = 6
+    const val MAX_ATTEMPT = 5
     const val FIRST_PLACE = 5
     const val SECOND_PLACE = 4
     const val THIRD_PLACE = 3
@@ -19,12 +20,13 @@ object Constants {
 
     const val ASK_AMOUNT = "구입금액을 입력해 주세요."
     const val AMOUNT_ERROR = "[ERROR] 1000원 단위의 숫자를 입력하세요.\n"
+    const val ALLOWED_ATTEMPTS_EXCEEDED = "[ERROR] 5회 이상 잘못 입력하셨습니다."
     const val PURCHASED = "개를 구매했습니다."
     const val ASK_WIN_NUM = "당첨 번호를 입력해 주세요."
     const val WIN_NUM_ERROR = "[ERROR] 1~45 범위의 숫자를 6개 입력해 주세요."
     const val ASK_BONUS_NUM = "보너스 번호를 입력해 주세요."
     const val BONUS_ERROR = "[ERROR] 당첨 번호를 제외한 1~45 범위의 숫자를 1개 입력해 주세요."
-    const val COUNT_ERROR = "로또 번호는 6개여야 합니다."
-    const val DUPLICATE_ERROR = "중복된 번호 입니다."
+    const val COUNT_ERROR = "[ERROR] 로또 번호는 6개여야 합니다."
+    const val DUPLICATE_ERROR = "[ERROR] 중복된 번호 입니다."
 
 }
