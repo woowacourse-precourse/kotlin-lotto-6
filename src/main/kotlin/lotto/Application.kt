69cbceb1d4213fbcdc62575fe29f442dda752c4a
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-08 01:43:07 +0900

    [feat] readLine for price

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,37 @@
 package lotto
 
+import kotlin.text.toInt
+
+// import camp.nextstep.edu.missionutils.Randoms // pickUniqueNumbersInRange()
+// import camp.nextstep.edu.missionutils.Console // readLine()
+
 fun main() {
-    TODO("프로그램 구현")
+    
+    println("구입금액을 입력해 주세요.")
+    
+    var stopPoint = 1
+    lateinit var price: String
+
+    while (stopPoint == 1) {
+        try {
+            price = readLine()!!
+            stopPoint = priceCheck(price)
+        } catch (e: IllegalArgumentException) {
+            println(e)
+        }
+    }
+
+    
+    println(price)
+
+}
+
+fun priceCheck(price: String): Int {
+    
+    val temp =price.toInt()
+    if (temp % 1000 == 0) {
+        return 0
+    } else {
+        throw IllegalArgumentException("[ERROR] 구입금액을 잘못 입력하였습니다.")
+    }
 }
