8275f0cd6c28359bd32adf79d6276ffb05282c54
jklom0326 <jklom0326@gmail.com>
2023-11-08 01:40:12 +0900

    test: LottoMachine 테스트 작성
    - 당첨 내역 결과
    - 당첨금 계산
    - 수익률 계산

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoMachineTest {
+    private val lottoMachine = LottoMachine()
     private val lotto = mutableListOf(
         Lotto(listOf(1, 2, 3, 4, 5, 6)),
         Lotto(listOf(1, 2, 4, 8, 16, 32)),
@@ -15,8 +16,23 @@ class LottoMachineTest {
 
     @Test
     fun `당첨 내역 결과`() {
-        val prize = LottoMachine().winningLotteryResult(lotto, inputLotto, bonusNumber)
+        val prize = lottoMachine.winningLotteryResult(lotto, inputLotto, bonusNumber)
         val result = Prize(first = 1, fifth = 1, miss = 2)
         assertThat(prize).isEqualTo(result)
+    }
+
+    @Test
+    fun `당첨금 계산`() {
+        val prize = lottoMachine.winningLotteryResult(lotto, inputLotto, bonusNumber)
+        val result = 2000005000.0
+        assertThat(prize.getPrizeMoney()).isEqualTo(result)
+    }
+
+    @Test
+    fun `수익률 계산`() {
+        val prize = lottoMachine.winningLotteryResult(lotto, inputLotto, bonusNumber)
+        val winRate = lottoMachine.calculateWinRate(prize, lotto)
+        val result = 50000125.0
+        assertThat(winRate).isEqualTo(result)
     }
 }
