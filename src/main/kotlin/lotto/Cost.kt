a3fefea17d2f5591c8a5d3d3c5024e5f0633e989
NOGUEN <nhg1113@naver.com>
2023-11-07 13:49:03 +0900

    feat: Cost 모델 클래스 추가

---
+++ src/main/kotlin/lotto/Cost.kt
@@ -0,0 +1,25 @@
+package lotto
+
+import lotto.exception.NotPositiveCostException
+import lotto.exception.UnvalidCostException
+
+class Cost(private val cost: Int) {
+    fun checkValidateCost(): Boolean {
+        if (!checkPositiveNumber())
+            throw NotPositiveCostException()
+        if (!checkDivideByCostUnit())
+            throw UnvalidCostException()
+    }
+
+    private fun checkDivideByCostUnit(): Boolean {
+        return cost / COST_UNIT == 0
+    }
+
+    private fun checkPositiveNumber(): Boolean {
+        return cost > 0
+    }
+
+    fun getCost(): Int {
+        return cost
+    }
+}
