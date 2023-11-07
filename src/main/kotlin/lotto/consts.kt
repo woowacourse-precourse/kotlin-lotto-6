72bbf902447c67549b8ab5b845256fb865539ec6
NOGUEN <nhg1113@naver.com>
2023-11-07 13:18:52 +0900

    feat: 출력문과 에러를 위한 상수들 추가

---
+++ src/main/kotlin/lotto/consts.kt
@@ -0,0 +1,25 @@
+package lotto
+
+const val EXCEPTION_MESSAGE_UNVALID_LOTTO_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+const val EXCEPTION_MESSAGE_UNVALID_LOTTO_NUMBER_COUNT = "[ERROR] 로또 번호는 6개 입력 되어야 합니다."
+const val EXCEPTION_MESSAGE_UNVALID_COST = "[ERROR] 금액은 1000단위의 값이어야 합니다."
+
+const val SEPARATOR = ","
+const val SEPARATOR_WITH_SPACE = ", "
+
+const val INPUT_MESSAGE_COST = "구입금액을 입력해 주세요."
+const val OUTPUT_MESSAGE_PURCHASE_RESULT = "개를 구매했습니다."
+const val INPUT_MESSAGE_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요."
+const val INPUT_MESSAGE_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+const val OUTPUT_MESSAGE_WINNING_STATISTICS = "당첨 통계"
+const val OUTPUT_MESSAGE_DIVIDER = "---"
+const val OUTPUT_MESSAGE_WIN_THREE = "3개 일치 (5,000원) - "
+const val OUTPUT_MESSAGE_WIN_FOUR = "4개 일치 (50,000원) - "
+const val OUTPUT_MESSAGE_WIN_FIVE = "5개 일치 (1,500,000원) - "
+const val OUTPUT_MESSAGE_WIN_FIVE_WITH_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+const val OUTPUT_MESSAGE_WIN_SIX = "6개 일치 (2,000,000,000원) - "
+const val OUTPUT_MESSAGE_RATE_OF_RETURN_1 = "총 수익률은 "
+const val OUTPUT_MESSAGE_RATE_OF_RETURN_2 = "입니다."
+
+const val OUTPUT_MESSAGE_NEWLINE = "\n"
+const val OUTPUT_MESSAGE_UNIT = "개"
