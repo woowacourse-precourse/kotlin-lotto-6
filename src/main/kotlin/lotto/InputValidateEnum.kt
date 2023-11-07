4a3b2050530360a0e82dc3d1f0fbba916773c88d
K_Gs:D <rootachieve6053@gmail.com>
2023-11-08 04:00:27 +0900

    feat<InputValidateEnum> enum class 생성
    입력의 검증을 처리합니다.

---
+++ src/main/kotlin/lotto/InputValidateEnum.kt
@@ -0,0 +1,51 @@
+package lotto
+
+enum class InputValidateEnum(val validator: (String) -> (Unit)) {
+    BUDGET({ input ->
+        require(input.isBlank().not()) {
+            StringRes.INPUT_EMPTY_LINE_ERR
+        }
+        require(input.toList().all {
+            it.code in 48..58
+        }) {
+            StringRes.INPUT_CHAR_ERR
+        }
+        require(input.toInt() % GameConst.COST == 0) {
+            StringRes.BUDGET_REMAIN_ERR
+        }
+    }),
+
+    WINNING_NUMBER({ input ->
+        require(input.isBlank().not()) {
+            StringRes.INPUT_EMPTY_LINE_ERR
+        }
+        val numbers = input.split(",").map { it.trim() }.map { it.toIntOrNull() ?: IllegalArgumentException(StringRes.INPUT_CHAR_ERR) }
+        require(numbers.distinct().size == numbers.size){
+            StringRes.WINNING_LIST_DISTINCT
+        }
+        require(numbers.all {
+            it in 1..GameConst.MAX_NUM
+        }){
+            StringRes.INPUT_OUT_OF_RANGE
+        }
+        require(numbers.size == GameConst.NUM_COUNT){
+            StringRes.WINNING_LIST_COUNT_ERR
+        }
+    }),
+    
+    BONUS({ input ->
+        require(input.isBlank().not()) {
+            StringRes.INPUT_EMPTY_LINE_ERR
+        }
+        require(input.toList().all {
+            it.code in 48..58
+        }) {
+            StringRes.INPUT_CHAR_ERR
+        }
+        require(input.toInt() in 1..lotto.GameConst.MAX_NUM) {
+            StringRes.INPUT_OUT_OF_RANGE
+        }
+    })
+
+
+}
