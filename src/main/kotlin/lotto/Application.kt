5a8090ab78a60d3ae4adc0d7d307359abac8df63
ii2001 <iicks2001@gmail.com>
2023-11-07 11:38:36 +0900

    feat: 사용자에게 로또 구매 가격 입력받는 함수 제작

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,7 +3,9 @@ import camp.nextstep.edu.missionutils.Randoms
 
 
 fun main() {
+    println("구입금액을 입력해 주세요.")
 
+    val purchaseAmount = getPurchaseAmount()
 }
 
 fun generateLottoNumbers(purchaseAmount: Int): List<Lotto> {
@@ -14,4 +16,31 @@ fun generateLottoNumbers(purchaseAmount: Int): List<Lotto> {
         lottoNumbers.add(lotto)
     }
     return lottoNumbers
+}
+
+fun getPurchaseAmount(): Int {
+    var amount = 1
+    while (amount % 1000 != 0) {
+        amount=inputPurchase(amount)
+    }
+    return amount
+}
+
+fun inputPurchase(amount: Int): Int {
+    var mutableAmount = amount
+    try {
+        mutableAmount = readLine()?.toInt()!!
+    } catch (e: NumberFormatException) {
+        println("[ERROR] 잘못된 입력 형식입니다.")
+        return amount
+    }
+    if (mutableAmount % 1000 != 0) {
+        println("[ERROR] 구입 금액은 1,000원 단위로 입력되어야 합니다.")
+        return amount
+    }
+    if (mutableAmount == 0) {
+        println("[ERROR] 구입 금액은 1000원 이상이어야 합니다.")
+        return amount
+    }
+    return mutableAmount
 }
