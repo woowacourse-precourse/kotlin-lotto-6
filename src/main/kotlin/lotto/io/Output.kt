80595627e8bb27c804edb51600f94c6cbe126203
aprilgom <aprilgom@users.noreply.github.com>
2023-11-08 08:44:25 +0900

    refactor: print 객체와 메서드들 이름 변경

---
+++ src/main/kotlin/lotto/io/Output.kt
@@ -0,0 +1,24 @@
+package lotto.io
+
+import lotto.Messages
+import lotto.Rate
+import lotto.domain.Position
+import java.text.DecimalFormat
+
+object Output {
+    fun printPrintpleaseInputBudget() = print(Messages.PLEASE_INPUT_BUDGET)
+    fun printHowMuchBought(amount: Int) = print("$amount${Messages.AFTER_BOUGHT}")
+    fun printPleaseInputWinningNumbers() = print(Messages.PLEASE_INPUT_WINNING_NUMBERS)
+    fun printPleaseInputBonusNumbers() = print(Messages.PLEASE_INPUT_BONUS_NUMBERS)
+    fun printWinningStatistics(statistics: Map<Position, Int>) {
+        val decimalFormat = DecimalFormat("#,###")
+        val sorted = statistics.toSortedMap().forEach {
+            if (it.key == Position.NoLuck) {
+                return@forEach
+            }
+            val formattedPrize = decimalFormat.format(it.key.winningPrize)
+            println("${it.key.message} ($formattedPrize) - ${it.value}개")
+        }
+    }
+    fun printProfitRate(rate: Rate) = println("총 수익률은 ${rate}입니다.")
+}
