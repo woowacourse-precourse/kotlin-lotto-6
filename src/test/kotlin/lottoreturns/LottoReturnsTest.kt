dcd19ba1822020fb5731445b802e6cf6d83d3461
lee989898 <wnsrud1352@gmail.com>
2023-11-08 00:32:57 +0900

    [test] 당첨된게 없을 때 수익률 정확하게 나오는지 확인 테스트 구현

--- src/test/kotlin/lottoreturns/LottoReturnsTest.kt
+++ src/test/kotlin/lottoreturns/LottoReturnsTest.kt
@@ -1,6 +1,10 @@
 package lottoreturns
 
+import lottopurchaseamount.LottoPurchaseAmount
+import lottoranking.LottoRanking
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class LottoReturnsTest {
     private lateinit var lottoReturns: LottoReturns
@@ -8,5 +12,12 @@ class LottoReturnsTest {
     @BeforeEach
     fun setUp() {
         lottoReturns = LottoReturnsImpl()
+    }
+
+    @Test
+    fun `당첨된게 없을 때 수익률 정확하게 나오는지 확인`() {
+        val result = lottoReturns.calculate(LottoPurchaseAmount(10000))
+
+        assertThat(result).isEqualTo(0.0)
     }
 }
