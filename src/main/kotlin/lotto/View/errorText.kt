98ec064f7b11df2d01a5bc9aa002715b02f5a4e7
gillyongs <khs20010327@gmail.com>
2023-11-08 00:05:30 +0900

    refactor: 에러 문구 상수화

---
+++ src/main/kotlin/lotto/View/errorText.kt
@@ -0,0 +1,10 @@
+package lotto.View
+
+object ErrorText {
+    const val NOT_VALID_PURCHASE_AMOUNT = "[ERROR] 구입 금액은 1,000원 단위여야 합니다."
+    const val NAN = "[ERROR] 유효한 숫자를 입력하세요."
+    const val DUPLICATE_WINNING_NUMBER = "[ERROR] 로또 번호에 중복된 숫자가 있습니다."
+    const val NOT_VALID_WINNING_NUMBER = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    const val NOT_VALID_BONUS_NUMBER = "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
+    const val NOT_VALID_WINNING_NUMBER_LENGTH = "[ERROR] 로또 번호는 6개여야 합니다."
+}
