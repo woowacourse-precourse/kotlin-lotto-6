d548465f23ab2ce30494c2fc013512055f16c771
stopkite <stopkite98@gmail.com>
2023-11-07 21:37:43 +0900

    refactor(LottoResult): 하나의 표현식으로 이루어진 함수일 경우 단일 표현식으로 수정

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -18,13 +18,8 @@ class LottoResult(private val lottoTickets: LottoTickets, private val winningLot
         }
     }
 
-    private fun calculateMatchedNumberCounts(winningLotto: WinningLotto): List<Int> {
-        return lottoTickets.tickets.map { it.countMatchingNumbers(winningLotto.winningNumbers) }
-    }
-
-    private fun checkForBonusNumber(winningLotto: WinningLotto): List<Boolean> {
-        return lottoTickets.tickets.map { it.hasBonusNumber(winningLotto.bonusNumber) }
-    }
+    private fun calculateMatchedNumberCounts(winningLotto: WinningLotto): List<Int> = lottoTickets.tickets.map { it.countMatchingNumbers(winningLotto.winningNumbers) }
+    private fun checkForBonusNumber(winningLotto: WinningLotto): List<Boolean> = lottoTickets.tickets.map { it.hasBonusNumber(winningLotto.bonusNumber) }
 
     fun printResult() {
         printWinningResults()
@@ -34,36 +29,29 @@ class LottoResult(private val lottoTickets: LottoTickets, private val winningLot
     private fun printWinningResults() {
         println(WIN_STATICS)
         println(STATICS_DIVIDER)
-        Prize.values().filterNot { it == Prize.Nothing }.reversed().forEach { displayEachWinningResult(it) }
+        displayWinningResult()
     }
 
-    private fun displayEachWinningResult(prize: Prize) {
-        val winCount = findWindCount(prize)
-        val matchedNumberCount = findMatchedNumberCount(prize)
-        val winningPrizeMoney = prize.amount.toWonFormat()
-        val bonusInfo = if (prize == Prize.Second) BONUS_NUMBER_MATCH else ""
-        val message = winCountMessage(matchedNumberCount, bonusInfo, winningPrizeMoney, winCount)
+    private fun displayWinningResult() {
+        val prizes = Prize.values().filterNot { it == Prize.Nothing }.reversed()
+        prizes.forEach { printEachWinningResult(it) }
+    }
+
+    private fun printEachWinningResult(prize: Prize) {
+        val message = winCountMessage(matchedNumberCount = findMatchedNumberCount(prize), bonusInfo = if (prize == Prize.Second) BONUS_NUMBER_MATCH else "", winningPrizeMoney = prize.amount.toWonFormat(), winTicketCount = findWinTicketCount(prize))
         println(message)
     }
 
-    private fun findWindCount(prize: Prize) = winningResult.count { it == prize }
+    private fun winCountMessage(matchedNumberCount: Int, bonusInfo: String, winningPrizeMoney: String, winTicketCount: Int): String = "${matchedNumberCount}개 일치, ${bonusInfo}(${winningPrizeMoney})원 - ${winTicketCount}개"
+
+    private fun findWinTicketCount(prize: Prize) = winningResult.count { it == prize }
+
     private fun findMatchedNumberCount(prize: Prize) = Prize.findPrizeMatchNumberCount(prize)
     private fun Int.toWonFormat(): String = DecimalFormat("#,###").format(this)
 
-    private fun winCountMessage(
-            matchedNumberCount: Int,
-            bonusInfo: String,
-            winningPrizeMoney: String,
-            winCount: Int
-    ): String {
-        return "${matchedNumberCount}개 일치, ${bonusInfo}(${winningPrizeMoney})원 - ${winCount}개"
-    }
+    private fun printWinningProfitRate() = println(TOTAL_PROFIT.format(calculateTotalStaticsResult()))
 
-    private fun printWinningProfitRate() {
-        println(TOTAL_PROFIT.format(calculateTotalStaticResult()))
-    }
-
-    private fun calculateTotalStaticResult(): Double {
+    private fun calculateTotalStaticsResult(): Double {
         val winnerCountMap = winningResult.groupingBy { it }.eachCount()
         val totalProfit = winnerCountMap.map { it.key.amount * it.value }.sum()
         val payment = lottoTickets.tickets.count() * 1_000
