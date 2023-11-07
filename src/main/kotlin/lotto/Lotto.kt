e27c9d13a330e4a63cb567df7bd952a51d497345
eunyeong <feijoaplz@gmail.com>
2023-11-08 02:53:43 +0900

    feat: 로또번호 예외처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,10 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {"[ERROR] 6개의 숫자만 입력해 주세요."}
+        require(numbers.distinct().size == 6) {"[ERROR] 중복 되지 않는 숫자를 입력해 주세요."}
+        require(numbers.all {it in 1..45}) {"[ERROR] 1에서 45사이의 숫자를 입력해 주세요."}
     }
+    
 
-    // TODO: 추가 기능 구현
 }
