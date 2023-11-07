8ece2aca9d757bfe566ab1b98e8f2544da37be5b
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-07 15:24:06 +0900

    [Feat] 당첨 번호와 비교하는 함수 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,26 @@
 package lotto
 
+private const val CNT_LOTTO_NUMBER = 6
+private const val MIN_LOTTO_NUMBER = 1
+private const val MAX_LOTTO_NUMBER = 45
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == CNT_LOTTO_NUMBER)
+        require(numbers.toSet().size == CNT_LOTTO_NUMBER)
+        require(numbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER })
     }
 
-    // TODO: 추가 기능 구현
+    fun checkMatch(winningNumbers: List<Int>, bonusNumber: Int): Int {
+        val matchingNumbers = numbers.intersect(winningNumbers.toSet())
+        return when (matchingNumbers.size) {
+            6 -> 1
+            5 -> if (numbers.contains(bonusNumber)) 2 else 3
+            4 -> 4
+            3 -> 5
+            else -> 0
+        }
+
+    }
+
 }
