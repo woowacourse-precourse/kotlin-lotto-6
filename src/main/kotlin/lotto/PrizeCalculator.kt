be2c7236e3660468d1b212873eeaf32f4142a9cd
aprilgom <aprilgom@users.noreply.github.com>
2023-11-08 05:07:55 +0900

    fix: LottoFacade 제거, PrizeCalculator 원복

--- src/main/kotlin/lotto/PrizeCalculator.kt
+++ src/main/kotlin/lotto/PrizeCalculator.kt
@@ -1,13 +1,11 @@
 package lotto
 
-class LottoFacade(
-    private val budget: Int,
-    private val winNumberList: List<Int>,
-    private val bonusNumber: Int,
+class PrizeCalculator(
+    private val lottoList: List<Lotto>,
+    winNumberList: List<Int>,
+    bonusNumber: Int,
 ) {
-    private val lottoBuyer = LottoBuyer(budget)
     private val lottoChecker = LottoChecker(winNumber = Lotto(winNumberList), bonusNumber)
-    val lottoList = lottoBuyer.buy()
 
     fun getWinningCountMap() = lottoList.groupingBy {
         lottoChecker.checkLotto(it)
