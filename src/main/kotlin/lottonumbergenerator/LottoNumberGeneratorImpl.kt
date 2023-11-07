074127f79c6286ac90cf7b44c9aa5afd8aad72f8
lee989898 <wnsrud1352@gmail.com>
2023-11-07 14:48:03 +0900

    [feat] 로또 번호 발행 하는 LottoNumberGeneratorImpl 클래스 생성

---
+++ src/main/kotlin/lottonumbergenerator/LottoNumberGeneratorImpl.kt
@@ -0,0 +1,13 @@
+package lottonumbergenerator
+
+import entity.LottoNumber
+
+class LottoNumberGeneratorImpl : LottoNumberGenerator {
+
+    override val lottoes: List<LottoNumber>
+        get() = TODO("Not yet implemented")
+
+    override fun generateLotto(numberOfIssuedLotto: Int) {
+        TODO("Not yet implemented")
+    }
+}
