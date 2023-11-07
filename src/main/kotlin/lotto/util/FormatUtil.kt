8153f3d7aa8d94f3c999a90248d6d2219dca98e3
pipeline <sorkwhwo@naver.com>
2023-11-07 13:53:17 +0900

    refactor: 로직 순서에 따른 함수 재배치 (FormatUtil)

--- src/main/kotlin/lotto/util/FormatUtil.kt
+++ src/main/kotlin/lotto/util/FormatUtil.kt
@@ -1,12 +1,12 @@
 package lotto.util
 
 object FormatUtil {
-    fun formatEarningRate(earningRate: Double): String {
-        return String.format("%,.1f", earningRate)
-    }
 
     fun formatReward(reward: Int): String {
         return String.format("%,d", reward)
     }
 
+    fun formatEarningRate(earningRate: Double): String {
+        return String.format("%,.1f", earningRate)
+    }
 }
