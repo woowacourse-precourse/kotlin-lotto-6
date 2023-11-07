c9101396f9252cdf035443c088081abf04e8413f
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 13:21:03 +0900

    Feat: 당첨 번호 유효성 검사 기능(당첨 번호 개수가 6개가 아닐 때)

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -5,10 +5,15 @@ class WinningNumber(private val numbers: String) {
     private val winningNumbers = numbers.split(",")
 
     init {
+        require(checkSize()) { "$ERROR $WINNING_NUMBER_ONLY_SIX_DIGITS" }
         require(checkNumbersOfCommas()) { "$ERROR $PLEASE_SEPARATE_NUMBERS_COMMAS" }
         require(isNumberOrNull()) { "$ERROR $INPUT_ONLY_NUMBER" }
         require(numbersOutOfRange()) { "$ERROR $WINNING_NUMBER_RANGE" }
         require(checkDuplicateNumbers()) { "$ERROR $WINNING_NUMBER_CANNOT_DUPLICATE" }
+    }
+
+    private fun checkSize(): Boolean {
+        return winningNumbers.size == 6
     }
 
     private fun checkNumbersOfCommas(): Boolean {
@@ -29,6 +34,7 @@ class WinningNumber(private val numbers: String) {
 
     companion object {
         const val ERROR = "[ERROR]"
+        const val WINNING_NUMBER_ONLY_SIX_DIGITS = "당첨 번호는 6자리여야 합니다."
         const val PLEASE_SEPARATE_NUMBERS_COMMAS = "당첨 번호는 쉼표(,) 기준으로 구분해주세요."
         const val INPUT_ONLY_NUMBER = "당첨 번호는 숫자만 입력 가능합니다."
         const val WINNING_NUMBER_RANGE = "당첨 번호는 1부터 45사이의 숫자여야 합니다."
