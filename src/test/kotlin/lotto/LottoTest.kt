18edeacb363282a1807b627e92be8bb079427e58
jklom0326 <jklom0326@gmail.com>
2023-11-08 00:59:04 +0900

    test: LottoTest 작성
    - 당첨 번호 입력값이 6개보다 적을 경우

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -19,9 +19,16 @@ class LottoTest {
     }
 
     @Test
-    fun `1 ~ 45 사이의 숫자가 아닐 때`() {
+    fun `1 ~ 45 사이의 숫자가 아닐 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 50))
+        }
+    }
+
+    @Test
+    fun `당첨번호 입력값이 숫자 6개가 보다 적을 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
         }
     }
 }
