5e989b037bf5452090faba9db78580657296455b
K_Gs:D <rootachieve6053@gmail.com>
2023-11-08 02:46:54 +0900

    feat<BaseCreateLotto> : 클래스 생성
    로또 생성 인터페이스를 구현합니다.
    주어진 리스트로 로또를 만들어냅니다.

---
+++ src/main/kotlin/lotto/BaseCreateLotto.kt
@@ -0,0 +1,13 @@
+package lotto
+
+class BaseCreateLotto(private vararg val numbers: List<Int>) : CreateLottoInterface {
+    private var index = 0
+    override fun getLotto(): Lotto {
+        require(index < numbers.size){
+            StringRes.BASE_CREATE_LOTTO_OUT_OF_RANGE
+        }
+
+        return Lotto(numbers[index++].sorted())
+    }
+
+}
