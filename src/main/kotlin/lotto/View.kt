d4dc824384c703326202d57ea2856498c2787a4d
jklom0326 <jklom0326@gmail.com>
2023-11-08 00:14:00 +0900

    refactor: 입력 받는 기능 수정
    - 로또 구입 금액 오류시 재입력받는 기능 추가
    - 당첨 번호 입력 오류시 재입력받는 기능 추가
    - 보너스 번호 입력 오류시 재입력받는 기능 추가

--- src/main/kotlin/lotto/View.kt
+++ src/main/kotlin/lotto/View.kt
@@ -3,18 +3,49 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class View {
-    private val validation = Validation()
 
-    fun inputPayment(): Int {
-        println("구입금액을 입력해 주세요.")
-        val input = Console.readLine()
-        return validation.checkInputPayment(input)
+    fun inputPayment(): Payment {
+        while (true) {
+            println("구입금액을 입력해 주세요.")
+            val input = Console.readLine()
+            try {
+                return Payment(input)
+            } catch (e: IllegalArgumentException) {
+                printErrorMessage(e)
+                continue
+            }
+        }
     }
 
     fun inputLottoNumber(): Lotto {
-        println("\n당첨 번호를 입력해 주세요.")
-        val input = Console.readLine()
-        return validation.checkInputLottoNumber(input)
+        while (true) {
+            println("\n당첨 번호를 입력해 주세요.")
+            val input = Console.readLine()
+            val number = input.split(",").sorted()
+            try {
+                return Lotto(number.map { it.toInt() })
+            } catch (e: IllegalArgumentException) {
+                printErrorMessage(e)
+                continue
+            }
+        }
+    }
+
+    fun inputBonusLottoNumber(inputLottoNumber: Lotto): BonusNumber {
+        while (true){
+            println("\n보너스 번호를 입력해 주세요.")
+            val input = Console.readLine()
+            try {
+                return BonusNumber(input, inputLottoNumber)
+            } catch (e: IllegalArgumentException){
+                printErrorMessage(e)
+                continue
+            }
+        }
+    }
+
+    private fun printErrorMessage(e: IllegalArgumentException) {
+        println("${e.message}\n")
     }
 
     fun printLotto(lotto: List<Lotto>) {
@@ -24,9 +55,21 @@ class View {
         }
     }
 
-    fun inputBonusLottoNumber(): Int {
-        println("\n보너스 번호를 입력해 주세요.")
-        val input = Console.readLine()
-        return validation.checkInputBonusNumber(input)
+    fun printPrizeResult(prize: Prize) {
+        println("\n당첨 통계\n---")
+        println("3개 일치 (5,000원) - ${prize.fifth}개")
+        println("4개 일치 (50,000원) - ${prize.fourth}개")
+        println("5개 일치 (1,500,000원) - ${prize.third}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${prize.second}개")
+        println("6개 일치 (2,000,000,000원) - ${prize.first}개")
+    }
+
+    fun printWinRate(winRate: Double) {
+        val rateFormat = String.format("%.1f", winRate)
+        println("총 수익률은 ${rateFormat}%입니다.")
+    }
+
+    companion object {
+
     }
 }
