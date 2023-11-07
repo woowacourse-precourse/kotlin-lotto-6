2c581865177b8948dc682b025377258bcb4320aa
Tnalxmsk <alstnwkd990@naver.com>
2023-11-07 11:17:25 +0900

    refactor(lotto): Input 클래스 코드 수정
    split 할 문자 상수화

--- src/main/kotlin/lotto/view/Input.kt
+++ src/main/kotlin/lotto/view/Input.kt
@@ -10,7 +10,7 @@ class Input {
 
     fun inputWinningNumber(): List<Int> {
         println(INPUT_WINNING_NUMBER)
-        return Console.readLine().split(",").map { number ->
+        return Console.readLine().split(SPLIT_STRING).map { number ->
             number.toInt()
         }
     }
@@ -24,5 +24,6 @@ class Input {
         private const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
         private const val INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요."
         private const val INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요."
+        private const val SPLIT_STRING = ","
     }
 }
