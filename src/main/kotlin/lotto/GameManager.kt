ca21660f6cba57cbec5be37c6ee53bd2502036c9
Jbrochan <hyeongchanj@gmail.com>
2023-11-07 17:35:34 +0900

    feat: 등수 구분을 위한 클래스 구현
    - 로또 당첨 등수 구분을 위한 클래스 구현, 한글 등수와 상금을 프로퍼티로 설정

--- src/main/kotlin/lotto/GameManager.kt
+++ src/main/kotlin/lotto/GameManager.kt
@@ -48,3 +48,11 @@ class GameManager {
         const val LOTTO_PRICE = 1000
     }
 }
+
+enum class Ranking(val grade: String, val prizeMoney: Int) {
+    FIRST("1등", 2000000000),
+    SECOND("2등", 30000000),
+    THIRD("3등", 1500000),
+    FOURTH("4등", 50000),
+    FIFTH("5등", 5000)
+}
