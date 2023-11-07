165a56248709d8b4f23da00b6bff7dadcf392988
yeji0214 <1225yj50@naver.com>
2023-11-07 20:43:53 +0900

    LottoPrizeCalculator.kt 보완 (enum class 사용)

--- src/main/kotlin/lotto/LottoPrizeCalculator.kt
+++ src/main/kotlin/lotto/LottoPrizeCalculator.kt
@@ -1,21 +1,22 @@
 package lotto
 
+import java.math.BigDecimal
+
+enum class Prize(val description: String, val amount: BigDecimal) {
+    THREE_MATCHES(MessageConstants.THREE_MATCHES, BigDecimal(5000)),
+    FOUR_MATCHES(MessageConstants.FOUR_MATCHES, BigDecimal(50000)),
+    FIVE_MATCHES(MessageConstants.FIVE_MATCHES, BigDecimal(1500000)),
+    FIVE_MATCHES_AND_BONUS_NUMBER_MATCH(MessageConstants.FIVE_MATCHES_AND_BONUS_NUMBER_MATCH, BigDecimal(30000000)),
+    SIX_MATCHES(MessageConstants.SIX_MATCHES, BigDecimal(2000000000))
+}
 class LottoPrizeCalculator {
-    private var lottoNumberMatch = INITIAL_NUM
+    private var lottoNumberMatch = 0
     private var bonusNumberMatch = false
 
-    companion object {
-        private const val INITIAL_NUM = 0
-        private const val SIX_MATCH_PRIZE = 2_000_000_000
-        private const val FIVE_AND_BONUS_MATCH_PRIZE = 30_000_000
-        private const val FIVE_MATCH_PRIZE = 1_500_000
-        private const val FOUR_MATCH_PRIZE = 50_000
-        private const val THREE_MATCH_PRIZE = 5_000
-    }
-    fun calculatePrizesAndReturnTotalPrize(lottoPurchaseCounts: List<List<Int>>, winningLotteryNumbers: List<Int>, bonusNumber: Int, prizeCounts: IntArray): Int {
-        var totalPrize = INITIAL_NUM
+    fun calculatePrizesAndReturnTotalPrize(lottoPurchaseCounts: List<List<Int>>, winningLotteryNumbers: List<Int>, bonusNumber: Int, prizeCounts: IntArray): BigDecimal {
+        var totalPrize = BigDecimal.ZERO
 
-        for (i in INITIAL_NUM until lottoPurchaseCounts.size) {
+        for (i in 0 until lottoPurchaseCounts.size) {
             val union = lottoPurchaseCounts[i] + winningLotteryNumbers
             val intersection = union.groupBy { it }.filter { it.value.size > 1 }.flatMap { it.value }.distinct()
             lottoNumberMatch = intersection.size
@@ -40,33 +41,50 @@ class LottoPrizeCalculator {
 
     }
 
-    private fun calculatePrizeAndIncrementCount(
-            lottoNumberMatch: Int,
-            prizeCounts: IntArray
-    ): Int {
+    private fun calculatePrizeAndIncrementCount(lottoNumberMatch: Int, prizeCounts: IntArray): BigDecimal {
         val prizeIndex = when (lottoNumberMatch) {
-            6 -> 4
-            5 -> if (bonusNumberMatch) 3 else 2
-            4 -> 1
-            3 -> 0
-            else -> -1
+            6 -> Prize.SIX_MATCHES
+            5 -> if (bonusNumberMatch) Prize.FIVE_MATCHES_AND_BONUS_NUMBER_MATCH else Prize.FIVE_MATCHES
+            4 -> Prize.FOUR_MATCHES
+            3 -> Prize.THREE_MATCHES
+            else -> null
         }
 
-        if (prizeIndex >= INITIAL_NUM) {
-            prizeCounts[prizeIndex]++
-            return calculatePrizeForMatch(lottoNumberMatch)
-        }
-
-        return INITIAL_NUM
-    }
-
-    private fun calculatePrizeForMatch(lottoNumberMatch: Int): Int {
-        return when (lottoNumberMatch) {
-            6 -> SIX_MATCH_PRIZE
-            5 -> if (bonusNumberMatch) FIVE_AND_BONUS_MATCH_PRIZE else FIVE_MATCH_PRIZE
-            4 -> FOUR_MATCH_PRIZE
-            3 -> THREE_MATCH_PRIZE
-            else -> INITIAL_NUM
+        return if (prizeIndex != null) {
+            prizeCounts[prizeIndex.ordinal]++
+            prizeIndex.amount
+        } else {
+            BigDecimal.ZERO
         }
     }
+
+//    private fun calculatePrizeAndIncrementCount(
+//            lottoNumberMatch: Int,
+//            prizeCounts: IntArray
+//    ): Int {
+//        val prizeIndex = when (lottoNumberMatch) {
+//            6 -> 4
+//            5 -> if (bonusNumberMatch) 3 else 2
+//            4 -> 1
+//            3 -> 0
+//            else -> -1
+//        }
+//
+//        if (prizeIndex >= INITIAL_NUM) {
+//            prizeCounts[prizeIndex]++
+//            return calculatePrizeForMatch(lottoNumberMatch)
+//        }
+//
+//        return INITIAL_NUM
+//    }
+//
+//    private fun calculatePrizeForMatch(lottoNumberMatch: Int): Int {
+//        return when (lottoNumberMatch) {
+//            6 -> SIX_MATCH_PRIZE
+//            5 -> if (bonusNumberMatch) FIVE_AND_BONUS_MATCH_PRIZE else FIVE_MATCH_PRIZE
+//            4 -> FOUR_MATCH_PRIZE
+//            3 -> THREE_MATCH_PRIZE
+//            else -> INITIAL_NUM
+//        }
+//    }
 }
--- src/main/kotlin/lotto/LottoRateOfReturn.kt
+++ src/main/kotlin/lotto/LottoRateOfReturn.kt
@@ -6,7 +6,7 @@ class LottoRateOfReturn {
     companion object {
         private const val ONE_HUNDRED = 100
     }
-    fun calculateRateOfReturn(totalPrize: Int, amount: Int): BigDecimal {
+    fun calculateRateOfReturn(totalPrize: BigDecimal, amount: Int): BigDecimal {
         return (totalPrize.toDouble() / amount * ONE_HUNDRED).toBigDecimal()
     }
 }
