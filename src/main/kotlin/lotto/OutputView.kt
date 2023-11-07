827fd73b34b5046b4c3439c1bf68aac1fd7f079d
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 13:25:31 +0900

    Feat: 보너스 번호 입력 요구 문구 출력 구현

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -14,10 +14,15 @@ class OutputView {
         println("\n$REQUEST_INPUT_WINNING_NUMBER")
     }
 
+    fun printRequestInputBonusNumber() {
+        println("\n$REQUEST_INPUT_BONUS_NUMBER")
+    }
+
     companion object {
         const val REQUEST_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
         const val NUMBER_OF_PURCHASES = "개를 구매했습니다."
         const val REQUEST_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+        const val REQUEST_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
     }
 
 }
