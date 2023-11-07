b9b529a493d08eedf77b9b9a3b2dcb0bcbddbc0c
eunyeong <feijoaplz@gmail.com>
2023-11-08 03:34:50 +0900

    feat: 당첨번호 입력하는 기능

--- src/main/kotlin/lotto/LottoProcess.kt
+++ src/main/kotlin/lotto/LottoProcess.kt
@@ -6,6 +6,7 @@ class LottoProcess {
     fun start() {
         val userCost = payLottery()
         val userTicket = getLottoTickets(userCost)
+        val winningNumbers = getWinningNumbers()
     }
 
     private fun payLottery(): Int {
@@ -24,15 +25,31 @@ class LottoProcess {
             println("${ticket.getDelimiter()}")
         }
     }
+    private fun getWinningNumbers(): WinningNumbers {
+        println("당첨 번호를 입력해 주세요.")
+        val inputWinningNumber = Console.readLine()
+        val winningNumbers = validateNumbersInput(inputWinningNumber)
+
+        return WinningNumbers(winningNumbers)
+    }
 
     private fun validatePayment(userPayment: String): String {
         if (userPayment.isEmpty()) throw IllegalArgumentException("[ERROR] 값을 입력해주세요.")
         if (userPayment.isBlank()) throw IllegalArgumentException("[ERROR] 값을 입력해주세요.")
         if (!userPayment.matches(Regex("^\\d*\$"))) {
-            throw IllegalArgumentException("[ERROR] 숫자만 입력해주세요.")
+            throw IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.")
         }
         if ((userPayment.toInt() % 1000) != 0) throw IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.")
         if (userPayment.toInt() < 1000) throw IllegalArgumentException("[ERROR] 최소 1000원 이상 입력해주세요.")
         return userPayment
+    }
+
+    private fun validateNumbersInput(inputWinningNumbers: String): List<Int> {
+        val inputs = inputWinningNumbers.split(",")
+        try {
+            return inputs.map { it.toInt() }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.")
+        }
     }
 }
--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -1,29 +1,22 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
-fun getWinningNumbers(): List<Int> {
-    println("당첨 번호를 입력해 주세요.")
-    val inputWinningNumber = Console.readLine()
-    validateWinningNumber(inputWinningNumber)
-    return inputWinningNumber.split(",").map { it.toInt()}
-}
-fun getBonusNumber(): Int {
-    println("보너스 번호를 입력해 주세요.")
-    val inputBonusNumber = Console.readLine().toInt()
-    validateBonusNumber(inputBonusNumber)
-    return inputBonusNumber
-}
-fun validateWinningNumber(inputWinningNumber: String):List<String> {
-    if (inputWinningNumber.contains(",").not()) throw IllegalArgumentException("[Error] 잘못된 입력입니다.")
-    if (inputWinningNumber.isEmpty()) throw IllegalArgumentException("[ERROR] 값을 입력해 주세요.")
-    if (inputWinningNumber.isBlank()) throw IllegalArgumentException("[ERROR] 값을 입력해 주세요.")
-    if (!inputWinningNumber.matches(Regex("^\\d*\$"))) throw IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.")
-    return listOf(inputWinningNumber)
-}
+
+class WinningNumbers() {
+
+    fun validateWinningNumber(inputWinningNumber: String):List<String> {
+        if (inputWinningNumber.contains(",").not()) throw IllegalArgumentException("[Error] 잘못된 입력입니다.")
+        if (inputWinningNumber.isEmpty()) throw IllegalArgumentException("[ERROR] 값을 입력해 주세요.")
+        if (inputWinningNumber.isBlank()) throw IllegalArgumentException("[ERROR] 값을 입력해 주세요.")
+        if (!inputWinningNumber.matches(Regex("^\\d*\$"))) throw IllegalArgumentException("[ERROR] 숫자만 입력해 주세요.")
+        return listOf(inputWinningNumber)
+    }
 
 
-fun validateBonusNumber(inputBonusNumber: Int):Int {
-    if(inputBonusNumber !in 1..45) throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자만 입력해 주세요.")
-    return inputBonusNumber
+    fun validateBonusNumber(inputBonusNumber: Int):Int {
+        if(inputBonusNumber !in 1..45) throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자만 입력해 주세요.")
+        return inputBonusNumber
+    }
 }
+
 
