01fe24439b7ce2463a8b65866f7c41c9c3b5959f
NOGUEN <nhg1113@naver.com>
2023-11-07 22:40:35 +0900

    feat: 우승 상금을 담고 있는 winPrize 필드와 이와 관련된 메소드 추가

--- src/main/kotlin/lotto/model/PurchasedLotto.kt
+++ src/main/kotlin/lotto/model/PurchasedLotto.kt
@@ -1,18 +1,20 @@
 package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
-import lotto.LOTTO_WIN_FIVE
+import lotto.*
 
 class PurchasedLotto(lottoList: List<Int>, bonusBall: Int) {
     private var numbers : List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
     private var matchingNumberCount = 0
     private var bonusBallMatched = false
+    private var winPrize = 0
 
     init {
         numbers = numbers.sorted()
         countMatchingNumber(lottoList)
         if (matchingNumberCount == LOTTO_WIN_FIVE)
             checkBonusBall(bonusBall)
+        getWinRankCost()
     }
 
     private fun countMatchingNumber(lottoList: List<Int>) {
@@ -26,5 +28,22 @@ class PurchasedLotto(lottoList: List<Int>, bonusBall: Int) {
     private fun checkBonusBall(bonusBall: Int) {
         if (numbers.contains(bonusBall))
             bonusBallMatched = true
+    }
+
+    private fun getWinRankCost() {
+        when (matchingNumberCount) {
+            WIN_RANK_FIRST -> winPrize = LOTTO_FIRST_WIN_COST
+            WIN_RANK_SECOND_AND_THIRD -> {
+                if (bonusBallMatched)
+                    winPrize = LOTTO_SECOND_WIN_COST
+                winPrize = LOTTO_THIRD_WIN_COST
+            }
+            WIN_RANK_FORTH -> winPrize = LOTTO_FORTH_WIN_COST
+            WIN_RANK_FIFTH -> winPrize = LOTTO_FIFTH_WIN_COST
+        }
+    }
+
+    fun getWinPrize(): Int {
+        return winPrize
     }
 }
