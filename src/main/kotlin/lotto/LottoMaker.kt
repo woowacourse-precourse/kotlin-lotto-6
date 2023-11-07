c1fc0204aa3b399541bdcef256d31bd1a9af262d
이용인 <yongin0517@gmail.com>
2023-11-07 13:32:24 +0900

    feat: 1~45 사이의 숫자를 6개 뽑아 반환하는 메소드 생성

---
+++ src/main/kotlin/lotto/LottoMaker.kt
@@ -0,0 +1,10 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Randoms
+
+object LottoMaker {
+
+    fun createRandomNumbers(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    }
+}
