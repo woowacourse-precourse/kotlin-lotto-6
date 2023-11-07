d7df6e485d547d7c36fe88efce341ca8308066de
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:08:04 +0900

    [feat] "보너스 번호를 입력해 주새요." 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -4,6 +4,7 @@ object OutputView {
     private const val PLEASE_ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val BOUGHT_NUMBER_OF_THEM = "개를 구매했습니다."
     private const val PLEASE_ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+    private const val PLEASE_ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주새요."
 
     fun pleaseEnterPurchaseAmount() {
         println(PLEASE_ENTER_PURCHASE_AMOUNT)
@@ -15,5 +16,9 @@ object OutputView {
 
     fun pleaseEnterWinningNumber() {
         println(PLEASE_ENTER_WINNING_NUMBER)
+    }
+
+    fun pleaseEnterBonusNumber() {
+        println(PLEASE_ENTER_BONUS_NUMBER)
     }
 }
