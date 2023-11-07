ae46fcfba59c25655ee40f6eb8f7407bfc980afe
m6z1 <sonmyungj1zz@gmail.com>
2023-11-07 22:37:02 +0900

    fix(LottoMC): 입력값을 ,로 구분지어서 map으로 변환

--- src/main/kotlin/lotto/domain/LottoMC.kt
+++ src/main/kotlin/lotto/domain/LottoMC.kt
@@ -4,7 +4,8 @@ import lotto.view.InputView
 
 class LottoMC(private val inputView: InputView) {
     fun pickLottoNum(): List<String> {
-        return listOf(inputView.inputView().trim())
+        val lottoNum = inputView.inputView().trim()
+        return lottoNum.split(",").map { it.trim() }
     }
 
     fun pickBonusNum(): String {
