51e2aa1a1f8b752f82211074cbdf3a6d6a5cacbe
jinuemong <jinwoo3661@naver.com>
2023-11-07 19:28:44 +0900

    Feat: ErrorType 파라미터 값 수정

--- src/main/kotlin/lotto/domain/ErrorType.kt
+++ src/main/kotlin/lotto/domain/ErrorType.kt
@@ -1,14 +1,13 @@
 package lotto.domain
 
 enum class ErrorType(
-    val type: Int,
     val message: String
 ) {
-    IS_NOT_POSITIVE_INTEGER(1,"입력 값이 정수가 아닙니다."),
-    IS_INCORRECT_AMOUNT(2,"올바른 로또 구매 금액이 아닙니다."),
-    IS_CONTAIN_DUPLICATES(3,"중복 된 로또 번호가 있습니다."),
-    IS_INCORRECT_NUMBER(4,"1~45 범위의 로또 번호가 아닙니다."),
-    IS_BLANK(5,"빈 값이 입력 되었습니다."),
-    IS_NOT_SIX_NUMBERS(6,"로또 번호가 6개가 아닙니다."),
-    NOT_ERROR(7,"")
+    IS_NOT_POSITIVE_INTEGER("입력 값이 정수가 아닙니다."),
+    IS_INCORRECT_AMOUNT("올바른 로또 구매 금액이 아닙니다."),
+    IS_CONTAIN_DUPLICATES("중복 된 로또 번호가 있습니다."),
+    IS_INCORRECT_NUMBER("1~45 범위의 로또 번호가 아닙니다."),
+    IS_BLANK("빈 값이 입력 되었습니다."),
+    IS_NOT_SIX_NUMBERS("로또 번호가 6개가 아닙니다."),
+    NOT_ERROR("")
 }
--- src/main/kotlin/lotto/validation/CheckInputValidation.kt
+++ src/main/kotlin/lotto/validation/CheckInputValidation.kt
@@ -7,7 +7,7 @@ import java.lang.NumberFormatException
 
 class CheckInputValidation {
 
-    fun checkIsNumber(userInput: String){
+    fun checkIsNumber(userInput: String) {
         try {
             userInput.toInt()
         } catch (e: NumberFormatException) {
@@ -17,21 +17,21 @@ class CheckInputValidation {
         }
     }
 
-    fun checkIsLottoNumber(number: String){
-        require(number.toInt() in LOTTO_START_INCLUSIVE..LOTTO_END_INCLUSIVE){
+    fun checkIsLottoNumber(number: String) {
+        require(number.toInt() in LOTTO_START_INCLUSIVE..LOTTO_END_INCLUSIVE) {
             ErrorType.IS_INCORRECT_NUMBER.message
         }
     }
 
     fun checkIsBlank(userInput: String) {
-        require(userInput.isNotEmpty()){
+        require(userInput.isNotEmpty()) {
             ErrorType.IS_BLANK.message
         }
     }
 
-    fun checkDuplication(inputList: List<Int>){
+    fun checkDuplication(inputList: List<Int>) {
         val distinctCount = inputList.distinct().count()
-        require(inputList.size == distinctCount){
+        require(inputList.size == distinctCount) {
             ErrorType.IS_CONTAIN_DUPLICATES.message
         }
     }
@@ -39,8 +39,8 @@ class CheckInputValidation {
     fun checkBonusNumberDuplication(
         numbers: Set<Int>,
         bonusNumber: Int
-    ){
-        require(numbers.contains(bonusNumber)){
+    ) {
+        require(numbers.contains(bonusNumber)) {
             ErrorType.IS_CONTAIN_DUPLICATES.message
         }
     }
@@ -48,8 +48,8 @@ class CheckInputValidation {
     fun checkLottoCount(
         userInput: List<String>
     ) {
-     require(userInput.size == 6){
-         ErrorType.IS_NOT_SIX_NUMBERS.message
-     }
+        require(userInput.size == 6) {
+            ErrorType.IS_NOT_SIX_NUMBERS.message
+        }
     }
 }
