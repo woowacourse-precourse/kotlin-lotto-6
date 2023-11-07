6dc6daf65decc53e36d4bcfeaf332609ef5bf06f
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-08 00:12:36 +0900

    [docs] feat requirements

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,10 @@
 package lotto
+// val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
 
+// 제공된 Lotto 클래스를 활용해 구현해야 한다.
+// numbers의 접근 제어자인 private을 변경할 수 없다.
+// Lotto에 필드를 추가할 수 없다.
+// Lotto의 패키지 변경은 가능하다.
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
