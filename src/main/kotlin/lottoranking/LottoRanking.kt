dc177e070ffa1bcae77acd3a6b936bf9ac78cc14
lee989898 <wnsrud1352@gmail.com>
2023-11-07 18:24:49 +0900

    [feat] LottoRanking에 enum 클래스에 count 프로퍼티 추가

--- src/main/kotlin/lottoranking/LottoRanking.kt
+++ src/main/kotlin/lottoranking/LottoRanking.kt
@@ -1,9 +1,9 @@
 package lottoranking
 
-enum class LottoRanking(val rank: Int, val reward: Int) {
-    FIRST(rank = 1, reward = 2_000_000_000),
-    SECOND(rank = 2, reward = 30_000_000),
-    THIRD(rank = 3, reward = 1_500_000),
-    FOURTH(rank = 4, reward = 50_000),
-    FIFTH(rank = 5, reward = 5_000)
+enum class LottoRanking(val rank: Int, val reward: Int, val count: Int) {
+    FIRST(rank = 1, reward = 2_000_000_000, count = 0),
+    SECOND(rank = 2, reward = 30_000_000, count = 0),
+    THIRD(rank = 3, reward = 1_500_000, count = 0),
+    FOURTH(rank = 4, reward = 50_000, count = 0),
+    FIFTH(rank = 5, reward = 5_000, count = 0)
 }
