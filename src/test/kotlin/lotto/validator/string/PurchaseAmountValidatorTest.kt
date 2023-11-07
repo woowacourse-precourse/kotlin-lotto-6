e0e13e3c48f86d61707989d084c1a02a1ef7d766
murjune <sis9221302@gmail.com>
2023-11-07 22:14:16 +0900

    test(PurchaseAmountValidatorTest) : 구입액 검사기 test

---
+++ src/test/kotlin/lotto/validator/string/PurchaseAmountValidatorTest.kt
@@ -0,0 +1,33 @@
+package lotto.validator.string
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class PurchaseAmountValidatorTest {
+    @Test
+    fun `구입 금액이 숫자형이고 1,000원 단위일 경우 정상`() {
+        val purchaseAmountValidator = PurchaseAmountValidator()
+        val validString = "1000"
+
+        purchaseAmountValidator.validate(validString)
+    }
+
+    @Test
+    fun `구입 금액이 숫자형이 아닐 경우 예외 발생`() {
+        val purchaseAmountValidator = PurchaseAmountValidator()
+        val invalidString = "abc"
+
+        assertThrows<IllegalArgumentException>("문자열 $invalidString 은 숫자여야 합니다") {
+            purchaseAmountValidator.validate(invalidString)
+        }
+    }
+    @Test
+    fun `구입 금액이 1,000원 단위가 아닐 경우 예외 발생`() {
+        val purchaseAmountValidator = PurchaseAmountValidator()
+        val invalidString = "1200"
+
+        assertThrows<IllegalArgumentException>("문자열 $invalidString 은 1,000원 단위여야 합니다") {
+            purchaseAmountValidator.validate(invalidString)
+        }
+    }
+}
