08fc9d3b19dbb9dbbfc38f57160f5ae20a25f82c
tgyuu-An <atg0614@gmail.com>
2023-11-08 01:01:14 +0900

    feature : 로또 구매 입력 받는 함수 구현

--- src/main/kotlin/lotto/presentation/LottoPresentation.kt
+++ src/main/kotlin/lotto/presentation/LottoPresentation.kt
@@ -1,4 +1,11 @@
 package lotto.presentation
 
+import camp.nextstep.edu.missionutils.Console
+
 class LottoPresentation {
+    fun getLottoPurchaseAmountInput(): String {
+        println("구입금액을 입력해 주세요.")
+        val purchaseLottoInput = Console.readLine()
+        return purchaseLottoInput
+    }
 }
