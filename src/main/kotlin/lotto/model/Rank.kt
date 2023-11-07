495b7f150529a22e300168e06795f983b02589cd
sseung416 <choiseungyeon416@gmail.com>
2023-11-07 14:23:25 +0900

    refactor: Rank 클래스에 당첨 번호와 몇 개 맞았는지를 저장하는 matchingCount 변수 추가

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -2,12 +2,12 @@ package lotto.model
 
 import lotto.model.seller.Money
 
-enum class Rank(val prize: Money) {
-    First(Money(2_000_000_000)),
-    Second(Money(30_000_000)),
-    Third(Money(1_500_000)),
-    Fourth(Money(50_000)),
-    Fifth(Money(5_000));
+enum class Rank(val prize: Money, val matchingCount: Int) {
+    First(Money(2_000_000_000), 6),
+    Second(Money(30_000_000), 5),
+    Third(Money(1_500_000), 5),
+    Fourth(Money(50_000), 4),
+    Fifth(Money(5_000), 3);
 
     companion object {
         fun of(matchingCount: Int, isMatchedBonus: Boolean): Rank? =
