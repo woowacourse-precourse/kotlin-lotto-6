1d7eca1def06d0678933409fe79c01973876d5bd
songpink <jejeje04@naver.com>
2023-11-07 18:21:51 +0900

    test : 테스트케이스 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -11,7 +11,7 @@ class LottoTest {
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7), listOf(1, 2, 3, 4, 5, 6), 10)
         }
     }
 
@@ -19,7 +19,7 @@ class LottoTest {
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(listOf(1, 2, 3, 4, 5), listOf(1, 2, 3, 4, 5, 6), 10)
         }
     }
 
