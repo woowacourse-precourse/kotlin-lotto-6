c566204c976a279d407c6dbebe2fe2de90ffa819
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-11-07 15:46:14 +0900

    Test (LottoTest) : 로또 번호에 대한 예외처리 테스트 코드 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,11 @@
 package lotto
 
+import lotto.model.isDigit
+import lotto.model.isLottoNumber
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 
 class LottoTest {
@@ -20,5 +24,26 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호의 개수가 6개 보다 작으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "46", "100", "-5"])
+    fun `로또 번호가 1 ~ 45 사이의 숫자가 아니면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            input.isLottoNumber()
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["q", "`", "ㅂ"])
+    fun `로또 번호가 숫자가 아니면 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            input.isDigit()
+        }
+    }
 }
