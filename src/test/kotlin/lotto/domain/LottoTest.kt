f4f33814e4e9d0636cef54cd3caa7f2c6ee3e8e2
jaehan4707 <jahan4707@naver.com>
2023-11-07 16:33:20 +0900

    refactor : 코드 정렬

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -20,20 +20,22 @@ class LottoTest {
         }
     }
 
-    @Test fun `로또 번호의 개수가 6개보다 적을 경우 예외가 발생한다`(){
+    @Test
+    fun `로또 번호의 개수가 6개보다 적을 경우 예외가 발생한다`() {
         val lottoLengthException = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1,2,3,4,5))
+            Lotto(listOf(1, 2, 3, 4, 5))
         }
         println(lottoLengthException.message)
     }
+
     @Test
-    fun `로또 번호에 1~45 범위를 벗어나는 숫자가 있으면 예외가 발생한다`(){
+    fun `로또 번호에 1~45 범위를 벗어나는 숫자가 있으면 예외가 발생한다`() {
         val lowerRangeException = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(0,1,2,3,4,5))
+            Lotto(listOf(0, 1, 2, 3, 4, 5))
         }
         println(lowerRangeException.message)
         val upperRangeException = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1,2,3,4,5,47))
+            Lotto(listOf(1, 2, 3, 4, 5, 47))
         }
         println(upperRangeException.message)
     }
--- src/test/kotlin/lotto/domain/UserTest.kt
+++ src/test/kotlin/lotto/domain/UserTest.kt
@@ -13,10 +13,11 @@ class UserTest {
             User(1200, listOf(Lotto(listOf(1, 2, 3, 4, 5, 6))))
         }
     }
+
     @Test
-    fun `구입 금액 만큼 로또를 구매하지 않을 경우 예외가 발생한다`(){
+    fun `구입 금액 만큼 로또를 구매하지 않을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            User(3000, listOf(Lotto(listOf(1,2,3,4,5,6)),Lotto(listOf(7,8,9,10,11,12))))
+            User(3000, listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)), Lotto(listOf(7, 8, 9, 10, 11, 12))))
         }
     }
 }
