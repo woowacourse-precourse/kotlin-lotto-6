b7de02fd091df6ee3e2c43eec3cdfe3935a6479c
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-07 23:19:14 +0900

    feat: lotto game v5 - 리펙토링 (변수 및 함수 수정) - 당첨 번호 및 계산 - 로또 수익률 및 통계 설정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,73 +4,106 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    val money = purchaseMoney() // 로또 구매 금액 입력
-    val lottery = purchaseLotto(money / 1000) // 로또 생성
-    printLotto(lottery) // 구매한 로또 출력(임의로 숫자 설정)
+    println("구입 금액을 입력해 주세요.") // 사용자가 구입 금액을 입력
+    // 입력받은 금액을 정수로 변환 및 잘못된 입력의 경우 예외 발생
+    val money = readLine()?.toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] 유효하지 않은 금액입니다.")
+    val lottoCount = money / 1000 // 구매할 로또의 수 설정
 
-    val winningNums = winningNumber() // 당첨 번호 설정
-    val bonusNum = bonusNumber() // 보너스 번호 설정
+    // 구매한 로또 개수만큼 로또 생성
+    // 1부터 45까지의 숫자 중 무작위로 6개 선택 후, Lotto 객체 생성
+    val lotteries = List(lottoCount) { Lotto(
+        Randoms.pickUniqueNumbersInRange(
+            1, 45, 6).sorted()
+    ) }
 
-    val wonLottery = checkLotto(lottery, winningNums, bonusNum)
-    printResults(wonLottery, money)
+    println("${lotteries.size}개를 구매했습니다.") // 구매한 로또 개수와 로또 번호들 출력
+    lotteries.forEach { println(it) }
+
+    println("\n당첨 번호를 입력해 주세요.") // 당첨 번호 입력
+    val winningNums = readWinningNums()
+
+    println("\n보너스 번호를 입력해 주세요.") // 보너스 번호 입력
+    val bonusNum = readBonusNum(winningNums)
+
+    // 입력받은 당첨 번호와 로또 목록을 비교
+    val result = lottoProcess(lotteries, winningNums, bonusNum)
+    printResult(result, money) // 결과 출력
 }
 
-// 사용자가 구입 금액 입력 함수
-fun purchaseMoney(): Int {
-    println("구입금액을 입력해 주세요.")
-    val money = Console.readLine().toIntOrNull()
-        ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
-    if (money < 1000 || money % 1000 != 0) {
-        throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+// 당첨 번호 가져오는 함수
+fun readWinningNums(): Set<Int> {
+    // 입력한 번호 쉼표로 분리 후 정수 변환
+    val nums = Console.readLine().split(",").map { it.trim().toIntOrNull() }
+    // 번호 중에 null이거나, 6개가 아닌 경우
+    if (nums.any { it == null } || nums.size != 6) {
+        throw IllegalArgumentException("[ERROR] 유효하지 않은 당첨 번호입니다.")
     }
-    return money
+
+    return nums.mapNotNull { it }.toSet()
 }
 
-// 구매한 로또 개수 입력 함수 (임의로 6개 설정)
-fun purchaseLotto(lottoCnt: Int): List<List<Int>> {
-    return List(lottoCnt) {
-        // 중복되지 않는 6개 숫자 설정
-        Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+// 보너스 번호 가져오는 함수
+fun readBonusNum(wonNumbers: Set<Int>): Int {
+    // 보너스 번호 정수로 변환
+    val bonus = Console.readLine().trim().toIntOrNull()
+    // 보너스 번호가 유효하지 않은 경우
+    if (bonus == null || bonus !in 1..45 || bonus in wonNumbers)
+        throw IllegalArgumentException("[ERROR] 유효하지 않은 보너스 번호입니다.")
+
+    return bonus
+}
+
+// 결과 계산 함수
+fun lottoProcess(
+    lotteries: List<Lotto>, winningNums: Set<Int>, bonusNums: Int): Map<Int, Int>
+{
+    // 결과를 저장할 맵 초기화
+    val results = mutableMapOf(
+        3 to 0, 4 to 0, 5 to 0, 5 to 0, 6 to 0
+    )
+
+    // 당첨 번호와 얼마나 일치하는지 계산
+    lotteries.forEach { lotto ->
+        val match = lotto.matches(winningNums)
+        // 5개가 일치하고 보너스 번호도 일치하는지 확인
+        val isBonus =
+            if (match == 5) lotto.contains(bonusNums)
+            else false
+
+        // 3개 이상 일치하는 경우
+        if (match >= 3) {
+            val key = if (isBonus) 5 else match
+            results[key] = results.getOrDefault(key, 0) + 1
+        }
     }
+    return results
 }
 
-// 구매한 로또 개수 출력하기
-fun printLotto(lotto: List<List<Int>>) {
-    println("${lotto.size}개를 구매했습니다.")
-    lotto.forEach { println(it) }
-}
+// 결과 출력 함수
+fun printResult(lottoResults: Map<Int, Int>, money: Int) {
+    // 각 당첨 별 상금 설정
+    val rank = mapOf(
+        3 to 5000, 4 to 50000, 5 to 1500000, 6 to 2000000000, 5 to 30000000)
+    var totalRank = 0
 
-// 당첨 번호 입력하는 함수
-fun winningNumber(): List<Int> {
-    println("당첨 번호를 입력해 주세요.")
-    return Console.readLine().split(",").map { it.trim().toIntOrNull()
-            ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
-    }
-}
-
-// 보너스 번호 입력하는 함수
-fun bonusNumber(): Int {
-    println("보너스 번호를 입력해 주세요.")
-    return Console.readLine().toIntOrNull()
-        ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
-}
-
-// 당첨 통계 및 수익률 출력 함수
-fun printResults(wonLottery: Lotto, money: Int) {
-    println("당첨 통계")
+    // 당첨 통계 출력
+    println("\n당첨 통계")
     println("---")
-    wonLottery.printStat()
-    println("총 수익률은 ${wonLottery.rateReturn(money)}입니다.")
-
-}
-
-// 당첨 번호 확인 함수
-fun checkLotto(lottery: List<List<Int>>, winningNums: List<Int>, bonusNum: Int): Lotto {
-    val result = Lotto(lottery)
-    for (ticket in lottery) {
-        val match = ticket.count { it in winningNums }
-        val isBounus = bonusNum in ticket
-        lotto.matchBonus(match, isBounus)
+    // 당첨 별 상금과 개수를 출력
+    lottoResults.forEach { (matchLotto, count) ->
+        // 5개가 일치하고 보너스 볼이 일치하는 경우
+        if (matchLotto == 5 && count > 0) {
+            println("${matchLotto}개 일치, 보너스 볼 일치 (30,000,000원) - ${lottoResults[5]}개")
+            // 상금 총액에 해당 상금 보너스
+            totalRank += lottoResults.getOrDefault(5, 0) * rank.getOrDefault(5, 0)
+        } else {
+            // 그 외의 경우 상금과 개수를 출력
+            println("${matchLotto}개 일치 (${rank.getOrDefault(matchLotto, 0)}원) - ${count}개")
+            // 상금 총액에 해당 상금을 더하기
+            totalRank += count * rank.getOrDefault(matchLotto, 0)
+        }
     }
-    return lotto
+    val returnRate = (totalRank.toDouble() / money) * 100 // 수익률 계산
+    println("총 수익률은 %.2f%%입니다.".format(returnRate)) // 수익률 출력
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,58 +1,23 @@
 package lotto
 
+// 로또 번호를 관리하는 Lotto 클래스 정의
 class Lotto(private val numbers: List<Int>) {
-    // 로또 번호가 유효한 숫자들인지 검증
-    init {
-        require(numbers.size == 6) {
-            "로또는 숫자 6개 입니다."
-        }
-        require(numbers.distinct().size == 6) {
-            "로또의 숫자는 서로 중복되지 않습니다."
-        }
-        require(numbers.all { it in 1..45 }) {
-            "로또는 1부터 45 중에서 결정됩니다."
-        }
-    }
-    // TODO: 추가 기능 구현
-    // 사용자가 구매한 로또와 당첨 번호가 얼마나 일치하는지 계산
-    fun match(winningNums: List<Int>): Int {
-        require(winningNums.size == 6) {
-            "당첨 번호는 6개의 중복되지 않은 수입니다."
-        }
-        return numbers.count { it in winningNums }
+    init { // Lotto 클래스가 인스턴스될 때 호출되는 초기화 블록
+        require(numbers.size == 6) { "로또 숫자는 반드시 6개여야 합니다." }
     }
 
-    // 보너스 숫자 검증
-    fun bonusNum(bonusNum: Int): Boolean {
-        require(bonusNum in 1..45) {
-            "보너스 숫자는 1부터 45 중에 있습니다."
-        }
+    // 당첨 번호와 몇 개가 일치하는 지 여부
+    // numbers 리스트에서 당첨 번호 숫자 개수 확인
+    fun matches(winningNums: Set<Int>): Int {
+        return numbers.count { winningNums.contains(it) }
+    }
+
+    // 보너스 번호가 이 로또 번호 안에 있는지 여부
+    // numbers 리스트에서 보너스 숫자 있는지 여부
+    fun contains(bonusNum: Int): Boolean {
         return bonusNum in numbers
     }
 
-    // 당첨 결과에 따른 통계 출력
-    fun printStat() {
-        val stat = numbers.groupBy { it }
-        println("3개 일치 (5,000원) - ${stat[3] ?: 0}개")
-        println("4개 일치 (50,000원) - ${stat[4] ?: 0}개")
-        println("5개 일치 (1,500,000원) - ${stat[5] ?: 0}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${stat[5 + 1] ?: 0}개")
-        println("6개 일치 (2,000,000,000원) - ${stat[6] ?: 0}개")
-    }
-
-    // 수익률 반환 함수
-    fun rateReturn(money: Int): String {
-        val winningMoney = mapOf(
-            3 to 5000, 4 to 50000, 5 to 1500000, 5 + 1 to 3000000, 6 to 2000000000
-        )
-        val totalMoney = numbers.sumOf { winningMoney[it] ?: 0 }
-        val rate = totalMoney.toDouble() / money.toDouble() * 100 // 수익률 계산
-        return "%.2f%%".format(rate)
-    }
-
-    fun matchBonus(matchCount: Int, isBonus: Boolean) {
-        if (matchCount == 5 && isBonus) {
-            numbers.a
-        }
-    }
+    // 객체가 문자열일 때 사용
+    override fun toString(): String = numbers.toString()
 }
