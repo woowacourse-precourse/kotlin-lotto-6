f88506e098c68dbde891ef2fca3ee5c6e8fef622
murjune <sis9221302@gmail.com>
2023-11-07 21:26:35 +0900

    add(DivideThousandValidator) : 1000으로 나눠지는지 검사

---
+++ src/main/kotlin/lotto/validator/string/DivideThousandValidator.kt
@@ -0,0 +1,18 @@
+package lotto.validator.string
+
+class DivideThousandValidator : StringValidator {
+    override fun validate(value: String) {
+        val digit = value.toInt()
+        require(digit.canDivideThousand()) {
+            DIVIDE_THOUSAND_ERROR_FORMAT.format(value)
+        }
+    }
+
+    private fun Int.canDivideThousand() = (this % DIVIDE_UNIT == ZERO)
+
+    companion object {
+        private const val DIVIDE_THOUSAND_ERROR_FORMAT = "구입 금액 %s 는 1000으로 나눠져야 합니다"
+        private const val DIVIDE_UNIT = 1000
+        private const val ZERO = 0
+    }
+}
