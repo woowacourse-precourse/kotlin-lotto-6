0fa22894a49435f3cb9445901de52efbad9cea4d
lee989898 <wnsrud1352@gmail.com>
2023-11-07 15:21:19 +0900

    [test] 발행한 로또가 오름차순으로 정렬되는지 확인 @ParameterizedTest ,@ValueSoruce로 변경

--- src/test/kotlin/lottonumbergenerator/LottoNumberGeneratorTest.kt
+++ src/test/kotlin/lottonumbergenerator/LottoNumberGeneratorTest.kt
@@ -1,10 +1,7 @@
 package lottonumbergenerator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.`in`
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.RepeatedTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
