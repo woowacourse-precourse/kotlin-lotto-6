cb8f6a72aab583db11513fe9e8bfa192bd32ea8c
applepepsi <dlaejrtnz@gmail.com>
2023-11-07 22:20:34 +0900

    fix(model/PurChaseMoneyVerification): Structural changes

--- src/main/kotlin/lotto/model/PurchaseMoneyVerification.kt
+++ src/main/kotlin/lotto/model/PurchaseMoneyVerification.kt
@@ -1,15 +1,19 @@
 package lotto.model
 
-import lotto.config.ExceptionMessage.UNIT_ERROR
 import lotto.config.GameConfigValue.PURCHASE_AMOUNT_UNIT
+import lotto.config.ExceptionMessage.UNIT_ERROR
 
-class PurchaseMoneyVerification {
-    fun inputMoneyCheck(lottoPurchaseMoney:String):Int{
-        val checkMoney=lottoPurchaseMoney.toIntOrNull()
 
-        if (checkMoney != null) {
-            return checkMoney / PURCHASE_AMOUNT_UNIT
-        } else {
+class PurchaseMoneyVerification(private val lottoPurchaseMoney:String) {
+    init{
+        moneyOnlyNumberCheck()
+        moneyOnlyThousand()
+    }
+    private fun moneyOnlyNumberCheck(){
+        lottoPurchaseMoney.toIntOrNull() ?: throw IllegalArgumentException(UNIT_ERROR)
+    }
+    private fun moneyOnlyThousand(){
+        if(lottoPurchaseMoney.toInt() % PURCHASE_AMOUNT_UNIT != 0){
             throw IllegalArgumentException(UNIT_ERROR)
         }
     }
