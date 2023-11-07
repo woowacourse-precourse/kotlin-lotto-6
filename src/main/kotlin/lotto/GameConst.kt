1074b136decf0bd5428c24fc0e5ea6422fbef155
K_Gs:D <rootachieve6053@gmail.com>
2023-11-07 21:48:00 +0900

    feat<Lotto> : init require 추가
    init에 있는 매직 넘버를 상수화하고, require을 몇개 더 추가했습니다.

--- src/main/kotlin/lotto/GameConst.kt
+++ src/main/kotlin/lotto/GameConst.kt
@@ -2,4 +2,5 @@ package lotto
 
 object GameConst {
     const val MAX_NUM = 45
+    const val NUM_COUNT = 6
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,11 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == GameConst.NUM_COUNT)
+        require(numbers.distinct().size == numbers.size)
+        require(numbers.all {
+            it in 1..GameConst.MAX_NUM
+        })
     }
 
     // TODO: 추가 기능 구현
