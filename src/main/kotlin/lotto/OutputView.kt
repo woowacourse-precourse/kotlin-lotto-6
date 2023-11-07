d8f96485208e2120ca8718190c9a541479986695
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 09:06:54 +0900

    Feat: 당첨 번호 입력 요구 문구 출력 구현

--- src/main/kotlin/lotto/OutputView.kt
+++ src/main/kotlin/lotto/OutputView.kt
@@ -10,9 +10,14 @@ class OutputView {
         println("\n$quantity$NUMBER_OF_PURCHASES")
     }
 
+    fun printRequestInputWinningNumber() {
+        println("\n$REQUEST_INPUT_WINNING_NUMBER")
+    }
+
     companion object {
         const val REQUEST_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
         const val NUMBER_OF_PURCHASES = "개를 구매했습니다."
+        const val REQUEST_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
     }
 
 }
