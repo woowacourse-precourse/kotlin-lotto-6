12a010bcab08395220b2a72320231bb8616cf9af
w36495 <w36495@naver.com>
2023-11-07 16:34:06 +0900

    feat : 당첨 금액 계산 기능 구현

--- src/main/kotlin/lotto/LottoWinResult.kt
+++ src/main/kotlin/lotto/LottoWinResult.kt
@@ -2,8 +2,10 @@ package lotto
 
 class LottoWinResult {
 
+    private var money: Long = 0L
     private val ranks = mutableListOf(0, 0, 0, 0, 0, 0)
 
+    fun getTotalMoney(): Long = money
     fun getRanks(): List<Int> = ranks.toList()
 
     fun calculateRank(ball: Int, bonus: Boolean) {
@@ -16,7 +18,21 @@ class LottoWinResult {
         }
     }
 
+    fun addPrizeMoney(ball: Int, bonus: Boolean) {
+        money += when (ball) {
+            6 -> Rank.ALL_MATCH.prize
+            5 -> calculatePrizeMoneyWithBonus(bonus)
+            4 -> Rank.FOUR_MATCH.prize
+            3 -> Rank.THREE_MATCH.prize
+            else -> Rank.NOT_MATCH.prize
+        }
+    }
+
     private fun calculateRankWithBonus(bonus: Boolean): Int {
         return if (bonus) Rank.FIVE_MATCH_WITH_BONUS.grade else Rank.FIVE_MATCH.grade
+    }
+
+    private fun calculatePrizeMoneyWithBonus(bonus: Boolean): Int {
+        return if (bonus) Rank.FIVE_MATCH_WITH_BONUS.prize else Rank.FIVE_MATCH.prize
     }
 }
