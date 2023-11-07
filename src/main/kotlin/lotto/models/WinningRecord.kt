b4e5fe5d174e84ac990f48a612fe4550b3cc2ebd
minuxx <minux8905@gmail.com>
2023-11-08 01:36:25 +0900

    refactor: 당첨 내역 업데이트 기능 개선
    recordWinningResults 메서드에서 record 업데이트 로직 분리 분리

--- src/main/kotlin/lotto/models/WinningRecord.kt
+++ src/main/kotlin/lotto/models/WinningRecord.kt
@@ -10,27 +10,31 @@ class WinningRecord {
 
     private fun initializeRecord() = WinningRank.entries.forEach { _record[it] = 0 }
 
-    fun calculateMatchingNumbers(purchasedLotto: Lotto, winningLotto: Lotto): Int {
+    fun recordWinningResults(purchasedLottos: List<Lotto>, winningLotto: Lotto, bonus: Bonus) {
+        purchasedLottos.forEach { purchasedLotto ->
+            val matchedCount = calculateMatchingNumbers(purchasedLotto, winningLotto)
+            val hasMatchingBonus = hasMatchingBonusNumber(purchasedLotto, bonus)
+            val winningRank = WinningRank.find(matchedCount, hasMatchingBonus)
+
+            updateRecord(winningRank)
+        }
+    }
+
+    internal fun calculateMatchingNumbers(purchasedLotto: Lotto, winningLotto: Lotto): Int {
         val purchasedLottoNumbers = purchasedLotto.getNumbers()
         val winningLottoNumbers = winningLotto.getNumbers()
 
         return purchasedLottoNumbers.count { it in winningLottoNumbers }
     }
 
-    fun hasMatchingBonusNumber(purchasedLotto: Lotto, bonus: Bonus): Boolean {
+    internal fun hasMatchingBonusNumber(purchasedLotto: Lotto, bonus: Bonus): Boolean {
         val bonusNumber = bonus.getNumber()
         val purchasedLottoNumbers = purchasedLotto.getNumbers()
 
         return bonusNumber in purchasedLottoNumbers
     }
 
-    fun recordWinningResults(purchasedLottos: List<Lotto>, winningLotto: Lotto, bonus: Bonus) {
-        purchasedLottos.forEach { purchasedLotto ->
-            val matchedCount = calculateMatchingNumbers(purchasedLotto, winningLotto)
-            val hasMatchingBonus = hasMatchingBonusNumber(purchasedLotto, bonus)
-
-            val winningRank = WinningRank.find(matchedCount, hasMatchingBonus)
-            _record[winningRank] = (_record[winningRank] ?: 0) + 1
-        }
+    private fun updateRecord(winningRank: WinningRank) {
+        _record[winningRank] = (_record[winningRank] ?: 0) + 1
     }
 }
