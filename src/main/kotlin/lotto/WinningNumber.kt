eca9a22b17da61bf3060780c219f14b2dfa84fb6
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 12:55:27 +0900

    Feat: 당첨 번호 유효성 검사 기능(1~45가 아닌 숫자를 입력했을 때)

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -7,6 +7,7 @@ class WinningNumber(private val numbers: String) {
     init {
         require(checkNumbersOfCommas()) { "$ERROR $PLEASE_SEPARATE_NUMBERS_COMMAS" }
         require(isNumberOrNull()) { "$ERROR $INPUT_ONLY_NUMBER" }
+        require(numbersOutOfRange()) { "$ERROR $WINNING_NUMBER_RANGE" }
     }
 
     private fun checkNumbersOfCommas(): Boolean {
@@ -17,10 +18,17 @@ class WinningNumber(private val numbers: String) {
         return winningNumbers.all { it.toIntOrNull() != null }
     }
 
+    private fun numbersOutOfRange(): Boolean {
+        return winningNumbers.all { it.toInt() in WINNING_MIN_NUMBER..WINNING_MAX_NUMBER }
+    }
+
     companion object {
         const val ERROR = "[ERROR]"
         const val PLEASE_SEPARATE_NUMBERS_COMMAS = "당첨 번호는 쉼표(,) 기준으로 구분해주세요."
         const val INPUT_ONLY_NUMBER = "당첨 번호는 숫자만 입력 가능합니다."
+        const val WINNING_NUMBER_RANGE = "당첨 번호는 1부터 45사이의 숫자여야 합니다."
+        const val WINNING_MIN_NUMBER = 1
+        const val WINNING_MAX_NUMBER = 45
     }
 
 }
