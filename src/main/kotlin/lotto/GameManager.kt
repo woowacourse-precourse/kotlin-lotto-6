45ce3c7f28a25beb212382c18881da3acce9f2bc
Jbrochan <hyeongchanj@gmail.com>
2023-11-07 11:37:47 +0900

    feat: 발행 로또 번호 생성 기능 구현

--- src/main/kotlin/lotto/GameManager.kt
+++ src/main/kotlin/lotto/GameManager.kt
@@ -1,10 +1,35 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class GameManager {
     private val inputManager = InputManager()
+    private var money = 0
+    private var lottoList = ArrayList<Lotto>()
 
     fun runLottoGame() {
         // 사용자 구입 금액 입력
-        inputManager.getMoney()
+        money = inputManager.getMoney()
+
+        // 발행 로또 번호 생성
+        getLottoTickets(money)
+    }
+
+    private fun getLottoTickets(money: Int) {
+        val numberOfLottoTickets = money % LOTTO_PRICE
+
+        for (i in 1..numberOfLottoTickets) {
+            lottoList.add(generateLotto())
+        }
+    }
+
+    private fun generateLotto(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+
+        return Lotto(numbers)
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
     }
 }
