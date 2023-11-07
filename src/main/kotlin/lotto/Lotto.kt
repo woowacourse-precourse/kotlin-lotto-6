42350e13a106784f03dc14412e08fc368913b652
murjune <sis9221302@gmail.com>
2023-11-07 22:42:05 +0900

    feat(Lotto) : 로또 유효성 검사 로직

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,29 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.isSizeOfSix()) { LENGTH_ERROR }
+        require(numbers.isNotDuplicated()) { DUPLICATED_ERROR }
+        require(numbers.isInRange()) { RANGE_ERROR }
     }
 
-    // TODO: 추가 기능 구현
+    private fun List<Int>.isSizeOfSix() = (size == LOTTO_NUMBER_SIZE)
+
+    private fun List<Int>.isNotDuplicated() = (distinct().size == size)
+
+    private fun List<Int>.isInRange() = all { it in RANGE }
+
+    }
+
+    override fun hashCode(): Int {
+}
+
+    companion object {
+        private const val MIN_LENGTH = 1
+        private const val MAX_LENGTH = 45
+        private const val LOTTO_NUMBER_SIZE = 6
+        private val RANGE = MIN_LENGTH..MAX_LENGTH
+        const val LENGTH_ERROR = "로또 번호는 6개여야 합니다"
+        const val DUPLICATED_ERROR = "로또 번호는 중복되면 안됩니다"
+        const val RANGE_ERROR = "로또 번호는 1 ~ 45 사이여야 합니다"
+    }
 }
