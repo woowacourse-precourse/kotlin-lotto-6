98d08ec4dcb6d14d2eb4aa82b16b039de1b71618
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 17:27:49 +0900

    Refactor: 보너스 번호 최소, 최대값 상수로 정의

--- src/main/kotlin/lotto/BonusNumber.kt
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -16,7 +16,7 @@ class BonusNumber(
     }
 
     private fun numbersOutOfRange(): Boolean {
-        return number.toInt() in 1..45
+        return number.toInt() in BONUS_MIN_NUMBER..BONUS_MAX_NUMBER
     }
 
     private fun isIncludeInWinningNumber(): Boolean {
@@ -28,6 +28,8 @@ class BonusNumber(
         const val INPUT_ONLY_NUMBER = "보너스 번호는 숫자만 입력 가능합니다."
         const val BONUS_NUMBER_RANGE = "보너스 번호는 1부터 45사이의 숫자여야 합니다."
         const val NOT_INCLUDE_WINNING_NUMBER = "보너스 번호는 당첨 번호에 포함된 숫자일 수 없습니다."
+        const val BONUS_MIN_NUMBER = 1
+        const val BONUS_MAX_NUMBER = 45
     }
 
 }
