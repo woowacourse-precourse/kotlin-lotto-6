ccd020e43c22cac05b990389e2415f2eb6dd406c
oh in seong <superohinsung@naver.com>
2023-11-07 22:11:29 +0900

    [refactor] Modify code formatting

--- src/main/kotlin/lotto/Printer.kt
+++ src/main/kotlin/lotto/Printer.kt
@@ -4,7 +4,7 @@ class Printer {
 
     fun printEnterPurchaseAnnouncement() = println(Message.ENTER_PURCHASE)
 
-    fun printBuyNPiecesAnnouncement() = println(Message.BOUGHT_N_PIECES)
+    fun printBuyNPiecesAnnouncement(purchaseNumber: Int) = println("$purchaseNumber${Message.BOUGHT_N_PIECES}")
 
     fun printEnterUserPickNumbersAnnouncement() = println(Message.ENTER_USER_PICK_NUMBERS)
 
@@ -14,7 +14,7 @@ class Printer {
 
     fun printCommaAnnouncement() = println(Message.COMMA)
 
-    fun printLottoNumber(lottery: List<Lotto>) {
+    fun printRandomLottoNumber(lottery: List<Lotto>) {
         repeat(lottery.size) {
             println(lottery[it].formatNumbers())
         }
@@ -22,8 +22,9 @@ class Printer {
 
     fun printWinningStatistics(types: List<LottoWinType>, yield: String) {
         for (winType in LottoWinType.entries) {
+            val formattedNumber = "%,d".format(winType.prize)
             val count = types.count { it == winType }
-            println("${winType.description} ( ${winType.prize}원) - ${count}개")
+            println("${winType.description} (${formattedNumber}원) - ${count}개")
         }
         println("총 수익률은 ${yield}입니다.")
     }
