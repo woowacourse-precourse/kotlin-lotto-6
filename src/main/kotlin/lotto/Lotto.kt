b1da0f6d9f8806d1d0b12eaf68cfb09926574fd7
jklom0326 <jklom0326@gmail.com>
2023-11-08 00:54:21 +0900

    refactor: 유효성 검사 함수 분리 및 수정
    - 유효성 검사를 함수로 분리
    - 문자열 상수화

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,18 +3,27 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        require(numbers.size == 6) { NUMBER_SIZE_ERROR }
-        require(numbers.distinct().size == 6) { NUMBER_DUPLICATE_ERROR }
+        require(isLottoSize()) { NUMBER_SIZE_ERROR }
+        require(isDuplicated()) { NUMBER_DUPLICATE_ERROR }
         numbers.forEach {
-            require(it in 1..45) { NUMBER_NOT_IN_RANGE_ERROR }
+            require(isNumberInRange(it)) { NUMBER_NOT_IN_RANGE_ERROR }
         }
     }
+
+    private fun isNumberInRange(it: Int) = it in MIN_NUM..MAX_NUM
+
+    private fun isDuplicated() = numbers.distinct().size == LOTTO_SIZE
+
+    private fun isLottoSize() = numbers.size == LOTTO_SIZE
 
     fun getLottoNumbers(): List<Int> {
         return numbers
     }
 
     companion object {
+        const val LOTTO_SIZE = 6
+        const val MIN_NUM = 1
+        const val MAX_NUM = 45
         const val NUMBER_SIZE_ERROR = "[ERROR] 번호를 6개만 입력해주세요"
         const val NUMBER_DUPLICATE_ERROR = "[ERROR] 중복된 번호가 있습니다."
         const val NUMBER_NOT_IN_RANGE_ERROR = "[ERROR] 1부터 45 사이의 수를 입력해주세요."
