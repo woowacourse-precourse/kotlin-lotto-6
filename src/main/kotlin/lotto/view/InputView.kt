b1a5232d3158f88d234c16c8397f99254e1079de
Jinhwan_ideapad_ubuntu <xsest55@ajou.ac.kr>
2023-11-07 17:06:38 +0900

    feat: input Winning Numbers

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -13,5 +13,26 @@ class InputView {
             throw IllegalArgumentException("[ERROR] 구입 금액은 1000으로 나눌수 있는 숫자여야 합니다.")
         }
     }
-
+    fun inputWinningNum(): List<Int> {
+        val line: String = Console.readLine()
+        try {
+            val tokens: List<String> = line.split(Regex(",\\s*"))
+            val nums: List<Int> = tokens.map { it.toInt() }
+            if (nums.size != 6)
+                throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.\n")
+            if (nums.size != nums.distinct().size)
+                throw IllegalArgumentException("[ERROR] 로또 번호는 중복이 없는 숫자여야 합니다.\n")
+            nums.forEach { num ->
+                if (num > 45 || num < 1)
+                    throw(java.lang.Exception())
+            }
+            return nums
+        } catch(e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.\n")
+        } catch (e: IllegalArgumentException){
+            throw e
+        } catch (e: Exception){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n")
+        }
+    }
 }
