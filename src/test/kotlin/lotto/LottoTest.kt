7c6decb719248a4d669648153ac4bf7720a74687
Nohsein <si2238@naver.com>
2023-11-08 03:43:23 +0900

    test: 당첨 통계 테스트
    로또 당첨 개수 테스트
    수익률 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,10 +1,20 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
-import org.junit.jupiter.api.Assertions
+import lotto.Constants.FIFTH_PLACE
+import lotto.Constants.FIRST_PLACE
+import lotto.Constants.FOURTH_PLACE
+import lotto.Constants.SECOND_PLACE
+import lotto.Constants.THIRD_PLACE
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 
 class LottoTest {
@@ -34,5 +44,48 @@ class LottoTest {
             }
             assertTrue(result.size == 3)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1,2,3,4,5,6,${FIRST_PLACE}",
+        "1,2,3,4,5,7,${SECOND_PLACE}",
+        "1,2,3,4,5,9,${THIRD_PLACE}",
+        "1,2,3,4,8,9,${FOURTH_PLACE}",
+        "1,2,3,7,8,9,${FIFTH_PLACE}"
+    )
+    fun `로또 등수 테스트`(arg1: Int, arg2: Int, arg3: Int, arg4: Int, arg5: Int, arg6: Int, place: Int) {
+        assertSimpleTest {
+            val lotto = listOf(arg1, arg2, arg3, arg4, arg5, arg6)
+            val result = Lotto(lotto).isWin(listOf(1, 2, 3, 4, 5, 6), 7)
+            assertEquals(place, result)
+        }
+    }
+
+    @Test
+    fun `당첨 통계 테스트`() {
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+
+        assertSimpleTest {
+            val lotto = mutableListOf(
+                listOf(1, 2, 3, 4, 5, 6),
+                listOf(1, 2, 3, 4, 5, 7),
+                listOf(1, 2, 3, 10, 11, 12)
+            )
+            Statistics(3000).winLotto(lotto, listOf(1, 2, 3, 4, 5, 6), 7)
+        }
+
+        System.setOut(System.out)
+        val printedOutput = outputStream.toString()
+
+        assertThat(printedOutput).contains(
+            "3개 일치 (5,000원) - 1개",
+            "4개 일치 (50,000원) - 0개",
+            "5개 일치 (1,500,000원) - 0개",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+            "6개 일치 (2,000,000,000원) - 1개",
+            "총 수익률은 67,666,833.3%입니다."
+        )
     }
 }
