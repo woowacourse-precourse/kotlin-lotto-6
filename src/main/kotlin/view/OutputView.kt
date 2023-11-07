3b3bd070583bfd1549df426b9ba761bcac9e17ee
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:03:48 +0900

    [feat] "구입금액을 입력해 주세요." 출력 메서드 구현

---
+++ src/main/kotlin/view/OutputView.kt
@@ -0,0 +1,9 @@
+package view
+
+object OutputView {
+    private const val PLEASE_ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+
+    fun pleaseEnterPurchaseAmount() {
+        println(PLEASE_ENTER_PURCHASE_AMOUNT)
+    }
+}
