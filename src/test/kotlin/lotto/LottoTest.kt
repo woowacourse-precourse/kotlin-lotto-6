b749f6b1964ddbcc8bd1f7026ac3c1eef6153146
getupminaaa <getupmina@gmail.com>
2023-11-07 14:09:35 +0900

    test: lotto 결과를 반환하는 메서드에 대해 테스트 코드 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,25 +1,20 @@
 package lotto
 
 import lotto.model.Lotto
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class LottoTest {
-    @Test
-    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-        }
-    }
+    private val lotto = Lotto(listOf(1,2,3,4,5,6))
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
-    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
-        }
+    @DisplayName("로또에 당첨되었는지 확인하고 로또 결과를 반환한다.")
+    fun checkLottoWinningTest(){
+        val bonusNum = 7
+        val lottoLists = listOf(listOf(8,21,23,41,42,43), listOf(1,2,3,5,6,7), listOf(1,3,4,5,9,21))
+        val expected = listOf(0,100,4)
+        val actualValue = lotto.checkLottoWinning(bonusNum,lottoLists)
+        assertEquals(expected,actualValue)
     }
-
-    // 아래에 추가 테스트 작성 가능
 }
