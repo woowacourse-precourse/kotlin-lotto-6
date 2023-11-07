996d544f53b05e6c9c1e292c6715c5a6e247d0a6
lee989898 <wnsrud1352@gmail.com>
2023-11-07 18:50:15 +0900

    [feat] compare 메서드 반환 값 제거

--- src/main/kotlin/lottonumbercomparator/LottoNumberComparator.kt
+++ src/main/kotlin/lottonumbercomparator/LottoNumberComparator.kt
@@ -1,10 +1,9 @@
 package lottonumbercomparator
 
 import lotto.Lotto
-import lottoranking.LottoRanking
 import winningnumber.WinningNumber
 
 fun interface LottoNumberComparator {
 
-    fun compare(winningNumber: WinningNumber, lotto: Lotto): LottoRanking
+    fun compare(winningNumber: WinningNumber, lotto: Lotto)
 }
