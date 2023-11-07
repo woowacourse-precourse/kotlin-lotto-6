06bd193b5c9b5bb697d6f8a1af5ba52154799388
stopstone <wltjr6432@naver.com>
2023-11-07 20:22:46 +0900

    refactor: 당첨통계 하드코딩 제거
    Prize enum class에 문자열을 추가하여 출력 시 하드코딩을 방지한다.

--- src/main/kotlin/lotto/domain/LottoRanks.kt
+++ src/main/kotlin/lotto/domain/LottoRanks.kt
@@ -2,12 +2,12 @@ package lotto.domain
 
 import lotto.Lotto
 
-enum class Prize(val matchingNumbers: Int, val prizeAmount: Int) {
-    THREE(3, 5000),
-    FOUR(4, 50000),
-    FIVE(5, 1500000),
-    FIVE_MATCH_WITH_BONUS(5, 30000000),
-    SIX(6, 2000000000)
+enum class Prize(val matchingNumbers: Int, val prizeAmount: Int, val message: String) {
+    THREE(3, 5_000, "3개 일치"),
+    FOUR(4, 50_000, "4개 일치"),
+    FIVE(5, 1_500_000, "5개 일치"),
+    FIVE_MATCH_WITH_BONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    SIX(6, 2_000_000_000, "6개 일치")
 }
 
 class LottoRanks {
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -36,11 +36,9 @@ object OutputView {
     }
 
     fun printResults(results: MutableMap<Prize, Int>) {
-        println("3개 일치 (5,000원) - ${results[Prize.THREE] ?: 0}개")
-        println("4개 일치 (50,000원) - ${results[Prize.FOUR] ?: 0}개")
-        println("5개 일치 (1,500,000원) - ${results[Prize.FIVE] ?: 0}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${results[Prize.FIVE_MATCH_WITH_BONUS] ?: 0}개")
-        println("6개 일치 (2,000,000,000원) - ${results[Prize.SIX] ?: 0}개")
+        Prize.entries.forEach {
+            println("${it.message} (${it.prizeAmount}원) - ${results[it]}개")
+        }
     }
 
 
