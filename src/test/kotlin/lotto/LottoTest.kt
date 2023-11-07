affcac6273e94cbe52ce939af60ad07a42a2627b
jujeong <jujeong0108@naver.com>
2023-11-08 03:35:07 +0900

    Created some testcase. Minor fixes.

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -4,7 +4,6 @@ import lotto.model.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-
 class LottoTest {
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
@@ -20,6 +19,16 @@ class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
-
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호 중 45를 초과하는 값이 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+    }
+    @Test
+    fun `로또 번호 중 0 이하인 값이 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 2, 3, 4, 5, 6))
+        }
+    }
 }
