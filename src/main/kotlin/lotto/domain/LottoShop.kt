6d82c519ba2d6b31e0a1867be8e4f036e152b47d
tgyuu-An <atg0614@gmail.com>
2023-11-08 01:47:33 +0900

    test : LottoShop 기능 구현 전, 테스트 코드 작성

--- src/main/kotlin/lotto/domain/LottoShop.kt
+++ src/main/kotlin/lotto/domain/LottoShop.kt
@@ -1,4 +1,4 @@
 package lotto.domain
 
-class LottoShop {
+class LottoShop() {
 }
--- src/test/kotlin/lotto/domain/LottoShopTest.kt
+++ src/test/kotlin/lotto/domain/LottoShopTest.kt
@@ -1,5 +1,82 @@
 package lotto.domain
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-class LottoShopTest
+class LottoShopTest {
+
+    @Test
+    fun `로또 구매 가격으로 문자를 입력받을 수 없다`() {
+        //given
+        val lottoShop = LottoShop()
+
+        //when
+
+
+        //then
+        assertThrows(IllegalArgumentException) {
+            lottoShop.purchaseLotto("abcd")
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "101", "500", "1001", "1500"])
+    fun `로또 구매 가격으로 1000의 배수가 아닌 수를 입력받을 수 없다`(input : String) {
+        //given
+        val lottoShop = LottoShop()
+
+        //when
+
+
+        //then
+        assertThrows(IllegalArgumentException) {
+            lottoShop.purchaseLotto(input)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1000", "2000", "3000", "4000", "5000"])
+    fun `로또 구매 가격으로 1000의 배수만을 입력받을 수 있다`(input : String) {
+        //given
+        val lottoShop = LottoShop()
+
+        //when
+
+
+        //then
+        assertDoesNotThrow {
+            lottoShop.purchaseLotto(input)
+        }
+    }
+
+    @Test
+    fun `로또 구매 가격으로 0원을 입력받을 수 없다`() {
+        //given
+        val lottoShop = LottoShop()
+
+        //when
+
+
+        //then
+        assertThrows(IllegalArgumentException) {
+            lottoShop.purchaseLotto("0")
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1000", "2000", "3000", "4000", "5000"])
+    fun `로또는 한 장당 1000원에 구매할 수 있다`(input : String) {
+        //given
+        val lottoShop = LottoShop()
+
+        //when
+
+
+        //then
+        val actual = lottoShop.purchaseLotto(input)
+        val excepted = (input.toInt())/1000
+        assertThat(actual).isEqualTo(excepted)
+    }
+}
