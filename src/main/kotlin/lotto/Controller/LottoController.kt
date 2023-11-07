0dcf2dc6232cecd54f7bd7db866b08945ef4379b
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-11-07 16:02:28 +0900

    (fix) 로또 구입 금액 에러사항 처리

--- src/main/kotlin/lotto/Controller/LottoController.kt
+++ src/main/kotlin/lotto/Controller/LottoController.kt
@@ -1,23 +1,32 @@
 package lotto.Controller
 
 fun howManyBuyLotto(lottoPrice: String): Int {
-
-//    lottoPrice.toInt()
-//
-//    if(lottoPrice%1000==0){
-//        return lottoPrice/1000
-//    }
-//    //TODO : Exceptions 처리
-    return -1
+    try {
+        val price = lottoPrice.toInt()
+        if (price % 1000 == 0) {
+            return price / 1000
+        } else {
+            // 1000으로 나누어 떨어지지 않는 경우 예외 던지기
+            throw IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.")
+        }
+    } catch (e: NumberFormatException) {
+        // 문자열을 정수로 변환할 수 없는 경우 예외 던지기
+        throw IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다. 구입 금액을 숫자로 입력해 주세요.")
+    }
 }
 
 object LottoController {
-    fun gameStart(){
+    fun gameStart() {
         println("구입금액을 입력해 주세요.")
         val lottoPrice = readLine()
 
-//        if (lottoPrice != null) {
-//            howManyBuyLotto(lottoPrice)
-//        }
+        if (lottoPrice != null) {
+            try {
+                val numberOfLottoTickets = howManyBuyLotto(lottoPrice)
+                println("${numberOfLottoTickets}개를 구매했습니다.")
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ${e.message}")
+            }
+        }
     }
 }
