0628f8742713010b85eca3202e578a5a82c6e6d8
leejieuns2 <2jjingni@gmail.com>
2023-11-08 05:03:31 +0900

    style: move package
    - WinningLotto Class package path 변경

--- src/main/kotlin/lotto/domain/model/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/model/WinningLotto.kt
@@ -1,9 +1,8 @@
-package lotto.domain
+package lotto.domain.model
 
-import lotto.domain.model.Lotto
 import lotto.util.Constants
 
-class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: Int) {
+class WinningLotto(val winningNumbers: Lotto, val bonusNumber: Int) {
     init {
         require(bonusNumber in Constants.LOTTO_START_NUM..Constants.LOTTO_END_NUM) {
             BONUS_NUM_RANGE_ERROR
@@ -12,6 +11,8 @@ class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: I
             BONUS_NUM_DUPLICATE_ERROR
         }
     }
+
+    fun contains(lottoNumber: Int): Boolean = winningNumbers.contains(lottoNumber)
 
     companion object {
         private const val BONUS_NUM_RANGE_ERROR =
