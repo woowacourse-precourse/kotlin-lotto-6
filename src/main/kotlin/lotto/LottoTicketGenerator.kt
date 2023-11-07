816f774a1efe16277acedcd91728ed0ad5d8c655
stopkite <stopkite98@gmail.com>
2023-11-07 19:43:12 +0900

    refactor(LottoTicketGenerator): 로또 관련 숫자 상수화

--- src/main/kotlin/lotto/LottoTicketGenerator.kt
+++ src/main/kotlin/lotto/LottoTicketGenerator.kt
@@ -3,7 +3,11 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoTicketGenerator {
-    fun createAutoTicket() = Lotto(
-        Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted().toList()
-    )
+    fun createAutoTicket() = Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT).sorted().toList())
+
+    companion object {
+        const val MIN_LOTTO_NUMBER = 1
+        const val MAX_LOTTO_NUMBER = 45
+        const val LOTTO_NUMBER_COUNT = 6
+    }
 }
