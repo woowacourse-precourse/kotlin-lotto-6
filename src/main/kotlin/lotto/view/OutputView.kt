44333bc68b59ca5e9716257ae139392588108f4e
MinhoiKoo <minlove2013@gmail.com>
2023-11-07 12:37:59 +0900

    feat: 플레이어에게 입력 받는 함수 추가
    -매직 리터럴 제거

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,12 +5,12 @@ import lotto.domain.WinningRank
 
 class OutputView {
 
-    fun printPurchaseMoney() {
-        println("구입금액을 입력해 주세요.")
+    fun printAskPurchaseMoney() {
+        println(INPUT_MONEY_MESSAGE)
     }
 
     fun printPurchaseLottoQuantity(quantity: Int) {
-        println("${quantity}개를 구매했습니다.")
+        println("$quantity$PURCHASE_LOTTO_QUANTITY_MESSAGE")
     }
 
     fun printLotto(lotto: List<Lotto>) {
@@ -19,10 +19,29 @@ class OutputView {
         }
     }
 
+    fun printAskWinningNumber() {
+        println(INPUT_WINNING_NUMBERS_MESSAGE)
+    }
+
+    fun printAskBonusNumber() {
+        println(INPUT_BONUS_NUMBER)
+    }
+
     fun printRank(rank: Map<WinningRank, Int>) {
+        println(WINNING_RANK_MESSAGE)
+        println(SEPARATOR)
         rank.forEach {
             println("${it.key.message} (${it.key.winningPrize}원) - ${it.value}개")
         }
+    }
+
+    companion object {
+        private const val INPUT_MONEY_MESSAGE = "구입 금액을 입력해 주세요."
+        private const val PURCHASE_LOTTO_QUANTITY_MESSAGE= "개를 구매했습니다."
+        private const val INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
+        private const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+        private const val WINNING_RANK_MESSAGE = "당첨 통계"
+        private const val SEPARATOR = "---"
     }
 
 }
