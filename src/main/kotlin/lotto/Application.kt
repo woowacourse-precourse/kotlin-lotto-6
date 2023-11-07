ac5cf8e1d74fb41b59d5294f30267ce02a39d286
ls2hn <s2hnsu@gmail.com>
2023-11-08 05:55:01 +0900

    feat: (입력값 % 1000) != 0인 경우에 대한 예외 처리 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,12 +2,37 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 
+const val ERROR = "[ERROR]"
+const val NOT_MULTIPLE_OF_1000_ERROR = " 구입금액은 1000의 배수여야 합니다. 다시 입력해 주세요."
+
 fun main() {
     println("구입금액을 입력해 주세요.")
-    print( getInputMoney() )
+    validateInputMoney( getInputMoney() )
 }
 
 fun getInputMoney(): Int {
 
     return Console.readLine().toInt()
+}
+
+fun checkException(inputMoney: Int) {
+    require(inputMoney % 1000 == 0) { throw IllegalArgumentException(ERROR+NOT_MULTIPLE_OF_1000_ERROR) }
+
+}
+
+fun validateInputMoney(inputMoney: Int) {
+    var isValidInput = false
+
+    while (!isValidInput) {
+        try {
+            checkException(inputMoney)
+            isValidInput = true
+
+        } catch (e: IllegalArgumentException) {
+            println("${e.message}")
+            getInputMoney()
+
+        }
+
+    }
 }
