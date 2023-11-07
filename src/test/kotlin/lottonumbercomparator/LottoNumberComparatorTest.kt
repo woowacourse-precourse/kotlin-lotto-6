155173db85fc902565146d0ef6dc4cb76f863854
lee989898 <wnsrud1352@gmail.com>
2023-11-07 18:51:11 +0900

    [test] generateData에 LottoRanking 데이터 추가

--- src/test/kotlin/lottonumbercomparator/LottoNumberComparatorTest.kt
+++ src/test/kotlin/lottonumbercomparator/LottoNumberComparatorTest.kt
@@ -24,21 +24,27 @@ class LottoNumberComparatorTest {
 
     @ParameterizedTest
     @MethodSource("generateData")
-    fun `각 등수 당첨됐을 때 각 등수 갯수가 오르는지 확인`(winningNumber: List<Int>, bonusNumber: Int, lotto: List<Int>, output: Int) {
-        val reuslt = lottoNumberComparator.compare(WinningNumber(winningNumber, bonusNumber), Lotto(lotto))
+    fun `각 등수 당첨됐을 때 각 등수 갯수가 오르는지 확인`(
+        winningNumber: List<Int>,
+        bonusNumber: Int,
+        lotto: List<Int>,
+        lottoRanking: LottoRanking,
+        output: Int,
+    ) {
+        lottoNumberComparator.compare(WinningNumber(winningNumber, bonusNumber), Lotto(lotto))
 
-        assertThat(reuslt.count).isEqualTo(output)
+        assertThat(lottoRanking.count).isEqualTo(output)
     }
 
     companion object {
         @JvmStatic
         fun generateData(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 6), 1),
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 7), 1),
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 45), 1),
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 44, 45), 1),
-                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 43, 44, 45), 1)
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 6), LottoRanking.FIRST, 1),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 7), LottoRanking.SECOND, 1),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 5, 45), LottoRanking.THIRD, 1),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 4, 44, 45), LottoRanking.FOURTH, 1),
+                Arguments.of(listOf(1, 2, 3, 4, 5, 6), 7, listOf(1, 2, 3, 43, 44, 45), LottoRanking.FIFTH, 1),
             )
         }
     }
