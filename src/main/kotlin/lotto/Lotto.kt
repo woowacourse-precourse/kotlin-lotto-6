f5e05c291b96fb0768fa32efa3aac41d1fca8d9e
jklom0326 <jklom0326@gmail.com>
2023-11-07 16:59:19 +0900

    refactor: 예외 메시지 문자열 상수화

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,14 +3,20 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        require(numbers.size == 6) { "[ERROR]: 번호를 6개만 입력해주세요" }
-        require(numbers.distinct().size == 6) { "[ERROR]: 중복된 번호가 있습니다." }
+        require(numbers.size == 6) { NUMBER_SIZE_ERROR }
+        require(numbers.distinct().size == 6) { NUMBER_DUPLICATE_ERROR }
         numbers.forEach {
-            require(it in 1..45) { "[ERROR]: 1부터 45 사이의 수를 입력해주세요." }
+            require(it in 1..45) { NUMBER_NOT_IN_RANGE_ERROR }
         }
     }
 
     fun getLottoNumbers(): List<Int> {
         return numbers
+    }
+
+    companion object {
+        const val NUMBER_SIZE_ERROR = "[ERROR] 번호를 6개만 입력해주세요"
+        const val NUMBER_DUPLICATE_ERROR = "[ERROR] 중복된 번호가 있습니다."
+        const val NUMBER_NOT_IN_RANGE_ERROR = "[ERROR] 1부터 45 사이의 수를 입력해주세요."
     }
 }
