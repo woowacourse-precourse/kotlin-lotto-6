7a348a5d587de9eb811d77fd5957760cacd7c0c5
SeongWooHong <pluto0303@naver.com>
2023-11-08 01:53:28 +0900

    feat: add Rotto number generator

--- src/main/kotlin/lotto/DecideRottoNumber.kt
+++ src/main/kotlin/lotto/DecideRottoNumber.kt
@@ -3,5 +3,7 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class DecideRottoNumber {
+    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
 
+    fun decideRottoNumber() = numbers
 }
