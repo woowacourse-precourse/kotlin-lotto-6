7a3d0def2682b70ae9832573637a7cef95dd0956
gretea5 <gretea4489@gmail.com>
2023-11-07 23:36:32 +0900

    chore: 로또 번호 중복 예외처리 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-        require(numbers.toSet().size != 6) { "[ERROR] 중복된 숫자가 올 수 없습니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 중복된 숫자가 올 수 없습니다." }
         require(numbers.all { it in 1..45 }) { "[ERROR] 숫자는 1에서 45 사이에 있어야 합니다."}
     }
 
