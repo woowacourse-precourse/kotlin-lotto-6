533ccaaf5d2db135cf70cc15f374aeb0c647f300
j2j2 <fineimagination@gmail.com>
2023-11-08 01:34:16 +0900

    refactor(lotto): name 등수를 유추할 수 있게 수정

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -1,29 +1,29 @@
 package lotto.domain
 
-enum class LottoRank(val matchCount: Int, val prize: Int) {
-    NO_MATCH(0, 0),
-    THREE_MATCHES(3, 5_000),
-    FOUR_MATCHES(4, 50_000),
-    FIVE_MATCHES(5, 1_500_000),
-    FIVE_AND_BONUS_MATCHES(5, 30_000_000),
-    SIX_MATCHES(6, 2_000_000_000);
+enum class LottoRank(private val matchCount: Int, val prize: Int) {
+    OUT_OF_RANK(-1, 0),
+    FIFTH_PLACE(3, 5_000),
+    FOURTH_PLACE(4, 50_000),
+    THIRD_PLACE(5, 1_500_000),
+    SECOND_PLACE(5, 30_000_000),
+    FIRST_PLACE(6, 2_000_000_000);
 
     companion object {
         fun fromMatches(matches: Int, bonus: Boolean): LottoRank {
             return when {
-                matches == 6 -> SIX_MATCHES
-                matches == 5 && bonus -> FIVE_AND_BONUS_MATCHES
-                matches == 5 -> FIVE_MATCHES
-                matches == 4 -> FOUR_MATCHES
-                matches == 3 -> THREE_MATCHES
-                else -> NO_MATCH
+                matches == 6 -> FIRST_PLACE
+                matches == 5 && bonus -> SECOND_PLACE
+                matches == 5 -> THIRD_PLACE
+                matches == 4 -> FOURTH_PLACE
+                matches == 3 -> FIFTH_PLACE
+                else -> OUT_OF_RANK
             }
         }
     }
 
     override fun toString(): String {
         return when (this) {
-            FIVE_AND_BONUS_MATCHES -> "5개 일치, 보너스 볼 일치 (${prize.toFormattedString()})"
+            SECOND_PLACE -> "5개 일치, 보너스 볼 일치 (${prize.toFormattedString()})"
             else -> "${matchCount}개 일치 (${prize.toFormattedString()})"
         }
     }
