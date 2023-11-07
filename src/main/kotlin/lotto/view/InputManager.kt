eae0e5987f1a5c55feaba8734c26b1897173232d
YuBeen-Park <park_ubin@naver.com>
2023-11-07 14:54:48 +0900

    mod(OutputManager) : class에서 object로 수정

--- src/main/kotlin/lotto/view/InputManager.kt
+++ src/main/kotlin/lotto/view/InputManager.kt
@@ -4,10 +4,8 @@ import camp.nextstep.edu.missionutils.Console
 import lotto.util.Error
 
 object InputManager {
-
-    private val outputManager = OutputManager()
     fun getPurchaseInput(): Int {
-        outputManager.printInputPurchaseAmount()
+        OutputManager.printInputPurchaseAmount()
         return runCatching {
             requireNotNull(Console.readLine().toIntOrNull()) { Error.NotNumber.message }
         }.getOrElse {
--- src/main/kotlin/lotto/view/OutputManager.kt
+++ src/main/kotlin/lotto/view/OutputManager.kt
@@ -1,6 +1,10 @@
 package lotto.view
 
-class OutputManager {
+object OutputManager {
+    private const val INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요."
+    private const val PURCHASED_LOTTO_AMOUNT_MSG = "개를 구매했습니다."
+    private const val INPUT_PRIZE_NUM_MSG = "당첨 번호를 입력해 주세요."
+    private const val INPUT_BONUS_NUM_MSG = "보너스 번호를 입력해 주세요."
 
     fun printInputPurchaseAmount() = println(INPUT_PURCHASE_AMOUNT_MSG)
 
@@ -9,11 +13,4 @@ class OutputManager {
     fun printInputPrizeNum() = println(INPUT_PRIZE_NUM_MSG)
 
     fun printInputBonusNum() = println(INPUT_BONUS_NUM_MSG)
-
-    companion object {
-        private const val INPUT_PURCHASE_AMOUNT_MSG = "구입금액을 입력해 주세요."
-        private const val PURCHASED_LOTTO_AMOUNT_MSG = "개를 구매했습니다."
-        private const val INPUT_PRIZE_NUM_MSG = "당첨 번호를 입력해 주세요."
-        private const val INPUT_BONUS_NUM_MSG = "보너스 번호를 입력해 주세요."
-    }
 }
