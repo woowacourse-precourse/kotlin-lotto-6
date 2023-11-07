ef7c1f14754775d088504ca0c4365b9c808d2069
DJ_Lumiere <djminstar573@gmail.com>
2023-11-07 23:25:00 +0900

    Update Lotto.kt
    로또 에러 메시지 입력

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,18 @@
 package lotto
 
-class Lotto(private val numbers: List<Int>) {
+
+class Lotto(private val numbers: List<Int>) : Iterable<Int> {
     init {
-        require(hasSixNumbers(numbers)){""}
-        require(isInValidRange(numbers)){""}
-        require(isUnique(numbers)){""}
+        require(hasSixNumbers(numbers)) { "[ERROR] 로또 티켓에는 숫자가 6개만 있어야합니다." }
+        require(isInValidRange(numbers)) { "[ERROR] 로또 숫자의 범위는 1 이상 45 미만 이어야합니다." }
+        require(isUnique(numbers)) { "[ERROR] 로또 숫자는 모두 달라야합니다." }
     }
 
-    override fun toString():String{
-        return numbers.toString()
+    override fun toString(): String {
+        return numbers.joinToString(prefix = "[", postfix = "]")
     }
-    // TODO: 추가 기능 구현
+
+    override fun iterator(): Iterator<Int> {
+        return numbers.iterator()
+    }
 }
