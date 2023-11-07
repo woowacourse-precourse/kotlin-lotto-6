0e9397ba8fb26e611e36bc0cd0f0b19b44bfdfbd
yeji0214 <1225yj50@naver.com>
2023-11-07 20:14:15 +0900

    PrintResults.kt 보완 (companion object 사용)

--- src/main/kotlin/lotto/PrintResults.kt
+++ src/main/kotlin/lotto/PrintResults.kt
@@ -3,6 +3,10 @@ package lotto
 import java.math.BigDecimal
 
 class PrintResults {
+    companion object {
+        private const val INITIAL_NUM = 0
+        private const val DECIMAL_PLACE = 1
+    }
     fun printResults(prizeCounts: IntArray, rateOfReturn: BigDecimal) {
         val prizeDescriptions = listOf(
                 MessageConstants.THREE_MATCHES,
@@ -13,11 +17,11 @@ class PrintResults {
         )
 
         println(MessageConstants.WINNING_STATISTICS)
-        for (i in 0 until prizeCounts.size) {
+        for (i in INITIAL_NUM until prizeCounts.size) {
             println("${prizeDescriptions[i]} - ${prizeCounts[i]}${MessageConstants.UNIT}")
         }
 
-        println("${MessageConstants.YIELD_MESSAGE_START}${roundDigit(rateOfReturn, 1)}${MessageConstants.YIELD_MESSAGE_END}")
+        println("${MessageConstants.YIELD_MESSAGE_START}${roundDigit(rateOfReturn, DECIMAL_PLACE)}${MessageConstants.YIELD_MESSAGE_END}")
     }
 
     fun roundDigit(number: BigDecimal, digits: Int): BigDecimal {
