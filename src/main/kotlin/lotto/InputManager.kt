103ca7358d7a6936f96cf6415ef42ef18a037c76
LEE <tpqms775@naver.com>
2023-11-07 14:55:52 +0900

    style : 출력문 개행

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -3,18 +3,22 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class InputManager {
-    fun inputMoney(): String {
+    fun inputMoney(): Int {
         println("구입금액을 입력해 주세요.")
-        return Console.readLine()
+        return Console.readLine().toInt()
     }
+
     fun inputWinningNumber(): List<Int> {
         println("당첨 번호를 입력해 주세요.")
         val nums = Console.readLine().split(",")
+        println()
         return nums.map { it.toInt() }
     }
+
     fun inputBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
         val num = Console.readLine()
+        println()
         return num.toInt()
     }
 
