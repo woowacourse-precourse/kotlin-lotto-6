427dfaf007b4668ee7d6d5bd5dda0ebea36ae8cb
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-07 12:22:07 +0900

    feat: lotto game v4 - 유효성 검증 - Lotto.match - Lotto.numbers - Lotto.bonusNum

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,19 +4,33 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    val money = purchaseMoney() // 로또 구매 금 입력
-    val lotto = purchaseLotto(money / 1000) // 로또 생성
-    printLotto(lotto) // 구매한 로또 출력
+    val money = purchaseMoney() // 로또 구매 금액 입력
+    val lottery = purchaseLotto(money / 1000) // 로또 생성
+    printLotto(lottery) // 구매한 로또 출력(임의로 숫자 설정)
 
-    val winningNums = winningNumber()
-    val bonusNum = bonusNumber()
-    print("구입금액을 입력해 주세요.")
-//
-//    print("당첨 번호를 입력해 주세요.")
-//    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-//
-//    print("보너스 번호를 입력해 주세요.")
+    val winningNums = winningNumber() // 당첨 번호 설정
+    val bonusNum = bonusNumber() // 보너스 번호 설정
 
+    val wonLottery = checkLotto(lottery, winningNums, bonusNum)
+    printResults(wonLottery, money)
+}
+
+fun printResults(wonLottery: Lotto, money: Int) {
+    println("당첨 통계")
+    println("---")
+    wonLottery.printStat()
+    println("총 수익률은 ${wonLottery.rateReturn(money)}입니다.")
+
+}
+
+fun checkLotto(lottery: List<List<Int>>, winningNums: List<Int>, bonusNum: Int): Lotto {
+    val result = lottery.map {
+        ticket -> val match = ticket.count() { it in winningNums }
+        val isBonus = bonusNum in ticket
+        // 매치 결과
+    }
+
+    return Lotto(result)
 }
 
 // 사용자가 구입 금액 입력 함수
@@ -30,9 +44,10 @@ fun purchaseMoney(): Int {
     return money
 }
 
-// 구매한 로또 개수 입력 함수
+// 구매한 로또 개수 입력 함수 (임의로 6개 설정)
 fun purchaseLotto(lottoCnt: Int): List<List<Int>> {
     return List(lottoCnt) {
+        // 중복되지 않는 6개 숫자 설정
         Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
     }
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,32 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+    // 로또 번호가 유효한 숫자들인지 검증
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            "로또는 숫자 6개 입니다."
+        }
+        require(numbers.distinct().size == 6) {
+            "로또의 숫자는 서로 중복되지 않습니다."
+        }
+        require(numbers.all { it in 1..45 }) {
+            "로또는 1부터 45 중에서 결정됩니다."
+        }
+    }
+    // TODO: 추가 기능 구현
+    // 사용자가 구매한 로또와 당첨 번호가 얼마나 일치하는지 계산
+    fun match(winningNums: List<Int>): Int {
+        require(winningNums.size == 6) {
+            "당첨 번호는 6개의 중복되지 않은 수입니다."
+        }
+        return numbers.count { it in winningNums }
     }
 
-    // TODO: 추가 기능 구현
+    // 보너스 숫자 검증
+    fun bonusNum(bonusNum: Int): Boolean {
+        require(bonusNum in 1..45) {
+            "보너스 숫자는 1부터 45 중에 있습니다."
+        }
+        return bonusNum in numbers
+    }
 }
