f4ab12af50821df47bdc574a855024a654b71fd1
hevton <hevton22@gmail.com>
2023-11-07 14:54:23 +0900

    test: LottoTest 진행

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.domain.Lotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -22,4 +23,30 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호의 범위가 1 ~ 45에 해당하지 않으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto.fromList(listOf(10, 2, 3, 4, 5, 4))
+        }
+    }
+
+    @Test
+    fun `toString() 리턴 검증`() {
+        val lotto = Lotto.fromList(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]")
+    }
+
+    @Test
+    fun `toList() 로또 리스트 리턴 검증`() {
+        val lotto = Lotto.fromList(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.toList()).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+    }
+
+    @Test
+    fun `contains() 번호 체크 리턴 검증`() {
+        val lotto = Lotto.fromList(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.contains(1)).isEqualTo(true)
+        assertThat(lotto.contains(2)).isEqualTo(true)
+        assertThat(lotto.contains(7)).isEqualTo(false)
+    }
 }
