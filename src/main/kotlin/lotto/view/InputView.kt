dafdcf018990d4eb644963766af9b1d9e92d1f86
OliviaYJH <0703olivia@naver.com>
2023-11-08 00:10:47 +0900

    refactor(ValidateLottoAmount): 로또 구입 금액 유효성 처리 함수 호출 위치 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,11 +2,13 @@ package lotto.view
 
 import lotto.constants.Constants
 import camp.nextstep.edu.missionutils.Console
+import lotto.domain.ValidateLottoAmount
+import lotto.domain.ValidateLottoNumbers
 
 class InputView {
-    fun inputBuyAmount(): String {
+    fun inputBuyAmount(): Int {
         println(Constants.INPUT_BUY_AMOUNT)
-        return Console.readLine()
+        return ValidateLottoAmount().buyAmount(Console.readLine())
     }
 
     fun inputWinningNumbers() {
