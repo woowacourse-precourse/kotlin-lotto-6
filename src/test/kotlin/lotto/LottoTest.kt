73e5e66382b7b7f98840910bd4f7d72af0f27b96
minuxx <minux8905@gmail.com>
2023-11-07 13:09:02 +0900

    test: 로또 번호 개수 예외 발생 테스트 수정

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,9 +2,13 @@ package lotto
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LottoTest {
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
@@ -20,7 +24,20 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    private fun invalidNumberCountOfLotto() = listOf(
+            listOf(1, 2, 3, 4, 5),
+            listOf(1, 2, 3, 4, 5, 6, 7)
+        )
+
+    @ParameterizedTest
+    @MethodSource("invalidNumberCountOfLotto")
+    fun `로또 번호의 개수가 6개가 아니면 예외가 발생한다`(numbers: List<Int>) {
+        // when
+        val exception = assertThrows<IllegalArgumentException> { Lotto(numbers) }
+
+        // then
+        assertThat(exception.message).isEqualTo(Lotto.INVALID_NUMBER_COUNT_ERROR_MESSAGE)
+    }
 
     @Test
     fun `로또 번호의 범위가 1이상 45이하가 아니면 예외가 발생한다`() {
