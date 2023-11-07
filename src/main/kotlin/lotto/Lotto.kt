35231fe7fcf459eab8226cca8a38d5e36149d627
yoon6 <yoonda5898@gmail.com>
2023-11-07 23:35:04 +0900

    feat: Add compare logic between lotto and winning numbers

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,7 +6,19 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun compare(winningNumbers: WinningNumbers): WinningState {
-        TODO()
+        val count = winningNumbers.winningNumbers.count { numbers.contains(it) }
+        val bonus = numbers.contains(winningNumbers.bonusNumber)
+        return if (bonus && count == 5) {
+            WinningState.SECOND
+        } else {
+            when (count) {
+                6 -> WinningState.FIRST
+                5 -> WinningState.THIRD
+                4 -> WinningState.FORTH
+                3 -> WinningState.FIFTH
+                else -> WinningState.NONE
+            }
+        }
     }
 
     override fun toString(): String {
