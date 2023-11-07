4127c6cf200f098279caa2828922607790af7eaa
OliviaYJH <0703olivia@naver.com>
2023-11-08 00:00:18 +0900

    feat(ValidateLottoAmount): 로또 구입 금액 유효성 처리 구현

---
+++ src/main/kotlin/lotto/domain/ValidateLottoAmount.kt
@@ -0,0 +1,44 @@
+package lotto.domain
+
+import java.lang.IllegalArgumentException
+
+class ValidateLottoAmount {
+
+    fun buyAmount(buyAmount : String): Int {
+        return validateBuyAmount(buyAmount)
+    }
+
+    private fun validateBuyAmount(buyAmount: String) : Int {
+        return validateBuyAmountEmpty(buyAmount)
+    }
+
+    private fun validateBuyAmountEmpty(buyAmount: String) : Int {
+        if (buyAmount.isEmpty()) throw IllegalArgumentException(BUY_AMOUNT_EMPTY_ERROR_MESSAGE)
+        return validateBuyAmountString(buyAmount)
+    }
+
+    private fun validateBuyAmountString(buyAmount: String) : Int {
+        for (char in buyAmount.toCharArray()) {
+            if (!char.isDigit()) throw IllegalArgumentException(BUY_AMOUNT_STRING_ERROR_MESSAGE)
+        }
+        return validateBuyAmountUnderLottoPrice(buyAmount.toInt())
+    }
+
+    private fun validateBuyAmountUnderLottoPrice(buyAmount: Int) : Int {
+        if (buyAmount < LOTTO_PRICE) throw IllegalArgumentException(BUY_AMOUNT_UNDER_LOTTO_PRICE_ERROR_MESSAGE)
+        return validateBuyAmountDivideByLottoPrice(buyAmount)
+    }
+
+    private fun validateBuyAmountDivideByLottoPrice(buyAmount: Int) : Int {
+        if (buyAmount % LOTTO_PRICE != 0) throw IllegalArgumentException(BUY_AMOUNT_DIVIDE_BY_LOTTO_PRICE_ERROR_MESSAGE)
+        return buyAmount / LOTTO_PRICE
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
+        const val BUY_AMOUNT_EMPTY_ERROR_MESSAGE = "[ERROR] 입력하지 않았습니다."
+        const val BUY_AMOUNT_STRING_ERROR_MESSAGE = "[ERROR] 입력한 값에 문자가 포함되어 있습니다."
+        const val BUY_AMOUNT_UNDER_LOTTO_PRICE_ERROR_MESSAGE = "[ERROR] 입력한 값으로 로또 구매가 불가능합니다."
+        const val BUY_AMOUNT_DIVIDE_BY_LOTTO_PRICE_ERROR_MESSAGE = "[ERROR] 입력한 값은 1,000원으로 나누어 떨어지지 않습니다."
+    }
+}
