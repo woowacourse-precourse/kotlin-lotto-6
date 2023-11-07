ba29eb35f9539b8a6c8dbb512118c369d32ceed0
lee989898 <wnsrud1352@gmail.com>
2023-11-07 19:40:11 +0900

    [feat] 로또 수익률을 계산하는 클래스 생성

---
+++ src/main/kotlin/lottoreturns/LottoReturnsImpl.kt
@@ -0,0 +1,9 @@
+package lottoreturns
+
+import lottopurchaseamount.LottoPurchaseAmount
+
+class LottoReturnsImpl : LottoReturns {
+    override fun calculate(lottoPurchaseAmount: LottoPurchaseAmount): Double {
+        TODO("Not yet implemented")
+    }
+}
