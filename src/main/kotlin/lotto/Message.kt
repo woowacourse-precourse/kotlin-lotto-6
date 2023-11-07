e555a0136a8579f04376706ac4b85d78db2d46b9
oh in seong <superohinsung@naver.com>
2023-11-08 00:15:54 +0900

    [feat] Add Error Message

--- src/main/kotlin/lotto/Message.kt
+++ src/main/kotlin/lotto/Message.kt
@@ -9,7 +9,17 @@ object Message {
     const val WINNING_STATISTICS = "당첨 통계"
     const val COMMA = "---"
 
-    const val ERROR_USER_PICK_NUMBERS_DUPLICATION = "[ERROR] 중복된 로또번호가 입력되었습니다."
-    const val ERROR_BONUS_NUMBER_DUPLICATION = "[ERROR] 앞서 입력된 로또번호와 중복되었습니다."
+    const val ERROR_USER_PICK_NUMBERS_DUPLICATION = "[ERROR] 중복된 번호를 입력하였습니다."
+    const val ERROR_BONUS_NUMBER_DUPLICATION = "[ERROR] 앞서 입력된 번호와 중복된 번호를 입력하였습니다."
 
+    const val ERROR_RANDOM_NUMBER_DUPLICATION = "[ERROR] 중복된 로또번호가 생성 되었습니다."
+    const val ERROR_RANDOM_NUMBER_RANGE = "[ERROR] 범위를 넘어선 로또번호가 생성 되었습니다."
+    const val ERROR_RANDOM_NUMBER_SIX_RANGE = "[ERROR] 6개의 로또번호가 아니게 생성되었습니다."
+    const val ERROR_RANDOM_NUMBER_SORTING = "[ERROR] 로또번호가 오름차순이 아닙니다."
+
+    const val ERROR_USER_INPUT_NOT_INT = "[ERROR] 정수가 아닌 값이 입력되었습니다."
+    const val ERROR_USER_INPUT_LESS_THAN_ZERO ="[ERROR] 0보다 작은 값이 입력되었습니다."
+    const val ERROR_USER_INPUT_LESS_THAN_THOUSAND ="[ERROR] 로또는 1개당 1000원입니다. 1000원 이상의 값을 입력해주세요."
+
+    const val ERROR_WIN_COUNT_CALCULATOR ="[ERROR] 당첨 계산이 잘못되었습니다."
 }
