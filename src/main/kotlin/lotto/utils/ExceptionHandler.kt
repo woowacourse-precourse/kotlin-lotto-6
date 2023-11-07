1dff37f861c3bf10d05ed71af0db91d579d29a70
MinhoiKoo <minlove2013@gmail.com>
2023-11-07 14:25:38 +0900

    feat: ExceptionHandler Object 생성
    -정수인지 확인
    -비어있는 값인지 확인
    -보너스 번호가 정답 로또에 중복되는지 확인

---
+++ src/main/kotlin/lotto/utils/ExceptionHandler.kt
@@ -0,0 +1,24 @@
+package lotto.utils
+
+import lotto.domain.LottoNumber
+import lotto.domain.WinningLotto
+import lotto.utils.Constant.BLANK_INPUT_EXCEPTION_MESSAGE
+import lotto.utils.Constant.BONUS_NUMBER_DUPLICATE_EXCEPTION_MESSAGE
+import lotto.utils.Constant.NOT_DIGIT_EXCEPTION_MESSAGE
+
+object ExceptionHandler {
+
+    fun isDigit(number: String) {
+        number.forEach { num ->
+            require(num.isDigit()) { NOT_DIGIT_EXCEPTION_MESSAGE }
+        }
+    }
+
+    fun isBlank(money: String) {
+        require(money.isNotBlank()) { BLANK_INPUT_EXCEPTION_MESSAGE }
+    }
+
+    fun checkBonusNumberIsDuplicate(winningLotto: WinningLotto, bonusNumber: LottoNumber) {
+        require(!winningLotto.contains(bonusNumber)) { BONUS_NUMBER_DUPLICATE_EXCEPTION_MESSAGE}
+    }
+}
