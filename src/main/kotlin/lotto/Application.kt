a8b94b3954cd0199ddee1869b8ec6ef4a87594e9
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-08 00:38:52 +0900

    all files

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,43 +2,134 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
 fun main() {
-    val money = purchaseMoney() // 로또 구매 금 입력
-    val lotto = purchaseLotto(money / 1000) // 로또 생성
-    printLotto(lotto) // 구매한 로또 출력
+    val money = readMoney()
+    val count = money / 1000
+    val lotteries = setLotteries(count)
 
-//    val winningNums = winningNumber()
-//    val bonusNum = bonusNumber()
-//    print("구입금액을 입력해 주세요.")
-//
-//    print("당첨 번호를 입력해 주세요.")
-//    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-//
-//    print("보너스 번호를 입력해 주세요.")
+    printLotto(lotteries)
 
+    val winningNums = readWinningNums()
+    val bonusNumber = readBonusNum(winningNums)
+
+    val result = checkResults(lotteries, winningNums, bonusNumber)
+    printResult(result, money)
 }
 
-// 사용자가 구입 금액 입력 함수
-fun purchaseMoney(): Int {
-    println("구입금액을 입력해 주세요.")
-    val money = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
-    if (money < 1000 || money % 1000 != 0) {
-        throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
-    }
+// 입력하는 부분 (구입 금액 입력)
+fun readMoney(): Int {
+    println("구입 금액을 입력해주세요.")
+    val money = readLine()?.toIntOrNull()
+        ?: throw IllegalArgumentException("[ERROR] 정확한 금액이 아닙니다.")
+    if (money < 1000)
+        throw IllegalArgumentException("[ERROR] 구입 금액이 너무 적습니다.")
     return money
 }
 
-// 구매한 로또 개수 입력 함수
-fun purchaseLotto(lottoCnt: Int): List<List<Int>> {
-    return List(lottoCnt) {
-        Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+// 로또 만드는 함수
+fun setLotteries(count: Int): List<Lotto> {
+    return List(count) {
+        Lotto(Randoms.pickUniqueNumbersInRange(
+            1, 45, 6).sorted()
+        )
     }
 }
 
-// 구매한 로또 개수 출력하기
-fun printLotto(lotto: List<List<Int>>) {
-    println("${lotto.size}개를 구매했습니다.")
-    lotto.forEach { println(it) }
+// 구매한 로또 번호 출력하기
+fun printLotto(lotteries: List<Lotto>) {
+    println("${lotteries.size}개를 구매했습니다.")
+    lotteries.forEach { println(it.getNumbers()) } // getNumbers 메서드로 numbers 접근
 }
 
+// 당첨 번호 입력하기
+fun readWinningNums(): Set<Int> {
+    println("당첨 번호를 입력해 주세요.")
+    return readNums()
+}
+
+// 번호 읽어서 Set 반환하기
+fun readNums(): Set<Int> {
+    return readLine()!!.split(",")
+        .mapNotNull { it.trim().toIntOrNull() }
+        .toSet()
+        .also { if (it.size != 6)
+            throw IllegalArgumentException("[ERROR] 정확하지 않은 로또 번호입니다.") }
+}
+
+// 보너스 번호 가져오는 함수
+fun readBonusNum(wonNumbers: Set<Int>): Int {
+    println("보너스 번호를 입력해 주세요.")
+    // 보너스 번호 정수로 변환
+    while (true) {
+        val bonus = Console.readLine().trim().toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR] 정확하지 않은 보너스 번호입니다.")
+        if (bonus in 1..45 && bonus !in wonNumbers) {
+            return bonus
+        } else {
+            println("[ERROR] 정확하지 않은 보너스 번호입니다.")
+        }
+    }
+}
+
+// 당첨 결과 확인 함수
+fun checkResults(lotteries: List<Lotto>, wonNums: Set<Int>, bonusNum: Int): Map<Rank, Int> {
+    // 모든 등수 당첨 개수 0으로 설정
+    val results = Rank.values().associateWith { 0 }.toMutableMap()
+
+    lotteries.forEach { lotto ->
+        val matchCount = lotto.getNumbers().count { it in wonNums } // 당첨 번호와 일치 개수
+        val matchBonus = bonusNum in lotto.getNumbers() // 보너스 번호 포함 여부
+        val rank = Rank.valueOf(matchCount, matchBonus) // 일치 번호와 보너스 번호 여부
+        results[rank] = results[rank]!! + 1 // 등수 당첨 개수 1 증가
+    }
+
+    return results
+}
+
+// 결과 출력 함수
+fun printResult(lottoResults: Map<Rank, Int>, money: Int) {
+    // 각 당첨 별 상금 설정
+    lottoResults.forEach { (rank, count) ->
+        if (count > 0)
+            println("${rank.matchNum}개 일치 (${rank.winningMoney}원)- $count 개")
+    }
+
+    // 수익금 계산
+    val totalMoney = lottoResults.entries.sumOf {
+        it.key.winningMoney * it.value }
+
+    val returnRate = (totalMoney.toDouble() / money) * 100 // 수익률 계산
+    println("총 수익률은 ${"%.2f".format(returnRate)}% 입니다.") // 수익률 출력
+}
+
+@Test
+fun `match count is correct`() {
+    // 준비
+    val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6).toList())
+    val winningNumbers = setOf(4, 5, 6, 7, 8, 9)
+
+    // 실행
+    val matchCount = lotto.matches(winningNumbers)
+
+    // 검증
+    assertThat(matchCount).isEqualTo(3)
+}
+
+@Test
+fun `bonus number is correctly identified`() {
+    // 준비
+    val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6).toList())
+    val winningNumbers = setOf(4, 5, 6, 7, 8, 9)
+    val bonusNumber = 1
+
+    // 실행
+    val matchCount = lotto.matches(winningNumbers)
+    val matchesBonus = lotto.contains(bonusNumber)
+
+    // 검증
+    assertThat(matchCount).isEqualTo(3)
+    assertThat(matchesBonus).isTrue
+}
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,40 @@
 package lotto
 
+// 로또 번호를 관리하는 Lotto 클래스 정의
 class Lotto(private val numbers: List<Int>) {
-    init {
-        require(numbers.size == 6)
+    // Lotto 내부 번호에 접근
+    fun getNumbers(): List<Int> = numbers
+
+    fun matches(winningNums: Set<Int>): Int {
+        return numbers.count { it in winningNums }
     }
 
-    // TODO: 추가 기능 구현
+    fun contains(number: Int): Boolean {
+        return number in numbers
+    }
+    // 객체가 문자열일 때 사용
+    override fun toString(): String = numbers.joinToString(separator = ", ")
+}
+
+// 로또 등수 설정
+enum class Rank(val matchNum: Int, val winningMoney: Long) {
+    Rank1(6, 2_000_000_000),
+    Rank2(5, 30_000_000), // 보너스 번호 일치 시
+    Rank3(5, 1_500_000), // 보너스 번호 불일치 시
+    Rank4(4, 50_000),
+    Rank5(3, 5_000),
+    NONE(0, 0);
+
+    // 보너스 번호 일치에 따라 등수 반환하는 함수
+    companion object {
+        fun valueOf(matchNum: Int, matchBonus: Boolean): Rank {
+            return when (matchNum) {
+                6 -> Rank1
+                5 -> if (matchBonus) Rank2 else Rank3
+                4 -> Rank4
+                3 -> Rank5
+                else -> NONE
+            }
+        }
+    }
 }
