f8f8bf13755e8fd8c09c0ac48e9e3c4fdb1c6414
gretea5 <gretea4489@gmail.com>
2023-11-07 17:16:00 +0900

    feat: 갯수 일치 로직 구현

--- src/main/kotlin/lotto/LottoCalculator.kt
+++ src/main/kotlin/lotto/LottoCalculator.kt
@@ -19,9 +19,36 @@ class LottoCalculator {
         }
         return dataList.toList()
     }
+
+    fun calWinningRate(
+        lottoList: List<Lotto>,
+        winningList: List<Int>,
+        bonusNumber: Int
+    ): WinningRate {
+        val dataList = calMatchCount(lottoList, winningList, bonusNumber)
+        val winningRate = WinningRate()
+        dataList.forEach {
+            when {
+                it.winningMatchCount == 3 && it.bonusNumber == 0 -> winningRate.threeMatchedRate += 1
+                it.winningMatchCount == 4 && it.bonusNumber == 0 -> winningRate.fourMatchedRate += 1
+                it.winningMatchCount == 5 && it.bonusNumber == 0 -> winningRate.fiveMatchedRate += 1
+                it.winningMatchCount == 5 && it.bonusNumber == 1 -> winningRate.fiveAndBonusMatchedData += 1
+                it.winningMatchCount == 6 && it.bonusNumber == 0 -> winningRate.sixMatchedRate += 1
+            }
+        }
+        return winningRate
+    }
 }
 
 data class MatchedData(
     var winningMatchCount: Int,
     var bonusNumber: Int,
+)
+
+data class WinningRate(
+    var threeMatchedRate: Int = 0,
+    var fourMatchedRate: Int = 0,
+    var fiveMatchedRate: Int = 0,
+    var fiveAndBonusMatchedData: Int = 0,
+    var sixMatchedRate: Int = 0
 )
