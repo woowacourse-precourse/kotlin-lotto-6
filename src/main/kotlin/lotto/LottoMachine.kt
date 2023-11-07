3bc4e855dc71b1db276821d5c2b4b6021f72e8ef
jklom0326 <jklom0326@gmail.com>
2023-11-07 16:07:14 +0900

    feat: 결과값을 계산하는 기능 구현
    - 당첨된 숫자를 계산하는 기능
    - 당첨된 금액의 수익률을 계산하는기능

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -12,6 +12,45 @@ class LottoMachine {
         val inputLottoNumber = view.inputLottoNumber()
         val inputBonusNumber = view.inputBonusLottoNumber()
 
+        val prize = winningLotteryResult(lottos, inputLottoNumber, inputBonusNumber)
+        val winRate = calculateWinRate(prize, lottos)
+
+        view.printPrizeResult(prize)
+        view.printWinRate(winRate)
+    }
+
+    private fun winningLotteryResult(lottos: List<Lotto>, inputLottoNumber: Lotto, inputBonusNumber: Int): Prize {
+        val inputNumber = inputLottoNumber.getLottoNumbers()
+        val prize = Prize()
+        lottos.forEach { lotto ->
+            val lottoNum = lotto.getLottoNumbers()
+            val count = countMatchNumber(lottoNum, inputNumber)
+            when (count) {
+                6 -> prize.countPrize(Rank.FIRST_RANK)
+                5 -> {
+                    if (lottoNum.contains(inputBonusNumber)) {
+                        prize.countPrize(Rank.SECOND_RANK)
+                    } else {
+                        prize.countPrize(Rank.THIRD_RANK)
+                    }
+                }
+
+                4 -> prize.countPrize(Rank.FOURTH_RANK)
+                3 -> prize.countPrize(Rank.FIFTH_RANK)
+                else -> Rank.MISS
+            }
+        }
+        return prize
+    }
+
+    private fun calculateWinRate(prize: Prize, lottos: List<Lotto>): Double {
+        return (prize.getPrizeMoney() / (lottos.size * 1000) * 100)
+    }
+
+    private fun countMatchNumber(lottoNum: List<Int>, inputNumber: List<Int>): Int {
+        return lottoNum.count {
+            inputNumber.contains(it)
+        }
     }
 
     private fun makeLotto(payment: Int): List<Lotto> {
