2530c3233942e2febe3c6d8c3ec712d8a6b0d605
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-11-07 18:36:19 +0900

    (fix) lottoGameView - printGameResult

--- src/main/kotlin/lotto/View/LottoGameView.kt
+++ src/main/kotlin/lotto/View/LottoGameView.kt
@@ -1,22 +1,19 @@
-package lotto.View
-
 object LottoGameView {
-    fun printGameResult(winning3, winning4, winning5, winning5Bonus,winning6, lottoListSize){
+    fun printGameResult(winning3: Int, winning4: Int, winning5: Int, winning5Bonus: Int, winning6: Int, lottoListSize: Int) {
         println("\n당첨 통계")
         println("---")
-        println("3개 일치 (5,000원) - ${winning3}개")
-        println("4개 일치 (50,000원) - ${winning4}개")
-        println("5개 일치 (1,500,000원) - ${winning5}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winning5Bonus}개")
-        println("6개 일치 (2,000,000,000원) - ${winning6}개")
+        println("3개 일치 (5,000원) - $winning3 개")
+        println("4개 일치 (50,000원) - $winning4 개")
+        println("5개 일치 (1,500,000원) - $winning5 개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - $winning5Bonus 개")
+        println("6개 일치 (2,000,000,000원) - $winning6 개")
 
         val totalWinningCount = winning3 + winning4 + winning5 + winning5Bonus + winning6
-        printProfitRate(totalWinningCount,lottoListSize)
+        printProfitRate(totalWinningCount, lottoListSize)
     }
 
-    fun printProfitRate(totalWinningCount,lottoListSize){
-        val profitRate = (totalWinningCount.toDouble()/lottoListSize) * 100
+    fun printProfitRate(totalWinningCount: Int, lottoListSize: Int) {
+        val profitRate = (totalWinningCount.toDouble() / lottoListSize) * 100
         println("총 수익률은 ${profitRate}%입니다.")
     }
-
 }
