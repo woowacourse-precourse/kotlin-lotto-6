f2dc272665051b6674660cdd4f4cce7e06e3cf67
LEE <tpqms775@naver.com>
2023-11-07 21:40:12 +0900

    refactor : Input안내문 상수화

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -4,22 +4,30 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputManager {
     fun inputMoney(): Int {
-        println("구입금액을 입력해 주세요.")
+        println(PRINT_INPUT_MONEY)
         return Console.readLine().toInt()
     }
 
     fun inputWinningNumber(): List<Int> {
-        println("당첨 번호를 입력해 주세요.")
-        val nums = Console.readLine().split(",")
+        println(PRINT_INPUT_WINNING_NUMBER)
+        val nums = Console.readLine().split(DELIMITER)
         println()
         return nums.map { it.toInt() }
     }
 
     fun inputBonusNumber(): Int {
-        println("보너스 번호를 입력해 주세요.")
+        println(PRINT_INPUT_BONUS_NUMBER)
         val num = Console.readLine()
         println()
         return num.toInt()
     }
 
+    companion object {
+
+        private const val DELIMITER = ","
+        private const val PRINT_INPUT_MONEY = "구입금액을 입력해 주세요."
+        private const val PRINT_INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+        private const val PRINT_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+
+    }
 }
