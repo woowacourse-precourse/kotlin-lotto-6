baa52437f1fa84dced38bc62229781b34e6ec84e
ekeonn <52497657+ekeonn@users.noreply.github.com>
2023-11-07 15:56:56 +0900

    Style(typo correction)
    BOUNS -> BONUS

--- src/main/kotlin/lotto/enums/OutPut.kt
+++ src/main/kotlin/lotto/enums/OutPut.kt
@@ -3,7 +3,7 @@ package lotto.enums
 enum class OutPut(val message: String) {
     PLEASE_INPUT_AMOUNT("구입금액을 입력해 주세요."),
     PLEASE_INPUT_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
-    PLEASE_INPUT_BOUNS_NUMBER("보너스 번호를 입력해 주세요."),
+    PLEASE_INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     TOTAL_PROFIT_IS("총 수익률은 %.1f%%입니다"),
     WINNING_STATISTICS("당첨 통계"),
     NEW_LINE("---"),
