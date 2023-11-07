9a6463f076de12add68761b38d715f36d92242e4
bak <bak4017@gmail.com>
2023-11-07 11:46:35 +0900

    refactor(lotto): LottoMachine클래스의 함수를 LottoController클래스로 이동 후 클래스 삭제

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,8 +1,9 @@
 package lotto.controller
 
 import lotto.domain.Lotto
-import lotto.domain.LottoMachine
 import lotto.domain.LottoStore
+import lotto.validator.BonusNumberValidator
+import lotto.validator.WinNumbersValidator
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -10,8 +11,9 @@ class LottoController {
     fun run() {
         val lottoStore: LottoStore = inputPrice()
         val tickets : List<Lotto> = issueTickets(lottoStore)
-        val winNumbers: List<String> = LottoMachine().drawWinNumbers()
-        val bonusNumber: String = LottoMachine().drawBonusNumber()
+        val winNumbers: List<String> = inputWinNumbers()
+        val bonusNumber: String = inputBonusNumber()
+//        tickets.forEach { Lotto(it) }
     }
 
     private fun inputPrice(): LottoStore {
@@ -34,5 +36,37 @@ class LottoController {
         val tickets = lottoStore.sellTickets()
         OutputView.printTickets(tickets).also { println() }
         return tickets
+    }
+
+    private fun inputWinNumbers(): List<String> {
+        var attempts = 0
+        while (attempts <= 5) {
+            try {
+                val winNumbers = InputView.promptForWinNumbers().also { println() }
+                WinNumbersValidator(winNumbers)
+                return winNumbers
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                attempts++
+            }
+        }
+
+        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
+    }
+
+    private fun inputBonusNumber(): String {
+        var attempts = 0
+        while (attempts <= 5) {
+            try {
+                val bonusNumber = InputView.promptForBonusNumber().also { println() }
+                BonusNumberValidator(bonusNumber)
+                return bonusNumber
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+                attempts++
+            }
+        }
+
+        throw IllegalArgumentException("입력에 여러 차례 실패했습니다. 프로그램을 종료합니다.")
     }
 }
