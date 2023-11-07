2943fc7c5e5054ebac6737e32b514e80053f2dfa
murjune <sis9221302@gmail.com>
2023-11-07 21:52:17 +0900

    test(SizeValidatorTest) : List<Int> 사이즈 검사 test

---
+++ src/test/kotlin/lotto/validator/numbers/SizeValidatorTest.kt
@@ -0,0 +1,28 @@
+package lotto.validator.numbers
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class SizeValidatorTest {
+    @Test
+    fun `List 크기가 6이면 정상`() {
+        val sizeValidator = SizeValidator()
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+        sizeValidator.validate(numbers)
+    }
+
+    @Test
+    fun `List 크기가 6이 아니면 예외`() {
+        val sizeValidator = SizeValidator()
+        val invalidNumbers = listOf(1, 2, 3, 4, 5)
+        val inValidNumbers2 = listOf(1, 2, 3, 4, 5, 6, 7)
+
+        assertThrows<IllegalArgumentException>("로또 번호 %s 는 6개여야 합니다") {
+            sizeValidator.validate(invalidNumbers)
+        }
+        assertThrows<IllegalArgumentException>("로또 번호 %s 는 6개여야 합니다") {
+            sizeValidator.validate(inValidNumbers2)
+        }
+    }
+}
