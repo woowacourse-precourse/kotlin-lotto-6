1ed35f189cdec46adb5b104d2347b0f144f80cb9
yeonghyeon <agilecatch@gmail.com>
2023-11-07 18:03:05 +0900

    [Fix] : 예외처리 문구 수정
    - 예외처리 문구를 수정하였습니다.

--- src/main/kotlin/lotto/Exception.kt
+++ src/main/kotlin/lotto/Exception.kt
@@ -14,11 +14,15 @@ object Exception {
     }
 
     fun enterWinningNumberException(input: String?): List<Int> {
-        val numbers = input?.split(",")?.map { it.trim().toInt() }
-        if (numbers != null && numbers.size == 6 && numbers.all { it in 1..45 } && numbers.toSet().size == numbers.size) {
-            return numbers
-        } else {
-            throw IllegalArgumentException("[ERROR] 올바른 당첨 번호를 입력해주세요.")
+        try {
+            val numbers = input?.split(",")?.map { it.trim().toInt() }
+            if (numbers != null && numbers.size == 6 && numbers.all { it in 1..45 } && numbers.toSet().size == numbers.size) {
+                return numbers
+            } else {
+                throw IllegalArgumentException("[ERROR] 올바른 당첨 번호를 입력해주세요.")
+            }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] 숫자를 입력해주세요.")
         }
     }
 
@@ -27,7 +31,7 @@ object Exception {
         if (number != null && number in 1..45 && number !in winningNumbers) {
             return number
         } else {
-            throw IllegalArgumentException("[ERROR] 1에서 45 사이의 숫자 중에서 당첨 번호와 중복되지 않게 입력해주세요.")
+            throw IllegalArgumentException("[ERROR] 당첨 번호와 중복되지 않게 입력해주세요.")
         }
     }
 
