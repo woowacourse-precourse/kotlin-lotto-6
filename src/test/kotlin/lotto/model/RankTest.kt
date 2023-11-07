125e9f0710c5707afd62d7bf3b22e9ffdddfdd96
sseung416 <choiseungyeon416@gmail.com>
2023-11-07 12:54:58 +0900

    refactor: Rank value 클래스에서 enum 클래스로 리팩토링

--- src/test/kotlin/lotto/model/RankTest.kt
+++ src/test/kotlin/lotto/model/RankTest.kt
@@ -1,11 +1,7 @@
 package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class RankTest {
 
@@ -18,19 +14,11 @@ class RankTest {
         val rank5 = Rank.of(3, false)
         val noRank = Rank.of(0, false)
 
-        assertThat(rank1?.prize).isEqualTo(Rank.Prize.First.money)
-        assertThat(rank2?.prize).isEqualTo(Rank.Prize.Second.money)
-        assertThat(rank3?.prize).isEqualTo(Rank.Prize.Third.money)
-        assertThat(rank4?.prize).isEqualTo(Rank.Prize.Fourth.money)
-        assertThat(rank5?.prize).isEqualTo(Rank.Prize.Fifth.money)
+        assertThat(rank1?.prize).isEqualTo(Rank.First.prize)
+        assertThat(rank2?.prize).isEqualTo(Rank.Second.prize)
+        assertThat(rank3?.prize).isEqualTo(Rank.Third.prize)
+        assertThat(rank4?.prize).isEqualTo(Rank.Fourth.prize)
+        assertThat(rank5?.prize).isEqualTo(Rank.Fifth.prize)
         assertThat(noRank).isNull()
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [-1, 0, 10, 6])
-    fun `범위에 벗어나는 랭크 값일 경우, 오류를 반환한다`(value: Int) {
-        assertThatThrownBy { Rank(value) }
-            .isExactlyInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage(Rank.OUT_OF_RANGE_ERROR)
     }
 }
