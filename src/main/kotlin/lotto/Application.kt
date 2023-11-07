289b9a5ed3ab1d7c65c64280995ddd3fd97bbd49
ChaeHyunsic <chaehyunsic630@gmail.com>
2023-11-07 23:37:13 +0900

    implemented a simple lotto drawing

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,132 @@
-package lotto
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+import lotto.Lotto
+import lotto.Prize
 
 fun main() {
-    TODO("프로그램 구현")
+    val purchaseAmount = getPurchaseAmount()
+    val lottos = generateLotto(purchaseAmount)
+
+    val winningNumbers = getWinningNumbers()
+    val bonusNumber = getBonusNumber()
+
+    val result = calculateResult(lottos, winningNumbers, bonusNumber)
+
+    printResult(result)
+}
+
+fun getPurchaseAmount(): Int {
+    while (true) {
+        try {
+            println("구입 금액을 입력해 주세요: ")
+            val amount = Console.readLine().toInt()
+            if (amount % 1000 == 0) {
+                return amount
+            } else {
+                throw IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.")
+            }
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 올바른 숫자를 입력해주세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun generateLotto(purchaseAmount: Int): List<Lotto> {
+    val numberOflottos = purchaseAmount / 1000
+    val lottos = mutableListOf<Lotto>()
+
+    repeat(numberOflottos) {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        val lotto = Lotto(numbers)
+        lottos.add(lotto)
+    }
+
+    println("\n${lottos.size}개를 구매했습니다.")
+    lottos.forEach { lotto ->
+        println(lotto.getSortedNumbers())
+    }
+
+    return lottos
+}
+
+fun getWinningNumbers(): List<Int> {
+    while (true) {
+        try {
+            println("\n당첨 번호를 입력해 주세요: ")
+            val input = Console.readLine()
+            val numbers = input.split(",").map { it.toInt() }
+            if (numbers.size == 6 && numbers.all { it in 1..45 }) {
+                return numbers
+            } else {
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            }
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 올바른 숫자를 입력해주세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun getBonusNumber(): Int {
+    while (true) {
+        try {
+            println("\n보너스 번호를 입력해 주세요: ")
+            val number = Console.readLine().toInt()
+            if (number in 1..45) {
+                return number
+            } else {
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            }
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 올바른 숫자를 입력해주세요.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun calculateResult(
+    lottos: List<Lotto>,
+    winningNumbers: List<Int>,
+    bonusNumber: Int
+): Map<Prize, Int> {
+    val result = mutableMapOf<Prize, Int>().withDefault { 0 }
+
+    for (lotto in lottos) {
+        val rank = lotto.matchWith(winningNumbers, bonusNumber)
+        result[rank] = result.getValue(rank) + 1
+    }
+
+    return result
+}
+
+fun printResult(result: Map<Prize, Int>) {
+    println("\n당첨 통계")
+    println("---")
+    Prize.entries.dropLast(1).forEach { rank ->
+        val count = result.getValue(rank)
+        val prize = rank.prize
+        if (prize.toInt() == 30_000_000) {
+            println("${rank.matchCount}개 일치, 보너스 볼 일치 (${prize.formatCurrency()}) - ${count}개")
+        }
+        else {
+            println("${rank.matchCount}개 일치 (${prize.formatCurrency()}) - ${count}개")
+        }
+    }
+    println("총 수익률은 ${calculateTotalProfitRate(result)}%입니다.")
+}
+
+fun Long.formatCurrency(): String {
+    return String.format("%,d원", this)
+}
+
+fun calculateTotalProfitRate(result: Map<Prize, Int>): Double {
+    val totalSpent = result.values.sum() * 1000
+    val totalPrize = result.map { it.key.prize * it.value }.sum()
+    
+    return (totalPrize.toDouble() / totalSpent) * 100
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,24 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.all { it in 1..45 })
+        require(numbers.toSet().size == 6)
+    }
+    
+    fun getSortedNumbers(): List<Int> {
+        return numbers.sorted()
     }
 
-    // TODO: 추가 기능 구현
+    fun matchWith(userNumbers: List<Int>, bonusNumber: Int): Prize {
+        val matchedNumbers = numbers.intersect(userNumbers.toSet())
+        val bonusMatch = numbers.contains(bonusNumber)
+
+        return when (matchedNumbers.size) {
+            6 -> Prize.FIRST
+            5 -> if (bonusMatch) Prize.SECOND else Prize.THIRD
+            4 -> Prize.FOURTH
+            3 -> Prize.FIFTH
+            else -> Prize.NONE
+        }
+    }
 }
