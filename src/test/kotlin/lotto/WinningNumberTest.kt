bc3ab958906a834f32f28c1d351fa76983ebeb0d
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 13:12:55 +0900

    Test: 당첨 번호 개수가 6개가 아닐 때 테스트 구현

--- src/test/kotlin/lotto/WinningNumberTest.kt
+++ src/test/kotlin/lotto/WinningNumberTest.kt
@@ -7,6 +7,13 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class WinningNumberTest {
 
+    @Test
+    fun `당첨 번호의 개수가 6개가 아닐 때`() {
+        assertThrows<IllegalArgumentException> {
+            WinningNumber("2,6,9,14,24,32,39,43")
+        }
+    }
+
     @ParameterizedTest
     @ValueSource(strings = ["1,3,4,6 8,9", "2/6,9,14,24,32"])
     fun `당첨 번호를 쉼표로 구분하지 않았을 때`(winningNumber: String) {
