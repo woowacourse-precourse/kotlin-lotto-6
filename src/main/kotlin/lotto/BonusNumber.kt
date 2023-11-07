d9706a260a1bf8d76437018485b28ac3d353b096
jklom0326 <jklom0326@gmail.com>
2023-11-08 00:14:55 +0900

    refactor: BounsNumber 유효성 검사 추가
    - 1부터 45사이의 수가 아닐 때 예외 처리

--- src/main/kotlin/lotto/BonusNumber.kt
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -5,6 +5,11 @@ class BonusNumber(private val number: String, val lotto: Lotto) {
     init {
         require(number.toIntOrNull() == null) { NOT_NUMBER_ERROR }
         require(isContains()) { DUPLICATED_NUMBER_ERROR }
+        require(isNumberInRange()) {NOT_IN_RANGE_ERROR}
+    }
+
+    private fun isNumberInRange(): Boolean {
+        return number.toInt() in 1..45
     }
 
     private fun isContains(): Boolean {
@@ -13,8 +18,13 @@ class BonusNumber(private val number: String, val lotto: Lotto) {
         return !lotto.contains(bonusNumber)
     }
 
+    fun getBonusNumber(): Int {
+        return number.toInt()
+    }
+
     companion object {
         const val NOT_NUMBER_ERROR = "[ERROR] 숫자만 입력해주세요"
         const val DUPLICATED_NUMBER_ERROR = "[ERROR] 중복된 숫자입니다."
+        const val NOT_IN_RANGE_ERROR = "[ERROR] 1 부터 45 사이의 수를 입력해주세요"
     }
 }
