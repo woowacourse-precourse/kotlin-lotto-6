5e66998bf71d94d9950478806432bb2c6eb34220
jklom0326 <jklom0326@gmail.com>
2023-11-07 17:00:02 +0900

    refactor: 금액의 자료형 변경

--- src/main/kotlin/lotto/Rank.kt
+++ src/main/kotlin/lotto/Rank.kt
@@ -1,13 +1,13 @@
 package lotto
 
 enum class Rank(
-    private val winningNumber: Int,
-    private val money: Int,
+    val winningNumber: Int,
+    val money: Double,
 ) {
-    FIRST_RANK(6, 2_000_000_000),
-    SECOND_RANK(5, 30_000_000),
-    THIRD_RANK(5, 1_500_000),
-    FOURTH_RANK(4, 50_000),
-    FIFTH_RANK(3, 5_000),
-    MISS(6, 0),
+    FIRST_RANK(6, 2_000_000_000.0),
+    SECOND_RANK(5, 30_000_000.0),
+    THIRD_RANK(5, 1_500_000.0),
+    FOURTH_RANK(4, 50_000.0),
+    FIFTH_RANK(3, 5_000.0),
+    MISS(0, 0.0),
 }
