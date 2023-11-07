e545ed7e05800eb77bae2295ca593d8a9071d4b5
Kim Gimun <kmkim7575@gmail.com>
2023-11-07 15:35:27 +0900

    test: 구매한 로또 정렬 테스트 케이스 추가

--- src/test/kotlin/lotto/domain/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/domain/lotto/LottoTest.kt
@@ -1,11 +1,35 @@
 package lotto.domain.lotto
 
 import domain.lotto.Lotto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
 class LottoTest {
+
+    lateinit var lotto: Lotto
+    lateinit var inputLotties: MutableList<List<Int>>
+    lateinit var expectedResult: MutableList<List<Int>>
+
+    @BeforeEach
+    fun setup() {
+        inputLotties = mutableListOf(
+            listOf(21, 8, 23, 43, 41, 42),
+            listOf(5, 38, 16, 32, 11, 3),
+            listOf(35, 7, 44, 16, 36, 11),
+            listOf(8, 42, 31, 1, 11, 41),
+        )
+
+        expectedResult = mutableListOf(
+            listOf(8, 21, 23, 41, 42, 43),
+            listOf(3, 5, 11, 16, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+        )
+    }
+
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -21,5 +45,12 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `구매한 로또 정렬 테스트`() {
+        inputLotties.zip(expectedResult) { inputItem, expectedItem ->
+            lotto = Lotto(inputItem)
+            val actualResult = lotto.sortedRandomNumbers()
+            assertThat(actualResult).isEqualTo(expectedItem)
+        }
+    }
 }
--- src/test/kotlin/lotto/domain/lotto/NumberPickerTest.kt
+++ src/test/kotlin/lotto/domain/lotto/NumberPickerTest.kt
@@ -18,35 +18,4 @@ class NumberPickerTest {
     fun `구매 횟수에 따른 로또 구매 시 각 로또 리스트의 번호 개수 테스트`() {
         assertThat(numberPicker.getRandomNumbers()).hasSize(8)
     }
-
-    @Test
-    fun `구매한 로또 정렬 테스트`() {
-        val inputLotties = mutableListOf(
-            listOf(21, 8, 23, 43, 41, 23, 42, 43),
-            listOf(5, 38, 16, 32, 11, 3),
-            listOf(35, 7, 44, 16, 36, 11),
-            listOf(8, 42, 31, 1, 11, 41),
-            listOf(38, 14, 16, 45, 13, 42),
-            listOf(7, 40, 42, 30, 43, 11),
-            listOf(2, 38, 22, 32, 13, 45),
-            listOf(22, 45, 1, 3, 5, 14,),
-        )
-
-        val expectedResult = mutableListOf(
-            listOf(8, 21, 23, 41, 42, 43),
-            listOf(3, 5, 11, 16, 32, 38),
-            listOf(7, 11, 16, 35, 36, 44),
-            listOf(1, 8, 11, 31, 41, 42),
-            listOf(13, 14, 16, 38, 42, 45),
-            listOf(7, 11, 30, 40, 42, 43),
-            listOf(2, 13, 22, 32, 38, 45),
-            listOf(1, 3, 5, 14, 22, 45),
-        )
-
-/*        val actualResult = numberPicker.sortRandomNumbers(inputLotties)
-
-        actualResult.forEach {
-            assertThat(it).isSorted
-        }*/
-    }
 }
