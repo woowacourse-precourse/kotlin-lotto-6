5d33863c1d6df5ae9bb25ac639b720c8f1318762
Bruce <bruce@Bruceui-MacBookPro.local>
2023-11-08 04:07:09 +0900

    [feat] lotto class

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,37 +1,60 @@
 package lotto
 
 import kotlin.text.toInt
+import java.util.Random
+import lotto.Lotto
 
 // import camp.nextstep.edu.missionutils.Randoms // pickUniqueNumbersInRange()
 // import camp.nextstep.edu.missionutils.Console // readLine()
-
+enum class Rank {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    BOOM
+}
 fun main() {
     
     println("구입금액을 입력해 주세요.")
     
-    var stopPoint = 1
-    lateinit var price: String
-
-    while (stopPoint == 1) {
+    lateinit var inputPrice: String
+    var price  = 0
+    
+    while (price == 0) {
         try {
-            price = readLine()!!
-            stopPoint = priceCheck(price)
+            inputPrice = readLine()!!
+            price = priceCheck(inputPrice)
         } catch (e: IllegalArgumentException) {
             println(e)
         }
     }
 
+    var lottoList = mutableListOf<Lotto>()
+    val randomValues = listOf<Int>(1,2,3,4,5,6)
+
+    for(i in 1..price / 1000) {
+        lottoList.add(Lotto(randomValues))
+    }
     
-    println(price)
+    val result = Array(6) { 0 }
+    val test1 = setOf<Int>(1, 2, 3, 4, 5)
+    val test2 = setOf<Int>(12, 23, 41, 13, 6)
+
+    val test3 = test1 + test2
+    
+    print("set size : ${test3.size}")
+    println(lottoList.size)
 
 }
 
-fun priceCheck(price: String): Int {
+fun priceCheck(inputPrice: String): Int {
     
-    val temp =price.toInt()
-    if (temp % 1000 == 0) {
-        return 0
+    val result =inputPrice.toInt()
+    if (result % 1000 == 0) {
+        return result
     } else {
         throw IllegalArgumentException("[ERROR] 구입금액을 잘못 입력하였습니다.")
     }
 }
+
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,40 @@
 package lotto
+
+import lotto.Rank
 // val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
 
 // 제공된 Lotto 클래스를 활용해 구현해야 한다.
 // numbers의 접근 제어자인 private을 변경할 수 없다.
 // Lotto에 필드를 추가할 수 없다.
 // Lotto의 패키지 변경은 가능하다.
+
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun compareNumber(finalNum: List<Int>, bonusNum: Int): Int {
+        val numbersSet = numbers.toSet()
+        val FinalNumSet = finalNum.toSet()
+        val compareSet = numbersSet + FinalNumSet
+        
+        when (compareSet.size) {
+            9 -> return Rank.FIFTH.ordinal
+            8 -> return Rank.FOURTH.ordinal
+            7 -> bonusCompare(numbers, bonusNum)
+            6 -> return Rank.FIRST.ordinal
+            else -> return Rank.BOOM.ordinal
+        }
+        return Rank.BOOM.ordinal
+    }
+
+    fun bonusCompare(numbers: List<Int>, bonusNum: Int): Int {
+        if (numbers.contains(bonusNum)) {
+            return Rank.SECOND.ordinal
+        } else {
+            return Rank.THIRD.ordinal
+        }
+        
+    }
 }
