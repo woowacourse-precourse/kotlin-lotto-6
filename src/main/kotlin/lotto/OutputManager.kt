739ae49449f6320686cc3ce05e7009f4d1a7ac7d
LEE <tpqms775@naver.com>
2023-11-07 15:26:23 +0900

    fix : 출력값 오타 및 출력 포맷 수정

--- src/main/kotlin/lotto/OutputManager.kt
+++ src/main/kotlin/lotto/OutputManager.kt
@@ -23,15 +23,15 @@ class OutputManager {
         println("---")
         println("3개 일치 (5,000원) - ${fifthPlace}개")
         println("4개 일치 (50,000원) - ${fourthPlace}개")
-        println("5개 일치 (50,000원) - ${thirdPlace}개")
+        println("5개 일치 (1,500,000원) - ${thirdPlace}개")
         println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${secondPlace}개")
         println("6개 일치 (2,000,000,000원) - ${firstPlace}개")
     }
 
-    fun printRate(rate : Double){
-        val decimalFormat = DecimalFormat("#,###.#")
-        val profitMargin = decimalFormat.format(rate)
-        //val profitMargin = Math.round(rate * 100)
+    fun printRate(rate: Double) {
+        val decimalFormat = DecimalFormat("#,###.0")
+        val profitMargin = decimalFormat.format(rate * 100)
+
         println("총 수익률은 ${profitMargin}%입니다.")
     }
 }
