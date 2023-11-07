04d8476657d7259149358ab956514d9df4b7b099
leejieuns2 <2jjingni@gmail.com>
2023-11-08 05:56:38 +0900

    style: move package due to add package
    - package 추가로 인한 package 변경

--- src/test/kotlin/lotto/domain/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -1,7 +1,7 @@
-package lotto
+package lotto.domain
 
-import lotto.domain.WinningLotto
 import lotto.domain.model.Lotto
+import lotto.domain.model.WinningLotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
