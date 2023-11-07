993a8efb18bc53ab2705a3da51fa70b16e5a11b8
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:06:26 +0900

    [feat] "당첨 번호를 입력해 주세요." 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -3,6 +3,7 @@ package view
 object OutputView {
     private const val PLEASE_ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
     private const val BOUGHT_NUMBER_OF_THEM = "개를 구매했습니다."
+    private const val PLEASE_ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
 
     fun pleaseEnterPurchaseAmount() {
         println(PLEASE_ENTER_PURCHASE_AMOUNT)
@@ -10,5 +11,9 @@ object OutputView {
 
     fun purchasedIssuedLottoTickets(lottoPurchaseAmount: Int) {
         println("$lottoPurchaseAmount$BOUGHT_NUMBER_OF_THEM")
+    }
+
+    fun pleaseEnterWinningNumber() {
+        println(PLEASE_ENTER_WINNING_NUMBER)
     }
 }
