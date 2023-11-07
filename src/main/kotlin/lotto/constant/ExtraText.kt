3534eb0ff50828f46bf2e1e513c9f27807f60a26
posite <su84053@gmail.com>
2023-11-07 16:46:53 +0900

    refactor: 등수 enum으로 변경 및 enummap으로 수정

--- src/main/kotlin/lotto/constant/ExtraText.kt
+++ src/main/kotlin/lotto/constant/ExtraText.kt
@@ -1,10 +1,5 @@
 package lotto.constant
 
 enum class ExtraText(val text: String) {
-    NUMBER_SEPARATOR(","),
-    SAME_THREE_NUMBER("3개 일치 (5,000원)"),
-    SAME_FOUR_NUMBER("4개 일치 (50,000원)"),
-    SAME_FIVE_NUMBER("5개 일치 (1,500,000원)"),
-    SAME_FIVE_NUMBER_WITH_BONUS("5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    SAME_SIX_NUMBER("6개 일치 (2,000,000,000원)")
+    NUMBER_SEPARATOR(",")
 }
--- src/main/kotlin/lotto/domain/LottoGameResult.kt
+++ src/main/kotlin/lotto/domain/LottoGameResult.kt
@@ -1,17 +1,18 @@
 package lotto.domain
 
-import lotto.constant.ExtraText
+import lotto.constant.LottoRank
 import lotto.constant.PrintText
+import java.util.*
 
 class LottoGameResult {
-    private val allSameCount = mutableMapOf<Int, Int>()
+    private val allSameCount: MutableMap<LottoRank, Int> = EnumMap(LottoRank::class.java)
 
     init {
-        allSameCount[WINNING_COUNT_THREE_WINNINGS] = INIT_SAME_COUNT
-        allSameCount[WINNING_COUNT_FOUR_WINNINGS] = INIT_SAME_COUNT
-        allSameCount[WINNING_COUNT_FIVE_WINNINGS] = INIT_SAME_COUNT
-        allSameCount[WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS] = INIT_SAME_COUNT
-        allSameCount[WINNING_COUNT_SIX_WINNINGS] = INIT_SAME_COUNT
+        allSameCount[LottoRank.FIFTH_PLACE] = INIT_SAME_COUNT
+        allSameCount[LottoRank.FOURTH_PLACE] = INIT_SAME_COUNT
+        allSameCount[LottoRank.THIRD_PLACE] = INIT_SAME_COUNT
+        allSameCount[LottoRank.SECOND_PLACE] = INIT_SAME_COUNT
+        allSameCount[LottoRank.FIRST_PLACE] = INIT_SAME_COUNT
     }
 
     fun calculateResult(sameCount: Int, bonus: Int) {
@@ -24,8 +25,8 @@ class LottoGameResult {
 
     private fun saveResult(sameTotalCount: Int, bonus: Int) {
         if (bonus == INIT_SAME_COUNT && sameTotalCount == MAX_WINNING_COUNT) {
-            allSameCount[WINNING_COUNT_SIX_WINNINGS] =
-                allSameCount[WINNING_COUNT_SIX_WINNINGS]!! + PLUS_WINNING_COUNT
+            allSameCount[LottoRank.FIRST_PLACE] =
+                allSameCount[LottoRank.FIRST_PLACE]!! + PLUS_WINNING_COUNT
             return
         }
         saveExceptSixResult(sameTotalCount, bonus)
@@ -33,8 +34,8 @@ class LottoGameResult {
 
     private fun saveExceptSixResult(sameTotalCount: Int, bonus: Int) {
         if (bonus == BONUS_NUMBER_SAME && sameTotalCount == MAX_WINNING_COUNT) {
-            allSameCount[WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS] =
-                allSameCount[WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS]!! + PLUS_WINNING_COUNT
+            allSameCount[LottoRank.SECOND_PLACE] =
+                allSameCount[LottoRank.SECOND_PLACE]!! + PLUS_WINNING_COUNT
             return
         }
         saveExceptTopTwoResult(sameTotalCount)
@@ -42,8 +43,8 @@ class LottoGameResult {
 
     private fun saveExceptTopTwoResult(sameTotalCount: Int) {
         if (sameTotalCount == MIN_WINNING_COUNT) {
-            allSameCount[WINNING_COUNT_THREE_WINNINGS] =
-                allSameCount[WINNING_COUNT_THREE_WINNINGS]!! + PLUS_WINNING_COUNT
+            allSameCount[LottoRank.FIFTH_PLACE] =
+                allSameCount[LottoRank.FIFTH_PLACE]!! + PLUS_WINNING_COUNT
             return
         }
         saveOtherResult(sameTotalCount)
@@ -51,37 +52,30 @@ class LottoGameResult {
 
     private fun saveOtherResult(sameTotalCount: Int) {
         if (sameTotalCount == WINNING_COUNT_FOUR) {
-            allSameCount[WINNING_COUNT_FOUR_WINNINGS] =
-                allSameCount[WINNING_COUNT_FOUR_WINNINGS]!! + PLUS_WINNING_COUNT
+            allSameCount[LottoRank.FOURTH_PLACE] =
+                allSameCount[LottoRank.FOURTH_PLACE]!! + PLUS_WINNING_COUNT
             return
         }
 
         if (sameTotalCount == WINNING_COUNT_FIVE) {
-            allSameCount[WINNING_COUNT_FIVE_WINNINGS] =
-                allSameCount[WINNING_COUNT_FIVE_WINNINGS]!! + PLUS_WINNING_COUNT
+            allSameCount[LottoRank.THIRD_PLACE] =
+                allSameCount[LottoRank.THIRD_PLACE]!! + PLUS_WINNING_COUNT
         }
     }
 
     fun toAllSameCountResult(): String {
         val sb = StringBuilder()
-        val sameNumberCounts = mapOf<String, Int>(
-            ExtraText.SAME_THREE_NUMBER.text to WINNING_COUNT_THREE_WINNINGS,
-            ExtraText.SAME_FOUR_NUMBER.text to WINNING_COUNT_FOUR_WINNINGS,
-            ExtraText.SAME_FIVE_NUMBER.text to WINNING_COUNT_FIVE_WINNINGS,
-            ExtraText.SAME_FIVE_NUMBER_WITH_BONUS.text to WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS,
-            ExtraText.SAME_SIX_NUMBER.text to WINNING_COUNT_SIX_WINNINGS
-        )
-        return toSameNumberCountsResult(sameNumberCounts, sb)
+
+        return toSameNumberCountsResult(sb)
     }
 
     private fun toSameNumberCountsResult(
-        sameNumberCounts: Map<String, Int>,
         sb: StringBuilder
     ): String {
-        for (sameNumberCount in sameNumberCounts) {
+        for (sameNumberCount in SAME_NUMBER_COUNTS) {
             sb.append(
-                sameNumberCount.key + PrintText.SEPARATE_SAME_COUNT.text
-                        + allSameCount[sameNumberCount.value] + PrintText.SAME_NUMBER_COUNT.text + PrintText.SEPARATE_LOTTES.text
+                sameNumberCount.key.text + PrintText.SEPARATE_SAME_COUNT.text
+                        + allSameCount[sameNumberCount.key] + PrintText.SAME_NUMBER_COUNT.text + PrintText.SEPARATE_LOTTES.text
             )
         }
         return sb.toString()
@@ -89,11 +83,11 @@ class LottoGameResult {
 
     fun calculateEarningRate(purchaseAmount: Int): String {
         var earningRate = 0.0
-        allSameCount.forEach {
-            earningRate += it.key * it.value
+        for (sameNumberCount in SAME_NUMBER_COUNTS) {
+            earningRate += allSameCount[sameNumberCount.key]!! * sameNumberCount.value
         }
         return PrintText.PRINT_EARNING_RATE.text + String.format(
-            "%.1f",
+            PERCENT_FORMAT,
             (earningRate / purchaseAmount) * PERCENT_VALUE
         ) + PrintText.PRINT_PERCENT.text
     }
@@ -112,5 +106,13 @@ class LottoGameResult {
         private const val WINNING_COUNT_FIVE_WINNINGS = 1500000
         private const val WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS = 30000000
         private const val WINNING_COUNT_SIX_WINNINGS = 2000000000
+        private const val PERCENT_FORMAT = "%.1f"
+        private val SAME_NUMBER_COUNTS = mapOf(
+            LottoRank.FIFTH_PLACE to WINNING_COUNT_THREE_WINNINGS,
+            LottoRank.FOURTH_PLACE to WINNING_COUNT_FOUR_WINNINGS,
+            LottoRank.THIRD_PLACE to WINNING_COUNT_FIVE_WINNINGS,
+            LottoRank.SECOND_PLACE to WINNING_COUNT_FIVE_WITH_BONUS_WINNINGS,
+            LottoRank.FIRST_PLACE to WINNING_COUNT_SIX_WINNINGS
+        )
     }
 }
