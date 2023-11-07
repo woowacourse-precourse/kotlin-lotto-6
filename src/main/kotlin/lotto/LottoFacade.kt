f1e3a83feae5da75147901b800044088664ab970
aprilgom <aprilgom@users.noreply.github.com>
2023-11-08 02:20:15 +0900

    refactor: PrizeCalculator 제거

--- src/main/kotlin/lotto/LottoFacade.kt
+++ src/main/kotlin/lotto/LottoFacade.kt
@@ -3,11 +3,17 @@ package lotto
 class LottoFacade(
     private val budget: Int,
     private val winNumberList: List<Int>,
-    private val bonusNumber: Int
+    private val bonusNumber: Int,
 ) {
     private val lottoBuyer = LottoBuyer(budget)
     private val lottoChecker = LottoChecker(winNumber = Lotto(winNumberList), bonusNumber)
     val lottoList = lottoBuyer.buy()
-    private val prizeCalculator = PrizeCalculator(lottoList, lottoChecker)
-    fun getWinningMap() = prizeCalculator.getWinningCountMap()
+
+    fun getWinningCountMap() = lottoList.groupingBy {
+        lottoChecker.checkLotto(it)
+    }.eachCount()
+    fun getSumOfWinningPrize() = lottoList.sumOf {
+        val position = lottoChecker.checkLotto(it)
+        position.winningPrize
+    }
 }
