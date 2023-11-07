dc337b0ce87cfdc21d4be625fa33b8edc0d7e64d
lee989898 <wnsrud1352@gmail.com>
2023-11-08 01:34:21 +0900

    [feat] "(3~6)개 일치 ({로또 상금}) - {로또 맞은 갯수}개" 형식으로 출력 메서드 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -10,6 +10,16 @@ object OutputView {
     private const val WINNING_STATISTICS = "당첨 통계"
     private const val THREE_DOT = "---"
     private const val WINNING_DETAIL = "당첨 내역"
+    private const val PART = "개"
+    private const val SAME = "일치"
+    private const val DOT = "-"
+    private const val FIRST_REWARD = "(2,000,000,000원)"
+    private const val SECOND_REWARD = "(30,000,000원)"
+    private const val THIRD_REWARD = "(1,500,000원)"
+    private const val FOURTH_REWARD = "(50,000원)"
+    private const val FIFTH_REWARD = "(5,000원)"
+    private const val BONUS_BALL = "보너스 볼"
+    private const val REST = ","
 
     fun pleaseEnterPurchaseAmount() {
         println(PLEASE_ENTER_PURCHASE_AMOUNT)
@@ -37,5 +47,15 @@ object OutputView {
 
     fun winningDetail() {
         println(WINNING_DETAIL)
+    }
+
+    fun winningMatchResult(lottoRank: LottoRanking) {
+        when (lottoRank) {
+            LottoRanking.FIRST -> println("${lottoRank.correct}$PART $SAME $FIRST_REWARD $DOT ${lottoRank.correct}$PART")
+            LottoRanking.SECOND -> println("${lottoRank.correct}$PART $SAME$REST $BONUS_BALL $SAME $SECOND_REWARD $DOT ${lottoRank.correct}$PART")
+            LottoRanking.THIRD -> println("${lottoRank.correct}$PART $SAME $THIRD_REWARD $DOT ${lottoRank.correct}$PART")
+            LottoRanking.FOURTH -> println("${lottoRank.correct}$PART $SAME $FOURTH_REWARD $DOT ${lottoRank.correct}$PART")
+            LottoRanking.FIFTH -> println("${lottoRank.correct}$PART $SAME $FIFTH_REWARD $DOT ${lottoRank.correct}$PART")
+        }
     }
 }
