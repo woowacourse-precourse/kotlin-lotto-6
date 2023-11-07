e4c3288e61d7e957c9f7af06d36b91daf1875247
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-11-07 15:57:41 +0900

    (feacture) 로또 구입 금액 입력

---
+++ src/main/kotlin/lotto/Controller/LottoController.kt
@@ -0,0 +1,23 @@
+package lotto.Controller
+
+fun howManyBuyLotto(lottoPrice: String): Int {
+
+//    lottoPrice.toInt()
+//
+//    if(lottoPrice%1000==0){
+//        return lottoPrice/1000
+//    }
+//    //TODO : Exceptions 처리
+    return -1
+}
+
+object LottoController {
+    fun gameStart(){
+        println("구입금액을 입력해 주세요.")
+        val lottoPrice = readLine()
+
+//        if (lottoPrice != null) {
+//            howManyBuyLotto(lottoPrice)
+//        }
+    }
+}
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,19 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.all { it in 1..45 })
     }
-
     // TODO: 추가 기능 구현
+}
+
+class LottoGameModel(howManyBuyLotto: Int) {
+
+    fun createLottoNumbers() : List<Int>{
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return numbers.sorted()
+    }
 }
