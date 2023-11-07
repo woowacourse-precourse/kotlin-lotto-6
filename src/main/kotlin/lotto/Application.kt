9b11403f2f96f6da6327b1b976cd418f52ef0189
sooohi <68193613+sooohi@users.noreply.github.com>
2023-11-07 23:02:30 +0900

    feat : 횟수만큼 랜덤번호생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,6 +2,9 @@ package lotto
 
 fun main() {
     val inputMoney =lottoMoneyInput()
+    val count = lottoCnt(inputMoney)
+    val generatedLottoList = lottoNumberLimit(count)
+
 }
 fun lottoMoneyInput(): Int {
     println("구입금액을 입력해 주세요.")
@@ -10,4 +13,22 @@ fun lottoMoneyInput(): Int {
         throw IllegalArgumentException("1000원 단위로 입력해 주세요.")
     }
     return inputMoney
+}
+
+fun lottoCnt(inputMoney: Int): Int {
+    return inputMoney / 1000
+}
+fun lottoNumberLimit(count: Int): List<List<Int>> {
+    val comLottoList = mutableListOf<List<Int>>()
+    val randomNumber = java.util.Random()
+    repeat(count) {
+        val lotto = mutableSetOf<Int>()
+        while (lotto.size < 6) {
+            val num = randomNumber.nextInt(45) + 1
+            lotto.add(num)
+        }
+        println("생성된 로또 번호: $lotto")
+        comLottoList.add(lotto.toList())
+    }
+    return comLottoList
 }
