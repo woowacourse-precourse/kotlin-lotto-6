4c86895442910b1fd281213d9b1230f2972e7189
sooohi <68193613+sooohi@users.noreply.github.com>
2023-11-07 22:48:45 +0900

    feat : 구매금액 입력받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    val inputMoney =lottoMoneyInput()
+}
+fun lottoMoneyInput(): Int {
+    println("구입금액을 입력해 주세요.")
+    val inputMoney = readLine()!!.toInt()
+    if (inputMoney % 1000 != 0) {
+        throw IllegalArgumentException("1000원 단위로 입력해 주세요.")
+    }
+    return inputMoney
 }
