7ed81546686c722e3555870b224f25b4fa3f913f
yoon <cbj0010@naver.com>
2023-11-08 02:21:55 +0900

    [Feat] 로또 아주머니가 원하는 로또만큼 제공해줌
    로또에 중복이 있을 경우와 중복이 없는 경우 오름차순으로 정리 된 제대로 된 예쁜 로또를 만들 수 있다.

--- src/main/kotlin/lotto/LottoSeller.kt
+++ src/main/kotlin/lotto/LottoSeller.kt
@@ -9,19 +9,23 @@ class LottoSeller(private val lottoMoney: Int) {
     //what/who 계속 생각하기!
     //어떤 행위가 필요한가! 이 행위를 수행할 객체는 어디인가!
 
-    fun generateLottoNumbers(): List<LottoGenerator> {
+    fun generateLottoNumbers(): List<LottoNumberGenerator> {
         //고객이 요구한 만큼 받아서 한번에 주는 거
-        val result = mutableListOf<LottoGenerator>()
+        val result = mutableListOf<LottoNumberGenerator>()
         repeat(checkLottoTicketCount()) {
-            result.add(LottoGenerator())
+            result.add(LottoNumberGenerator())
         }
         return result
+    }
+
+    private fun createLotto(): List<Int> {
+        return LottoNumberGenerator().generate()
     }
 
     private fun checkLottoTicketCount(): Int {   //x원 받았는데 로또 몇장이냐?에 대한 대답
         //가격을 파는 사람이 알지 사는 사람이 우째 안대유
         if (lottoMoney % LOTTO_TICKET_PRICE != LOTTO_TICKET_REMAINDER) {
-            return throw IllegalArgumentException(EROOR_INPUT_MONEY + "돈을 입력받는 곳으로 다시 가야됨")
+            throw IllegalArgumentException(EROOR_INPUT_MONEY + "돈을 입력받는 곳으로 다시 가야됨")
         }
         return lottoMoney / LOTTO_TICKET_PRICE
     }
