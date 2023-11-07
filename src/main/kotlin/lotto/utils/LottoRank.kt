475f93546f9ce2e44b6dfbed0ad50a734eedd3db
Namyunsuk <yunsuk0328@naver.com>
2023-11-07 23:29:31 +0900

    feat(LottoRank): 열거형 클래스를 사용해 당첨에 대한 정보 저장, 일치한 수에 따른 값을 찾는 기능 구현

---
+++ src/main/kotlin/lotto/utils/LottoRank.kt
@@ -0,0 +1,16 @@
+package lotto.utils
+
+enum class LottoRank(val numberMatch: Int, val prize: Int, val bonusNumber: Boolean) {
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    MISS(0, 0, false);
+
+    companion object {
+        fun matchRank(numberMatch: Int, bonusNumber: Boolean): LottoRank {
+            return entries.first{it.numberMatch==numberMatch && it.bonusNumber==bonusNumber}
+        }
+    }
+}
