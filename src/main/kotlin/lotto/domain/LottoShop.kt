949f33405e0292dcc36a9b83ba73710414daa27f
tgyuu-An <atg0614@gmail.com>
2023-11-08 02:31:33 +0900

    add: RandomNumberGenerator 생성

--- src/main/kotlin/lotto/domain/LottoShop.kt
+++ src/main/kotlin/lotto/domain/LottoShop.kt
@@ -2,23 +2,23 @@ package lotto.domain
 
 import lotto.domain.validator.LottoShopValidator
 
-class LottoShop() {
+class LottoShop {
 
     private val lottoShopValidator = LottoShopValidator()
 
-    fun purchaseLotto(input : String) : List<Lotto>{
+    fun purchaseLotto(input: String): List<Lotto> {
         lottoShopValidator.validatePurchaseLottoInput(input)
 
-        val lottos : MutableList<Lotto> = mutableListOf()
+        val lottos: MutableList<Lotto> = mutableListOf()
 
-        repeat(input.toInt()/ LOTTO_PRICE){
-            lottos.add(Lotto(listOf(1,2,3,4,5,6)))
+        repeat(input.toInt() / LOTTO_PRICE) {
+            lottos.add(Lotto(listOf(1, 2, 3, 4, 5, 6)))
         }
 
         return lottos.toList()
     }
 
-    companion object{
+    companion object {
         val LOTTO_PRICE = 1000
     }
 }
