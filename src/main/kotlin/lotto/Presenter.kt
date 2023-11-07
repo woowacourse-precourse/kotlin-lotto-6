4dcfcb49bfa8189739f83dfb5ad0ae3c6eb2199b
oh in seong <superohinsung@naver.com>
2023-11-07 17:38:55 +0900

    [feat] Add Presenter fuctions init Setting

--- src/main/kotlin/lotto/Presenter.kt
+++ src/main/kotlin/lotto/Presenter.kt
@@ -3,11 +3,52 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class Presenter(
-    private val printer: Printer = Printer()
+    private val printer: Printer = Printer(),
+    private val game: Game = Game()
 ) {
 
     fun execute() {
 
+    }
+
+    fun purchaseLottery() {
+        try {
+
+        } catch (e: IllegalArgumentException) {
+            print(Message.ERROR)
+            println(e)
+            execute()
+        }
+    }
+
+    fun createRandomLottery(){
+        try {
+
+        } catch (e: IllegalArgumentException) {
+            print(Message.ERROR)
+            println(e)
+            execute()
+        }
+    }
+
+    fun inputUserPickNumberAndBonusNumber() {
+        try {
+            val input = Console.readLine().trim()
+        } catch (e: IllegalArgumentException) {
+            print(Message.ERROR)
+            println(e)
+            execute()
+        }
+    }
+
+    fun getLotteryWinning(){
+        try {
+            
+        } catch (e: IllegalArgumentException) {
+            print(Message.ERROR)
+            println(e)
+            execute()
+        }
     }
 
 
