10cf5d134f20534f65b8088c46bfd1366fb9e563
lh99j <grand2181@gmail.com>
2023-11-07 13:54:13 +0900

    test(LottoResultTest): 테스트 커버리지 추가

--- src/test/kotlin/lotto/model/LottoResultTest.kt
+++ src/test/kotlin/lotto/model/LottoResultTest.kt
@@ -1,10 +1,12 @@
 package lotto.model
 
 import lotto.util.Constants.RANKING_1ST_INDEX
+import lotto.util.Constants.RANKING_2ST_INDEX
+import lotto.util.Constants.RANKING_3ST_INDEX
 import lotto.util.Constants.RANKING_4ST_INDEX
+import lotto.util.Constants.RANKING_5ST_INDEX
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -15,7 +17,14 @@ class LottoResultTest {
 
     @ParameterizedTest
     @DisplayName("로또 랭킹이 올바르게 반환되는지 확인한다.")
-    @CsvSource(value = ["1,2,3,4,10,11,${RANKING_4ST_INDEX}", "1,2,3,4,5,6,${RANKING_1ST_INDEX}"], delimiter = ',')
+    @CsvSource(
+        value = ["1,2,3,4,5,6,${RANKING_1ST_INDEX}",
+            "1,2,3,4,5,7,${RANKING_2ST_INDEX}",
+            "1,2,3,4,5,8,${RANKING_3ST_INDEX}",
+            "1,2,3,4,10,11,${RANKING_4ST_INDEX}",
+            "1,2,3,41,42,43,${RANKING_5ST_INDEX}"],
+        delimiter = ','
+    )
     fun calculateRankingTest(num1: Int, num2: Int, num3: Int, num4: Int, num5: Int, num6: Int, expected: Int) {
         val validation = lottoResult.calculateRanking(listOf(num1, num2, num3, num4, num5, num6))
         assertThat(validation).isEqualTo(expected)
