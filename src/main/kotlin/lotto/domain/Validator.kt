8d8ce34ed58503266c2528cd07ed1013c8be3557
m6z1 <sonmyungj1zz@gmail.com>
2023-11-08 02:45:10 +0900

    refactor(Validator): 보너스 유효성 검사 부분 삭제

--- src/main/kotlin/lotto/domain/Validator.kt
+++ src/main/kotlin/lotto/domain/Validator.kt
@@ -19,23 +19,4 @@ class Validator {
     private fun validatePrice1000Unit(price: String) {
         require(price.toInt() % 1000 == 0)
     }
-
-
-    fun validateBonusNum(bonusNum: String, lottoNum: List<Int>) {
-        validateBonusNumIsBlank(bonusNum)
-        validateBonusNumRange(bonusNum)
-        validateBonusNumDuplicateLottoNum(bonusNum, lottoNum)
-    }
-
-    private fun validateBonusNumIsBlank(bonusNum: String) {
-        require(bonusNum.isNotBlank())
-    }
-
-    private fun validateBonusNumRange(bonusNum: String) {
-        require(bonusNum.toInt() in 1..45)
-    }
-
-    private fun validateBonusNumDuplicateLottoNum(bonusNum: String, lottoNum: List<Int>) {
-        require(!lottoNum.contains(bonusNum.toInt()))
-    }
 }
