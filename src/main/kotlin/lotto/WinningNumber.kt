01de4395753bf099587ba9d233ed1dda390eca70
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 12:38:52 +0900

    Feat: 당첨 번호 유효성 검사(숫자가 아닌 문자를 입력했을 때, 값이 비어있을 때)

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -2,17 +2,25 @@ package lotto
 
 class WinningNumber(private val numbers: String) {
 
+    private val winningNumbers = numbers.split(",")
+
     init {
         require(checkNumbersOfCommas()) { "$ERROR $PLEASE_SEPARATE_NUMBERS_COMMAS" }
+        require(isNumberOrNull()) { "$ERROR $INPUT_ONLY_NUMBER" }
     }
 
     private fun checkNumbersOfCommas(): Boolean {
         return numbers.count { it == ',' } == 5
     }
 
+    private fun isNumberOrNull(): Boolean {
+        return winningNumbers.all { it.toIntOrNull() != null }
+    }
+
     companion object {
         const val ERROR = "[ERROR]"
         const val PLEASE_SEPARATE_NUMBERS_COMMAS = "당첨 번호는 쉼표(,) 기준으로 구분해주세요."
+        const val INPUT_ONLY_NUMBER = "당첨 번호는 숫자만 입력 가능합니다."
     }
 
 }
