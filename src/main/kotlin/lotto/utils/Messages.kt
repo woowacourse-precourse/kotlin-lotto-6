2314684825c23db943d4d168dc9af1b8a6606db5
stopstone <wltjr6432@naver.com>
2023-11-07 20:04:46 +0900

    style: 모호한 에러 메시지 수정

--- src/main/kotlin/lotto/utils/Messages.kt
+++ src/main/kotlin/lotto/utils/Messages.kt
@@ -11,12 +11,12 @@ object Messages {
     const val PROFIT_MESSAGE = "총 수익률은 %.1f%%입니다."
 
     const val ERROR_MESSAGE = "[ERROR]"
-    const val INVALID_INPUT = "유효한 값이 아닙니다."
+    const val INVALID_INPUT = "숫자가 아닌 다른 값은 입력될 수 없습니다."
     const val VALIDATE_INPUT_EMPTY = "입력값이 없습니다."
-    const val VALIDATE_INPUT_UNDER_THOUSAND = "1000보다 적은 금액은 입력될 수 없습니다."
+    const val VALIDATE_INPUT_UNDER_THOUSAND = "1000원보다 적은 금액은 입력될 수 없습니다."
     const val MY_NUMBERS_SIZE_MESSAGE = "숫자 6개가 입력되지 않았습니다."
-    const val VALIDATE_NO_REMINDER = "금액을 1000으로 나눌 수 없습니다."
-    const val MY_NUMBERS_DELIMITER_MESSAGE = "구분하기 위한 쉼표를 찾을 수 없습니다."
-    const val MY_NUMBERS_OVER_RANGE_MESSAGE = "입력 범위를 벗어났습니다. (1~45에서 입력가능)"
-    const val MY_NUMBERS_DUPLICATED_MESSAGE = "중복된 값은 입력할 수 없습니다."
+    const val VALIDATE_NO_REMINDER = "입력 금액이 1000원 단위가 아닙니다."
+    const val MY_NUMBERS_DELIMITER_MESSAGE = "숫자를 구분하기 위한 쉼표를 찾을 수 없습니다."
+    const val MY_NUMBERS_OVER_RANGE_MESSAGE = "입력 범위가 아닙니다. (1~45에서 입력가능)"
+    const val MY_NUMBERS_DUPLICATED_MESSAGE = "당첨번호는 중복된 값을 입력할 수 없습니다."
 }
