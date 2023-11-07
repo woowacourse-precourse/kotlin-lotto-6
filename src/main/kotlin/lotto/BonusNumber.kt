0dcf764691b088805b744cf1e1caeb742fcda65b
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 15:19:42 +0900

    Feat: 보너스 번호 유효성 검사 기능(1~45가 아닌 숫자를 입력했을 때)

--- src/main/kotlin/lotto/BonusNumber.kt
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -6,15 +6,21 @@ class BonusNumber(
 
     init {
         require(isNumber()) { "$ERROR $INPUT_ONLY_NUMBER" }
+        require(numbersOutOfRange()) { "$ERROR $BONUS_NUMBER_RANGE" }
     }
 
     private fun isNumber(): Boolean {
         return number.toIntOrNull() != null
     }
 
+    private fun numbersOutOfRange(): Boolean {
+        return number.toInt() in 1..45
+    }
+
     companion object {
         const val ERROR = "[ERROR]"
         const val INPUT_ONLY_NUMBER = "보너스 번호는 숫자만 입력 가능합니다."
+        const val BONUS_NUMBER_RANGE = "보너스 번호는 1부터 45사이의 숫자여야 합니다."
     }
 
 }
