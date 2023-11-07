42079aa5e7aff67ced4b0eecb7edb6ab52a25db2
stopkite <stopkite98@gmail.com>
2023-11-08 08:53:01 +0900

    refactor(Lotto/LottoTicketGenerator): companion object 제거

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,9 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == LOTTO_NUMBER_COUNT) { LOTTO_NUMBER_ERROR }
-        require(numbers.toSet().size == LOTTO_NUMBER_COUNT) { SAME_NUMBER_ERROR }
-        require(numbers.all { it in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER }) { NUMBER_RANG_ERROR }
+        require(numbers.size == LottoNumber.LOTTO_NUMBER_COUNT.number) { ErrorMessage.LOTTO_NUMBER_COUNT.message }
+        require(numbers.toSet().size == LottoNumber.LOTTO_NUMBER_COUNT.number) { ErrorMessage.SAME_NUMBER.message }
+        require(numbers.all { it in LottoNumber.MIN_LOTTO_NUMBER.number..LottoNumber.MAX_LOTTO_NUMBER.number }) { ErrorMessage.NUMBER_RANGE.message}
     }
 
     override fun toString(): String = numbers.joinToString(", ", "[", "]")
@@ -12,13 +12,4 @@ class Lotto(private val numbers: List<Int>) {
     fun countMatchingNumbers(winningNumbers: List<Int>): Int = numbers.count { winningNumbers.contains(it) }
 
     fun hasBonusNumber(bonusNumber: Int): Boolean = numbers.contains(bonusNumber)
-
-    companion object {
-        const val LOTTO_NUMBER_ERROR = "로또에는 6개의 숫자가 입력되어야 합니다."
-        const val SAME_NUMBER_ERROR = "중복된 숫자가 있으면 안됩니다."
-        const val NUMBER_RANG_ERROR = "1과 45 사이의 숫자가 입력되어야 합니다."
-        const val LOTTO_NUMBER_COUNT = 6
-        const val MIN_LOTTO_NUMBER = 1
-        const val MAX_LOTTO_NUMBER = 45
-    }
 }
--- src/main/kotlin/lotto/LottoTicketGenerator.kt
+++ src/main/kotlin/lotto/LottoTicketGenerator.kt
@@ -3,11 +3,5 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoTicketGenerator {
-    fun createAutoTicket() = Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT).sorted().toList())
-
-    companion object {
-        const val MIN_LOTTO_NUMBER = 1
-        const val MAX_LOTTO_NUMBER = 45
-        const val LOTTO_NUMBER_COUNT = 6
-    }
+    fun createAutoTicket() = Lotto(Randoms.pickUniqueNumbersInRange(LottoNumber.MIN_LOTTO_NUMBER.number, LottoNumber.MAX_LOTTO_NUMBER.number, LottoNumber.LOTTO_NUMBER_COUNT.number).sorted().toList())
 }
