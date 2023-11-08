10ee62efe968fc1a995a91bef46a041c1379ba3e
sseung416 <choiseungyeon416@gmail.com>
2023-11-08 13:13:45 +0900

    feat, refactor: Payment 유효성 검증 로직 PaymentValidator로 옮기고 구입 금액 10만원 제한 로직 추가

--- src/main/kotlin/lotto/model/Payment.kt
+++ src/main/kotlin/lotto/model/Payment.kt
@@ -2,27 +2,21 @@ package lotto.model
 
 import lotto.model.seller.Money
 import lotto.model.seller.toMoney
-import lotto.requireAndReturn
-import lotto.toValidInt
-import lotto.validPositiveNumber
+import lotto.model.validator.PaymentValidation
+import lotto.model.validator.PaymentValidator
 
 class Payment private constructor(lottoPrice: Money, val cost: Money) {
 
     val purchase: Int = cost.value / lottoPrice.value
 
     companion object {
-        private const val INVALID_MONEY_ERROR = "1000원 단위의 금액만 입력할 수 있습니다."
+        fun from(input: String, lottoPrice: Int, validator: PaymentValidator = PaymentValidator()): Payment {
+            validator.validate(PaymentValidation(input, lottoPrice))
 
-        fun from(input: String, lottoPrice: Int): Payment {
             val price = lottoPrice.toMoney()
-            val cost = input.toValidInt()
-                .validPositiveNumber()
-                .validLottoPrice(price)
-                .toMoney()
+            val cost = input.toInt().toMoney()
+
             return Payment(price, cost)
         }
-
-        private fun Int.validLottoPrice(price: Money): Int =
-            requireAndReturn(this % price.value == 0, INVALID_MONEY_ERROR)
     }
 }
--- src/test/kotlin/lotto/PaymentTest.kt
+++ src/test/kotlin/lotto/PaymentTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.model.Payment
+import lotto.model.validator.PaymentValidator
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
@@ -37,5 +38,14 @@ class PaymentTest {
     @Test
     fun `구입 금액이 8000원일 때, 로또 구매 개수가 8개이다`() {
         assertThat(Payment.from("8000", 1000).purchase).isEqualTo(8)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["100001", "999999999999999999999999999999"])
+    fun `구입 금액이 10만원 이상일 때, 오류를 반환한다`(input: String) {
+        assertThatThrownBy {
+            Payment.from(input, 1)
+        }.isExactlyInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage(PaymentValidator.OVERFLOW_ERROR)
     }
 }
