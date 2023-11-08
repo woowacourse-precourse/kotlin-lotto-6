9fd88931ba9127c25e9c68fcfe31d195d11be3a7
Taehwani-Storage <wiz_th1024@naver.com>
2023-11-08 14:34:55 +0900

    feat: lotto game v15 - 기능 테스트 오류 수정3(출력 결과)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -25,7 +25,7 @@ fun main() {
 fun readMoney(): Int {
     while (true) {
         try {
-            println("구입 금액을 입력해주세요.")
+            println("구입금액을 입력해 주세요.")
             val input = Console.readLine()
             val money = input.toIntOrNull() ?: throw IllegalArgumentException("[ERROR] 정확한 금액이 아닙니다!!")
             if (money < 1000 || money % 1000 != 0)
@@ -47,7 +47,7 @@ fun setLotteries(count: Int): List<Lotto> {
 
 // 구매한 로또 번호 출력하기
 fun printLotto(lotteries: List<Lotto>) {
-    println("${lotteries.size}개를 구매했습니다.")
+    println("\n${lotteries.size}개를 구매했습니다.")
     // getNumbers 메서드로 numbers 접근
     lotteries.forEach { lotto ->
         val lottoNums = lotto.getNumbers()
@@ -59,7 +59,7 @@ fun printLotto(lotteries: List<Lotto>) {
 fun readWinningNums(): Set<Int> {
     while (true) {
         try {
-            println("당첨 번호를 쉼표로 구분하여 입력해 주세요.")
+            println("\n당첨 번호를 입력해 주세요.")
             val input = Console.readLine()
             val numbers = input.split(",").map { it.trim().toIntOrNull() }
             if (numbers.size != 6 || numbers.any { it == null || it !in 1..45 })
@@ -77,7 +77,7 @@ fun readWinningNums(): Set<Int> {
 fun readBonusNum(wonNumbers: Set<Int>): Int {
     while (true) {
         try {
-            println("보너스 번호를 입력해 주세요.")
+            println("\n보너스 번호를 입력해 주세요.")
             val input = Console.readLine()
             val bonus = input.toIntOrNull() // 보너스 번호 정수로 변환
             if (bonus == null || bonus !in 1..45) throw IllegalArgumentException("[ERROR] 정확하지 않은 보너스 번호입니다!!")
@@ -92,7 +92,7 @@ fun readBonusNum(wonNumbers: Set<Int>): Int {
 // 당첨 결과 확인 함수
 fun checkResults(lotteries: List<Lotto>, wonNums: Set<Int>, bonusNum: Int): Map<Rank, Int> {
     // 모든 등수 당첨 개수 0으로 설정
-    val results = Rank.values().associateWith { 0 }.toMutableMap()
+    val results = Rank.entries.associateWith { 0 }.toMutableMap()
 
     lotteries.forEach { lotto ->
         val matchCount = lotto.getNumbers().count { it in wonNums } // 당첨 번호와 일치 개수
@@ -115,7 +115,7 @@ fun printStatistics(lottoResults: Map<Rank, Int>) {
     rankPrint.forEach { rank ->
         val count = lottoResults[rank] ?: 0
         val resultString = rankResult(rank)
-        println("$resultString - $count 개")
+        println("$resultString - ${count}개")
     }
 }
 
