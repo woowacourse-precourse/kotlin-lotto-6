8361907858e473b2af68f878e08eeb259e7102d6
MinJun <agvber12@gmail.com>
2023-11-08 16:28:12 +0900

    feat: 구입 금액 받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
+private var lottoPurchaseAmount: Int = -1
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("구입금액을 입력해 주세요.")
+    inputLottoPurchaseAmount()
+
+}
+
+private fun inputLottoPurchaseAmount() {
+    lottoPurchaseAmount = readLine().toInt()
 }
