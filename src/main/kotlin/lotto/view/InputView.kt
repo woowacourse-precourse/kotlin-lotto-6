678532806a8447f59be2fb14e446f520420212d4
mino0001 <minge271@gmail.com>
2023-11-08 17:20:19 +0900

    feat : InputView, OutputView 초안 작성

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,10 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+class InputView {
+
+    fun readUserInput(): String = Console.readLine().trim()
+
+    fun finish() = Console.close()
+
+}
