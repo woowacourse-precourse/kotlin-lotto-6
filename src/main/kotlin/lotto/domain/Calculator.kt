6307cf582e532338787b3a8a30b2fa337bcfdb52
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2023-11-08 18:14:27 +0900

    refactor : 총 당첨 금액 계산 코드 변경

--- src/main/kotlin/lotto/domain/Calculator.kt
+++ src/main/kotlin/lotto/domain/Calculator.kt
@@ -4,16 +4,18 @@ import lotto.model.Winning
 
 class Calculator {
 
-    fun calculateYield(amount : Int, reward: HashMap<Winning, Int>) : String{
+    fun calculateYield(amount: Int, reward: HashMap<Winning, Int>): String {
+
         return "%.1f".format(addTotalEarnings(reward).toDouble() / amount.toDouble() * 100)
     }
 
     private fun addTotalEarnings(reward: HashMap<Winning, Int>): Int {
 
-        return reward.getOrDefault(Winning.FIFTH,0) * Winning.FIFTH.reward +
-                reward.getOrDefault(Winning.FOURTH,0) * Winning.FOURTH.reward +
-                reward.getOrDefault(Winning.THIRD,0) * Winning.THIRD.reward +
-                reward.getOrDefault(Winning.SECOND,0) * Winning.SECOND.reward +
-                reward.getOrDefault(Winning.FIRST,0) * Winning.FIRST.reward
+        var totalWinning = 0
+
+        for ((winning, count) in reward) {
+            totalWinning += winning.reward * count
+        }
+        return totalWinning
     }
 }
