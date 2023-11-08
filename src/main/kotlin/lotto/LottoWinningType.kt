1d05fcc054c12f73f6fd91ae95ad7a5c35fbe881
dogmania <hj296914@gmail.com>
2023-11-08 23:31:40 +0900

    :sparkles: Feat: 당첨 금액에 대한 enum class 구현

--- src/main/kotlin/lotto/LottoWinningType.kt
+++ src/main/kotlin/lotto/LottoWinningType.kt
@@ -1,4 +1,9 @@
 package lotto
 
-enum class LottoWinningType {
+enum class LottoWinningType(val winningAmount: Int) {
+    FIFTH(5000),
+    FOURTH(50000),
+    THIRD(1500000),
+    SECOND(30000000),
+    FIRST(2000000000)
 }
