65efa0e764655f2ae2a80bbadfa29bfecb85192b
NOGUEN <nhg1113@naver.com>
2023-11-08 09:11:10 +0900

    fix: inputCost()메소드 로직 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,23 +1,50 @@
 package lotto.view
 
-import lotto.INPUT_MESSAGE_BONUS_NUMBER
-import lotto.INPUT_MESSAGE_COST
-import lotto.INPUT_MESSAGE_LOTTO_NUMBERS
+import camp.nextstep.edu.missionutils.Console
+import lotto.*
+import lotto.exception.ContainsNotDigitException
+import lotto.exception.UnvalidCostException
+import lotto.utility.Utils
+
+import kotlin.NumberFormatException
 
 class InputView {
+    val inputValidator = InputValidator()
     fun inputCost(): Int {
-        println(INPUT_MESSAGE_COST)
+        while (true) {
+            println(INPUT_MESSAGE_COST)
+            val userInput = Console.readLine()
 
-        return readLine()!!.toInt()
+            try {
+                inputValidator.checkStringHasNonDigits(userInput)
+                inputValidator.checkCostDevidedByUnit(userInput)
+                return userInput.toInt()
+            } catch(error: ContainsNotDigitException) {
+                println(error.message)
+            } catch(error: UnvalidCostException) {
+                println(error.message)
+            }
+        }
     }
 
     fun inputLottoNumbers(): String {
-        println(INPUT_MESSAGE_LOTTO_NUMBERS)
-        return readLine()!!
+        while (true) {
+            println(INPUT_MESSAGE_LOTTO_NUMBERS)
+            val userInput = Console.readLine()
+
+
+        }
     }
 
     fun inputBonusNumber(): Int {
         println(INPUT_MESSAGE_BONUS_NUMBER)
         return readLine()!!.toInt()
+    }
+
+    fun checkInputString(input: String): Boolean {
+        if (Utils.containsNonDigits(input)) {
+            throw NumberFormatException(EXCEPTION_CONTAINS_NOT_DIGIT)
+        }
+        return true
     }
 }
