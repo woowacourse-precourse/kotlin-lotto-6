d46c0a5b65e438ec53c49ac7b166140c51dcf5c3
bak <bak4017@gmail.com>
2023-11-08 16:47:33 +0900

    test(lotto): 당첨 번호 입력검증 테스트 추가

--- src/test/kotlin/lotto/validator/WinNumbersValidatorTest.kt
+++ src/test/kotlin/lotto/validator/WinNumbersValidatorTest.kt
@@ -13,10 +13,20 @@ import org.junit.jupiter.params.provider.ValueSource
 class WinNumbersValidatorTest : InputValidator() {
 
     @ParameterizedTest
+    @ValueSource(strings = [" ", ""])
+    fun `공백 or 아무 것도 입력하지 않았을 시`(value: String) {
+        val exception = assertThrows<IllegalArgumentException> {
+            WinNumbersValidator(listOf(value))
+        }
+
+        assertEquals("[ERROR] 공백을 제외한 값을 입력해 주세요", exception.message)
+    }
+
+    @ParameterizedTest
     @ValueSource(strings = ["abc", "-2000", "#421", "421#", "가나다"])
     fun `양의 정수가 아닐 시`(value: String) {
         val exception = assertThrows<IllegalArgumentException> {
-            checkForPositiveInteger(value)
+            WinNumbersValidator(listOf(value))
         }
 
         assertEquals("[ERROR] 양의 정수 이외의 숫자를 입력하지 말아주세요", exception.message)
@@ -26,7 +36,7 @@ class WinNumbersValidatorTest : InputValidator() {
     @ValueSource(strings = ["0", "46"])
     fun `범위 밖의 수 입력 시`(value: String) {
         val exception = assertThrows<IllegalArgumentException> {
-            checkForNumberRange(value)
+            WinNumbersValidator(listOf(value))
         }
 
         assertEquals("[ERROR] 로또 번호는 ${START_INCLUSIVE}부터 ${END_INCLUSIVE}사이만 가능합니다", exception.message)
@@ -35,7 +45,7 @@ class WinNumbersValidatorTest : InputValidator() {
     @Test
     fun `중복된 값 입력 시`() {
         val exception = assertThrows<IllegalArgumentException> {
-            checkForDuplicates(listOf("1", "1", "2", "3", "4", "5"))
+            WinNumbersValidator(listOf("1", "1", "2", "3", "4", "5"))
         }
 
         assertEquals("[ERROR] 중복된 값이 존재합니다", exception.message)
@@ -44,7 +54,7 @@ class WinNumbersValidatorTest : InputValidator() {
     @Test
     fun `COUNT개 미만 입력 시`() {
         val exception = assertThrows<IllegalArgumentException> {
-            checkForWinNumbersLength(listOf("1", "2", "3", "4", "5"))
+            WinNumbersValidator(listOf("1", "2", "3", "4", "5"))
         }
 
         assertEquals("[ERROR] 당첨 번호는 ${COUNT}개가 필요합니다", exception.message)
