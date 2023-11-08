0abdebd10f58d6ec9631b0d8d1564dd8987855b2
vonvolous <hanyoojin39@gmail.com>
2023-11-08 15:56:26 +0900

    Refactor : Application의 사용자 입력받는 부분 각각 함수로 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,17 +2,16 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 
-fun main() {
-    val seller = LottoSeller()
-    var winningResult = Lotto(listOf(1,2,3,4,5,6))
-    val winningPrint = arrayListOf("3개 일치 (5,000원) - ", "4개 일치 (50,000원) - ", "5개 일치 (1,500,000원) - ", "5개 일치, 보너스 볼 일치 (30,000,000원) - ", "6개 일치 (2,000,000,000원) - ")
-    var userInput = ""
-    var inputMoney = 0
+data class MoneyOutput(var seller: LottoSeller, var inputMoney: Int)
+
+fun getInputMoney() : MoneyOutput {
     var flag = 0
+    val seller = LottoSeller()
+    var inputMoney = 0
 
     while (flag != 1) {
         println("구입금액을 입력해 주세요.")
-        userInput = Console.readLine()
+        val userInput = Console.readLine()
         try {
             inputMoney = userInput.toInt()
             seller.buyTickets(inputMoney)
@@ -21,25 +20,32 @@ fun main() {
             println("[ERROR] " + error.message)
         }
     }
-    seller.createLotto()
+    return MoneyOutput(seller, inputMoney)
+}
 
-    flag = 0
+fun getWinningNumbers(seller: LottoSeller) : Pair<LottoSeller, Lotto> {
+    var flag = 0
+    var userLotto = Lotto(listOf(1,2,3,4,5,6))
+
     while (flag != 1) {
         println("당첨 번호를 입력해 주세요.")
-        userInput = Console.readLine()
+        val userInput = Console.readLine()
         seller.winningNumbers = userInput.split(",").map { it.toInt() }.toMutableList()
         try {
-            winningResult = Lotto(seller.winningNumbers)
+            userLotto = Lotto(seller.winningNumbers)
             flag = 1
         } catch (error: IllegalArgumentException) {
             println("[ERROR] " + error.message)
         }
     }
+    return Pair(seller, userLotto)
+}
 
-    flag = 0
+fun getBonusNumber(seller: LottoSeller) : LottoSeller {
+    var flag = 0
     while (flag != 1) {
         println("보너스 번호를 입력해 주세요.")
-        userInput = Console.readLine()
+        val userInput = Console.readLine()
         seller.bonusNumber = userInput.toInt()
         try {
             seller.validateBonusNumber()
@@ -49,14 +55,30 @@ fun main() {
             println("[ERROR] " + error.message)
         }
     }
+    return seller
+}
+
+fun main() {
+    //사용자에게 구입금액 입력받기
+    val winningPrint = arrayListOf("3개 일치 (5,000원) - ", "4개 일치 (50,000원) - ", "5개 일치 (1,500,000원) - ", "5개 일치, 보너스 볼 일치 (30,000,000원) - ", "6개 일치 (2,000,000,000원) - ")
+    var (seller, inputMoney) = getInputMoney()
+    seller.createLotto()
+
+    //사용자에게 당첨 로또 번호 입력받기
+    val getWinningOutput = getWinningNumbers(seller)
+    seller = getWinningOutput.first
+    val userLotto = getWinningOutput.second
+
+    //사용자에게 보너스 번호 입력받기
+    seller = getBonusNumber(seller)
 
     // winningCount는 index0부터 순서대로 3,4,5,5+보너스,6개 일치하는 개수 저장
     println("당첨 통계\n---")
-    val winningCount = winningResult.winningCount(seller.tickets, seller.bonusNumber)
+    val winningCount = userLotto.winningCount(seller.tickets, seller.bonusNumber)
     println(winningCount)
     for (i in 0..4){
         println("${winningPrint[i]}${winningCount[i]}개")
     }
-    winningResult.getProfitRate(winningCount, inputMoney)
+    userLotto.getProfitRate(winningCount, inputMoney)
 
 }
