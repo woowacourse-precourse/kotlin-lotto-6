57e2727de82a8b3d55b93ff750ebb3a8be9f76ec
seoyeon <seoyeonn051@gmail.com>
2023-11-08 09:06:43 +0900

    fix: 로또 구입 금액 string to int

--- src/main/kotlin/lotto/model/validation/InputValidation.kt
+++ src/main/kotlin/lotto/model/validation/InputValidation.kt
@@ -1,10 +1,21 @@
 package lotto.model.validation
 
 class InputValidation {
-    fun validateLottoPurchaseAmount(amount: Int) {
-        isLottoPurchaseAmountDivisibleByThousand(amount)
-        isLottoPurchaseAmountNotZero(amount)
-        isLottoPurchaseAmountNotPositive(amount)
+    fun validateLottoPurchaseAmount(amount: String): Int {
+        var lottoAmountNum = parseToInt(amount)
+        isLottoPurchaseAmountDivisibleByThousand(lottoAmountNum)
+        isLottoPurchaseAmountNotZero(lottoAmountNum)
+        isLottoPurchaseAmountNotPositive(lottoAmountNum)
+
+        return lottoAmountNum
+    }
+
+    private fun parseToInt(input: String): Int {
+        return try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(IS_NOT_NUMBER_ERROR)
+        }
     }
 
     private fun isLottoPurchaseAmountDivisibleByThousand(amount: Int) {
@@ -36,10 +47,10 @@ class InputValidation {
 
     companion object {
         private const val DIVISION_NUM = 1000
+        const val IS_NOT_NUMBER_ERROR = "[ERROR] 숫자를 입력해주세요."
         const val DIVISION_BY_THOUSAND_ERROR = "[ERROR] 로또는 ${DIVISION_NUM}원 단위로 구매 가능합니다."
         const val INPUT_ZERO_AMOUNT_ERROR = "[ERROR] 로또 구매 금액으로 0을 입력할 수 없습니다."
         const val INPUT_NEGATIVE_AMOUNT_ERROR = "[ERROR] 로또 구매 금액으로 음수를 입력할 수 없습니다."
-
         const val SEPARATE_BY_COMMA_ERROR = "로또 당첨 숫자는 쉼표(,)로 구분되어야 합니다."
     }
 }
