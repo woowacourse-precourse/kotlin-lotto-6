4e6f88a85a4bfb2798a43f02ff745dbb483fe183
w36495 <w36495@naver.com>
2023-11-08 17:32:49 +0900

    feat : 프로그램의 입/출력을 담당하는 View 구현

--- src/main/kotlin/lotto/ui/LottoView.kt
+++ src/main/kotlin/lotto/ui/LottoView.kt
@@ -1,33 +1,79 @@
 package lotto.ui
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.validation.InputValidator
+import lotto.stringToIntList
 
 class LottoView {
-    fun askPurchaseTickets(): String {
-        println("구입금액을 입력해 주세요.")
-        val price = Console.readLine()
 
-        return price
+    private lateinit var luckyNumbers: List<Int>
+
+    fun getTicketPrice(): String {
+        var inputPrice = ""
+        var isValid = false
+
+        while (!isValid) {
+            try {
+                inputPrice = askPurchaseTickets()
+                validTicketPrice(inputPrice)
+
+                isValid = true
+            } catch (exception: IllegalArgumentException) {
+                println(exception.message)
+            } catch (exception: NumberFormatException) {
+                println(exception.message)
+            }
+        }
+
+        return inputPrice
+    }
+
+    fun getLuckyNumbers(): List<Int> {
+        var inputLuckyNumbers = ""
+        var isValid = false
+
+        while (!isValid) {
+            try {
+                inputLuckyNumbers = askLuckyNumbers()
+                validLuckyNumbers(inputLuckyNumbers)
+
+                isValid = true
+            } catch (exception: IllegalArgumentException) {
+                println(exception.message)
+            } catch (exception: NumberFormatException) {
+                println(exception.message)
+            }
+        }
+
+        this.luckyNumbers = inputLuckyNumbers.stringToIntList()
+
+        return inputLuckyNumbers.stringToIntList()
+    }
+
+    fun getBonusNumber(): Int {
+        var inputBonusNumber = ""
+        var isValid = false
+
+        while (!isValid) {
+            try {
+                inputBonusNumber = askBonusNumber()
+                validBonusNumber(inputBonusNumber)
+
+                isValid = true
+            } catch (exception: IllegalArgumentException) {
+                println(exception.message)
+            } catch (exception: NumberFormatException) {
+                println(exception.message)
+            }
+        }
+
+        return inputBonusNumber.toInt()
     }
 
     fun calculateTicketCount(price: Int): Int = price / PRICE_PER_TICKET
 
     fun showPurchasedTicketCount(ticketCount: Int) {
         printWithNewLine("${ticketCount}개를 구매했습니다.")
-    }
-
-    fun askLuckyNumbers(): List<Int> {
-        printWithNewLine("당첨 번호를 입력해 주세요.")
-        val luckyNumbers = Console.readLine()
-
-        return luckyNumbers.split(DELIMITER_COMMA).map { it.toInt() }.toList()
-    }
-
-    fun askBonusNumber(): Int {
-        printWithNewLine("보너스 번호를 입력해 주세요.")
-        val bonusNumber = Console.readLine()
-
-        return bonusNumber.toInt()
     }
 
     fun showWinResult(ranks: List<Int>) {
@@ -46,7 +92,9 @@ class LottoView {
     fun showRateOfProfit(rateOfProfit: Double) {
         print("총 수익률은 ${rateOfProfit}%입니다.")
     }
-    
+
+    fun closeConsole() = Console.close()
+
     private fun printWithNewLine(output: String) {
         println(
             """
@@ -56,16 +104,71 @@ class LottoView {
         )
     }
 
+    private fun askPurchaseTickets(): String {
+        println("구입금액을 입력해 주세요.")
+        val inputPrice = getInputDataFromPlayer()
+
+        return inputPrice
+    }
+
+    private fun askLuckyNumbers(): String {
+        printWithNewLine("당첨 번호를 입력해 주세요.")
+        val luckyNumbers = getInputDataFromPlayer()
+
+        return luckyNumbers
+    }
+
+    private fun askBonusNumber(): String {
+        printWithNewLine("보너스 번호를 입력해 주세요.")
+        val bonusNumber = getInputDataFromPlayer()
+
+        return bonusNumber
+    }
+
+    private fun validTicketPrice(price: String) {
+        with(InputValidator) {
+            checkInputBlank(price)
+            checkInputEmpty(price)
+            checkInputTypeAsInt(price)
+            checkTicketPrice(price.toInt())
+            checkMaxPrice(price.toInt())
+        }
+    }
+
+    private fun validLuckyNumbers(numbers: String) {
+        with(InputValidator) {
+            checkInputBlank(numbers)
+            checkInputEmpty(numbers)
+            checkCommaBetweenNumbers(numbers)
+            checkLottoNumberPrefix(numbers)
+            checkLottoNumberPostfix(numbers)
+            checkEmptyBetweenNumbers(numbers)
+            checkLimitLottoNumber(numbers.split(DELIMITER_COMMA).map { it.toInt() })
+        }
+    }
+
+    private fun validBonusNumber(number: String) {
+        with(InputValidator) {
+            checkInputBlank(number)
+            checkInputEmpty(number)
+            checkInputTypeAsInt(number)
+            checkLimitBonusNumber(number.toInt())
+            checkDuplicateBetweenBonusAndLuckyNumbers(number.toInt(), luckyNumbers)
+        }
+    }
+
+    private fun getInputDataFromPlayer(): String = Console.readLine()
+
     companion object {
-        const val DELIMITER_COMMA: String = ","
-        const val DIVIDER: String = "---"
+        private const val DELIMITER_COMMA: String = ","
+        private const val DIVIDER: String = "---"
 
-        const val PRICE_PER_TICKET: Int = 1_000
+        private const val PRICE_PER_TICKET: Int = 1_000
 
-        const val RANK_1ST: Int = 1
-        const val RANK_2ND: Int = 2
-        const val RANK_3RD: Int = 3
-        const val RANK_4TH: Int = 4
-        const val RANK_5TH: Int = 5
+        private const val RANK_1ST: Int = 1
+        private const val RANK_2ND: Int = 2
+        private const val RANK_3RD: Int = 3
+        private const val RANK_4TH: Int = 4
+        private const val RANK_5TH: Int = 5
     }
 }
