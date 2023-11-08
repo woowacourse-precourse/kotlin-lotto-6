ebc05c2b418ed47b543488af293c002c3d53b84f
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-08 10:16:54 +0900

    [Feat] 사용자 입력 오류 시 재시도 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,7 +5,18 @@ fun main() {
 
     val player = Player()
     output.printReceivePurchaseAmountInput()
-    val amount = player.inputPurchaseAmount()
+
+    var amount: Int
+    while (true) {
+        try {
+            amount = player.inputPurchaseAmount()
+            break
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+
+        }
+    }
+
     val count = player.calculateLottoGenerateCount(amount)
     output.printLottoGenerateCount(count)
 
@@ -15,9 +26,28 @@ fun main() {
 
     val lottoMachine = LottoMachine()
     output.printReceiveWinningNumbersInput()
-    val winningNumbers = lottoMachine.inputWinningNumbers()
+    var winningNumbers: List<Int>
+    while (true) {
+        try {
+            winningNumbers = lottoMachine.inputWinningNumbers()
+            break
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+
+        }
+    }
     output.printReceiveBonusNumberInput()
-    val bonusNumber = lottoMachine.inputBonusNumber(winningNumbers)
+
+    var bonusNumber: Int
+    while (true) {
+        try {
+            bonusNumber = lottoMachine.inputBonusNumber(winningNumbers)
+            break
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+
+        }
+    }
 
     val winningResult = WinningResult(lottoTickets, winningNumbers, bonusNumber)
     output.printWinningStatistics()
