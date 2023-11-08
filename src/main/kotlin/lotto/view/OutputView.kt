e821a452efde2d71ff47509e78fe31a01f2bd92d
sh1mj1 <7wlgns@gmail.com>
2023-11-08 19:55:30 +0900

    style($project): remove blanks in the string where appropriate and add a new line

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -6,17 +6,17 @@ import lotto.model.WinningRank
 class OutputView {
     fun purchasePrompt() = println(PURCHASE_PROMPT)
 
-    fun lottoNumbersPrompt(count: Int) = println(count.toString() + LOTTO_COUNT_PROMPT)
+    fun lottoNumbersPrompt(count: Int) = println("\n$count$LOTTO_COUNT_PROMPT")
 
-    fun inputLottoNumbersPrompt() = println(INPUT_LOTTO_NUMBERS_PROMPT)
+    fun inputLottoNumbersPrompt() = println("\n$INPUT_LOTTO_NUMBERS_PROMPT")
 
     fun lottoNumbers(lottoes: List<Lotto>) = lottoes.forEach {
         println(it.toString())
     }
 
-    fun inputBonusNumberPrompt() = println(INPUT_BONUS_NUMBER_PROMPT)
+    fun inputBonusNumberPrompt() = println("\n$INPUT_BONUS_NUMBER_PROMPT")
 
-    fun winningResultPrompt() = println(WINNING_RESULT_PROMPT)
+    fun winningResultPrompt() = print(WINNING_RESULT_PROMPT)
 
     fun winningResult(lottoesResult: Map<WinningRank, Int>) {
         println(
@@ -31,7 +31,7 @@ class OutputView {
     }
 
     fun totalProfitRate(profitRate: Double) =
-        println("$PROFIT_RATE_DESCRIPTION $profitRate$PERCENTAGE.")
+        println("$PROFIT_RATE_DESCRIPTION $profitRate$PERCENTAGE")
 
     companion object {
         const val PURCHASE_PROMPT = "구입 금액을 입력해주세요."
@@ -41,10 +41,10 @@ class OutputView {
         const val WINNING_RESULT_PROMPT = """
 당첨 통계
 ---
-        """
+"""
         const val HYPHEN = " - "
         const val COUNT_DESCRIPTION = "개"
-        const val PROFIT_RATE_DESCRIPTION = "총 수익률은 "
+        const val PROFIT_RATE_DESCRIPTION = "총 수익률은"
         const val PERCENTAGE = "%입니다."
     }
 
