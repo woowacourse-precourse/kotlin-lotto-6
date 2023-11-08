c414c49849796a82868b2c3b275d61e52e4b0e0d
bak <bak4017@gmail.com>
2023-11-08 17:02:20 +0900

    style(lotto): 함수간의 순서 변경

--- src/main/kotlin/lotto/validator/InputValidator.kt
+++ src/main/kotlin/lotto/validator/InputValidator.kt
@@ -5,12 +5,12 @@ import lotto.constant.Constants.MAX_ATTEMPTS
 import lotto.constant.Constants.START_INCLUSIVE
 
 open class InputValidator {
-    fun checkForPositiveInteger(value: String) {
-        require(value.matches("^[0-9]\\d*$".toRegex())) { "[ERROR] 양의 정수 이외의 숫자를 입력하지 말아주세요" }
-    }
-
     fun checkBlank(value: String) {
         require(value.isNotBlank()) { "[ERROR] 공백을 제외한 값을 입력해 주세요" }
+    }
+
+    fun checkForPositiveInteger(value: String) {
+        require(value.matches("^[0-9]\\d*$".toRegex())) { "[ERROR] 양의 정수 이외의 숫자를 입력하지 말아주세요" }
     }
 
     fun checkForNumberRange(value: String) {
