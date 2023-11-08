6befb9ce96c58b469f42f64a949a8ae9913eed5b
MinJun <agvber12@gmail.com>
2023-11-08 16:40:55 +0900

    feat: 로또 출력 숫자를 계산합니다

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,12 +3,15 @@ package lotto
 import camp.nextstep.edu.missionutils.Console.readLine
 
 private var lottoPurchaseAmount: Int = -1
+private var lottoCount: Int = -1
 
 fun main() {
 
     println("구입금액을 입력해 주세요.")
     inputLottoPurchaseAmount()
     checkLottoPurchaseAmount()
+
+    calculatorLottoCount()
 }
 
 private fun inputLottoPurchaseAmount() {
@@ -23,4 +26,8 @@ private fun checkLottoPurchaseAmount() {
     if (lottoPurchaseAmount.rem(1000) != 0) {
         throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위만 입력 받습니다.")
     }
+}
+
+private fun calculatorLottoCount() {
+    lottoCount = lottoPurchaseAmount / 1000
 }
