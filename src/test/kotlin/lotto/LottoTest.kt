77442e30396f18dc2072dbc281fd53c67f141b75
이아름 <souk0712@naver.com>
2023-11-08 22:26:25 +0900

    test: LottoTest.kt 추가
    - Lotto class 객체 테스트 코드 작성
    - 로또 번호 범위 검사
    - 로또 번호 맞추기

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import lotto.model.Lotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,5 +22,26 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호가 1~45 사이의 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 46, 77, 88, 99, -1))
+        }
+    }
+
+    @Test
+    fun `로또 번호와 당첨 번호를 맞춰서 개수를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        assertThat(lotto.getMatchCount(winningNumbers)).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호와 번호 하나를 맞춘 여부를 반환한다`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val matchNumber = 3
+        val missNumber = 10
+        assertThat(lotto.isMatchNumber(matchNumber)).isTrue()
+        assertThat(lotto.isMatchNumber(missNumber)).isFalse()
+    }
 }
