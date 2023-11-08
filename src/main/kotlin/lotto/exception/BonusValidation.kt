79800b42decf8b066000dbeca6b8ce835e667bf0
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-11-08 18:51:10 +0900

    [FEAT] 입력 받을 보너스 번호의 예외처리 추가

---
+++ src/main/kotlin/lotto/exception/BonusValidation.kt
@@ -0,0 +1,30 @@
+package lotto.exception
+
+class BonusValidation(private val lotto: List<Int>, private val bonus: String) {
+
+    init {
+        validateBonusRange()
+        validateBonusNumber()
+        validateBonusDuplicate()
+    }
+
+    private fun validateBonusRange() {
+        require(bonus.toInt() in 1..45) {
+            "[ERROR] 1~45 사이의 숫자만 가능합니다."
+        }
+    }
+
+    private fun validateBonusNumber() {
+        require(bonus.matches(Regex("[0-9]"))) {
+            "[ERROR] 숫자만 입력해주세요."
+        }
+    }
+
+    private fun validateBonusDuplicate() {
+        repeat(lotto.size) {
+            require(lotto[it] != bonus.toInt()) {
+                "[ERROR] 로또 입력값과 보너스 번호의 값이 중복됩니다. 다른 값을 입력해주세요."
+            }
+        }
+    }
+}
