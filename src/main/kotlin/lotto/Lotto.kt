e88c95456667b18b83b2314e2e6ee84a8881ec8f
nhkim <nhkim1529@gmail.com>
2023-11-08 22:44:54 +0900

    Feat: 로또 번호를 생성하고, 유효성을 검증하는 Lotto 클래스 생성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,29 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "Numbers must contain exactly 6 integers." }
+        require(numbers.all { it in 1..45 }) {
+            "All numbers must be between 1 and 45."
+        }
     }
 
-    // TODO: 추가 기능 구현
+    fun getNumbers(): List<Int> = numbers
+
+    fun checkMatch(winningNumbers: List<Int>): Int {
+        return numbers.intersect(winningNumbers.toSet()).size
+    }
+
+    fun hasBonus(bonusNumber: Int): Boolean {
+        return numbers.contains(bonusNumber)
+    }
+
+    override fun toString(): String {
+        return numbers.joinToString(prefix = "[", postfix = "]")
+    }
+
+    companion object {
+        fun generateNumbers(): List<Int> {
+            return (1..45).shuffled().take(6).sorted()
+        }
+    }
 }
