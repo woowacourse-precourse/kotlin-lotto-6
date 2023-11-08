6d68ea7250ce399a9b08ca185d05818f0cc87fcf
yeji0214 <1225yj50@naver.com>
2023-11-08 15:56:09 +0900

    PurchaseAmountTest.kt 수정

--- src/test/kotlin/lotto/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/PurchaseAmountTest.kt
@@ -5,38 +5,39 @@ import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class PurchaseAmountTest {
+    private val purchaseAmount = PurchaseAmount()
     @Test
     fun `구입금액이 1000보다 작으면 예외가 발생한다(음수인 경우)`() {
         assertThrows<IllegalArgumentException> {
-            PurchaseAmount().validateLottoNumbers(-5)
+            purchaseAmount.validateLottoNumbers(-5)
         }
     }
 
     @Test
     fun `구입금액이 1000보다 작으면 예외가 발생한다(양수인 경우)`() {
         assertThrows<IllegalArgumentException> {
-            PurchaseAmount().validateLottoNumbers(100)
+            purchaseAmount.validateLottoNumbers(100)
         }
     }
 
     @Test
     fun `구입금액이 1000으로 나누어 떨어지지 않으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            PurchaseAmount().validateLottoNumbers(1234)
+            purchaseAmount.validateLottoNumbers(1234)
         }
     }
 
     @Test
     fun `구입금액이 숫자가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            PurchaseAmount().validateLottoNumbers("invalid".toInt())
+            purchaseAmount.validateLottoNumbers("invalid".toInt())
         }
     }
 
     @Test
     fun `구입금액 정상 입력`() {
         assertDoesNotThrow {
-            PurchaseAmount().validateLottoNumbers(3000)
+            purchaseAmount.validateLottoNumbers(3000)
         }
     }
 }
