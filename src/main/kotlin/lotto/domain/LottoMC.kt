cc47580a9d3a90917a73f135962ae57738ff18ae
m6z1 <sonmyungj1zz@gmail.com>
2023-11-08 12:02:15 +0900

    refactor(LottoMC): 하드코딩 값을 상수화로 수정

--- src/main/kotlin/lotto/domain/LottoMC.kt
+++ src/main/kotlin/lotto/domain/LottoMC.kt
@@ -20,20 +20,22 @@ class LottoMC {
         require(numbers.isNotEmpty()) { ERROR_LOTTO_NUM_EMPTY }
         require(numbers.size == LOTTO_SIZE) { ERROR_LOTTO_NUM_SIZE }
         for (num in numbers) {
-            require(num in 1..45) { ERROR_LOTTO_NUM_RANGE }
+            require(num in LOTTO_NUM_MIN..LOTTO_NUM_MAX) { ERROR_LOTTO_NUM_RANGE }
         }
         require(numbers.size == numbers.distinct().count()) { ERROR_LOTTO_NUM_DUPLICATE }
     }
 
     private fun validateBonusNumber(bonusNum: String, lottoNum: List<Int>) {
         require(bonusNum.isNotBlank()) { ERROR_BONUS_NUM_EMPTY }
-        require(bonusNum.toInt() in 1..45) { ERROR_BONUS_NUM_RANGE }
+        require(bonusNum.toInt() in LOTTO_NUM_MIN..LOTTO_NUM_MAX) { ERROR_BONUS_NUM_RANGE }
         require(bonusNum.toInt() !in lottoNum) { ERROR_BONUS_NUM_DUPLICATE }
     }
 
     companion object {
         const val LOTTO_NUM_DIVIDING = ","
         const val LOTTO_SIZE = 6
+        const val LOTTO_NUM_MIN = 1
+        const val LOTTO_NUM_MAX = 45
         const val ERROR_LOTTO_NUM_EMPTY = "[ERROR] 로또 당첨 번호 6개를 필수로 입력해야 합니다."
         const val ERROR_LOTTO_NUM_SIZE = "[ERROR] 로또 번호는 6개로만 입력해야 합니다."
         const val ERROR_LOTTO_NUM_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
