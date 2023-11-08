464722afa744c0b821edaa0ffc959a95faa055b3
gaeun <geun5744@gmail.com>
2023-11-08 22:02:07 +0900

    [REFACTOR(ValidationTest)] @Test -> @ParameterizedTest로 리팩토링

--- src/test/kotlin/lotto/model/ValidationTest.kt
+++ src/test/kotlin/lotto/model/ValidationTest.kt
@@ -2,97 +2,35 @@ package lotto.model
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import java.lang.IllegalArgumentException
 
 class ValidationTest {
 
-    @Test
-    @DisplayName("구입 금액 입력 테스트")
-    fun `숫자가 아닌 문자를 입력한 경우 예외 발생`() {
-        val input = "횟수"
+    @ParameterizedTest
+    @ValueSource(strings = ["횟수", "1200"])
+    fun `로또 구입 금액 입력 테스트`(input: String) {
         assertThrows<IllegalArgumentException> {
             Validation.getPurchaseAmount(input)
         }
     }
 
-    @Test
-    @DisplayName("구입 금액 입력 테스트")
-    fun `구입 단위가 1000원이 아닌 경우 예외 발생`() {
-        val input = "1200"
-        assertThrows<IllegalArgumentException> {
-            Validation.getPurchaseAmount(input)
-        }
-    }
-
-    @Test
-    @DisplayName("당첨 번호 입력 테스트")
-    fun `숫자가 아닌 문자를 입력할 경우 예외 발생`() {
-        val input = "횟수"
+    @ParameterizedTest
+    @ValueSource(strings = ["횟수","1 2 3","1,2,3,4,5","1,2,3,4,5,100","1,2,3,4,5,5"])
+    fun `당첨 번호 입력 테스트`(input: String) {
         assertThrows<IllegalArgumentException> {
             Validation.getAnswerNumber(input)
         }
     }
 
-    @Test
-    @DisplayName("당첨 번호 입력 테스트")
-    fun `쉼표가 없는 경우 예외 발생`() {
-        val input = "1 2 3"
-        assertThrows<IllegalArgumentException> {
-            Validation.getAnswerNumber(input)
-        }
-    }
-
-    @Test
-    @DisplayName("당첨 번호 입력 테스트")
-    fun `6개의 숫자를 입력하지 않은 경우 예외 발생`() {
-        val input = "1,2,3,4,5"
-        assertThrows<IllegalArgumentException> {
-            Validation.getAnswerNumber(input)
-        }
-    }
-
-    @Test
-    @DisplayName("당첨 번호 입력 테스트")
-    fun `1~45 사이의 숫자가 아닌 경우 예외 발생`() {
-        val input = "1,2,3,4,5,100"
-        assertThrows<IllegalArgumentException> {
-            Validation.getAnswerNumber(input)
-        }
-    }
-
-    @Test
-    @DisplayName("당첨 번호 입력 테스트")
-    fun `같은 숫자를 입력한 경우 예외 발생`() {
-        val input = "1,2,3,4,5,5"
-        assertThrows<IllegalArgumentException> {
-            Validation.getAnswerNumber(input)
-        }
-    }
-
-    @Test
-    @DisplayName("보너스 번호 입력 테스트")
-    fun `숫자가 아닌 경우 예외 발생`() {
-        val input = "횟수"
-        assertThrows<IllegalArgumentException> {
-            Validation.getBonusNum(input)
-        }
-    }
-
-    @Test
-    @DisplayName("보너스 번호 입력 테스트")
-    fun `1 ~ 45 사이의 숫자가 아닌 경우 예외 발생`() {
+    @ParameterizedTest
+    @ValueSource(strings = ["횟수","100","1,2"])
+    fun `보너스 번호 입력 테스트`(input:String) {
         val input = "1,2"
-        assertThrows<IllegalArgumentException> {
-            Validation.getBonusNum(input)
-        }
-    }
-
-    @Test
-    @DisplayName("보너스 번호 입력 테스트")
-    fun `여러 숫자를 입력한 경우 예외 발생`() {
-        val input = "1,2,3,4,5,5"
         assertThrows<IllegalArgumentException> {
             Validation.getBonusNum(input)
         }
