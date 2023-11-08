5645ca3f043f6dc9e5a2c80154e656c296d77bc2
Wonhui Roh <rwhui012@gmail.com>
2023-11-08 21:10:14 +0900

    fix(lotto): count의 접근 제어자 변경 (private -> public)

--- src/main/kotlin/lotto/domain/LottoRank.kt
+++ src/main/kotlin/lotto/domain/LottoRank.kt
@@ -11,7 +11,7 @@ import lotto.constants.Lotto.SECOND_REWARD
 import lotto.constants.Lotto.THIRD_MESSAGE
 import lotto.constants.Lotto.THIRD_REWARD
 
-enum class LottoRank(val message: String, val reward: Int, private var count: Int = 0) {
+enum class LottoRank(val message: String, val reward: Int, var count: Int = 0) {
   FIRST(FIRST_MESSAGE, FIRST_REWARD),
   SECOND(SECOND_MESSAGE, SECOND_REWARD),
   THIRD(THIRD_MESSAGE, THIRD_REWARD),
