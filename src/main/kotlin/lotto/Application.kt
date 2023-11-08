2ded268cd04b234d8112951ed350e87293987a42
moyeongmin <qa98589858@gmail.com>
2023-11-08 17:34:26 +0900

    당첨 번호를 입력 받는 `lotto_input` 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,9 +5,12 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val ammount = lotto_pay()
     Lottogenerator(ammount)
+    val winningnum = lotto_input()
+    print(winningnum)
 
 }
-fun lotto_pay() : Int {
+fun lotto_pay() : Int//로또 구입 금액을 받아 처리하는 `lotto_pay`
+{
 
     while (true) {
         try {
@@ -22,7 +25,24 @@ fun lotto_pay() : Int {
         }
     }
 }
-fun Lottogenerator(ammount : Int) :List<Lotto>
+fun lotto_input() : List<Int>//당첨 번호를 입력 받는 `lotto_input`
+{
+    while (true) {
+        try {
+            println("당첨 번호를 입력해 주세요.")
+            val input = Console.readLine()?.split(',')?.map { it.trim().toInt() } ?: throw IllegalArgumentException("[ERROR] 로또 번호를 입력해 주세요.")
+            if (input.size != 6 || input.any { it < 1 || it > 45 }) {
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            }
+            return input
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+
+}
+
+fun Lottogenerator(ammount : Int) :List<Lotto>//사용자 입력 횟수만큼 로또 번호를 생성해주는 `Lottogenerator`
 {
     val lottoammount = ammount / 1000
     val lottos = mutableListOf<Lotto>()
@@ -34,7 +54,7 @@ fun Lottogenerator(ammount : Int) :List<Lotto>
 
     return lottos
 }
-fun lotto_numbers_print( Lottos : List<Lotto>)
+fun lotto_numbers_print( Lottos : List<Lotto>)//로또 번호 생성 결과를 출력해주는 `lotto_numbers_print`
 {
     println("${Lottos.size}개를 구매했습니다.")
     for(lotto in Lottos)
@@ -43,4 +63,5 @@ fun lotto_numbers_print( Lottos : List<Lotto>)
         println(numbers)
     }
 }
+
 
