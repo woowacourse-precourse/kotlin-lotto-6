38ec8e2586d8f11800957e1face2fde57785d60b
stopkite <stopkite98@gmail.com>
2023-11-08 09:19:14 +0900

    test(LottoTest): 범위 외의 숫자 입력 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,10 @@
 package lotto
 
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 
 class LottoTest {
@@ -21,4 +24,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호에 1과 45 범위 외의 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 48))
+        }
+    }
 }
