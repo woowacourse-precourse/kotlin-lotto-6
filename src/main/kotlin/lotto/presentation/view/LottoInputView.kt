5555f6678b22f90bf329b94f0506704eb8b2402b
윤동현 <donghyunyoon81@gmail.com>
2023-11-08 18:58:42 +0900

    feat: 로또 게임에서 값을 입력 받는 기능 추가

--- src/main/kotlin/lotto/presentation/view/LottoInputView.kt
+++ src/main/kotlin/lotto/presentation/view/LottoInputView.kt
@@ -1,5 +1,13 @@
 package lotto.presentation.view
 
+import camp.nextstep.edu.missionutils.Console
+
 class LottoInputView {
+
+    fun getPrice() = Console.readLine()
+
+    fun getWinningNumber() = Console.readLine()
+
+    fun getBonusNumber() = Console.readLine()
 
 }
