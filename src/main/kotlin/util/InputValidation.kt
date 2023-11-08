404e5f942f400c70cefabecf611a587cf6105d83
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-11-08 22:57:43 +0900

    refactor: 로또 관련 설정 값 상수 처리

--- src/main/kotlin/util/InputValidation.kt
+++ src/main/kotlin/util/InputValidation.kt
@@ -1,5 +1,7 @@
 package util
 
+import java.text.DecimalFormat
+
 enum class InputValidation(val message: String, val isValid: (String) -> Unit) {
     ONLY_NUMBER(
         "[ERROR] 숫자만 입력이 가능합니다.",
@@ -14,17 +16,18 @@ enum class InputValidation(val message: String, val isValid: (String) -> Unit) {
         fun(value: String) = require(value.first() != '0') { NUMBER_FORMAT.message },
     ),
     VALID_RANGE(
-        "[ERROR] 최소 1,000원 부터 최대 100,000원 까지 구매가 가능합니다.",
+        "[ERROR] 최소 ${DecimalFormat("#,###").format(LottoConfiguration.MIN_PURCHASE_PRICE.value)}원 부터 " +
+            "최대 ${DecimalFormat("#,###").format(LottoConfiguration.MAX_PURCHASE_PRICE.value)}원 까지 구매가 가능합니다.",
         fun(value: String) =
             require(value.toIntOrNull() in LottoConfiguration.MIN_PURCHASE_PRICE.value..LottoConfiguration.MAX_PURCHASE_PRICE.value) { VALID_RANGE.message },
     ),
     PURCHASE_IN_THOUSAND_WON(
-        "[ERROR] 1,000원 단위로 구매가 가능합니다.",
+        "[ERROR] ${DecimalFormat("#,###").format(LottoConfiguration.TICKET_PRICE.value)}원 단위로 구매가 가능합니다.",
         fun(value: String) =
             require(value.toInt() % LottoConfiguration.TICKET_PRICE.value == 0) { PURCHASE_IN_THOUSAND_WON.message },
     ),
     NUMBER_RANGE(
-        "[ERROR] 1 부터 45까지만 입력이 가능합니다.",
+        "[ERROR] ${LottoConfiguration.MIN_NUMBER.value}부터 ${LottoConfiguration.MAX_NUMBER.value}까지만 입력이 가능합니다.",
         fun(value: String) =
             require(value.toIntOrNull() in LottoConfiguration.MIN_NUMBER.value..LottoConfiguration.MAX_NUMBER.value) { NUMBER_RANGE.message },
     ),
--- src/main/kotlin/util/LottoValidation.kt
+++ src/main/kotlin/util/LottoValidation.kt
@@ -2,7 +2,7 @@ package util
 
 enum class LottoValidation(val message: String, val isValid: (List<Int>) -> Unit) {
     REQUIRED_LOTTO_NUMBER_COUNT(
-        "[ERROR] 6개의 숫자만 당첨번호로 지정할 수 있습니다.",
+        "[ERROR] 당첨번호는 ${LottoConfiguration.NUMBER_COUNT.value}개의 숫자로만 지정할 수 있습니다.",
         fun(numbers: List<Int>) =
             require(numbers.size == LottoConfiguration.NUMBER_COUNT.value) { REQUIRED_LOTTO_NUMBER_COUNT.message },
     ),
@@ -11,7 +11,7 @@ enum class LottoValidation(val message: String, val isValid: (List<Int>) -> Unit
         fun(numbers: List<Int>) = require(numbers.toSet().size == numbers.size) { UNIQUE_LOTTO_NUMBERS.message },
     ),
     LOTTO_NUMBER_RANGE(
-        "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+        "[ERROR] 로또 번호는 ${LottoConfiguration.MIN_NUMBER.value}부터 ${LottoConfiguration.MAX_NUMBER.value}사이의 숫자여야 합니다.",
         fun(numbers: List<Int>) =
             require(numbers.all { it in LottoConfiguration.MIN_NUMBER.value..LottoConfiguration.MAX_NUMBER.value }) { LOTTO_NUMBER_RANGE.message },
     ),
