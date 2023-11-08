5d4f997b7b58b852b3437849d5dd8e532a92de9c
Seannnn55 <sally99h@gmail.com>
2023-11-08 18:11:03 +0900

    refactor: [Lotto] 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -12,13 +12,27 @@ class Lotto(private val numbers: List<Int>) {
         return lottoNums.sorted()
     }
 
-    fun checkLotto(userNumbers: List<Int>): Int {
-        var sameNums: Int = 0
+    fun checkLotto(userNumbers: List<Int>,bonusNum:Int): MutableList<Int> {
+        var sameNums: MutableList<Int> = mutableListOf(0,0)
         for (i in userNumbers) {
             if (i in numbers) {
-                sameNums += 1
+                sameNums[0] += 1
+            }
+            if (i == bonusNum){
+                sameNums[1] += 1
             }
         }
         return sameNums
     }
+
+    fun checkMoney(num: MutableList<Int>): Int {
+        var money: Int
+        if (num[0] == 3) return 5000
+        if (num[0] == 4) return 50000
+        if (num[0] == 5) return 1500000
+        if (num[0] == 5 && num[1] == 1) return 30000000
+        if (num[0] == 6) return 2000000000
+        return 0
+    }
+
 }
