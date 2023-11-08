a34de2128b69778a694bb40c85e3ca7ef7c0bb60
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-11-08 18:08:01 +0900

    [Feat] #05 - check Lotto numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,10 +8,41 @@ import kotlin.contracts.contract
 fun main() {
     val lottos = inputAndBuyLottos()
     printLottoNum(lottos)
-    val winnerNum = inputWinnerNum()
-    println("당첨 번호: $winnerNum")
+    val winningNum = inputWinnerNum()
+    val bonusNum = inputBonusNum(winningNum)
+    println("당첨 번호: $winningNum")
+
+    lottos.forEach { lotto ->
+        val lottoRank = checkLotto(lotto, winningNum, bonusNum)
+        println("당신의 로또 번호는 ${lottoRank.name}이며, 상금은 ${lottoRank.prize}원 입니다.")
+    }
 }
 
+enum class LottoRank(val matchNum: Int, val prize: Long) {
+    FIRST(6,2_000_000_000),
+    SECOND(5, 30_000_000), // bonus number match
+    THIRD(5, 15_000_000), // without bonus number
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0,0);
+}
+
+fun findLottoRankByMatchCount(matchCount: Int, matchBonus: Boolean): LottoRank {
+    return when {
+        matchCount == 6 -> LottoRank.FIRST
+        matchCount == 5 && matchBonus -> LottoRank.SECOND
+        matchCount == 5 -> LottoRank.THIRD
+        matchCount == 4 -> LottoRank.FOURTH
+        matchCount == 3 -> LottoRank.FIFTH
+        else -> LottoRank.NONE
+    }
+}
+
+fun checkLotto(lotto: Lotto, winningNumbers: List<Int>, bonus:Int) : LottoRank {
+    val matchCount = lotto.getMatchCount(winningNumbers)
+    val matchBonus = lotto.contains(bonus)
+    return findLottoRankByMatchCount(matchCount, matchBonus)
+}
 fun inputAndBuyLottos(): MutableList<Lotto> {
     while (true) {
         try {
@@ -36,9 +67,19 @@ fun inputWinnerNum(): List<Int> {
     }
 }
 
+fun inputBonusNum(winningNumbers: List<Int>): Int {
+    while (true) {
+        try {
+            return bonusNumInput(winningNumbers)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
 fun winnerNumInput(): List<Int> {
     println("당첨 번호를 입력해 주세요.")
-    var input = userInput()
+    val input = userInput()
     val numberList = input.split(",").map { it.trim()}
 
     if (numberList.size != 6) {
@@ -53,19 +94,24 @@ fun winnerNumInput(): List<Int> {
         throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
     }
 
+    return numbers
+}
+
+fun bonusNumInput(winningNumbers: List<Int>): Int {
     println("보너스 번호를 입력해 주세요")
-    input = userInput()
+    val input = userInput()
     val bonus = ensureInt(input)
     if (bonus !in 1..45 ) {
         throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
     }
 
-    if (numbers.contains(bonus)) {
+    if (winningNumbers.contains(bonus)) {
         throw IllegalArgumentException("[ERROR] 당첨 번호와 중복될 수 없습니다.")
     }
 
-    return numbers + bonus
+    return bonus
 }
+
 fun printLottoNum(lottos: MutableList<Lotto>) {
     lottos.forEach { lotto ->
         println(lotto)
