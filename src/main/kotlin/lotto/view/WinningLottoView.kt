0a1f42928a34af5b9dc0a60a9de2633dc639d92e
Wonhui Roh <rwhui012@gmail.com>
2023-11-08 13:41:52 +0900

    feat(lotto): 당첨 번호 입력 요청 메시지 출력 기능

--- src/main/kotlin/lotto/view/WinningLottoView.kt
+++ src/main/kotlin/lotto/view/WinningLottoView.kt
@@ -1,8 +1,10 @@
 package lotto.view
 
+import lotto.constants.GuideMessage.ENTER_WINNING_NUMBERS
+
 class WinningLottoView {
   fun printWinningNumbersInputMessage() {
-
+    println(ENTER_WINNING_NUMBERS)
   }
 
   fun inputWinningNumbers() {
