07fd40e16076fc9d87e222558df17e26cea8da78
m6z1 <sonmyungj1zz@gmail.com>
2023-11-08 12:38:56 +0900

    test(LottoMCTest): 로또 번호 및 보너스 번호 입력의 예외처리 테스트 추가

--- src/test/kotlin/lotto/LottoMCTest.kt
+++ src/test/kotlin/lotto/LottoMCTest.kt
@@ -28,6 +28,13 @@ class LottoMCTest {
     }
 
     @Test
+    fun `로또 번호가 공백일 때의 예외처리`() {
+        assertThrows<IllegalArgumentException> {
+            LottoMC().pickLottoNum("")
+        }
+    }
+
+    @Test
     fun `로또 보너스 번호 범위가 1~45 사이가 아닐 때의 예외처리`() {
         assertThrows<IllegalArgumentException> {
             LottoMC().pickBonusNum("46", listOf(1, 2, 3, 4, 5, 6))
@@ -45,6 +52,20 @@ class LottoMCTest {
     fun `로또 보너스 번호가 공백일 때의 예외처리`() {
         assertThrows<IllegalArgumentException> {
             LottoMC().pickBonusNum("", listOf(1, 2, 3, 4, 5, 6))
+        }
+    }
+
+    @Test
+    fun `로또 보너스 번호가 비어있을 때의 예외처리`() {
+        assertThrows<IllegalArgumentException> {
+            LottoMC().pickBonusNum(" ", listOf(1, 2, 3, 4, 5, 6))
+        }
+    }
+
+    @Test
+    fun `로또 보너스 번호를 여러 개 입력했을 때의 예외처리`() {
+        assertThrows<IllegalArgumentException> {
+            LottoMC().pickBonusNum("22,34", listOf(1, 2, 3, 4, 5, 6))
         }
     }
 }
