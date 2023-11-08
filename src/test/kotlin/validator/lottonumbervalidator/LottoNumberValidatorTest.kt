f253fff193299f7a1b0dcd7dfd37b0e219e797c0
lee989898 <wnsrud1352@gmail.com>
2023-11-08 14:41:12 +0900

    [test] 로또 번호 숫자가 6자리가 아닐 때 예외를 던지는지 확인

--- src/test/kotlin/validator/lottonumbervalidator/LottoNumberValidatorTest.kt
+++ src/test/kotlin/validator/lottonumbervalidator/LottoNumberValidatorTest.kt
@@ -13,4 +13,12 @@ class LottoNumberValidatorTest {
             LottoNumberValidator.appropriateLottoNumber(input)
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1,2,3,4,5", "1,2,3,4,5,6,7", "12,24,38"])
+    fun `로또 번호 숫자가 6자리가 아닐 때 예외를 던지는지`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            LottoNumberValidator.appropriateLottoNumber(input)
+        }
+    }
 }
