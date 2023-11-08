6a54fa1d57781f04b3d15345b2bd039909d21ed9
sh1mj1 <7wlgns@gmail.com>
2023-11-08 19:53:39 +0900

    feat(InputView): change a method with a nullable return to not

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,14 +3,14 @@ package lotto.view
 import camp.nextstep.edu.missionutils.Console.readLine
 
 class InputView {
-    fun purchaseAmount(): Int? = readLine().toIntOrNull()
+    fun purchaseAmount(): Int = readLine().toIntOrNull() ?: 0
 
-    fun lottoNumbers(): List<Int?> =
+    fun lottoNumbers(): List<Int> =
         readLine().split(DELIMITER).map {
-            it.trim().toIntOrNull()
+            it.trim().toInt()
         }.distinct()
 
-    fun bonusLottoNumber() = readLine().toIntOrNull()
+    fun bonusLottoNumber() = readLine().toIntOrNull() ?: 0
 
     companion object {
         const val DELIMITER = ","
