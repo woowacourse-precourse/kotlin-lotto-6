bf6c32ba89a940a11f7ce325d92d07985fe3d5e3
Junyoung-WON <wjyo60@gmail.com>
2023-11-08 22:05:34 +0900

    FEAT: 로또 클래스 내부에 로또 번호가 올바른지 검증하는 메소드 구현
    로또 클래스에 로또 번호의 범위가 1~45사이의 숫자인지, 중복되는 숫자는
    없는지를 확인하는 메소드를 구현하여, 객체 생성 시 검증하도록 하였다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,31 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        // 로또 번호의 개수는 6개여야 한다.
+        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
+        // 로또 번호는 1부터 45 사이의 정수여야 한다.
+        require(inCorrectRange(numbers)) { "로또 번호는 1부터 45 사이의 숫자여야 합니다." }
+        // 로또 번호는 중복이 없어야 한다.
+        require(hasDuplicateNumbers(numbers)) { "로또 번호는 중복되지 않아야 합니다." }
     }
 
-    // TODO: 추가 기능 구현
+    private fun inCorrectRange(lottoNumbers: List<Int>): Boolean {
+        for (number in lottoNumbers) {
+            if (number !in 1..45) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun hasDuplicateNumbers(lottoNumbers: List<Int>): Boolean {
+        val validator = mutableListOf<Int>()
+        for (lotto in lottoNumbers) {
+            if (validator.contains(lotto.toInt())) {
+                return false
+            }
+            validator.add(lotto.toInt())
+        }
+        return true
+    }
 }
