d64d821b2d8ecf81dbcd8a989b6133988746f3b7
Wonhui Roh <rwhui012@gmail.com>
2023-11-08 14:04:48 +0900

    test(lotto): LottoTest 파일의 `로또 번호에 중복된 숫자가 있으면 예외가 발생한다` 테스트가 통과할 수 있게 구현 코드 작성

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,6 +3,7 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.size == numbers.toSet().size)
     }
 
     fun getLottoNumbers(): List<Int> {
