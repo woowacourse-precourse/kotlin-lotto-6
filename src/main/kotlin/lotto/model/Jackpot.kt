0d4364e13ee55af3f111577646adf0cab1cff0a0
조형석 <chs9902@naver.com>
2023-11-08 15:32:34 +0900

    refactor: enum 구조에 따른 랭크 업데이트 구조 조정

--- src/main/kotlin/lotto/model/Jackpot.kt
+++ src/main/kotlin/lotto/model/Jackpot.kt
@@ -1,5 +1,13 @@
 package lotto.model
 
+enum class Rank(val label: String, val count: Int, val countLabel: String, val bonus: Boolean) {
+    THIRD(Constants.THREE_SAME, 3, Constants.COUNT, false),
+    FOURTH(Constants.FOUR_SAME, 4, Constants.COUNT, false),
+    FIFTH(Constants.FIVE_SAME, 5, Constants.COUNT, false),
+    FIFTH_WITH_BONUS(Constants.FIVE_WITH_BONUS_SAME, 5, Constants.COUNT, true),
+    SIXTH(Constants.SIX_SAME, 6, Constants.COUNT, false);
+}
+
 class Jackpot {
     fun discriminate(answers: List<Int>, bonus: Int, lotto: List<List<Int>>): List<Int> {
         val rank = mutableMapOf<Rank, Int>().apply {
@@ -20,19 +28,8 @@ class Jackpot {
     }
 
     fun rankUpdate(count: Int, rank: MutableMap<Rank, Int>, bonusCount: Boolean): MutableMap<Rank, Int> {
-        when (count) {
-            3 -> rank[Rank.THIRD] = rank.getOrDefault(Rank.THIRD, 0) + 1
-            4 -> rank[Rank.FOURTH] = rank.getOrDefault(Rank.FOURTH, 0) + 1
-            5 -> {
-                if (bonusCount) {
-                    rank[Rank.FIFTH_WITH_BONUS] = rank.getOrDefault(Rank.FIFTH_WITH_BONUS, 0) + 1
-                } else {
-                    rank[Rank.FIFTH] = rank.getOrDefault(Rank.FIFTH, 0) + 1
-                }
-            }
-
-            6 -> rank[Rank.SIXTH] = rank.getOrDefault(Rank.SIXTH, 0) + 1
-        }
+        val matchingRank = Rank.values().find { it.count == count && it.bonus == bonusCount }
+        matchingRank?.let { rank[it] = rank.getOrDefault(it, 0) + 1 }
         return rank
     }
 
--- src/main/kotlin/lotto/model/JackpotEnum.kt
+++
@@ -1,9 +0,0 @@
-package lotto.model
-
-enum class Rank(val label: String, val count: Int, val countLabel: String) {
-    THIRD(Constants.THREE_SAME, 0, Constants.COUNT),
-    FOURTH(Constants.FOUR_SAME, 0, Constants.COUNT),
-    FIFTH(Constants.FIVE_SAME, 0, Constants.COUNT),
-    FIFTH_WITH_BONUS(Constants.FIVE_WITH_BONUS_SAME, 0, Constants.COUNT),
-    SIXTH(Constants.SIX_SAME, 0, Constants.COUNT);
-}
