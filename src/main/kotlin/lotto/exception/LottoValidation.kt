57bfc04d2ccc546c973a5b28b4a653117dac8f61
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-11-08 18:50:49 +0900

    [FEAT] 입력 받을 로또 번호의 예외처리 추가

---
+++ src/main/kotlin/lotto/exception/LottoValidation.kt
@@ -0,0 +1,47 @@
+package lotto.exception
+
+class LottoValidation(private val lotto: String) {
+    private val input: List<String> = lotto.split(",")
+
+    init {
+        validateLottoComma()
+        validateLottoRange()
+        validateLottoNumber()
+        validateLottoDuplication()
+        validateLottoSix()
+    }
+
+    private fun validateLottoComma() {
+        require(lotto.contains(",")) {
+            "[ERROR] 번호를 쉼표 기준으로 입력해주세요."
+        }
+    }
+
+    private fun validateLottoRange() {
+        repeat(input.size) {
+            require(input[it].toInt() in 1..45) {
+                "[ERROR] 1~45 사이의 숫자만 가능합니다."
+            }
+        }
+    }
+
+    private fun validateLottoNumber() {
+        repeat(input.size) {
+            require(input[it].matches(Regex("[0-9]"))) {
+                "[ERROR] 숫자만 입력해주세요."
+            }
+        }
+    }
+
+    private fun validateLottoDuplication() {
+        require(input.toSet().size == input.size) {
+            "[ERROR] 중복된 숫자는 입력할 수 없습니다."
+        }
+    }
+
+    private fun validateLottoSix() {
+        require(input.size == 6) {
+            "[ERROR] 로또 숫자는 6개를 입력해야 합니다."
+        }
+    }
+}
