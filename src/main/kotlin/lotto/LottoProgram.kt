6471d65397a65fb698cf5162feb62b69fbb707bc
iamsseaa <homingsea@gmail.com>
2023-11-08 19:02:37 +0900

    당첨 통계 계산 기능 추가

--- src/main/kotlin/lotto/LottoProgram.kt
+++ src/main/kotlin/lotto/LottoProgram.kt
@@ -84,4 +84,15 @@ class LottoProgram {
     fun calculateProfitRate() {
         profitRate = (this.totalProfit / this.inputMoney) * 100
     }
+
+    fun printResult() {
+        println("당첨 통계")
+        println("---")
+        println("3개 일치 (5,000원) - ${totalRanking[1]}개")
+        println("4개 일치 (50,000원) - ${totalRanking[2]}개")
+        println("5개 일치 (1,500,000원) - ${totalRanking[3]}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${totalRanking[4]}개")
+        println("6개 일치 (2,000,000,000원) - ${totalRanking[5]}개")
+        println("총 수익률은 ${round((profitRate*10).toDouble())/10}%입니다.")
+    }
 }
