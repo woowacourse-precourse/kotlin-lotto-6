fbdc5e23d0c4b3e69c65a03020721b1c5251cd86
sh1mj1 <7wlgns@gmail.com>
2023-11-08 14:33:45 +0900

    refactor(Profit): add variable `profitRate` & modify the method
    * 인스턴스 변수로 profitRate 을 추가한다.
    * 수익률을 계산하는 메서드 `calculateProfitRate`를 추가한다

--- src/main/kotlin/lotto/model/Profit.kt
+++ src/main/kotlin/lotto/model/Profit.kt
@@ -1,8 +1,13 @@
 package lotto.model
 
-data class Profit(val amount: Int) {
+data class Profit(val totalProfit: Int) {
+    private var _profitRate: Double = 0.0
 
-    private fun getTotalProfit() {
+    val profitRate: Double
+        get() = _profitRate
 
+    fun calculateProfitRate(payment: Int): Double {
+        return 0.0
     }
+
 }
