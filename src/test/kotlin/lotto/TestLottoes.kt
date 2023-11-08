1a4b2c58d835dc4d765dce031ffa259c302b228d
sh1mj1 <7wlgns@gmail.com>
2023-11-08 11:13:44 +0900

    feat(TestLottoes): implement calculateLottoesResult method
    * 테스트에서 사용하는 Lottoes 인 TestLottoes 를 구현한다.

--- src/test/kotlin/lotto/TestLottoes.kt
+++ src/test/kotlin/lotto/TestLottoes.kt
@@ -9,8 +9,16 @@ class TestLottoes(private val userNumbers: Set<Int>, private val bonusNumber: In
     private var lottoesResult: MutableMap<WinningRank, Int> =
         WinningRank.values().associateWith { 0 }.toMutableMap()
 
+
     override fun calculateLottoesResult(): Map<WinningRank, Int> {
-        TODO("Not yet implemented")
+        lottoes.forEach {
+            val numMatchCount = it.calculateMatchingCount(userNumbers)
+            val bonusNumberMatch = it.containBonusNumber(bonusNumber)
+            val result = it.calculateLottoRank(numMatchCount, bonusNumberMatch)
+
+            lottoesResult[result] = (lottoesResult[result] ?: 0) + 1
+        }
+        return lottoesResult
     }
 
     override fun getTotalProfit(): Profit {
