5c24164b5cb94a019bd7c96822b5c02c76cd4b39
bak <bak4017@gmail.com>
2023-11-08 19:53:22 +0900

    refactor(lotto): 구입금액 입력 문구 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,7 +6,7 @@ class InputView {
     companion object {
         private const val WIN_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요."
         private const val BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요."
-        private const val PURCHASE_PRICE_PROMPT = "구입 금액을 입력해 주세요."
+        private const val PURCHASE_PRICE_PROMPT = "구입금액을 입력해 주세요."
 
         fun promptForPurchasePrice(): String {
             println(PURCHASE_PRICE_PROMPT)
