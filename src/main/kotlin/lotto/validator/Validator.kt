70dea95f33d4f433ec51093e5335418a4f9b97a5
정채훈 <mrchjung@peopleinnet.com>
2023-11-08 21:12:21 +0900

    feat(validator): 범위 안에 숫자가 있는지 검증하는 기능

--- src/main/kotlin/lotto/validator/Validator.kt
+++ src/main/kotlin/lotto/validator/Validator.kt
@@ -1,4 +1,8 @@
 package lotto.validator
 
 class Validator {
+    fun validatedNumberInRange(num: Int, start: Int, end: Int): Int {
+        if (num !in start..end) throw IllegalArgumentException("[ERROR] 로또 번호는 ${start}부터 ${end}사이 숫자여야 합니다.")
+        return num
+    }
 }
