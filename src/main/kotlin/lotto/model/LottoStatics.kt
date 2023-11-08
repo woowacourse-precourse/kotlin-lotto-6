ee08bf04abbb3e0afc99888623a83b39e50c9e46
gaeun <geun5744@gmail.com>
2023-11-08 15:35:34 +0900

    [FEAT(LottoStatics)] 당첨 통계 메서드 구현

--- src/main/kotlin/lotto/model/LottoStatics.kt
+++ src/main/kotlin/lotto/model/LottoStatics.kt
@@ -1,9 +1,24 @@
 package lotto.model
 
-class LottoStatics {
+class LottoStatics(
+    private val lottoManager: LottoManager
+) {
 
-    fun getWinStatics() {
+    private val winStatics = mutableMapOf(
+        WinningType.FIRST to 0,
+        WinningType.SECOND to 0,
+        WinningType.THIRD to 0,
+        WinningType.FOURTH to 0,
+        WinningType.FIFTH to 0,
+        WinningType.NOTHING to 0
+    )
 
+    fun getWinStatics(answerNumbers: List<Int>, bonusNumber: Int): Map<WinningType, Int> {
+        lottoManager.lottoes.forEach { lotto ->
+            val winningType = lotto.matchAnswer(answerNumbers, bonusNumber)
+            winStatics[winningType] = winStatics[winningType]!! +1
+        }
+        return winStatics
     }
 
     fun getTotalReturn() {
