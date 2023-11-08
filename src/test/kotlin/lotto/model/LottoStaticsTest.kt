7e631472e821c6764b7c60ea245227bbe637c95e
gaeun <geun5744@gmail.com>
2023-11-08 20:48:07 +0900

    [TEST(LottoStaticsTest)] 당첨 통계 테스트 구현

---
+++ src/test/kotlin/lotto/model/LottoStaticsTest.kt
@@ -0,0 +1,38 @@
+package lotto.model
+
+import camp.nextstep.edu.missionutils.test.Assertions
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class LottoStaticsTest {
+    @Test
+    fun `당첨 통계 테스트`() {
+
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                val lottoManager = LottoManager(PURCHASE_AMOUNT)
+                lottoManager.create()
+                val lottoStatics = LottoStatics(lottoManager)
+                val winStatics = lottoStatics.getWinStatics(ANSWER, BONUS)
+                assertEquals(winStatics[WinningType.FOURTH], 1)
+                assertEquals(winStatics[WinningType.FIFTH], 1)
+            },
+            listOf(1, 2, 3, 4, 7, 8),
+            listOf(4, 5, 6, 18, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+            listOf(13, 14, 16, 38, 42, 45),
+        )
+    }
+
+    companion object {
+        private val ANSWER = listOf(1, 2, 3, 4, 5, 6)
+        private const val BONUS = 7
+        private const val PURCHASE_AMOUNT = 5000
+    }
+
+}
