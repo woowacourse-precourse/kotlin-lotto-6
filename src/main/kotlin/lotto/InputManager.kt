f35850f9ac47005066b05af77d9838f90956d298
LEE <tpqms775@naver.com>
2023-11-08 13:17:22 +0900

    feat : 사용자가 잘못된 값을 입력할 경우 에러 발생 후 재입력

--- src/main/kotlin/lotto/InputManager.kt
+++ src/main/kotlin/lotto/InputManager.kt
@@ -5,33 +5,37 @@ import camp.nextstep.edu.missionutils.Console
 class InputManager {
     private val exceptionManager = ExceptionManager()
     fun inputMoney(): Int {
-        println(PRINT_INPUT_MONEY)
-        val money = Console.readLine().trim()
-        exceptionManager.moneyException(money)
-        return money.toInt()
+        return try {
+            println(PRINT_INPUT_MONEY)
+            val money = Console.readLine().trim()
+            exceptionManager.moneyException(money)
+            return money.toInt()
+        }catch (e: IllegalArgumentException){
+            println(e.message)
+            inputMoney()
+        }
+
     }
 
     fun inputWinningNumber(): Pair<List<Int>,Int> {
-        println(PRINT_INPUT_WINNING_NUMBER)
-        val nums = Console.readLine().split(DELIMITER)
-        exceptionManager.winningNumberException(nums)
-        println()
+       return try {
+            println(PRINT_INPUT_WINNING_NUMBER)
+            val nums = Console.readLine().split(DELIMITER)
+            exceptionManager.winningNumberException(nums)
+            println()
 
-        println(PRINT_INPUT_BONUS_NUMBER)
-        val num = Console.readLine().trim()
-        exceptionManager.bonusNumberException(Pair(nums,num))
-        println()
+            println(PRINT_INPUT_BONUS_NUMBER)
+            val num = Console.readLine().trim()
+            exceptionManager.bonusNumberException(Pair(nums,num))
+            println()
 
-        return Pair(nums.map { it.toInt() } ,num.toInt())
+            return Pair(nums.map { it.toInt() } ,num.toInt())
+        }catch (e: IllegalArgumentException){
+           inputWinningNumber()
+        }
+
     }
 
-//    fun qinputBonusNumber(): Int {
-//        println(PRINT_INPUT_BONUS_NUMBER)
-//        val num = Console.readLine().trim()
-//        exceptionManager.bonusNumberException(num)
-//        println()
-//        return num.toInt()
-//    }
 
     companion object {
 
