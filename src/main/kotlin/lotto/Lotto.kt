9e15c127dc7eac0eb5aa43b1fdf920685cdad924
Seannnn55 <sally99h@gmail.com>
2023-11-08 17:54:54 +0900

    feat: [Lotto] 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun makeLotto(): List<Int>? {
+        var lottoNums: MutableList<Int> = pickUniqueNumbersInRange(1, 45, 6)
+        return lottoNums.sorted()
+    }
+
+    fun checkLotto(userNumbers: List<Int>): Int {
+        var sameNums: Int = 0
+        for (i in userNumbers) {
+            if (i in numbers) {
+                sameNums += 1
+            }
+        }
+        return sameNums
+    }
 }
