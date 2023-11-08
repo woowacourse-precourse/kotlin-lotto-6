28c9424f3b347843cc6b3cd6b6d86fc55063f965
Jtility <leejewon97@gmail.com>
2023-11-08 18:28:13 +0900

    feat(LottoUI): 구입금액 예외 처리 열거형들 적용

--- src/main/kotlin/lotto/LottoUI.kt
+++ src/main/kotlin/lotto/LottoUI.kt
@@ -22,8 +22,8 @@ class LottoUI {
 
     private fun checkInvalidBuyPrice(buyPrice: String) {
         when {
-            buyPrice.toIntOrNull() == null -> throw IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.")
-            buyPrice.toInt() % 1000 != 0 -> throw IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.")
+            buyPrice.toIntOrNull() == null -> throw IllegalArgumentException(PrintBuy.ERROR_NOT_INTEGER.value)
+            buyPrice.toInt() % 1000 != 0 -> throw IllegalArgumentException(PrintBuy.ERROR_NOT_THOUSAND.value)
         }
     }
 
