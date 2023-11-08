959eae5c75ea5b7049c4fb1542f628fb96be9857
bak <bak4017@gmail.com>
2023-11-08 17:09:17 +0900

    test(lotto): 입력검증 공통 로직 테스트 추가

--- src/test/kotlin/lotto/validator/InputValidatorTest.kt
+++ src/test/kotlin/lotto/validator/InputValidatorTest.kt
@@ -3,6 +3,7 @@ package lotto.validator
 
 import lotto.constant.Constants.END_INCLUSIVE
 import lotto.constant.Constants.START_INCLUSIVE
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -34,5 +35,29 @@ class InputValidatorTest {
         }
 
         assertEquals("[ERROR] 로또 번호는 ${START_INCLUSIVE}부터 ${END_INCLUSIVE}사이만 가능합니다", exception.message)
+    }
+
+    @Test
+    fun `공백이 아닌 올바른 값 입력 시`() {
+        val inputValidator = InputValidator()
+        assertDoesNotThrow {
+            inputValidator.checkBlank("NoBlank")
+        }
+    }
+
+    @Test
+    fun `양의 정수로 올바른 값 입력 시`() {
+        val inputValidator = InputValidator()
+        assertDoesNotThrow {
+            inputValidator.checkForPositiveInteger("1")
+        }
+    }
+
+    @Test
+    fun `올바른 범위 내의 수 입력 시`() {
+        val inputValidator = InputValidator()
+        assertDoesNotThrow {
+            inputValidator.checkForNumberRange("25")
+        }
     }
 }
