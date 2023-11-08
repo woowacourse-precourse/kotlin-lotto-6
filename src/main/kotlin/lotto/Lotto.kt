6e81724518e899a92907af957b1c6e175c47aa57
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-11-08 19:57:39 +0900

    Refactor : Lotto.kt Validator에 맞게 변화

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,22 +3,9 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        vaildateLength()
-        validateDuplication()
-        validateBound()
-    }
-
-    private fun vaildateLength() {
-        require(numbers.size == 6) { SIZEERROR }
-    }
-
-    private fun validateBound(){
-        numbers.forEach{
-            require(it >= LottoInput.MIN_NUMBER && it <= LottoInput.MAX_NUMBER) { LottoInput.NUMBEROUTOFBOUND }
-        }
-    }
-    private fun validateDuplication() {
-        require(numbers.size == numbers.distinct().count()) { ERRORDUPLICATION }
+        Validator.lottoLength(numbers)
+        Validator.duplication(numbers)
+        Validator.lottoRange(numbers)
     }
 
     fun matchingNum(winningNumber: List<Int>, bonus: Int): Int {
@@ -30,12 +17,6 @@ class Lotto(private val numbers: List<Int>) {
 
     fun printLotto() {
         println(numbers.sorted())
-    }
-
-    companion object {
-        const val SIZE = 6
-        const val SIZEERROR = "ERROR: 로또 번호를 ${SIZE} 보다 적거나 많이 입력했습니다.."
-        const val ERRORDUPLICATION = "ERROR: 입력하신 로또 번호는 중복이 있습니다."
     }
 
 }
