4b9a4fc5aadc5229f2d661b6d320868ccbaa2ea0
3uomlkh <cw1205@duksung.ac.kr>
2023-11-08 14:27:39 +0900

    refactor: 출력 기능 클래스 분리

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,23 +1,33 @@
 package lotto.view
 
-object OutputView {
-    fun printStartMessage() {
-        println("구입금액을 입력해 주세요.")
-    }
+enum class OutputView(val message: String) {
+    PURCHASE_AMOUNT_PROMPT("구입금액을 입력해 주세요."),
+    PURCHASE_AMOUNT_PRINT("개를 구매했습니다."),
+    WINNING_NUMBER_PROMPT("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_PROMPT("\n보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("당첨 통계\n---")
+}
 
-    fun printPurchaseTotal(total: Int) {
-        println("\n${total/1000}개를 구매했습니다.")
-    }
+fun printStartMessage() {
+    println(OutputView.PURCHASE_AMOUNT_PROMPT)
+}
 
-    fun printWinningMessage() {
-        println("\n당첨 번호를 입력해 주세요.")
-    }
+fun printPurchaseTotal(total: Int) {
+    println("${total}개를 구매했습니다.")
+}
 
-    fun printBonusMessage() {
-        println("\n보너스 번호를 입력해 주세요.")
-    }
+fun printWinningMessage() {
+    println(OutputView.WINNING_NUMBER_PROMPT)
+}
 
-    fun printWinningReport() {
-        println("당첨 통계\n---")
-    }
+fun printBonusMessage() {
+    println(OutputView.BONUS_NUMBER_PROMPT)
+}
+
+fun printWinningReport() {
+    println(OutputView.WINNING_STATISTICS)
+}
+
+fun printEarningRate(rate: Float) {
+    println("총 수익률은 {$rate}%입니다.")
 }
