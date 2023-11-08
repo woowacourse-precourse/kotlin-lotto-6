40b638d5e7448ae3c5300272d67c3eb52a68532a
minuxx <minux8905@gmail.com>
2023-11-08 14:40:45 +0900

    feat: 로또 발행 기능 추가

--- src/main/kotlin/lotto/models/Publisher.kt
+++ src/main/kotlin/lotto/models/Publisher.kt
@@ -1,10 +1,22 @@
 package lotto.models
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Publisher(private val publishingLottoCount: Int) {
 
     fun publishLottos(): List<Lotto> {
         val publishedLottos = mutableListOf<Lotto>()
 
+        repeat(publishingLottoCount) {
+            val lottoNumbers = generateLottoNumbers()
+            val publishedLotto = Lotto(lottoNumbers)
+
+            publishedLottos.add(publishedLotto)
+        }
+
         return publishedLottos
     }
+
+    private fun generateLottoNumbers() =
+        Randoms.pickUniqueNumbersInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER, Lotto.NUMBER_COUNT)
 }
--- src/test/kotlin/lotto/models/PublisherTest.kt
+++ src/test/kotlin/lotto/models/PublisherTest.kt
@@ -1,7 +1,6 @@
 package lotto.models
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class PublisherTest {
