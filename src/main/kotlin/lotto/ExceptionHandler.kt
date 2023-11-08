a6ad4696a4560ebe92c2ff71e6667acf302d7618
2kjin <lkjin1202@gmail.com>
2023-11-08 17:36:53 +0900

    ✨ feat : 예외처리 생성

---
+++ src/main/kotlin/lotto/ExceptionHandler.kt
@@ -0,0 +1,24 @@
+package lotto
+
+object ExceptionHandler {
+     // 중복 여부 확인
+    fun isDuplicate(): Boolean {
+        return true
+    }
+
+     // 길이가 6을 넘는지 확인
+    fun isOverLength() {
+
+    }
+
+
+     // 1~45사이 숫자인지 확인
+    fun isInRange() {
+
+    }
+
+     // 1000으로 나누어 떨어지는지 확인
+    fun isDivideBy1000() {
+
+    }
+}
