4804113a133f89bc07745d5f6b53573fac32de89
gaeun <geun5744@gmail.com>
2023-11-08 18:13:14 +0900

    [TEST(ValidationTest)] 당첨 번호 입력 테스트 구현

--- src/test/kotlin/lotto/model/ValidationTest.kt
+++ src/test/kotlin/lotto/model/ValidationTest.kt
@@ -26,4 +26,50 @@ class ValidationTest {
         }
     }
 
+    @Test
+    @DisplayName("당첨 번호 입력 테스트")
+    fun `숫자가 아닌 문자를 입력할 경우 예외 발생`() {
+        val input = "횟수"
+        assertThrows<IllegalArgumentException> {
+            Validation.getAnswerNumber(input)
+        }
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 테스트")
+    fun `쉼표가 없는 경우 예외 발생`() {
+        val input = "1 2 3"
+        assertThrows<IllegalArgumentException> {
+            Validation.getAnswerNumber(input)
+        }
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 테스트")
+    fun `6개의 숫자를 입력하지 않은 경우 예외 발생`() {
+        val input = "1,2,3,4,5"
+        assertThrows<IllegalArgumentException> {
+            Validation.getAnswerNumber(input)
+        }
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 테스트")
+    fun `1~45 사이의 숫자가 아닌 경우 예외 발생`() {
+        val input = "1,2,3,4,5,100"
+        assertThrows<IllegalArgumentException> {
+            Validation.getAnswerNumber(input)
+        }
+    }
+
+    @Test
+    @DisplayName("당첨 번호 입력 테스트")
+    fun `같은 숫자를 입력한 경우 예외 발생`() {
+        val input = "1,2,3,4,5,5"
+        assertThrows<IllegalArgumentException> {
+            Validation.getAnswerNumber(input)
+        }
+    }
+
+
 }
