2184658986f554e9319bcf57e2393734a8241d52
jinuemong <jinwoo3661@naver.com>
2023-11-08 13:35:21 +0900

    Feat: 게임결과 출력 기능 추가, 게임 금액에 컴마 추가

--- src/main/kotlin/lotto/validation/CheckInputValidation.kt
+++ src/main/kotlin/lotto/validation/CheckInputValidation.kt
@@ -1,5 +1,6 @@
 package lotto.validation
 
+import lotto.domain.ErrorType
 import lotto.domain.LottoManager.Companion.LOTTO_COST
 import lotto.domain.LottoManager.Companion.LOTTO_COUNT
 import lotto.domain.LottoManager.Companion.LOTTO_END_INCLUSIVE
@@ -13,39 +14,39 @@ class CheckInputValidation {
             userInput.toInt()
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException(
-                IS_NOT_INTEGER
+                ErrorType.IS_NOT_INTEGER.message
             )
         }
     }
 
     fun checkIsPositiveInteger(number: String) {
         require(number.toInt() > 0) {
-            IS_NOT_POSITIVE_INTEGER
+            ErrorType.IS_NOT_POSITIVE_INTEGER.message
         }
     }
 
     fun checkIsCorrectCost(cost: Int) {
         require(cost % LOTTO_COST != 0) {
-            IS_INCORRECT_PURCHASE
+            ErrorType.IS_INCORRECT_PURCHASE.message
         }
     }
 
     fun checkIsLottoNumber(number: String) {
         require(number.toInt() in LOTTO_START_INCLUSIVE..LOTTO_END_INCLUSIVE) {
-            IS_INCORRECT_NUMBER
+            ErrorType.IS_INCORRECT_NUMBER.message
         }
     }
 
     fun checkIsBlank(userInput: String) {
         require(userInput.isNotEmpty()) {
-            IS_BLANK
+            ErrorType.IS_BLANK.message
         }
     }
 
     fun checkDuplication(inputList: List<Int>) {
         val distinctCount = inputList.distinct().count()
         require(inputList.size == distinctCount) {
-            IS_CONTAIN_DUPLICATES
+            ErrorType.IS_CONTAIN_DUPLICATES.message
         }
     }
 
@@ -54,7 +55,7 @@ class CheckInputValidation {
         bonusNumber: Int
     ) {
         require(numbers.contains(bonusNumber)) {
-            IS_CONTAIN_DUPLICATES
+            ErrorType.IS_CONTAIN_DUPLICATES.message
         }
     }
 
@@ -62,17 +63,7 @@ class CheckInputValidation {
         userInput: List<String>
     ) {
         require(userInput.size == LOTTO_COUNT) {
-            IS_NOT_SIX_NUMBERS
+            ErrorType.IS_NOT_SIX_NUMBERS.message
         }
-    }
-
-    companion object {
-        const val IS_NOT_INTEGER = "입력 값이 숫자가 아닙니다."
-        const val IS_NOT_POSITIVE_INTEGER = "입력 값이 정수가 아닙니다."
-        const val IS_CONTAIN_DUPLICATES = "중복 된 로또 번호가 있습니다."
-        const val IS_INCORRECT_NUMBER = "1~45 범위의 로또 번호가 아닙니다."
-        const val IS_INCORRECT_PURCHASE = "올바르지 않은 구매 금액입니다.(1,000원 단위 입력)"
-        const val IS_BLANK = "빈 값이 입력 되었습니다."
-        const val IS_NOT_SIX_NUMBERS = "로또 번호가 6개가 아닙니다."
     }
 }
