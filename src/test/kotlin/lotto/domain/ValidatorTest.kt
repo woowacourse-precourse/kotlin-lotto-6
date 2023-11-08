0bb5b4e6ec265a59ae9f47b7776abcaef7248ffd
DoDoBest <av449@naver.com>
2023-11-08 11:41:30 +0900

    chore: Validator 함수 이름 변경된 것 테스트 함수 이름에 반영

--- src/test/kotlin/lotto/domain/ValidatorTest.kt
+++ src/test/kotlin/lotto/domain/ValidatorTest.kt
@@ -26,8 +26,8 @@ class ValidatorTest {
 
     @ParameterizedTest(name = "check {0}")
     @ValueSource(strings = ["1", "17", "500", "999", "1001", "20001", "100001"])
-    @DisplayName("Validator : checkInputOfPurchasingCorrect() - success")
-    fun `checkInputOfPurchasingCorrect 함수에 0보다 큰 양수 문자열 입력하면 true를 반환한다`(input: String) {
+    @DisplayName("Validator : checkInputIsPositiveNum() - success")
+    fun `checkInputIsPositiveNum 함수에 0보다 큰 양수 문자열 입력하면 true를 반환한다`(input: String) {
         // when
         val actual = validator.checkInputIsPositiveNum(input)
 
@@ -38,8 +38,8 @@ class ValidatorTest {
 
     @ParameterizedTest(name = "check {0}")
     @ValueSource(strings = ["천원", "10공0", "I000", "0", "-1", "-1000", "1 dollar", "lI00", "1000원", "2_000"])
-    @DisplayName("Validator : checkInputOfPurchasingCorrect() - fail")
-    fun `checkInputOfPurchasingCorrect 함수에 0보다 큰 양수가 아닌 문자열을 입력하면 false를 반환한다`(input: String) {
+    @DisplayName("Validator : checkInputIsPositiveNum() - fail")
+    fun `checkInputIsPositiveNum 함수에 0보다 큰 양수가 아닌 문자열을 입력하면 false를 반환한다`(input: String) {
         // when
         val actual = validator.checkInputIsPositiveNum(input)
 
