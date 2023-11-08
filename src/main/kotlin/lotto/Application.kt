f4524c6e5adb7af8bdfa2cf0548b718e1a9e0dd5
MinJun <agvber12@gmail.com>
2023-11-08 16:29:16 +0900

    feat: 구입 금액에서 잘못된 값을 받을 경우 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,9 +8,19 @@ fun main() {
 
     println("구입금액을 입력해 주세요.")
     inputLottoPurchaseAmount()
-
+    checkLottoPurchaseAmount()
 }
 
 private fun inputLottoPurchaseAmount() {
-    lottoPurchaseAmount = readLine().toInt()
+    try {
+        lottoPurchaseAmount = readLine().toInt()
+    } catch (e: Exception) {
+        throw IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자만 입력 받습니다.")
+    }
+}
+
+private fun checkLottoPurchaseAmount() {
+    if (lottoPurchaseAmount.rem(1000) != 0) {
+        throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000원 단위만 입력 받습니다.")
+    }
 }
