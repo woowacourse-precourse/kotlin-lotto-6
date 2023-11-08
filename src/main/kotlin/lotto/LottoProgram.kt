3b1c5c201d334c7ea1a404e45e22a2133ab3b8d6
iamsseaa <homingsea@gmail.com>
2023-11-08 16:29:32 +0900

    로또 발행 기능 추가

--- src/main/kotlin/lotto/LottoProgram.kt
+++ src/main/kotlin/lotto/LottoProgram.kt
@@ -4,15 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoProgram {
     var count: Int = 0
+    var lottos:MutableList<Lotto> = mutableListOf()
 
     fun makeCalculationMoney(money: Int) {
         this.count = money / 1000
     }
 
-    fun makeRandomNumbers(): List<Int>? {
+    fun makeRandomNumbers(): List<Int> {
         var numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
         return numbers
     }
 
-
+    fun makeLottos() {
+        for(i in 0 until this.count) {
+            val numbers = makeRandomNumbers().sorted()
+            val lotto = Lotto(numbers)
+            this.lottos.add(lotto)
+        }
+    }
 }
