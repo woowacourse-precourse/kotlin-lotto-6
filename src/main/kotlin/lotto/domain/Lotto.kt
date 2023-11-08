df62a4f7074979ac1cf1ebbe0ec874e6045929fa
tgyuu-An <atg0614@gmail.com>
2023-11-08 13:38:27 +0900

    feature : 로또 랭크 계산 로직 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -7,6 +7,54 @@ class Lotto(private val numbers: List<Int>) {
         validateLottoNumbers(numbers)
     }
 
+    var lottoRank: LottoRank = LottoRank.INIT
+        private set
+
+    fun getLottoNumbers(): List<Int> {
+        return numbers.toList().sorted()
+    }
+
+    fun calculateWinningRank(
+        winningNumbers: List<Int>,
+        bonusNumber: Int
+    ) {
+        val score: Pair<BonusFlag, Int> = calculateMatchingNumbers(winningNumbers, bonusNumber)
+        val bonusFlag = score.first
+        val matchingCount = score.second
+
+        lottoRank = when (matchingCount) {
+            6 -> LottoRank.FIRST
+            5 -> calculateSecondThirdRank(bonusFlag)
+            4 -> LottoRank.FOURTH
+            3 -> LottoRank.FIFTH
+            else -> LottoRank.NOTHING
+        }
+    }
+
+    private fun calculateMatchingNumbers(
+        winningNumbers: List<Int>,
+        bonusNumber: Int
+    ): Pair<BonusFlag, Int> {
+        var bonusFlag = BonusFlag.MISS_BONUS
+        var matchingCount = 0
+
+        winningNumbers.forEach { number ->
+            if (number in winningNumbers) matchingCount += 1
+            if (number == bonusNumber) {
+                bonusFlag = BonusFlag.HIT_BONUS
+                matchingCount += 1
+            }
+        }
+        return bonusFlag to matchingCount
+    }
+
+    private fun calculateSecondThirdRank(bonusFlag: BonusFlag): LottoRank {
+        return when (bonusFlag) {
+            BonusFlag.HIT_BONUS -> LottoRank.SECOND
+            BonusFlag.MISS_BONUS -> LottoRank.THIRD
+        }
+    }
+
     companion object {
         val LOTTO_NUMBER_COUNT = 6
         val LOTTO_MIN_VALUE = 1
--- src/main/kotlin/lotto/domain/LottoManager.kt
+++ src/main/kotlin/lotto/domain/LottoManager.kt
@@ -20,41 +20,4 @@ class LottoManager {
         validateNoDuplicateNumbers(winningNumbers, input)
         bonusNumber = input.toInt()
     }
-
-    fun calculateMatchingNumbers(lottoNumbers: List<Int>): Pair<BonusFlag, Int> {
-        var bonusFlag = BonusFlag.MISS_BONUS
-        var matchingCount = 0
-
-        lottoNumbers.forEach { number ->
-            if (number in winningNumbers) {
-                matchingCount += 1
-            }
-            if (number == bonusNumber) {
-                bonusFlag = BonusFlag.HIT_BONUS
-                matchingCount += 1
-            }
-        }
-
-        return bonusFlag to matchingCount
-    }
-
-    fun calculateWinningRank(score: Pair<BonusFlag, Int>): LottoRank {
-        val bonusFlag = score.first
-        val matchingCount = score.second
-
-        return when (matchingCount) {
-            6 -> LottoRank.FIRST
-            5 -> calculateSecondThirdRank(bonusFlag)
-            4 -> LottoRank.FOURTH
-            3 -> LottoRank.FIFTH
-            else -> LottoRank.NOTHING
-        }
-    }
-
-    private fun calculateSecondThirdRank(bonusFlag: BonusFlag): LottoRank {
-        return when (bonusFlag) {
-            BonusFlag.HIT_BONUS -> LottoRank.SECOND
-            BonusFlag.MISS_BONUS -> LottoRank.THIRD
-        }
-    }
 }
