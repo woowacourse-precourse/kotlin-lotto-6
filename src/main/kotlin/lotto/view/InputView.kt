34f7a0c0a26f927754134dd7b46d8e0889dfaf72
MinhoiKoo <minlove2013@gmail.com>
2023-11-08 23:38:13 +0900

    refactor: 잘못된 값 입력시 재입력 받는 로직 변경
    -while문 -> 재귀 호출로 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -10,29 +10,27 @@ class InputView {
     private val outputView = OutputView()
 
     fun inputMoney(): Money {
-        while (true) {
-            try {
-                val input = Console.readLine().trim()
-                ExceptionHandler.isBlank(input)
-                ExceptionHandler.isDigit(input)
-                val money = Money(input.toInt())
-                return money
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
-            }
+        return try {
+            val input = Console.readLine().trim()
+            ExceptionHandler.isBlank(input)
+            ExceptionHandler.isDigit(input)
+            val money = Money(input.toInt())
+            money
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            inputMoney()
         }
     }
 
     fun inputWinningNumbersWithBonusNumber(): Pair<WinningLotto, LottoNumber> {
-        while (true) {
-            try {
-                val winningLotto = inputWinningNumber()
-                val bonusNumber = inputBonusNumber()
-                ExceptionHandler.checkBonusNumberIsDuplicate(winningLotto, bonusNumber)
-                return Pair(winningLotto, bonusNumber)
-            } catch (e: IllegalArgumentException) {
-                println(e.message)
-            }
+        return try {
+            val winningLotto = inputWinningNumber()
+            val bonusNumber = inputBonusNumber()
+            ExceptionHandler.checkBonusNumberIsDuplicate(winningLotto, bonusNumber)
+            Pair(winningLotto, bonusNumber)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            inputWinningNumbersWithBonusNumber()
         }
     }
 
