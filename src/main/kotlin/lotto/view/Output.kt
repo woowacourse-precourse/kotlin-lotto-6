a5cb0e4b3667f7adf62bc7468136a864d50947a9
gaeun <geun5744@gmail.com>
2023-11-08 15:39:52 +0900

    [FEAT(Output)] 당첨 통계 print 메서드 구현

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -1,9 +1,17 @@
 package lotto.view
 
+import lotto.model.WinningType
+
 class Output {
 
-    fun printWinStatics() {
-
+    fun printWinStatics(winStatics: Map<WinningType, Int>) {
+        println("당첨 통계")
+        println("---")
+        println("3개 일치 (5,000원) - ${winStatics[WinningType.FIFTH]}개")
+        println("4개 일치 (50,000원) - ${winStatics[WinningType.FOURTH]}개")
+        println("5개 일치 (1,500,000원) - ${winStatics[WinningType.THIRD]}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winStatics[WinningType.SECOND]}개")
+        println("6개 일치 (2,000,000,000원) - ${winStatics[WinningType.FIRST]}개")
     }
 
     fun printTotalReturn() {
