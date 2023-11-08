4ae3af06a309b6ee7454387f740f9d4771bc039c
wnsgml517 <wnsgml517@naver.com>
2023-11-08 15:26:52 +0900

    Application.kt | 보너스 번호 입력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -11,6 +11,7 @@ fun main() {
         println("${purchasedTickets}개를 구매했습니다.")
 
         val winningNumbers = getWinningNumbers()
+        val bonusNumber = getBonusNumber()
 
     } catch (e: IllegalArgumentException) {
         println(e.message)
@@ -39,12 +40,35 @@ fun generateLottoTickets(purchaseAmount: Int): Int {
 
 fun getWinningNumbers(): List<Int> {
     println("당첨 번호를 입력해 주세요.")
-    val input = Console.readLine()
-    val numbers = input.split(",").map { it.trim().toInt() }
+    while (true) {
+        try {
+            val input = Console.readLine()
+            val numbers = input.split(",").map { it.trim().toInt() }
 
-    if (numbers.size != 6 || numbers.any { it < 1 || it > 45 }) {
-        throw IllegalArgumentException("[ERROR] 1에서 45 사이의 6개 번호를 입력해야 합니다.")
+            if (numbers.size != 6 || numbers.any { it < 1 || it > 45 }) {
+                println("[ERROR] 1에서 45 사이의 6개 번호를 중복되지 않게 입력해야 합니다.")
+            } else if (numbers.toSet().size != 6) {
+                println("[ERROR] 중복된 번호가 있습니다. 중복되지 않은 번호 6개를 입력해야 합니다.")
+            } else {
+                return numbers
+            }
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 유효하지 않은 숫자를 입력했습니다. 다시 입력하세요.")
+        }
     }
-
-    return numbers
+}
+fun getBonusNumber(): Int {
+    println("보너스 번호를 입력해 주세요.")
+    try {
+        val input = Console.readLine()
+        val bonusNumber = input.toInt()
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            println("[ERROR] 1에서 45 사이의 번호를 입력하세요.")
+        } else {
+            return bonusNumber
+        }
+    } catch (e: NumberFormatException) {
+        println("[ERROR] 유효하지 않은 숫자를 입력했습니다. 다시 입력하세요.")
+    }
+    return 0
 }
