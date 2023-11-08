0e3bbeda0f83024355d32bda48c54264d366762e
최형우 <guddn106901@gmail.com>
2023-11-08 23:04:31 +0900

    매칭 결과 확인, error 코드 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,20 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_NUMBER_SIZE) { ERROR_SIZE }
+        require(numbers.all { it in 1..45 }) { ERROR_NUMBER_RANGE }
+        require(numbers.distinct().size == numbers.size) { ERROR_DUPLICATE }
+    }
+    fun checkMatching(prizeLottoNumber: List<Int>, bonusNumber: Int):Pair<Int, Boolean>{
+        val lottoNumber = numbers.count { it in prizeLottoNumber }
+        val bonusNumberMatchStatus = numbers.any { it == bonusNumber }
+        return Pair(lottoNumber, bonusNumberMatchStatus)
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        private const val LOTTO_NUMBER_SIZE = 6
+        private const val ERROR_SIZE = "[ERROR] 6개의 로또 번호를 입력하세요"
+        private const val ERROR_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+        private const val ERROR_DUPLICATE = "[ERROR] 중복된 값은 입력할 수 없습니다."
+    }
 }
