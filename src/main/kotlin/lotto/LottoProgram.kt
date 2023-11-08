75352c49bfdcbefe153dffbff415dd78c5d87013
iamsseaa <homingsea@gmail.com>
2023-11-08 16:17:43 +0900

    6개의 랜덤 숫자 구하는 기능 수정

--- src/main/kotlin/lotto/LottoProgram.kt
+++ src/main/kotlin/lotto/LottoProgram.kt
@@ -4,14 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoProgram {
     var count: Int = 0
-    var numbers: List<Int> = listOf()
 
     fun makeCalculationMoney(money: Int) {
         this.count = money / 1000
     }
 
-    fun makeRandomNumbers() {
-        this.numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    fun makeRandomNumbers(): List<Int>? {
+        var numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return numbers
     }
 
 
