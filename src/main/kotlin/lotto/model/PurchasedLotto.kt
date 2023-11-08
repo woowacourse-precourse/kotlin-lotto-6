ca59bfee88780a9f2c3a4961e0ba5f12a5cc11f1
NOGUEN <nhg1113@naver.com>
2023-11-08 09:54:50 +0900

    refactor: 불필요한 메소드 제거와 메소드 이름 수정 getWinPrize -> setWinPrize, getWinRank -> setWinRank

--- src/main/kotlin/lotto/model/PurchasedLotto.kt
+++ src/main/kotlin/lotto/model/PurchasedLotto.kt
@@ -2,19 +2,22 @@ package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.*
+import lotto.dto.PurchasedLottoDto
 
-class PurchasedLotto(lottoList: List<Int>, bonusBall: Int) {
+class PurchasedLotto {
     private var numbers : List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
     private var matchingNumberCount = 0
     private var bonusBallMatched = false
     private var winPrize = 0
+    private var winRank = WIN_RANK_NOTHING
 
-    init {
+    fun setPurchasedLotto(lottoList: List<Int>, bonusBall: Int) {
         numbers = numbers.sorted()
         countMatchingNumber(lottoList)
         if (matchingNumberCount == LOTTO_WIN_FIVE)
             checkBonusBall(bonusBall)
-        getWinRankCost()
+        setWinRankPrize()
+        setWinRank()
     }
 
     private fun countMatchingNumber(lottoList: List<Int>) {
@@ -30,20 +33,35 @@ class PurchasedLotto(lottoList: List<Int>, bonusBall: Int) {
             bonusBallMatched = true
     }
 
-    private fun getWinRankCost() {
+    private fun setWinRankPrize() {
         when (matchingNumberCount) {
-            WIN_RANK_FIRST -> winPrize = LOTTO_FIRST_WIN_COST
-            WIN_RANK_SECOND_AND_THIRD -> {
-                if (bonusBallMatched)
-                    winPrize = LOTTO_SECOND_WIN_COST
-                winPrize = LOTTO_THIRD_WIN_COST
+            MATCH_SIX -> winPrize = LOTTO_FIRST_WIN_COST
+            MATCH_FIVE -> {
+                winPrize = if (bonusBallMatched)
+                    LOTTO_SECOND_WIN_COST
+                else
+                    LOTTO_THIRD_WIN_COST
             }
-            WIN_RANK_FORTH -> winPrize = LOTTO_FORTH_WIN_COST
-            WIN_RANK_FIFTH -> winPrize = LOTTO_FIFTH_WIN_COST
+            MATCH_FOUR -> winPrize = LOTTO_FORTH_WIN_COST
+            MATCH_THREE -> winPrize = LOTTO_FIFTH_WIN_COST
+        }
+    }
+
+    private fun setWinRank() {
+        when (matchingNumberCount) {
+            MATCH_SIX -> winRank = WIN_RANK_FIRST
+            MATCH_FIVE -> {
+                winRank = if (bonusBallMatched)
+                    WIN_RANK_SECOND
+                else
+                    WIN_RANK_THIRD
+            }
+            MATCH_FOUR -> winRank = WIN_RANK_FORTH
+            MATCH_THREE -> winRank = WIN_RANK_FIFTH
         }
     }
 
     fun getWinPrize(): Int {
         return winPrize
-    }
+    }   
 }
