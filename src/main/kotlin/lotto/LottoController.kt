264c7f646fafc5f056ebcbaa6908b4bd9cebd7da
최형우 <guddn106901@gmail.com>
2023-11-08 23:49:38 +0900

    lotto 입력 메소드

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoController(
     private val
     inputview: InputView = InputView(),
@@ -16,6 +18,37 @@ class LottoController(
                 outputview.amountMessage()
                 amount = inputview.inputAmount()
                 return amount
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+    }
+    private fun buyLotto(): Lotto{
+        val nums: List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Lotto(nums.sorted())
+    }
+
+    private fun getWinnigNums(): List<Int> {
+        var winnigNums: List<Int>
+        while(true) {
+            try{
+                outputview.winningMessage()
+                winnigNums = inputview.inputWinningNum()
+                return winnigNums
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+    }
+    private fun getBonusNum(winningNumbers: List<Int>): Int {
+        var bonusNumber: Int
+        while (true) {
+            try {
+                outputview.bonusMessage()
+                bonusNumber = inputview.inputBonusNum()
+                if (bonusNumber in winningNumbers)
+                    throw IllegalArgumentException("[Error] 보너스 번호는 당첨번호와 겹치지 않는 1부터 45사이의 숫자입니다.")
+                return bonusNumber
             } catch (e: IllegalArgumentException) {
                 println(e)
             }
