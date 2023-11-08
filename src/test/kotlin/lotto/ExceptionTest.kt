2b38f4d18773f0ac9f8ebda5739f0d7d0f569196
yeonghyeon <agilecatch@gmail.com>
2023-11-08 10:46:20 +0900

    [Test] : 보너스 번호에대한 테스트케이스 추가
    보너스 번호에대한 테스트 케이스를 추가하였습니다.

--- src/test/kotlin/lotto/ExceptionTest.kt
+++ src/test/kotlin/lotto/ExceptionTest.kt
@@ -1,77 +1,89 @@
 package lotto
 
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
-import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import lotto.Exception
 import org.junit.jupiter.api.assertThrows
 
 class ExceptionTest {
 
     @Test
-    fun `purchaseAmountEntryException 유효한 입력값이 들어왔을 때 DoesNotThrow`() {
-        // Arrange
+    fun `구입금액에 유효한 입력값이 들어왔을 때 DoesNotThrow`() {
         val input = "5000"
 
-        // Act and Assert
         assertDoesNotThrow { Exception.purchaseAmountEntryException(input) }
     }
 
     @Test
-    fun `purchaseAmountEntryException null 또는 빈 문자열이 들어왔을 때 예외처리`() {
-        // Arrange
+    fun `구입금액에 null 또는 빈 문자열이 들어왔을 때 예외처리`() {
         val input: String? = null
 
-        // Act and Assert
         assertThrows<IllegalArgumentException> { Exception.purchaseAmountEntryException(input) }
     }
 
     @Test
-    fun `purchaseAmountEntryException 숫자가 아닌 값이 들어왔을 때 예외처리`() {
-        // Arrange
+    fun `구입금액에 숫자가 아닌 값이 들어왔을 때 예외처리`() {
         val input = "abc"
 
-        // Act and Assert
         assertThrows<IllegalArgumentException> { Exception.purchaseAmountEntryException(input) }
     }
 
     @Test
-    fun `purchaseAmountEntryException 1,000원 단위가 아닌 금액이 들어왔을 때 예외처리`() {
-        // Arrange
+    fun `구입금액에 1,000원 단위가 아닌 금액이 들어왔을 때 예외처리`() {
         val input = "1500"
 
-        // Act and Assert
         assertThrows<IllegalArgumentException> { Exception.purchaseAmountEntryException(input) }
     }
 
     @Test
-    fun `enterWinningNumberException 유효한 입력값이 들어왔을 때 DoesNotThrow`() {
-        // Arrange
+    fun `당첨번호에 유효한 입력값이 들어왔을 때 DoesNotThrow`() {
         val input = "1, 2, 3, 4, 5, 6"
 
-        // Act and Assert
         assertDoesNotThrow { Exception.enterWinningNumberException(input) }
     }
 
     @Test
-    fun `enterWinningNumberException null 또는 빈 문자열이 들어왔을 때 예외처리`() {
-        // Arrange
+    fun `당첨번호에 null 또는 빈 문자열이 들어왔을 때 예외처리`() {
         val input: String? = null
 
-        // Act and Assert
         assertThrows<IllegalArgumentException> { Exception.enterWinningNumberException(input) }
     }
 
     @Test
-    fun `enterWinningNumberException 숫자가 아닌 값이 포함되어 있을 때 예외처리`() {
-        // Arrange
+    fun `당첨번호에 숫자가 아닌 값이 포함되어 있을 때 예외처리`() {
         val input = "1, 2, abc, 4, 5, 6"
 
-        // Act and Assert
         assertThrows<IllegalArgumentException> { Exception.enterWinningNumberException(input) }
     }
 
-    // 나머지 테스트 케이스 작성하기
+    @Test
+    fun `유효한 보너스 번호를 입력했을 때 정상적으로 반환하는지 확인`() {
+        val input = "10"
+        val winningNumbers = listOf(1, 2, 3, 4, 5)
 
-    // enterBonusNumberException에 대한 테스트도 작성해야 합니다.
+        val result = Exception.enterBonusNumberException(input, winningNumbers)
+
+        assertEquals(10, result)
+    }
+
+    @Test
+    fun `보너스 번호가 중복되었을 때 예외가 발생하는지 확인`() {
+        val input = "3"
+        val winningNumbers = listOf(1, 2, 3, 4, 5)
+
+        assertThrows<IllegalArgumentException> {
+            Exception.enterBonusNumberException(input, winningNumbers)
+        }
+    }
+
+    @Test
+    fun `보너스 번호에 1부터 45 이외의 번호가 입력되었을 때 예외가 발생하는지 확인`() {
+        val input = "50"
+        val winningNumbers = listOf(1, 2, 3, 4, 5)
+
+        assertThrows<IllegalArgumentException> {
+            Exception.enterBonusNumberException(input, winningNumbers)
+        }
+    }
+
 }
--- src/test/kotlin/lotto/LottoControllerTest.kt
+++ src/test/kotlin/lotto/LottoControllerTest.kt
@@ -10,20 +10,16 @@ import java.io.PrintStream
 class LottoControllerTest {
 
     @Test
-    fun `generateLottoTickets 구매 금액이 3000원일 때 정확한 로또 티켓이 생성되는지 확인`() {
-        // Arrange
+    fun `구매 금액이 3000원일 때 정확한 로또 티켓이 생성되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
         val purchaseAmount = 3000
 
-        // Mocking Lotto.generateLottoNumbers() 메서드
         `when`(lottoMock.generateLottoNumbers()).thenReturn(listOf(1, 2, 3, 4, 5, 6))
 
-        // Act
         val result = lottoController.generateLottoTickets(purchaseAmount)
 
-        // Assert
         assertEquals(3, result.size)
         assertEquals(listOf(1, 2, 3, 4, 5, 6), result[0].numbers)
         assertEquals(listOf(1, 2, 3, 4, 5, 6), result[1].numbers)
@@ -31,48 +27,39 @@ class LottoControllerTest {
     }
 
     @Test
-    fun `generateLottoTickets 구매 금액이 0일 때 빈 리스트가 반환되는지 확인`() {
-        // Arrange
+    fun `구매 금액이 0일 때 로또티켓이 빈 리스트가 반환되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
         val purchaseAmount = 0
 
-        // Act
         val result = lottoController.generateLottoTickets(purchaseAmount)
 
-        // Assert
         assertTrue(result.isEmpty())
     }
 
     @Test
-    fun `getWinningNumbersAndBonus 호출 시 뷰의 메서드가 올바르게 호출되는지 확인`() {
-        // Arrange
+    fun `보너스번호 호출 시 뷰의 메서드가 올바르게 호출되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
 
-        // Act
         lottoController.getWinningNumbersAndBonus()
 
-        // Assert
         verify(lottoViewMock).getWinningNumbersFromUser()
         verify(lottoViewMock).getBonusNumberFromUser(anyList())
     }
 
     @Test
-    fun `calculateWinnings 정확한 당첨 횟수가 반환되는지 확인`() {
-        // Arrange
+    fun `당첨번호가 정확한 횟수로 반환되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
 
         val matchCounts = listOf(3, 3, 3, 4, 4, 4, 4, 1, 2, 1, 6)
 
-        // Act
         val result = lottoController.calculateWinnings(matchCounts)
 
-        // Assert
         assertEquals(3, result[LottoRank.FIFTH])
         assertEquals(4, result[LottoRank.FOURTH])
         assertEquals(null, result[LottoRank.THIRD])
@@ -81,24 +68,20 @@ class LottoControllerTest {
     }
 
     @Test
-    fun `calculateWinnings 빈 리스트가 들어왔을 때 빈 맵이 반환되는지 확인`() {
-        // Arrange
+    fun `당첨번호가 빈 리스트가 들어왔을 때 빈 맵이 반환되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
 
         val matchCounts = emptyList<Int>()
 
-        // Act
         val result = lottoController.calculateWinnings(matchCounts)
 
-        // Assert
         assertTrue(result.isEmpty())
     }
 
     @Test
-    fun `calculateTotalProfitRate 정확한 출력이 되는지 확인`() {
-        // Arrange
+    fun `총수익률이 정확하게 출력 되는지 확인한다`() {
         val lottoViewMock = mock(LottoView::class.java)
         val lottoMock = mock(Lotto::class.java)
         val lottoController = LottoController(lottoViewMock, lottoMock)
@@ -113,24 +96,20 @@ class LottoControllerTest {
 
         val purchaseAmount = 8000
 
-        // Redirecting System.out to a ByteArrayOutputStream
         val outContent = ByteArrayOutputStream()
         System.setOut(PrintStream(outContent))
 
-        // Act
         lottoController.calculateTotalProfitRate(winnings, purchaseAmount)
 
-        // Restore System.out
         System.setOut(System.out)
 
-        // Assert
         val expectedOutput = (
                 "\n3개 일치 (5,000원) - 1개" +
-                "\n4개 일치 (50,000원) - 0개" +
-                "\n5개 일치 (1,500,000원) - 0개" +
-                "\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개" +
-                "\n6개 일치 (2,000,000,000원) - 0개" +
-                "\n총 수익률은 62.5%입니다.").trimIndent()
+                        "\n4개 일치 (50,000원) - 0개" +
+                        "\n5개 일치 (1,500,000원) - 0개" +
+                        "\n5개 일치, 보너스 볼 일치 (30,000,000원) - 0개" +
+                        "\n6개 일치 (2,000,000,000원) - 0개" +
+                        "\n총 수익률은 62.5%입니다.").trimIndent()
 
         assertEquals(expectedOutput, outContent.toString().trim())
     }
