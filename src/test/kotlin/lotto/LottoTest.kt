a7d27cce7f68a653225b3aed17f08e9983acf5dc
이빵주 <yungju9982@naver.com>
2023-11-08 23:04:58 +0900

    [test] 로또 번호에 동일한 숫자가 있을 경우와 없을 경우 테스트코드 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,5 +21,29 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호에 동일한 숫자가 있을 경우 true를 반환한다`() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(numbers)
+
+        // When
+        val result = lotto.contains(1)
+
+        // Then
+        assertTrue(result)
+    }
+
+    @Test
+    fun `로또 번호에 동일한 숫자가 있지 않을 경우 false를 반환한다`() {
+        // Given
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(numbers)
+
+        // When
+        val result = lotto.contains(7)
+
+        // Then
+        assertFalse(result)
+    }
 }
