d9aafef2dbe149bc14ac321ba81bc49b68de4417
w36495 <w36495@naver.com>
2023-11-08 17:29:01 +0900

    feat : 생성된 로또 번호에 대하여 예외 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,7 +2,12 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            "$ERROR_PREFIX 로또 번호는 총 6개여야 합니다."
+        }
+        require(numbers.distinct().size == 6) {
+            "$ERROR_PREFIX 중복된 숫자가 존재합니다."
+        }
     }
 
     fun matchNumbers(luckyNumbers: List<Int>): Int {
@@ -16,4 +21,8 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun hasBonusNumber(bonusNumber: Int): Boolean = numbers.contains(bonusNumber)
+
+    companion object {
+        private const val ERROR_PREFIX: String = "[ERROR]"
+    }
 }
