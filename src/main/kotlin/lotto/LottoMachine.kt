8abba2ff6dbbf79bdbb61ee0ee7a56cea59a89b2
이빵주 <yungju9982@naver.com>
2023-11-08 19:58:35 +0900

    [feat] LottoMachine에서 티켓을 사는 buyTickets 메서도 구현

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -4,8 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms
 import lotto.LottoConstants.LOTTO_NUMBER_MAX
 import lotto.LottoConstants.LOTTO_NUMBER_MIN
 import lotto.LottoConstants.LOTTO_NUMBER_SIZE
+import lotto.LottoConstants.LOTTO_PRICE
 
 object LottoMachine : LottoGenerator {
+
+    fun buyTickets(money: Int): List<Lotto> =
+        (1..money / LOTTO_PRICE).map { generate() }
 
     override fun generate(): Lotto {
         val numbers =
