f96c5a8bd1deff732fa0565bd4d87d0330079791
MinJun <agvber12@gmail.com>
2023-11-08 17:04:10 +0900

    feat: Lotto 번호가 범위 안에 있는지 체크

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,19 @@
 package lotto
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
         require(numbers.size == 6)
+        checkLottoNumber(numbers)
     }
 
-    // TODO: 추가 기능 구현
+    private fun checkLottoNumber(numbers: List<Int>) {
+        val lottoNumberRange = 1..45
+        numbers.forEach {
+            if (!lottoNumberRange.contains(it)) {
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            }
+        }
+    }
+
 }
