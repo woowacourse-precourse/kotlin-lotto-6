af4b6740b1f9736353008b69115699e18a4811f7
kkosang <87695921+kkosang@users.noreply.github.com>
2023-11-08 20:03:38 +0900

    test: 중복 검증하는 메서드 테스트

--- src/test/kotlin/lotto/BonusNumberTest.kt
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -34,4 +34,21 @@ class BonusNumberTest {
 
         assertThrows<IllegalArgumentException> { bonusNumber.isBonusNumberRange(55) }
     }
+
+    @Test
+    fun `중복 확인 메서드는 중복되지 않은 입력에 대해 예외를 던지지 않아야 함`() {
+        val bonusNumber = BonusNumber()
+        val winningNumber = listOf(1, 2, 3)
+
+        assertDoesNotThrow { bonusNumber.isBonusNumberDuplicate(4, winningNumber) }
+    }
+
+    @Test
+    fun `중복 확인 메서드는 중복된 입력에 대해 예외를 던져야 함`() {
+        val bonusNumber = BonusNumber()
+        val winningNumber = listOf(1, 2, 3)
+
+        assertThrows<IllegalArgumentException> { bonusNumber.isBonusNumberDuplicate(3, winningNumber) }
+    }
+
 }
