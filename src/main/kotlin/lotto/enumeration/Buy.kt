fc60c79b3e500f578051ce4d5c0b67ee63afcf2e
Jtility <leejewon97@gmail.com>
2023-11-08 18:32:43 +0900

    refactor(enumeration/Buy.kt): 클래스명 및 인스턴스명 변경
    PrintBuy -> Buy
    PRICE -> PRICE_INPUT

--- src/main/kotlin/lotto/enumeration/Buy.kt
+++ src/main/kotlin/lotto/enumeration/Buy.kt
@@ -1,7 +1,7 @@
 package lotto.enumeration
 
-enum class PrintBuy(val value: String) {
-    PRICE("구입금액을 입력해 주세요."),
+enum class Buy(val value: String) {
+    PRICE_INPUT("구입금액을 입력해 주세요."),
     ERROR_NOT_INTEGER("[ERROR] 구입 금액은 정수여야 합니다."),
     ERROR_NOT_THOUSAND("[ERROR] 구입 금액은 1000원 단위여야 합니다.")
 }
