722ab123343e9367df5ab9a4c52ca7682835ab84
moyeongmin <qa98589858@gmail.com>
2023-11-08 16:46:07 +0900

    로또 구입 금액을 받는 `lotto_pay` 메서드 구현 Lotto 클래스에 접근을 위해 companion object로 `createLottoNumber` 구현 로또 번호 생성을 위한 `Lottogenerator` 구현 생성된 로또 번호를 출력하는 `lotto_numbers_print` 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,46 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    val ammount = lotto_pay()
+    Lottogenerator(ammount)
+
 }
+fun lotto_pay() : Int {
+
+    while (true) {
+        try {
+            print("구입 금액을 입력해 주세요: ")
+            val input = Console.readLine()?.toInt() ?: throw IllegalArgumentException("[ERROR] 구입 금액을 입력해 주세요.")
+            if (input % 1000 != 0) {
+                throw IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.")
+            }
+            return input
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+fun Lottogenerator(ammount : Int) :List<Lotto>
+{
+    val lottoammount = ammount / 1000
+    val lottos = mutableListOf<Lotto>()
+    for(i in 1..lottoammount){
+        lottos.add(Lotto.createLottoNumber())
+    }
+
+    lotto_numbers_print(lottos)
+
+    return lottos
+}
+fun lotto_numbers_print( Lottos : List<Lotto>)
+{
+    println("${Lottos.size}개를 구매했습니다.")
+    for(lotto in Lottos)
+    {
+        val numbers = lotto.getlottonum().sorted()
+        println(numbers)
+    }
+}
+
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,22 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.all {it in 1..45 })
+    }
+    fun getlottonum(): List<Int> {
+        return numbers
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        fun createLottoNumber() : Lotto{
+            val randomNumber = Randoms.pickUniqueNumbersInRange(1,45,6)
+            return Lotto(randomNumber)
+        }
+
+    }
+
 }
