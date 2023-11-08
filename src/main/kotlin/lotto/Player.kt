669ca72fed52c9ab8b81ef2fd86c31511ea5f7f3
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-11-08 13:04:32 +0900

    [Feat] 예외 처리 : 입력한 구입 금액이 숫자가 아닐 경우

--- src/main/kotlin/lotto/Player.kt
+++ src/main/kotlin/lotto/Player.kt
@@ -2,26 +2,28 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 
-private const val LOTTO_UNIT = 1000
+private const val LOTTO_UNIT = 1_000
 
 class Player {
 
     fun inputPurchaseAmount(): Int {
-        val amount = Console.readLine().toInt()
+        val amount = Console.readLine()
         validatePurchaseAmount(amount)
-        return amount
+        return amount.toInt()
     }
 
     fun calculateLottoGenerateCount(amount: Int): Int {
         return amount / LOTTO_UNIT
     }
 
-    private fun validatePurchaseAmount(amount: Int) {
-        require(amount % LOTTO_UNIT == 0) { throw IllegalArgumentException(PURCHASE_AMOUNT_UNIT_ERROR) }
+    private fun validatePurchaseAmount(amount: String) {
+        val purchaseAmount = amount.toIntOrNull() ?: throw IllegalArgumentException(INPUT_TYPE_ERROR)
+        require(purchaseAmount % LOTTO_UNIT == 0) { throw IllegalArgumentException(PURCHASE_AMOUNT_UNIT_ERROR) }
     }
 
     companion object {
-        const val PURCHASE_AMOUNT_UNIT_ERROR = "[ERROR] 1,000원 단위로 입력해야 합니다."
+        const val PURCHASE_AMOUNT_UNIT_ERROR = "[ERROR] 1000원 단위로 입력해야 합니다."
+        const val INPUT_TYPE_ERROR = "[ERROR] 숫자로 입력해야 합니다."
     }
 
 }
