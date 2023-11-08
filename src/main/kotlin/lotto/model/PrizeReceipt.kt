a707dde711484c3317a1031db79f34b2202bd463
sseung416 <choiseungyeon416@gmail.com>
2023-11-08 14:50:50 +0900

    refactor: PrizeReceipt 상수화

--- src/main/kotlin/lotto/model/PrizeReceipt.kt
+++ src/main/kotlin/lotto/model/PrizeReceipt.kt
@@ -16,7 +16,7 @@ class PrizeReceipt(private val cost: Money) {
 
     private fun getRateResult(): Double {
         val sum = getPrizeSum()
-        if (sum == 0) return 0.0
+        if (sum == NO_WINNING_NUMBERS) return 0.0
         return 100 + ((sum - cost.value) / cost.value.toDouble()) * 100
     }
 
@@ -25,5 +25,6 @@ class PrizeReceipt(private val cost: Money) {
 
     companion object {
         private const val INITIAL_COUNT = 0
+        private const val NO_WINNING_NUMBERS = 0
     }
 }
