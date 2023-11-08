df6842a61044eca86985159b3dffa7ed49a260fb
wnsgml517 <wnsgml517@naver.com>
2023-11-08 15:05:57 +0900

    "read me. 기능 요구 사항 변경"

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,31 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해 주세요.")
+
+    try {
+        val purchaseAmount = getValidPurchaseAmount()
+        println("로또 구입 금액: $purchaseAmount 원")
+
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+}
+
+fun getValidPurchaseAmount(): Int {
+    try {
+        val input = Console.readLine()
+        val purchaseAmount = input.toInt()
+
+        if (purchaseAmount % 1000 != 0) {
+            throw IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.")
+        }
+
+        return purchaseAmount
+    } catch (e: NumberFormatException) {
+        println("[ERROR] 유효하지 않은 숫자를 입력했습니다. 다시 입력하세요.")
+    }
+    return 0
 }
