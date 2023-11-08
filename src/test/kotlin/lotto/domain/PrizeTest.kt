cd47a4e88a7aec58899ec9832118ae4b77cfe95f
tgyuu-An <atg0614@gmail.com>
2023-11-08 17:27:40 +0900

    test : prize 출력 테스트 코드 추가

--- src/test/kotlin/lotto/domain/PrizeTest.kt
+++ src/test/kotlin/lotto/domain/PrizeTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -17,14 +18,15 @@ class PrizeTest{
     }
 
     @Test
-    fun `상금을 출력하면 세 자리마다 `(){
+    fun `상금을 출력할 때에는 화폐 단위로 출력된다`(){
         //given
+        val prize = Prize(2_000_000_000)
 
         //when
 
         //then
-        assertThrows<IllegalArgumentException>{
-            val prize = Prize(-1)
-        }
+        val actual = prize.toString()
+        val expected = "2,000,000,000원"
+        assertThat(actual).isEqualTo(expected)
     }
 }
