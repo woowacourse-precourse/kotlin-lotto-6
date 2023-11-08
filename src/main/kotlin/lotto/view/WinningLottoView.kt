c1ae303d5b56e1d82c7191d74a5ed33ba347b538
Wonhui Roh <rwhui012@gmail.com>
2023-11-08 20:23:32 +0900

    feat(lotto): 당첨 번호(6개) 입력 받는 기능

--- src/main/kotlin/lotto/view/WinningLottoView.kt
+++ src/main/kotlin/lotto/view/WinningLottoView.kt
@@ -1,14 +1,29 @@
 package lotto.view
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.constants.GuideMessage.ENTER_BONUS_NUMBER
 import lotto.constants.GuideMessage.ENTER_WINNING_NUMBERS
+import lotto.utils.WinningNumbersExceptionHandler
+import lotto.utils.WinningNumbersSeparator
 
-class WinningLottoView {
+object WinningLottoView {
   fun printWinningNumbersInputMessage() {
     println(ENTER_WINNING_NUMBERS)
   }
 
-  fun inputWinningNumbers() {
+  fun inputWinningNumbers(): List<Int> {
+    val inputWinningNumbers: String = Console.readLine()
+    val separatedWinningNumbers = WinningNumbersSeparator.separateWinningNumbers(inputWinningNumbers)
+    var winningNumbers: List<Int> = listOf()
 
+    try {
+      winningNumbers = WinningNumbersExceptionHandler.validateWinningNumbers(separatedWinningNumbers)
+    } catch (e: IllegalArgumentException) {
+      ExceptionView.printExceptionMessage(e)
+      inputWinningNumbers()
+    }
+
+    return winningNumbers
   }
 
   fun printBonusNumberInputMessage() {
