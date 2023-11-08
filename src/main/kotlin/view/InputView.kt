b37070e420b573b39b3c9ec5fb9008531e13b3f3
lee989898 <wnsrud1352@gmail.com>
2023-11-08 16:49:32 +0900

    [feat] 당첨 번호 입력 메서드 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,6 +1,7 @@
 package view
 
 import camp.nextstep.edu.missionutils.Console
+import validator.lottonumbervalidator.LottoNumberValidator
 import validator.purchaseamountvalidator.PurchaseAmountValidator
 
 object InputView {
@@ -15,5 +16,17 @@ object InputView {
         }
 
         return lottoPurchaseAmount.toInt()
+    }
+
+    fun lottoNumber(): List<Int> {
+        val lottoNumber = Console.readLine()
+
+        try {
+            LottoNumberValidator.isAppropriateLottoNumber(lottoNumber)
+        } catch (e: IllegalArgumentException) {
+            return lottoNumber()
+        }
+
+        return lottoNumber.split(",").map { it.toInt() }
     }
 }
