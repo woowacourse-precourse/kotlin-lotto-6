c189256c8500f11a0faab43a68f3f16dde9ffac4
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2023-11-08 22:26:00 +0900

    feat: 구매 갯수 출력

--- src/main/kotlin/lotto/utils/ConsoleUtils.kt
+++ src/main/kotlin/lotto/utils/ConsoleUtils.kt
@@ -2,10 +2,17 @@ package lotto.utils
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.Constants.Companion.INPUT_PURCHASE_AMOUNT_MESSAGE
+import lotto.Constants.Companion.OUTPUT_PURCHASE_COUNT_MESSAGE
 class ConsoleUtils {
     //구매 금액 입력
      fun inputPurchaseAmountMessage(): String{
          print(INPUT_PURCHASE_AMOUNT_MESSAGE)
         return Console.readLine()
      }
+
+    //구매 갯수 출력
+    fun outputPurchaseCountMessage(): String{
+        print(OUTPUT_PURCHASE_COUNT_MESSAGE)
+        return Console.readLine()
+    }
 }
