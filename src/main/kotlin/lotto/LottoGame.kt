2c303b54918c62a74bde2bc27f4725de58614c35
iiolo <alohalo98@naver.com>
2023-11-08 22:23:48 +0900

    refactor: 출력문을 Output class에서 관리하도록 변경

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -2,25 +2,24 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.view.Input
+import lotto.view.Output
 
 class LottoGame {
-    var threeMatch = 0
-    var fourMatch = 0
-    var fiveMatch = 0
-    var fiveBonusMatch = 0
-    var sixMatch = 0
+    private var matchCountList = mutableListOf(0,0,0,0,0)
 
     fun start() {
         val money = Input().inputMoney().toInt()
         val lottoCount = getLottoCount(money)
         val lottoList = getLotto(lottoCount)
-        lottoList.forEach { println(it) }
+
+        Output().outputLottos(lottoList)
+
         val winningNum = Input().inputWinningNum()
         val bonusNum = Input().inputBonusNum()
 
         lottoList.forEach { checkLottoAndWinningNumMatch(it,winningNum,bonusNum) }
 
-        printMatchCount()
+        Output().outputMatchCount(matchCountList)
 
     }
 
@@ -28,8 +27,6 @@ class LottoGame {
     private fun getLottoCount(money: Int): Int = money / 1000
 
     private fun getLotto(lottoCount: Int): List<List<Int>> {
-        println()
-        println("${lottoCount}개를 구매했습니다.")
         val lottoList = List(lottoCount) {
             Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
         }
@@ -41,20 +38,12 @@ class LottoGame {
         val winningNumList = winningNum.split(",").map { it.toInt() }.toSet()
         val bonusNumToInt = bonusNum.toInt()
         val match = lotto.intersect(winningNumList).size
-        when (match) {
-            3 -> threeMatch++
-            4 -> fourMatch++
-            5 -> if (lotto.contains(bonusNumToInt)) fiveBonusMatch++ else fiveMatch++
-            6 -> sixMatch++
-        }
-    }
 
-    private fun printMatchCount() {
-        println()
-        println("3개 일치 (5,000원) - ${threeMatch}개")
-        println("4개 일치 (50,000원) - ${fourMatch}개")
-        println("5개 일치 (1,500,000원) - ${fiveMatch}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${fiveBonusMatch}개")
-        println("6개 일치 (2,000,000,000원) - ${sixMatch}개")
+        when (match) {
+            3 -> matchCountList[0]++
+            4 -> matchCountList[1]++
+            5 -> if (lotto.contains(bonusNumToInt)) matchCountList[3]++ else matchCountList[2]++
+            6 -> matchCountList[4]++
+        }
     }
 }
---
+++ src/main/kotlin/lotto/view/Output.kt
@@ -0,0 +1,20 @@
+package lotto.view
+
+class Output {
+
+    fun outputLottos(lottoList: List<List<Int>>) {
+        println()
+        println("${lottoList.size}개를 구매했습니다.")
+        lottoList.forEach { println(it) }
+    }
+
+    fun outputMatchCount(matchCountList: MutableList<Int>) {
+        println()
+        println("3개 일치 (5,000원) - ${matchCountList[0]}개")
+        println("4개 일치 (50,000원) - ${matchCountList[1]}개")
+        println("5개 일치 (1,500,000원) - ${matchCountList[2]}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchCountList[3]}개")
+        println("6개 일치 (2,000,000,000원) - ${matchCountList[4]}개")
+    }
+}
+
