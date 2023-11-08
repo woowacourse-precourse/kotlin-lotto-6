c369b7bcac97ecb92f198f4068aae1b3522500e0
MinhoiKoo <minlove2013@gmail.com>
2023-11-08 16:44:42 +0900

    refactor: 플레이어가 잘못된 값을 입력할 경우 다시 입력받는 로직 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,18 +1,47 @@
 package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.domain.LottoNumber
+import lotto.domain.WinningLotto
+import lotto.utils.ExceptionHandler
 
 class InputView {
 
     fun inputMoney(): Int {
-        return Console.readLine().toInt()
+        while (true) {
+            try {
+                val money = Console.readLine().trim()
+                ExceptionHandler.isBlank(money)
+                ExceptionHandler.isDigit(money)
+                return money.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
-    fun inputWinningNumbers(): List<Int> {
-        return Console.readLine().split(",").map { it.toInt() }
+    fun inputWinningNumbers(): WinningLotto {
+        while (true) {
+            try {
+                val winningNumbers = Console.readLine()
+                ExceptionHandler.winningNumbersIsDigit(winningNumbers)
+                val winningLotto = WinningLotto(winningNumbers.split(",").map { LottoNumber(it.toInt()) })
+                return winningLotto
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
     fun inputBonusNumber(): Int {
-        return Console.readLine().toInt()
+        while (true) {
+            try {
+                val number = Console.readLine()
+                ExceptionHandler.isDigit(number)
+                return number.toInt()
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 }
