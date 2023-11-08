527fd1771930c7da2fb331db483ad3702e728369
somin <salth0601@gmail.com>
2023-11-08 16:59:06 +0900

    test: MoneyTest 추가

---
+++ src/test/kotlin/lotto/MoneyTeset.kt
@@ -0,0 +1,21 @@
+package lotto
+
+import lotto.model.domain.Money
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class MoneyTest {
+    @Test
+    fun `금액이 0 이하이면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Money(0)
+        }
+    }
+
+    @Test
+    fun `금액이 1000 단위가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Money(999)
+        }
+    }
+}
