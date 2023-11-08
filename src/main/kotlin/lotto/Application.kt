f5959ab2c95006b3190991959601085c2012c175
Bon Kim <qhsl0615@naver.com>
2023-11-08 20:57:40 +0900

    [feat] butLotto()

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    val lotto_tickets = buyLotto()
+    print(lotto_tickets)
+}
+
+fun buyLotto(): Int {
+    println("구입금액을 입력해 주세요.")
+    val price = readLine()!!.toInt()
+    if (price % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 로또 구매는 1000원 단위로만 가능합니다.")
+    } else if (price == null) {
+        throw IllegalArgumentException("[ERROR] 구입금액을 입력해주세요.")
+    } else return price / 1000
 }
