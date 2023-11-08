8fbca5ada1b550feb65f4e8780625544402755b2
lee989898 <wnsrud1352@gmail.com>
2023-11-08 17:41:13 +0900

    [feat] 소수점 첫째 자리가 아니라 둘째 자리에서 반올림하게 변경

--- src/main/kotlin/lottoreturns/LottoReturnsImpl.kt
+++ src/main/kotlin/lottoreturns/LottoReturnsImpl.kt
@@ -6,12 +6,18 @@ import kotlin.math.round
 
 class LottoReturnsImpl : LottoReturns {
     override fun calculate(lottoPurchaseAmount: LottoPurchaseAmount): Double {
-        var totalWinningAmount = 0.0
+        var totalWinningAmount = ZERO
 
         LottoRanking.entries.forEach {
             totalWinningAmount += it.count * it.reward
         }
 
-        return round((totalWinningAmount * 100) / lottoPurchaseAmount.money)
+        return round((totalWinningAmount * THOUSAND) / lottoPurchaseAmount.money) / TEN
+    }
+
+    companion object {
+        private const val ZERO = 0.0
+        private const val THOUSAND = 1_000
+        private const val TEN = 10.0
     }
 }
