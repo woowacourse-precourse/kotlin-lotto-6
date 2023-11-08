ff3d41a0db68410010f077ad71b448a5eed9426b
lee989898 <wnsrud1352@gmail.com>
2023-11-08 13:35:52 +0900

    [feat] 로또 구입 금액이 수로 이루어져 있는지 확인하는 로직 추가

--- src/main/kotlin/validator/purchaseamountvalidator/PurchaseAmountValidator.kt
+++ src/main/kotlin/validator/purchaseamountvalidator/PurchaseAmountValidator.kt
@@ -4,9 +4,15 @@ import view.OutputView
 
 object PurchaseAmountValidator {
     private const val PURCHASE_AMOUNT_ERROR_MESSAGE = "로또 구입 금액은 1,000 단위여야 합니다."
+    private const val PURCHASE_AMOUNT_TYPE_ERROR_MESSAGE = "로또 구입 금액은 수여야 합니다."
+    private const val LOTTO_AMOUNT_UNIT = 1000
 
-    fun isAppropriatePurchaseAmount(purchaseAmount: Int): Boolean {
-        require(purchaseAmount % 1000 == 0) { OutputView.error(PURCHASE_AMOUNT_ERROR_MESSAGE) }
+    fun isAppropriatePurchaseAmount(purchaseAmount: String): Boolean {
+        val result = purchaseAmount.toIntOrNull()
+        requireNotNull(result) { OutputView.error(PURCHASE_AMOUNT_TYPE_ERROR_MESSAGE) }
+        require(result % LOTTO_AMOUNT_UNIT == 0 && result >= LOTTO_AMOUNT_UNIT) {
+            OutputView.error(PURCHASE_AMOUNT_ERROR_MESSAGE)
+        }
         return true
     }
 }
