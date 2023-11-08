2d207bb8b4eac7c2e1d1da6a80bb2ade588f6823
정채훈 <mrchjung@peopleinnet.com>
2023-11-08 20:55:55 +0900

    feat(domain): 중복된 로또 번호 입력에 대한 예외 처리

--- src/main/kotlin/lotto/model/domain/Lotto.kt
+++ src/main/kotlin/lotto/model/domain/Lotto.kt
@@ -3,7 +3,6 @@ package lotto.model.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == numbers.size)
     }
-
-    // TODO: 추가 기능 구현
 }
