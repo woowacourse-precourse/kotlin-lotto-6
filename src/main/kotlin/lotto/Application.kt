fbf848a2570c8384579a509b7c8c8aa28a50b408
yuhyun <dlugus@naver.com>
2023-11-08 21:18:45 +0900

    feat: 로또번호+보너스번호 추첨

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,9 +7,27 @@ fun main() {
     TODO("프로그램 구현")
 }
 
-fun userInputMoney() {
+fun getUserMoney() {
     print("구입금액을 입력해 주세요.")
     val userInput = Console.readLine()
-    
+
     return userInput
+}
+ 
+fun getLottoNumber() {
+    print("당첨 번호를 입력해 주세요.")
+    val userInput = Console.readline()
+
+    return userInput
+}
+
+fun getBonusNumber() {
+    print("보너스 번호를 입력해 주세요.")
+    val userInput = Console.readline()
+
+    return userInput
+}
+
+fun makeLottoNumber() {
+    var lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6)
 }
