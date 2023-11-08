82ac0de24b30a4d1c757bd9f1588369a69c15f3f
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-08 13:48:19 +0900

    Refactor: 로또 번호 출력 기능 return 값 추가

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -4,13 +4,19 @@ class LottoMachine(private val amount: String) {
 
     private val outputView = OutputView()
     private val lottoGenerator = LottoGenerator()
+    private var allLotto = mutableListOf<List<Int>>()
 
     fun calculateLottoQuantity(): Int {
         return amount.toInt() / PRICE_LOTTO
     }
 
-    fun printNumbers(quantity: Int) {
-        repeat(quantity) { outputView.printLottoNumbers(lottoGenerator.generateLotto()) }
+    fun printNumbers(quantity: Int): List<List<Int>> {
+        repeat(quantity) {
+            val numbers = lottoGenerator.generateLotto()
+            allLotto.add(numbers)
+            outputView.printLottoNumbers(numbers)
+        }
+        return allLotto
     }
 
     companion object {
