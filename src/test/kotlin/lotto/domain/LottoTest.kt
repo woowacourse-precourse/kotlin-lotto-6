38178f52207ed1a6aca2df1ad14b5e35792c09a0
jun <hky08067@naver.com>
2023-11-08 10:44:49 +0900

    test: 로또 번호 매칭 결과 테스트

---
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -0,0 +1,101 @@
+package lotto.domain
+
+import lotto.model.LottoWinnerInfo
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+
+class LottoTest {
+
+    companion object {
+        private lateinit var lotto: Lotto
+
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            lotto = Lotto(numbers)
+        }
+    }
+
+    @Test
+    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+    @Test
+    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 하나일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 7, 8, 9, 10, 11),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isNull()
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 둘일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 8, 9, 10, 11),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isNull()
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 셋일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 3, 9, 10, 11),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isEqualTo(LottoWinnerInfo.MatchThree)
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 넷일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 3, 4, 10, 11),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isEqualTo(LottoWinnerInfo.MatchFour)
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 다섯일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 3, 4, 5, 11),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isEqualTo(LottoWinnerInfo.MatchFive)
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 다섯이고 보너스 숫자가 있을 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 3, 4, 5, 11),
+            bonusNumber = 6
+        )
+        Assertions.assertThat(received).isEqualTo(LottoWinnerInfo.MatchFiveWithBonus)
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 여섯일 때 `() {
+        val received = lotto.matchingLotto(
+            winningNumbers = listOf(1, 2, 3, 4, 5, 6),
+            bonusNumber = 12
+        )
+        Assertions.assertThat(received).isEqualTo(LottoWinnerInfo.MatchSix)
+    }
+}
