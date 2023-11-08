6114b9222ef3580cbc7df0101e799daebe75b2f9
lee989898 <wnsrud1352@gmail.com>
2023-11-08 16:49:05 +0900

    [feat] 로또 구입 금액 입력 메서드 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,4 +1,19 @@
 package view
 
+import camp.nextstep.edu.missionutils.Console
+import validator.purchaseamountvalidator.PurchaseAmountValidator
+
 object InputView {
+
+    fun lottoPurchaseAmount(): Int {
+        val lottoPurchaseAmount = Console.readLine()
+
+        try {
+            PurchaseAmountValidator.isAppropriatePurchaseAmount(lottoPurchaseAmount)
+        } catch (e: IllegalArgumentException) {
+            return lottoPurchaseAmount()
+        }
+
+        return lottoPurchaseAmount.toInt()
+    }
 }
