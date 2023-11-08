e9712ed339f1770a1ffa5bce0789028a487a789d
Namyunsuk <yunsuk0328@naver.com>
2023-11-08 11:37:36 +0900

    feat: 예외처리 기능 구현

--- src/main/kotlin/lotto/utils/UserInputException.kt
+++ src/main/kotlin/lotto/utils/UserInputException.kt
@@ -1,7 +1,37 @@
 package lotto.utils
 
+import lotto.domain.Lotto
+
 class UserInputException {
     companion object {
+        private const val PICK_NUMBER = 6
+        private const val MIN_NUMBER = 1
+        private const val MAX_NUMBER = 45
+
+        fun purchaseAmountException(userInput: String): Int {
+            require(isNumber(userInput)) { "[ERROR] 입력 값은 숫자여야 합니다." }
+            require(userInput.toInt() % 1000 == 0) { "[ERROR] 구입 금액은 1,000원 단위여야 합니다." }
+            return userInput.toInt()
+        }
+
+        fun lottoException(userInput: String): List<Int> {
+            val lottoList = userInput.split(",")
+            require(lottoSizeException(lottoList)) { "[ERROR] 로또 번호의 수는 6개여야 합니다." }
+            require(lottoNumberException(lottoList)) { "[ERROR] 로또 번호는 숫자여야 합니다." }
+            val lottoNumberList = lottoList.map { it.toInt() }
+            require(lottoNumberDuplicateException(lottoNumberList)) { "[ERROR] 로또 번호는 중복된 값이 있으면 안됩니다." }
+            require(lottoNumberOutOfRangeException(lottoNumberList)) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
+
+            return lottoNumberList
+        }
+
+        fun bonusNumberException(userInput: String): Int {
+            require(isNumber(userInput)) { "[ERROR] 입력 값은 숫자여야 합니다." }
+            val bonusNumber = userInput.toInt()
+            require(bonusNumberOutOfRange(bonusNumber)) { "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다." }
+
+            return bonusNumber
+        }
 
         fun isNumber(userInput: String): Boolean {
             for (numberIndex in 0 until userInput.length) {
@@ -12,10 +42,24 @@ class UserInputException {
             return true
         }
 
-        fun purchaseAmountException(userInput: String):Int {
-            require(isNumber(userInput)) { "[ERROR] 입력 값은 숫자여야 합니다." }
-            require(userInput.toInt() % 1000 == 0) { "[ERROR] 구입 금액은 1,000원 단위여야 합니다." }
-            return userInput.toInt()
+        fun lottoSizeException(lottoList: List<String>): Boolean {
+            return lottoList.size == PICK_NUMBER
+        }
+
+        fun lottoNumberException(lottoNumberList: List<String>): Boolean {
+            return lottoNumberList.all { it.toIntOrNull() != null }
+        }
+
+        fun lottoNumberDuplicateException(lottoNumberList: List<Int>): Boolean {
+            return lottoNumberList.size == lottoNumberList.distinct().size
+        }
+
+        fun lottoNumberOutOfRangeException(lottoNumberList: List<Int>): Boolean {
+            return lottoNumberList.all { it in MIN_NUMBER..MAX_NUMBER }
+        }
+
+        fun bonusNumberOutOfRange(bonusNumber: Int): Boolean {
+            return (bonusNumber in MIN_NUMBER..MAX_NUMBER)
         }
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,34 +2,39 @@ package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.utils.UserInputException
-import java.lang.IllegalArgumentException
+import kotlin.IllegalArgumentException
 
 class InputView {
     fun inputPurchaseAmount(): Int {
         while (true) {
             val purchaseAmount = Console.readLine()
             try {
-                return UserInputException.purchaseAmountException(purchaseAmount) ?: continue
-            }catch (e: IllegalArgumentException){
+                return UserInputException.purchaseAmountException(purchaseAmount)
+            } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
-
         }
-
     }
 
     fun inputPrizeLottoNumber(): List<Int> {
-        val prizeLottoNumber = Console.readLine()
-        val prizeLottoNumberList = prizeLottoNumber.split(SEPARATOR)
-        return prizeLottoNumberList.map { it.toInt() }
+        while (true) {
+            val prizeLottoNumber = Console.readLine()
+            try {
+                return UserInputException.lottoException(prizeLottoNumber)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 
     fun inputPrizeBonusNumber(): Int {
-        val bonusNumber = Console.readLine()
-        return bonusNumber.toInt()
-    }
-
-    companion object {
-        private const val SEPARATOR = ","
+        while (true) {
+            val bonusNumber = Console.readLine()
+            try {
+                return UserInputException.bonusNumberException(bonusNumber)
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
 }
