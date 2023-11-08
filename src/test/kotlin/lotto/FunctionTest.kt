065e8bd1c3b5f024d60e659dbc11e951c67f3cb6
조형석 <chs9902@naver.com>
2023-11-08 14:01:41 +0900

    test: 로또 판별 업데이트 메소드 테스트

--- src/test/kotlin/lotto/FunctionTest.kt
+++ src/test/kotlin/lotto/FunctionTest.kt
@@ -16,7 +16,16 @@ class FunctionTest {
             listOf(1, 2, 3, 4, 5, 7)
         )
         val result = Jackpot().discriminate(answers, bonus, lotto)
-        val expectedRank = listOf(0,0,0,1,1)
+        val expectedRank = listOf(0, 0, 0, 1, 1)
         Assertions.assertEquals(expectedRank, result)
+    }
+
+    @Test
+    fun testRankUpdate() {
+        val initialRank = mutableListOf(0, 0, 0, 0, 0)
+
+        val rank4 = Jackpot().rankUpdate(5, initialRank, false)
+        Assertions.assertIterableEquals(listOf(0, 0, 1, 0, 0), rank4)
+
     }
 }
