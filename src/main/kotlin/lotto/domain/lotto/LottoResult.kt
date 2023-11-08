bf5eb9634800c256c2c8b2992cbf22e2fea1672c
YuBeen-Park <park_ubin@naver.com>
2023-11-08 17:24:50 +0900

    mod(LottoResult, LottoWallet) : return 구문 삭제

--- src/main/kotlin/lotto/domain/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/domain/lotto/LottoResult.kt
@@ -6,14 +6,12 @@ enum class LottoResult(val count: Int, val prize: Double) {
     THREE_MATCH(3, 5000.0),
     FOUR_MATCH(4, 50000.0),
     FIVE_MATCH(5, 1500000.0) {
-        override fun isSame(matchCount: Int, matchBonus: Boolean): Boolean {
-            return super.isSame(matchCount, matchBonus) && !matchBonus
-        }
+        override fun isSame(matchCount: Int, matchBonus: Boolean): Boolean =
+            super.isSame(matchCount, matchBonus) && !matchBonus
     },
     FIVE_WITH_BONUS_MATCH(5, 30000000.0) {
-        override fun toMessage(): String {
-            return "${count}개 일치, 보너스 볼 일치 (${applyPriceFormat(prize.toInt())}) - "
-        }
+        override fun toMessage(): String =
+            "${count}개 일치, 보너스 볼 일치 (${applyPriceFormat(prize.toInt())}) - "
     },
     SIX_MATCH(6, 2000000000.0),
     NONE(0, 0.0);
@@ -23,10 +21,9 @@ enum class LottoResult(val count: Int, val prize: Double) {
     open fun toMessage(): String = "${count}개 일치 (${applyPriceFormat(prize.toInt())}) - "
 
     companion object {
-        fun valueOf(matchCount: Int, matchBonus: Boolean): LottoResult {
-            return values().find {
+        fun valueOf(matchCount: Int, matchBonus: Boolean): LottoResult =
+            values().find {
                 it.isSame(matchCount, matchBonus)
             } ?: NONE
-        }
     }
 }
--- src/main/kotlin/lotto/domain/lotto/LottoWallet.kt
+++ src/main/kotlin/lotto/domain/lotto/LottoWallet.kt
@@ -6,11 +6,10 @@ import lotto.view.OutputManager.PURCHASED_LOTTO_AMOUNT_MSG
 class LottoWallet(private val lotteries: List<Lotto>) {
     val lottoCount = lotteries.size
 
-    fun compareWithWinningLotto(winningLotto: WinningLotto): List<LottoResult> {
-        return lotteries.map { lotto ->
+    fun compareWithWinningLotto(winningLotto: WinningLotto): List<LottoResult> =
+        lotteries.map { lotto ->
             winningLotto.checkWinning(lotto)
         }
-    }
 
     override fun toString(): String {
         var message = "\n${lotteries.size}$PURCHASED_LOTTO_AMOUNT_MSG\n"
