1cf4da023d8da871288df90e6fed66fb0fb6f06d
yoon <cbj0010@naver.com>
2023-11-08 13:28:49 +0900

    [Feat] 로또 생성 조건 체크
    당첨번호가 6개 일때, 중복숫자가 없을 때, 로또번호 오름차순 정렬, 조건을 지키지 못했을 때 다시 입력받을 수 있도록

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,28 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == DEFAULT_LOTTO_SIZE)
     }
 
-    // TODO: 추가 기능 구현
+    fun checkLottoNumberException(): List<Int> {
+//중복숫자 체크 및 6자리 확인
+        if (numbers.size != 6)  throw IllegalArgumentException(ERROR_INPUT_NUMBER_LENGTH)
+        if (numbers.distinct().size != 6) throw IllegalArgumentException(ERROR_INPUT_NUMBER_DISTINCT)
+        return numbers.sorted()
+    }
+   private fun isAllNumbersInRange(list: List<Int>): Boolean {
+        for (number in list) {
+            if (number < 1 || number > 45) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+    companion object {
+        private const val DEFAULT_LOTTO_SIZE = 6
+        private const val ERROR_INPUT_NUMBER_LENGTH = "[ERROR] 6개의 숫자만 입력 가능합니다"
+        private const val ERROR_INPUT_NUMBER_DISTINCT = "수 중에 중복이 있습니다."
+    }
 }
--- src/main/kotlin/lotto/LottoSeller.kt
+++ src/main/kotlin/lotto/LottoSeller.kt
@@ -2,31 +2,61 @@ package lotto
 
 private const val LOTTO_TICKET_PRICE = 1000
 private const val LOTTO_TICKET_REMAINDER = 0
-private const val EROOR_INPUT_MONEY = "로또 한장은 1,000원 입니다. 다시 입력해주세요"
+private const val ERROR_INPUT_MONEY = "로또 한장은 1,000원 입니다. 다시 입력해주세요"
 
-class LottoSeller(private val lottoMoney: Int) {
-//랜덤수에 맞게 로또를 판매하는 곳, 돈을 받은 만큼 줬지?
+class LottoSeller() {
+    //랜덤수에 맞게 로또를 판매하는 곳, 돈을 받은 만큼 줬지?
     //what/who 계속 생각하기!
     //어떤 행위가 필요한가! 이 행위를 수행할 객체는 어디인가!
-
-    fun generateLottoNumbers(): List<LottoNumberGenerator> {
+    private var inputLottoMoney = 0
+    fun generateLottoNumbers(lottoMoney: Int): List<LottoNumberGenerator> {
         //고객이 요구한 만큼 받아서 한번에 주는 거
         val result = mutableListOf<LottoNumberGenerator>()
+        inputLottoMoney = lottoMoney
         repeat(checkLottoTicketCount()) {
             result.add(LottoNumberGenerator())
         }
         return result
     }
 
-    private fun createLotto(): List<Int> {
-        return LottoNumberGenerator().generate()
+    //야 이거 잘못된 당첨번호 로또잖아!
+    //다시 적어서 내렴
+    fun isValidateLotto() {
+        try {
+            Lotto(User().inputLottoNumber()).checkLottoNumberException()
+        } catch (e: Exception) {
+            checkExceptionStatement(e)
+            LottoSeller().isValidateLotto()
+        }
+    }
+
+    private fun checkExceptionStatement(e: Exception) {
+        when (e.message) {
+            ERROR_INPUT_NUM_LENGTH -> println(ERROR_INPUT_NUM_LENGTH_PRINT)
+            ERROR_INPUT_NUM -> println(ERROR_INPUT_NUM_PRINT)
+            else -> println(ERROR_TRY_AGAIN_NUM_PRINT)
+        }
+    }
+
+    //미쳐버려 이거 잘못된 보너스 번호잖아!
+    //당첨번호랑 안겹치는 걸로 다시 적어서 내렴
+    fun checkLottoHasBonusNum() {
+
     }
 
     private fun checkLottoTicketCount(): Int {   //x원 받았는데 로또 몇장이냐?에 대한 대답
         //가격을 파는 사람이 알지 사는 사람이 우째 안대유
-        if (lottoMoney % LOTTO_TICKET_PRICE != LOTTO_TICKET_REMAINDER) {
-            throw IllegalArgumentException(EROOR_INPUT_MONEY + "돈을 입력받는 곳으로 다시 가야됨")
+        if (inputLottoMoney % LOTTO_TICKET_PRICE != LOTTO_TICKET_REMAINDER) {
+            throw IllegalArgumentException(ERROR_INPUT_MONEY)
         }
-        return lottoMoney / LOTTO_TICKET_PRICE
+        return inputLottoMoney / LOTTO_TICKET_PRICE
+    }
+
+    companion object {
+        const val ERROR_INPUT_NUM_LENGTH = "For input string: \"\""
+        const val ERROR_INPUT_NUM_LENGTH_PRINT = "[ERROR]번호 개수를 체크해주세요"
+        const val ERROR_INPUT_NUM = "Failed requirement."
+        const val ERROR_INPUT_NUM_PRINT = "[ERROR]잘못된 번호를 입력했습니다."
+        const val ERROR_TRY_AGAIN_NUM_PRINT = "[ERROR]당첨번호를 다시 입력해주세요"
     }
 }
