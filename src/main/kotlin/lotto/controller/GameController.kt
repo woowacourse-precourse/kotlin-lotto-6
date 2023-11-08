9e43f269296901d6c4154970eade7f1794eb8979
sh1mj1 <7wlgns@gmail.com>
2023-11-08 21:02:26 +0900

    feat(GameController): divide the monster method into small methods.

--- src/main/kotlin/lotto/controller/GameController.kt
+++ src/main/kotlin/lotto/controller/GameController.kt
@@ -1,6 +1,7 @@
 package lotto.controller
 
 import lotto.model.RealLottoes
+import lotto.model.WinningRank
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -12,33 +13,55 @@ class GameController {
     private lateinit var lottoes: RealLottoes
 
     fun start() {
+        buyLottoes()
+        showLottoInformation()
+
+        val inputNumbers = insertLottoNumbers()
+        insertBonusNumber(inputNumbers)
+
+        val winningResult = lottoes.calculateLottoesResult()
+        showLottoResult(winningResult)
+    }
+
+    private fun buyLottoes() {
         var isValid: Boolean
-        var inputNumbers: List<Int>
         do {
             outputView.purchasePrompt()
             val payment = inputView.purchaseAmount()
             isValid = validator.validatePurchaseAmount(payment)
             lottoes = RealLottoes(payment)
         } while (!isValid)
+    }
 
+    private fun showLottoInformation() {
         outputView.lottoNumbersPrompt(lottoes.lottoTicketCount)
         outputView.lottoNumbers(lottoes.lottoes)
+    }
 
+    private fun insertLottoNumbers(): List<Int> {
+        var isValid: Boolean
+        var inputNumbers: List<Int>
         do {
             outputView.inputLottoNumbersPrompt()
             inputNumbers = inputView.lottoNumbers()
             isValid = validator.validateLottoNumbers(inputNumbers)
             lottoes.userNumbers = inputNumbers.toSet()
         } while (!isValid)
+        return inputNumbers
+    }
 
+    private fun insertBonusNumber(inputNumbers: List<Int>) {
+        var isValid: Boolean
         do {
             outputView.inputBonusNumberPrompt()
             val bonusNumber = inputView.bonusLottoNumber()
             isValid = validator.validateBonusNumber(bonusNumber, inputNumbers)
             lottoes.bonusNumber = bonusNumber
         } while (!isValid)
+    }
 
-        val winningResult = lottoes.calculateLottoesResult()
+
+    private fun showLottoResult(winningResult: Map<WinningRank, Int>) {
         outputView.winningResultPrompt()
         outputView.winningResult(winningResult)
         outputView.totalProfitRate(lottoes.getProfitRate())
