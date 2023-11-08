09b0bb66cd84eabd42d74114e21bac439839bb1a
altpfwlzh <altpfwlzh@gmail.com>
2023-11-08 16:31:34 +0900

    refactor : LottoGenerator 인터페이스에 동반 객체 수정
    1. UserLottosGenerator에 있던 것을 모두 인터페이스에 옮김

--- src/main/kotlin/lotto/util/UserLottosGenerator.kt
+++ src/main/kotlin/lotto/util/UserLottosGenerator.kt
@@ -2,6 +2,7 @@ package lotto.util
 
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.model.Lotto
+import lotto.util.LottosGenerator.Companion.LOTTO_NUM_CNT
 import lotto.util.LottosGenerator.Companion.MAX_NUM
 import lotto.util.LottosGenerator.Companion.MIN_NUM
 
@@ -10,9 +11,5 @@ class UserLottosGenerator : LottosGenerator {
         return (0 until cnt).map {
             Lotto(Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LOTTO_NUM_CNT).sorted())
         }
-    }
-
-    companion object {
-        const val LOTTO_NUM_CNT = 6
     }
 }
