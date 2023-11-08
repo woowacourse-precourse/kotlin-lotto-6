8e75951b343a415b82d055e8e575c174ac646537
wnsgml517 <wnsgml517@naver.com>
2023-11-08 15:16:54 +0900

    로또 구입 금액에 대한 로또개수 발행

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,13 +7,14 @@ fun main() {
 
     try {
         val purchaseAmount = getValidPurchaseAmount()
-
+        val purchasedTickets = generateLottoTickets(purchaseAmount)
+        println("${purchasedTickets}개를 구매했습니다.")
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
 }
 
-fun getValidPurchaseAmount(): Int {
+fun getValidPurchaseAmount(): Int { // 로또 구입 금액 및 유효성 검사
     try {
         val input = Console.readLine()
         val purchaseAmount = input.toInt()
@@ -27,4 +28,8 @@ fun getValidPurchaseAmount(): Int {
         println("[ERROR] 유효하지 않은 숫자를 입력했습니다. 다시 입력하세요.")
     }
     return 0
+}
+
+fun generateLottoTickets(purchaseAmount: Int): Int {
+    return purchaseAmount / 1000
 }
