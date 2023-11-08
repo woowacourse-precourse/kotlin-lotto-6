15a1b15ea4e7e7541d1ea72c8d05d24e59406367
amazansia <asd111050@naver.com>
2023-11-08 22:39:18 +0900

    fix: 출력 형식 포맷팅을 위해 rate 변환 과정에 toDouble() 추가

--- src/main/kotlin/lotto/model/LottoGenerator.kt
+++ src/main/kotlin/lotto/model/LottoGenerator.kt
@@ -3,7 +3,7 @@ package lotto.model
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoGenerator {
-    private fun createLotto() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+    private fun createLotto() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted())
 
     fun getLottos(num: Int): List<Lotto> {
         val lottoList = mutableListOf<Lotto>()
