755820e5e1587688dc9a493e04c5524ea5ac26d3
iamsseaa <homingsea@gmail.com>
2023-11-08 19:20:14 +0900

    에러 문구 추가

--- src/main/kotlin/lotto/LottoProgramError.kt
+++ src/main/kotlin/lotto/LottoProgramError.kt
@@ -2,25 +2,25 @@ package lotto
 
 class LottoProgramError {
     fun checkInputMoneyDivided1000Won(money: Int) {
-        if (money % 1000 != 0) throw IllegalArgumentException()
+        if (money % 1000 != 0) throw IllegalArgumentException(Error.INPUTMONEYDIVIDED1000WON.output)
     }
 
     fun checkNumberCountIsSix(numbers: List<Int>) {
-        if (numbers.size != 6) throw IllegalArgumentException()
+        if (numbers.size != 6) throw IllegalArgumentException(Error.NUMBERCOUNTISSIX.output)
     }
 
     fun checkNumberIsRandom(numbers: List<Int>) {
         var numbersRandom = numbers.toSet()
-        if (numbers.size != numbersRandom.size) throw IllegalArgumentException()
+        if (numbers.size != numbersRandom.size) throw IllegalArgumentException(Error.NUMBERISRANDOM.output)
     }
 
     fun checkNumberRange(numbers: List<Int>) {
         numbers.forEach {
-            if (it > 45 || it < 1) throw IllegalArgumentException()
+            if (it > 45 || it < 1) throw IllegalArgumentException(Error.NUMBERRANGE.output)
         }
     }
 
     fun checkBonusNumberRange(bonusNumber: Int) {
-        if (bonusNumber > 45 || bonusNumber < 1) throw IllegalArgumentException()
+        if (bonusNumber > 45 || bonusNumber < 1) throw IllegalArgumentException(Error.BONUSNUMBERRANGE.output)
     }
 }
