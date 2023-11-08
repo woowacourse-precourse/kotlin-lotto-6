391d674cd0684167bf81fa3e51dc88333c424b5e
민대인 <bamin0422@gmail.com>
2023-11-08 09:33:13 +0900

    test: getWinningMap - 구매한 로또에서 당첨내역을 확인 메서드 구현 및 테스트

--- src/test/kotlin/lotto/model/LottoServiceTest.kt
+++ src/test/kotlin/lotto/model/LottoServiceTest.kt
@@ -1,15 +1,21 @@
 package lotto.model
 
 import camp.nextstep.edu.missionutils.Randoms
+import lotto.model.Winner.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertTimeoutPreemptively
 import org.junit.jupiter.api.function.Executable
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.MockedStatic.Verification
 import org.mockito.Mockito
 import java.time.Duration
 import java.util.*
+import java.util.stream.Stream
 
 class LottoServiceTest {
 
@@ -29,8 +35,12 @@ class LottoServiceTest {
         )
     }
 
-    @Test
-    fun getWinningMap() {
+    @DisplayName("구매한 로또에서 당첨내역을 확인해, EnumMap<Winner, Int> 타입을 리턴")
+    @ParameterizedTest(name = "로또번호 {1}, 보너스 번호 {2}, 당첨내역 {3}")
+    @MethodSource("getWinningMapProvider")
+    fun `getWinningMap - 구매한 로또에서 당첨내역을 확인`(purchasedLottoList: List<Lotto>, winningNumber: Lotto, bonusNumber: Int, expectedWinningMap: EnumMap<Winner, Int>) {
+        val winningMap = LottoService.getWinningMap(purchasedLottoList, winningNumber, bonusNumber)
+        assertEquals(winningMap, expectedWinningMap)
     }
 
     @Test
@@ -68,5 +78,45 @@ class LottoServiceTest {
 
     companion object {
         private val RANDOM_TEST_TIMEOUT = Duration.ofSeconds(10L)
+
+        @JvmStatic
+        fun getWinningMapProvider(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(
+                    listOf(
+                        Lotto(listOf(1, 2, 3, 4, 5, 6)), // 1등 : Winner.FIRST_GRADE
+                        Lotto(listOf(1, 2, 3, 4, 5, 7)), // 2등 : Winner.SECOND_GRADE
+                        Lotto(listOf(1, 2, 3, 4, 5, 8)), // 3등 : Winner.THIRD_GRADE
+                    ), Lotto(listOf(1, 2, 3, 4, 5, 6)), 7,
+                    EnumMap(mapOf(
+                        FIRST_GRADE to 1,
+                        SECOND_GRADE to 1,
+                        THIRD_GRADE to 1,
+                    ))
+                ),
+                Arguments.of(
+                    listOf(
+                        Lotto(listOf(2, 3, 5, 6, 7, 44)), // 3등 : Winner.THIRD_GRADE
+                        Lotto(listOf(2, 3, 4, 5, 10, 11)), // 4등 : Winner.FOURTH_GRADE
+                        Lotto(listOf(2, 3, 4, 11, 12, 13)), // 5등 : Winner.FIFTH_GRADE
+                    ), Lotto(listOf(2, 3, 4, 5, 6, 7)), 9,
+                    EnumMap(mapOf(
+                        THIRD_GRADE to 1,
+                        FOURTH_GRADE to 1,
+                        FIFTH_GRADE to 1,
+                    ))
+                ),
+                Arguments.of(
+                    listOf(
+                        Lotto(listOf(2, 3, 4, 5, 10, 11)), // 4등 : Winner.FOURTH_GRADE
+                        Lotto(listOf(2, 3, 4, 11, 12, 13)), // 5등 : Winner.FIFTH_GRADE
+                        Lotto(listOf(2, 3, 18, 25, 38, 44)), // 당첨되지 않음.
+                    ), Lotto(listOf(2, 3, 4, 5, 6, 7)), 9,
+                    EnumMap(mapOf(
+                        FOURTH_GRADE to 1,
+                        FIFTH_GRADE to 1,
+                    ))
+                ),
+            )
     }
 }
