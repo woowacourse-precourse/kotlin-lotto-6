f8e7d0f50564e444e4e4b91f17860281ca8847b9
yujin37 <yj061628@gmail.com>
2023-11-08 11:09:13 +0900

    refactor: 로또 번호 랜덤과 입력 부분 클래스 분리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,7 +2,7 @@ package lotto
 
 fun main() {
     val cost = LottoPurchase().purchaseCheck()
-    val lottoNumbers = LottoPick().randomLotto(cost/1000)
+    val lottoNumbers = LottoGenerater().randomLotto(cost/1000)
     val prizeNumber = LottoPick().pickNumber()
     val bonusNumber = LottoBonus().bonusPickNumber(prizeNumber)
     val totalProfit = Lotto(prizeNumber).compareLotto(lottoNumbers, bonusNumber)
--- src/main/kotlin/lotto/LottoPick.kt
+++ src/main/kotlin/lotto/LottoPick.kt
@@ -1,22 +1,8 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
 
 class LottoPick {
-
-    private fun randomNumber() : List<Int> {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
-    }
-    fun randomLotto(cnt: Int):MutableList<List<Int>> {
-        val lottoList: MutableList<List<Int>> = mutableListOf()
-        (0 until cnt).forEach{
-            val numbers = randomNumber()
-            lottoList.add(numbers)
-            LottoView().numbersList(numbers)
-        }
-        return lottoList
-    }
 
     fun pickValid(pickList: List<String>) : List<Int> {
         val numbers = pickList.mapNotNull { it.toIntOrNull() }
