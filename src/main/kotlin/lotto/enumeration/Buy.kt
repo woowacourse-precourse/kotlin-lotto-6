a09c523849325e4fa12e3029ef21567cd5836ea6
Jtility <leejewon97@gmail.com>
2023-11-08 19:19:55 +0900

    feat(enumeration/Buy.kt): 열거형 HOW_MANY 추가

--- src/main/kotlin/lotto/enumeration/Buy.kt
+++ src/main/kotlin/lotto/enumeration/Buy.kt
@@ -3,5 +3,6 @@ package lotto.enumeration
 enum class Buy(val value: String) {
     PRICE_INPUT("구입금액을 입력해 주세요."),
     ERROR_NOT_INTEGER("[ERROR] 구입 금액은 정수여야 합니다."),
-    ERROR_NOT_THOUSAND("[ERROR] 구입 금액은 1000원 단위여야 합니다.")
+    ERROR_NOT_THOUSAND("[ERROR] 구입 금액은 1000원 단위여야 합니다."),
+    HOW_MANY("개를 구매했습니다.")
 }
