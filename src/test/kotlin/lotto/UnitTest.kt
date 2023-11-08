e01b378bbb22995b7412dd8f5c78a1158dbb4bb2
Jbrochan <hyeongchanj@gmail.com>
2023-11-08 17:49:55 +0900

    feat: 보너스 번호 입력 단위 테스트 구현

---
+++ src/test/kotlin/lotto/UnitTest.kt
@@ -0,0 +1,24 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class UnitTest : NsTest() {
+
+    @Test
+    fun `보너스 번호 테스트`() {
+        camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
+            runException("8000", "1,2,3,4,5,6", "512")
+            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+    }
+}
