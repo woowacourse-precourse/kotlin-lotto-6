c823130b5fe745588e36e6e8ef6bb0af12074716
Junyoung-WON <wjyo60@gmail.com>
2023-11-08 23:17:55 +0900

    FEAT: 등수에 따른 상금과 표시할 문자열, 당첨 개수를 구현

--- src/main/kotlin/lotto/WinEnum.kt
+++ src/main/kotlin/lotto/WinEnum.kt
@@ -1,4 +1,9 @@
 package lotto
 
-enum class WinEnum {
+enum class WinEnum(val win: Int, val winMoney: String, var counts: Int) {
+    FIFTH(5000, "5,000원", 0),
+    FOURTH(50000, "50,000원", 0),
+    THIRD(1500000, "1,500,000원", 0),
+    SECOND(30000000, "30,000,000원", 0),
+    FIRST(2000000000, "2,000,000,000원", 0)
 }
