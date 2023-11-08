2966cd31aa0ae110db20f58bbcdd708729faf676
sungsik-git <kss950708@gamil.com>
2023-11-08 11:36:37 +0900

    test: 보너스 번호 예외처리 테스트 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -18,7 +18,7 @@ class LottoTest {
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException>(UserInterface.NOT_INPUT_SAME_NUMBER_EXCEPTION.mention) {
-            val lotto = Lotto(listOf(1,2,3,4,5,5))
+            val lotto = Lotto(listOf(1, 2, 3, 4, 5, 5))
             val lottoSize = lotto.getNumbers().size
             val distinctLottoSize = lotto.getNumbers().distinct().size
             assertThat(lottoSize).isNotEqualTo(distinctLottoSize)
@@ -35,7 +35,7 @@ class LottoTest {
 
     @Test
     fun `당첨 번호가 1~45 사이의 숫자가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> (UserInterface.UNDER_AND_OVER_NUMBER_EXCEPTION.mention) {
+        assertThrows<IllegalArgumentException>() {
             val input = mutableListOf(1, 2, 3, 4, 5, 46)
             for (number in input) {
                 if (number !in 1..45) {
@@ -46,11 +46,42 @@ class LottoTest {
     }
 
     @Test
-    fun `당첨 번호에 공백이 입력되면 예외가 발생한다`(){
+    fun `당첨 번호에 공백이 입력되면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             val input = mutableListOf(1, 2, 3, 4, 5, 6)
             require(input.any { it.toString().contains(" ") }) {
                 UserInterface.NOT_INPUT_SPACE_EXCEPTION.mention
+            }
+        }
+    }
+
+    @Test
+    fun `보너스 번호는 1~45 사이의 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            var input = 46
+            if (input !in 1 .. 45){
+                throw IllegalArgumentException(UserInterface.UNDER_AND_OVER_NUMBER_EXCEPTION.mention)
+            }
+        }
+    }
+
+    @Test
+    fun `보너스 번호는 1자리의 숫자만 입력 가능하다`(){
+        assertThrows<IllegalArgumentException> {
+            var input = "13,25"
+            if(input.indexOf(",") != -1)
+                throw IllegalArgumentException(UserInterface.INPUT_BONUS_NUMBER_SIZE_NOT_1_EXCEPTION.mention)
+        }
+    }
+
+    @Test
+    fun `보너스 번호는 당첨번호와 중복된 숫자입력시 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            var winningNumbers = mutableListOf(1,2,3,4,5,6)
+            var bonusNumber = 6
+            for (index in 0 until winningNumbers.size){
+                if(winningNumbers[index] == bonusNumber)
+                    throw IllegalArgumentException(UserInterface.NOT_INPUT_SAME_NUMBER_EXCEPTION.mention)
             }
         }
     }
