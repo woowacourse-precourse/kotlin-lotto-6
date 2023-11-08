27e55f8dd576b30fe7f2eb4e971db9aade632ad1
Jtility <leejewon97@gmail.com>
2023-11-08 18:16:40 +0900

    feat(LottoUI): 구입 금액 입력 예외 처리
    예외 상활 시 IllegalArgumentException 발생 시킬 메서드 구현

--- src/main/kotlin/lotto/LottoUI.kt
+++ src/main/kotlin/lotto/LottoUI.kt
@@ -20,4 +20,11 @@ class LottoUI {
         return buyPrice
     }
 
+    private fun checkInvalidBuyPrice(buyPrice: String) {
+        when {
+            buyPrice.toIntOrNull() == null -> throw IllegalArgumentException("[ERROR] 구입 금액은 정수여야 합니다.")
+            buyPrice.toInt() % 1000 != 0 -> throw IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.")
+        }
+    }
+
 }
