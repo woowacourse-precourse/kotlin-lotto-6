ae8f7fa4372631420958e5460cb25aa86134d9b7
minuxx <minux8905@gmail.com>
2023-11-08 12:35:20 +0900

    feat: 당첨 로또 번호 입력 예외 처리 기능 추가

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -5,31 +5,43 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
     fun inputPurchaseAmount(): Int {
         val input = Console.readLine() ?: throw IllegalArgumentException(INPUT_ERROR_MESSAGE)
+        require(isEmptyOrBlankInput(input)) { INPUT_ERROR_MESSAGE }
 
         return validatePurchaseAmountInput(input.trim())
     }
 
     internal fun validatePurchaseAmountInput(input: String): Int {
-        require(isEmptyOrBlankInput(input)) { INPUT_ERROR_MESSAGE }
-        require(isNotDigits(input.trim())) { NOT_DIGIT_INPUT_ERROR_MESSAGE }
+        require(isNotDigitInput(input.trim())) { NOT_DIGIT_INPUT_ERROR_MESSAGE }
 
         return input.toInt()
     }
 
     fun inputWinningNumbers(): List<Int> {
         val input = Console.readLine() ?: throw IllegalArgumentException(INPUT_ERROR_MESSAGE)
+        require(isEmptyOrBlankInput(input)) { INPUT_ERROR_MESSAGE }
 
-        return input.split(SEPARATOR).map { it.trim().toInt() }
+        return validateWinningNumbersInput(input)
+    }
+
+    private fun validateWinningNumbersInput(input: String): List<Int> {
+        val splitInputs = input.split(SEPARATOR)
+        val trimmedInputs = splitInputs.map { it.trim() }
+        trimmedInputs.forEach {
+            require(isNotDigitInput(it)) { LOTTO_NUMBER_DIGIT_ERROR_MESSAGE }
+        }
+
+        return trimmedInputs.map { it.toInt() }
     }
 
     private fun isEmptyOrBlankInput(input: String) = input.isEmpty() or input.isBlank()
 
-    private fun isNotDigits(input: String) = input.all { it.isDigit() }
+    private fun isNotDigitInput(input: String) = input.all { it.isDigit() }
 
     companion object {
         const val INPUT_ERROR_MESSAGE = "다시 입력해주세요."
         const val NOT_DIGIT_INPUT_ERROR_MESSAGE = "숫자로만 입력해주세요."
 
         const val SEPARATOR = ","
+        const val LOTTO_NUMBER_DIGIT_ERROR_MESSAGE = "로또 번호는 숫자로만 입력해주세요."
     }
 }
