d7f4e6a458ee523ce9a0b45a378c8e8b7139c3ea
Gahee Lim <ka614323@naver.com>
2023-11-08 10:23:56 +0900

    feat: 당첨 번호를 입력받는 기능 구현

--- src/main/kotlin/lotto/view/Input.kt
+++ src/main/kotlin/lotto/view/Input.kt
@@ -4,4 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 
 class Input {
     fun buyLotto(): Int = Console.readLine().toInt()
+
+    fun drawWinNumber(): MutableList<Int> {
+        val winNum: MutableList<String> = Console.readLine().split(",").toMutableList()
+        val winNumber = winNum.map { it.toInt() }
+        return winNumber.toMutableList()
+    }
 }
