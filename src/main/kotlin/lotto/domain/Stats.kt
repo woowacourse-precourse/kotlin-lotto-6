607e6a160b9ceb68243fc2d4a4f0b2c6b1b8d7b8
OliviaYJH <0703olivia@naver.com>
2023-11-08 18:36:44 +0900

    style(Lotto): 코드 정렬

--- src/main/kotlin/lotto/domain/Stats.kt
+++ src/main/kotlin/lotto/domain/Stats.kt
@@ -4,19 +4,19 @@ enum class Stats(
     val correctNumberCount: Int,
     val winningPrize: Int,
     val winningMessage: String
-){
-    NONE(0,  0, ""),
-    FIFTH(3,  5000, " (5,000원) -"),
-    FOURTH(4,  50000, " (50,000원) -"),
-    THIRD(5,  1500000, " (1,500,000원) -"),
-    SECOND(5,  30000000, ", 보너스 볼 일치 (30,000,000원) -"),
+) {
+    NONE(0, 0, ""),
+    FIFTH(3, 5000, " (5,000원) -"),
+    FOURTH(4, 50000, " (50,000원) -"),
+    THIRD(5, 1500000, " (1,500,000원) -"),
+    SECOND(5, 30000000, ", 보너스 볼 일치 (30,000,000원) -"),
     FIRST(6, 2000000000, " (2,000,000,000원) -")
 }
 
-fun checkStat(matchNumbersCount: Int, bonusMatch: Boolean) : Stats {
-    return when{
+fun checkStat(matchNumbersCount: Int, bonusMatch: Boolean): Stats {
+    return when {
         matchNumbersCount == Stats.FIRST.correctNumberCount -> Stats.FIRST
-        matchNumbersCount == Stats.SECOND.correctNumberCount && bonusMatch-> Stats.SECOND
+        matchNumbersCount == Stats.SECOND.correctNumberCount && bonusMatch -> Stats.SECOND
         matchNumbersCount == Stats.THIRD.correctNumberCount -> Stats.THIRD
         matchNumbersCount == Stats.FOURTH.correctNumberCount -> Stats.FOURTH
         matchNumbersCount == Stats.FIFTH.correctNumberCount -> Stats.FIFTH
