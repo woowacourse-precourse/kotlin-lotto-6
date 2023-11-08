c7138a31f1bd04eeb55430ed6f01a4d6875fb519
Jbrochan <hyeongchanj@gmail.com>
2023-11-08 18:14:04 +0900

    feat: 구매 금액 입력시 단위 입력 예외 처리 단위 테스트 구현
    - 구매 금액 입력시 1,000원 단위가 아닌 금액 입력 단위 테스트 구현

--- src/test/kotlin/lotto/UnitTest.kt
+++ src/test/kotlin/lotto/UnitTest.kt
@@ -1,16 +1,25 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class UnitTest : NsTest() {
 
     @Test
+    fun `구입 금액 입력시 1,000원 단위가 아닌 금액을 입력했을 때 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            runException("800", "1,2,3,4,5,6", "13")
+        }
+    }
+
+    @Test
     fun `보너스 번호에 숫자가 아닌 문자가 들어간 경우 예외가 발생한다`() {
-        camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
+        assertSimpleTest {
             runException("8000", "1,2,3,4,5,6", "512abs")
-            Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+            assertThat(output()).contains(ERROR_MESSAGE)
         }
     }
 
