8f1f5d0cd7c4b202b5d98cd658d5cbb16f4b8639
2kjin <lkjin1202@gmail.com>
2023-11-08 17:35:49 +0900

    ✨ feat : 입력 받는 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    println(makeLotto().toString())
 }
+
+fun getMoney(): Int {
+    println("구입금액을 입력해 주세요.")
+    return Console.readLine().trim().toInt()
+}
+
+fun makeLotto(): Lotto {
+    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    return Lotto(numbers.sorted())
