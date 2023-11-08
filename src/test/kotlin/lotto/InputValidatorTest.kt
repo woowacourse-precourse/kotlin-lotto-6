4a01f6489f62ce77ea6765f8a71284e393788f01
seongsan <tjdtks030@naver.com>
2023-11-08 13:11:37 +0900

    test: InputValidatorTest DisplayName 및 코드 일부 수정

--- src/test/kotlin/lotto/InputValidatorTest.kt
+++ src/test/kotlin/lotto/InputValidatorTest.kt
@@ -10,24 +10,24 @@ class InputValidatorTest {
     @Test
     @DisplayName("공백 여부")
     fun `checkIsNotBlank 메서드 사용 시 입력값이 공백일 때 예외 발생`() {
-        val input = ""
-
-        assertThrows<IllegalArgumentException> { Validator.checkIsNotBlank(input) }
+        assertThrows<IllegalArgumentException> {
+            Validator.checkIsNotBlank("")
+        }
     }
 
     @Test
     @DisplayName("정수 여부")
     fun `checkIsDigit 메서드 사용 시 입력값이 정수의 형태가 아닐 때 예외 발생`() {
-        val input = "11.5"
-
-        assertThrows<IllegalArgumentException> { Validator.checkIsDigit(input) }
+        assertThrows<IllegalArgumentException> {
+            Validator.checkIsDigit("11.5")
+        }
     }
 
     @Test
     @DisplayName("구분자 유무")
     fun `checkHasCommaSeparator 메서드 사용 시 입력값이 쉼표(',') 로 구분되어있지 않을 때 예외 발생`() {
-        val input = "0 1 2"
-
-        assertThrows<IllegalArgumentException> { Validator.checkHasCommaSeparator(input) }
+        assertThrows<IllegalArgumentException> {
+            Validator.checkHasCommaSeparator("0 1 2")
+        }
     }
 }
