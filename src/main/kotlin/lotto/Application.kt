1a1956192d55b2ac1efbf6c45454eb4557670558
EJ-pro <hsshss2522@naver.com>
2023-11-08 16:42:38 +0900

    feat(input): 구입 금액 입력 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,12 @@
 package lotto
-
+import camp.nextstep.edu.missionutils.Console
 fun main() {
-    TODO("프로그램 구현")
+    val purchaseAmount = getPurchaseAmount() // 구입 금액을 입력 받음
+}
+// 구입 번호를 입력하는 함수
+fun getPurchaseAmount(): Int {
+    while(true) {
+        println("구입금액을 입력해 주세요.")
+        val input = Console.readLine() ?: ""
+    }
 }
