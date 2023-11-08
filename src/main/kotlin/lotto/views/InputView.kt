1f7b625f1c688ea0c520a793fff10d847126d718
minuxx <minux8905@gmail.com>
2023-11-08 12:14:29 +0900

    feat: 로또 구입 금액 입력 예외 처리 기능 추가

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -4,13 +4,24 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun inputPurchaseAmount(): Int {
-        val input = Console.readLine() ?: throw IllegalArgumentException(EMPTY_OR_BLANK_INPUT_ERROR_MESSAGE)
+        val input = Console.readLine() ?: throw IllegalArgumentException(INPUT_ERROR_MESSAGE)
+
+        return validatePurchaseAmountInput(input.trim())
+    }
+
+    internal fun validatePurchaseAmountInput(input: String): Int {
+        require(isEmptyOrBlankInput(input)) { INPUT_ERROR_MESSAGE }
+        require(isNotDigits(input.trim())) { NOT_DIGIT_INPUT_ERROR_MESSAGE }
 
         return input.toInt()
     }
 
-    companion object {
-        const val EMPTY_OR_BLANK_INPUT_ERROR_MESSAGE = "다시 입력해주세요."
+    private fun isEmptyOrBlankInput(input: String) = input.isEmpty() or input.isBlank()
 
+    private fun isNotDigits(input: String) = input.all { it.isDigit() }
+
+    companion object {
+        const val INPUT_ERROR_MESSAGE = "다시 입력해주세요."
+        const val NOT_DIGIT_INPUT_ERROR_MESSAGE = "숫자로만 입력해주세요."
     }
 }
