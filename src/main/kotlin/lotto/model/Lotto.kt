ab8a0050543a1962382fa6999cfc7c3aa657d933
sseung416 <choiseungyeon416@gmail.com>
2023-11-08 14:35:13 +0900

    refactor: Lotto 검증 로직을 LottoValidator로 분리

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,13 +1,9 @@
 package lotto.model
 
+import lotto.model.validator.LottoValidator
 import lotto.toValidInt
 
 class Lotto(private val numbers: List<LottoNumber>) {
-
-    init {
-        validateSize()
-        validateDuplicatedNumber()
-    }
 
     fun isMatchedBonus(bonus: LottoNumber): Boolean = numbers.contains(bonus)
 
@@ -15,25 +11,19 @@ class Lotto(private val numbers: List<LottoNumber>) {
 
     fun toStringNumbers(): String = numbers.toString()
 
-    private fun validateSize() {
-        require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_SIZE_ERROR }
-    }
-
-    private fun validateDuplicatedNumber() {
-        require(numbers.toSet().size == numbers.size) { DUPLICATED_NUMBER_ERROR }
-    }
-
     companion object {
         private const val SEPARATOR = ","
-        private const val LOTTO_NUMBER_SIZE = 6
-        private const val INVALID_SIZE_ERROR = "로또 값이 6개가 아닙니다."
-        private const val DUPLICATED_NUMBER_ERROR = "동일한 숫자는 값으로 입력할 수 없습니다. 로또 값이 중복되지 않았는지 확인해주세요."
 
-        fun create(userInput: String): Lotto {
-            val balls = userInput.split(SEPARATOR).map { number -> number.trim().toValidInt() }.toLottoNumbers()
-            return Lotto(balls)
+        fun create(userInput: String, validator: LottoValidator = LottoValidator()): Lotto {
+            val numbers = userInput.split(SEPARATOR).map { number -> number.trim().toValidInt() }.toLottoNumbers()
+            validator.validate(numbers)
+            return Lotto(numbers)
         }
 
-        fun of(vararg balls: Int) = Lotto(balls.toList().toLottoNumbers())
+        fun of(vararg balls: Int, validator: LottoValidator = LottoValidator()): Lotto {
+            val numbers = balls.toList().toLottoNumbers()
+            validator.validate(numbers)
+            return Lotto(numbers)
+        }
     }
 }
