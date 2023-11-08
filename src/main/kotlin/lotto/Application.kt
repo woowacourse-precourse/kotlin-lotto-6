ba7a234f154c060efebdd176090cc5ce1eac0a29
EJ-pro <hsshss2522@naver.com>
2023-11-08 16:44:26 +0900

    fix(input): 구입 금액 입력 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,8 +5,15 @@ fun main() {
 }
 // 구입 번호를 입력하는 함수
 fun getPurchaseAmount(): Int {
-    while(true) {
-        println("구입금액을 입력해 주세요.")
-        val input = Console.readLine() ?: ""
+    while (true) {
+        try {
+            println("구입금액을 입력해 주세요.")
+            val input = Console.readLine() ?: ""
+            val purchaseAmount = input.toIntOrNull()
+            if (purchaseAmount == null) { throw IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력해야 합니다.") }
+            if (purchaseAmount <= 0) { throw IllegalArgumentException("[ERROR] 구입 금액은 1 이상이어야 합니다.") }
+            if (purchaseAmount % 1000 != 0) { throw IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.") }
+            return purchaseAmount
+        }catch (e: IllegalArgumentException) { println(e.message) }
     }
 }
