e4d5bd47edd68186d1ef1b1ebaaa2201a1421893
pearhyunjin <pearhyunjin@gmail.com>
2023-11-08 17:26:49 +0900

    feat: add toString() and intersect() function in Lotto class
    add toString() function to modify util name into string. add intersect() function to compare lotto random number and winning number with intersect operation.

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,16 @@
 package lotto.domain
 
+import lotto.util.WRONG_COUNT_EXCEPTION
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { WRONG_COUNT_EXCEPTION }
     }
 
-    // TODO: 추가 기능 구현
+    override fun toString(): String = numbers.joinToString(", ", "[", "]")
+    fun intersect(winning: List<Int>): Set<Int> {
+        return numbers.intersect(winning.toSet())
+    }
+
+//    fun sortNumber(): List<Int> = numbers.sorted()
 }
