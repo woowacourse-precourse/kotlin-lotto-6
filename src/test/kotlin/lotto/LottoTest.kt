ca43b10fca48a80b13e7cb2054b1f8a572762874
MinhoiKoo <minlove2013@gmail.com>
2023-11-08 16:34:37 +0900

    test: 로또 번호 개수 6개이고 중복된 숫자 없으면 예외 발생하지 않음 확인

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -6,6 +6,7 @@ import lotto.domain.NumberIssuer
 import lotto.utils.Constant.LOTTO_NUMBER_SIZE
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 private const val OUT_RANGE_NUMBER = 46
@@ -28,9 +29,9 @@ class LottoTest {
     // 아래에 추가 테스트 작성 가능
 
     @Test
-    fun `발행된 로또 숫자의 범위가 1~45 사이가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            LottoNumber(OUT_RANGE_NUMBER)
+    fun `로또 번호의 개수가 6개이고 에 중복된 숫자가 없으면 예외가 발생하지 않는다`() {
+        assertDoesNotThrow {
+            Lotto(listOf(1,2,3,4,5,6).map {LottoNumber(it)})
         }
     }
 
