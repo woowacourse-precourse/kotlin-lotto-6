e9a63b0d2471e3483be98dfdf723464061b77167
Jtility <leejewon97@gmail.com>
2023-11-08 22:52:39 +0900

    feat(Bonus): 수정된 보너스 예외 사항 적용

--- src/main/kotlin/lotto/enumeration/Bonus.kt
+++ src/main/kotlin/lotto/enumeration/Bonus.kt
@@ -2,8 +2,7 @@ package lotto.enumeration
 
 enum class Bonus(val value: String) {
     NUMBER_INPUT("보너스 번호를 입력해 주세요."),
-    ERROR_NOT_INTEGER("[ERROR] 당첨 번호는 정수여야 합니다."),
-    ERROR_NOT_RANGE("[ERROR] 당첨 번호는 1 ~ 45 사이여야 합니다."),
-    ERROR_NOT_SIX("[ERROR] 당첨 번호는 6개여야 합니다."),
-    ERROR_NOT_UNIQUE("[ERROR] 당첨 번호는 중복되지 않아야 합니다.")
+    ERROR_NOT_INTEGER("[ERROR] 보너스 번호는 정수여야 합니다."),
+    ERROR_NOT_RANGE("[ERROR] 보너스 번호는 1 ~ 45 사이여야 합니다."),
+    ERROR_NOT_UNIQUE("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.")
 }
