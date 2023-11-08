a0957ed5dd7ac8f56ee8705576d38f7b4b2beb36
yoon <cbj0010@naver.com>
2023-11-08 13:40:40 +0900

    [Feat/docs] 로또 생성 조건 체크
    1~45 사이의 숫자

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,14 +7,16 @@ class Lotto(private val numbers: List<Int>) {
 
     fun checkLottoNumberException(): List<Int> {
 //중복숫자 체크 및 6자리 확인
-        if (numbers.size != 6)  throw IllegalArgumentException(ERROR_INPUT_NUMBER_LENGTH)
+        if (numbers.size != 6) throw IllegalArgumentException(ERROR_INPUT_NUMBER_LENGTH)
         if (numbers.distinct().size != 6) throw IllegalArgumentException(ERROR_INPUT_NUMBER_DISTINCT)
+        isAllNumbersInRange(numbers)
         return numbers.sorted()
     }
-   private fun isAllNumbersInRange(list: List<Int>): Boolean {
+
+    private fun isAllNumbersInRange(list: List<Int>): Boolean {
         for (number in list) {
             if (number < 1 || number > 45) {
-                return false
+                throw IllegalArgumentException(ERROR_INPUT_NUMBER_RANGE)
             }
         }
         return true
@@ -24,6 +26,7 @@ class Lotto(private val numbers: List<Int>) {
     companion object {
         private const val DEFAULT_LOTTO_SIZE = 6
         private const val ERROR_INPUT_NUMBER_LENGTH = "[ERROR] 6개의 숫자만 입력 가능합니다"
-        private const val ERROR_INPUT_NUMBER_DISTINCT = "수 중에 중복이 있습니다."
+        private const val ERROR_INPUT_NUMBER_DISTINCT = "[ERROR]수 중에 중복이 있습니다."
+        private const val ERROR_INPUT_NUMBER_RANGE = "[ERROR]1~45사이의 수가 아닙니다"
     }
 }
