c5527754e8e2ee6afdbdad4e1bcf4eca1ac7970d
ls2hn <s2hnsu@gmail.com>
2023-11-08 23:24:39 +0900

    test: 로또번호 생성에 대한 예외처리 테스트 코드 작성

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,9 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+
 
 
 class LottoTest {
@@ -12,7 +14,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -21,4 +22,18 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `생성한 로또 번호가 1 이상 45이하가 아니면 예외 발생`() {
+        val numbers = generateLottoNumbers()
+        assertThat(numbers).hasSize(6)
+        assertThat(numbers).allSatisfy { it >= 1 && it <= 45 }
+    }
+
+    @Test
+    fun `로또 번호에 중복이 있으면 예외가 발생한다`() {
+        val numbers = generateLottoNumbers()
+        assertThat(numbers.toSet()).hasSize(numbers.size)
+    }
+
+
 }
