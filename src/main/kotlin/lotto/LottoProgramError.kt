373fa436b98cdbdd1622d2208105c27410b6d5b1
iamsseaa <homingsea@gmail.com>
2023-11-08 19:09:44 +0900

    입력한 당첨 번호가 6개가 아닌 경우 예외 처리 기능 추가

--- src/main/kotlin/lotto/LottoProgramError.kt
+++ src/main/kotlin/lotto/LottoProgramError.kt
@@ -4,4 +4,8 @@ class LottoProgramError {
     fun checkInputMoneyDivided1000Won(money: Int) {
         if (money % 1000 != 0) throw IllegalArgumentException()
     }
+
+    fun checkNumberCountIsSix(numbers: List<Int>) {
+        if (numbers.size != 6) throw IllegalArgumentException()
+    }
 }
