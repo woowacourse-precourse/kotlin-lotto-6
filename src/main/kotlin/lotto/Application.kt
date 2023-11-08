d83dda6b70d7f13da65381c67e1e071c620fa369
ii2001 <iicks2001@gmail.com>
2023-11-08 20:55:57 +0900

    refactor: 코드 enum class 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,16 @@
 package lotto
+
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
+
+enum class Prize(val description: String, val winningAmount: Long) {
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
+    THIRD("5개 일치 (1,500,000원)", 1_500_000),
+    FOURTH("4개 일치 (50,000원)", 50_000),
+    FIFTH("3개 일치 (5,000원)", 5_000),
+    NO_PRIZE("꽝",0)
+}
 
 fun main() {
     println("구입금액을 입력해 주세요.")
@@ -14,20 +24,23 @@ fun main() {
         println(lottoNumber)
     }
     println("당첨 번호를 입력해 주세요.")
-    val winningNumbers=getWinningNumbers()
+    val winningNumbers = getWinningNumbers()
 
     println("보너스 번호를 입력해 주세요.")
-    val bonusNumber=getBonusNumber()
+    val bonusNumber = getBonusNumber()
 
-    val results=calculateResults(lottoNumbers,winningNumbers,bonusNumber)
+    val results = calculateResults(lottoNumbers, winningNumbers, bonusNumber)
     println("\n당첨 통계")
     println("---")
-    for ((key, value) in results) {
-        println("$key - ${value}개")
+    for (prize in Prize.values()) {
+        val count = results[prize]
+        println("${prize.description} - ${count}개")
     }
+
     val totalProfitRate = calculateTotalProfitRate(purchaseAmount, results)
     println("\n총 수익률은 ${String.format("%.1f", totalProfitRate)}%입니다.")
 }
+
 fun generateLottoNumbers(purchaseAmount: Int): List<Lotto> {
     val lottoNumbers = mutableListOf<Lotto>()
     for (i in 1..(purchaseAmount / 1000)) {
@@ -42,7 +55,7 @@ fun generateLottoNumbers(purchaseAmount: Int): List<Lotto> {
 fun getPurchaseAmount(): Int {
     var amount = 1
     while (amount % 1000 != 0) {
-        amount=inputPurchase(amount)
+        amount = inputPurchase(amount)
     }
     return amount
 }
@@ -63,9 +76,9 @@ fun inputPurchase(amount: Int): Int {
 }
 
 fun getWinningNumbers(): List<Int> {
-    var numbers=listOf(0)
-    while (numbers==listOf(0)) {
-        numbers=inputWinningNumbers()
+    var numbers = listOf(0)
+    while (numbers == listOf(0)) {
+        numbers = inputWinningNumbers()
     }
     return numbers
 }
@@ -76,26 +89,28 @@ fun inputWinningNumbers(): List<Int> {
         val input = Console.readLine()
         numbers = input?.split(",")!!.mapNotNull { it.trim().toIntOrNull() }
         validateLottoNumbers(numbers)
-        return numbers?: throw NumberFormatException("[ERROR] 잘못된 입력 형식입니다.")// 유효한 입력이 들어왔으면 루프를 종료
+        return numbers ?: throw NumberFormatException("[ERROR] 잘못된 입력 형식입니다.")// 유효한 입력이 들어왔으면 루프를 종료
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
     return listOf(0)
 }
+
 fun validateLottoNumbers(numbers: List<Int>?) {
     if (numbers == null || numbers.size != 6 || numbers.any { it < 1 || it > 45 } || numbers.size != numbers.distinct().size) {
         throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자 6개를 입력해야 하며 중복이 없어야 합니다.")
     }
 }
+
 fun getBonusNumber(): Int {
     var number = 0
     while (number < 1 || number > 45) {
-        number=inputBonusNumbers()
+        number = inputBonusNumbers()
     }
     return number
 }
 
-fun inputBonusNumbers(): Int{
+fun inputBonusNumbers(): Int {
     var result = 0
     try {
         result = Console.readLine().toInt()
@@ -108,59 +123,51 @@ fun inputBonusNumbers(): Int{
     }
     return result
 }
-fun validateBonusNumbers(number: Int){
+
+fun validateBonusNumbers(number: Int) {
     if (number < 1 || number > 45) {
         throw IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
     }
 }
 
-fun calculateResults(lottoNumbers: List<Lotto>, winningNumbers: List<Int>, bonusNumber: Int): MutableMap<String, Int> {
-    var results = mutableMapOf(
-        "3개 일치 (5,000원)" to 0,
-        "4개 일치 (50,000원)" to 0,
-        "5개 일치 (1,500,000원)" to 0,
-        "5개 일치, 보너스 볼 일치 (30,000,000원)" to 0,
-        "6개 일치 (2,000,000,000원)" to 0)
-
+fun calculateResults(lottoNumbers: List<Lotto>, winningNumbers: List<Int>, bonusNumber: Int): Map<Prize, Int> {
+    val results = mutableMapOf<Prize, Int>()
+    for (prize in Prize.values()) {
+        results[prize] = 0
+    }
     for (lotto in lottoNumbers) {
         val numbers = lotto.getNumbers()
-        results=matchingNumber(results,numbers,winningNumbers,bonusNumber)
+        val prize = determinePrize(numbers, winningNumbers, bonusNumber)
+        results[prize] = results.getOrDefault(prize, 0) + 1
     }
-
     return results
 }
 
-fun matchingNumber(results: MutableMap<String, Int>, numbers: List<Int>, winningNumbers: List<Int>, bonusNumber: Int): MutableMap<String, Int> {
+fun determinePrize(numbers: List<Int>, winningNumbers: List<Int>, bonusNumber: Int): Prize {
     val matchingNumbers = numbers.intersect(winningNumbers).toList()
     val bonusMatch = if (numbers.contains(bonusNumber)) 1 else 0
 
-    when (matchingNumbers.size) {
-        6 -> results["6개 일치 (2,000,000,000원)"] = results["6개 일치 (2,000,000,000원)"]!! + 1
-        5 -> results["5개 일치 (1,500,000원)"] = results["5개 일치 (1,500,000원)"]!! + 1
-        4 -> results["4개 일치 (50,000원)"] = results["4개 일치 (50,000원)"]!! + 1
-        3 -> results["3개 일치 (5,000원)"] = results["3개 일치 (5,000원)"]!! + 1
-    }
-
-    if (matchingNumbers.size == 5 && bonusMatch == 1) {
-        results["5개 일치, 보너스 볼 일치 (30,000,000원)"] = results["5개 일치, 보너스 볼 일치 (30,000,000원)"]!! + 1
-    }
-    return results
-}
-
-fun calculatePrice(entry: Map.Entry<String, Int>): Int {
-    return when (entry.key) {
-        "6개 일치 (2,000,000,000원)" -> entry.value * 2_000_000_000
-        "5개 일치, 보너스 볼 일치 (30,000,000원)" -> entry.value * 30_000_000
-        "5개 일치 (1,500,000원)" -> entry.value * 1_500_000
-        "4개 일치 (50,000원)" -> entry.value * 50_000
-        "3개 일치 (5,000원)" -> entry.value * 5_000
-        else -> 0
+    return when (matchingNumbers.size) {
+        6 -> Prize.FIRST
+        5 -> if (bonusMatch == 1) Prize.SECOND else Prize.THIRD
+        4 -> Prize.FOURTH
+        3 -> Prize.FIFTH
+        else -> Prize.NO_PRIZE
     }
 }
 
-fun calculateTotalProfitRate(purchaseAmount: Int, results: Map<String, Int>): Double {
-    val totalWinningAmount = results.entries.sumBy { entry ->
-        calculatePrice(entry)
+
+fun calculateTotalProfitRate(purchaseAmount: Int, results: Map<Prize, Int>): Double {
+    val totalWinningAmount = results.entries.sumByLong { entry ->
+        entry.key.winningAmount * entry.value.toLong()
     }
     return (totalWinningAmount.toDouble() / purchaseAmount) * 100.0
+}
+
+inline fun <K, V> Iterable<Map.Entry<K, V>>.sumByLong(selector: (Map.Entry<K, V>) -> Long): Long {
+    var sum: Long = 0
+    for (element in this) {
+        sum += selector(element)
+    }
+    return sum
 }
