ce80548e0a72c3e7d494710d3d6602dc5feca898
yoon <cbj0010@naver.com>
2023-11-08 21:53:48 +0900

    [Feat] 몇개가 당첨되었는지

--- src/main/kotlin/lotto/Calculator.kt
+++ src/main/kotlin/lotto/Calculator.kt
@@ -2,9 +2,7 @@ package lotto
 
 import lotto.LottoSeller.Companion.LOTTO_TICKET_PRICE
 
-class Calculator(
-
-) {
+class Calculator {
 //비교하는 곳 , 나 이거 비교해줘! 라는 곳에 대한 대답
 //그러려면 사용자가 입력한 값과 로또 리스트를 비교하는자가 가지고 있어야겠지?
 
@@ -26,27 +24,24 @@ class Calculator(
         lottoTicketCount = lottoMachine.size
         for (lotto in lottoMachine) {
 
-            val matchedNumbers = lotto.intersect(userLotto).size
+            val matchedNumbers = lotto.intersect(userLotto.toSet()).size
             val isBonusMatched = userLotto.contains(bonusNum)    //보너스 볼이 포함 되어 있는지 확인
-//            println("matchedNumbers $matchedNumbers")
             val lottoRank = MatchedCount.fromMatchedNumbers(matchedNumbers, isBonusMatched)
-            if (lottoRank != MatchedCount.NONE) lottoResult[lottoRank] = lottoResult.getOrDefault(lottoRank, 0) + 1
+            if (lottoRank != MatchedCount.NONE) {
+                lottoResult[lottoRank] = lottoResult.getOrDefault(lottoRank, 0) + 1
+            }
         }
     }
 
     fun calculateProfitRate(): Float {
         calculateProfit()
         val moneySpent = lottoTicketCount * LOTTO_TICKET_PRICE
-//        println("수익률 계산하는 곳 $moneySpent")
-//        println("수익률 $profitability")
         return (profitability * 100f) / moneySpent
     }
 
     private fun calculateProfit() {
         lottoResult.forEach { (rank, count) ->
-//            println("lottoResult $lottoResult")
             profitability += rank.prize * count
         }
     }
-
 }
