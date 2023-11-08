ac06ccb825fb0a4dd3d4ce3ebfffee403fc52bfc
sumin <gosumin0710@naver.com>
2023-11-08 14:22:57 +0900

    feat : 로또 금액에 맞춰 로또 발행 기능 추가

--- src/main/kotlin/lotto/GameManager.kt
+++ src/main/kotlin/lotto/GameManager.kt
@@ -1,8 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class GameManager() {
-    fun playGame(money: Int, winningNumbers: Lotto, bonusNumber: Int){
+    fun playGame(money: Int, winningNumbers: Lotto, bonusNumber: Int) {
+        val lottoNumber = getLottoNumber(money)
+
 
     }
 
+    private fun getLottoNumber(money: Int): Int = money / 1000
+
+    private fun makeLottos(lottoNumber: Int) : List<Lotto> {
+        val lottos = mutableListOf<Lotto>()
+        repeat(lottoNumber) {
+            lottos.add(getRandomLottoNumbers())
+        }
+        return lottos
+    }
+
+    private fun getRandomLottoNumbers() = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
 }
