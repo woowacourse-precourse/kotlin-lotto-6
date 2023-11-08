87f9a828253af3fce27223b93440322fbb2fe41a
gaeun <geun5744@gmail.com>
2023-11-08 15:50:16 +0900

    [FEAT(LottoController)] 유효한 input을 입력할 때 까지 loop문 유지 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,4 +1,59 @@
 package lotto.controller
 
-class LottoController() {
+import lotto.model.LottoManager
+import lotto.model.LottoStatics
+import lotto.model.Validation
+import lotto.view.Output
+import lotto.view.UserInput
+import java.lang.IllegalArgumentException
+
+class LottoController(
+    private val userInput: UserInput = UserInput(),
+    private val output: Output = Output(),
+) {
+
+    private lateinit var lottoManager: LottoManager
+    private lateinit var lottoStatics: LottoStatics
+
+    fun run() {
+        val purchaseAmount = inputPurchaseAmountLoop()
+        val answerAmount = inputAnswerNumLoop()
+        val bonusNum = inputBonusNumLoop()
+
+    }
+
+
+    private fun inputPurchaseAmountLoop(): Int {
+        while (true) {
+            try {
+                return Validation.getPurchaseAmount(userInput.getPurchaseAmount())
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun inputAnswerNumLoop(): List<Int> {
+        while (true) {
+            try {
+                return Validation.getAnswerNumber(userInput.getAnswerNumber())
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun inputBonusNumLoop(): Int {
+        while (true) {
+            try {
+                return Validation.getBonusNum(userInput.getBonusNum())
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
 }
