6b6ef85be26d31fbad4e81dacd2765cc8e3419c9
gaeun <geun5744@gmail.com>
2023-11-08 14:22:43 +0900

    [FEAT(Lotto)] 예외 전환 처리

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,8 +2,13 @@ package lotto.model
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            "[ERROR] 6개의 숫자만 입력해주세요"
+        }
+
+        require(numbers.toSet().size == numbers.size) {
+            "[ERROR] 서로 다른 숫자를 입력해주세요."
+        }
     }
 
-    // TODO: 추가 기능 구현
 }
