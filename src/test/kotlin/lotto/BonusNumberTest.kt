51fd255b2d5f272b692d634e38c28c72c9197ce5
yeji0214 <1225yj50@naver.com>
2023-11-08 14:49:16 +0900

    BonusNumberTest.kt 수정

--- src/test/kotlin/lotto/BonusNumberTest.kt
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -6,36 +6,37 @@ import org.junit.jupiter.api.assertThrows
 
 
 class BonusNumberTest {
+    private val ANSWER = listOf(1,2,3,4,5,6)
     @Test
     fun `보너스 번호가 1보다 작으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            BonusNumber(-1, listOf(1,2,3,4,5,6))
+            BonusNumber(-1, ANSWER)
         }
     }
 
     @Test
     fun `보너스 번호가 45보다 크면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            BonusNumber(50, listOf(1,2,3,4,5,6))
+            BonusNumber(50, ANSWER)
         }
     }
 
     @Test
     fun `당첨 번호로 입력한 숫자와 보너스 번호가 중복된다면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            BonusNumber(3, listOf(1,2,3,4,5,6))
+            BonusNumber(3, ANSWER)
         }
     }
 
     @Test
     fun `당첨 번호가 숫자가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            BonusNumber("invalid".toInt(), listOf(1,2,3,4,5,6))
+            BonusNumber("invalid".toInt(), ANSWER)
         }
     }
 
     @Test
     fun `보너스 번호 정상 입력`() {
-        assertDoesNotThrow { BonusNumber(10, listOf(1, 2, 3, 4, 5, 6)) }
+        assertDoesNotThrow { BonusNumber(10, ANSWER) }
     }
 }
