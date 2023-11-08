7ba9a72fa65d95996ba7016f2da3eb06c05eae29
bak <bak4017@gmail.com>
2023-11-08 17:41:13 +0900

    refactor(lotto): 중복되는 테스트 클래스 병합

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -6,19 +6,17 @@ import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
-    fun `로또 번호는 6개여야 한다`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5))
+    fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
         }
-        assertEquals("Failed requirement.", exception.message)
     }
 
     @Test
-    fun `로또 번호는 중복되지 않아야 한다`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 1, 2, 3, 4, 5))
+    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
-        assertEquals("Failed requirement.", exception.message)
     }
 
     @Test
