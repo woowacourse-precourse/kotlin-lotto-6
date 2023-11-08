1ead1ee7fe0abecf5f11abbe67f0f1c938e983ec
gaeun <geun5744@gmail.com>
2023-11-08 21:28:38 +0900

    [REFACTOR(Output)] 하드 코딩 제거

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -6,25 +6,40 @@ import lotto.model.WinningType
 class Output {
 
     fun printWinStatics(winStatics: Map<WinningType, Int>) {
-        println("당첨 통계")
-        println("---")
-        println("3개 일치 (5,000원) - ${winStatics[WinningType.FIFTH]}개")
-        println("4개 일치 (50,000원) - ${winStatics[WinningType.FOURTH]}개")
-        println("5개 일치 (1,500,000원) - ${winStatics[WinningType.THIRD]}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winStatics[WinningType.SECOND]}개")
-        println("6개 일치 (2,000,000,000원) - ${winStatics[WinningType.FIRST]}개")
+        println(WINNING_STATISTICS_MESSAGE)
+        println(DIVIDER_MESSAGE)
+        println("$FIFTH_WIN_MESSAGE${winStatics[WinningType.FIFTH]}개")
+        println("$FOURTH_WIN_MESSAGE${winStatics[WinningType.FOURTH]}개")
+        println("$THIRD_WIN_MESSAGE${winStatics[WinningType.THIRD]}개")
+        println("$SECOND_WIN_MESSAGE${winStatics[WinningType.SECOND]}개")
+        println("$FIRST_WIN_MESSAGE${winStatics[WinningType.FIRST]}개")
     }
 
     fun printTotalReturn(totalReturn: Int, purchaseAmount: Int) {
         val returnRate = (totalReturn.toDouble() / purchaseAmount) * 100
         val roundedRate = String.format("%.1f", returnRate)
-        println("총 수익률은 $roundedRate%입니다.")
+        println(returnRateMessage(roundedRate))
     }
 
     fun printLottoesNum(lottoes: List<Lotto>) {
-        println("${lottoes.size}개를 구매했습니다.")
+        println(purchaseNumMessage(lottoes.size))
         lottoes.forEach { lotto ->
             lotto.printLottoNumbers()
         }
+    }
+
+    companion object {
+        const val WINNING_STATISTICS_MESSAGE = "당첨 통계"
+        const val DIVIDER_MESSAGE = "---"
+
+        const val FIFTH_WIN_MESSAGE = "3개 일치 (5,000원) - "
+        const val FOURTH_WIN_MESSAGE = "4개 일치 (50,000원) - "
+        const val THIRD_WIN_MESSAGE = "5개 일치 (1,500,000원) - "
+        const val SECOND_WIN_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+        const val FIRST_WIN_MESSAGE = "6개 일치 (2,000,000,000원) - "
+
+        fun returnRateMessage(returnRate: String) = "총 수익률은 $returnRate%입니다."
+        fun purchaseNumMessage(purchaseNum: Int) = "${purchaseNum}개를 구매했습니다."
+
     }
 }
