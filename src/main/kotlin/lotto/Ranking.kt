3207d5562ffebef1e1d5317871aecd96b6ef704e
iamsseaa <homingsea@gmail.com>
2023-11-08 16:46:34 +0900

    당첨 결과 순위 Enum 생성

---
+++ src/main/kotlin/lotto/Ranking.kt
@@ -0,0 +1,11 @@
+package lotto
+
+enum class Ranking (
+    val rankingNumber: String
+) {
+    THREE("3개 일치 (5,000원) - "),
+    FOUR("4개 일치 (50,000원) - "),
+    FIVE("5개 일치 (1,500,000원) - "),
+    FIVEBONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX("6개 일치 (2,000,000,000원) - ")
+}
