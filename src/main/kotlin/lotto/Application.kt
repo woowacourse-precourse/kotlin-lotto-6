364910cc9c39da1422964186b4749ce90e50d442
Jtility <leejewon97@gmail.com>
2023-11-08 19:57:34 +0900

    feat(main): buyLotto 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,4 +4,6 @@ fun main() {
     LottoUI().printBuyPrice()
     val buyPrice = LottoUI().inputBuyPrice()
     LottoUI().printBuyLottoCount(buyPrice)
+    val buyCount = buyPrice.toInt() / 1000
+    val buyLottos = LottoService().buyLotto(buyCount)
 }
