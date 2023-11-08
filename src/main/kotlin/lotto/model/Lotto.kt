473fc4d43fd24a72302816047197f24fa3266878
전형선 <hsun5163@gmail.com>
2023-11-08 15:22:53 +0900

    refactor : validateLottoNumberRange 함수를 require 함수를 사용하여 축약.

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,17 +4,10 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) {"[ERROR] 로또 번호는 정확히 6개여야 합니다."}
         require(numbers.toSet().size == 6) {"[ERROR] 로또 번호는 중복되지 않는 6개여야 합니다."}
-        validateLottoNumberRange()
+        require(numbers.all { it in 1..45 }) {"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."}
     }
 
-    private fun validateLottoNumberRange() {
-        for (num in numbers) {
-            if (num !in 1.. 45) {
-                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
-            }
-                return
-        }
-    }
+
     fun getLotto(): List<Int>{
         return numbers
     }
