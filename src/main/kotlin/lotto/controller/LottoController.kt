2033f288f23ba528f4e731d38f54e7fc5cc41aea
tgyuu-An <atg0614@gmail.com>
2023-11-08 14:27:42 +0900

    add : LottoBundle 추가

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -5,6 +5,7 @@ import lotto.domain.LottoShop
 import lotto.view.InputView.getBonusNumbersInput
 import lotto.view.InputView.getLottoPurchaseCostInput
 import lotto.view.InputView.getWinningNumbersInput
+import lotto.view.OutputView.printLottosNumbers
 
 class LottoController {
     private val lottoManager: LottoManager by lazy { LottoManager() }
@@ -13,12 +14,15 @@ class LottoController {
     fun run() {
         val purchaseMoney = getLottoPurchaseCostInput()
         val lottos = lottoShop.purchaseLottos(purchaseMoney)
-        lottos.forEach {
-
-        }
+        printLottosNumbers(lottos)
 
         val winningNumbers = getWinningNumbersInput()
+        lottoManager.setWinningNumbers(winningNumbers)
+        println()
+
         val bonusNumber = getBonusNumbersInput()
+        lottoManager.setBonusNumber(bonusNumber)
+        println()
     }
 
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,4 +1,18 @@
 package lotto.view
 
+import lotto.domain.Lotto
+
 object OutputView {
+    fun printLottosNumbers(lottos: List<Lotto>) {
+        println("${lottos.size}개를 구매했습니다.")
+        lottos.forEach { lotto ->
+            printLottoNumbers(lotto)
+        }
+        println()
+    }
+
+    fun printLottoNumbers(lotto: Lotto) {
+        val lottoNumbers = lotto.getLottoNumbers()
+        print("["+lottoNumbers+"]")
+    }
 }
