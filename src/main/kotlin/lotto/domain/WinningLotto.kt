4fd2836bd621045e43fd458f8ccd570338eda599
Wonhui Roh <rwhui012@gmail.com>
2023-11-08 21:04:39 +0900

    feat(lotto): '사용자가 구매한 로또 번호'와 '당첨 번호 및 보너스 번호'를 비교하는 기능 - #compareLottos()

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,8 +1,33 @@
 package lotto.domain
 
-class WinningLotto {
-  fun compareLottos() {
+import lotto.domain.LottoRank.*
 
+class WinningLotto(private val winningNumbers: List<Int>, private val bonusNumber: List<Int>) {
+
+  init {
+    require(winningNumbers.size == 6)
+    require(winningNumbers.size == winningNumbers.toSet().size)
+    require(bonusNumber.size == 1)
+    require(!winningNumbers.any{ number -> number == bonusNumber[0] })
+  }
+
+  fun compareLottos(lotto: Lotto) {
+    val lottoNumbers = lotto.getLottoNumbers().toSet()
+    val winningNumbers = this.winningNumbers.toSet()
+    val intersection = lottoNumbers.intersect(winningNumbers)
+
+    when (intersection.size) {
+      3 -> FIFTH.addCount()
+      4 -> FOURTH.addCount()
+      5 -> {
+        if (bonusNumber[0] in lottoNumbers) {
+          SECOND.addCount()
+        } else {
+          THIRD.addCount()
+        }
+      }
+      6 -> FIRST.addCount()
+    }
   }
 
   fun calculateProfitRate() {
