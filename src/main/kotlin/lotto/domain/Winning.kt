6293acedfd5113e629de2efffe12e8d25b6b0ed4
3uomlkh <cw1205@duksung.ac.kr>
2023-11-08 20:33:00 +0900

    refactor: 당첨 통계 구하는 기능 수정

--- src/main/kotlin/lotto/domain/Winning.kt
+++ src/main/kotlin/lotto/domain/Winning.kt
@@ -2,13 +2,13 @@ package lotto.domain
 
 import lotto.Lotto
 
-enum class Winning(val message: String, val match: Int, val bonus: Boolean, val prize: Int) {
+enum class Winning(val message: String, val match: Int, val prize: Int) {
 
-    THREE("3개 일치 (5,000원) - ", 3, false, 5000),
-    FOUR("4개 일치 (50,000원) - ",4, false, 50000),
-    FIVE("5개 일치 (1,500,000원) - ",5, false, 1500000),
-    BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5, true, 30000000),
-    SIX("6개 일치 (2,000,000,000원) - ", 6, false, 2000000000)
+    THREE("3개 일치 (5,000원) - ", 3, 5000),
+    FOUR("4개 일치 (50,000원) - ",4, 50000),
+    FIVE("5개 일치 (1,500,000원) - ",5, 1500000),
+    BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - ", 5, 30000000),
+    SIX("6개 일치 (2,000,000,000원) - ", 6, 2000000000)
 }
 
 // 당첨 번호와 로또 번호 비교
@@ -28,15 +28,38 @@ fun compareToLotto(lottos: MutableList<Lotto>, winningNumber: List<Int>, BonusNu
 }
 
 // 당첨된 횟수 적립
-fun matchCheck(matches: Pair<MutableList<Int>, Int>) {
-    var totalMatch = mutableListOf<Int>()
+fun matchCheck(matches: Pair<MutableList<Int>, Int>): MutableList<Int> {
+    var totalMatch = mutableListOf(0,0,0,0,0)
 
     for (index in matches.first.indices) {
         when (matches.first[index]) {
             Winning.THREE.match -> totalMatch[0]++
             Winning.FOUR.match -> totalMatch[1]++
             Winning.FIVE.match -> totalMatch[2]++
-            Winning.SIX.match -> totalMatch[3]++
+            Winning.SIX.match -> totalMatch[4]++
         }
     }
+    repeat(matches.second) {
+        totalMatch[3]++
+    }
+
+    return totalMatch
+}
+
+// 당첨된 금액 합계
+fun getTotalAmount(matches: MutableList<Int>): Int {
+    var total = matches[0] * Winning.THREE.prize
+    + matches[1] * Winning.FOUR.prize
+    + matches[2] * Winning.FIVE.prize
+    + matches[3] * Winning.BONUS.prize
+    + matches[4] * Winning.SIX.prize
+
+    return total
+}
+
+// 수익률 구하기
+fun getEarningRate(amount: Int, total: Int): Float {
+    val amount = amount.toFloat()
+    val total = total.toFloat()
+    return (total/amount)*100
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,33 +1,43 @@
 package lotto.view
 
+import lotto.domain.Winning
+
 enum class OutputView(val message: String) {
-    PURCHASE_AMOUNT_PROMPT("구입금액을 입력해 주세요."),
+    PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     PURCHASE_AMOUNT_PRINT("개를 구매했습니다."),
-    WINNING_NUMBER_PROMPT("\n당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_PROMPT("\n보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS("당첨 통계\n---")
+    WINNING_NUMBER("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("\n당첨 통계\n---")
 }
 
 fun printStartMessage() {
-    println(OutputView.PURCHASE_AMOUNT_PROMPT)
+    println(OutputView.PURCHASE_AMOUNT.message)
 }
 
 fun printPurchaseTotal(total: Int) {
-    println("${total}개를 구매했습니다.")
+    println("\n${total/1000}" + OutputView.PURCHASE_AMOUNT_PRINT)
 }
 
 fun printWinningMessage() {
-    println(OutputView.WINNING_NUMBER_PROMPT)
+    println(OutputView.WINNING_NUMBER.message)
 }
 
 fun printBonusMessage() {
-    println(OutputView.BONUS_NUMBER_PROMPT)
+    println(OutputView.BONUS_NUMBER.message)
 }
 
 fun printWinningReport() {
-    println(OutputView.WINNING_STATISTICS)
+    println(OutputView.WINNING_STATISTICS.message)
+}
+
+fun printWinningStatistics(matches: MutableList<Int>) {
+    println(Winning.THREE.message + "${matches[0]}개")
+    println(Winning.FOUR.message + "${matches[1]}개")
+    println(Winning.FIVE.message + "${matches[2]}개")
+    println(Winning.BONUS.message + "${matches[3]}개")
+    println(Winning.SIX.message + "${matches[4]}개")
 }
 
 fun printEarningRate(rate: Float) {
-    println("총 수익률은 {$rate}%입니다.")
+    println("총 수익률은 ${rate}%입니다.")
 }
