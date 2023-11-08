2ec2606d990def5fa2341ce274e9cada2ce3f3a5
정채훈 <mrchjung@peopleinnet.com>
2023-11-08 21:44:07 +0900

    feat(domain) : WinningLotto 생성 기능
    WinningLotto 클래스 추가
    Service#generateWinningLotto 메서드 추가

---
+++ src/main/kotlin/lotto/model/domain/WinningLotto.kt
@@ -0,0 +1,3 @@
+package lotto.model.domain
+
+class WinningLotto(private val lotto: Lotto, private val bonusNumber: Int)
--- src/main/kotlin/lotto/model/service/Service.kt
+++ src/main/kotlin/lotto/model/service/Service.kt
@@ -1,4 +1,13 @@
 package lotto.model.service
 
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.model.domain.Lotto
+import lotto.model.domain.WinningLotto
+
 class Service {
+    fun generateWinningLotto(): WinningLotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        val bonusNumber: Int = Randoms.pickNumberInList((1..45).toList().minus(numbers))
+        return WinningLotto(Lotto(numbers), bonusNumber)
+    }
 }
