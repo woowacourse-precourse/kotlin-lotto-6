68d6c4340bdd070a63ef424d25f31798a7e6d6cf
Jinhwan_ideapad_ubuntu <xsest55@ajou.ac.kr>
2023-11-08 17:23:06 +0900

    feat: input winning nums and bonus num

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -13,6 +13,8 @@ class LottoController(private val inputview: InputView = InputView(),
             lottos.add(buyLotto())
         }
         outputview.printPurchase(amount, lottos)
+        val winningNumbers:List<Int> = getWinnigNums()
+        val bonusNumber: Int = getBonusNum(winningNumbers)
 
     }
     private fun getAmount(): Int {
@@ -33,4 +35,30 @@ class LottoController(private val inputview: InputView = InputView(),
         return Lotto(nums.sorted())
     }
 
+    private fun getWinnigNums(): List<Int> {
+        var winnigNums: List<Int>
+        while(true) {
+            try{
+                outputview.winningMessage()
+                winnigNums = inputview.inputWinningNum()
+                return winnigNums
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+    }
+    private fun getBonusNum(winningNumbers: List<Int>): Int {
+        var bonusNumber: Int
+        while(true) {
+            try{
+                outputview.bonusMessage()
+                bonusNumber = inputview.inputBonusNum()
+                if(bonusNumber in winningNumbers)
+                    throw IllegalArgumentException("[Error] 보너스 번호는 당첨번호와 겹치지 않는 1부터 45사이의 숫자입니다.")
+                return bonusNumber
+            } catch (e: IllegalArgumentException) {
+                println(e)
+            }
+        }
+    }
 }
