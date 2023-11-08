48f98e1fb9b70a18d87caebb83dfdca7d154a65a
gaeun <geun5744@gmail.com>
2023-11-08 22:47:44 +0900

    [REFACTOR(ValidationTest)] assertThrows -> assertThatThrownBy로 리팩토링

--- src/test/kotlin/lotto/model/ValidationTest.kt
+++ src/test/kotlin/lotto/model/ValidationTest.kt
@@ -1,38 +1,35 @@
 package lotto.model
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import java.lang.IllegalArgumentException
 
 class ValidationTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["횟수", "1200"])
-    fun `로또 구입 금액 입력 테스트`(input: String) {
-        assertThrows<IllegalArgumentException> {
+    fun `로또 구입 금액 입력 에러 메시지 테스트`(input: String) {
+        assertThatThrownBy {
             Validation.getPurchaseAmount(input)
-        }
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["횟수", "1 2 3", "1,2,3,4,5", "1,2,3,4,5,100", "1,2,3,4,5,5"])
-    fun `당첨 번호 입력 테스트`(input: String) {
-        assertThrows<IllegalArgumentException> {
+    fun `당첨 번호 입력 에러 메시지 테스트`(input: String) {
+
+        assertThatThrownBy {
             Validation.getAnswerNumber(input)
-        }
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["횟수", "100", "1,2"])
-    fun `보너스 번호 입력 테스트`(input: String) {
-        assertThrows<IllegalArgumentException> {
+    fun `보너스 번호 입력 에러 메시지 테스트`(input: String) {
+        assertThatThrownBy {
             Validation.getBonusNum(input)
-        }
+        }.isInstanceOf(IllegalArgumentException::class.java).hasMessageContaining("[ERROR]")
     }
 
 }
