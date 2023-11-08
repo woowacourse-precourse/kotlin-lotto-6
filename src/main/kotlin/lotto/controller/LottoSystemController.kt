65c769357edb4655795d13df45fd0556d6e58aa0
seoyeon <seoyeonn051@gmail.com>
2023-11-08 09:31:29 +0900

    fix: 로또 금액 입력, 로또 페이퍼 생성 기능 추가
     createLottoPaper() 함수 생성
     inputLottoPurchaseMount() 함수 기능 생성
     printLottoPaper() 함수 생성
     로또 당첨 번호, 보너스 번호 입력 기능 추가

--- src/main/kotlin/lotto/controller/LottoSystemController.kt
+++ src/main/kotlin/lotto/controller/LottoSystemController.kt
@@ -2,32 +2,50 @@ package lotto.controller
 
 import lotto.model.Lotto
 import lotto.model.LottoPaper
+import lotto.model.validation.InputValidation
 import lotto.view.LottoSystemView
 
 class LottoSystemController(lottoSystemView: LottoSystemView) {
     private var lottoSystemView: LottoSystemView
+    private lateinit var winningLottoNum: Lotto
 
     init {
         this.lottoSystemView = lottoSystemView
     }
 
     fun run() {
-
+        var lottoPaper = createLottoPaper()
+        printLottoPaper(lottoPaper)
     }
 
-    private fun inputLottoPurchaseMount() {
-        lottoSystemView.getLottoPurchaseMount()
+    private fun createLottoPaper(): LottoPaper {
+        var lottoLintCount: Int = inputLottoPurchaseMount()
+        return createLottoLineList(lottoLintCount)
     }
 
-    private fun createLottoPaper(lottoLineCount: Int): LottoPaper {
+    private fun inputLottoPurchaseMount(): Int {
+        var inputLottoPurchaseAmount = lottoSystemView.getLottoPurchaseMount()
+        return InputValidation().validateLottoPurchaseAmount(inputLottoPurchaseAmount)
+    }
+
+    private fun createLottoLineList(lottoLineCount: Int): LottoPaper {
         return LottoPaper(lottoLineCount)
     }
 
-    private fun inputWinningNumbers() {
-        lottoSystemView.getWinningLottoNum()
+    private fun printLottoPaper(lottoPaper: LottoPaper) {
+        lottoSystemView.printLottoNumList(lottoPaper)
     }
 
-    private fun inputBonusNumbers() {
-        lottoSystemView.getBonusLottoNum()
+    private fun inputWinningNumbers(): Lotto {
+        var inputWinningNum = lottoSystemView.getWinningLottoNum()
+        InputValidation().validateInputWinningLottoNum(inputWinningNum)
+
+        return Lotto(inputWinningNum.split(",").map { it.toInt() })
+    }
+
+    private fun inputBonusNumbers(): Int {
+        var inputBonusNum = lottoSystemView.getBonusLottoNum()
+        var bonusNum = InputValidation().validateInputBonusNum(inputBonusNum)
+        return bonusNum
     }
 }
