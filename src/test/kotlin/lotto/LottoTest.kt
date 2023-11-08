1be2e3d9dc24ba3ba542ab6b9cfbf27595daead9
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-11-08 18:00:21 +0900

    lotto 입력 testcase

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -13,6 +13,19 @@ class LottoTest {
         }
     }
 
+    @Test
+    fun `로또 번호의 개수가 6개 이하면 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1,2))
+        }
+    }
+
+    fun `공백을 입력하는 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf())
+        }
+    }
+    
     // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
@@ -20,6 +33,16 @@ class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
+
+    @Test
+    fun `로또 번호의 범위가 1~45 사이가 아니면 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1,2,3,4,78,3))
+        }
+    }
+
+
+
 
     // 아래에 추가 테스트 작성 가능
 }
