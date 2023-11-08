09528572b78e7d208e379f797a8b930087d5df9f
LEE <tpqms775@naver.com>
2023-11-08 19:00:02 +0900

    test : 로또 번호 비교 테스트 및 로또번호에 보너스 숫자가 있는지 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,10 +1,12 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
 class LottoTest {
+    private val lotto = Lotto(listOf(1,2,3,4,5,6))
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -12,13 +14,37 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
+    @Test
+    fun `겹치는 숫자가 5개인 경우`() {
+        val myNumbers = listOf(1, 2, 3, 4, 5, 7)
+        val result = lotto.compareNumber(myNumbers)
 
-    // 아래에 추가 테스트 작성 가능
+        assertEquals(5, result)
+    }
+    @Test
+    fun `겹치는 숫자가 없는 경우`() {
+        val myNumbers = listOf(7, 8, 9, 10, 11, 12)
+        val result = lotto.compareNumber(myNumbers)
+
+        assertEquals(0, result)
+    }
+    @Test
+    fun `로또번호에 보너스 숫자가 있는경우`() {
+        val number = 1
+        val result = lotto.compareBonusNumber(number)
+        assertTrue(result)
+    }
+
+    @Test
+    fun `로또번호에 보너스 숫자가 없는 경우`() {
+        val number = 7
+        val result = lotto.compareBonusNumber(number)
+        assertFalse(result)
+    }
 }
