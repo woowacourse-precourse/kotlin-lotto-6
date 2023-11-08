8dc532bf65be9e15091ef05003ccb22cbee3659e
crAy <craycoding@gmail.com>
2023-11-08 15:08:53 +0900

    fix : fix lotto test fail

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,10 +2,15 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_NUMBERS)
+        require(numbers.distinct().size == LOTTO_NUMBERS)
     }
 
     fun getLottoNumber(): List<Int> {
         return numbers.sorted()
+    }
+
+    companion object {
+        const val LOTTO_NUMBERS = 6
     }
 }
