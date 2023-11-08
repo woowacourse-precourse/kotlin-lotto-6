1892546c776e4391a74502e7e28029acf5cbdb66
gaeun <geun5744@gmail.com>
2023-11-08 22:12:59 +0900

    [REFACTOR(Output)] 변수 -> 함수로 리팩토링 & 접근제어자 변경

--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -8,11 +8,11 @@ class Output {
     fun printWinStatics(winStatics: Map<WinningType, Int>) {
         println(WINNING_STATISTICS_MESSAGE)
         println(DIVIDER_MESSAGE)
-        println("$FIFTH_WIN_MESSAGE${winStatics[WinningType.FIFTH]}개")
-        println("$FOURTH_WIN_MESSAGE${winStatics[WinningType.FOURTH]}개")
-        println("$THIRD_WIN_MESSAGE${winStatics[WinningType.THIRD]}개")
-        println("$SECOND_WIN_MESSAGE${winStatics[WinningType.SECOND]}개")
-        println("$FIRST_WIN_MESSAGE${winStatics[WinningType.FIRST]}개")
+        println(fifthWinMessage(winStatics[WinningType.FIFTH]))
+        println(fourthWinMessage(winStatics[WinningType.FOURTH]))
+        println(thirdWinMessage(winStatics[WinningType.THIRD]))
+        println(secondWinMessage(winStatics[WinningType.SECOND]))
+        println(firstWinMessage(winStatics[WinningType.FIRST]))
     }
 
     fun printTotalReturn(totalReturn: Int, purchaseAmount: Int) {
@@ -29,14 +29,15 @@ class Output {
     }
 
     companion object {
-        const val WINNING_STATISTICS_MESSAGE = "당첨 통계"
-        const val DIVIDER_MESSAGE = "---"
+        private const val WINNING_STATISTICS_MESSAGE = "당첨 통계"
+        private const val DIVIDER_MESSAGE = "---"
 
-        const val FIFTH_WIN_MESSAGE = "3개 일치 (5,000원) - "
-        const val FOURTH_WIN_MESSAGE = "4개 일치 (50,000원) - "
-        const val THIRD_WIN_MESSAGE = "5개 일치 (1,500,000원) - "
-        const val SECOND_WIN_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
-        const val FIRST_WIN_MESSAGE = "6개 일치 (2,000,000,000원) - "
+
+        fun fifthWinMessage(num:Int?) = "3개 일치 (5,000원) - ${num}개"
+        fun fourthWinMessage(num:Int?) = "4개 일치 (50,000원) - ${num}개"
+        fun thirdWinMessage(num:Int?) = "5개 일치 (1,500,000원) - ${num}개"
+        fun secondWinMessage(num:Int?) = "5개 일치, 보너스 볼 일치 (30,000,000원) - ${num}개"
+        fun firstWinMessage(num:Int?) = "6개 일치 (2,000,000,000원) - ${num}개"
 
         fun returnRateMessage(returnRate: String) = "총 수익률은 $returnRate%입니다."
         fun purchaseNumMessage(purchaseNum: Int) = "${purchaseNum}개를 구매했습니다."
