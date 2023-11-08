12336e4a5ddd754e031530bf6ee5aa8adcc3182f
DESKTOP-H9O9ENN\이상지 <isj4194@naver.com>
2023-11-08 21:46:42 +0900

    fix: Constants.kt 수정 - 구매갯수 Count와 로또번호 Number로 분리

--- src/main/kotlin/lotto/Constants.kt
+++ src/main/kotlin/lotto/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
         const val MIN_LOTTO_NUMBER = 1
         const val MAX_LOTTO_NUMBER = 45
         const val INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.\n"
-        const val OUTPUT_PURCHASE_NUMBER_MESSAGE = "%d개를 구매했습니다.\n"
+        const val OUTPUT_PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.\n"
         const val INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.\n"
         const val INPUT_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.\n"
         const val OUTPUT_WINNING_STATISTICS_MESSAGE = "당첨 통계\n"
@@ -19,8 +19,8 @@ class Constants {
         const val OUTPUT_PROFIT_PERCENTAGE_MESSAGE = "총 수익률은 %.1f%%입니다."
         const val ERROR_INVALID_PURCHASE_AMOUNT_MESSAGE = "[ERROR] 알맞는 금액을 입력해주세요.(구입 금액은 1,000원 단위로 입력)"
         const val ERROR_OUT_OF_RANGE_NUMBER_MESSAGE = "[ERROR] 1~45 사이의 숫자를 입력해 주세요."
-        const val ERROR_INVALID_PURCHASE_NUMBER_MESSAGE = "[ERROR] 총 6개의 숫자를 입력해 주세요."
-        const val ERROR_DUPLICATE_WINNING_NUMBER_MESSAGE = "[ERROR] 당첨 번호는 서로 다른 숫자여야합니다."
-        const val ERROR_DUPLICATE_WINNING_AND_BONUS_NUMBER_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
+        const val ERROR_INVALID_NUMBER_MESSAGE = "[ERROR] 총 6개의 숫자를 입력해 주세요."
+        const val ERROR_OVERLAP_NUMBER_MESSAGE = "[ERROR] 당첨 번호는 서로 다른 숫자여야합니다."
+        const val ERROR_OVERLAP_TOTAL_NUMBER_MESSAGE = "[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."
     }
 }
