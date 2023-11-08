2fa35356f1ec17401f994db2f528ee3c108f7914
Jbrochan <hyeongchanj@gmail.com>
2023-11-08 17:29:17 +0900

    feat: 로또 번호 중복 예외 처리 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,9 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        if (numbers.size != numbers.distinct().size) {
+            throw IllegalArgumentException("[ERROR] 로또 번호 중복.")
+        }
     }
 
     fun getNumbers(): List<Int> {
