8d28e0db58a507f2498f3e5b0c674ddbfacf4bfc
이빵주 <yungju9982@naver.com>
2023-11-08 23:15:14 +0900

    [test] 금액에 따른 로또 티켓 개수 테스트코드 작성

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -1,4 +1,20 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 class LottoMachineTest {
+    @Test
+    fun `금액에 따른 로또 티켓 개수`() {
+        // Given
+        val money = 5000
+
+        // When
+        val tickets = LottoMachine.buyTickets(money)
+
+        // Then
+        assertEquals(5, tickets.size)
+    }
+
 }
