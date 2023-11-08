5903b9bc138fa9f67dcc2d8a110669a352af7631
SeungGu Kang <alex9366@naver.com>
2023-11-08 23:00:04 +0900

    test: 테스트 코드 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.model.Lotto
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -13,7 +14,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -22,4 +22,19 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `toString은 올바른 형식의 문자열을 반환해야 한다`() {
+        val numbers = listOf(8, 27, 34, 4, 19, 10)
+        val lotto = Lotto(numbers)
+        val expectedFormat = "[8, 27, 34, 4, 19, 10]"
+        assertEquals(expectedFormat, lotto.toString())
+    }
+
+    @Test
+    fun `toSet은 로또 번호를 포함하는 집합을 반환해야 한다`() {
+        val numbers = listOf(12, 7, 26, 33, 45, 11)
+        val lotto = Lotto(numbers)
+        val expectedSet = setOf(12, 7, 26, 33, 45, 11)
+        assertEquals(expectedSet, lotto.toSet())
+    }
 }
