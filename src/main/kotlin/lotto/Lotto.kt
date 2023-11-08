afbda788145c845af2b9ceee8ba886082573d06c
EJ-pro <hsshss2522@naver.com>
2023-11-08 16:50:57 +0900

    fix(ticket): 로또 티켓 생성 예외 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { throw IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.")}
+        require(numbers.toSet().size == 6) { throw IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.")}
+        require(numbers.all { it in 1..45 }) { throw IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.")}
     }
     fun isSameAs(other: Lotto): Boolean {
         return numbers.toSet() == other.numbers.toSet()
