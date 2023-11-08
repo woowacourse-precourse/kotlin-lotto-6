4e8d2d522f452e751a0a6a7d9087a0fefb9e23d1
nighttwo1 <ys12j1@gmail.com>
2023-11-08 17:56:17 +0900

    refactor: input 관리 클래스들이 모두 validate외의 기능을 수행하고 있지 않아 object로 변경하여 작성

--- src/main/kotlin/lotto/presentation/InputMoney.kt
+++ src/main/kotlin/lotto/presentation/InputMoney.kt
@@ -5,24 +5,22 @@ import lotto.exception.NonDivisibleLottoPurchaseAmountException
 import lotto.exception.NonNumberLottoPurchaseAmountException
 import lotto.exception.NonPositiveLottoPurchaseAmountException
 
-class InputMoney {
-    companion object {
-        fun validateInputMoney(input: String) {
-            validateInputMoneyIsNumber(input)
-            validatePurchaseMoney(input.toInt())
-        }
+object InputMoney {
+    fun validateInputMoney(input: String) {
+        validateInputMoneyIsNumber(input)
+        validatePurchaseMoney(input.toInt())
+    }
 
-        private fun validateInputMoneyIsNumber(inputMoney: String) {
-            inputMoney.toIntOrNull() ?: throw NonNumberLottoPurchaseAmountException()
-        }
+    private fun validateInputMoneyIsNumber(inputMoney: String) {
+        inputMoney.toIntOrNull() ?: throw NonNumberLottoPurchaseAmountException()
+    }
 
-        private fun validatePurchaseMoney(inputMoney: Int) {
-            require(inputMoney >= 1000) {
-                throw NonPositiveLottoPurchaseAmountException()
-            }
-            require(inputMoney % Lotto.LOTTO_PRICE_PER_GAME == 0) {
-                throw NonDivisibleLottoPurchaseAmountException()
-            }
+    private fun validatePurchaseMoney(inputMoney: Int) {
+        require(inputMoney >= 1000) {
+            throw NonPositiveLottoPurchaseAmountException()
+        }
+        require(inputMoney % Lotto.LOTTO_PRICE_PER_GAME == 0) {
+            throw NonDivisibleLottoPurchaseAmountException()
         }
     }
 }
