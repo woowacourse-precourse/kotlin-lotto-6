9d05dd27c6bd829223ef303dae58bf274ef99451
sungsik-git <kss950708@gamil.com>
2023-11-08 13:29:56 +0900

    test: 로또 번호와 당첨 번호 비교한 결과 테스트 작성

---
+++ src/test/kotlin/lotto/RevenueTest.kt
@@ -0,0 +1,10 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import output.UserInterface
+
+class RevenueTest {
+
+}
