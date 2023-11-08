0492bd554b0bc52726127902178b50dfe7cd78d0
eunsxx <88652027+eunsxx@users.noreply.github.com>
2023-11-08 16:33:54 +0900

    [Feat] #04 - input winner number and bonus number

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,29 +6,66 @@ import java.util.logging.Logger
 import kotlin.contracts.contract
 
 fun main() {
-    println("구입금액을 입력해 주세요.")
-    var money: Int
-    var buyNum: Int
-    var input: String
-    var lottos = mutableListOf<Lotto>()
+    val lottos = inputAndBuyLottos()
+    printLottoNum(lottos)
+    val winnerNum = inputWinnerNum()
+    println("당첨 번호: $winnerNum")
+}
 
+fun inputAndBuyLottos(): MutableList<Lotto> {
     while (true) {
         try {
-            input = userInput()
-            money = ensureInt(input)
-            buyNum = ensureBuyNum(money)
-            println("${buyNum}개를 구매했습니다.")
-            lottos = buyLottos(buyNum)
-            printLottoNum(lottos)
-
-
-            break // If no error occurs, exit the loop
+            println("구입금액을 입력해 주세요.")
+            val input = readLine() ?: throw IllegalArgumentException("[ERROR] 입력을 받지 못했습니다.")
+            val money = ensureInt(input)
+            val buyNum = ensureBuyNum(money)
+            return buyLottos(buyNum)
         } catch (e: IllegalArgumentException) {
-            println(e.message) // Print the error message and prompt again
+            println(e.message)
         }
     }
 }
 
+fun inputWinnerNum(): List<Int> {
+    while (true) {
+        try {
+            return winnerNumInput()
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun winnerNumInput(): List<Int> {
+    println("당첨 번호를 입력해 주세요.")
+    var input = userInput()
+    val numberList = input.split(",").map { it.trim()}
+
+    if (numberList.size != 6) {
+        throw IllegalArgumentException("[ERROR] 6개의 숫자를 입력해야 합니다.")
+    }
+
+    val numbers = numberList.map {
+        ensureInt(it)
+    }
+
+    if (numbers.any { it !in 1..45 }) {
+        throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
+
+    println("보너스 번호를 입력해 주세요")
+    input = userInput()
+    val bonus = ensureInt(input)
+    if (bonus !in 1..45 ) {
+        throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
+
+    if (numbers.contains(bonus)) {
+        throw IllegalArgumentException("[ERROR] 당첨 번호와 중복될 수 없습니다.")
+    }
+
+    return numbers + bonus
+}
 fun printLottoNum(lottos: MutableList<Lotto>) {
     lottos.forEach { lotto ->
         println(lotto)
@@ -41,6 +78,8 @@ fun buyLottos(buyNum: Int) : MutableList<Lotto> {
         val sortedNumbers = sortingNum(generateLottoNum())
         lottos.add(Lotto(sortedNumbers))
     }
+
+    println("${buyNum}개를 구매했습니다.")
 
     return lottos
 }
