f6658e4a7931ea4a621bcae84d72836abda57a76
Jbrochan <hyeongchanj@gmail.com>
2023-11-08 17:58:11 +0900

    feat: 보너스 번호 입력 단위 테스트 변경
    - 보너스 번호 입력시 숫자가 아닌 문자가 입력됐을 때를 정확하게 명시하고 이에 따른 테스트 케이스 부여

--- src/test/kotlin/lotto/UnitTest.kt
+++ src/test/kotlin/lotto/UnitTest.kt
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test
 class UnitTest : NsTest() {
 
     @Test
-    fun `보너스 번호 테스트`() {
+    fun `보너스 번호에 숫자가 아닌 문자가 들어간 경우 예외가 발생한다`() {
         camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest {
-            runException("8000", "1,2,3,4,5,6", "512")
+            runException("8000", "1,2,3,4,5,6", "512abs")
             Assertions.assertThat(output()).contains(ERROR_MESSAGE)
         }
     }
