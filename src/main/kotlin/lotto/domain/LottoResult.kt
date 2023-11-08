101c602bb610a729e1c508c6e64dd357cb74296a
bak <bak4017@gmail.com>
2023-11-08 15:15:21 +0900

    refactor(lotto): 총 수익률 단위 100%로 수정

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -3,8 +3,6 @@ package lotto.domain
 class LottoResult {
     fun calculateRateOfReturn(results: List<LottoRank>, purchasePrice: Int): Double {
         val totalPrizeMoney = results.sumOf { it.prizeMoney.replace(",", "").toInt() }
-        val rateOfReturn = (totalPrizeMoney.toDouble() - purchasePrice) / purchasePrice
-
-        return if (rateOfReturn < 0) rateOfReturn * 100 else rateOfReturn
+        return (totalPrizeMoney.toDouble() - purchasePrice) / purchasePrice * 100
     }
 }
