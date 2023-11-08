b84b0c5011c4b89ad8d32a9d4bcaeb5a13e09d72
nighttwo1 <ys12j1@gmail.com>
2023-11-08 18:28:39 +0900

    chore: 출력 결과 형식 변경

--- src/main/kotlin/lotto/presentation/Constants.kt
+++ src/main/kotlin/lotto/presentation/Constants.kt
@@ -6,4 +6,4 @@ const val LOTTO_PURCHASE_RESPONSE_MESSAGE = "개를 구매했습니다."
 const val LOTTO_WINNING_NUMBER_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요."
 const val LOTTO_BONUS_NUMBER_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요."
 
-const val PLAYER_LOTTO_RESULT_RESPONSE_MESSAGE = "당첨 통계\n---\n"
+const val PLAYER_LOTTO_RESULT_RESPONSE_MESSAGE = "당첨 통계\n---"
