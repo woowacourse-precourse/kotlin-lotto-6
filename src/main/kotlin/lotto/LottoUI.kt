d22c16331c996a8770944ca71c9a17115d9bf0d9
Jtility <leejewon97@gmail.com>
2023-11-08 18:14:42 +0900

    feat(LottoUI): 구입 금액 입력
    정상적인 구입 금액을 String으로 return할 메서드 inputBuyPrice 구현

--- src/main/kotlin/lotto/LottoUI.kt
+++ src/main/kotlin/lotto/LottoUI.kt
@@ -1,10 +1,23 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
 import lotto.enumeration.PrintBuy
 
 class LottoUI {
     fun printBuyPrice() {
         println(PrintBuy.PRICE.value)
+    }
+
+    fun inputBuyPrice(): String {
+        val buyPrice = Console.readLine()
+        try {
+            checkInvalidBuyPrice(buyPrice)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            printBuyPrice()
+            return inputBuyPrice()
+        }
+        return buyPrice
     }
 
 }
