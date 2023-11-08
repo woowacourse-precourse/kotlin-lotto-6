5fbd5ab17d248925ee5bc291eff8ff50b632d186
gaeun <geun5744@gmail.com>
2023-11-08 18:13:30 +0900

    [TEST(ValidationTest)] 보너스 번호 입력 테스트 구현

--- src/test/kotlin/lotto/model/ValidationTest.kt
+++ src/test/kotlin/lotto/model/ValidationTest.kt
@@ -71,5 +71,31 @@ class ValidationTest {
         }
     }
 
+    @Test
+    @DisplayName("보너스 번호 입력 테스트")
+    fun `숫자가 아닌 경우 예외 발생`() {
+        val input = "횟수"
+        assertThrows<IllegalArgumentException> {
+            Validation.getBonusNum(input)
+        }
+    }
+
+    @Test
+    @DisplayName("보너스 번호 입력 테스트")
+    fun `1 ~ 45 사이의 숫자가 아닌 경우 예외 발생`() {
+        val input = "1,2"
+        assertThrows<IllegalArgumentException> {
+            Validation.getBonusNum(input)
+        }
+    }
+
+    @Test
+    @DisplayName("보너스 번호 입력 테스트")
+    fun `여러 숫자를 입력한 경우 예외 발생`() {
+        val input = "1,2,3,4,5,5"
+        assertThrows<IllegalArgumentException> {
+            Validation.getBonusNum(input)
+        }
+    }
 
 }
