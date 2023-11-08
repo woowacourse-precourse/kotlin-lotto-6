fe1f2ed8e2a0e94297d105e6130c47af7711d5ff
aprilgom <aprilgom@users.noreply.github.com>
2023-11-08 11:48:38 +0900

    feat: 에러 메시지 출력

--- src/main/kotlin/lotto/Controller.kt
+++ src/main/kotlin/lotto/Controller.kt
@@ -10,7 +10,7 @@ class Controller {
     val lottoBuyer by lazy { LottoBuyer(budget) }
     val lottoList by lazy { lottoBuyer.lottoList }
     val winningNumber by lazy { Input.getWinningNumber() }
-    val bonusNumber by lazy { Input.getBonusNumber() }
+    val bonusNumber by lazy { Input.getBonusNumber(winningNumber) }
     val prizeCalculator by lazy { PrizeCalculator(lottoList, winningNumber, bonusNumber) }
     val sumOfWinningPrize by lazy { prizeCalculator.getSumOfWinningPrize() }
     val rate by lazy { Rate(sumOfWinningPrize, budget, 10) }
--- src/main/kotlin/lotto/io/Input.kt
+++ src/main/kotlin/lotto/io/Input.kt
@@ -2,39 +2,48 @@ package lotto.io
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.Constants
+import lotto.ErrorMessage
+import lotto.ErrorMessage.BONUS_NUMBER_INTERSECTED
+import lotto.ErrorMessage.BUDGET_NOT_DIVIDED
+import lotto.ErrorMessage.BUDGET_UNDER_ZERO
+import lotto.ErrorMessage.EMPTY_INPUT
+import lotto.ErrorMessage.INVALID_LOTTO_RANGE
+import lotto.ErrorMessage.INVALID_NUMBER
+import lotto.ErrorMessage.LACK_OF_NUMBER
 
 object Input {
     private fun getInput(): String {
         val input = Console.readLine().trim()
-        require(input.isNotEmpty())
+        require(input.isNotEmpty()) { ErrorMessage.print(EMPTY_INPUT) }
         return input
     }
     private fun getNumberInput(): Int {
         val input = getInput()
-        require(isNumeric(input))
+        require(isNumeric(input)) { ErrorMessage.print(INVALID_NUMBER) }
         return input.toInt()
     }
     fun getBudget(): Int = repeatUntilGetValidInput {
         val budget = getNumberInput()
-        require(budget > 0)
-        require(budget % Constants.PRICE_OF_LOTTO == 0)
+        require(budget > 0) { ErrorMessage.print(BUDGET_UNDER_ZERO) }
+        require(budget % Constants.PRICE_OF_LOTTO == 0) { ErrorMessage.print(BUDGET_NOT_DIVIDED) }
         budget
     }
     fun getWinningNumber(): List<Int> = repeatUntilGetValidInput {
         val input = getInput()
         val splitted = input.split(",")
-        require(splitted.size == Constants.LOTTO_COUNT)
+        require(splitted.size == Constants.LOTTO_COUNT) { ErrorMessage.print(LACK_OF_NUMBER) }
         splitted.map {
-            require(isNumeric(it))
+            require(isNumeric(it)) { ErrorMessage.print(INVALID_NUMBER) }
             val num = it.toInt()
-            require(isValidLottoNum(num))
+            require(isValidLottoNum(num)) { ErrorMessage.print(INVALID_LOTTO_RANGE) }
             num
         }
     }
-    fun getBonusNumber(): Int = repeatUntilGetValidInput {
+    fun getBonusNumber(winningNumber: List<Int>): Int = repeatUntilGetValidInput {
         val bonusNumber = getNumberInput()
         require(bonusNumber > 0)
-        require(isValidLottoNum(bonusNumber))
+        require(isValidLottoNum(bonusNumber)) { ErrorMessage.print(INVALID_LOTTO_RANGE) }
+        require(!winningNumber.contains(bonusNumber)) { ErrorMessage.print(BONUS_NUMBER_INTERSECTED) }
         bonusNumber
     }
     private fun isValidLottoNum(num: Int): Boolean = num in Constants.MIN_LOTTO_NUMBER..Constants.MAX_LOTTO_NUMBER
