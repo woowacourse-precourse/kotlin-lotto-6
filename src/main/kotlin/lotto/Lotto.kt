20e5e54a9bcccc0c715c73a60dab5aa2afbbdeb2
Jtility <leejewon97@gmail.com>
2023-11-08 22:15:39 +0900

    test(Lotto): 중복이 들어왔을 경우 불허

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct().size == 6)
     }
 
     fun getNumbers(): List<Int> {
