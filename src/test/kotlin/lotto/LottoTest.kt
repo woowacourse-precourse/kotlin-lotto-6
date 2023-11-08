071bebb68eda764441864ce22896f50ac7a0c0b6
이홍진\iit80 <hong80714@gmail.com>
2023-11-08 21:44:15 +0900

    [test, docs] test코드 추가, 추가 진행사항 체크
    test : 로또 번호 범위에 대해 테스트 코드 추가
    docs : 진행 후 체크, 취소선

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -20,5 +20,10 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호가 1 ~ 45 사이에 있지 않으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(mutableListOf(1, 2, 3, 4, 50, 6)) // 50은 1 ~ 45 범위 밖의 숫자
+        }
+    }
 }
