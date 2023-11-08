df294c9414d5fabd37437dba4f20a23cb3322d73
iiolo <alohalo98@naver.com>
2023-11-08 22:56:41 +0900

    feat: 6번 기능 구현

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -3,9 +3,11 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import lotto.view.Input
 import lotto.view.Output
+import java.math.BigDecimal
+import java.math.BigInteger
 
 class LottoGame {
-    private var matchCountList = mutableListOf(0,0,0,0,0)
+    private var matchCountList = mutableListOf(0, 0, 0, 0, 0)
 
     fun start() {
         val money = Input().inputMoney().toInt()
@@ -17,9 +19,12 @@ class LottoGame {
         val winningNum = Input().inputWinningNum()
         val bonusNum = Input().inputBonusNum()
 
-        lottoList.forEach { checkLottoAndWinningNumMatch(it,winningNum,bonusNum) }
+        lottoList.forEach { checkLottoAndWinningNumMatch(it, winningNum, bonusNum) }
 
         Output().outputMatchCount(matchCountList)
+
+        val profitability = getProfitability(money)
+        Output().outputProfitability(profitability)
 
     }
 
@@ -45,5 +50,18 @@ class LottoGame {
             5 -> if (lotto.contains(bonusNumToInt)) matchCountList[3]++ else matchCountList[2]++
             6 -> matchCountList[4]++
         }
+    }
+
+    private fun getProfitability(money: Int): BigDecimal {
+        val priceList = listOf(5000, 50000, 1500000, 30000000, 2000000000)
+        var sum = BigInteger("0")
+        for (i in matchCountList.indices) {
+            val temp = BigInteger(matchCountList[i].toString()) * BigInteger(priceList[i].toString())
+            sum += temp
+        }
+
+        val tempProfitability = BigDecimal(sum).divide(BigDecimal(money.toString()))
+        return tempProfitability.multiply(BigDecimal("100")).setScale(1, BigDecimal.ROUND_HALF_UP)
+
     }
 }
--- src/main/kotlin/lotto/view/Output.kt
+++ src/main/kotlin/lotto/view/Output.kt
@@ -1,5 +1,7 @@
 package lotto.view
 
+import java.math.BigDecimal
+
 class Output {
 
     fun outputLottos(lottoList: List<List<Int>>) {
@@ -15,6 +17,10 @@ class Output {
         println("5개 일치 (1,500,000원) - ${matchCountList[2]}개")
         println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${matchCountList[3]}개")
         println("6개 일치 (2,000,000,000원) - ${matchCountList[4]}개")
+    }
+
+    fun outputProfitability(profitability : BigDecimal){
+        println("총 수익률은 $profitability%입니다.")
     }
 }
 
